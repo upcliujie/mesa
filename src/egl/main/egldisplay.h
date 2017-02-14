2e34d2b714d61b0e9e25a82022006d65005b8699
Eric Engestrom <eric@engestrom.ch>
2017-02-14 21:14:05 +0000

    egl: implement EGL_KHR_display_reference on all platforms
    https://www.khronos.org/registry/EGL/extensions/KHR/EGL_KHR_display_reference.txt
    
    Signed-off-by: Eric Engestrom <eric@engestrom.ch>

--- src/egl/main/egldisplay.h
+++ src/egl/main/egldisplay.h
@@ -193,6 +193,10 @@ struct _egl_display
 
    EGLSetBlobFuncANDROID BlobCacheSet;
    EGLGetBlobFuncANDROID BlobCacheGet;
+
+   /* KHR_display_reference */
+   EGLBoolean TrackReferences;
+   EGLint RefCount;
 };
 
 
