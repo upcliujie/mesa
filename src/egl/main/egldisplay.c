eceb46d9e237be3d2d11dbcfa6a5589ca5430295
Eric Engestrom <eric@engestrom.ch>
2017-04-08 23:52:08 +0200

    egl: split drm display attrib list parsing to its own function
    Signed-off-by: Eric Engestrom <eric@engestrom.ch>

--- src/egl/main/egldisplay.c
+++ src/egl/main/egldisplay.c
@@ -580,17 +580,41 @@ _eglGetXcbDisplay(xcb_connection_t *native_display,
 #endif /* HAVE_XCB_PLATFORM */
 
 #ifdef HAVE_DRM_PLATFORM
+static EGLBoolean
+_eglParseDRMDisplayAttribList(_EGLDisplay *display,
+                              const EGLAttrib *attrib_list)
+{
+   if (attrib_list == NULL) {
+      return EGL_TRUE;
+   }
+
+   for (int i = 0; attrib_list[i] != EGL_NONE; i += 2) {
+      EGLint attrib = attrib_list[i];
+      EGLint value = attrib_list[i + 1];
+
+      switch (attrib) {
+      default:
+         _eglError(EGL_BAD_ATTRIBUTE, "eglGetPlatformDisplay");
+         return EGL_FALSE;
+      }
+   }
+
+   return EGL_TRUE;
+}
+
 _EGLDisplay*
 _eglGetGbmDisplay(struct gbm_device *native_display,
                   const EGLAttrib *attrib_list)
 {
-   /* EGL_MESA_platform_gbm recognizes no attributes. */
-   if (attrib_list != NULL && attrib_list[0] != EGL_NONE) {
-      _eglError(EGL_BAD_ATTRIBUTE, "eglGetPlatformDisplay");
+   _EGLDisplay *dpy = _eglFindDisplay(_EGL_PLATFORM_DRM,
+                                      native_display,
+                                      attrib_list);
+
+   if (!_eglParseDRMDisplayAttribList(dpy, attrib_list)) {
       return NULL;
    }
 
-   return _eglFindDisplay(_EGL_PLATFORM_DRM, native_display, attrib_list);
+   return dpy;
 }
 #endif /* HAVE_DRM_PLATFORM */
 
