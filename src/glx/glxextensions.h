f3cd5519da82175335dca40b8984c6cb9fed6b35
Adam Jackson <ajax@redhat.com>
2017-11-14 15:13:06 -0500

    glx: Implement GLX_EXT_no_config_context
    This is the GLX counterpart to EGL_KHR_no_config_context. Contexts may
    now be created without reference to an fbconfig, in which case it is
    treated as compatible with any fbconfig (and thus any GLX drawable).
    
    Khronos: https://github.com/KhronosGroup/OpenGL-Registry/pull/102

--- src/glx/glxextensions.h
+++ src/glx/glxextensions.h
@@ -51,6 +51,7 @@ enum
    EXT_fbconfig_packed_float_bit,
    EXT_framebuffer_sRGB_bit,
    EXT_import_context_bit,
+   EXT_no_config_context_bit,
    EXT_swap_control_bit,
    EXT_swap_control_tear_bit,
    EXT_texture_from_pixmap_bit,
