b01e09d7acb6c9ec40e3e3fb484ff836522e73a7
Jordan Justen <jordan.l.justen@intel.com>
2019-07-19 15:37:35 -0700

    iris/blorp: Support compute for slow clears with INTEL_DEBUG=blocs
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/drivers/iris/iris_state.c
+++ src/gallium/drivers/iris/iris_state.c
@@ -1357,6 +1357,8 @@ iris_bind_blend_state(struct pipe_context *ctx, void *state)
    struct iris_blend_state *cso = state;
 
    ice->state.cso_blend = cso;
+   ice->state.blend_enables = cso ? cso->blend_enables : 0;
+   ice->state.color_write_enables = cso ? cso->color_write_enables : 0;
 
    ice->state.dirty |= IRIS_DIRTY_PS_BLEND;
    ice->state.dirty |= IRIS_DIRTY_BLEND_STATE;
