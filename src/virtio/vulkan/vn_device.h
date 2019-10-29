ef4aef31322b03418bce69b77b7e7f6765b060be
Chia-I Wu <olvaffe@gmail.com>
2019-10-29 12:03:53 -0700

    venus: initial support for vkCreateInstance
    Connect to the renderer.
    
    Signed-off-by: Chia-I Wu <olvaffe@gmail.com>

--- src/virtio/vulkan/vn_device.h
+++ src/virtio/vulkan/vn_device.h
@@ -13,8 +13,11 @@
 
 #include "vn_common.h"
 
+#include "vn_cs.h"
+#include "vn_renderer.h"
+
 struct vn_instance {
-   struct vk_object_base base;
+   struct vn_cs_object base;
 
    VkAllocationCallbacks allocator;
 
@@ -24,8 +27,24 @@ struct vn_instance {
    struct vn_instance_dispatch_table dispatch;
    struct vn_physical_device_dispatch_table physical_device_dispatch;
    struct vn_device_dispatch_table device_dispatch;
+
+   struct vn_renderer *renderer;
+   struct vn_renderer_info renderer_info;
+   uint32_t renderer_version;
+
+   mtx_t cs_mutex;
+   struct vn_cs cs;
+   struct {
+      struct vn_renderer_bo *bo;
+      size_t size;
+      size_t used;
+      void *ptr;
+   } cs_reply;
 };
-VK_DEFINE_HANDLE_CASTS(vn_instance, base, VkInstance, VK_OBJECT_TYPE_INSTANCE)
+VK_DEFINE_HANDLE_CASTS(vn_instance,
+                       base.base,
+                       VkInstance,
+                       VK_OBJECT_TYPE_INSTANCE)
 
 struct vn_physical_device {
    struct vk_object_base base;
@@ -60,5 +79,47 @@ VK_DEFINE_HANDLE_CASTS(vn_command_buffer,
                        base,
                        VkCommandBuffer,
                        VK_OBJECT_TYPE_COMMAND_BUFFER)
+
+static inline struct vn_cs *
+vn_instance_lock_cs(struct vn_instance *instance)
+{
+   mtx_lock(&instance->cs_mutex);
+   return &instance->cs;
+}
+
+static inline void
+vn_instance_unlock_cs(struct vn_instance *instance)
+{
+   assert(mtx_trylock(&instance->cs_mutex) == thrd_busy);
+   mtx_unlock(&instance->cs_mutex);
+}
+
+bool
+vn_instance_grow_cs_reply_locked(struct vn_instance *instance, size_t size);
+
+static inline struct vn_renderer_bo *
+vn_instance_alloc_cs_reply_locked(struct vn_instance *instance,
+                                  size_t size,
+                                  size_t *offset,
+                                  void **ptr)
+{
+   if (unlikely(instance->cs_reply.used + size > instance->cs_reply.size)) {
+      if (!vn_instance_grow_cs_reply_locked(instance, size))
+         return NULL;
+   }
+
+   *offset = instance->cs_reply.used;
+   *ptr = instance->cs_reply.ptr + instance->cs_reply.used;
+   instance->cs_reply.used += size;
+
+   return vn_renderer_bo_ref(instance->cs_reply.bo);
+}
+
+static inline void
+vn_instance_free_cs_reply(struct vn_instance *instance,
+                          struct vn_renderer_bo *bo)
+{
+   vn_renderer_bo_unref(bo, &instance->allocator);
+}
 
 #endif /* VN_DEVICE_H */
