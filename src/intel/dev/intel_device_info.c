3d8fb2da8002cd376ca5f40b15496024884c522c
Jordan Justen <jordan.l.justen@intel.com>
2019-12-09 10:51:40 -0800

    intel: Add device info for DG2
    v2: Drop is_dg2 (Lionel)
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/intel/dev/intel_device_info.c
+++ src/intel/dev/intel_device_info.c
@@ -1025,6 +1025,23 @@ static const struct intel_device_info intel_device_info_sg1 = {
    GFX12_DG1_SG1_FEATURES,
 };
 
+#define XEHP_FEATURES                                           \
+   GFX12_FEATURES(0, 0, 0),                                     \
+   .verx10 = 125,                                               \
+   .has_llc = false,                                            \
+   .has_local_mem = true,                                       \
+   .has_aux_map = false,                                        \
+   .simulator_id = 29
+
+#define DG2_FEATURES                                            \
+   XEHP_FEATURES,                                               \
+   .platform = INTEL_PLATFORM_DG2,                              \
+   .has_lsc = true
+
+UNUSED static const struct intel_device_info intel_device_info_dg2 = {
+   DG2_FEATURES,
+};
+
 static void
 reset_masks(struct intel_device_info *devinfo)
 {
