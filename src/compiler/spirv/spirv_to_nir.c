821fc97f67ccc8f5aa5e0b0ad8cef82596cf22d4
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-02-28 17:10:03 +0100

    spirv: record which inputs are pointers to global memory

--- src/compiler/spirv/spirv_to_nir.c
+++ src/compiler/spirv/spirv_to_nir.c
@@ -5665,8 +5665,13 @@ vtn_emit_kernel_entry_point_wrapper(struct vtn_builder *b,
 
    nir_call_instr *call = nir_call_instr_create(b->nb.shader, entry_point);
 
+   b->shader->info.cs.global_inputs = 0;
    for (unsigned i = 0; i < entry_point->num_params; ++i) {
       struct vtn_type *param_type = b->entry_point->func->type->params[i];
+
+      if (param_type->base_type == vtn_base_type_pointer &&
+          param_type->storage_class == SpvStorageClassCrossWorkgroup)
+        b->shader->info.cs.global_inputs |= BITFIELD64_BIT(i);
 
       /* consider all pointers to function memory to be parameters passed
        * by value
