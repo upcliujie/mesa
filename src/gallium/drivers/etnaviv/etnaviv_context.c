679e9b88de39859845ac6fbc9b6eec5b8fe63540
Christian Gmeiner <christian.gmeiner@gmail.com>
2020-03-20 20:23:11 +0100

    etnaviv: add support for KHR_partial_update
    Implement ->set_damage_region() region to support partial updates.
    Tested with weston.
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- src/gallium/drivers/etnaviv/etnaviv_context.c
+++ src/gallium/drivers/etnaviv/etnaviv_context.c
@@ -285,8 +285,15 @@ etna_draw_vbo(struct pipe_context *pctx, const struct pipe_draw_info *info)
       .front_ccw = ctx->rasterizer->front_ccw,
    };
 
-   if (pfb->cbufs[0])
+   if (pfb->cbufs[0]) {
+      const struct etna_surface *surf = etna_surface(pfb->cbufs[0]);
+      const struct etna_resource *rsc = etna_resource(surf->prsc);
+
       key.frag_rb_swap = !!translate_pe_format_rb_swap(pfb->cbufs[0]->format);
+
+      if (rsc->damage_used)
+         ctx->dirty |= ETNA_DIRTY_SCISSOR;
+   }
 
    if (!etna_get_vs(ctx, key) || !etna_get_fs(ctx, key)) {
       BUG("compiled shaders are not okay");
