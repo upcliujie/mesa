d796f35da3f2c458cf20fe0235b801f6dede478e
Jason Ekstrand <jason@jlekstrand.net>
2020-04-07 17:12:58 -0500

    nir: Make opt_shrink_load more idiomatic

--- src/compiler/nir/nir_opt_shrink_load.c
+++ src/compiler/nir/nir_opt_shrink_load.c
@@ -24,18 +24,36 @@
 #include "nir.h"
 
 static bool
-opt_shrink_load(nir_intrinsic_instr *instr)
+opt_shrink_load_impl(nir_function_impl *impl)
 {
    bool progress = false;
 
-   if (instr->intrinsic == nir_intrinsic_load_push_constant) {
-      unsigned mask = nir_ssa_def_components_read(&instr->dest.ssa);
+   nir_foreach_block(block, impl) {
+      nir_foreach_instr(instr, block) {
+         if (instr->type != nir_instr_type_intrinsic)
+            continue;
 
-      if (instr->num_components > util_last_bit(mask)) {
-         instr->num_components = util_last_bit(mask);
-         instr->dest.ssa.num_components = instr->num_components;
-         progress = true;
+         nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+         if (intrin->intrinsic != nir_intrinsic_load_push_constant)
+            continue;
+
+         unsigned mask = nir_ssa_def_components_read(&intrin->dest.ssa);
+
+         if (intrin->num_components > util_last_bit(mask)) {
+            intrin->num_components = util_last_bit(mask);
+            intrin->dest.ssa.num_components = intrin->num_components;
+            progress = true;
+         }
       }
+   }
+
+   if (progress) {
+      nir_metadata_preserve(impl, nir_metadata_block_index |
+                                  nir_metadata_dominance);
+   } else {
+#ifndef NDEBUG
+      impl->valid_metadata &= ~nir_metadata_not_properly_reset;
+#endif
    }
 
    return progress;
@@ -47,20 +65,8 @@ nir_opt_shrink_load(nir_shader *shader)
    bool progress = false;
 
    nir_foreach_function(function, shader) {
-      if (!function->impl)
-         continue;
-
-      nir_foreach_block(block, function->impl) {
-         nir_foreach_instr(instr, block) {
-            if (instr->type != nir_instr_type_intrinsic)
-               continue;
-
-            progress |= opt_shrink_load(nir_instr_as_intrinsic(instr));
-         }
-      }
-
-      nir_metadata_preserve(function->impl, nir_metadata_block_index |
-                                            nir_metadata_dominance);
+      if (!function->impl && opt_shrink_load_impl(function->impl))
+         progress = true;
    }
 
    return progress;
