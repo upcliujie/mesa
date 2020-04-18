ed4e885187e94dd6613d8ce24f01741418f6e186
Jason Ekstrand <jason@jlekstrand.net>
2020-04-18 10:23:25 -0500

    intel: Run nir_opt_rematerialize_compares before out-of-SSA
    Shader-db results on ICL:
    
        total instructions in shared programs: 16043613 -> 16043197 (<.01%)
        instructions in affected programs: 19888 -> 19472 (-2.09%)
        helped: 52
        HURT: 0
        helped stats (abs) min: 1 max: 20 x̄: 8.00 x̃: 6
        helped stats (rel) min: 0.06% max: 4.27% x̄: 2.29% x̃: 2.41%
        95% mean confidence interval for instructions value: -9.68 -6.32
        95% mean confidence interval for instructions %-change: -2.59% -1.98%
        Instructions are helped.
    
        total cycles in shared programs: 334766372 -> 334765029 (<.01%)
        cycles in affected programs: 3548967 -> 3547624 (-0.04%)
        helped: 53
        HURT: 3
        helped stats (abs) min: 1 max: 230 x̄: 35.68 x̃: 22
        helped stats (rel) min: <.01% max: 2.15% x̄: 0.59% x̃: 0.50%
        HURT stats (abs)   min: 22 max: 270 x̄: 182.67 x̃: 256
        HURT stats (rel)   min: <.01% max: 0.07% x̄: 0.04% x̃: 0.04%
        95% mean confidence interval for cycles value: -42.03 -5.93
        95% mean confidence interval for cycles %-change: -0.65% -0.46%
        Cycles are helped.
    
    No spill/fill changes or gained/lost

--- src/intel/compiler/brw_nir.c
+++ src/intel/compiler/brw_nir.c
@@ -985,6 +985,7 @@ brw_postprocess_nir(nir_shader *nir, const struct brw_compiler *compiler,
    OPT(nir_lower_to_source_mods, nir_lower_all_source_mods);
    OPT(nir_copy_prop);
    OPT(nir_opt_dce);
+   OPT(nir_opt_rematerialize_compares);
    OPT(nir_opt_move, nir_move_comparisons);
 
    OPT(nir_lower_bool_to_int32);
@@ -1013,9 +1014,6 @@ brw_postprocess_nir(nir_shader *nir, const struct brw_compiler *compiler,
    }
 
    OPT(nir_opt_dce);
-
-   if (OPT(nir_opt_rematerialize_compares))
-      OPT(nir_opt_dce);
 
    /* This is the last pass we run before we start emitting stuff.  It
     * determines when we need to insert boolean resolves on Gen <= 5.  We
