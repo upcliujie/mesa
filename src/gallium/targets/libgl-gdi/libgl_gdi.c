bbbeacc3eb644d5b2b6b0a75e11010b12d7dc72a
Jesse Natalie <jenatali@ntdev.microsoft.com>
2020-04-25 16:26:56 -0400

    d3d12: Implement winsys framebuffer

--- src/gallium/targets/libgl-gdi/libgl_gdi.c
+++ src/gallium/targets/libgl-gdi/libgl_gdi.c
@@ -208,6 +208,19 @@ gdi_get_pfd_flags(struct pipe_screen *screen)
 }
 
 
+static struct stw_winsys_framebuffer *
+gdi_create_framebuffer(struct pipe_screen *screen,
+                       HDC hDC,
+                       int iPixelFormat)
+{
+#ifdef GALLIUM_D3D12
+   if (use_d3d12)
+      return d3d12_wgl_create_framebuffer(screen, hDC, iPixelFormat);
+#endif
+   return NULL;
+}
+
+
 static const struct stw_winsys stw_winsys = {
    &gdi_screen_create,
    &gdi_present,
@@ -220,6 +233,7 @@ static const struct stw_winsys stw_winsys = {
    NULL, /* shared_surface_close */
    NULL, /* compose */
    &gdi_get_pfd_flags,
+   &gdi_create_framebuffer,
 };
 
 
