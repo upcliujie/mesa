7624a2230af98ac035b79400212abe56e5f2d8a5
Ian Romanick <ian.d.romanick@intel.com>
2020-04-29 12:10:29 -0700

    intel/fs: Enable glsl_correct_derivatives_after_discard driconf option
    Support is added to all three drivers in a single commit rather than
    separate commits because the next commit will change the compiler that
    is shared by all three drivers.
    
    v2: Rebase on about 16k commits.  Crocus too.
    
    v3: Add fs_correct_derivs_after_kill to the flags stored by
    brw_get_compiler_config_value. Suggested by Jason.
    
    v4: Replace FINISHME comment in brw_nir.c with some hints about fixing
    possible problems in shaders expecting pre-1.30 rules.  Suggested by
    Jason.
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/intel/compiler/brw_nir.c
+++ src/intel/compiler/brw_nir.c
@@ -773,6 +773,13 @@ brw_preprocess_nir(const struct brw_compiler *compiler, nir_shader *nir,
    if (nir->info.stage == MESA_SHADER_GEOMETRY)
       OPT(nir_lower_gs_intrinsics, 0);
 
+   /* If we encounter any GLSL 1.10, 1.20, or ES 1.00 shaders that have
+    * problems with discard-as-terminate behavior, they should use the driconf
+    * option to get demote behavior.
+    */
+   OPT(nir_lower_discard_or_demote,
+       compiler->fs_correct_derivs_after_kill || nir->info.is_arb_asm);
+
    /* See also brw_nir_trig_workarounds.py */
    if (compiler->precise_trig &&
        !(devinfo->ver >= 10 || devinfo->platform == INTEL_PLATFORM_KBL))
