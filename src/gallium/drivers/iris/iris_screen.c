400098e37e2d8d3f4d8aa0a0720e939b5df11d52
Jordan Justen <jordan.l.justen@intel.com>
2020-05-15 03:16:51 -0700

    intel/iris: Build gen 12.5
    Reworks:
     * genX_call in iris_screen.c (found by Jason)
    
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/drivers/iris/iris_screen.c
+++ src/gallium/drivers/iris/iris_screen.c
@@ -59,9 +59,13 @@
 #include "iris_monitor.h"
 
 #define genX_call(devinfo, func, ...)             \
-   switch (devinfo.gen) {                        \
+   switch (devinfo.gen) {                         \
    case 12:                                       \
-      gen12_##func(__VA_ARGS__);                  \
+      if (gen_device_info_is_12hp(devinfo)) {     \
+         gen125_##func(__VA_ARGS__);              \
+      } else {                                    \
+         gen12_##func(__VA_ARGS__);               \
+      }                                           \
       break;                                      \
    case 11:                                       \
       gen11_##func(__VA_ARGS__);                  \
