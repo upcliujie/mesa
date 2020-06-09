3408814b53d41982744942c6513753637e9946d3
Rohan Garg <rohan.garg@collabora.com>
2020-06-09 19:13:19 +0200

    i965: Implement semaphore support for EXT_external_objects
    Implement importing, signalling and waiting for EXT_external_objects
    
    Signed-off-by: Rohan Garg <rohan.garg@collabora.com>
    Reviewed-by: Eleni Maria Stea <estea@igalia.com>

--- src/mesa/drivers/dri/i965/intel_fbo.c
+++ src/mesa/drivers/dri/i965/intel_fbo.c
@@ -1136,4 +1136,5 @@ intel_fbo_init(struct brw_context *brw)
                                                _mesa_key_pointer_equal);
    brw->depth_cache = _mesa_set_create(brw, _mesa_hash_pointer,
                                        _mesa_key_pointer_equal);
+   util_dynarray_init(&brw->batch.exec_fences, NULL);
 }
