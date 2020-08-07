35d0bd1f1413767b8c87433345de22b258fd1910
Rhys Perry <pendingchaos02@gmail.com>
2020-08-07 19:24:17 +0100

    spirv: fix GLSLstd450Modf/GLSLstd450Frexp when the destination is vector
    We can't write to an individual component in a function_temp vector, so we
    have to use vtn_variable_store() which does a load+insert+store.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3484
    Cc: mesa-stable

--- src/compiler/spirv/vtn_glsl450.c
+++ src/compiler/spirv/vtn_glsl450.c
@@ -339,8 +339,13 @@ handle_glsl450_alu(struct vtn_builder *b, enum GLSLstd450 entrypoint,
       nir_ssa_def *sign = nir_fsign(nb, src[0]);
       nir_ssa_def *abs = nir_fabs(nb, src[0]);
       dest->def = nir_fmul(nb, sign, nir_ffract(nb, abs));
-      nir_store_deref(nb, vtn_nir_deref(b, w[6]),
-                      nir_fmul(nb, sign, nir_ffloor(nb, abs)), 0xf);
+
+      struct vtn_pointer *i_ptr = vtn_value(b, w[6], vtn_value_type_pointer)->pointer;
+      struct vtn_ssa_value i;
+      memset(&i, 0, sizeof(i));
+      i.def = nir_fmul(nb, sign, nir_ffloor(nb, abs));
+      i.type = i_ptr->type->type;
+      vtn_variable_store(b, &i, i_ptr, 0);
       break;
    }
 
@@ -526,9 +531,14 @@ handle_glsl450_alu(struct vtn_builder *b, enum GLSLstd450 entrypoint,
       break;
 
    case GLSLstd450Frexp: {
-      nir_ssa_def *exponent = nir_frexp_exp(nb, src[0]);
       dest->def = nir_frexp_sig(nb, src[0]);
-      nir_store_deref(nb, vtn_nir_deref(b, w[6]), exponent, 0xf);
+
+      struct vtn_pointer *i_ptr = vtn_value(b, w[6], vtn_value_type_pointer)->pointer;
+      struct vtn_ssa_value i;
+      memset(&i, 0, sizeof(i));
+      i.def = nir_frexp_exp(nb, src[0]);
+      i.type = i_ptr->type->type;
+      vtn_variable_store(b, &i, i_ptr, 0);
       break;
    }
 
