857abb4be07e7acb1d114f78be5e041c42ddffe0
Daniel Schürmann <daniel@schuermann.dev>
2020-08-12 16:58:35 +0200

    aco: make pred_by_exec_mask() accessible in other files

--- src/amd/compiler/aco_ir.h
+++ src/amd/compiler/aco_ir.h
@@ -1415,6 +1415,7 @@ bool can_use_opsel(chip_class chip, aco_opcode op, int idx, bool high);
 bool can_use_SDWA(chip_class chip, const aco_ptr<Instruction>& instr);
 /* updates "instr" and returns the old instruction (or NULL if no update was needed) */
 aco_ptr<Instruction> convert_to_SDWA(chip_class chip, aco_ptr<Instruction>& instr);
+bool pred_by_exec_mask(const Instruction* instr);
 
 uint32_t get_reduction_identity(ReduceOp op, unsigned idx);
 
