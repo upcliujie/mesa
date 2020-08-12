9280f23fec0df4a61e68ac8be03442de4cb209d7
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-08-12 16:03:31 -0400

    zink: handle nir_intrinsic_memory_barrier_shared in ntv

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -2627,6 +2627,12 @@ emit_intrinsic(struct ntv_context *ctx, nir_intrinsic_instr *intr)
                                         SpvMemorySemanticsMakeVisibleMask  | SpvMemorySemanticsAcquireReleaseMask);
       break;
 
+   case nir_intrinsic_memory_barrier_shared:
+      spirv_builder_emit_memory_barrier(&ctx->builder, SpvScopeWorkgroup,
+                                        SpvMemorySemanticsWorkgroupMemoryMask |
+                                        SpvMemorySemanticsAcquireReleaseMask);
+      break;
+
    case nir_intrinsic_control_barrier:
       spirv_builder_emit_control_barrier(&ctx->builder, SpvScopeWorkgroup,
                                          SpvScopeWorkgroup,
