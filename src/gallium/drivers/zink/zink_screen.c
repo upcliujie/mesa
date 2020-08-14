4571a92e9b9b8c330fa70616923acd534f6e5580
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-08-14 17:39:49 -0400

    zink: enable PIPE_CAP_TGSI_ARRAY_COMPONENTS

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -170,6 +170,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_ROBUST_BUFFER_ACCESS_BEHAVIOR:
    case PIPE_CAP_BUFFER_MAP_PERSISTENT_COHERENT:
    case PIPE_CAP_CLEAR_TEXTURE:
+   case PIPE_CAP_TGSI_ARRAY_COMPONENTS:
       return 1;
 
    case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
