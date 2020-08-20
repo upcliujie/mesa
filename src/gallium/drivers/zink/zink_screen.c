cf33d1644015f8db4ba0ad5e983fae3c91ca1725
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-08-20 16:42:10 -0400

    zink: enable PIPE_CAP_TEXTURE_HALF_FLOAT_LINEAR

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -187,6 +187,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_QUERY_PIPELINE_STATISTICS:
    case PIPE_CAP_QUERY_PIPELINE_STATISTICS_SINGLE:
    case PIPE_CAP_GL_SPIRV:
+   case PIPE_CAP_TEXTURE_HALF_FLOAT_LINEAR:
       return 1;
 
    case PIPE_CAP_VERTEX_ELEMENT_INSTANCE_DIVISOR:
