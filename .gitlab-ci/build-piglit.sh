a915caa22dc476684c8b950adb920eeb6db31f22
Andres Gomez <agomez@igalia.com>
2020-08-24 14:10:24 +0300

    gitlab-ci: build piglit in the Vulkan testing image
    Signed-off-by: Andres Gomez <agomez@igalia.com>

--- .gitlab-ci/build-piglit.sh
+++ .gitlab-ci/build-piglit.sh
@@ -11,7 +11,7 @@ pushd /piglit
 git checkout 5d3fbc00e32293d0b7029b986bfb99fa891c9bae
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS
-ninja
+ninja $PIGLIT_BUILD_TARGETS
 find -name .git -o -name '*ninja*' -o -iname '*cmake*' -o -name '*.[chao]' | xargs rm -rf
 rm -rf target_api
 popd
