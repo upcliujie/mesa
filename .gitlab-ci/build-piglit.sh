052476eeaea3a7d6f194a452299948c16aedcca0
Andres Gomez <agomez@igalia.com>
2020-08-27 22:18:17 +0300

    ci: build piglit inside baremetal and LAVA's rootfs
    Signed-off-by: Andres Gomez <agomez@igalia.com>

--- .gitlab-ci/build-piglit.sh
+++ .gitlab-ci/build-piglit.sh
@@ -10,7 +10,7 @@ git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-ch
 pushd /piglit
 git checkout 5d3fbc00e32293d0b7029b986bfb99fa891c9bae
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
-cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS
+cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
 find -name .git -o -name '*ninja*' -o -iname '*cmake*' -o -name '*.[chao]' | xargs rm -rf
 rm -rf target_api
