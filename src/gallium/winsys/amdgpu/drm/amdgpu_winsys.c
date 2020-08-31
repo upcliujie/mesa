085eee7a5bd07720ce991273843de8634c6d2180
Emil Velikov <emil.velikov@collabora.com>
2020-08-31 14:04:35 +0100

    amdgpu: use new symbol sharing mechanism
    Move to the new symbol sharing mechanism (as used by the winsys cache).
    This will ultimately allow us to stop dlopen(RTLD_GLOBAL) of the drivers
    while also making for shorter and more consistent handling.
    
    In particular, this correctly tracks LLVM initialization state across the
    gallium drivers and RADV.
    
    v2: Call the llvm init once, in the static case (thanks pepp)
    
    Cc: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Cc: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Cc: Marek Olšák <marek.olsak@amd.com>
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

--- src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c
+++ src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c
@@ -44,8 +44,13 @@
 #include "ac_llvm_util.h"
 #include "sid.h"
 
-static struct hash_table *dev_tab = NULL;
-static simple_mtx_t dev_tab_mutex = _SIMPLE_MTX_INITIALIZER_NP;
+#include "tls/symbol_cache.h"
+
+static struct winsys_cache *AMDGPU_WINSYS_CACHE;
+static mtx_t cache_sym_mutex = _MTX_INITIALIZER_NP;
+
+#define dev_tab         AMDGPU_WINSYS_CACHE->hash
+#define dev_tab_mutex   AMDGPU_WINSYS_CACHE->hash_mutex
 
 DEBUG_GET_ONCE_BOOL_OPTION(all_bos, "RADEON_ALL_BOS", false)
 
@@ -164,7 +169,7 @@ static void amdgpu_winsys_destroy(struct radeon_winsys *rws)
     * amdgpu_winsys_create in another thread doesn't get the winsys
     * from the table when the counter drops to 0.
     */
-   simple_mtx_lock(&dev_tab_mutex);
+   mtx_lock(&dev_tab_mutex);
 
    destroy = pipe_reference(&ws->reference, NULL);
    if (destroy && dev_tab) {
@@ -175,7 +180,7 @@ static void amdgpu_winsys_destroy(struct radeon_winsys *rws)
       }
    }
 
-   simple_mtx_unlock(&dev_tab_mutex);
+   mtx_unlock(&dev_tab_mutex);
 
    if (destroy)
       do_winsys_deinit(ws);
@@ -350,7 +355,7 @@ static void amdgpu_cs_set_secure(struct radeon_cmdbuf *rcs, bool secure)
    cs->csc->secure = secure;
 }
 
-PUBLIC struct radeon_winsys *
+struct radeon_winsys *
 amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
 		     radeon_screen_create_t screen_create)
 {
@@ -367,8 +372,13 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
    pipe_reference_init(&ws->reference, 1);
    ws->fd = os_dupfd_cloexec(fd);
 
+   if (symbol_cache(AMDGPU_WINSYS_CACHE, &cache_sym_mutex)) {
+      FREE(ws);
+      return NULL;
+   }
+
    /* Look up the winsys from the dev table. */
-   simple_mtx_lock(&dev_tab_mutex);
+   mtx_lock(&dev_tab_mutex);
    if (!dev_tab)
       dev_tab = util_hash_table_create_ptr_keys();
 
@@ -461,7 +471,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
                             amdgpu_bo_slab_alloc_normal,
                             amdgpu_bo_slab_free)) {
             amdgpu_winsys_destroy(&ws->base);
-            simple_mtx_unlock(&dev_tab_mutex);
+            mtx_unlock(&dev_tab_mutex);
             return NULL;
          }
 
@@ -473,7 +483,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
                                            amdgpu_bo_slab_alloc_encrypted,
                                            amdgpu_bo_slab_free)) {
             amdgpu_winsys_destroy(&ws->base);
-            simple_mtx_unlock(&dev_tab_mutex);
+            mtx_unlock(&dev_tab_mutex);
             return NULL;
          }
 
@@ -496,7 +506,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
       if (!util_queue_init(&aws->cs_queue, "cs", 8, 1,
                            UTIL_QUEUE_INIT_RESIZE_IF_FULL)) {
          amdgpu_winsys_destroy(&ws->base);
-         simple_mtx_unlock(&dev_tab_mutex);
+         mtx_unlock(&dev_tab_mutex);
          return NULL;
       }
 
@@ -506,7 +516,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
          r = amdgpu_vm_reserve_vmid(dev, 0);
          if (r) {
             amdgpu_winsys_destroy(&ws->base);
-            simple_mtx_unlock(&dev_tab_mutex);
+            mtx_unlock(&dev_tab_mutex);
             return NULL;
          }
       }
@@ -538,7 +548,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
    ws->base.screen = screen_create(&ws->base, config);
    if (!ws->base.screen) {
       amdgpu_winsys_destroy(&ws->base);
-      simple_mtx_unlock(&dev_tab_mutex);
+      mtx_unlock(&dev_tab_mutex);
       return NULL;
    }
 
@@ -551,7 +561,7 @@ unlock:
    /* We must unlock the mutex once the winsys is fully initialized, so that
     * other threads attempting to create the winsys from the same fd will
     * get a fully initialized winsys and not just half-way initialized. */
-   simple_mtx_unlock(&dev_tab_mutex);
+   mtx_unlock(&dev_tab_mutex);
 
    return &ws->base;
 
@@ -562,6 +572,6 @@ fail:
       _mesa_hash_table_destroy(ws->kms_handles, NULL);
    close(ws->fd);
    FREE(ws);
-   simple_mtx_unlock(&dev_tab_mutex);
+   mtx_unlock(&dev_tab_mutex);
    return NULL;
 }
