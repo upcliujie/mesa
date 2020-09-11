18637f0bfb44efe503a60b9dd77b27ae03c0758d
Jonathan Marek <jonathan@marek.ca>
2020-09-10 22:51:53 -0400

    turnip: rework fences to use syncobjs
    Fences are now just a syncobj, which makes our life easier.
    
    The next step will be to fill out ImportFenceFdKHR()/GetFenceFdKHR().
    
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>

--- src/freedreno/vulkan/tu_private.h
+++ src/freedreno/vulkan/tu_private.h
@@ -291,21 +291,8 @@ struct tu_pipeline_key
 struct tu_fence
 {
    struct vk_object_base base;
-   struct wsi_fence *fence_wsi;
-   bool signaled;
-   int fd;
+   uint32_t syncobj;
 };
-
-void
-tu_fence_init(struct tu_fence *fence, bool signaled);
-void
-tu_fence_finish(struct tu_fence *fence);
-void
-tu_fence_update_fd(struct tu_fence *fence, int fd);
-void
-tu_fence_copy(struct tu_fence *fence, const struct tu_fence *src);
-void
-tu_fence_wait_idle(struct tu_fence *fence);
 
 struct tu_queue
 {
@@ -317,7 +304,7 @@ struct tu_queue
    VkDeviceQueueCreateFlags flags;
 
    uint32_t msm_queue_id;
-   struct tu_fence submit_fence;
+   int fence;
 };
 
 struct tu_bo
@@ -1532,13 +1519,14 @@ tu_physical_device_init(struct tu_physical_device *device,
 VkResult
 tu_enumerate_devices(struct tu_instance *instance);
 
-int
-tu_drm_submitqueue_new(const struct tu_device *dev,
-                       int priority,
-                       uint32_t *queue_id);
-
+VkResult
+tu_queue_init(struct tu_device *device,
+              struct tu_queue *queue,
+              uint32_t queue_family_index,
+              int idx,
+              VkDeviceQueueCreateFlags flags);
 void
-tu_drm_submitqueue_close(const struct tu_device *dev, uint32_t queue_id);
+tu_queue_finish(struct tu_queue *queue);
 
 #define TU_DEFINE_HANDLE_CASTS(__tu_type, __VkType)                          \
                                                                              \
