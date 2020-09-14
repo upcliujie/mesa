01aacebd23e0b0e2feba5b52fb3638effca97ae3
Connor Abbott <cwabbott0@gmail.com>
2020-09-14 10:14:55 +0200

    nir: Add read_invocation_cond intrinsic
    On qualcomm, we have shared registers similar to SGPR's on AMD. However,
    there is no readlane or readfirstlane primitive. shared registers can
    only be written to when just one lane is active. This means that we have
    to lower readInvocation(val, id) to something like:
    
    if (gl_SubgroupInvocation == id) {
        scalar_reg = val;
    }
    
    return scalar_reg;
    
    However it's a bit difficult to actually get the value of
    gl_SubgroupInvocation in the backend, because for compute it requires
    some calculations and we don't have any CSE support in the backend. This
    intrinsic lets us turn it into
    "readInvocationCond(val, id == gl_SubgroupInvocation)" in NIR at which
    point the backend code generation is a lot easier.

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -4365,6 +4365,7 @@ typedef struct nir_lower_subgroups_options {
    bool lower_quad_broadcast_dynamic_to_const:1;
    bool lower_subgroup_id_to_local_id:1;
    bool lower_elect:1;
+   bool lower_read_invocation_to_cond:1;
 } nir_lower_subgroups_options;
 
 bool nir_lower_subgroups(nir_shader *shader,
