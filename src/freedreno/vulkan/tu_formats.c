f4e2a929d7d1b0298ebe5351e858e2b6283489b9
Jonathan Marek <jonathan@marek.ca>
2020-09-16 10:15:48 -0400

    turnip: do not clear pImageFormatProperties for unsupported images
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>

--- src/freedreno/vulkan/tu_formats.c
+++ src/freedreno/vulkan/tu_formats.c
@@ -562,7 +562,7 @@ tu_get_image_format_properties(
    }
 
    if (format_feature_flags == 0)
-      goto unsupported;
+      return VK_ERROR_FORMAT_NOT_SUPPORTED;
 
    switch (info->type) {
    default:
@@ -599,7 +599,7 @@ tu_get_image_format_properties(
            VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
 
    if ((format_feature_flags & required) != required)
-      goto unsupported;
+      return VK_ERROR_FORMAT_NOT_SUPPORTED;
 
    *pImageFormatProperties = (VkImageFormatProperties) {
       .maxExtent = maxExtent,
@@ -627,16 +627,6 @@ tu_get_image_format_properties(
       *p_feature_flags = format_feature_flags;
 
    return VK_SUCCESS;
-unsupported:
-   *pImageFormatProperties = (VkImageFormatProperties) {
-      .maxExtent = { 0, 0, 0 },
-      .maxMipLevels = 0,
-      .maxArrayLayers = 0,
-      .sampleCounts = 0,
-      .maxResourceSize = 0,
-   };
-
-   return VK_ERROR_FORMAT_NOT_SUPPORTED;
 }
 
 static VkResult
