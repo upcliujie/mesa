b78484ec4fce76cf4b28ce95cf65231e4e5cc974
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2020-09-17 20:02:11 +0200

    radv: Disable DCC explicitly for incompatible copies.
    If we enable DCC for GENERAL we cannot set the layout to GENERAL to
    disable DCC, so do it explicitly.

--- src/amd/vulkan/radv_meta.h
+++ src/amd/vulkan/radv_meta.h
@@ -142,6 +142,7 @@ struct radv_meta_blit2d_surf {
 	unsigned layer;
 	VkImageAspectFlags aspect_mask;
 	VkImageLayout current_layout;
+	bool disable_compression;
 };
 
 struct radv_meta_blit2d_buffer {
