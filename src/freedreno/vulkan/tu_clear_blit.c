4a98a2397b15ee21b83106c77ca39c956823640b
Jonathan Marek <jonathan@marek.ca>
2020-09-19 15:56:40 -0400

    freedreno/layout: add option for different UBWC block size for Y plane
    We don't have a pipe format to represent this, so just add an argument to
    fdl6_layout().
    
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>

--- src/freedreno/vulkan/tu_clear_blit.c
+++ src/freedreno/vulkan/tu_clear_blit.c
@@ -1425,7 +1425,7 @@ tu_copy_image_to_image(struct tu_cmd_buffer *cmd,
                   staging_image.level_count,
                   staging_image.layer_count,
                   staging_image.type == VK_IMAGE_TYPE_3D,
-                  NULL);
+                  NULL, false);
 
       VkResult result = tu_get_scratch_bo(cmd->device,
                                           staging_image.layout[0].size,
