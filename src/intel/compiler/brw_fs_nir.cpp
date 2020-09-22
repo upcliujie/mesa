84965f97792a7e9ac69c2c4478b864ef4f09d946
Kenneth Graunke <kenneth@whitecape.org>
2020-09-22 16:25:58 -0700

    intel/fs: Fix bit-size check for store_scratch implementation.
    We can use the DWord message for 32-bit or 64-bit values.  The load
    implementation already used >=, but this one used == for some reason.

--- src/intel/compiler/brw_fs_nir.cpp
+++ src/intel/compiler/brw_fs_nir.cpp
@@ -4879,7 +4879,7 @@ fs_visitor::nir_emit_intrinsic(const fs_builder &bld, nir_intrinsic_instr *instr
       assert(nir_src_bit_size(instr->src[0]) <= 32);
       assert(nir_intrinsic_write_mask(instr) == 1);
       assert(nir_intrinsic_align(instr) > 0);
-      if (nir_src_bit_size(instr->src[0]) == 32 &&
+      if (nir_src_bit_size(instr->src[0]) >= 32 &&
           nir_intrinsic_align(instr) >= 4) {
          srcs[SURFACE_LOGICAL_SRC_DATA] = data;
 
