b595ebde1fced8d077dda0bb5d5115a6b4ed995a
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-09-23 15:48:27 -0400

    zink: handle dual blending override from driconf
    when this is enabled, we need to push gl_FragData[1] to location 0 and
    index 1 so that it gets blended like the application expects
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>

--- src/gallium/drivers/zink/zink_screen.h
+++ src/gallium/drivers/zink/zink_screen.h
@@ -110,6 +110,10 @@ struct zink_screen {
    PFN_vkUseIOSurfaceMVK vk_UseIOSurfaceMVK;
    PFN_vkGetIOSurfaceMVK vk_GetIOSurfaceMVK;
 #endif
+
+   struct {
+      bool dual_color_blend_by_location;
+   } driconf;
 };
 
 static inline struct zink_screen *
