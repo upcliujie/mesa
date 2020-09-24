6b69f45643e9726db873992e6cf0899c398eabd0
Rhys Perry <pendingchaos02@gmail.com>
2020-09-24 14:50:54 +0100

    radv: move radv_declare_shader_args() out of shader_variant_compile()
    Declaring them earlier will allow us to access them in NIR.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/vulkan/radv_shader_args.h
+++ src/amd/vulkan/radv_shader_args.h
@@ -31,6 +31,7 @@
 struct radv_shader_args {
    struct ac_shader_args ac;
    struct radv_shader_info *shader_info;
+   struct radv_shader_variant_key *key;
    const struct radv_nir_compiler_options *options;
 
    struct ac_arg descriptor_sets[MAX_SETS];
@@ -45,6 +46,8 @@ struct radv_shader_args {
    struct ac_arg ngg_viewport_scale[2];
    struct ac_arg ngg_viewport_translate[2];
 
+   enum chip_class chip_class;
+   bool explicit_scratch_args;
    bool is_gs_copy_shader;
    bool is_trap_handler_shader;
 };
