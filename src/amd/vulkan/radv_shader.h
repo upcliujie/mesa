efda84ee2745cc53c065203176ed24bea713312d
Rhys Perry <pendingchaos02@gmail.com>
2020-09-24 14:50:54 +0100

    radv: move radv_declare_shader_args() out of shader_variant_compile()
    Declaring them earlier will allow us to access them in NIR.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/vulkan/radv_shader.h
+++ src/amd/vulkan/radv_shader.h
@@ -46,6 +46,7 @@ struct radv_device;
 struct radv_pipeline;
 struct radv_pipeline_cache;
 struct radv_pipeline_key;
+struct radv_shader_args;
 
 struct radv_vs_out_key {
    uint32_t as_es : 1;
@@ -127,7 +128,6 @@ enum radv_compiler_debug_level {
 struct radv_nir_compiler_options {
    struct radv_pipeline_layout *layout;
    struct radv_shader_variant_key key;
-   bool explicit_scratch_args;
    bool clamp_shadow_reference;
    bool robust_buffer_access;
    bool adjust_frag_coord_z;
@@ -454,15 +454,16 @@ VkResult radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device 
 struct radv_shader_variant *radv_shader_variant_create(struct radv_device *device,
                                                        const struct radv_shader_binary *binary,
                                                        bool keep_shader_info);
-struct radv_shader_variant *radv_shader_variant_compile(
-   struct radv_device *device, struct vk_shader_module *module, struct nir_shader *const *shaders,
-   int shader_count, struct radv_pipeline_layout *layout, const struct radv_shader_variant_key *key,
-   struct radv_shader_info *info, bool keep_shader_info, bool keep_statistic_info,
-   bool disable_optimizations, struct radv_shader_binary **binary_out);
+struct radv_shader_variant *
+radv_shader_variant_compile(struct radv_device *device, struct vk_shader_module *module,
+                            struct nir_shader *const *shaders, int shader_count,
+                            struct radv_pipeline_layout *layout, bool keep_shader_info,
+                            bool keep_statistic_info, bool disable_optimizations,
+                            struct radv_shader_args *args, struct radv_shader_binary **binary_out);
 
 struct radv_shader_variant *
 radv_create_gs_copy_shader(struct radv_device *device, struct nir_shader *nir,
-                           struct radv_shader_info *info, struct radv_shader_binary **binary_out,
+                           struct radv_shader_args *args, struct radv_shader_binary **binary_out,
                            bool multiview, bool keep_shader_info, bool keep_statistic_info,
                            bool disable_optimizations);
 
