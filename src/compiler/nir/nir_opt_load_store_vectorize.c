eaa81c8766caa0be1739fe8182a172f6c647037d
Rhys Perry <pendingchaos02@gmail.com>
2020-09-29 17:00:45 +0100

    nir/opt_load_store_vectorize: loop internally
    To vectorize to vec8/16 or vec4 (without vec3), we can't incrementally add
    components to a load/store. This patch loops vectorization so that two new
    vec2/4/8 operations can be combined into a larger operation.
    
    fossil-db (Navi):
    Totals from 22 (0.02% of 137413) affected shaders:
    SGPRs: 2136 -> 2128 (-0.37%)
    SpillVGPRs: 1749 -> 1771 (+1.26%)
    CodeSize: 923336 -> 919128 (-0.46%); split: -0.51%, +0.06%
    Scratch: 178176 -> 184320 (+3.45%)
    Instrs: 163750 -> 163090 (-0.40%); split: -0.43%, +0.03%
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/compiler/nir/nir_opt_load_store_vectorize.c
+++ src/compiler/nir/nir_opt_load_store_vectorize.c
@@ -1130,6 +1130,43 @@ update_align(struct entry *entry)
 }
 
 static bool
+vectorize_sorted_entries(struct vectorize_ctx *ctx, nir_function_impl *impl,
+                         struct util_dynarray *arr)
+{
+   unsigned num_entries = util_dynarray_num_elements(arr, struct entry *);
+
+   bool progress = false;
+   for (unsigned first_idx = 0; first_idx < num_entries; first_idx++) {
+      struct entry *low = *util_dynarray_element(arr, struct entry *, first_idx);
+      if (!low)
+         continue;
+
+      for (unsigned second_idx = first_idx + 1; second_idx < num_entries; second_idx++) {
+         struct entry *high = *util_dynarray_element(arr, struct entry *, second_idx);
+         if (!high)
+            continue;
+
+         uint64_t diff = high->offset_signed - low->offset_signed;
+         if (diff > get_bit_size(low) / 8u * low->intrin->num_components)
+            break;
+
+         struct entry *first = low->index < high->index ? low : high;
+         struct entry *second = low->index < high->index ? high : low;
+
+         if (try_vectorize(impl, ctx, low, high, first, second)) {
+            low = low->is_store ? second : first;
+            *util_dynarray_element(arr, struct entry *, second_idx) = NULL;
+            progress = true;
+         }
+      }
+
+      *util_dynarray_element(arr, struct entry *, first_idx) = low;
+   }
+
+   return progress;
+}
+
+static bool
 vectorize_entries(struct vectorize_ctx *ctx, nir_function_impl *impl, struct hash_table *ht)
 {
    if (!ht)
@@ -1145,34 +1182,8 @@ vectorize_entries(struct vectorize_ctx *ctx, nir_function_impl *impl, struct has
             util_dynarray_num_elements(arr, struct entry *),
             sizeof(struct entry *), &sort_entries);
 
-      unsigned num_entries = util_dynarray_num_elements(arr, struct entry *);
-
-      for (unsigned first_idx = 0; first_idx < num_entries; first_idx++) {
-         struct entry *low = *util_dynarray_element(arr, struct entry *, first_idx);
-         if (!low)
-            continue;
-
-         for (unsigned second_idx = first_idx + 1; second_idx < num_entries; second_idx++) {
-            struct entry *high = *util_dynarray_element(arr, struct entry *, second_idx);
-            if (!high)
-               continue;
-
-            uint64_t diff = high->offset_signed - low->offset_signed;
-            if (diff > get_bit_size(low) / 8u * low->intrin->num_components)
-               break;
-
-            struct entry *first = low->index < high->index ? low : high;
-            struct entry *second = low->index < high->index ? high : low;
-
-            if (try_vectorize(impl, ctx, low, high, first, second)) {
-               low = low->is_store ? second : first;
-               *util_dynarray_element(arr, struct entry *, second_idx) = NULL;
-               progress = true;
-            }
-         }
-
-         *util_dynarray_element(arr, struct entry *, first_idx) = low;
-      }
+      while (vectorize_sorted_entries(ctx, impl, arr))
+         progress = true;
 
       util_dynarray_foreach(arr, struct entry *, elem) {
          if (*elem)
