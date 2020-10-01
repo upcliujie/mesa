81d96af07a51b1897a113a499af5f5d23d7317b7
Dave Airlie <airlied@redhat.com>
2020-10-02 04:52:20 +1000

    address some review comments
    put back OCL path

--- .gitlab-ci/piglit/run_cl.sh
+++ .gitlab-ci/piglit/run_cl.sh
@@ -5,11 +5,14 @@ set -o xtrace
 
 VERSION=`cat install/VERSION`
 
+#export LP_CL=1
+#export OCL_ICD_VENDORS=$OLDPWD/install/etc/OpenCL/vendors/
+
+echo $LP_CL
+echo $OCL_ICD_VENDORS
+
 rm -rf results
 cd /piglit
-
-export LP_CL=1
-export OCL_ICD_VENDORS=$OLDPWD/install/etc/OpenCL/vendors/
 
 PIGLIT_OPTIONS=$(echo $PIGLIT_OPTIONS | head -n 1)
 set +e
