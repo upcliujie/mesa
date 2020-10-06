7e6f67b41a172c5646d224ae10a900e9844fdfad
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-10-06 16:31:59 -0400

    zink: pre-size descriptor transition hash table
    this avoids costly rehashing

--- src/gallium/drivers/zink/zink_draw.c
+++ src/gallium/drivers/zink/zink_draw.c
@@ -759,6 +759,7 @@ update_descriptors(struct zink_context *ctx, struct zink_screen *screen, bool is
    int num_transitions = 0;
    struct set *persistent = _mesa_pointer_set_create(NULL);
    struct set *ht = _mesa_set_create(NULL, transition_hash, transition_equals);
+   _mesa_set_resize(ht, num_bindings);
 
 
    bool need_flush = false;
