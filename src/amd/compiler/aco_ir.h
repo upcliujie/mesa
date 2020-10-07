2fcc36e13b71fbf5a682e74a18801fe7db75cba4
Rhys Perry <pendingchaos02@gmail.com>
2020-10-07 14:35:21 +0100

    aco: make validate_ir() output usable in tests
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/compiler/aco_ir.h
+++ src/amd/compiler/aco_ir.h
@@ -1915,6 +1915,8 @@ public:
    unsigned next_uniform_if_depth = 0;
 
    struct {
+      FILE *output = stderr;
+      bool shorten_messages = false;
       void (*func)(void *private_data,
                    enum radv_compiler_debug_level level,
                    const char *message);
