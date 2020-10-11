3cbd1dd65e1851a360fffb8b9643adfd6a853ada
Aaron Watry <awatry@gmail.com>
2020-10-11 21:34:24 +0200

    clover/image: add dimension property
    With that we can fix CL_IMAGE_HEIGHT and CL_IMAGE_DEPTH.
    
    v2 (Karol Herbst): split up commit
       (Serge Martin): convert to virtual method

--- src/gallium/frontends/clover/core/memory.cpp
+++ src/gallium/frontends/clover/core/memory.cpp
@@ -201,6 +201,11 @@ image::format() const {
    return _format;
 }
 
+cl_uint
+image::dimensions() const {
+   return 1;
+}
+
 size_t
 image::width() const {
    return _width;
@@ -294,6 +299,11 @@ image2d::type() const {
    return CL_MEM_OBJECT_IMAGE2D;
 }
 
+cl_uint
+image2d::dimensions() const {
+   return 2;
+}
+
 image2d_array::image2d_array(clover::context &ctx, cl_mem_flags flags,
                              const cl_image_format *format,
                              size_t width, size_t height, size_t array_size,
@@ -306,6 +316,11 @@ image2d_array::image2d_array(clover::context &ctx, cl_mem_flags flags,
 cl_mem_object_type
 image2d_array::type() const {
    return CL_MEM_OBJECT_IMAGE2D_ARRAY;
+}
+
+cl_uint
+image2d_array::dimensions() const {
+   return 2;
 }
 
 image3d::image3d(clover::context &ctx, cl_mem_flags flags,
@@ -321,4 +336,9 @@ image3d::image3d(clover::context &ctx, cl_mem_flags flags,
 cl_mem_object_type
 image3d::type() const {
    return CL_MEM_OBJECT_IMAGE3D;
+}
+
+cl_uint
+image3d::dimensions() const {
+   return 3;
 }
