52a30568457825d296821da15c6bfca187c19469
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-10-13 10:12:40 -0400

    zink: add some asserts for pipeline barriers to check renderpass state
    it's illegal to emit barriers during a renderpass

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -1481,6 +1481,7 @@ zink_resource_image_barrier(struct zink_context *ctx, struct zink_batch *batch, 
       else
          batch = zink_batch_no_rp(ctx);
    }
+   assert(!batch->in_rp);
    VkImageSubresourceRange isr = {
       res->aspect,
       0, VK_REMAINING_MIP_LEVELS,
@@ -1580,6 +1581,7 @@ zink_resource_buffer_barrier(struct zink_context *ctx, struct zink_batch *batch,
       else
          batch = zink_batch_no_rp(ctx);
    }
+   assert(!batch->in_rp);
    VkBufferMemoryBarrier bmb = {
       VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER,
       NULL,
