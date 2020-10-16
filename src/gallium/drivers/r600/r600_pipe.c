440ab4a1d15b44e0cf6d113e9889c956bf4c6d9f
Gert Wollny <gert.wollny@collabora.com>
2020-10-16 18:18:18 +0200

    r600: enable fp64 lowering to softemu with NIR
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

--- src/gallium/drivers/r600/r600_pipe.c
+++ src/gallium/drivers/r600/r600_pipe.c
@@ -412,12 +412,15 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 	case PIPE_CAP_GLSL_OPTIMIZE_CONSERVATIVELY:
 		return 0;
 
+        case PIPE_CAP_INT64:
 	case PIPE_CAP_DOUBLES:
 		if (rscreen->b.family == CHIP_ARUBA ||
 		    rscreen->b.family == CHIP_CAYMAN ||
 		    rscreen->b.family == CHIP_CYPRESS ||
 		    rscreen->b.family == CHIP_HEMLOCK)
 			return 1;
+                if (is_nir_enabled(&rscreen->b))
+                   return 1;
 		return 0;
 	case PIPE_CAP_CULL_DISTANCE:
 		return 1;
