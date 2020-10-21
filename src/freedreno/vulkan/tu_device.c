660db149ada664270c2e20efaeba535ffdfb01b7
Hyunjun Ko <zzoon@igalia.com>
2020-10-21 06:56:58 +0000

    turnip: Implement VK_KHR_performance_query
    There are still some commands unimplemented yet.
    
    - vkGetPhysicalDeviceQueueFamilyPerformanceQueryPassesKHR:
      The following patch supports this.
    
    - vkAcquireProfilingLockKHR / vkReleaseProfilingLock
      This patch supports only monitoring perf counters for each submit.
      To reserve/configure counters across submits we would need a kernel
      interface to be able to do that.
    
    Signed-off-by: Hyunjun Ko <zzoon@igalia.com>

--- src/freedreno/vulkan/tu_device.c
+++ src/freedreno/vulkan/tu_device.c
@@ -633,6 +633,14 @@ tu_GetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
          features->extendedDynamicState = true;
          break;
       }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PERFORMANCE_QUERY_FEATURES_KHR: {
+         VkPhysicalDevicePerformanceQueryFeaturesKHR *feature =
+            (VkPhysicalDevicePerformanceQueryFeaturesKHR *)ext;
+         feature->performanceCounterQueryPools = true;
+         feature->performanceCounterMultipleQueryPools = false;
+         break;
+      }
+
       default:
          break;
       }
@@ -875,6 +883,12 @@ tu_GetPhysicalDeviceProperties2(VkPhysicalDevice physicalDevice,
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_CUSTOM_BORDER_COLOR_PROPERTIES_EXT: {
          VkPhysicalDeviceCustomBorderColorPropertiesEXT *props = (void *)ext;
          props->maxCustomBorderColorSamplers = TU_BORDER_COLOR_COUNT;
+         break;
+      }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PERFORMANCE_QUERY_PROPERTIES_KHR: {
+         VkPhysicalDevicePerformanceQueryPropertiesKHR *properties =
+            (VkPhysicalDevicePerformanceQueryPropertiesKHR *)ext;
+         properties->allowCommandBufferQueryCopies = false;
          break;
       }
       default:
