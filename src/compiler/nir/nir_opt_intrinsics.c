554469b44ffc5741c1d4df0ac76c732631dc1744
Jason Ekstrand <jason@jlekstrand.net>
2020-10-23 16:48:38 -0500

    nir/opt_intrinsic: Optimize bcsel(b, shuffle(x, i), shuffle(x, j))
    The shuffles provided by the SPV_INTEL_subgroups extension generate
    
        bcsel(b, shuffle(x, i), shuffle(y, j))
    
    In the case where x and y are the same, we can turn this into a shuffle
    with the bcsel on the index which lets us drop a whole shuffle.

--- src/compiler/nir/nir_opt_intrinsics.c
+++ src/compiler/nir/nir_opt_intrinsics.c
@@ -29,6 +29,107 @@
  */
 
 static bool
+alu_src_is_simple(nir_alu_instr *alu, unsigned src)
+{
+   assert(alu->dest.dest.is_ssa);
+   assert(alu->src[src].src.is_ssa);
+
+   unsigned num_components = nir_ssa_alu_instr_src_components(alu, src);
+
+   if (alu->src[src].src.ssa->num_components != num_components)
+      return false;
+
+   for (unsigned c = 0; c < num_components; c++) {
+      if (alu->src[src].swizzle[c] != c)
+         return false;
+   }
+
+   return true;
+}
+
+static bool
+is_singe_use_shuffle(nir_ssa_def *def, nir_ssa_def **data, nir_ssa_def **index)
+{
+   if (def->parent_instr->type != nir_instr_type_intrinsic)
+      return false;
+
+   nir_intrinsic_instr *shuffle = nir_instr_as_intrinsic(def->parent_instr);
+   if (shuffle->intrinsic != nir_intrinsic_shuffle)
+      return false;
+
+   if (!list_is_empty(&shuffle->dest.ssa.if_uses) ||
+       !list_is_singular(&shuffle->dest.ssa.uses))
+      return false;
+
+   assert(shuffle->src[0].is_ssa);
+   assert(shuffle->src[1].is_ssa);
+
+   *data = shuffle->src[0].ssa;
+   *index = shuffle->src[1].ssa;
+
+   return true;
+}
+
+static nir_ssa_def *
+try_opt_bcsel_of_shuffle(nir_builder *b, nir_alu_instr *alu)
+{
+   assert(alu->op == nir_op_bcsel);
+
+   if (!alu_src_is_simple(alu, 0))
+      return NULL;
+
+   nir_ssa_def *data1, *index1;
+   if (!alu_src_is_simple(alu, 1) ||
+       !is_singe_use_shuffle(alu->src[1].src.ssa, &data1, &index1))
+      return NULL;
+
+   nir_ssa_def *data2, *index2;
+   if (!alu_src_is_simple(alu, 2) ||
+       !is_singe_use_shuffle(alu->src[2].src.ssa, &data2, &index2))
+      return NULL;
+
+   if (data1 != data2)
+      return NULL;
+
+   nir_ssa_def *index = nir_bcsel(b, alu->src[0].src.ssa, index1, index2);
+   nir_intrinsic_instr *shuffle =
+      nir_intrinsic_instr_create(b->shader, nir_intrinsic_shuffle);
+   shuffle->src[0] = nir_src_for_ssa(index);
+   shuffle->src[1] = nir_src_for_ssa(data1);
+   shuffle->num_components = alu->dest.dest.ssa.num_components;
+   nir_ssa_dest_init(&shuffle->instr, &shuffle->dest,
+                     alu->dest.dest.ssa.num_components,
+                     alu->dest.dest.ssa.bit_size, NULL);
+   nir_builder_instr_insert(b, &shuffle->instr);
+
+   return &shuffle->dest.ssa;
+}
+
+static bool
+opt_intrinsics_alu(nir_builder *b, nir_alu_instr *alu)
+{
+   nir_ssa_def *replacement = NULL;
+
+   switch (alu->op) {
+   case nir_op_bcsel:
+      replacement = try_opt_bcsel_of_shuffle(b, alu);
+      break;
+
+   default:
+      break;
+   }
+
+   if (replacement) {
+      nir_ssa_def_rewrite_uses(&alu->dest.dest.ssa,
+                               nir_src_for_ssa(replacement));
+      nir_instr_remove(&alu->instr);
+      return true;
+   } else {
+      return false;
+   }
+}
+
+static bool
 opt_intrinsics_intrin(nir_builder *b, nir_intrinsic_instr *intrin,
                       const struct nir_shader_compiler_options *options)
 {
@@ -89,6 +190,11 @@ opt_intrinsics_impl(nir_function_impl *impl,
          b.cursor = nir_before_instr(instr);
 
          switch (instr->type) {
+         case nir_instr_type_alu:
+            if (opt_intrinsics_alu(&b, nir_instr_as_alu(instr)))
+               progress = true;
+            break;
+
          case nir_instr_type_intrinsic:
             if (opt_intrinsics_intrin(&b, nir_instr_as_intrinsic(instr),
                                       options))
