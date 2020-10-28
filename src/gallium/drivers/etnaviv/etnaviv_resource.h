79d97c619160af84cd4058248f6515f3957acefe
Lucas Stach <l.stach@pengutronix.de>
2020-10-28 20:13:45 +0100

    etnaviv: add support for sharing the TS buffer
    This adds support for sharing the TS buffer, which up until now has been
    an internal implementation detail, with the outside world. This mainly
    improves performance with a GPU compositor present, but on i.MX8M also
    direct to display use-cases benefit.
    
    The impact of this change depends on the GPU generation:
    - old GPUs with a single pipe won't see any difference
    - GC2000 can skip the TS resolve in the client and will benefit from a
      more efficient blit into the sampler compatible format when the client
      buffer contains cleared tiles
    - GC3000 can directly sample with TS support, so saves both write and read
      memory bandwidth when the client buffer contains cleared tiles
    - GC7000 with compression support can keep the client buffer in compressed
      format, thus saving both read and write bandwidth even for fully filled
      client buffers
    - GC7000 coupled to a display unit supporting the compression format (DCSS
      on i.MX8M) does not even need to uncompress the render buffer for display
      so will see significant bandwidth saving even when GPU compositing is
      bypassed
    
    There is a slight complication in that the tile clear color isn't part of
    the TS buffer, but is programmed into state registers in the GPU. To handle
    this externally shared TS buffers now contain a software metadata area,
    where the clear color is stored by the driver, so the receiving end of the
    TS buffer can retrieve the clear color from this area.
    
    The compression format is handled in the same way by storing it in the SW
    meta area. While we can derive the compression format from the color buffer
    format in most cases, some users, like weston, expect that they can "upgrade"
    ARGB to XRGB color formats. While this works with plain color formats, as
    it's just masking a channel, the compression format differs when alpha is in
    use. Receivers of the TS buffer should thus not try to infer the compression
    format from the color buffer format, but instead fetch it from the SW meta.
    
    The import/export handling of the TS buffer is modelled after the Intel iris
    driver: we add a separate plane for the TS buffer and fold it into the base
    resource after the import.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- src/gallium/drivers/etnaviv/etnaviv_resource.h
+++ src/gallium/drivers/etnaviv/etnaviv_resource.h
@@ -36,9 +36,17 @@
 #include "util/u_helpers.h"
 #include "util/u_range.h"
 
+#include "drm-uapi/drm_fourcc.h"
+
 struct etna_context;
 struct pipe_screen;
 struct util_dynarray;
+
+struct etna_ts_sw_meta {
+   uint64_t clear_value;
+   uint32_t comp_format;
+   uint8_t pad[52];
+};
 
 struct etna_resource_level {
    unsigned width, padded_width; /* in pixels */
@@ -79,10 +87,12 @@ struct etna_resource {
    /* only lod 0 used for non-texture buffers */
    /* Layout for surface (tiled, multitiled, split tiled, ...) */
    enum etna_surface_layout layout;
+   uint64_t modifier;
    /* Horizontal alignment for texture unit (TEXTURE_HALIGN_*) */
    unsigned halign;
    struct etna_bo *bo; /* Surface video memory */
    struct etna_bo *ts_bo; /* Tile status video memory */
+   struct renderonly_scanout *ts_scanout; /* display compatible TS */
 
    struct etna_resource_level levels[ETNA_NUM_LOD];
 
@@ -147,6 +157,22 @@ etna_resource_hw_tileable(bool use_blt, const struct pipe_resource *pres)
           util_format_get_blocksize(pres->format) == 4;
 }
 
+/* returns TRUE is resource TS buffer is exposed externally */
+static inline bool
+etna_resource_ext_ts(const struct etna_resource *res)
+{
+   return res->modifier & VIVANTE_MOD_TS_MASK;
+}
+
+static inline bool
+etna_resource_unfinished_ts_import(const struct etna_resource *res)
+{
+   return etna_resource_ext_ts(res) && !res->ts_bo && res->base.next;
+}
+
+void etna_resource_finish_ts_import(struct pipe_screen *pscreen,
+                                    struct etna_resource *res);
+
 static inline struct etna_resource *
 etna_resource(struct pipe_resource *p)
 {
@@ -175,7 +201,8 @@ resource_written(struct etna_context *ctx, struct pipe_resource *prsc)
  * This is also called "fast clear". */
 bool
 etna_screen_resource_alloc_ts(struct pipe_screen *pscreen,
-                              struct etna_resource *prsc);
+                              struct etna_resource *prsc,
+                              uint64_t modifier);
 
 struct pipe_resource *
 etna_resource_alloc(struct pipe_screen *pscreen, unsigned layout,
