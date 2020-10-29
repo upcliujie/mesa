d29c396ecc5b0ecea39eb86b9da62f4b846ce462
Jason Ekstrand <jason@jlekstrand.net>
2020-10-29 12:01:22 -0500

    spirv: Implement OpTerminateRayKHR and OpIgnoreIntersectionKHR
    In the final version of SPV_KHR_ray_tracing, these are now block
    terminators like OpKill or OpReturn.  This means that they need special
    handling in vtn_cfg.c.

--- src/compiler/spirv/vtn_private.h
+++ src/compiler/spirv/vtn_private.h
@@ -130,7 +130,9 @@ enum vtn_branch_type {
    vtn_branch_type_loop_continue,
    vtn_branch_type_loop_back_edge,
    vtn_branch_type_discard,
-   vtn_branch_type_terminate,
+   vtn_branch_type_terminate_invocation,
+   vtn_branch_type_ignore_intersection,
+   vtn_branch_type_terminate_ray,
    vtn_branch_type_return,
 };
 
