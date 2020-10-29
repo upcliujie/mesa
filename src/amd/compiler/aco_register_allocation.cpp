a7a21796bc4fdc6dd32a53b54b0002de316dcdcb
Tony Wasserka <tony.wasserka@gmx.de>
2020-10-29 11:55:28 +0100

    aco/ra: Remove unused function parameter

--- src/amd/compiler/aco_register_allocation.cpp
+++ src/amd/compiler/aco_register_allocation.cpp
@@ -1203,7 +1203,6 @@ std::pair<PhysReg, bool> get_reg_impl(ra_ctx& ctx,
 bool get_reg_specified(ra_ctx& ctx,
                        RegisterFile& reg_file,
                        RegClass rc,
-                       std::vector<std::pair<Operand, Definition>>& parallelcopies,
                        aco_ptr<Instruction>& instr,
                        PhysReg reg)
 {
@@ -1260,7 +1259,7 @@ PhysReg get_reg(ra_ctx& ctx,
          if (affinity_it != ctx.affinities.end() && ctx.assignments[affinity_it->second].assigned) {
             PhysReg reg = ctx.assignments[affinity_it->second].reg;
             reg.reg_b -= offset;
-            if (get_reg_specified(ctx, reg_file, temp.regClass(), parallelcopies, instr, reg))
+            if (get_reg_specified(ctx, reg_file, temp.regClass(), instr, reg))
                return reg;
          }
          offset += def.bytes();
@@ -1270,7 +1269,7 @@ PhysReg get_reg(ra_ctx& ctx,
    if (ctx.affinities.find(temp.id()) != ctx.affinities.end() &&
        ctx.assignments[ctx.affinities[temp.id()]].assigned) {
       PhysReg reg = ctx.assignments[ctx.affinities[temp.id()]].reg;
-      if (get_reg_specified(ctx, reg_file, temp.regClass(), parallelcopies, instr, reg))
+      if (get_reg_specified(ctx, reg_file, temp.regClass(), instr, reg))
          return reg;
    }
 
@@ -1291,7 +1290,7 @@ PhysReg get_reg(ra_ctx& ctx,
              ctx.assignments[op.tempId()].assigned) {
             PhysReg reg = ctx.assignments[op.tempId()].reg;
             reg.reg_b += (byte_offset - k);
-            if (get_reg_specified(ctx, reg_file, temp.regClass(), parallelcopies, instr, reg))
+            if (get_reg_specified(ctx, reg_file, temp.regClass(), instr, reg))
                return reg;
          }
          k += op.bytes();
@@ -1303,7 +1302,7 @@ PhysReg get_reg(ra_ctx& ctx,
       if (res.second) {
          reg.reg_b += byte_offset;
          /* make sure to only use byte offset if the instruction supports it */
-         if (get_reg_specified(ctx, reg_file, temp.regClass(), parallelcopies, instr, reg))
+         if (get_reg_specified(ctx, reg_file, temp.regClass(), instr, reg))
             return reg;
       }
    }
@@ -1975,7 +1974,7 @@ void register_allocation(Program *program, std::vector<IDSet>& live_out_per_bloc
                /* we tried this already on the previous loop */
                if (reg == scc || reg == exec)
                   continue;
-               if (get_reg_specified(ctx, register_file, definition.regClass(), parallelcopy, phi, reg)) {
+               if (get_reg_specified(ctx, register_file, definition.regClass(), phi, reg)) {
                   definition.setFixed(reg);
                   break;
                }
@@ -2251,7 +2250,7 @@ void register_allocation(Program *program, std::vector<IDSet>& live_out_per_bloc
                PhysReg reg = instr->operands[0].physReg();
                for (unsigned j = 0; j < i; j++)
                   reg.reg_b += instr->definitions[j].bytes();
-               if (get_reg_specified(ctx, register_file, definition->regClass(), parallelcopy, instr, reg))
+               if (get_reg_specified(ctx, register_file, definition->regClass(), instr, reg))
                   definition->setFixed(reg);
             } else if (instr->opcode == aco_opcode::p_wqm || instr->opcode == aco_opcode::p_parallelcopy) {
                PhysReg reg = instr->operands[i].physReg();
