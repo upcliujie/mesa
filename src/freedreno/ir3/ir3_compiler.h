5953473fbb522e704ba876f22da60547a5f086d7
Connor Abbott <cwabbott0@gmail.com>
2020-10-30 16:38:40 +0100

    ir3: Enable nir_lower_vars_to_scratch on a6xx

--- src/freedreno/ir3/ir3_compiler.h
+++ src/freedreno/ir3/ir3_compiler.h
@@ -108,6 +108,9 @@ struct ir3_compiler {
 
 	/* Whether clip+cull distances are supported */
 	bool has_clip_cull;
+
+	/* Whether private memory is supported */
+	bool has_pvtmem;
 };
 
 void ir3_compiler_destroy(struct ir3_compiler *compiler);
