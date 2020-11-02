5b4c0f41f15759bb37ca2507f9f2dbeccff0c1a2
Marek Olšák <marek.olsak@amd.com>
2020-11-02 01:46:24 -0500

    vbo: remove _mesa_prim parameter from vbo_try_prim_conversion

--- src/mesa/vbo/vbo_exec_api.c
+++ src/mesa/vbo/vbo_exec_api.c
@@ -867,7 +867,7 @@ try_vbo_merge(struct vbo_exec_context *exec)
 
    assert(exec->vtx.prim_count >= 1);
 
-   vbo_try_prim_conversion(cur);
+   vbo_try_prim_conversion(&cur->mode, &cur->count);
 
    if (exec->vtx.prim_count >= 2) {
       struct _mesa_prim *prev = &exec->vtx.prim[exec->vtx.prim_count - 2];
