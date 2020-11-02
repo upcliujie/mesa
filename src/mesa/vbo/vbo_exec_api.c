d0f09c98a069083c37d0b5f2dc0b6f011ae7fa87
Marek Olšák <marek.olsak@amd.com>
2020-11-02 01:57:48 -0500

    vbo: only set count and end when closing _mesa_prim
    It's already set everywhere except vbo_exec_wrap_buffers.

--- src/mesa/vbo/vbo_exec_api.c
+++ src/mesa/vbo/vbo_exec_api.c
@@ -83,13 +83,13 @@ vbo_exec_wrap_buffers(struct vbo_exec_context *exec)
    else {
       struct _mesa_prim *last_prim = &exec->vtx.prim[exec->vtx.prim_count - 1];
       const GLuint last_begin = last_prim->begin;
-      GLuint last_count;
+      GLuint last_count = 0;
 
       if (_mesa_inside_begin_end(exec->ctx)) {
          last_prim->count = exec->vtx.vert_count - last_prim->start;
+         last_count = last_prim->count;
+         last_prim->end = 0;
       }
-
-      last_count = last_prim->count;
 
       /* Special handling for wrapping GL_LINE_LOOP */
       if (last_prim->mode == GL_LINE_LOOP &&
@@ -123,9 +123,7 @@ vbo_exec_wrap_buffers(struct vbo_exec_context *exec)
       if (_mesa_inside_begin_end(exec->ctx)) {
          exec->vtx.prim[0].mode = exec->ctx->Driver.CurrentExecPrimitive;
          exec->vtx.prim[0].begin = 0;
-         exec->vtx.prim[0].end = 0;
          exec->vtx.prim[0].start = 0;
-         exec->vtx.prim[0].count = 0;
          exec->vtx.prim_count++;
 
          if (exec->vtx.copied.nr == last_count)
@@ -839,9 +837,7 @@ vbo_exec_Begin(GLenum mode)
    i = exec->vtx.prim_count++;
    exec->vtx.prim[i].mode = mode;
    exec->vtx.prim[i].begin = 1;
-   exec->vtx.prim[i].end = 0;
    exec->vtx.prim[i].start = exec->vtx.vert_count;
-   exec->vtx.prim[i].count = 0;
 
    ctx->Driver.CurrentExecPrimitive = mode;
 
