e080495e9583037ceeb7a031bed95e4c7e120202
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-11-02 13:35:33 -0500

    zink: move zink_flush_compute() users to zink_flush_queue()

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -1341,12 +1341,6 @@ zink_batch_no_rp(struct zink_context *ctx)
 }
 
 void
-zink_flush_compute(struct zink_context *ctx)
-{
-   flush_batch(ctx, ZINK_QUEUE_COMPUTE);
-}
-
-void
 zink_flush_queue(struct zink_context *ctx, enum zink_queue queue)
 {
    flush_batch(ctx, queue);
@@ -1861,7 +1855,7 @@ zink_texture_barrier(struct pipe_context *pctx, unsigned flags)
    struct zink_context *ctx = zink_context(pctx);
    if (zink_batch_g(ctx)->has_work)
       pctx->flush(pctx, NULL, 0);
-   zink_flush_compute(ctx);
+   zink_flush_queue(ctx, ZINK_QUEUE_COMPUTE);
 }
 
 static void
