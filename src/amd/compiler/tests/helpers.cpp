70c54fa3fe4eb4bcd98cfa74c42866af0967c53b
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2020-11-04 13:52:05 +0100

    aco/tests: initialize debug.func to avoid crashes when errors are logged
    We don't need to register debug callbacks for tests.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/compiler/tests/helpers.cpp
+++ src/amd/compiler/tests/helpers.cpp
@@ -80,6 +80,8 @@ void create_program(enum chip_class chip_class, Stage stage, unsigned wave_size,
    info.wave_size = wave_size;
 
    program.reset(new Program);
+   program->debug.func = NULL;
+
    aco::init_program(program.get(), stage, &info, chip_class, family, &config);
 
    Block *block = program->create_and_insert_block();
