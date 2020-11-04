4fd157dd49b55f123ea35c49d91b4bed099dadf8
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2020-11-04 08:08:45 +0100

    radv: compact MRTs to save PS export memory space
    If there are holes between color outputs (e.g. a shader exports MRT1,
    but not MRT0), we can remove the holes by moving higher MRTs lower.
    
    The hardware will remap the MRTs to their correct locations if we remove
    holes in SPI_SHADER_COL_FORMAT but not CB_SHADER_MASK.
    
    This re-applies 7a5e6fd2 ("radv: add support for MRTs compaction
    to avoid holes").
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/compiler/aco_instruction_selection.cpp
+++ src/amd/compiler/aco_instruction_selection.cpp
@@ -10260,15 +10260,15 @@ static bool export_fs_mrt_z(isel_context *ctx)
    return true;
 }
 
-static bool export_fs_mrt_color(isel_context *ctx, int slot)
+static bool export_fs_mrt_color(isel_context *ctx, int slot,
+                                unsigned write_mask, Temp *outputs)
 {
    Builder bld(ctx->program, ctx->block);
-   unsigned write_mask = ctx->outputs.mask[slot];
    Operand values[4];
 
    for (unsigned i = 0; i < 4; ++i) {
       if (write_mask & (1 << i)) {
-         values[i] = Operand(ctx->outputs.temps[slot * 4u + i]);
+         values[i] = Operand(outputs[i]);
       } else {
          values[i] = Operand(v1);
       }
@@ -10278,7 +10278,6 @@ static bool export_fs_mrt_color(isel_context *ctx, int slot)
    unsigned enabled_channels = 0;
    aco_opcode compr_op = (aco_opcode)0;
 
-   slot -= FRAG_RESULT_DATA0;
    target = V_008DFC_SQ_EXP_MRT + slot;
    col_format = (ctx->options->key.fs.col_format >> (4 * slot)) & 0xf;
 
@@ -10469,6 +10468,7 @@ static void create_fs_null_export(isel_context *ctx)
 
 static void create_fs_exports(isel_context *ctx)
 {
+   unsigned compacted_mrt_index = 0;
    bool exported = false;
 
    /* Export depth, stencil and sample mask. */
@@ -10478,9 +10478,15 @@ static void create_fs_exports(isel_context *ctx)
       exported |= export_fs_mrt_z(ctx);
 
    /* Export all color render targets. */
-   for (unsigned i = FRAG_RESULT_DATA0; i < FRAG_RESULT_DATA7 + 1; ++i)
+   for (unsigned i = FRAG_RESULT_DATA0; i < FRAG_RESULT_DATA7 + 1; ++i) {
       if (ctx->outputs.mask[i])
-         exported |= export_fs_mrt_color(ctx, i);
+         if (export_fs_mrt_color(ctx, compacted_mrt_index,
+                                 ctx->outputs.mask[i],
+                                 &ctx->outputs.temps[i * 4u])) {
+            compacted_mrt_index++;
+            exported = true;
+         }
+   }
 
    if (!exported)
       create_fs_null_export(ctx);
