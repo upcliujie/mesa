9ff90924006cb818bc2c54bd2643218a3b3474d0
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-11-05 10:28:32 -0500

    zink: simplify bufferview and imageview descriptor state hashing
    now that we have the hash for the internal objects available at all times, we
    can directly reuse this instead of computing it each time, giving us more
    accurate hashes as well as saving us cpu cycles

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -82,31 +82,6 @@ calc_descriptor_state_hash_ssbo(struct zink_context *ctx, struct zink_shader *zs
    return XXH32(hash_data, data_size, hash);
 }
 
-static void
-calc_descriptor_hash_sampler_view(struct zink_context *ctx, struct zink_sampler_view *sampler_view)
-{
-   void *hash_data;
-   size_t data_size;
-   struct zink_screen *screen = zink_screen(ctx->base.screen);
-
-   if (!sampler_view) {
-      sampler_view->hash = screen->null_descriptor_hashes.sampler_view;
-      return;
-   }
-
-   uint32_t hash = _mesa_hash_pointer(sampler_view->base.texture);
-   if (sampler_view->base.target == PIPE_BUFFER) {
-      hash_data = &sampler_view->base.u.buf;
-      data_size = sizeof(sampler_view->base.u.buf);
-      sampler_view->hash = XXH32(hash_data, data_size, hash);
-      return;
-   }
-
-   hash_data = &sampler_view->image_view;
-   data_size = sizeof(VkImageView);
-   sampler_view->hash = XXH32(hash_data, data_size, hash);
-}
-
 static uint32_t
 calc_descriptor_state_hash_sampler(struct zink_context *ctx, struct zink_shader *zs, enum pipe_shader_type shader, int i, int idx, uint32_t hash)
 {
@@ -118,7 +93,7 @@ calc_descriptor_state_hash_sampler(struct zink_context *ctx, struct zink_shader 
          hash = maybe_hash_u32(screen->null_descriptor_hashes.sampler_view, hash);
          continue;
       }
-      hash = maybe_hash_u32(sampler_view->hash, hash);
+      hash = maybe_hash_u32(get_sampler_view_hash(sampler_view), hash);
       if (sampler_view->base.target == PIPE_BUFFER)
          continue;
 
@@ -154,17 +129,7 @@ calc_descriptor_state_hash_image(struct zink_context *ctx, struct zink_shader *z
          hash = XXH32(hash_data, data_size, hash);
          break;
       }
-      struct zink_resource *res = zink_resource(ctx->image_views[shader][idx + k].base.resource);
-      if (res->base.target == PIPE_BUFFER) {
-         hash = XXH32(&ctx->image_views[shader][idx + k].base.resource, sizeof(void*), hash);
-         hash_data = &ctx->image_views[shader][idx + k].base.u.buf;
-         data_size = sizeof(ctx->image_views[shader][idx + k].base.u.buf);
-         hash = XXH32(hash_data, data_size, hash);
-      } else {
-         hash_data = &ctx->image_views[shader][idx + k].surface->image_view;
-         data_size = sizeof(VkImageView);
-         hash = XXH32(hash_data, data_size, hash);
-      }
+      hash = maybe_hash_u32(get_image_view_hash(&ctx->image_views[shader][idx + k]), hash);
    }
    return hash;
 }
@@ -688,7 +653,6 @@ zink_create_sampler_view(struct pipe_context *pctx, struct pipe_resource *pres,
       return NULL;
    }
    util_dynarray_init(&sampler_view->desc_set_refs.refs, NULL);
-   calc_descriptor_hash_sampler_view(zink_context(pctx), sampler_view);
    return &sampler_view->base;
 }
 
@@ -983,8 +947,10 @@ zink_set_sampler_views(struct pipe_context *pctx,
    for (unsigned i = 0; i < num_views; ++i) {
       struct zink_sampler_view *a = zink_sampler_view(ctx->sampler_views[shader_type][start_slot + i]);
       struct zink_sampler_view *b = zink_sampler_view(views[i]);
+      uint32_t hash_a = get_sampler_view_hash(a);
+      uint32_t hash_b = get_sampler_view_hash(b);
       if (usage & BITFIELD64_BIT(start_slot + i))
-         update |= !!a != !!b || (a && a->hash != b->hash);
+         update |= !!a != !!b || hash_a != hash_b;
       pipe_sampler_view_reference(&ctx->sampler_views[shader_type][start_slot + i], views[i]);
    }
    ctx->num_sampler_views[shader_type] = start_slot + num_views;
