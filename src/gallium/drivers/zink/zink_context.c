0b25ab4a0e3420c334c2ef2563b283beb0be2038
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-11-05 12:10:57 -0500

    zink: move active query pruning to batch reset
    this didn't ever really need to be on the fence in the first place

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -2241,6 +2241,7 @@ init_batch(struct zink_context *ctx, struct zink_batch *batch, unsigned idx)
       return false;
 
    batch->fbs = _mesa_pointer_set_create(NULL);
+   batch->active_queries = _mesa_pointer_set_create(NULL);
    batch->resources = _mesa_pointer_set_create(NULL);
    batch->sampler_views = _mesa_pointer_set_create(NULL);
    batch->surfaces = _mesa_pointer_set_create(NULL);
