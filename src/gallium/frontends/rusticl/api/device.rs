6f84b0d3e669d87862c1320a8a9abbd89e661b9d
Karol Herbst <kherbst@redhat.com>
2020-11-08 20:28:21 +0100

    rusticl: added
    We also export all function required by cl_khr_icd.

---
+++ src/gallium/frontends/rusticl/api/device.rs
@@ -0,0 +1,239 @@
+extern crate mesa_rust_util;
+extern crate rusticl_opencl_gen;
+
+use crate::api::platform::get_platform;
+use crate::api::util::*;
+use crate::core::device::*;
+
+use self::mesa_rust_util::ptr::*;
+use self::rusticl_opencl_gen::*;
+
+use std::cmp::min;
+use std::mem::size_of;
+use std::ptr;
+use std::sync::Once;
+
+impl CLInfo<cl_device_info> for crate::core::device::_cl_device_id {
+    fn query(&self, q: cl_device_info) -> Result<Vec<u8>, cl_int> {
+        Ok(match q {
+            CL_DEVICE_ADDRESS_BITS => cl_prop::<cl_uint>(self.address_bits()),
+            CL_DEVICE_ATOMIC_FENCE_CAPABILITIES => cl_prop::<cl_device_atomic_capabilities>(0),
+            CL_DEVICE_ATOMIC_MEMORY_CAPABILITIES => cl_prop::<cl_device_atomic_capabilities>(0),
+            CL_DEVICE_AVAILABLE => cl_prop::<bool>(true),
+            CL_DEVICE_BUILT_IN_KERNELS => cl_prop::<&str>(""),
+            CL_DEVICE_BUILT_IN_KERNELS_WITH_VERSION => cl_prop::<Vec<cl_name_version>>(Vec::new()),
+            CL_DEVICE_COMPILER_AVAILABLE => cl_prop::<bool>(true),
+            CL_DEVICE_DEVICE_ENQUEUE_CAPABILITIES => {
+                cl_prop::<cl_device_device_enqueue_capabilities>(0)
+            }
+            CL_DEVICE_DOUBLE_FP_CONFIG => cl_prop::<cl_device_fp_config>(0),
+            CL_DEVICE_ENDIAN_LITTLE => cl_prop::<bool>(self.little_endian()),
+            CL_DEVICE_ERROR_CORRECTION_SUPPORT => cl_prop::<bool>(false),
+            CL_DEVICE_EXECUTION_CAPABILITIES => {
+                cl_prop::<cl_device_exec_capabilities>(CL_EXEC_KERNEL.into())
+            }
+            CL_DEVICE_EXTENSIONS => cl_prop::<&str>(&self.extension_string),
+            CL_DEVICE_EXTENSIONS_WITH_VERSION => cl_prop::<&Vec<cl_name_version>>(&self.extensions),
+            CL_DEVICE_GENERIC_ADDRESS_SPACE_SUPPORT => cl_prop::<bool>(false),
+            CL_DEVICE_GLOBAL_MEM_CACHE_TYPE => cl_prop::<cl_device_mem_cache_type>(CL_NONE),
+            CL_DEVICE_GLOBAL_MEM_CACHE_SIZE => cl_prop::<cl_ulong>(0),
+            CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE => cl_prop::<cl_uint>(0),
+            CL_DEVICE_GLOBAL_MEM_SIZE => cl_prop::<cl_ulong>(self.global_mem_size()),
+            CL_DEVICE_GLOBAL_VARIABLE_PREFERRED_TOTAL_SIZE => cl_prop::<usize>(0),
+            CL_DEVICE_HALF_FP_CONFIG => cl_prop::<cl_device_fp_config>(0),
+            CL_DEVICE_HOST_UNIFIED_MEMORY => cl_prop::<bool>(self.unified_memory()),
+            CL_DEVICE_IL_VERSION => cl_prop::<&str>(""),
+            CL_DEVICE_ILS_WITH_VERSION => cl_prop::<Vec<cl_name_version>>(Vec::new()),
+            CL_DEVICE_IMAGE_BASE_ADDRESS_ALIGNMENT => {
+                cl_prop::<cl_uint>(self.image_base_address_alignment())
+            }
+            CL_DEVICE_IMAGE_MAX_ARRAY_SIZE => cl_prop::<usize>(self.image_array_size()),
+            CL_DEVICE_IMAGE_MAX_BUFFER_SIZE => cl_prop::<usize>(self.image_buffer_size()),
+            CL_DEVICE_IMAGE_PITCH_ALIGNMENT => cl_prop::<cl_uint>(0),
+            CL_DEVICE_IMAGE_SUPPORT => cl_prop::<bool>(self.image_supported()),
+            CL_DEVICE_IMAGE2D_MAX_HEIGHT => cl_prop::<usize>(self.image_2d_size()),
+            CL_DEVICE_IMAGE2D_MAX_WIDTH => cl_prop::<usize>(self.image_2d_size()),
+            CL_DEVICE_IMAGE3D_MAX_HEIGHT => cl_prop::<usize>(self.image_3d_size()),
+            CL_DEVICE_IMAGE3D_MAX_WIDTH => cl_prop::<usize>(self.image_3d_size()),
+            CL_DEVICE_IMAGE3D_MAX_DEPTH => cl_prop::<usize>(self.image_3d_size()),
+            CL_DEVICE_SUB_GROUP_INDEPENDENT_FORWARD_PROGRESS => cl_prop::<bool>(false),
+            CL_DEVICE_LATEST_CONFORMANCE_VERSION_PASSED => cl_prop::<&str>(""),
+            CL_DEVICE_LINKER_AVAILABLE => cl_prop::<bool>(true),
+            CL_DEVICE_LOCAL_MEM_SIZE => cl_prop::<cl_ulong>(self.local_mem_size()),
+            // TODO add query for CL_LOCAL vs CL_GLOBAL
+            CL_DEVICE_LOCAL_MEM_TYPE => cl_prop::<cl_device_local_mem_type>(CL_GLOBAL),
+            CL_DEVICE_MAX_CLOCK_FREQUENCY => cl_prop::<cl_uint>(self.max_clock_freq()),
+            CL_DEVICE_MAX_COMPUTE_UNITS => cl_prop::<cl_uint>(self.max_compute_units()),
+            // TODO atm implemented as mem_const
+            CL_DEVICE_MAX_CONSTANT_ARGS => cl_prop::<cl_uint>(1024),
+            CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE => cl_prop::<cl_ulong>(self.const_max_size()),
+            CL_DEVICE_MAX_GLOBAL_VARIABLE_SIZE => cl_prop::<usize>(0),
+            CL_DEVICE_MAX_MEM_ALLOC_SIZE => cl_prop::<cl_ulong>(self.max_mem_alloc()),
+            CL_DEVICE_MAX_NUM_SUB_GROUPS => cl_prop::<cl_uint>(0),
+            CL_DEVICE_MAX_ON_DEVICE_EVENTS => cl_prop::<cl_uint>(0),
+            CL_DEVICE_MAX_ON_DEVICE_QUEUES => cl_prop::<cl_uint>(0),
+            CL_DEVICE_MAX_PARAMETER_SIZE => cl_prop::<usize>(self.param_max_size()),
+            CL_DEVICE_MAX_PIPE_ARGS => cl_prop::<cl_uint>(0),
+            CL_DEVICE_MAX_READ_IMAGE_ARGS => cl_prop::<cl_uint>(self.image_read_count()),
+            CL_DEVICE_MAX_READ_WRITE_IMAGE_ARGS => cl_prop::<cl_uint>(0),
+            CL_DEVICE_MAX_SAMPLERS => cl_prop::<cl_uint>(self.max_samplers()),
+            CL_DEVICE_MAX_WORK_GROUP_SIZE => cl_prop::<usize>(self.max_threads_per_block()),
+            CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS => cl_prop::<cl_uint>(self.max_grid_dimensions()),
+            CL_DEVICE_MAX_WORK_ITEM_SIZES => cl_prop::<Vec<usize>>(self.max_block_sizes()),
+            CL_DEVICE_MAX_WRITE_IMAGE_ARGS => cl_prop::<cl_uint>(self.image_write_count()),
+            // TODO proper retrival from devices
+            CL_DEVICE_MEM_BASE_ADDR_ALIGN => cl_prop::<cl_uint>(0x1000),
+            CL_DEVICE_MIN_DATA_TYPE_ALIGN_SIZE => {
+                cl_prop::<cl_uint>(size_of::<cl_ulong16>() as cl_uint)
+            }
+            CL_DEVICE_NAME => cl_prop(self.screen().name()),
+            CL_DEVICE_NATIVE_VECTOR_WIDTH_CHAR => cl_prop::<cl_uint>(1),
+            CL_DEVICE_NATIVE_VECTOR_WIDTH_DOUBLE => cl_prop::<cl_uint>(0),
+            CL_DEVICE_NATIVE_VECTOR_WIDTH_FLOAT => cl_prop::<cl_uint>(1),
+            CL_DEVICE_NATIVE_VECTOR_WIDTH_HALF => cl_prop::<cl_uint>(0),
+            CL_DEVICE_NATIVE_VECTOR_WIDTH_INT => cl_prop::<cl_uint>(1),
+            CL_DEVICE_NATIVE_VECTOR_WIDTH_LONG => cl_prop::<cl_uint>(1),
+            CL_DEVICE_NATIVE_VECTOR_WIDTH_SHORT => cl_prop::<cl_uint>(1),
+            CL_DEVICE_NON_UNIFORM_WORK_GROUP_SUPPORT => cl_prop::<bool>(false),
+            CL_DEVICE_NUMERIC_VERSION => cl_prop::<cl_version>(self.cl_version as cl_version),
+            // TODO subdevice support
+            CL_DEVICE_PARENT_DEVICE => cl_prop::<cl_device_id>(ptr::null()),
+            CL_DEVICE_PARTITION_AFFINITY_DOMAIN => cl_prop::<cl_device_affinity_domain>(0),
+            CL_DEVICE_PARTITION_MAX_SUB_DEVICES => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PIPE_MAX_ACTIVE_RESERVATIONS => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PIPE_MAX_PACKET_SIZE => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PIPE_SUPPORT => cl_prop::<bool>(false),
+            CL_DEVICE_PLATFORM => cl_prop::<cl_platform_id>(get_platform()),
+            CL_DEVICE_PREFERRED_GLOBAL_ATOMIC_ALIGNMENT => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PREFERRED_INTEROP_USER_SYNC => cl_prop::<bool>(true),
+            CL_DEVICE_PREFERRED_LOCAL_ATOMIC_ALIGNMENT => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PREFERRED_PLATFORM_ATOMIC_ALIGNMENT => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PREFERRED_VECTOR_WIDTH_CHAR => cl_prop::<cl_uint>(1),
+            CL_DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT => cl_prop::<cl_uint>(1),
+            CL_DEVICE_PREFERRED_VECTOR_WIDTH_HALF => cl_prop::<cl_uint>(0),
+            CL_DEVICE_PREFERRED_VECTOR_WIDTH_INT => cl_prop::<cl_uint>(1),
+            CL_DEVICE_PREFERRED_VECTOR_WIDTH_LONG => cl_prop::<cl_uint>(1),
+            CL_DEVICE_PREFERRED_VECTOR_WIDTH_SHORT => cl_prop::<cl_uint>(1),
+            CL_DEVICE_PREFERRED_WORK_GROUP_SIZE_MULTIPLE => cl_prop::<usize>(1),
+            // TODO
+            CL_DEVICE_PRINTF_BUFFER_SIZE => cl_prop::<usize>(0),
+            // TODO
+            CL_DEVICE_PROFILING_TIMER_RESOLUTION => cl_prop::<usize>(0),
+            CL_DEVICE_OPENCL_C_FEATURES => cl_prop::<Vec<cl_name_version>>(Vec::new()),
+            CL_DEVICE_OPENCL_C_VERSION => cl_prop::<String>(
+                [
+                    String::from("OpenCL C "),
+                    self.clc_version.into(),
+                    String::from(" "),
+                ]
+                .join(""),
+            ),
+            CL_DEVICE_OPENCL_C_ALL_VERSIONS => cl_prop::<&Vec<cl_name_version>>(&self.clc_versions),
+            CL_DEVICE_PROFILE => cl_prop(if self.embedded {
+                "EMBEDDED_PROFILE"
+            } else {
+                "FULL_PROFILE"
+            }),
+            CL_DEVICE_QUEUE_ON_DEVICE_MAX_SIZE => cl_prop::<cl_uint>(0),
+            CL_DEVICE_QUEUE_ON_DEVICE_PREFERRED_SIZE => cl_prop::<cl_uint>(0),
+            CL_DEVICE_QUEUE_ON_DEVICE_PROPERTIES => cl_prop::<cl_command_queue_properties>(0),
+            CL_DEVICE_QUEUE_ON_HOST_PROPERTIES => {
+                cl_prop::<cl_command_queue_properties>(CL_QUEUE_PROFILING_ENABLE.into())
+            }
+            // TODO sub devices
+            CL_DEVICE_REFERENCE_COUNT => cl_prop::<cl_uint>(1),
+            CL_DEVICE_SINGLE_FP_CONFIG => cl_prop::<cl_device_fp_config>(
+                (CL_FP_ROUND_TO_ZERO | CL_FP_ROUND_TO_NEAREST) as cl_device_fp_config,
+            ),
+            CL_DEVICE_SVM_CAPABILITIES => cl_prop::<cl_device_svm_capabilities>(0),
+            CL_DEVICE_TYPE => cl_prop::<cl_device_type>(self.device_type()),
+            CL_DEVICE_VENDOR => cl_prop(self.screen().device_vendor()),
+            CL_DEVICE_VENDOR_ID => cl_prop::<cl_uint>(self.vendor_id()),
+            CL_DEVICE_VERSION => {
+                cl_prop::<String>([String::from("OpenCL "), self.cl_version.into()].join(""))
+            }
+            CL_DRIVER_VERSION => cl_prop("0.1"),
+            CL_DEVICE_WORK_GROUP_COLLECTIVE_FUNCTIONS_SUPPORT => cl_prop::<bool>(false),
+            // CL_INVALID_VALUE if param_name is not one of the supported values
+            // CL_INVALID_VALUE [...] if param_name is a value that is available as an extension and the corresponding extension is not supported by the device.
+            _ => Err(CL_INVALID_VALUE)?,
+        })
+    }
+}
+
+// TODO replace with const new container
+static mut DEVICES: Vec<CLDeviceRef> = Vec::new();
+static INIT: Once = Once::new();
+
+fn load_devices() {
+    CLDevice::all()
+        .into_iter()
+        .for_each(|d| unsafe { DEVICES.push(d) });
+}
+
+fn devs() -> &'static Vec<CLDeviceRef> {
+    INIT.call_once(load_devices);
+    unsafe { &DEVICES }
+}
+
+pub fn get_devs_for_type(device_type: cl_device_type) -> Vec<&'static CLDeviceRef> {
+    devs().iter()
+      .filter(|d| match device_type as u32 {
+         CL_DEVICE_TYPE_ACCELERATOR => device_type & d.device_type(),
+         CL_DEVICE_TYPE_CPU => device_type & d.device_type(),
+         CL_DEVICE_TYPE_CUSTOM => device_type & d.device_type(),
+         CL_DEVICE_TYPE_DEFAULT => device_type & d.device_type(),
+         CL_DEVICE_TYPE_GPU => device_type & d.device_type(),
+         CL_DEVICE_TYPE_ALL => CL_DEVICE_TYPE_CUSTOM as u64 & !d.device_type(),
+         _ => 0,
+      } != 0)
+      .collect()
+}
+
+pub fn get_device_ids(
+    platform: cl_platform_id,
+    device_type: cl_device_type,
+    num_entries: cl_uint,
+    devices: *mut cl_device_id,
+    num_devices: *mut cl_uint,
+) -> Result<(), cl_int> {
+    // CL_INVALID_PLATFORM if platform is not a valid platform.
+    platform.check()?;
+
+    // CL_INVALID_DEVICE_TYPE if device_type is not a valid value.
+    check_cl_device_type(device_type)?;
+
+    // CL_INVALID_VALUE if num_entries is equal to zero and devices is not NULL
+    if num_entries == 0 && !devices.is_null() {
+        return Err(CL_INVALID_VALUE);
+    }
+
+    // CL_INVALID_VALUE [...] if both num_devices and devices are NULL.
+    if num_devices.is_null() && devices.is_null() {
+        return Err(CL_INVALID_VALUE);
+    }
+
+    let devs = get_devs_for_type(device_type);
+    // CL_DEVICE_NOT_FOUND if no OpenCL devices that matched device_type were found
+    if devs.is_empty() {
+        return Err(CL_DEVICE_NOT_FOUND);
+    }
+
+    // num_devices returns the number of OpenCL devices available that match device_type. If
+    // num_devices is NULL, this argument is ignored.
+    num_devices.write_checked(devs.len() as cl_uint);
+
+    if !devices.is_null() {
+        let n = min(num_entries as usize, devs.len());
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..n {
+            unsafe {
+                *devices.add(i) = devs[i].cl;
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ src/gallium/frontends/rusticl/api/program.rs
@@ -0,0 +1,266 @@
+extern crate mesa_rust;
+extern crate mesa_rust_util;
+extern crate rusticl_opencl_gen;
+
+use crate::api::types::*;
+use crate::api::util::*;
+use crate::core::device::*;
+use crate::core::program::*;
+
+use self::mesa_rust::compiler::clc::*;
+use self::mesa_rust_util::string::*;
+use self::rusticl_opencl_gen::*;
+
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::os::raw::c_char;
+use std::ptr;
+use std::slice;
+
+impl CLInfo<cl_program_info> for crate::core::program::_cl_program {
+    fn query(&self, q: cl_program_info) -> Result<Vec<u8>, cl_int> {
+        Ok(match q {
+            CL_PROGRAM_CONTEXT => cl_prop::<cl_context>(self.context.cl),
+            CL_PROGRAM_DEVICES => {
+                cl_prop::<&Vec<cl_device_id>>(&self.devs.iter().map(|d| d.cl).collect())
+            }
+            CL_PROGRAM_NUM_DEVICES => cl_prop::<cl_uint>(self.devs.len() as cl_uint),
+            CL_PROGRAM_NUM_KERNELS => cl_prop::<usize>(self.kernels.len()),
+            CL_PROGRAM_REFERENCE_COUNT => cl_prop::<cl_uint>(self.refs()),
+            CL_PROGRAM_SOURCE => cl_prop::<&CStr>(self.src.as_c_str()),
+            // CL_INVALID_VALUE if param_name is not one of the supported values
+            _ => Err(CL_INVALID_VALUE)?,
+        })
+    }
+}
+
+impl CLInfoObj<cl_program_build_info, cl_device_id> for crate::core::program::_cl_program {
+    fn query(&self, d: cl_device_id, q: cl_program_build_info) -> Result<Vec<u8>, cl_int> {
+        let dev = d.check()?;
+        Ok(match q {
+            CL_PROGRAM_BUILD_LOG => cl_prop::<String>(self.log(dev)),
+            CL_PROGRAM_BUILD_OPTIONS => cl_prop::<String>(self.options(dev)),
+            CL_PROGRAM_BUILD_STATUS => cl_prop::<cl_build_status>(self.status(dev)),
+            // CL_INVALID_VALUE if param_name is not one of the supported values
+            _ => Err(CL_INVALID_VALUE)?,
+        })
+    }
+}
+
+fn validate_devices(
+    device_list: *const cl_device_id,
+    num_devices: cl_uint,
+    default: &Vec<CLDeviceRef>,
+) -> Result<Vec<&CLDeviceRef>, cl_int> {
+    let mut devs = check_cl_objs(device_list, num_devices)?;
+
+    // If device_list is a NULL value, the compile is performed for all devices associated with
+    // program.
+    if devs.is_empty() {
+        devs = default.iter().collect();
+    }
+
+    Ok(devs)
+}
+
+fn call_cb(
+    pfn_notify: Option<ProgramCB>,
+    program: cl_program,
+    user_data: *mut ::std::os::raw::c_void,
+) {
+    if pfn_notify.is_some() {
+        unsafe { pfn_notify.unwrap()(program, user_data) };
+    }
+}
+
+pub fn create_program_with_source(
+    context: cl_context,
+    count: cl_uint,
+    strings: *mut *const c_char,
+    lengths: *const usize,
+) -> Result<cl_program, cl_int> {
+    let c = context.check()?;
+
+    // CL_INVALID_VALUE if count is zero or if strings ...
+    if count == 0 || strings.is_null() {
+        Err(CL_INVALID_VALUE)?
+    }
+
+    // ... or any entry in strings is NULL.
+    let srcs = unsafe { slice::from_raw_parts(strings, count as usize) };
+    if srcs.contains(&ptr::null()) {
+        Err(CL_INVALID_VALUE)?
+    }
+
+    let mut source = String::new();
+    // we don't want encoding or any other problems with the source to prevent compilations, so
+    // just use CString::from_vec_unchecked and to_string_lossy
+    for i in 0..count as usize {
+        unsafe {
+            if lengths.is_null() || *lengths.add(i) == 0 {
+                source.push_str(&CStr::from_ptr(*strings.add(i)).to_string_lossy());
+            } else {
+                let l = *lengths.add(i);
+                let arr = slice::from_raw_parts(*strings.add(i) as *const u8, l);
+                source.push_str(&CString::from_vec_unchecked(arr.to_vec()).to_string_lossy());
+            }
+        }
+    }
+
+    Ok(CLProgram::new(
+        c,
+        &c.devs,
+        CString::new(source).map_err(|_| CL_INVALID_VALUE)?,
+    )
+    .cl)
+}
+
+pub fn build_program(
+    program: cl_program,
+    num_devices: cl_uint,
+    device_list: *const cl_device_id,
+    options: *const c_char,
+    pfn_notify: Option<ProgramCB>,
+    user_data: *mut ::std::os::raw::c_void,
+) -> Result<(), cl_int> {
+    let mut res = true;
+    let p = program.check()?;
+    let devs = validate_devices(device_list, num_devices, &p.devs)?;
+
+    check_cb(&pfn_notify, user_data)?;
+
+    // CL_BUILD_PROGRAM_FAILURE if there is a failure to build the program executable. This error
+    // will be returned if clBuildProgram does not return until the build has completed.
+    for dev in devs {
+        res &= p.compile(&dev, c_string_to_string(options), &Vec::new());
+    }
+
+    call_cb(pfn_notify, program, user_data);
+
+    // TODO link
+
+    //• CL_INVALID_BINARY if program is created with clCreateProgramWithBinary and devices listed in device_list do not have a valid program binary loaded.
+    //• CL_INVALID_BUILD_OPTIONS if the build options specified by options are invalid.
+    //• CL_INVALID_OPERATION if the build of a program executable for any of the devices listed in device_list by a previous call to clBuildProgram for program has not completed.
+    //• CL_INVALID_OPERATION if there are kernel objects attached to program.
+    //• CL_INVALID_OPERATION if program was not created with clCreateProgramWithSource, clCreateProgramWithIL or clCreateProgramWithBinary.
+
+    if res {
+        Ok(())
+    } else {
+        Err(CL_BUILD_PROGRAM_FAILURE)
+    }
+}
+
+pub fn compile_program(
+    program: cl_program,
+    num_devices: cl_uint,
+    device_list: *const cl_device_id,
+    options: *const c_char,
+    num_input_headers: cl_uint,
+    input_headers: *const cl_program,
+    header_include_names: *mut *const c_char,
+    pfn_notify: Option<ProgramCB>,
+    user_data: *mut ::std::os::raw::c_void,
+) -> Result<(), cl_int> {
+    let mut res = true;
+    let p = program.check()?;
+    let devs = validate_devices(device_list, num_devices, &p.devs)?;
+
+    check_cb(&pfn_notify, user_data)?;
+
+    // CL_INVALID_VALUE if num_input_headers is zero and header_include_names or input_headers are
+    // not NULL or if num_input_headers is not zero and header_include_names or input_headers are
+    // NULL.
+    if num_input_headers == 0 && (!header_include_names.is_null() || !input_headers.is_null())
+        || num_input_headers != 0 && (header_include_names.is_null() || input_headers.is_null())
+    {
+        Err(CL_INVALID_VALUE)?
+    }
+
+    let mut headers = Vec::new();
+    for h in 0..num_input_headers as usize {
+        unsafe {
+            headers.push(spirv::CLCHeader {
+                name: CStr::from_ptr(*header_include_names.add(h)).to_owned(),
+                source: &(*input_headers.add(h)).check()?.src,
+            });
+        }
+    }
+
+    // CL_COMPILE_PROGRAM_FAILURE if there is a failure to compile the program source. This error
+    // will be returned if clCompileProgram does not return until the compile has completed.
+    for dev in devs {
+        res &= p.compile(&dev, c_string_to_string(options), &headers);
+    }
+
+    call_cb(pfn_notify, program, user_data);
+
+    // CL_INVALID_OPERATION if program has no source or IL available, i.e. it has not been created with clCreateProgramWithSource or clCreateProgramWithIL.
+    // • CL_INVALID_COMPILER_OPTIONS if the compiler options specified by options are invalid.
+    // • CL_INVALID_OPERATION if the compilation or build of a program executable for any of the devices listed in device_list by a previous call to clCompileProgram or clBuildProgram for program has not completed.
+    // • CL_INVALID_OPERATION if there are kernel objects attached to program.
+
+    if res {
+        Ok(())
+    } else {
+        Err(CL_COMPILE_PROGRAM_FAILURE)
+    }
+}
+
+pub fn link_program(
+    context: cl_context,
+    num_devices: cl_uint,
+    device_list: *const cl_device_id,
+    _options: *const ::std::os::raw::c_char,
+    num_input_programs: cl_uint,
+    input_programs: *const cl_program,
+    pfn_notify: Option<ProgramCB>,
+    user_data: *mut ::std::os::raw::c_void,
+) -> Result<(cl_program, cl_int), cl_int> {
+    let c = context.check()?;
+    let devs = validate_devices(device_list, num_devices, &c.devs)?;
+    let progs = check_cl_objs_mut(input_programs, num_input_programs)?;
+
+    check_cb(&pfn_notify, user_data)?;
+
+    // CL_INVALID_VALUE if num_input_programs is zero and input_programs is NULL
+    if progs.is_empty() {
+        Err(CL_INVALID_VALUE)?;
+    }
+
+    // CL_INVALID_DEVICE if any device in device_list is not in the list of devices associated with
+    // context.
+    if !devs.iter().all(|d| c.devs.contains(d)) {
+        Err(CL_INVALID_DEVICE)?;
+    }
+
+    // CL_INVALID_OPERATION if the compilation or build of a program executable for any of the
+    // devices listed in device_list by a previous call to clCompileProgram or clBuildProgram for
+    // program has not completed.
+    for d in &devs {
+        if progs
+            .iter()
+            .map(|p| p.status(d))
+            .any(|s| s != CL_BUILD_SUCCESS as cl_build_status)
+        {
+            Err(CL_INVALID_OPERATION)?;
+        }
+    }
+
+    // CL_LINK_PROGRAM_FAILURE if there is a failure to link the compiled binaries and/or libraries.
+    let res = CLProgram::link(c, &devs, progs);
+    let code = if devs
+        .iter()
+        .map(|d| res.status(d))
+        .all(|s| s == CL_BUILD_SUCCESS as cl_build_status)
+    {
+        CL_SUCCESS as cl_int
+    } else {
+        CL_LINK_PROGRAM_FAILURE
+    };
+    Ok((res.cl, code))
+
+    //• CL_INVALID_LINKER_OPTIONS if the linker options specified by options are invalid.
+    //• CL_INVALID_OPERATION if the rules for devices containing compiled binaries or libraries as described in input_programs argument above are not followed.
+}
