Grima04/mesa-turnip-kgsl

commit 60c5729d1669f9d91cc77d71f8e1893f2c0d0c94

ci: Distribute ADMGPU driver to LAVA as a module
As it needs firmware to probe, and we cannot bundle it within the kernel
image because it is incompatible with the GPL.

Currently we rebind the driver after boot but that's slow and fragile,
as unloads of DRM drivers aren't generally tested.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7420>

.gitlab-ci.yml
@@ -243,7 +243,7 @@ x86_build-base:
     - .fdo.container-build@debian
     - .container
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_build-base "2020-10-06-clang10-2"
+    FDO_DISTRIBUTION_TAG: &x86_build-base "2020-11-05-kmod"
 
 .use-x86_build-base:
   extends:
@@ -261,7 +261,7 @@ x86_build:
   extends:
     - .use-x86_build-base
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_build "2020-10-09-spvtools-1"
+    FDO_DISTRIBUTION_TAG: &x86_build "2020-11-05-kmod"
 
 .use-x86_build:
   variables:
@@ -276,7 +276,7 @@ i386_build:
   extends:
     - .use-x86_build-base
   variables:
-    FDO_DISTRIBUTION_TAG: &i386_build "2020-10-06-clang10-2"
+    FDO_DISTRIBUTION_TAG: &i386_build "2020-11-05-kmod"
 
 .use-i386_build:
   variables:
@@ -291,7 +291,7 @@ ppc64el_build:
   extends:
     - .use-x86_build-base
   variables:
-    FDO_DISTRIBUTION_TAG: &ppc64el_build "2020-10-06-clang10-2"
+    FDO_DISTRIBUTION_TAG: &ppc64el_build "2020-11-05-kmod"
 
 .use-ppc64el_build:
   variables:
@@ -306,7 +306,7 @@ s390x_build:
   extends:
     - .use-x86_build-base
   variables:
-    FDO_DISTRIBUTION_TAG: &s390x_build "2020-10-06-clang10-2"
+    FDO_DISTRIBUTION_TAG: &s390x_build "2020-11-05-kmod"
 
 .use-s390x_build:
   variables:
@@ -321,7 +321,7 @@ android_build:
   extends:
     - .use-x86_build-base
   variables:
-    FDO_DISTRIBUTION_TAG: &android_build "2020-10-05-shrink"
+    FDO_DISTRIBUTION_TAG: &android_build "2020-11-05-kmod"
 
 .use-android_build:
   variables:
@@ -335,7 +335,7 @@ android_build:
 x86_test-base:
   extends: x86_build-base
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_test-base "2020-10-06-clang10-2"
+    FDO_DISTRIBUTION_TAG: &x86_test-base "2020-11-05-kmod"
 
 .use-x86_test-base:
   extends:
@@ -352,19 +352,19 @@ x86_test-base:
 x86_test-gl:
   extends: .use-x86_test-base
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_test-gl "2020-10-30-python3-modules-1"
+    FDO_DISTRIBUTION_TAG: &x86_test-gl "2020-11-05-kmod"
 
 # Debian 10 based x86 test image for VK
 x86_test-vk:
   extends: .use-x86_test-base
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_test-vk "2020-10-06-clang10-2"
+    FDO_DISTRIBUTION_TAG: &x86_test-vk "2020-11-05-kmod"
 
 # Debian 9 based x86 build image (old LLVM)
 x86_build_old:
   extends: x86_build-base
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_build_old "2020-08-13-gold"
+    FDO_DISTRIBUTION_TAG: &x86_build_old "2020-11-05-kmod"
     FDO_DISTRIBUTION_VERSION: stretch-slim
 
 .use-x86_build_old:
@@ -381,7 +381,7 @@ arm_build:
     - .fdo.container-build@debian@arm64v8
     - .container
   variables:
-    FDO_DISTRIBUTION_TAG: &arm_build "2020-09-10-libdrm"
+    FDO_DISTRIBUTION_TAG: &arm_build "2020-11-05-kmod"
 
 .use-arm_build:
   variables:

.gitlab-ci/container/lava_build.sh
@@ -141,15 +141,22 @@ for i in /usr/bin/*-ld /usr/bin/ld; do
 done
 export PATH=`pwd`/ld-links:$PATH
 
+# Disable all modules in defconfig, so we only build the ones we want
+sed -i 's/=m/=n/g' ${DEFCONFIG}
+
 ./scripts/kconfig/merge_config.sh ${DEFCONFIG} ../.gitlab-ci/${KERNEL_ARCH}.config
 make ${KERNEL_IMAGE_NAME}
 for image in ${KERNEL_IMAGE_NAME}; do
     cp arch/${KERNEL_ARCH}/boot/${image} /lava-files/.
 done
+
 if [[ -n ${DEVICE_TREES} ]]; then
     make dtbs
     cp ${DEVICE_TREES} /lava-files/.
 fi
+
+make modules
+INSTALL_MOD_PATH=/lava-files/rootfs-${DEBIAN_ARCH}/ make modules_install
 
 if [[ ${DEBIAN_ARCH} = "arm64" ]] && which mkimage > /dev/null; then
     make Image.lzma

.gitlab-ci/lava-gitlab-ci.yml
@@ -1,5 +1,5 @@
 variables:
-  DISTRIBUTION_TAG: "2020-11-05-new-kernel"
+  DISTRIBUTION_TAG: "2020-11-05-new-kernel-2"
 
 .kernel+rootfs:
   stage: container-2

.gitlab-ci/container/arm_build.sh
@@ -21,6 +21,7 @@ apt-get -y install \
 	flex \
 	g++ \
 	git \
+	kmod \
 	lavacli \
 	libdrm-dev \
 	libelf-dev \

.gitlab-ci/container/x86_build-base.sh
@@ -34,6 +34,7 @@ apt-get install -y --no-remove \
         g++-mingw-w64-x86-64 \
         gcc \
         git \
+        kmod \
         libclang-9-dev \
         libclang-10-dev \
         libclc-dev \

.gitlab-ci/lava-deqp.yml.jinja2
@@ -77,8 +77,7 @@ actions:
           - echo "nameserver 8.8.8.8" > /etc/resolv.conf
           - for i in 1 2 3; do sntp -sS pool.ntp.org && break || sleep 2; done
 
-          # amdgpu failed to find its firmware during boot, rebind
-          - echo 0000:00:01.0 > /sys/bus/pci/drivers/amdgpu/bind || true
+          - modprobe amdgpu || true
 
           - DEVFREQ_GOVERNOR=`find /sys/devices -name governor | grep gpu || true`
           - echo performance > $DEVFREQ_GOVERNOR || true

.gitlab-ci/lava-tracie.yml.jinja2
@@ -77,8 +77,7 @@ actions:
           - echo "nameserver 8.8.8.8" > /etc/resolv.conf
           - for i in 1 2 3; do sntp -sS pool.ntp.org && break || sleep 2; done
 
-          # amdgpu failed to find its firmware during boot, rebind
-          - echo 0000:00:01.0 > /sys/bus/pci/drivers/amdgpu/bind || true
+          - modprobe amdgpu || true
 
 {% if env_vars %}
           - export {{ env_vars }}

.gitlab-ci/x86_64.config
@@ -52,8 +52,8 @@ CONFIG_ACPI_VIDEO=y
 CONFIG_X86_AMD_FREQ_SENSITIVITY=y
 CONFIG_PINCTRL=y
 CONFIG_PINCTRL_AMD=y
-CONFIG_DRM_AMDGPU=y
-CONFIG_DRM_AMDGPU_SI=y
+CONFIG_DRM_AMDGPU=m
+CONFIG_DRM_AMDGPU_SI=m
 CONFIG_DRM_AMD_ACP=n
 CONFIG_ACPI_WMI=y
 CONFIG_MXM_WMI=y

upcliujie/mesa

commit 4651bc67c8009a798edbee8da8dc41fb43bf7710

ci: Only install kernel modules for LAVA devices.
The recent change to install kernel modules for AMD included a sed job to
disable kernel modules in the defconfig.  This somehow broke booting on
a307, except the commit failed to bump the arm64_test tag so it wasn't
noticed until the next uprev. (I didn't notice when landing the next
change to that container to add the deqp runner, because I didn't get a
git conflict on rebasing my tag bump so I didn't bump the tag again to
pull in the kernel changes and catch the fail).

I've spent a while trying to debug what's happened (including what
*should* be a replication of the kernel build on my local db410c) and come
up empty.  Just punt and disable the AMD kernel module changes on
baremetal to fix it.  Bump every container using lava_build.sh to make
sure we don't screw anything up with the script changes.

Fixes: 60c5729d1669 ("ci: Distribute ADMGPU driver to LAVA as a module")

.gitlab-ci.yml
@@ -414,7 +414,7 @@ arm64_test:
   extends:
     - .use-arm_test-base
   variables:
-    FDO_DISTRIBUTION_TAG: &arm64_test "2020-11-03-deqp-runner"
+    FDO_DISTRIBUTION_TAG: &arm64_test "2020-11-09-kmod"
 
 .use-arm64_test:
   variables:

.gitlab-ci/container/lava_build.sh
@@ -153,8 +153,10 @@ for i in /usr/bin/*-ld /usr/bin/ld; do
 done
 export PATH=`pwd`/ld-links:$PATH
 
-# Disable all modules in defconfig, so we only build the ones we want
-sed -i 's/=m/=n/g' ${DEFCONFIG}
+if [ -n "$INSTALL_KERNEL_MODULES" ]; then
+    # Disable all modules in defconfig, so we only build the ones we want
+    sed -i 's/=m/=n/g' ${DEFCONFIG}
+fi
 
 ./scripts/kconfig/merge_config.sh ${DEFCONFIG} ../.gitlab-ci/${KERNEL_ARCH}.config
 make ${KERNEL_IMAGE_NAME}
@@ -167,8 +169,10 @@ if [[ -n ${DEVICE_TREES} ]]; then
     cp ${DEVICE_TREES} /lava-files/.
 fi
 
-make modules
-INSTALL_MOD_PATH=/lava-files/rootfs-${DEBIAN_ARCH}/ make modules_install
+if [ -n "$INSTALL_KERNEL_MODULES" ]; then
+    make modules
+    INSTALL_MOD_PATH=/lava-files/rootfs-${DEBIAN_ARCH}/ make modules_install
+fi
 
 if [[ ${DEBIAN_ARCH} = "arm64" ]] && which mkimage > /dev/null; then
     make Image.lzma

.gitlab-ci/lava-gitlab-ci.yml
@@ -1,5 +1,5 @@
 variables:
-  DISTRIBUTION_TAG: "2020-11-05-deqp-runner"
+  DISTRIBUTION_TAG: "2020-11-09-kmod-fix"
 
 .kernel+rootfs:
   stage: container-2
@@ -9,6 +9,7 @@ variables:
     GIT_STRATEGY: fetch
     KERNEL_URL: "https://gitlab.freedesktop.org/tomeu/linux/-/archive/v5.10-rc2-for-mesa-ci/linux-v5.10-rc2-for-mesa-ci.tar.gz"
     UPLOAD_FOR_LAVA: 1
+    INSTALL_KERNEL_MODULES: 1
   script:
     - .gitlab-ci/container/lava_build.sh
 