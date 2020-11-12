f3d0cc1b037d6090f6c1b4a36e89af9a32f1aed6
Andres Gomez <agomez@igalia.com>
2020-11-12 17:23:49 +0200

    ci: build piglit's cmake-include-after-project branch
    Signed-off-by: Andres Gomez <agomez@igalia.com>

--- .gitlab-ci/build-piglit.sh
+++ .gitlab-ci/build-piglit.sh
@@ -6,9 +6,9 @@ if [ -n "$INCLUDE_OPENCL_TESTS" ]; then
     PIGLIT_OPTS="-DPIGLIT_BUILD_CL_TESTS=ON"
 fi
 
-git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
+git clone https://gitlab.freedesktop.org/tanty/piglit.git -b tanty/cmake-include-after-project --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout 5d3fbc00e32293d0b7029b986bfb99fa891c9bae
+git checkout ab8308747fecc12f9cceaf3a2031d1b6ca54df60
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
