b7cdc0fd330ce2b8eb080e7ab02fabac2043fa00
Dave Airlie <airlied@redhat.com>
2020-11-13 15:35:15 +1000

    lavapipe: add conformance test version

--- src/gallium/frontends/lavapipe/lvp_device.c
+++ src/gallium/frontends/lavapipe/lvp_device.c
@@ -837,9 +837,9 @@ VKAPI_ATTR void VKAPI_CALL lvp_GetPhysicalDeviceProperties2(
 #endif
                  );
          driver_props->conformanceVersion.major = 1;
-         driver_props->conformanceVersion.minor = 0;
-         driver_props->conformanceVersion.subminor = 0;
-         driver_props->conformanceVersion.patch = 0;;
+         driver_props->conformanceVersion.minor = 2;
+         driver_props->conformanceVersion.subminor = 4;
+         driver_props->conformanceVersion.patch = 0;
          break;
       }
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_POINT_CLIPPING_PROPERTIES: {
