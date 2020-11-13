b5bc1e7589c8140df20913515783a396f0539c8f
Hyunjun Ko <zzoon@igalia.com>
2020-11-13 12:01:41 +0000

    turnip: enable VK_KHR_performance_query with new debug flag
    Signed-off-by: Hyunjun Ko <zzoon@igalia.com>

--- src/freedreno/vulkan/tu_extensions.py
+++ src/freedreno/vulkan/tu_extensions.py
@@ -100,7 +100,7 @@ EXTENSIONS = [
     Extension('VK_KHR_push_descriptor',                   1, True),
     Extension('VK_KHR_incremental_present',               1, 'TU_HAS_SURFACE'),
     Extension('VK_KHR_image_format_list',                 1, True),
-    Extension('VK_KHR_performance_query',                 1, False),
+    Extension('VK_KHR_performance_query',                 1, 'device->instance->debug_flags & TU_DEBUG_PERFC'),
 ]
 
 MAX_API_VERSION = VkVersion(MAX_API_VERSION)
