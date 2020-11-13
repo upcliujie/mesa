Grima04/mesa-turnip-kgsl

commit dd2bd68fa69124c86cd008b256d06f44fab8e6cd

zink: use general-layout when blitting to/from same resource
This avoids a validator warning when for instance generating mipmaps.

Fixes: d2bb63c8d4c ("zink: Use optimal layout instead of general. Reduces valid layer warnings. Fixes RADV image noise.")
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5199>

src/gallium/drivers/zink/zink_blit.c
@@ -91,13 +91,35 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info)
    zink_batch_reference_resoure(batch, src);
    zink_batch_reference_resoure(batch, dst);
 
-   if (src->layout != VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL)
-      zink_resource_barrier(batch->cmdbuf, src, src->aspect,
-                            VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL);
+   if (src == dst) {
+      /* The Vulkan 1.1 specification says the following about valid usage
+       * of vkCmdBlitImage:
+       *
+       * "srcImageLayout must be VK_IMAGE_LAYOUT_SHARED_PRESENT_KHR,
+       *  VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL or VK_IMAGE_LAYOUT_GENERAL"
+       *
+       * and:
+       *
+       * "dstImageLayout must be VK_IMAGE_LAYOUT_SHARED_PRESENT_KHR,
+       *  VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL or VK_IMAGE_LAYOUT_GENERAL"
+       *
+       * Since we cant have the same image in two states at the same time,
+       * we're effectively left with VK_IMAGE_LAYOUT_SHARED_PRESENT_KHR or
+       * VK_IMAGE_LAYOUT_GENERAL. And since this isn't a present-related
+       * operation, VK_IMAGE_LAYOUT_GENERAL seems most appropriate.
+       */
+      if (src->layout != VK_IMAGE_LAYOUT_GENERAL)
+         zink_resource_barrier(batch->cmdbuf, src, src->aspect,
+                               VK_IMAGE_LAYOUT_GENERAL);
+   } else {
+      if (src->layout != VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL)
+         zink_resource_barrier(batch->cmdbuf, src, src->aspect,
+                               VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL);
 
-   if (dst->layout != VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
-      zink_resource_barrier(batch->cmdbuf, dst, dst->aspect,
-                            VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+      if (dst->layout != VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+         zink_resource_barrier(batch->cmdbuf, dst, dst->aspect,
+                               VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+   }
 
    VkImageBlit region = {};
    region.srcSubresource.aspectMask = src->aspect;

upcliujie/mesa

commit df0bad9a0351f39673a63d4dc377a55752e8406f

zink: do not use GENERAL for blits within same resource
It seems like there's a problem with blitting in the GENERAL state when
using RADV, causing corruption in mipmap levels.

This should really be fixed in RADV, but for now let's just side-step
the issue instead.

In the longer run, we should extend the tracking here to track states
per level in cases like this. But this is tricky to do without things
getting complicated, so for now let's just temporarily transition each
range, and restore them to the state they were in afterwards.

It's not at all a work of beauty, but it makes quake3 work on RADV for
me.

Fixes: dd2bd68fa69 ("zink: use general-layout when blitting to/from same resource")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3790

src/gallium/drivers/zink/zink_blit.c
@@ -105,6 +105,10 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info)
    }
 
    if (src == dst) {
+      assert(MAX2(src_base_layer, dst_base_layer) <
+             MIN2(src_base_layer + src_layer_count,
+                  dst_base_layer + dst_layer_count));
+
       /* The Vulkan 1.1 specification says the following about valid usage
        * of vkCmdBlitImage:
        *
@@ -116,14 +120,22 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info)
        * "dstImageLayout must be VK_IMAGE_LAYOUT_SHARED_PRESENT_KHR,
        *  VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL or VK_IMAGE_LAYOUT_GENERAL"
        *
-       * Since we cant have the same image in two states at the same time,
-       * we're effectively left with VK_IMAGE_LAYOUT_SHARED_PRESENT_KHR or
-       * VK_IMAGE_LAYOUT_GENERAL. And since this isn't a present-related
-       * operation, VK_IMAGE_LAYOUT_GENERAL seems most appropriate.
+       * Since we only track a single layout per resource, we need to
+       * temporarily whack these into the right layout, and back again.
        */
-      if (src->layout != VK_IMAGE_LAYOUT_GENERAL)
-         zink_resource_barrier(batch->cmdbuf, src, src->aspect,
-                               VK_IMAGE_LAYOUT_GENERAL);
+
+      if (src->layout != VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL)
+         zink_resource_barrier_range(batch->cmdbuf, src, src->aspect,
+                                     src->layout,
+                                     VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+                                     info->src.level, 1,
+                                     src_base_layer, src_layer_count);
+      if (dst->layout != VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+         zink_resource_barrier_range(batch->cmdbuf, dst, dst->aspect,
+                                     dst->layout,
+                                     VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                                     info->dst.level, 1,
+                                     dst_base_layer, dst_layer_count);
    } else
       zink_resource_setup_transfer_layouts(batch, src, dst);
 
@@ -155,19 +167,33 @@ blit_native(struct zink_context *ctx, const struct pipe_blit_info *info)
    if (dst->base.array_size > 1) {
       region.dstOffsets[0].z = 0;
       region.dstOffsets[1].z = 1;
-      region.dstSubresource.baseArrayLayer = info->dst.box.z;
-      region.dstSubresource.layerCount = info->dst.box.depth;
    } else {
       region.dstOffsets[0].z = info->dst.box.z;
       region.dstOffsets[1].z = info->dst.box.z + info->dst.box.depth;
-      region.dstSubresource.baseArrayLayer = 0;
-      region.dstSubresource.layerCount = 1;
    }
+   region.dstSubresource.baseArrayLayer = dst_base_layer;
+   region.dstSubresource.layerCount = dst_layer_count;
 
    vkCmdBlitImage(batch->cmdbuf, src->image, src->layout,
                   dst->image, dst->layout,
                   1, &region,
                   zink_filter(info->filter));
+
+   if (src == dst) {
+      /* restore the layouts back to their tracked state */
+      if (src->layout != VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL)
+         zink_resource_barrier_range(batch->cmdbuf, src, src->aspect,
+                                     VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+                                     src->layout,
+                                     info->src.level, 1,
+                                     src_base_layer, src_layer_count);
+      if (dst->layout != VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
+         zink_resource_barrier_range(batch->cmdbuf, dst, dst->aspect,
+                                     VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                                     dst->layout,
+                                     info->dst.level, 1,
+                                     dst_base_layer, dst_layer_count);
+   }
 
    return true;
 }