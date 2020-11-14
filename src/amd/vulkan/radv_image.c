Grima04/mesa-turnip-kgsl

commit 0833dd7d124bfd94161bff15e5b4293987920fce

amd/common: Add support for modifiers.
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6176>

src/amd/vulkan/radv_image.c
@@ -25,6 +25,8 @@
  * IN THE SOFTWARE.
  */
 
+#include <libdrm/drm_fourcc.h>
+
 #include "radv_debug.h"
 #include "radv_private.h"
 #include "vk_format.h"
@@ -451,7 +453,6 @@ radv_get_surface_flags(struct radv_device *device,
 
 	is_depth = vk_format_has_depth(desc);
 	is_stencil = vk_format_has_stencil(desc);
-
 
 	flags = RADEON_SURF_SET(array_mode, MODE);
 
@@ -1317,9 +1318,11 @@ radv_image_reset_layout(struct radv_image *image)
 		VkFormat format = vk_format_get_plane_format(image->vk_format, i);
 
 		uint32_t flags = image->planes[i].surface.flags;
+		uint64_t modifier = image->planes[i].surface.modifier;
 		memset(image->planes + i, 0, sizeof(image->planes[i]));
 
 		image->planes[i].surface.flags = flags;
+		image->planes[i].surface.modifier = modifier;
 		image->planes[i].surface.blk_w = vk_format_get_blockwidth(format);
 		image->planes[i].surface.blk_h = vk_format_get_blockheight(format);
 		image->planes[i].surface.bpe = vk_format_get_blocksize(vk_format_depth_only(format));
@@ -1474,6 +1477,7 @@ radv_image_create(VkDevice _device,
 	for (unsigned plane = 0; plane < image->plane_count; ++plane) {
 		image->planes[plane].surface.flags =
 			radv_get_surface_flags(device, image, plane, pCreateInfo, format);
+		image->planes[plane].surface.modifier = DRM_FORMAT_MOD_INVALID;
 	}
 
 	bool delay_layout = external_info &&

src/amd/common/ac_surface.h
@@ -27,6 +27,7 @@
 #define AC_SURFACE_H
 
 #include "amd_family.h"
+#include "util/format/u_format.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -207,6 +208,12 @@ struct radeon_surf {
    unsigned micro_tile_mode : 3;
    uint32_t flags;
 
+    /*
+     * DRM format modifier. Set to DRM_FORMAT_MOD_INVALID to have addrlib
+     * select tiling parameters instead.
+     */
+    uint64_t modifier;
+
    /* These are return values. Some of them can be set by the caller, but
     * they will be treated as hints (e.g. bankw, bankh) and might be
     * changed by the calculator.
@@ -312,6 +319,32 @@ void ac_surface_get_umd_metadata(const struct radeon_info *info, struct radeon_s
 
 void ac_surface_override_offset_stride(const struct radeon_info *info, struct radeon_surf *surf,
                                        unsigned num_mipmap_levels, uint64_t offset, unsigned pitch);
+
+
+struct ac_modifier_options {
+	bool dcc; /* Whether to allow DCC. */
+	bool dcc_retile; /* Whether to allow use of a DCC retile map. */
+};
+
+bool ac_is_modifier_supported(const struct radeon_info *info,
+                              const struct ac_modifier_options *options,
+                              enum pipe_format format,
+                              uint64_t modifier);
+bool ac_get_supported_modifiers(const struct radeon_info *info,
+                                const struct ac_modifier_options *options,
+                                enum pipe_format format,
+                                unsigned *mod_count,
+                                uint64_t *mods);
+bool ac_modifier_has_dcc(uint64_t modifier);
+bool ac_modifier_has_dcc_retile(uint64_t modifier);
+
+unsigned ac_surface_get_nplanes(const struct radeon_surf *surf);
+uint64_t ac_surface_get_plane_offset(enum chip_class chip_class,
+                                     const struct radeon_surf *surf,
+                                     unsigned plane, unsigned layer);
+uint64_t ac_surface_get_plane_stride(enum chip_class chip_class,
+                                     const struct radeon_surf *surf,
+                                     unsigned plane);
 
 #ifdef __cplusplus
 }

src/gallium/drivers/radeonsi/si_texture.c
@@ -316,6 +316,8 @@ static int si_init_surface(struct si_screen *sscreen, struct radeon_surf *surfac
          surface->u.gfx9.surf.swizzle_mode = ADDR_SW_64KB_R_X;
    }
 
+   surface->modifier = DRM_FORMAT_MOD_INVALID;
+
    r = sscreen->ws->surface_init(sscreen->ws, ptex, flags, bpe, array_mode, surface);
    if (r) {
       return r;

upcliujie/mesa

commit 06ce099417bf15dd8034ed960d987497f68d7002

radv: Use internal drm_fourcc.h
Fixes: 0833dd7d124 "amd/common: Add support for modifiers."
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3794

src/amd/vulkan/radv_image.c
@@ -25,8 +25,7 @@
  * IN THE SOFTWARE.
  */
 
-#include <libdrm/drm_fourcc.h>
-
+#include "drm-uapi/drm_fourcc.h"
 #include "radv_debug.h"
 #include "radv_private.h"
 #include "vk_format.h"