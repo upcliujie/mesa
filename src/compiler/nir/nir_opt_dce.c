ddd5f63b7f9ba6aaa8c363f1d5cdeacef059ba13
Rhys Perry <pendingchaos02@gmail.com>
2020-11-17 17:10:47 +0000

    nir/dce: inline nir_foreach_src
    Compile-time (nir_opt_dce):
    Difference at 95.0% confidence
    	-1160.75 +/- 10.4685
    	-11.047% +/- 0.0943856%
    	(Student's t, pooled s = 4.61862)
    
    Compile-time (entire run):
    Difference at 95.0% confidence
    	-0.36 +/- 0.066082
    	-0.598305% +/- 0.109207%
    	(Student's t, pooled s = 0.0291548)
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/compiler/nir/nir_opt_dce.c
+++ src/compiler/nir/nir_opt_dce.c
@@ -41,65 +41,17 @@ mark_src_live(const nir_src *src, BITSET_WORD *defs_live)
       BITSET_WORD new_ = BITSET_SET(defs_live, src->ssa->index);
       return new_ != old;
    } else {
+      if (src->reg.indirect)
+         return mark_src_live(src->reg.indirect, defs_live);
       return false;
    }
 }
 
-static bool
-mark_live_cb(nir_src *src, void *defs_live)
+static void
+mark_indirect_live(const nir_dest *dest, BITSET_WORD *defs_live)
 {
-   mark_src_live(src, defs_live);
-   return true;
-}
-
-static bool
-is_live(BITSET_WORD *defs_live, nir_instr *instr)
-{
-   switch (instr->type) {
-   case nir_instr_type_call:
-   case nir_instr_type_jump:
-      return true;
-   case nir_instr_type_alu: {
-      nir_alu_instr *alu = nir_instr_as_alu(instr);
-      return is_dest_live(&alu->dest.dest, defs_live);
-   }
-   case nir_instr_type_deref: {
-      nir_deref_instr *deref = nir_instr_as_deref(instr);
-      return is_dest_live(&deref->dest, defs_live);
-   }
-   case nir_instr_type_intrinsic: {
-      nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
-      const nir_intrinsic_info *info = &nir_intrinsic_infos[intrin->intrinsic];
-      return !(info->flags & NIR_INTRINSIC_CAN_ELIMINATE) ||
-             (info->has_dest && is_dest_live(&intrin->dest, defs_live));
-   }
-   case nir_instr_type_tex: {
-      nir_tex_instr *tex = nir_instr_as_tex(instr);
-      return is_dest_live(&tex->dest, defs_live);
-   }
-   case nir_instr_type_phi: {
-      nir_phi_instr *phi = nir_instr_as_phi(instr);
-      return is_dest_live(&phi->dest, defs_live);
-   }
-   case nir_instr_type_load_const: {
-      nir_load_const_instr *lc = nir_instr_as_load_const(instr);
-      return BITSET_TEST(defs_live, lc->def.index);
-   }
-   case nir_instr_type_ssa_undef: {
-      nir_ssa_undef_instr *undef = nir_instr_as_ssa_undef(instr);
-      return BITSET_TEST(defs_live, undef->def.index);
-   }
-   case nir_instr_type_parallel_copy: {
-      nir_parallel_copy_instr *pc = nir_instr_as_parallel_copy(instr);
-      nir_foreach_parallel_copy_entry(entry, pc) {
-         if (is_dest_live(&entry->dest, defs_live))
-            return true;
-      }
-      return false;
-   }
-   default:
-      unreachable("unexpected instr type");
-   }
+   if (!dest->is_ssa && dest->reg.indirect)
+      mark_src_live(dest->reg.indirect, defs_live);
 }
 
 struct loop_state {
@@ -112,19 +64,121 @@ mark_block(nir_block *block, BITSET_WORD *defs_live, struct loop_state *loop)
 {
    bool phis_changed = false;
    nir_foreach_instr_reverse(instr, block) {
-      if (is_live(defs_live, instr)) {
+      instr->pass_flags = 0;
+      switch (instr->type) {
+      case nir_instr_type_alu: {
+         nir_alu_instr *alu = nir_instr_as_alu(instr);
+         if (is_dest_live(&alu->dest.dest, defs_live)) {
+            instr->pass_flags = 1;
+
+            for (unsigned i = 0; i < nir_op_infos[alu->op].num_inputs; i++)
+               mark_src_live(&alu->src[i].src, defs_live);
+
+            mark_indirect_live(&alu->dest.dest, defs_live);
+         }
+         break;
+      }
+      case nir_instr_type_deref: {
+         nir_deref_instr *deref = nir_instr_as_deref(instr);
+         if (is_dest_live(&deref->dest, defs_live)) {
+            instr->pass_flags = 1;
+
+            if (deref->deref_type != nir_deref_type_var)
+               mark_src_live(&deref->parent, defs_live);
+
+            if (deref->deref_type == nir_deref_type_array ||
+                deref->deref_type == nir_deref_type_ptr_as_array)
+               mark_src_live(&deref->arr.index, defs_live);
+
+            mark_indirect_live(&deref->dest, defs_live);
+         }
+         break;
+      }
+      case nir_instr_type_call: {
+         nir_call_instr *call = nir_instr_as_call(instr);
          instr->pass_flags = 1;
 
-         if (instr->type == nir_instr_type_phi) {
-            nir_foreach_phi_src(src, nir_instr_as_phi(instr)) {
+         for (unsigned i = 0; i < call->num_params; i++)
+            mark_src_live(&call->params[i], defs_live);
+         break;
+      }
+      case nir_instr_type_tex: {
+         nir_tex_instr *tex = nir_instr_as_tex(instr);
+         if (is_dest_live(&tex->dest, defs_live)) {
+            instr->pass_flags = 1;
+
+            for (unsigned i = 0; i < tex->num_srcs; i++)
+               mark_src_live(&tex->src[i].src, defs_live);
+
+            mark_indirect_live(&tex->dest, defs_live);
+         }
+         break;
+      }
+      case nir_instr_type_intrinsic: {
+         nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+         const nir_intrinsic_info *info = &nir_intrinsic_infos[intrin->intrinsic];
+         if (!(info->flags & NIR_INTRINSIC_CAN_ELIMINATE) ||
+             (info->has_dest && is_dest_live(&intrin->dest, defs_live))) {
+            instr->pass_flags = 1;
+
+            for (unsigned i = 0; i < info->num_srcs; i++)
+               mark_src_live(&intrin->src[i], defs_live);
+
+            if (info->has_dest)
+               mark_indirect_live(&intrin->dest, defs_live);
+         }
+         break;
+      }
+      case nir_instr_type_load_const: {
+         nir_load_const_instr *lc = nir_instr_as_load_const(instr);
+         instr->pass_flags = BITSET_TEST(defs_live, lc->def.index);
+         break;
+      }
+      case nir_instr_type_jump: {
+         nir_jump_instr *jump = nir_instr_as_jump(instr);
+         instr->pass_flags = 1;
+
+         if (jump->type == nir_jump_goto_if)
+            mark_src_live(&jump->condition, defs_live);
+         break;
+      }
+      case nir_instr_type_ssa_undef: {
+         nir_ssa_undef_instr *undef = nir_instr_as_ssa_undef(instr);
+         instr->pass_flags = BITSET_TEST(defs_live, undef->def.index);
+         break;
+      }
+      case nir_instr_type_phi: {
+         nir_phi_instr *phi = nir_instr_as_phi(instr);
+         if (is_dest_live(&phi->dest, defs_live)) {
+            instr->pass_flags = 1;
+
+            nir_foreach_phi_src(src, phi) {
                phis_changed |= mark_src_live(&src->src, defs_live) &&
                                src->pred != loop->preheader;
             }
-         } else {
-            nir_foreach_src(instr, mark_live_cb, defs_live);
+
+            mark_indirect_live(&phi->dest, defs_live);
          }
-      } else {
-         instr->pass_flags = 0;
+         break;
+      }
+      case nir_instr_type_parallel_copy: {
+         nir_parallel_copy_instr *pc = nir_instr_as_parallel_copy(instr);
+         bool live = false;
+         nir_foreach_parallel_copy_entry(entry, pc) {
+            if (is_dest_live(&entry->dest, defs_live)) {
+               live = true;
+               break;
+            }
+         }
+         if (live) {
+            instr->pass_flags = 1;
+            nir_foreach_parallel_copy_entry(entry, pc) {
+               mark_src_live(&entry->src, defs_live);
+               mark_indirect_live(&entry->dest, defs_live);
+            }
+         }
+         break;
+      }
       }
    }
    loop->phis_changed = phis_changed;
