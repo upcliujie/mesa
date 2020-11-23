Grima04/mesa-turnip-kgsl

commit 5b77b14448c39d664460fd00d2a356d47858880f

nir: Use src_is_invocation_id in get_deref_info.
Signed-off-by: Timur Kristof <timur.kristof@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7622>

src/compiler/nir/nir_gather_info.c
@@ -53,11 +53,7 @@ get_deref_info(nir_shader *shader, nir_variable *var, nir_deref_instr *deref,
    /* Vertex index is the outermost array index. */
    if (per_vertex) {
       assert((*p)->deref_type == nir_deref_type_array);
-      nir_instr *vertex_index_instr = (*p)->arr.index.ssa->parent_instr;
-      *cross_invocation =
-         vertex_index_instr->type != nir_instr_type_intrinsic ||
-         nir_instr_as_intrinsic(vertex_index_instr)->intrinsic !=
-            nir_intrinsic_load_invocation_id;
+      *cross_invocation = src_is_invocation_id(&(*p)->arr.index);
       p++;
    }
 

upcliujie/mesa

commit 2960e1746f5202e96f7cd44a6bb511b57e465c3e

nir: fix gathering cross invocation info
Fixes: 5b77b14448c ("nir: Use src_is_invocation_id in get_deref_info.")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/compiler/nir/nir_gather_info.c
@@ -53,7 +53,7 @@ get_deref_info(nir_shader *shader, nir_variable *var, nir_deref_instr *deref,
    /* Vertex index is the outermost array index. */
    if (per_vertex) {
       assert((*p)->deref_type == nir_deref_type_array);
-      *cross_invocation = src_is_invocation_id(&(*p)->arr.index);
+      *cross_invocation = !src_is_invocation_id(&(*p)->arr.index);
       p++;
    }
 