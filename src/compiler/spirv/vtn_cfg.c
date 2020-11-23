Grima04/mesa-turnip-kgsl

commit 886d2d1a9abcb0572a957c24ae44de4d6c055bc0

spirv: Handle SpvOpTerminateInvocation
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7150>

src/compiler/spirv/vtn_cfg.c
@@ -255,6 +255,7 @@ vtn_cfg_handle_prepass_instruction(struct vtn_builder *b, SpvOp opcode,
    case SpvOpBranchConditional:
    case SpvOpSwitch:
    case SpvOpKill:
+   case SpvOpTerminateInvocation:
    case SpvOpReturn:
    case SpvOpReturnValue:
    case SpvOpUnreachable:
@@ -690,6 +691,10 @@ vtn_process_block(struct vtn_builder *b,
       block->branch_type = vtn_branch_type_discard;
       return NULL;
 
+   case SpvOpTerminateInvocation:
+      block->branch_type = vtn_branch_type_terminate;
+      return NULL;
+
    case SpvOpBranchConditional: {
       struct vtn_value *cond_val = vtn_untyped_value(b, block->branch[1]);
       vtn_fail_if(!cond_val->type ||
@@ -949,6 +954,12 @@ vtn_emit_branch(struct vtn_builder *b, enum vtn_branch_type branch_type,
       nir_intrinsic_instr *discard =
          nir_intrinsic_instr_create(b->nb.shader, op);
       nir_builder_instr_insert(&b->nb, &discard->instr);
+      break;
+   }
+   case vtn_branch_type_terminate: {
+      nir_intrinsic_instr *terminate =
+         nir_intrinsic_instr_create(b->nb.shader, nir_intrinsic_terminate);
+      nir_builder_instr_insert(&b->nb, &terminate->instr);
       break;
    }
    default:

src/compiler/spirv/vtn_private.h
@@ -130,6 +130,7 @@ enum vtn_branch_type {
    vtn_branch_type_loop_continue,
    vtn_branch_type_loop_back_edge,
    vtn_branch_type_discard,
+   vtn_branch_type_terminate,
    vtn_branch_type_return,
 };
 

upcliujie/mesa

commit 9c0683483943b669617cd90caa4bc4e5988ca09b

spirv: Call repair SSA for OpTerminateInvocation
Fixes: 886d2d1a9abcb "spirv: Handle SpvOpTerminateInvocation"

src/compiler/spirv/vtn_cfg.c
@@ -687,11 +687,12 @@ vtn_process_block(struct vtn_builder *b,
       return NULL;
 
    case SpvOpKill:
-      b->has_kill = true;
+      b->has_early_terminate = true;
       block->branch_type = vtn_branch_type_discard;
       return NULL;
 
    case SpvOpTerminateInvocation:
+      b->has_early_terminate = true;
       block->branch_type = vtn_branch_type_terminate;
       return NULL;
 
@@ -1377,7 +1378,8 @@ vtn_function_emit(struct vtn_builder *b, struct vtn_function *func,
     * but instructions in the continue may use SSA defs in the loop body.
     * Therefore, we need to repair SSA to insert the needed phi nodes.
     */
-   if (b->func->impl->structured && (b->has_loop_continue || b->has_kill))
+   if (b->func->impl->structured &&
+       (b->has_loop_continue || b->has_early_terminate))
       nir_repair_ssa_impl(func->impl);
 
    func->emitted = true;

src/compiler/spirv/vtn_private.h
@@ -702,7 +702,19 @@ struct vtn_builder {
    unsigned func_param_idx;
 
    bool has_loop_continue;
-   bool has_kill;
+
+   /** True if this shader has any early termination instructions like OpKill
+    *
+    * In the SPIR-V, the following instructions are block terminators:
+    *
+    *  - OpKill
+    *  - OpTerminateInvocation
+    *
+    * However, in NIR, they're represented by regular intrinsics with no
+    * control-flow semantics.  This means that the SSA form from the SPIR-V
+    * may not 100% match NIR and we have to fix it up at the end.
+    */
+   bool has_early_terminate;
 
    /* false by default, set to true by the ContractionOff execution mode */
    bool exact;