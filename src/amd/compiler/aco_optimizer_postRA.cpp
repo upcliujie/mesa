a526477ff496241a11555260527f57cd9699c006
Timur Kristóf <timur.kristof@gmail.com>
2020-11-24 11:39:28 +0100

    aco: Introducing a new, post-RA optimizer.
    This commit adds the skeleton of a new ACO post-RA optimizer,
    which is intended to be a simple pass called after RA,
    and works on the same basic idea as the pre-RA optimizer, but
    is meant to do trivial code changes which can only be done
    after RA.
    
    It is currently empty, the actual optimizations will be added
    in their own commits. It only has a DCE pass.
    
    Fossil DB changes on Navi 10:
    Totals from 270 (0.19% of 139391) affected shaders:
    CodeSize: 5343088 -> 5257112 (-1.61%)
    Instrs: 993451 -> 981955 (-1.16%)
    Cycles: 115362032 -> 114373460 (-0.86%)
    VMEM: 40958 -> 40887 (-0.17%)
    SMEM: 17679 -> 17669 (-0.06%)
    SClause: 23971 -> 23967 (-0.02%)
    Copies: 143867 -> 132470 (-7.92%)
    Branches: 35966 -> 35869 (-0.27%)
    
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

---
+++ src/amd/compiler/aco_optimizer_postRA.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright © 2020 Valve Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Timur Kristóf <timur.kristof@gmail.com
+ *
+ */
+
+#include "aco_ir.h"
+
+#include <vector>
+#include <bitset>
+#include <algorithm>
+
+namespace aco {
+
+namespace {
+
+enum pr_opt_label
+{
+   num_labels,
+};
+
+struct pr_opt_ssa_info
+{
+   std::bitset<num_labels> labels;
+   Block *block = nullptr;
+   int instr_idx = -1;
+
+   constexpr bool has_instr() const
+   {
+      return instr_idx != -1;
+   }
+
+   aco_ptr<Instruction> &instr()
+   {
+      assert(has_instr());
+      return block->instructions[instr_idx];
+   }
+};
+
+struct pr_opt_ctx
+{
+   Program *program;
+   Block *current_block;
+   int current_instr_idx;
+   std::vector<uint16_t> uses;
+   std::vector<pr_opt_ssa_info> info;
+};
+
+void set_label(pr_opt_ctx &ctx, uint32_t tempId, pr_opt_label label)
+{
+   auto &info = ctx.info[tempId];
+   const Instruction *instr = ctx.current_block->instructions[ctx.current_instr_idx].get();
+
+   /* When the labelled instruction doesn't match, clear the pre-existing labels */
+   if (info.has_instr() && info.instr().get() != instr)
+      info.labels.reset();
+
+   info.labels.set(label);
+   info.block = ctx.current_block;
+   info.instr_idx = ctx.current_instr_idx;
+}
+
+void process_instruction(pr_opt_ctx &ctx, aco_ptr<Instruction> &instr)
+{
+   ctx.current_instr_idx++;
+}
+
+} /* End of empty namespace */
+
+void optimize_postRA(Program* program)
+{
+   pr_opt_ctx ctx;
+   ctx.program = program;
+   ctx.uses = dead_code_analysis(program);
+   ctx.info.resize(program->peekAllocationId());
+
+   /* Forward pass
+    * Goes through each instruction exactly once, and can transform
+    * instructions or adjust the use counts of temps.
+    */
+   for (auto &block : program->blocks) {
+      ctx.current_block = &block;
+      ctx.current_instr_idx = -1;
+      for (aco_ptr<Instruction> &instr : block.instructions)
+         process_instruction(ctx, instr);
+   }
+
+   /* Cleanup pass
+    * Gets rid of instructions which are deleted or no longer have
+    * any uses.
+    */
+   for (auto &block : program->blocks) {
+      ctx.current_block = &block;
+      std::vector<aco_ptr<Instruction>> sel_instr;
+      sel_instr.reserve(block.instructions.size());
+
+      for (aco_ptr<Instruction> &instr : block.instructions) {
+         if (instr && !is_dead(ctx.uses, instr.get()))
+            sel_instr.emplace_back(std::move(instr));
+      }
+
+      block.instructions.swap(sel_instr);
+   }
+}
+
+} /* End of aco namespace */
