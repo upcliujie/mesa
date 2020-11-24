e62f669fe6ad0644cbd6375557a480596a014f7c
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2020-11-24 23:02:54 +0100

    radv: Enable sparse buffer and image support.

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -1001,8 +1001,12 @@ void radv_GetPhysicalDeviceFeatures(
 		.shaderInt64                              = true,
 		.shaderInt16                              = true,
 		.sparseBinding                            = true,
+		.sparseResidencyBuffer                    = pdevice->rad_info.chip_class >= GFX8,
+		.sparseResidencyImage2D                   = pdevice->rad_info.chip_class >= GFX8,
+		.sparseResidencyAliased                   = pdevice->rad_info.chip_class >= GFX8,
 		.variableMultisampleRate                  = true,
 		.shaderResourceMinLod                     = true,
+		.shaderResourceResidency                  = true,
 		.inheritedQueries                         = true,
 	};
 }
@@ -1628,7 +1632,10 @@ void radv_GetPhysicalDeviceProperties(
 		.deviceID = pdevice->rad_info.pci_id,
 		.deviceType = pdevice->rad_info.has_dedicated_vram ? VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU : VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU,
 		.limits = limits,
-		.sparseProperties = {0},
+		.sparseProperties = {
+			.residencyNonResidentStrict = pdevice->rad_info.chip_class >= GFX8,
+			.residencyStandard2DBlockShape = pdevice->rad_info.chip_class >= GFX8,
+		},
 	};
 
 	strcpy(pProperties->deviceName, pdevice->name);
