Grima04/mesa-turnip-kgsl

commit e49748521ec9182e8d2eec823182cc463709123f

freedreno: reduce extra height alignment in a6xx layout
Signed-off-by: Jonathan Marek <jonathan@marek.ca>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4611>

src/freedreno/fdl/fd6_layout.c
@@ -169,14 +169,14 @@ fdl6_layout(struct fdl_layout *layout,
 			nblocksy = align(nblocksy, ta->heightalign);
 
 		/* The blits used for mem<->gmem work at a granularity of
-		 * 32x32, which can cause faults due to over-fetch on the
+		 * 16x4, which can cause faults due to over-fetch on the
 		 * last level.  The simple solution is to over-allocate a
 		 * bit the last level to ensure any over-fetch is harmless.
 		 * The pitch is already sufficiently aligned, but height
-		 * may not be:
+		 * may not be. note this only matters if last level is linear
 		 */
 		if (level == mip_levels - 1)
-			nblocksy = align(nblocksy, 32);
+			height = align(nblocksy, 4);
 
 		uint32_t nblocksx =
 			util_align_npot(util_format_get_nblocksx(format, u_minify(pitch0, level)),

upcliujie/mesa

commit a85b12ae574a9dd71628a24c2d0b9da604ae0aaf

freedreno/a6xx: Fix typo in height alignment calculation in a6xx layout
Fixes KHR-GL31.texture_size_promotion.functional

Fixes: e49748521ec9182e8d2eec823182cc463709123f
Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

src/freedreno/fdl/fd6_layout.c
@@ -211,7 +211,7 @@ fdl6_layout(struct fdl_layout *layout,
 		 * may not be. note this only matters if last level is linear
 		 */
 		if (level == mip_levels - 1)
-			height = align(nblocksy, 4);
+			nblocksy = align(nblocksy, 4);
 
 		slice->offset = offset + layout->size;
 