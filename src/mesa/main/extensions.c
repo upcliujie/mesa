dfa00fd71818c808ec6c444dcb561c177c5fdd81
Adam Jackson <ajax@redhat.com>
2020-12-01 16:37:33 -0500

    mesa: ATI_texture_env_combine3 is always supported

--- src/mesa/main/extensions.c
+++ src/mesa/main/extensions.c
@@ -133,7 +133,6 @@ _mesa_enable_sw_extensions(struct gl_context *ctx)
    ctx->Extensions.ARB_texture_compression_rgtc = GL_TRUE;
    ctx->Extensions.APPLE_object_purgeable = GL_TRUE;
    ctx->Extensions.ATI_texture_compression_3dc = GL_TRUE;
-   ctx->Extensions.ATI_texture_env_combine3 = GL_TRUE;
    ctx->Extensions.ATI_texture_mirror_once = GL_TRUE;
    ctx->Extensions.EXT_depth_bounds_test = GL_TRUE;
    ctx->Extensions.EXT_draw_buffers2 = GL_TRUE;
--- src/mesa/main/extensions_table.h
+++ src/mesa/main/extensions_table.h
@@ -202,7 +202,7 @@ EXT(ATI_fragment_shader                     , dummy_true                        
 EXT(ATI_meminfo                             , ATI_meminfo                            , GLL, GLC,  x ,  x , 2009)
 EXT(ATI_separate_stencil                    , dummy_true                             , GLL,  x ,  x ,  x , 2006)
 EXT(ATI_texture_compression_3dc             , ATI_texture_compression_3dc            , GLL,  x ,  x ,  x , 2004)
-EXT(ATI_texture_env_combine3                , ATI_texture_env_combine3               , GLL,  x ,  x ,  x , 2002)
+EXT(ATI_texture_env_combine3                , dummy_true                             , GLL,  x ,  x ,  x , 2002)
 EXT(ATI_texture_float                       , ARB_texture_float                      , GLL, GLC,  x ,  x , 2002)
 EXT(ATI_texture_mirror_once                 , ATI_texture_mirror_once                , GLL, GLC,  x ,  x , 2006)
 
