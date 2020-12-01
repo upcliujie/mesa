44c4165fb020220f756fb9381ee782906ca77060
Adam Jackson <ajax@redhat.com>
2020-12-01 15:47:52 -0500

    mesa: ARB_fragment_program_shadow is always supported

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -1035,7 +1035,6 @@ void st_init_extensions(struct pipe_screen *screen,
    /*
     * Extensions that are supported by all Gallium drivers:
     */
-   extensions->ARB_fragment_program_shadow = GL_TRUE;
    extensions->ARB_half_float_vertex = GL_TRUE;
    extensions->ARB_internalformat_query = GL_TRUE;
    extensions->ARB_internalformat_query2 = GL_TRUE;
