f407d689fdeec372bbe7060f841e0a241bc9f198
Rhys Perry <pendingchaos02@gmail.com>
2020-12-01 17:41:16 +0000

    radv,aco: use per-attribute vertex descriptors for robustness
    We have to use a different num_records for each attribute to correctly
    implement robust buffer access.
    
    fossil-db (Navi, always enabled):
    Totals from 52541 (37.86% of 138791) affected shaders:
    SGPRs: 2415477 -> 2608564 (+7.99%); split: -0.57%, +8.57%
    VGPRs: 1840936 -> 1841100 (+0.01%); split: -0.00%, +0.01%
    SpillSGPRs: 1497 -> 1559 (+4.14%)
    CodeSize: 62943788 -> 63342544 (+0.63%); split: -0.02%, +0.65%
    MaxWaves: 778681 -> 778612 (-0.01%); split: +0.00%, -0.01%
    Instrs: 12466075 -> 12534869 (+0.55%); split: -0.02%, +0.58%
    Cycles: 58491504 -> 58766632 (+0.47%); split: -0.02%, +0.49%
    VMEM: 18136823 -> 18222606 (+0.47%); split: +0.94%, -0.47%
    SMEM: 4209626 -> 3856401 (-8.39%); split: +0.71%, -9.10%
    VClause: 375796 -> 370047 (-1.53%); split: -1.96%, +0.43%
    SClause: 443800 -> 452903 (+2.05%); split: -0.57%, +2.62%
    Copies: 898820 -> 900646 (+0.20%); split: -0.12%, +0.32%
    PreSGPRs: 1250247 -> 1606017 (+28.46%); split: -0.20%, +28.66%
    PreVGPRs: 1377048 -> 1377058 (+0.00%)
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -2768,7 +2768,8 @@ radv_fill_shader_info(struct radv_pipeline *pipeline,
 
 	if (nir[MESA_SHADER_FRAGMENT]) {
 		radv_nir_shader_info_init(&infos[MESA_SHADER_FRAGMENT]);
-		radv_nir_shader_info_pass(nir[MESA_SHADER_FRAGMENT],
+		radv_nir_shader_info_pass(pipeline->device,
+					  nir[MESA_SHADER_FRAGMENT],
 					  pipeline->layout,
 					  &keys[MESA_SHADER_FRAGMENT],
 					  &infos[MESA_SHADER_FRAGMENT]);
@@ -2820,7 +2821,8 @@ radv_fill_shader_info(struct radv_pipeline *pipeline,
 		radv_nir_shader_info_init(&infos[MESA_SHADER_TESS_CTRL]);
 
 		for (int i = 0; i < 2; i++) {
-			radv_nir_shader_info_pass(combined_nir[i],
+			radv_nir_shader_info_pass(pipeline->device,
+						  combined_nir[i],
 						  pipeline->layout, &key,
 						  &infos[MESA_SHADER_TESS_CTRL]);
 		}
@@ -2840,7 +2842,8 @@ radv_fill_shader_info(struct radv_pipeline *pipeline,
 		radv_nir_shader_info_init(&infos[MESA_SHADER_GEOMETRY]);
 
 		for (int i = 0; i < 2; i++) {
-			radv_nir_shader_info_pass(combined_nir[i],
+			radv_nir_shader_info_pass(pipeline->device,
+						  combined_nir[i],
 						  pipeline->layout,
 						  &keys[pre_stage],
 						  &infos[MESA_SHADER_GEOMETRY]);
@@ -2860,8 +2863,8 @@ radv_fill_shader_info(struct radv_pipeline *pipeline,
 		}
 
 		radv_nir_shader_info_init(&infos[i]);
-		radv_nir_shader_info_pass(nir[i], pipeline->layout,
-					  &keys[i], &infos[i]);
+		radv_nir_shader_info_pass(pipeline->device, nir[i],
+					  pipeline->layout, &keys[i], &infos[i]);
 	}
 
 	for (int i = 0; i < MESA_SHADER_STAGES; i++) {
@@ -3331,7 +3334,7 @@ VkResult radv_create_shaders(struct radv_pipeline *pipeline,
 			key.has_multiview_view_index =
 				keys[MESA_SHADER_GEOMETRY].has_multiview_view_index;
 
-			radv_nir_shader_info_pass(nir[MESA_SHADER_GEOMETRY],
+			radv_nir_shader_info_pass(device, nir[MESA_SHADER_GEOMETRY],
 						  pipeline->layout, &key,
 						  &info);
 			info.wave_size = 64; /* Wave32 not supported. */
@@ -5143,6 +5146,14 @@ radv_pipeline_init_vertex_input_state(struct radv_pipeline *pipeline,
 		pipeline->binding_stride[desc->binding] = desc->stride;
 		pipeline->num_vertex_bindings =
 			MAX2(pipeline->num_vertex_bindings, desc->binding + 1);
+	}
+
+	for (uint32_t i = 0; i < vi_info->vertexAttributeDescriptionCount; i++) {
+		const VkVertexInputAttributeDescription *desc =
+			&vi_info->pVertexAttributeDescriptions[i];
+
+		pipeline->attrib_ends[i] = desc->offset + vk_format_get_blocksize(desc->format);
+		pipeline->attrib_bindings[i] = desc->binding;
 	}
 }
 
