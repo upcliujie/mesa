a58304f538bde516248f7fb4dcc5b49ceff06abe
Adam Jackson <ajax@redhat.com>
2020-12-01 15:34:01 -0500

    mesa: ARB_explicit_attrib_location is always supported

--- src/mesa/main/extensions_table.h
+++ src/mesa/main/extensions_table.h
@@ -69,7 +69,7 @@ EXT(ARB_draw_elements_base_vertex           , dummy_true                        
 EXT(ARB_draw_indirect                       , ARB_draw_indirect                      , GLL, GLC,  x ,  x , 2010)
 EXT(ARB_draw_instanced                      , ARB_draw_instanced                     , GLL, GLC,  x ,  x , 2008)
 EXT(ARB_enhanced_layouts                    , ARB_enhanced_layouts                   , GLL, GLC,  x ,  x , 2013)
-EXT(ARB_explicit_attrib_location            , ARB_explicit_attrib_location           , GLL, GLC,  x ,  x , 2009)
+EXT(ARB_explicit_attrib_location            , dummy_true                             , GLL, GLC,  x ,  x , 2009)
 EXT(ARB_explicit_uniform_location           , ARB_explicit_uniform_location          , GLL, GLC,  x ,  x , 2012)
 EXT(ARB_fragment_coord_conventions          , ARB_fragment_coord_conventions         , GLL, GLC,  x ,  x , 2009)
 EXT(ARB_fragment_layer_viewport             , ARB_fragment_layer_viewport            , GLL, GLC,  x ,  x , 2012)
