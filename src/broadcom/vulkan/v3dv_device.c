Grima04/mesa-turnip-kgsl

commit b14679ab226bd3e263a778ea8e39828bde38b403

v3dv: check return value of drmGetMagic
Fixes coverty issue:

** CID 1470345: Error handling issues (CHECKED_RETURN)
/src/broadcom/vulkan/v3dv_device.c: 407 in v3dv_drm_handle_device()
Calling "drmGetMagic" without checking return value (as is done
elsewhere 5 out of 6 times).

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7828>

src/broadcom/vulkan/v3dv_device.c
@@ -404,7 +404,10 @@ v3dv_drm_handle_device(void *data, struct wl_drm *drm, const char *device)
    info->is_set = info->fd != -1;
 
    drm_magic_t magic;
-   drmGetMagic(info->fd, &magic);
+   if (drmGetMagic(info->fd, &magic)) {
+      fprintf(stderr, "drmGetMagic failed.");
+      return;
+   }
    wl_drm_authenticate(info->wl_drm, magic);
 }
 

upcliujie/mesa

commit 4b8c65e5160c9faed8b6bc7903fff56b4272cf68

v3dv: fix early return from failed drmGetMagic
Fixes: b14679ab226 ('v3dv: check return value of drmGetMagic')

src/broadcom/vulkan/v3dv_device.c
@@ -402,10 +402,15 @@ v3dv_drm_handle_device(void *data, struct wl_drm *drm, const char *device)
    struct v3dv_wayland_info *info = data;
    info->fd = open(device, O_RDWR | O_CLOEXEC);
    info->is_set = info->fd != -1;
+   if (!info->is_set)
+      return;
 
    drm_magic_t magic;
    if (drmGetMagic(info->fd, &magic)) {
       fprintf(stderr, "drmGetMagic failed.");
+      close(info->fd);
+      info->fd = -1;
+      info->is_set = false;
       return;
    }
    wl_drm_authenticate(info->wl_drm, magic);