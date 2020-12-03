c4509004c420339266b99e8bff17d3719b4ce1f5
Rhys Perry <pendingchaos02@gmail.com>
2020-12-03 19:55:10 +0000

    aco: don't assume src=lower when splitting self-intersecting copies
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Fixes: 09c584caeb2 ("aco: split self-intersecting copies instead of swapping")

--- src/amd/compiler/aco_lower_to_hw_instr.cpp
+++ src/amd/compiler/aco_lower_to_hw_instr.cpp
@@ -1621,23 +1621,25 @@ void handle_operands(std::map<PhysReg, copy_operation>& copy_map, lower_context*
          unsigned offset = higher.reg_b - lower.reg_b;
          RegType type = swap.def.regClass().type();
 
+         PhysReg src = swap.op.physReg(), dst = swap.def.physReg();
+
          copy_operation middle;
-         lower.reg_b += offset;
-         higher.reg_b += offset;
+         src.reg_b += offset;
+         dst.reg_b += offset;
          middle.bytes = swap.bytes - offset * 2;
          memcpy(middle.uses, swap.uses + offset, middle.bytes);
-         middle.op = Operand(lower, RegClass::get(type, middle.bytes));
-         middle.def = Definition(higher, RegClass::get(type, middle.bytes));
-         copy_map[higher] = middle;
+         middle.op = Operand(src, RegClass::get(type, middle.bytes));
+         middle.def = Definition(dst, RegClass::get(type, middle.bytes));
+         copy_map[dst] = middle;
 
          copy_operation end;
-         lower.reg_b += middle.bytes;
-         higher.reg_b += middle.bytes;
+         src.reg_b += middle.bytes;
+         dst.reg_b += middle.bytes;
          end.bytes = swap.bytes - (offset + middle.bytes);
          memcpy(end.uses, swap.uses + offset + middle.bytes, end.bytes);
-         end.op = Operand(lower, RegClass::get(type, end.bytes));
-         end.def = Definition(higher, RegClass::get(type, end.bytes));
-         copy_map[higher] = end;
+         end.op = Operand(src, RegClass::get(type, end.bytes));
+         end.def = Definition(dst, RegClass::get(type, end.bytes));
+         copy_map[dst] = end;
 
          memset(swap.uses + offset, 0, swap.bytes - offset);
          swap.bytes = offset;
