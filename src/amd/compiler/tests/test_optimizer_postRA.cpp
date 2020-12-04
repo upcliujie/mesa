d049532453b27e44537193abdb1d499623a19589
Timur Kristóf <timur.kristof@gmail.com>
2020-12-04 12:38:34 +0100

    aco: Test the s_cbranch_vccz/nz optimization.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/amd/compiler/tests/test_optimizer_postRA.cpp
+++ src/amd/compiler/tests/test_optimizer_postRA.cpp
@@ -25,3 +25,40 @@
 #include "helpers.h"
 
 using namespace aco;
+
+BEGIN_TEST(optimizer_postRA.vcmp)
+    //>> v1: %a, s2: %x:exec = p_startpgm
+    ASSERTED bool setup_ok = setup_cs("v1", GFX6);
+    assert(setup_ok);
+
+    Temp v_in = inputs[0];
+
+    {
+        /* Recognize when the result of VOPC goes to VCC, and use that for the branching then. */
+
+        //! s2: %b:vcc = v_cmp_eq_u32 0, %a
+        //! s2: %e = p_cbranch_z %b:vcc
+        //! p_unit_test 0, %e
+        auto vcmp = bld.vopc(aco_opcode::v_cmp_eq_u32, bld.vcc(bld.def(bld.lm)), Operand(0u), v_in);
+        auto sand = bld.sop2(Builder::s_and, bld.def(bld.lm), bld.def(s1, scc), bld.vcc(vcmp), bld.exec(exec_input));
+        auto br = bld.branch(aco_opcode::p_cbranch_z, bld.hint_vcc(bld.def(s2)), bld.scc(sand.def(1).getTemp()));
+        writeout(0, br);
+    }
+
+    //; del b, e
+
+    {
+        /* When the result of VOPC goes to an SGPR pair other than VCC, don't optimize */
+
+        //! s2: %b = v_cmp_eq_u32 0, %a
+        //! s2: %c, s1: %d:scc = s_and_b64 %b, %x:exec
+        //! s2: %e = p_cbranch_z %d:scc
+        //! p_unit_test 1, %e
+        auto vcmp = bld.vopc(aco_opcode::v_cmp_eq_u32, bld.def(bld.lm), Operand(0u), v_in);
+        auto sand = bld.sop2(Builder::s_and, bld.def(bld.lm), bld.def(s1, scc), vcmp, bld.exec(exec_input));
+        auto br = bld.branch(aco_opcode::p_cbranch_z, bld.hint_vcc(bld.def(s2)), bld.scc(sand.def(1).getTemp()));
+        writeout(1, br);
+    }
+
+    finish_optimizer_postRA_test();
+END_TEST
