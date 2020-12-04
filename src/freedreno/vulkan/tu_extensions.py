aa5f68d042269729c7c3aca7f29c69bad3dcb10e
Connor Abbott <cwabbott0@gmail.com>
2020-12-04 17:26:31 +0100

    tu: Expose VK_KHR_relaxed_block_layout
    This was absorbed into Vulkan 1.1, but we forgot to expose it
    separately. It's a subset of what's allowed by
    VK_EXT_scalar_block_layout.

--- src/freedreno/vulkan/tu_extensions.py
+++ src/freedreno/vulkan/tu_extensions.py
@@ -104,6 +104,7 @@ EXTENSIONS = [
     Extension('VK_KHR_performance_query',                 1, 'device->instance->debug_flags & TU_DEBUG_PERFC'),
     Extension('VK_EXT_scalar_block_layout',               1, True),
     Extension('VK_KHR_spirv_1_4',                         1, True),
+    Extension('VK_KHR_relaxed_block_layout',              1, True),
 ]
 
 MAX_API_VERSION = VkVersion(MAX_API_VERSION)
