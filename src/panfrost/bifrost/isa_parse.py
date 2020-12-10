97dac3ee2fa95baeb569d2a198e57b1c7ff2020b
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-12-10 16:47:44 -0500

    pan/bi: Add staging register counts to ISA.xml
    Message-passing instructions that read/write staging registers access
    either:
    
    * a fixed number of registers
    * vecsize registers (/2 for LD/ST_CVT if register_format is 16-bit)
    * a computed number for TEXC
    
    This adds the fixed counts into the XML for the first type and space to
    specify the latter types.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

--- src/panfrost/bifrost/isa_parse.py
+++ src/panfrost/bifrost/isa_parse.py
@@ -89,17 +89,25 @@ def parse_copy(enc, existing):
             if ex[0][0] == name:
                 ex[0][1] = node.get('start')
 
-def parse_instruction(ins):
+def parse_instruction(ins, include_pseudo):
     common = {
             'srcs': [],
             'modifiers': [],
             'immediates': [],
             'swaps': [],
             'derived': [],
-            'staging': ins.attrib.get('staging', ''),
+            'staging': ins.attrib.get('staging', '').split('=')[0],
+            'staging_count': None,
             'unused': ins.attrib.get('unused', False),
             'pseudo': ins.attrib.get('pseudo', False),
     }
+
+    if 'staging' in ins.attrib:
+        parts = ins.attrib['staging'].split('=')
+
+        if len(parts) > 1:
+            assert(len(parts) == 2)
+            common['staging_count'] = int(parts[1])
 
     if 'exact' in ins.attrib:
         common['exact'] = parse_exact(ins)
@@ -109,7 +117,11 @@ def parse_instruction(ins):
         common['srcs'].append([int(src.attrib['start'], 0), mask])
 
     for imm in ins.findall('immediate'):
-        common['immediates'].append([imm.attrib['name'], int(imm.attrib['start']), int(imm.attrib['size'])])
+        if imm.attrib.get('pseudo', False) and not include_pseudo:
+            continue
+
+        start = int(imm.attrib['start']) if 'start' in imm.attrib else None
+        common['immediates'].append([imm.attrib['name'], start, int(imm.attrib['size'])])
 
     common['derived'] = parse_derived(ins)
     common['modifiers'] = parse_modifiers(ins)
@@ -153,7 +165,7 @@ def parse_instructions(xml, include_unused = False, include_pseudo = False):
     instructions = ET.parse(xml).getroot().findall('ins')
 
     for ins in instructions:
-        parsed = parse_instruction(ins)
+        parsed = parse_instruction(ins, include_pseudo)
 
         # Some instructions are for useful disassembly only and can be stripped
         # out of the compiler, particularly useful for release builds
