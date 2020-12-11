e054c67165833ba1433149c9aa73920e0c8e92fe
Adam Jackson <ajax@redhat.com>
2020-12-11 12:07:12 -0500

    mesa: Wire up GL_EXT_texture_sRGB_RG8 for softpipe and llvmpipe
    Also fix the extension date for sRGB_R8, initial revision of both was
    2015. sRGB_RG8 is not registered for big-GL yet, see this Khronos issue
    for updates on that:
    
    https://github.com/KhronosGroup/OpenGL-Registry/issues/450

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -983,6 +983,10 @@ void st_init_extensions(struct pipe_screen *screen,
         { PIPE_FORMAT_R8_SRGB },
         GL_TRUE },
 
+      { { o(EXT_texture_sRGB_RG8) },
+        { PIPE_FORMAT_R8G8_SRGB },
+        GL_TRUE },
+
       { { o(EXT_texture_type_2_10_10_10_REV) },
         { PIPE_FORMAT_R10G10B10A2_UNORM,
           PIPE_FORMAT_B10G10R10A2_UNORM },
