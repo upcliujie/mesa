638506b29a567b981fa74817a98ca7e31da41a1e
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-12-11 18:56:46 -0500

    zink: add spirv caps/extension for shader LAYER variable
    this is required if gl_Layer is used outside of GEOMETRY stage

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -3847,6 +3847,16 @@ nir_to_spirv(struct nir_shader *s, const struct zink_so_info *so_info,
       unreachable("invalid stage");
    }
 
+   if (s->info.stage != MESA_SHADER_GEOMETRY) {
+      /* TODO: check vk features for this */
+      if (s->info.outputs_written & BITFIELD64_BIT(VARYING_SLOT_LAYER) ||
+          s->info.inputs_read & BITFIELD64_BIT(VARYING_SLOT_LAYER)) {
+         spirv_builder_emit_extension(&ctx.builder, "SPV_EXT_shader_viewport_index_layer");
+         spirv_builder_emit_cap(&ctx.builder, SpvCapabilityShaderViewportIndexLayerEXT);
+         spirv_builder_emit_cap(&ctx.builder, SpvCapabilityShaderLayer);
+      }
+   }
+
    if (s->info.num_ssbos || s->info.num_abos)
       spirv_builder_emit_extension(&ctx.builder, "SPV_KHR_storage_buffer_storage_class");
 
