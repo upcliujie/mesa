Grima04/mesa-turnip-kgsl

commit 456b57802eaf95d36e7b950bdb5ffd86a1c0dc63

zink: fix direct image mapping offset
the x and y offsets here were improperly calculated without taking into account:
* layer/level offset
* x/y coord bpp

Fixes: 8d46e35d16e ("zink: introduce opengl over vulkan")
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8058>

src/gallium/drivers/zink/zink_resource.c
@@ -613,9 +613,12 @@ zink_transfer_map(struct pipe_context *pctx,
          vkGetImageSubresourceLayout(screen->dev, res->image, &isr, &srl);
          trans->base.stride = srl.rowPitch;
          trans->base.layer_stride = srl.arrayPitch;
-         ptr = ((uint8_t *)ptr) + box->z * srl.depthPitch +
-                                  box->y * srl.rowPitch +
-                                  box->x;
+         const struct util_format_description *desc = util_format_description(res->format);
+         unsigned offset = srl.offset +
+                           box->z * srl.depthPitch +
+                           (box->y / desc->block.height) * srl.rowPitch +
+                           (box->x / desc->block.width) * (util_format_get_blocksize(res->format) / desc->nr_channels);
+         ptr = ((uint8_t *)ptr) + offset;
       }
    }
 

upcliujie/mesa

commit 3d86978153ceb7169036962b3f0bda70e3921041

zink: really fix direct image mapping offset (I mean it this time)
I got confused and:
* used the vkformat instead of the pipe format for getting format description
* incorrectly calculated bpp

but this time it's definitely 100% fixed I promise

Fixes: 456b57802ea ("zink: fix direct image mapping offset")

src/gallium/drivers/zink/zink_resource.c
@@ -613,11 +613,11 @@ zink_transfer_map(struct pipe_context *pctx,
          vkGetImageSubresourceLayout(screen->dev, res->image, &isr, &srl);
          trans->base.stride = srl.rowPitch;
          trans->base.layer_stride = srl.arrayPitch;
-         const struct util_format_description *desc = util_format_description(res->format);
+         const struct util_format_description *desc = util_format_description(res->base.format);
          unsigned offset = srl.offset +
                            box->z * srl.depthPitch +
                            (box->y / desc->block.height) * srl.rowPitch +
-                           (box->x / desc->block.width) * (util_format_get_blocksize(res->format) / desc->nr_channels);
+                           (box->x / desc->block.width) * (desc->block.bits / 8);
          ptr = ((uint8_t *)ptr) + offset;
       }
    }