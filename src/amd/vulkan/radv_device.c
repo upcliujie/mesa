c9309375908db194b4a893cf51484d5091665bc8
Rhys Perry <pendingchaos02@gmail.com>
2020-12-14 22:00:23 +0000

    radv: set invariantgeom for Shadow of the Tomb Raider
    Work around flickering foliage on GFX10.3
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Cc: mesa-stable

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -606,6 +606,9 @@ radv_handle_per_app_options(struct radv_instance *instance,
 		} else if (!strcmp(name, "DOOMEternal")) {
 			/* Zero VRAM for Doom Eternal to fix rendering issues. */
 			instance->debug_flags |= RADV_DEBUG_ZERO_VRAM;
+		} else if (!strcmp(name, "ShadowOfTheTomb")) {
+			/* Work around flickering foliage on GFX10.3 */
+			instance->debug_flags |= RADV_DEBUG_INVARIANT_GEOM;
 		}
 	}
 
