3646b078ee80094d3b9d656815a5fb4e67e1e690
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2020-12-14 12:15:11 +0200

    egl: plumb protected context creation
    In bd182777c8f2c4 ("egl: implement EGL_EXT_protected_surface support")
    which added support for EGL_EXT_protected_content, the part of the
    extension that deals with protected context creation was mostly not
    implemented.
    
    This add the additional bits plumbing the attribute describing a
    context as protected down the DRI layer.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- include/GL/internal/dri_interface.h
+++ include/GL/internal/dri_interface.h
@@ -1216,6 +1216,12 @@ struct __DRIdri2LoaderExtensionRec {
 #define __DRI_CTX_FLAG_NO_ERROR			0x00000008
 
 /**
+ * \requires __DRI2_RENDERER_HAS_PROTECTED_CONTENT.
+ *
+ */
+#define __DRI_CTX_FLAG_PROTECTED		0x00000010
+
+/**
  * \name Context reset strategies.
  */
 /*@{*/
