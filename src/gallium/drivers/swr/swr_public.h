Grima04/mesa-turnip-kgsl

commit 9ac8f8f490d0ee08ac9ca6f1c6963043c2991443

gallium: Add optional pipe_context to flush_frontbuffer
It's hooked up in all the pipe wrapper drivers, and all the
frontends except a couple places in glx/xlib.

This enables a more efficient path for drivers which use
swrast's Present, but hardware rendering (e.g. d3d12, zink).

Reviewed-by: Dave Airlie <airlied@redhat.com>
Acked-by: Marek Olák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8045>

src/gallium/drivers/swr/swr_public.h
@@ -25,6 +25,7 @@
 #define SWR_PUBLIC_H
 
 struct pipe_screen;
+struct pipe_context;
 struct sw_displaytarget;
 struct sw_winsys;
 struct swr_screen;
@@ -44,6 +45,7 @@ void swr_destroy_screen_internal(struct swr_screen **screen);
 
 #ifdef _WIN32
 void swr_gdi_swap(struct pipe_screen *screen,
+                  struct pipe_conext *ctx,
                   struct pipe_resource *res,
                   void *hDC);
 #endif /* _WIN32 */

src/gallium/auxiliary/driver_ddebug/dd_screen.c
@@ -167,14 +167,16 @@ dd_screen_can_create_resource(struct pipe_screen *_screen,
 
 static void
 dd_screen_flush_frontbuffer(struct pipe_screen *_screen,
+                            struct pipe_context *_pipe,
                             struct pipe_resource *resource,
                             unsigned level, unsigned layer,
                             void *context_private,
                             struct pipe_box *sub_box)
 {
    struct pipe_screen *screen = dd_screen(_screen)->screen;
+   struct pipe_context *pipe = _pipe ? dd_context(_pipe)->pipe : NULL;
 
-   screen->flush_frontbuffer(screen, resource, level, layer, context_private,
+   screen->flush_frontbuffer(screen, pipe, resource, level, layer, context_private,
                              sub_box);
 }
 

src/gallium/auxiliary/driver_noop/noop_pipe.c
@@ -399,6 +399,7 @@ static struct pipe_context *noop_create_context(struct pipe_screen *screen,
  * pipe_screen
  */
 static void noop_flush_frontbuffer(struct pipe_screen *_screen,
+                                   struct pipe_context *ctx,
                                    struct pipe_resource *resource,
                                    unsigned level, unsigned layer,
                                    void *context_private, struct pipe_box *box)

src/gallium/auxiliary/driver_rbug/rbug_screen.c
@@ -356,6 +356,7 @@ rbug_screen_resource_destroy(struct pipe_screen *screen,
 
 static void
 rbug_screen_flush_frontbuffer(struct pipe_screen *_screen,
+                              struct pipe_context *_ctx,
                               struct pipe_resource *_resource,
                               unsigned level, unsigned layer,
                               void *context_private, struct pipe_box *sub_box)
@@ -364,8 +365,10 @@ rbug_screen_flush_frontbuffer(struct pipe_screen *_screen,
    struct rbug_resource *rb_resource = rbug_resource(_resource);
    struct pipe_screen *screen = rb_screen->screen;
    struct pipe_resource *resource = rb_resource->resource;
+   struct pipe_context *ctx = _ctx ? rbug_context(_ctx)->pipe : NULL;
 
    screen->flush_frontbuffer(screen,
+                             ctx,
                              resource,
                              level, layer,
                              context_private, sub_box);

src/gallium/auxiliary/driver_trace/tr_screen.c
@@ -279,6 +279,7 @@ trace_screen_context_create(struct pipe_screen *_screen, void *priv,
 
 static void
 trace_screen_flush_frontbuffer(struct pipe_screen *_screen,
+                               struct pipe_context *_pipe,
                                struct pipe_resource *resource,
                                unsigned level, unsigned layer,
                                void *context_private,
@@ -286,6 +287,7 @@ trace_screen_flush_frontbuffer(struct pipe_screen *_screen,
 {
    struct trace_screen *tr_scr = trace_screen(_screen);
    struct pipe_screen *screen = tr_scr->screen;
+   struct pipe_context *pipe = _pipe ? trace_context(_pipe)->pipe : NULL;
 
    trace_dump_call_begin("pipe_screen", "flush_frontbuffer");
 
@@ -297,7 +299,7 @@ trace_screen_flush_frontbuffer(struct pipe_screen *_screen,
    trace_dump_arg(ptr, context_private);
    */
 
-   screen->flush_frontbuffer(screen, resource, level, layer, context_private, sub_box);
+   screen->flush_frontbuffer(screen, pipe, resource, level, layer, context_private, sub_box);
 
    trace_dump_call_end();
 }

src/gallium/auxiliary/vl/vl_winsys_dri.c
@@ -120,6 +120,7 @@ vl_dri2_get_flush_reply(struct vl_dri_screen *scrn)
 
 static void
 vl_dri2_flush_frontbuffer(struct pipe_screen *screen,
+                          struct pipe_context *pipe,
                           struct pipe_resource *resource,
                           unsigned level, unsigned layer,
                           void *context_private, struct pipe_box *sub_box)

src/gallium/auxiliary/vl/vl_winsys_dri3.c
@@ -555,6 +555,7 @@ dri3_get_screen_for_root(xcb_connection_t *conn, xcb_window_t root)
 
 static void
 vl_dri3_flush_frontbuffer(struct pipe_screen *screen,
+                          struct pipe_context *pipe,
                           struct pipe_resource *resource,
                           unsigned level, unsigned layer,
                           void *context_private, struct pipe_box *sub_box)

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -627,6 +627,7 @@ d3d12_destroy_screen(struct pipe_screen *pscreen)
 
 static void
 d3d12_flush_frontbuffer(struct pipe_screen * pscreen,
+                        struct pipe_context *pctx,
                         struct pipe_resource *pres,
                         unsigned level, unsigned layer,
                         void *winsys_drawable_handle,

upcliujie/mesa

commit e60e96f5ba088f1b6a166093bc9091408f222758

gallium/swr: Fix Windows build
Fixing small mistake in the swr code that caused
compilation issues on Windows

Fixes: 9ac8f8f490d0ee08ac9ca6f1c6963043c2991443

src/gallium/drivers/swr/swr_public.h
@@ -45,7 +45,7 @@ void swr_destroy_screen_internal(struct swr_screen **screen);
 
 #ifdef _WIN32
 void swr_gdi_swap(struct pipe_screen *screen,
-                  struct pipe_conext *ctx,
+                  struct pipe_context *ctx,
                   struct pipe_resource *res,
                   void *hDC);
 #endif /* _WIN32 */