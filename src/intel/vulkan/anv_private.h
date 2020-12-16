2fc8b10f0dbe1015b2d7a4068e169d13733885f6
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2020-12-16 14:09:55 +0200

    intel/common: add detection of protected context support
    v2: Add anv bits
        Fix missing i915 extension chaining helper
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -1037,6 +1037,9 @@ struct anv_physical_device {
      */
     bool                                        has_implicit_ccs;
 
+    /** True if we can create protected contexts. */
+    bool                                        has_protected_contexts;
+
     bool                                        always_flush_cache;
 
     uint32_t                                    eu_total;
