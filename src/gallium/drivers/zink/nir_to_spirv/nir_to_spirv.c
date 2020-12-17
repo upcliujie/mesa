4bb66a88677b159d7a0a9ab282d5b0fb9cd79051
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-12-17 11:12:49 -0500

    zink: add struct type support for ntv

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -248,6 +248,24 @@ get_glsl_type_element(struct ntv_context *ctx, const struct glsl_type *type, Spv
          spirv_builder_emit_array_stride(&ctx->builder, ret, stride);
       return ret;
    }
+   if (glsl_type_is_struct(type)) {
+      static unsigned recursion;
+      recursion++;
+      SpvId types[glsl_get_length(type)];
+      for (unsigned i = 0; i < glsl_get_length(type); i++)
+         types[i] = get_glsl_type_element(ctx, glsl_get_struct_field(type, i), element);
+      SpvId ret = spirv_builder_type_struct(&ctx->builder,
+                                           types,
+                                           glsl_get_length(type));
+      /* only the outermost struct can have the Block decoration */
+      if (recursion == 1)
+         spirv_builder_emit_decoration(&ctx->builder, ret,
+                                       SpvDecorationBlock);
+      for (unsigned i = 0; i < glsl_get_length(type); i++)
+         spirv_builder_emit_member_offset(&ctx->builder, ret, i, glsl_get_struct_field_offset(type, i));
+      recursion--;
+      return ret;
+   }
 
    if (glsl_type_is_matrix(type))
       return spirv_builder_type_matrix(&ctx->builder,
