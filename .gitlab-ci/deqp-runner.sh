Grima04/mesa-turnip-kgsl

commit bf29daa1b5a33ce2cc62b7743b4bf7220c7b8539

ci/deqp: Switch to a new dEQP runner written in Rust.
I found the C++ runner hard to develop on, and we had stability issues and
outstanding feature needs that made me want something I felt good about
hacking on.  Thus, Rewrite It In Rust of the deqp runner.

The new runner includes:

- Skip lists don't reshuffle the test list.
- Known-flake handling without resorting to skip lists (fixing our main CI
  reliability issue on a3xx right now).
- Per-thread Vulkan shader caches should speed up VK CI runtime.
- Tracking of crashes separate from fails (so we can see progress on that
  front).
- Logging of deqp stderr spam (particularly assertion failures!) in the CI
  log.
- Integrated QPA filtering so we don't have bash perf issues for it.
- Logging of what caselist to go look at for a given error report (in red,
  so it's easier to find in your CI log).
- The code is 1/3 unit tests, and easy to extend for more coverage.
- Non-LAVA CI runs create a failures.csv in artifacts that you can check
  in as your deqp-*-fails.txt file.
- Test runtime is included in results.csv so you can debug how to speed up
  your CI job.
- Pretty summary at the end of the run of slow/flaky/failed tests.

Since this is a new runner with a different RNG, the test groups are
shuffled one more time.  This seems to result in some panfrost T720
stability issues (See its new deqp-panfrost-t720-flakes.txt), and one new
flake in freedreno a630.

Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7434>

.gitlab-ci/deqp-runner.sh
@@ -12,11 +12,6 @@ DEQP_OPTIONS="$DEQP_OPTIONS --deqp-surface-type=pbuffer"
 DEQP_OPTIONS="$DEQP_OPTIONS --deqp-gl-config-name=$DEQP_CONFIG"
 DEQP_OPTIONS="$DEQP_OPTIONS --deqp-visibility=hidden"
 
-# deqp's shader cache (for vulkan) is not multiprocess safe for a common
-# filename, see:
-# https://gitlab.freedesktop.org/mesa/parallel-deqp-runner/-/merge_requests/13
-DEQP_OPTIONS="$DEQP_OPTIONS --deqp-shadercache=disable"
-
 if [ -z "$DEQP_VER" ]; then
    echo 'DEQP_VER must be set to something like "gles2", "gles31" or "vk" for the test run'
    exit 1
@@ -45,7 +40,7 @@ export VK_ICD_FILENAMES=`pwd`/install/share/vulkan/icd.d/"$VK_DRIVER"_icd.`uname
 # I never figured out.
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-RESULTS=`pwd`/results
+RESULTS=`pwd`/${DEQP_RESULTS_DIR:-results}
 mkdir -p $RESULTS
 
 # Generate test case list file.
@@ -78,42 +73,60 @@ if [ ! -s /tmp/case-list.txt ]; then
 fi
 
 if [ -n "$DEQP_EXPECTED_FAILS" ]; then
-    XFAIL="--xfail-list $INSTALL/$DEQP_EXPECTED_FAILS"
+    BASELINE="--baseline $INSTALL/$DEQP_EXPECTED_FAILS"
+fi
+
+if [ -n "$DEQP_FLAKES" ]; then
+    FLAKES="--flakes $INSTALL/$DEQP_FLAKES"
 fi
 
 set +e
 
 if [ -n "$DEQP_PARALLEL" ]; then
-   JOB="--job $DEQP_PARALLEL"
+   JOB="--jobs $DEQP_PARALLEL"
 elif [ -n "$FDO_CI_CONCURRENT" ]; then
-   JOB="--job $FDO_CI_CONCURRENT"
+   JOB="--jobs $FDO_CI_CONCURRENT"
 else
-   JOB="--job 4"
+   JOB="--jobs 4"
 fi
+
+# If this CI lab lacks artifacts support, print the whole list of failures/flakes.
+if [ -z "$DEQP_NO_SAVE_RESULTS" ]; then
+   SUMMARY_LIMIT="--summary-limit 0"
+fi
+
+# Silence the debug output for apps triggering GL errors, since dEQP will do a lot of that.
+export MESA_DEBUG=silent
 
 run_cts() {
     deqp=$1
     caselist=$2
     output=$3
     deqp-runner \
+        run \
         --deqp $deqp \
-        --output $output \
+        --output $RESULTS \
         --caselist $caselist \
-        --exclude-list $INSTALL/$DEQP_SKIPS \
-        --compact-display false \
-        $XFAIL \
+        --skips $INSTALL/$DEQP_SKIPS \
+        $BASELINE \
+        $FLAKES \
         $JOB \
-	--allow-flakes true \
+        $SUMMARY_LIMIT \
 	$DEQP_RUNNER_OPTIONS \
         -- \
         $DEQP_OPTIONS
 }
 
 report_flakes() {
+    flakes=`grep ",Flake" $1 | sed 's|,Flake.*||g'`
+    if [ -z "$flakes" ]; then
+        return 0
+    fi
+
     if [ -z "$FLAKES_CHANNEL" ]; then
         return 0
     fi
-    flakes=$1
+
     # The nick needs to be something unique so that multiple runners
     # connecting at the same time don't race for one nick and get blocked.
     # freenode has a 16-char limit on nicks (9 is the IETF standard, but
@@ -138,57 +151,13 @@ report_flakes() {
         desc="$desc on branch $CI_COMMIT_BRANCH ($CI_COMMIT_TITLE)"
     fi
     echo "PRIVMSG $channel :$desc"
-    for flake in `cat $flakes`; do
+    for flake in $flakes; do
         echo "PRIVMSG $channel :$flake"
     done
     echo "PRIVMSG $channel :See $CI_JOB_URL/artifacts/browse/results/"
     echo "QUIT"
     ) | nc irc.freenode.net 6667 > /dev/null
 
-}
-
-extract_xml_result() {
-    testcase=$1
-    shift 1
-    qpas=$*
-    start="#beginTestCaseResult $testcase"
-
-    # Pick the first QPA mentioning our testcase
-    qpa=`grep -l "$start" $qpas | head -n 1`
-
-    # If we found one, go extract just that testcase's contents from the QPA
-    # to a new QPA, then do testlog-to-xml on that.
-    if [ -n "$qpa" ]; then
-        while IFS= read -r line; do
-            if [ "$line" = "$start" ]; then
-                dst="$testcase.qpa"
-                echo "#beginSession" > $dst
-                echo "$line" >> $dst
-                while IFS= read -r line; do
-                    if [ "$line" = "#endTestCaseResult" ]; then
-                        echo "$line" >> $dst
-                        echo "#endSession" >> $dst
-                        /deqp/executor/testlog-to-xml $dst "$RESULTS/$testcase$DEQP_RUN_SUFFIX.xml"
-                        # copy the stylesheets here so they only end up in artifacts
-                        # if we have one or more result xml in artifacts
-                        cp /deqp/testlog.css "$RESULTS/"
-                        cp /deqp/testlog.xsl "$RESULTS/"
-                        return 0
-                    fi
-                    echo "$line" >> $dst
-                done
-                return 1
-            fi
-        done < $qpa
-    fi
-}
-
-extract_xml_results() {
-    qpas=$*
-    while IFS= read -r testcase; do
-        testcase=${testcase%,*}
-        extract_xml_result $testcase $qpas
-    done
 }
 
 # Generate junit results
@@ -278,74 +247,39 @@ else
     quiet check_renderer
 fi
 
-RESULTSFILE=$RESULTS/cts-runner-results$DEQP_RUN_SUFFIX.txt
-UNEXPECTED_RESULTSFILE=$RESULTS/cts-runner-unexpected-results$DEQP_RUN_SUFFIX.txt
-FLAKESFILE=$RESULTS/cts-runner-flakes$DEQP_RUN_SUFFIX.txt
+RESULTS_CSV=$RESULTS/results.csv
+FAILURES_CSV=$RESULTS/failures.csv
 
-run_cts $DEQP /tmp/case-list.txt $RESULTSFILE
+run_cts $DEQP /tmp/case-list.txt $RESULTS_CSV
 DEQP_EXITCODE=$?
 
 echo "System load: $(cut -d' ' -f1-3 < /proc/loadavg)"
 echo "# of CPU cores: $(cat /proc/cpuinfo | grep processor | wc -l)"
 
+# Remove the shader cache, no need to include in the artifacts.
+find $RESULTS -name \*.shader_cache | xargs rm -f
+
 # junit is disabled, because it overloads gitlab.freedesktop.org to parse it.
-#quiet generate_junit $RESULTSFILE > $RESULTS/results.xml
+# quiet generate_junit $RESULTS_CSV > $RESULTS/results.xml
 
-if [ $DEQP_EXITCODE -ne 0 ]; then
-    # preserve caselist files in case of failures:
-    cp /tmp/deqp_runner.*.txt $RESULTS/
-    egrep -v ",Pass|,Skip|,ExpectedFail" $RESULTSFILE > $UNEXPECTED_RESULTSFILE
+# Turn up to the first 50 individual test QPA files from failures or flakes into
+# XML results you can view from the browser.
+qpas=`find $RESULTS -name \*.qpa -a ! -name deqp-info.qpa`
+if [ -n "$qpas" ]; then
+    shard_qpas=`echo "$qpas" | grep dEQP- | head -n 50`
+    for qpa in $shard_qpas; do
+        xml=`echo $qpa | sed 's|\.qpa|.xml|'`
+        /deqp/executor/testlog-to-xml $qpa $xml
+    done
 
-    # deqp-runner's flake detection won't perfectly detect all flakes, so
-    # allow the driver to list some known flakes that won't intermittently
-    # fail people's pipelines (while still allowing them to run and be
-    # reported to IRC in the usual flake detection path).  If we had some
-    # fails listed (so this wasn't a total runner failure), then filter out
-    # the known flakes and see if there are any issues left.
-    if [ -n "$DEQP_FLAKES" -a -s $UNEXPECTED_RESULTSFILE ]; then
-        set +x
-        while read line; do
-            line=`echo $line | sed 's|#.*||g'`
-            if [ -n "$line" ]; then
-                sed -i "/$line/d" $UNEXPECTED_RESULTSFILE
-            fi
-        done < $INSTALL/$DEQP_FLAKES
-        set -x
+    cp /deqp/testlog.css "$RESULTS/"
+    cp /deqp/testlog.xsl "$RESULTS/"
 
-        if [ ! -s $UNEXPECTED_RESULTSFILE ]; then
-            exit 0
-        fi
-    fi
-
-    if [ -z "$DEQP_NO_SAVE_RESULTS" ]; then
-        echo "Some unexpected results found (see cts-runner-results.txt in artifacts for full results):"
-        head -n 50 $UNEXPECTED_RESULTSFILE
-
-        # Save the logs for up to the first 50 unexpected results:
-        head -n 50 $UNEXPECTED_RESULTSFILE | quiet extract_xml_results /tmp/*.qpa
-    else
-        echo "Unexpected results found:"
-        cat $UNEXPECTED_RESULTSFILE
-    fi
-else
-    grep ",Flake" $RESULTSFILE > $FLAKESFILE
-
-    count=`cat $FLAKESFILE | wc -l`
-    if [ $count -gt 0 ]; then
-        echo "Some flakes found (see cts-runner-flakes.txt in artifacts for full results):"
-        head -n 50 $FLAKESFILE
-
-        if [ -z "$DEQP_NO_SAVE_RESULTS" ]; then
-            # Save the logs for up to the first 50 flakes:
-            head -n 50 $FLAKESFILE | quiet extract_xml_results /tmp/*.qpa
-        fi
-
-        # Report the flakes to IRC channel for monitoring (if configured):
-        quiet report_flakes $FLAKESFILE
-    else
-        # no flakes, so clean-up:
-        rm $FLAKESFILE
-    fi
+    # Remove all the QPA files (extracted or not) now that we have the XML we want.
+    echo $qpas | xargs rm -f
 fi
+
+# Report the flakes to the IRC channel for monitoring (if configured):
+quiet report_flakes $RESULTS_CSV
 
 exit $DEQP_EXITCODE

.gitlab-ci.yml
@@ -352,13 +352,13 @@ x86_test-base:
 x86_test-gl:
   extends: .use-x86_test-base
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_test-gl "2020-11-05-kmod"
+    FDO_DISTRIBUTION_TAG: &x86_test-gl "2020-11-05-deqp-runner"
 
 # Debian 10 based x86 test image for VK
 x86_test-vk:
   extends: .use-x86_test-base
   variables:
-    FDO_DISTRIBUTION_TAG: &x86_test-vk "2020-11-05-kmod"
+    FDO_DISTRIBUTION_TAG: &x86_test-vk "2020-11-05-deqp-runner"
 
 # Debian 9 based x86 build image (old LLVM)
 x86_build_old:
@@ -414,7 +414,7 @@ arm64_test:
   extends:
     - .use-arm_test-base
   variables:
-    FDO_DISTRIBUTION_TAG: &arm64_test "2020-09-28-deqp-surfaceless-fix"
+    FDO_DISTRIBUTION_TAG: &arm64_test "2020-11-03-deqp-runner"
 
 .use-arm64_test:
   variables:
@@ -1122,6 +1122,7 @@ softpipe-gles2:
   variables:
     DEQP_EXPECTED_FAILS: deqp-softpipe-fails.txt
     DEQP_SKIPS: deqp-softpipe-skips.txt
+    DEQP_FLAKES: deqp-softpipe-flakes.txt
     GALLIUM_DRIVER: "softpipe"
     DEQP_EXPECTED_RENDERER: softpipe
 

.gitlab-ci/bare-metal/rootfs-setup.sh
@@ -35,7 +35,7 @@ for var in \
     DEQP_NO_SAVE_RESULTS \
     DEQP_FLAKES \
     DEQP_PARALLEL \
-    DEQP_RUN_SUFFIX \
+    DEQP_RESULTS_DIR \
     DEQP_SKIPS \
     DEQP_VARIANT \
     DEQP_VER \

.gitlab-ci/bare-metal/serial_buffer.py
@@ -109,7 +109,7 @@ class SerialBuffer:
                     line = line.decode(errors="replace")
 
                     time = datetime.now().strftime('%y-%m-%d %H:%M:%S')
-                    print("{time} {prefix}{line}{endc}".format(
+                    print("{endc}{time} {prefix}{line}".format(
                         time=time, prefix=self.prefix, line=line, endc='\033[0m'), flush=True, end='')
 
                     self.line_queue.put(line)

.gitlab-ci/build-cts-runner.sh
@@ -1,10 +1,0 @@
-#!/bin/bash
-
-set -ex
-
-git clone https://gitlab.freedesktop.org/mesa/parallel-deqp-runner.git --depth 1 -b mesa-ci-2020-06-15 /parallel-deqp-runner
-pushd /parallel-deqp-runner
-meson build/ $EXTRA_MESON_ARGS
-ninja -C build install
-popd
-rm -rf /parallel-deqp-runner

.gitlab-ci/build-deqp-runner.sh
@@ -1,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+cargo install deqp-runner \
+  -j ${FDO_CI_CONCURRENT:-4} \
+  --version 0.1.5 \
+  --root /usr/local \
+  $EXTRA_CARGO_ARGS

.gitlab-ci/build-rust.sh
@@ -1,0 +1,31 @@
+#!/bin/bash
+
+# Note that this script is not actually "building" rust, but build- is the
+# convention for the shared helpers for putting stuff in our containers.
+
+set -ex
+
+# cargo (and rustup) wants to store stuff in $HOME/.cargo, and binaries in
+# $HOME/.cargo/bin.  Make bin a link to a public bin directory so the commands
+# are just available to all build jobs.
+mkdir -p $HOME/.cargo
+ln -s /usr/local/bin $HOME/.cargo/bin
+
+# For rust in Mesa, we use rustup to install.  This lets us pick an arbitrary
+# version of the compiler, rather than whatever the container's Debian comes
+# with.
+#
+# Pick the rust compiler (1.41) available in Debian stable, and pick a specific
+# snapshot from rustup so the compiler doesn't drift on us.
+wget https://sh.rustup.rs -O - | \
+    sh -s -- -y --default-toolchain 1.41.1-2020-02-27
+
+# Set up a config script for cross compiling -- cargo needs your system cc for
+# linking in cross builds, but doesn't know what you want to use for system cc.
+cat > /root/.cargo/config <<EOF
+[target.armv7-unknown-linux-gnueabihf]
+linker = "arm-linux-gnueabihf-gcc"
+
+[target.aarch64-unknown-linux-gnu]
+linker = "aarch64-linux-gnu-gcc"
+EOF

.gitlab-ci/container/container_post_build.sh
@@ -2,4 +2,7 @@
 
 apt-get autoremove -y --purge
 
+# Clean up any build cache for rust.
+rm -rf /.cargo
+
 ccache --show-stats

upcliujie/mesa

commit 0d6d81a82572ba005ebb958bbbdfc59ae54a5f6e

ci/deqp: Fix inverted meaning of DEQP_NO_SAVE_RESULTS.
When set, we want to summary-limit 0, but the meaning was inverted.  Sigh,
coding in bash.

Fixes: bf29daa1b5a3 ("ci/deqp: Switch to a new dEQP runner written in Rust.")

.gitlab-ci/deqp-runner.sh
@@ -98,7 +98,7 @@ else
 fi
 
 # If this CI lab lacks artifacts support, print the whole list of failures/flakes.
-if [ -z "$DEQP_NO_SAVE_RESULTS" ]; then
+if [ -n "$DEQP_NO_SAVE_RESULTS" ]; then
    SUMMARY_LIMIT="--summary-limit 0"
 fi
 