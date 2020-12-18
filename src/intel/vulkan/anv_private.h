c3a1a8c8dd4ddcf76a0d245a51f449cb195918b0
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2020-12-18 16:19:16 +0200

    anv: track buffer creation flags
    So we know a buffer is protected.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -2320,6 +2320,7 @@ struct anv_buffer {
    struct anv_device *                          device;
    VkDeviceSize                                 size;
 
+   VkBufferCreateFlags                          create_flags;
    VkBufferUsageFlags                           usage;
 
    /* Set when bound */
