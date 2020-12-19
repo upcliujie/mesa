82ad5aa3f1144a49aca7ea0f223e7baff88ae486
Marek Olšák <marek.olsak@amd.com>
2020-12-19 01:41:04 -0500

    mesa: remove/replace FLUSH_VERTICES when it doesn't do anything
    In _mesa_PopAttrib, we call FLUSH_VERTICES at the beginning, so we don't
    need to do it again.
    
    In the MultiMode functions, we call standard Draw functions
    using the dispatch, which do the flushing by themselves.

--- src/mesa/main/draw.c
+++ src/mesa/main/draw.c
@@ -2185,8 +2185,6 @@ _mesa_MultiModeDrawArraysIBM( const GLenum * mode, const GLint * first,
    GET_CURRENT_CONTEXT(ctx);
    GLint i;
 
-   FLUSH_VERTICES(ctx, 0);
-
    for ( i = 0 ; i < primcount ; i++ ) {
       if ( count[i] > 0 ) {
          GLenum m = *((GLenum *) ((GLubyte *) mode + i * modestride));
@@ -2204,10 +2202,6 @@ _mesa_MultiModeDrawElementsIBM( const GLenum * mode, const GLsizei * count,
 {
    GET_CURRENT_CONTEXT(ctx);
    GLint i;
-
-   FLUSH_VERTICES(ctx, 0);
-
-   /* XXX not sure about ARB_vertex_buffer_object handling here */
 
    for ( i = 0 ; i < primcount ; i++ ) {
       if ( count[i] > 0 ) {
