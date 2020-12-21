018c0a034c34c137e3036c4d8653dc4ba57c9cc2
Marek Olšák <marek.olsak@amd.com>
2020-12-21 01:34:38 -0500

    radeonsi: unify uploaders on APUs too
    const_uploader and stream_uploader point to the same uploader.

--- src/gallium/drivers/radeonsi/si_pipe.c
+++ src/gallium/drivers/radeonsi/si_pipe.c
@@ -498,14 +498,15 @@ static struct pipe_context *si_create_context(struct pipe_screen *screen, unsign
 
    /* Initialize public allocators. */
    bool smart_access_memory = sscreen->info.smart_access_memory;
+   bool is_apu = !sscreen->info.has_dedicated_vram;
    sctx->b.stream_uploader =
       u_upload_create(&sctx->b, 1024 * 1024, 0,
-                      smart_access_memory ? PIPE_USAGE_DEFAULT : PIPE_USAGE_STREAM,
+                      smart_access_memory && !is_apu ? PIPE_USAGE_DEFAULT : PIPE_USAGE_STREAM,
                       SI_RESOURCE_FLAG_32BIT); /* same flags as const_uploader */
    if (!sctx->b.stream_uploader)
       goto fail;
 
-   if (smart_access_memory) {
+   if (smart_access_memory || is_apu) {
       sctx->b.const_uploader = sctx->b.stream_uploader;
    } else {
       sctx->b.const_uploader =
