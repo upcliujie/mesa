a92c5c982df8db276e800825542311e9e186b760
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-12-24 09:16:33 -0500

    zink: add batch flag for checking renderpass state

--- src/gallium/drivers/zink/zink_batch.h
+++ src/gallium/drivers/zink/zink_batch.h
@@ -56,6 +56,8 @@ struct zink_batch {
    struct util_dynarray zombie_samplers;
 
    struct set *active_queries; /* zink_query objects which were active at some point in this batch */
+
+   bool in_rp; //renderpass is currently active
 };
 
 void
