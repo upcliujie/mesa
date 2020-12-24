221413c164b460822926f18d26f8609a08602284
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-12-24 09:17:23 -0500

    zink: remove renderpass refcounting
    this is useless because we never destroy renderpasses during the lifetime
    of a process and don't want to do so
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- src/gallium/drivers/zink/zink_batch.c
+++ src/gallium/drivers/zink/zink_batch.c
@@ -26,7 +26,6 @@ reset_batch(struct zink_context *ctx, struct zink_batch *batch)
    zink_fence_finish(screen, batch->fence, PIPE_TIMEOUT_INFINITE);
    zink_fence_reference(screen, &batch->fence, NULL);
 
-   zink_render_pass_reference(screen, &batch->rp, NULL);
    zink_framebuffer_reference(screen, &batch->fb, NULL);
    set_foreach(batch->programs, entry) {
       struct zink_gfx_program *prog = (struct zink_gfx_program*)entry->key;
