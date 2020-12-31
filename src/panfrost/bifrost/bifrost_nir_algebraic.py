26bf5336860438e555bd90fbec19888a6e2e27fd
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2020-12-31 16:51:43 -0500

    pan/bi: Ingest 1-bit booleans
    It's not clear to me yet if this is a win or not, but it should allow us
    more flexibility, I hope.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

--- src/panfrost/bifrost/bifrost_nir_algebraic.py
+++ src/panfrost/bifrost/bifrost_nir_algebraic.py
@@ -38,17 +38,15 @@ algebraic_late = [
     (('ineg', a), ('isub', 0, a)),
 ]
 
-for isz in ('8', '16', '32'):
-        for osz in ('16', '32', '64'):
-                algebraic_late += [(('b2f' + osz, 'a@' + isz), ('b' + isz + 'csel', a, 1.0, 0.0))]
+for osz in ('16', '32'):
+    algebraic_late += [(('b2f' + osz, 'a@1'), ('bcsel', a, 1.0, 0.0))]
 
 # There's no native integer min/max instruction, lower those to cmp+bcsel
-for sz in ('8', '16', '32'):
-    for t in ('i', 'u'):
-        algebraic_late += [
-            ((t + 'min', 'a@' + sz, 'b@' + sz), ('b' + sz + 'csel', (t + 'lt' + sz, a, b), a, b)),
-            ((t + 'max', 'a@' + sz, 'b@' + sz), ('b' + sz + 'csel', (t + 'lt' + sz, b, a), a, b))
-        ]
+for t in ('i', 'u'):
+    algebraic_late += [
+        ((t + 'min', a, b), ('bcsel', (t + 'lt', a, b), a, b)),
+        ((t + 'max', a, b), ('bcsel', (t + 'lt', b, a), a, b))
+    ]
 
 # Bifrost doesn't have fp16 for a lot of special ops
 SPECIAL = ['fexp2', 'flog2', 'fsin', 'fcos']
@@ -56,9 +54,9 @@ SPECIAL = ['fexp2', 'flog2', 'fsin', 'fcos']
 for op in SPECIAL:
         algebraic_late += [((op + '@16', a), ('f2f16', (op, ('f2f32', a))))]
 
-algebraic_late += [(('f2b32', a), ('fneu32', a, 0.0)),
-             (('i2b32', a), ('ine32', a, 0)),
-             (('b2i32', a), ('iand', 'a@32', 1))]
+algebraic_late += [(('f2b1', a), ('fneu', a, 0.0)),
+             (('i2b1', a), ('ine', a, 0)),
+             (('b2b1', a), ('ine', a, 0))]
 
 def main():
     parser = argparse.ArgumentParser()
