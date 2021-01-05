e963d35efe1c1c1e0f5b0faea37480fdc6073f6a
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-01-05 11:58:28 +0100

    zink: use ConstOffset for nir_tex_src_offset
    Quote from the OpenGL Shading Language spec, version 4.40, section 8.9.2
    "Texel Lookup Functions":
    
    > The offset value must be a constant expression.
    
    So, until we start consuming SPIR-V shaders, it seems we don't need to
    deal with non-constant offsets.
    
    This means we can avoid lowering this away in some cases.
    
    Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>

--- src/gallium/drivers/zink/nir_to_spirv/spirv_builder.c
+++ src/gallium/drivers/zink/nir_to_spirv/spirv_builder.c
@@ -646,7 +646,7 @@ spirv_builder_emit_image_sample(struct spirv_builder *b,
                                 SpvId dref,
                                 SpvId dx,
                                 SpvId dy,
-                                SpvId offset)
+                                SpvId const_offset)
 {
    SpvId result = spirv_builder_new_id(b);
 
@@ -676,9 +676,9 @@ spirv_builder_emit_image_sample(struct spirv_builder *b,
       extra_operands[++num_extra_operands] = dy;
       operand_mask |= SpvImageOperandsGradMask;
    }
-   if (offset) {
-      extra_operands[++num_extra_operands] = offset;
-      operand_mask |= SpvImageOperandsOffsetMask;
+   if (const_offset) {
+      extra_operands[++num_extra_operands] = const_offset;
+      operand_mask |= SpvImageOperandsConstOffsetMask;
    }
 
    /* finalize num_extra_operands / extra_operands */
@@ -721,7 +721,7 @@ spirv_builder_emit_image_gather(struct spirv_builder *b,
                                SpvId component,
                                SpvId lod,
                                SpvId sample,
-                               SpvId offset,
+                               SpvId const_offset,
                                SpvId dref)
 {
    SpvId result = spirv_builder_new_id(b);
@@ -738,9 +738,9 @@ spirv_builder_emit_image_gather(struct spirv_builder *b,
       extra_operands[++num_extra_operands] = sample;
       operand_mask |= SpvImageOperandsSampleMask;
    }
-   if (offset) {
-      extra_operands[++num_extra_operands] = offset;
-      operand_mask |= SpvImageOperandsOffsetMask;
+   if (const_offset) {
+      extra_operands[++num_extra_operands] = const_offset;
+      operand_mask |= SpvImageOperandsConstOffsetMask;
    }
    if (dref)
       op = SpvOpImageDrefGather;
@@ -773,7 +773,7 @@ spirv_builder_emit_image_fetch(struct spirv_builder *b,
                                SpvId coordinate,
                                SpvId lod,
                                SpvId sample,
-                               SpvId offset)
+                               SpvId const_offset)
 {
    SpvId result = spirv_builder_new_id(b);
 
@@ -788,9 +788,9 @@ spirv_builder_emit_image_fetch(struct spirv_builder *b,
       extra_operands[++num_extra_operands] = sample;
       operand_mask |= SpvImageOperandsSampleMask;
    }
-   if (offset) {
-      extra_operands[++num_extra_operands] = offset;
-      operand_mask |= SpvImageOperandsOffsetMask;
+   if (const_offset) {
+      extra_operands[++num_extra_operands] = const_offset;
+      operand_mask |= SpvImageOperandsConstOffsetMask;
    }
 
    /* finalize num_extra_operands / extra_operands */
