af7bd5ab3780ee1cfb00d1ef13609a72c857c3ea
Adam Jackson <ajax@redhat.com>
2021-01-07 13:48:34 -0500

    s390x tripwire

--- src/mesa/main/tests/mesa_formats.cpp
+++ src/mesa/main/tests/mesa_formats.cpp
@@ -150,8 +150,12 @@ TEST(MesaFormatsTest, IntensityToRed)
 static mesa_format fffat_wrap(GLenum format, GLenum type)
 {
    uint32_t f = _mesa_format_from_format_and_type(format, type);
+   if (!f)
+      fprintf(stderr, "AAA mfffat failed!\n");
    if (_mesa_format_is_mesa_array_format(f))
       f = _mesa_format_from_array_format((mesa_array_format)f);
+   if (!f)
+      fprintf(stderr, "BBB mfffat failed!\n");
    return (mesa_format)f;
 }
 
