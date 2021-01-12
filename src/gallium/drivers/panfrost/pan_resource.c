Grima04/mesa-turnip-kgsl

commit 3c92abe359379066a166f86fe7bfbb4da8029478

panfrost: Use panfrost_get_layer_stride() instead of open-coding it
If we don't do that we don't account for CRC buffers.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8125>

src/gallium/drivers/panfrost/pan_resource.c
@@ -882,7 +882,8 @@ panfrost_ptr_map(struct pipe_context *pctx,
         if (drm_is_afbc(rsrc->layout.modifier)) {
                 struct panfrost_resource *staging = pan_alloc_staging(ctx, rsrc, level, box);
                 transfer->base.stride = staging->layout.slices[0].line_stride;
-                transfer->base.layer_stride = transfer->base.stride * box->height;
+                transfer->base.layer_stride =
+                        panfrost_get_layer_stride(&staging->layout, level);
 
                 transfer->staging.rsrc = &staging->base;
 

upcliujie/mesa

commit 170ba204f1568c010755fa4fd6f1b3d6cce3abb1

panfrost: Get layer stride of level 0 on staging resources
Staging resource have one level, we shouldn't query the stride of
level > 0.

Fixes: 3c92abe35937 ("panfrost: Use panfrost_get_layer_stride() instead of open-coding it")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

src/gallium/drivers/panfrost/pan_resource.c
@@ -1053,7 +1053,7 @@ panfrost_ptr_map(struct pipe_context *pctx,
                 struct panfrost_resource *staging = pan_alloc_staging(ctx, rsrc, level, box);
                 transfer->base.stride = staging->layout.slices[0].line_stride;
                 transfer->base.layer_stride =
-                        panfrost_get_layer_stride(&staging->layout, level);
+                        panfrost_get_layer_stride(&staging->layout, 0);
 
                 transfer->staging.rsrc = &staging->base;
 