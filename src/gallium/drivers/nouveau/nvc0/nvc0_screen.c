c000e0470617b7d6ac020a7f1ef108b656ece50a
Karol Herbst <kherbst@redhat.com>
2021-01-12 10:56:50 +0100

    460

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -131,9 +131,9 @@ nvc0_screen_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_MAX_TEXTURE_BUFFER_SIZE:
       return 128 * 1024 * 1024;
    case PIPE_CAP_GLSL_FEATURE_LEVEL:
-      return 430;
+      return 460;
    case PIPE_CAP_GLSL_FEATURE_LEVEL_COMPATIBILITY:
-      return 430;
+      return 460;
    case PIPE_CAP_MAX_RENDER_TARGETS:
       return 8;
    case PIPE_CAP_MAX_DUAL_SOURCE_RENDER_TARGETS:
