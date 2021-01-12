Grima04/mesa-turnip-kgsl

commit 5e8f6967b1d148109fbaa38fc4a70f71f8965932

radv: add support for fast-clearing DCC layers on GFX9+
Layers are contiguous in memory.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8368>

src/amd/vulkan/radv_meta_clear.c
@@ -1401,6 +1401,7 @@ radv_clear_dcc(struct radv_cmd_buffer *cmd_buffer,
 	       const VkImageSubresourceRange *range, uint32_t value)
 {
 	uint32_t level_count = radv_get_levelCount(image, range);
+	uint32_t layer_count = radv_get_layerCount(image, range);
 	uint32_t flush_bits = 0;
 
 	/* Mark the image as being compressed. */
@@ -1414,7 +1415,9 @@ radv_clear_dcc(struct radv_cmd_buffer *cmd_buffer,
 		if (cmd_buffer->device->physical_device->rad_info.chip_class >= GFX9) {
 			/* Mipmap levels aren't implemented. */
 			assert(level == 0);
-			size = image->planes[0].surface.dcc_size;
+
+			offset += image->planes[0].surface.dcc_slice_size * range->baseArrayLayer;
+			size = image->planes[0].surface.dcc_slice_size * layer_count;
 		} else {
 			const struct legacy_surf_level *surf_level =
 				&image->planes[0].surface.u.legacy.level[level];

upcliujie/mesa

commit 7fd85499b551d44a1e6522dc112c357a8c104401

radv: fix clearing DCC on GFX9
dcc_slice_size is in DWORD on GFX9... Also, layers aren't supported
because they might be interleaved. Fix this by clearing the entire
DCC buffer.

Fixes: 5e8f6967b1d ("radv: add support for fast-clearing DCC layers on GFX9+")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_meta_clear.c
@@ -1412,12 +1412,16 @@ radv_clear_dcc(struct radv_cmd_buffer *cmd_buffer,
 		uint32_t level = range->baseMipLevel + l;
 		uint64_t size;
 
-		if (cmd_buffer->device->physical_device->rad_info.chip_class >= GFX9) {
+		if (cmd_buffer->device->physical_device->rad_info.chip_class >= GFX10) {
 			/* Mipmap levels aren't implemented. */
 			assert(level == 0);
 
 			offset += image->planes[0].surface.dcc_slice_size * range->baseArrayLayer;
 			size = image->planes[0].surface.dcc_slice_size * layer_count;
+		} else if (cmd_buffer->device->physical_device->rad_info.chip_class == GFX9) {
+			/* Mipmap levels and layers aren't implemented. */
+			assert(level == 0);
+			size = image->planes[0].surface.dcc_size;
 		} else {
 			const struct legacy_surf_level *surf_level =
 				&image->planes[0].surface.u.legacy.level[level];