Grima04/mesa-turnip-kgsl

commit 07c1504d1b08d4f61aeada7364d63e26d05d05f9

radeonsi: implement SQTT support
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8002>

src/gallium/drivers/radeonsi/si_pipe.h
@@ -31,6 +31,7 @@
 #include "util/u_idalloc.h"
 #include "util/u_suballoc.h"
 #include "util/u_threaded_context.h"
+#include "ac_sqtt.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -233,6 +234,7 @@ enum
    DBG_NO_FMASK,
 
    DBG_TMZ,
+   DBG_SQTT,
 
    DBG_COUNT
 };
@@ -1297,6 +1299,11 @@ struct si_context {
    struct hash_table *dirty_implicit_resources;
 
    pipe_draw_vbo_func draw_vbo[NUM_GFX_VERSIONS - GFX6][2][2][2][2];
+
+   /* SQTT */
+   struct ac_thread_trace_data *thread_trace;
+   struct pipe_fence_handle *last_sqtt_fence;
+   bool thread_trace_enabled;
 };
 
 /* si_blit.c */
@@ -1557,6 +1564,16 @@ void vi_separate_dcc_process_and_reset_stats(struct pipe_context *ctx, struct si
 bool si_texture_disable_dcc(struct si_context *sctx, struct si_texture *tex);
 void si_init_screen_texture_functions(struct si_screen *sscreen);
 void si_init_context_texture_functions(struct si_context *sctx);
+
+/* si_sqtt.c */
+void si_sqtt_write_event_marker(struct si_context* sctx, struct radeon_cmdbuf *rcs,
+                                enum rgp_sqtt_marker_event_type api_type,
+                                uint32_t vertex_offset_user_data,
+                                uint32_t instance_offset_user_data,
+                                uint32_t draw_index_user_data);
+bool si_init_thread_trace(struct si_context *sctx);
+void si_destroy_thread_trace(struct si_context *sctx);
+void si_handle_thread_trace(struct si_context *sctx, struct radeon_cmdbuf *rcs);
 
 /*
  * common helpers

src/gallium/drivers/radeonsi/Makefile.sources
@@ -37,6 +37,7 @@ C_SOURCES := \
 	si_shader_llvm_vs.c \
 	si_shader_nir.c \
 	si_shaderlib_tgsi.c \
+	si_sqtt.c \
 	si_state.c \
 	si_state_binning.c \
 	si_state_draw.cpp \

src/gallium/drivers/radeonsi/meson.build
@@ -57,6 +57,7 @@ files_libradeonsi = files(
   'si_shader_llvm_vs.c',
   'si_shader_nir.c',
   'si_shaderlib_tgsi.c',
+  'si_sqtt.c',
   'si_state.c',
   'si_state.h',
   'si_state_binning.c',

src/gallium/drivers/radeonsi/si_compute.c
@@ -793,6 +793,11 @@ static void si_emit_dispatch_packets(struct si_context *sctx, const struct pipe_
       radeon_emit(cs, info->grid[2]);
       radeon_emit(cs, dispatch_initiator);
    }
+
+   if (unlikely(sctx->thread_trace_enabled && sctx->chip_class >= GFX9)) {
+      radeon_emit(&sctx->gfx_cs, PKT3(PKT3_EVENT_WRITE, 0, 0));
+      radeon_emit(&sctx->gfx_cs, EVENT_TYPE(V_028A90_THREAD_TRACE_MARKER) | EVENT_INDEX(0));
+   }
 }
 
 static void si_launch_grid(struct pipe_context *ctx, const struct pipe_grid_info *info)

src/gallium/drivers/radeonsi/si_gfx_cs.c
@@ -215,6 +215,11 @@ void si_flush_gfx_cs(struct si_context *ctx, unsigned flags, struct pipe_fence_h
       si_check_vm_faults(ctx, &ctx->current_saved_cs->gfx, RING_GFX);
    }
 
+   if (unlikely(ctx->thread_trace &&
+                (flags & PIPE_FLUSH_END_OF_FRAME))) {
+      si_handle_thread_trace(ctx, &ctx->gfx_cs);
+   }
+
    if (ctx->current_saved_cs)
       si_saved_cs_reference(&ctx->current_saved_cs, NULL);
 

src/gallium/drivers/radeonsi/si_pipe.c
@@ -113,6 +113,7 @@ static const struct debug_named_value radeonsi_debug_options[] = {
    {"nofmask", DBG(NO_FMASK), "Disable MSAA compression"},
 
    {"tmz", DBG(TMZ), "Force allocation of scanout/depth/stencil buffer as encrypted"},
+   {"sqtt", DBG(SQTT), "Enable SQTT"},
 
    DEBUG_NAMED_VALUE_END /* must be last */
 };
@@ -185,6 +186,9 @@ static void si_destroy_context(struct pipe_context *context)
 
    if (sctx->chip_class >= GFX10 && sctx->has_graphics)
       gfx10_destroy_query(sctx);
+
+   if (sctx->thread_trace)
+      si_destroy_thread_trace(sctx);
 
    pipe_resource_reference(&sctx->esgs_ring, NULL);
    pipe_resource_reference(&sctx->gsvs_ring, NULL);
@@ -733,6 +737,13 @@ static struct pipe_context *si_pipe_create_context(struct pipe_screen *screen, v
       flags |= PIPE_CONTEXT_DEBUG;
 
    ctx = si_create_context(screen, flags);
+
+   if (ctx && sscreen->info.chip_class >= GFX9 && sscreen->debug_flags & DBG(SQTT)) {
+      if (!si_init_thread_trace((struct si_context *)ctx)) {
+         FREE(ctx);
+         return NULL;
+      }
+   }
 
    if (!(flags & PIPE_CONTEXT_PREFER_THREADED))
       return ctx;

src/gallium/drivers/radeonsi/si_state_draw.cpp
@@ -23,6 +23,7 @@
  */
 
 #include "ac_debug.h"
+#include "ac_sqtt.h"
 #include "si_build_pm4.h"
 #include "sid.h"
 #include "util/u_index_modify.h"
@@ -815,6 +816,15 @@ static void si_emit_draw_registers(struct si_context *sctx, const struct pipe_dr
    }
 }
 
+#define EMIT_SQTT_END_DRAW do {                                          \
+      if (GFX_VERSION >= GFX9 && unlikely(sctx->thread_trace_enabled)) { \
+         radeon_emit(&sctx->gfx_cs, PKT3(PKT3_EVENT_WRITE, 0, 0));       \
+         radeon_emit(&sctx->gfx_cs,                                      \
+                     EVENT_TYPE(V_028A90_THREAD_TRACE_MARKER) |          \
+                     EVENT_INDEX(0));                                    \
+      }                                                                  \
+   } while (0)
+
 template <chip_class GFX_VERSION, si_has_ngg NGG, si_has_prim_discard_cs ALLOW_PRIM_DISCARD_CS>
 static void si_emit_draw_packets(struct si_context *sctx, const struct pipe_draw_info *info,
                                  const struct pipe_draw_indirect_info *indirect,
@@ -830,6 +840,11 @@ static void si_emit_draw_packets(struct si_context *sctx, const struct pipe_draw
    uint32_t index_max_size = 0;
    uint32_t use_opaque = 0;
    uint64_t index_va = 0;
+
+   if (unlikely(sctx->thread_trace_enabled)) {
+      si_sqtt_write_event_marker(sctx, &sctx->gfx_cs, EventCmdDraw,
+                                 UINT_MAX, UINT_MAX, UINT_MAX);
+   }
 
    if (indirect && indirect->count_from_stream_output) {
       struct si_streamout_target *t = (struct si_streamout_target *)indirect->count_from_stream_output;
@@ -1031,6 +1046,7 @@ static void si_emit_draw_packets(struct si_context *sctx, const struct pipe_draw
                                                     original_index_size, base_vertex,
                                                     va, MIN2(index_max_size, draws[i].count));
             }
+            EMIT_SQTT_END_DRAW;
             return;
          }
 
@@ -1088,6 +1104,7 @@ static void si_emit_draw_packets(struct si_context *sctx, const struct pipe_draw
                radeon_emit(cs, draws[i].count);
                radeon_emit(cs, V_0287F0_DI_SRC_SEL_AUTO_INDEX);
             }
+            EMIT_SQTT_END_DRAW;
             return;
          }
 
@@ -1114,6 +1131,8 @@ static void si_emit_draw_packets(struct si_context *sctx, const struct pipe_draw
             sctx->last_base_vertex = draws[num_draws - 1].start;
       }
    }
+
+   EMIT_SQTT_END_DRAW;
 }
 
 extern "C"

upcliujie/mesa

commit f1e54d8191bb1ba58424e32564fc543aa9810344

radeonsi: inhibit clockgating when using SQTT
Ported from PAL.

Fixes: 07c1504d1b0 ("radeonsi: implement SQTT support")

src/gallium/drivers/radeonsi/si_perfcounter.c
@@ -919,14 +919,14 @@ static void si_pc_query_destroy(struct si_context *sctx, struct si_query *squery
    FREE(query);
 }
 
-static void si_inhibit_clockgating(struct si_context *sctx, bool inhibit)
+void si_inhibit_clockgating(struct si_context *sctx, struct radeon_cmdbuf *cs, bool inhibit)
 {
    if (sctx->chip_class >= GFX10) {
-      radeon_set_uconfig_reg(&sctx->gfx_cs, R_037390_RLC_PERFMON_CLK_CNTL,
-                            S_037390_PERFMON_CLOCK_STATE(inhibit));
+      radeon_set_uconfig_reg(cs, R_037390_RLC_PERFMON_CLK_CNTL,
+                             S_037390_PERFMON_CLOCK_STATE(inhibit));
    } else if (sctx->chip_class >= GFX8) {
-      radeon_set_uconfig_reg(&sctx->gfx_cs, R_0372FC_RLC_PERFMON_CLK_CNTL,
-                            S_0372FC_PERFMON_CLOCK_STATE(inhibit));
+      radeon_set_uconfig_reg(cs, R_0372FC_RLC_PERFMON_CLK_CNTL,
+                             S_0372FC_PERFMON_CLOCK_STATE(inhibit));
    }
 }
 
@@ -946,7 +946,7 @@ static void si_pc_query_resume(struct si_context *sctx, struct si_query *squery)
    if (query->shaders)
       si_pc_emit_shaders(sctx, query->shaders);
 
-   si_inhibit_clockgating(sctx, true);
+   si_inhibit_clockgating(sctx, &sctx->gfx_cs, true);
 
    for (struct si_query_group *group = query->groups; group; group = group->next) {
       struct si_pc_block *block = group->block;
@@ -1000,7 +1000,7 @@ static void si_pc_query_suspend(struct si_context *sctx, struct si_query *squery
 
    si_pc_emit_instance(sctx, -1, -1);
 
-   si_inhibit_clockgating(sctx, false);
+   si_inhibit_clockgating(sctx, &sctx->gfx_cs, false);
 }
 
 static bool si_pc_query_begin(struct si_context *ctx, struct si_query *squery)

src/gallium/drivers/radeonsi/si_pipe.h
@@ -1491,6 +1491,7 @@ void si_init_compiler(struct si_screen *sscreen, struct ac_llvm_compiler *compil
 /* si_perfcounters.c */
 void si_init_perfcounters(struct si_screen *screen);
 void si_destroy_perfcounters(struct si_screen *screen);
+void si_inhibit_clockgating(struct si_context *sctx, struct radeon_cmdbuf *cs, bool inhibit);
 
 /* si_query.c */
 void si_init_screen_query_functions(struct si_screen *sscreen);

src/gallium/drivers/radeonsi/si_sqtt.c
@@ -378,6 +378,8 @@ si_thread_trace_start(struct si_context *sctx, int family, struct radeon_cmdbuf 
       SI_CONTEXT_INV_L2;
    sctx->emit_cache_flush(sctx, cs);
 
+   si_inhibit_clockgating(sctx, cs, true);
+
    /* Enable SQG events that collects thread trace data. */
    si_emit_spi_config_cntl(sctx, cs, true);
 
@@ -418,6 +420,8 @@ si_thread_trace_stop(struct si_context *sctx, int family, struct radeon_cmdbuf *
 
    /* Restore previous state by disabling SQG events. */
    si_emit_spi_config_cntl(sctx, cs, false);
+
+   si_inhibit_clockgating(sctx, cs, false);
 }
 
 