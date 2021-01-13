f44d2a252964b224ae1e7efe8b4a105bb7fe637e
Rhys Perry <pendingchaos02@gmail.com>
2021-01-13 15:51:01 +0000

    radv: always consider some outputs as invariant
    Now that we try to create ffma from the beginning and are less reliant on
    inexact optimizations, we can enable this with much less regressions on
    GFX10.3.
    
    fossil-db (GFX10.3):
    Totals from 53274 (36.42% of 146267) affected shaders:
    VGPRs: 1979216 -> 1975944 (-0.17%); split: -0.67%, +0.50%
    SpillSGPRs: 1462 -> 1446 (-1.09%)
    CodeSize: 77389256 -> 79231200 (+2.38%); split: -0.06%, +2.44%
    MaxWaves: 1418040 -> 1419400 (+0.10%); split: +0.34%, -0.24%
    Instrs: 15015014 -> 15453979 (+2.92%); split: -0.14%, +3.06%
    Latency: 89229974 -> 89562172 (+0.37%); split: -0.76%, +1.13%
    InvThroughput: 15488878 -> 15990928 (+3.24%); split: -0.25%, +3.50%
    
    fossil-db (GFX10):
    Totals from 12563 (8.59% of 146267) affected shaders:
    VGPRs: 532880 -> 532200 (-0.13%); split: -0.42%, +0.29%
    SpillSGPRs: 1462 -> 1446 (-1.09%)
    CodeSize: 30125368 -> 30275508 (+0.50%); split: -0.06%, +0.56%
    MaxWaves: 327120 -> 327484 (+0.11%); split: +0.31%, -0.20%
    Instrs: 5783753 -> 5834581 (+0.88%); split: -0.02%, +0.89%
    Latency: 29271625 -> 29349407 (+0.27%); split: -0.14%, +0.41%
    InvThroughput: 6801730 -> 6855695 (+0.79%); split: -0.04%, +0.83%
    
    fossil-db (GFX9):
    Totals from 12563 (8.58% of 146401) affected shaders:
    SGPRs: 717840 -> 717888 (+0.01%); split: -0.16%, +0.16%
    VGPRs: 531464 -> 531892 (+0.08%); split: -0.23%, +0.31%
    SpillSGPRs: 1770 -> 1750 (-1.13%)
    CodeSize: 27658752 -> 27814028 (+0.56%); split: -0.05%, +0.61%
    MaxWaves: 81149 -> 81224 (+0.09%); split: +0.24%, -0.14%
    Instrs: 5489024 -> 5537939 (+0.89%); split: -0.02%, +0.91%
    Latency: 43480815 -> 43657720 (+0.41%); split: -0.08%, +0.49%
    InvThroughput: 19853380 -> 20017416 (+0.83%); split: -0.04%, +0.87%
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/util/driconf.h
+++ src/util/driconf.h
@@ -540,10 +540,6 @@
    DRI_CONF_OPT_B(radv_lower_discard_to_demote, def, \
                   "Lower discard instructions to demote")
 
-#define DRI_CONF_RADV_INVARIANT_GEOM(def) \
-   DRI_CONF_OPT_B(radv_invariant_geom, def, \
-                  "Mark geometry-affecting outputs as invariant")
-
 #define DRI_CONF_RADV_SPLIT_FMA(def) \
    DRI_CONF_OPT_B(radv_split_fma, def, \
                   "Split application-provided fused multiply-add in geometry stages")
