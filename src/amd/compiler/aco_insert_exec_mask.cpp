Grima04/mesa-turnip-kgsl

commit 661922f6ac9576fc2077c5d50b53ef7edf9e8a18

aco: add block to worklist in mark_block_wqm()
Since we're requiring the branch condition to be in WQM, we have to ensure
that the block is in the worklist.

Fixes Trials Fusion hang at 4K and High settings.

fossil-db (Sienna):
Totals from 216 (0.15% of 139391) affected shaders:
SGPRs: 13392 -> 13360 (-0.24%)
CodeSize: 1321184 -> 1318592 (-0.20%)
Instrs: 255310 -> 254662 (-0.25%)
Cycles: 2178360 -> 2174652 (-0.17%)

Affected fossils in fossil-db are dirt4, nier and youngblood.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Gitlab: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3863
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8145>

src/amd/compiler/aco_insert_exec_mask.cpp
@@ -165,6 +165,8 @@ void mark_block_wqm(wqm_ctx &ctx, unsigned block_idx)
       return;
 
    ctx.branch_wqm[block_idx] = true;
+   ctx.worklist.insert(block_idx);
+
    Block& block = ctx.program->blocks[block_idx];
 
    /* TODO: this sets more branch conditions to WQM than it needs to

upcliujie/mesa

commit 1caf4ced1c5be9d7495317a2105d9224439eac5f

aco: remove loop to flag loop blocks as WQM
This should no longer be necessary since the mark_block_wqm() we use to
flag break conditions as WQM now adds block to the worklist. With them
added to the worklist, get_block_needs() will add WQM to block_needs.

Adding WQM to block_needs here without adding the block to the worklist
(like we do here) can cause issues because it does not ensure that the
predecessors' branches are in WQM (needed for it to be possible to
transition to WQM in the block). This happened in an Overwatch shader.

No fossil-db changes.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Fixes: 661922f6ac9 ("aco: add block to worklist in mark_block_wqm()")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4066

src/amd/compiler/aco_insert_exec_mask.cpp
@@ -54,14 +54,10 @@ struct wqm_ctx {
    std::vector<uint16_t> defined_in;
    std::vector<bool> needs_wqm;
    std::vector<bool> branch_wqm; /* true if the branch condition in this block should be in wqm */
-   bool loop;
-   bool wqm;
    wqm_ctx(Program* program_) : program(program_),
                                defined_in(program->peekAllocationId(), 0xFFFF),
                                needs_wqm(program->peekAllocationId()),
-                               branch_wqm(program->blocks.size()),
-                               loop(false),
-                               wqm(false)
+                               branch_wqm(program->blocks.size())
    {
       for (unsigned i = 0; i < program->blocks.size(); i++)
          worklist.insert(i);
@@ -149,24 +145,6 @@ void get_block_needs(wqm_ctx &ctx, exec_ctx &exec_ctx, Block* block)
 
    std::vector<WQMState> instr_needs(block->instructions.size());
 
-   if (block->kind & block_kind_top_level) {
-      if (ctx.loop && ctx.wqm) {
-         unsigned block_idx = block->index + 1;
-         while (!(ctx.program->blocks[block_idx].kind & block_kind_top_level)) {
-            /* flag all break conditions as WQM:
-             * the conditions might be computed outside the nested CF */
-            if (ctx.program->blocks[block_idx].kind & block_kind_break)
-               mark_block_wqm(ctx, block_idx);
-            /* flag all blocks as WQM to ensure we enter all (nested) loops in WQM */
-            exec_ctx.info[block_idx].block_needs |= WQM;
-            block_idx++;
-         }
-      }
-
-      ctx.loop = false;
-      ctx.wqm = false;
-   }
-
    for (int i = block->instructions.size() - 1; i >= 0; --i) {
       aco_ptr<Instruction>& instr = block->instructions[i];
 
@@ -226,10 +204,7 @@ void get_block_needs(wqm_ctx &ctx, exec_ctx &exec_ctx, Block* block)
    if (info.block_needs & WQM && !(block->kind & block_kind_top_level)) {
       for (unsigned pred_idx : block->logical_preds)
          mark_block_wqm(ctx, pred_idx);
-      ctx.wqm = true;
    }
-   if (block->kind & block_kind_loop_header)
-      ctx.loop = true;
 }
 
 void calculate_wqm_needs(exec_ctx& exec_ctx)