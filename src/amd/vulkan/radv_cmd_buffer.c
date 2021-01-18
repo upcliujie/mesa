4cc580e51e640c78619ec99c38cbde00a3ed8020
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-01-18 11:19:20 +0100

    radv: flush L2 for images affected by the pipe misaligned issue on GFX10+
    In some rare cases, L2 needs to be flushed if an image is affected
    by the pipe misaligned issue. This is roughly based on AMDVLK.
    
    I confirmed that disabling TC-compat HTILE, and respectively DCC,
    for the relevant images also fixes the regressions below.
    
    This fixes some regressions introduced with L2 coherency for
    dEQP-VK.renderpass2.depth_stencil_resolve.image_2d_* and for
    dEQP-VK.renderpass2.suballocation.multisample_resolve.*.
    
    Fixes: 4a783a3c784 ("radv: Use L2 coherency on GFX9+.")
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -3267,10 +3267,65 @@ static void radv_stage_flush(struct radv_cmd_buffer *cmd_buffer,
 	}
 }
 
+/* Determine if the image is affected by the pipe misaligned metadata issue
+ * which requires to invalidate L2.
+ */
+static bool
+radv_image_is_pipe_misaligned(const struct radv_device *device,
+			      const struct radv_image *image)
+{
+	struct radeon_info *rad_info = &device->physical_device->rad_info;
+	bool is_non_pow2_vram = !util_is_power_of_two_or_zero(rad_info->vram_bit_width);
+	unsigned log2_samples = util_logbase2(image->info.samples);
+	unsigned log2_bpp = util_logbase2(vk_format_get_blocksize(image->vk_format));
+	unsigned log2_bpp_and_samples;
+
+	assert(rad_info->chip_class >= GFX10);
+
+	if (rad_info->chip_class >= GFX10_3) {
+		log2_bpp_and_samples = log2_bpp + log2_samples;
+	} else {
+		if (vk_format_is_depth(image->vk_format) &&
+		     image->info.array_size >= 8) {
+			log2_bpp = 2;
+		}
+
+		log2_bpp_and_samples = MIN2(6, log2_bpp + log2_samples);
+	}
+
+	unsigned num_pipes = G_0098F8_NUM_PIPES(rad_info->gb_addr_config);
+	int overlap = MAX2(0, log2_bpp_and_samples + num_pipes - 8);
+
+	if (vk_format_is_depth(image->vk_format)) {
+		if (radv_image_is_tc_compat_htile(image) &&
+		    (is_non_pow2_vram || overlap)) {
+			return true;
+		}
+	} else {
+		unsigned max_compressed_frags = G_0098F8_MAX_COMPRESSED_FRAGS(rad_info->gb_addr_config);
+		int log2_samples_frag_diff = MAX2(0, log2_samples - max_compressed_frags);
+		int samples_overlap = MIN2(log2_samples, overlap);
+
+		/* TODO: It shouldn't be necessary if the image has DCC but
+		 * not readable by shader.
+		 */
+		if ((radv_image_has_dcc(image) ||
+		     radv_image_is_tc_compat_cmask(image)) &&
+		    (is_non_pow2_vram ||
+		     (samples_overlap > log2_samples_frag_diff))) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
 static bool
 radv_image_is_l2_coherent(const struct radv_device *device, const struct radv_image *image)
 {
 	if (device->physical_device->rad_info.chip_class >= GFX10) {
+		if (image && radv_image_is_pipe_misaligned(device, image))
+			return false;
 		return !device->physical_device->rad_info.tcc_harvested;
 	} else if (device->physical_device->rad_info.chip_class == GFX9 && image) {
 		if (image->info.samples == 1 &&
