bf7174ba88eed7bde9f1587e717500fbf333d2a3
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-01-18 14:15:43 +0100

    radv: allow more fast clears for depth surfaces without TC-compat HTILE
    With HTILE only, all values between 0.0 and 1.0 are fetchable.
    This should allow more fast clears for depth surfaces where
    TC-compat HTILE is disabled.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_meta_clear.c
+++ src/amd/vulkan/radv_meta_clear.c
@@ -1022,10 +1022,11 @@ radv_can_fast_clear_depth(struct radv_cmd_buffer *cmd_buffer,
 	if (!view_mask && clear_rect->layerCount != iview->image->info.array_size)
 		return false;
 
-	if (((aspects & VK_IMAGE_ASPECT_DEPTH_BIT) &&
-	    !radv_is_fast_clear_depth_allowed(clear_value)) ||
-	    ((aspects & VK_IMAGE_ASPECT_STENCIL_BIT) &&
-	     !radv_is_fast_clear_stencil_allowed(clear_value)))
+	if (radv_image_is_tc_compat_htile(iview->image) &&
+	    (((aspects & VK_IMAGE_ASPECT_DEPTH_BIT) &&
+	     !radv_is_fast_clear_depth_allowed(clear_value)) ||
+	     ((aspects & VK_IMAGE_ASPECT_STENCIL_BIT) &&
+	      !radv_is_fast_clear_stencil_allowed(clear_value))))
 		return false;
 
 	return true;
