c70ccbcb7ed715ffb0fe8dfc9e1e9326fc8a9569
Rhys Perry <pendingchaos02@gmail.com>
2021-01-20 16:05:04 +0000

    Revert "radv,aco: don't use MUBUF for multi-channel loads on GFX8 with robustness2"
    This reverts commit a8a6b9fb2fdcb1bea55707fa0c2b8e96f03c6b5b.
    
    This is no longer necessary now that we fixup the size when creating the
    descriptors.

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -2679,7 +2679,6 @@ radv_CreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo *pCr
 
    bool keep_shader_info = false;
    bool robust_buffer_access = false;
-   bool robust_buffer_access2 = false;
    bool overallocation_disallowed = false;
    bool custom_border_colors = false;
    bool vrs_enabled = false;
@@ -2726,12 +2725,6 @@ radv_CreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo *pCr
                        vrs->attachmentFragmentShadingRate;
          break;
       }
-      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_ROBUSTNESS_2_FEATURES_EXT: {
-         const VkPhysicalDeviceRobustness2FeaturesEXT *features = (const void *)ext;
-         if (features->robustBufferAccess2)
-            robust_buffer_access2 = true;
-         break;
-      }
       default:
          break;
       }
@@ -2773,8 +2766,7 @@ radv_CreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo *pCr
                                 device->vk.enabled_extensions.EXT_buffer_device_address ||
                                 device->vk.enabled_extensions.KHR_buffer_device_address;
 
-   device->robust_buffer_access = robust_buffer_access || robust_buffer_access2;
-   device->robust_buffer_access2 = robust_buffer_access2;
+   device->robust_buffer_access = robust_buffer_access;
 
    device->adjust_frag_coord_z =
       (vrs_enabled || device->vk.enabled_extensions.KHR_fragment_shading_rate ||
