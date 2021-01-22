02d10f97ca64da2d0b7031234efa62d19e228452
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-01-22 19:55:41 +0200

    turnip/ir3: implement VK_KHR_buffer_device_address
    We don't advertise bufferDeviceAddressCaptureReplay capability and
    neither does blob, because at the moment there is no way to allocate
    bo with predefined iova.
    
    There is no support of any arithmetic with addresses since shaderInt64
    is not enabled. However, NIR produces 64bit arithmetic to access
    struct members and array elements in referenced memory, thus we still
    have to use 64bit math internally.
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/freedreno/ir3/ir3_a4xx.c
+++ src/freedreno/ir3/ir3_a4xx.c
@@ -329,6 +329,12 @@ emit_intrinsic_atomic_image(struct ir3_context *ctx, nir_intrinsic_instr *intr)
    return atomic;
 }
 
+static struct ir3_instruction *
+emit_intrinsic_atomic_global(struct ir3_context *ctx, nir_intrinsic_instr *intr)
+{
+   unreachable("Global atomic are unsupported on A5xx");
+}
+
 const struct ir3_context_funcs ir3_a4xx_funcs = {
    .emit_intrinsic_load_ssbo = emit_intrinsic_load_ssbo,
    .emit_intrinsic_store_ssbo = emit_intrinsic_store_ssbo,
@@ -338,4 +344,5 @@ const struct ir3_context_funcs ir3_a4xx_funcs = {
    .emit_intrinsic_image_size = emit_intrinsic_image_size_tex,
    .emit_intrinsic_load_global_ir3 = NULL,
    .emit_intrinsic_store_global_ir3 = NULL,
+   .emit_intrinsic_atomic_global = emit_intrinsic_atomic_global,
 };
