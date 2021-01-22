Grima04/mesa-turnip-kgsl

commit 0c70268ff73adfc9d588fa5a10c500ed9f66bab6

zink: mark ARB_sample_shading as supported
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7192>

src/gallium/drivers/zink/zink_screen.c
@@ -121,6 +121,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return 1;
 
    case PIPE_CAP_TEXTURE_MULTISAMPLE:
+   case PIPE_CAP_SAMPLE_SHADING:
       return 1;
 
    case PIPE_CAP_TEXTURE_SWIZZLE:

upcliujie/mesa

commit 7221fab7faf22ef321065d50c820d8dd68ec7577

zink: respect feature-cap for sample-shading
Vulkan has a cap to enable this, we should check that one rather than
always claiming support.

Fixes: 0c70268ff73 ("zink: mark ARB_sample_shading as supported")

src/gallium/drivers/zink/zink_screen.c
@@ -153,8 +153,10 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return screen->timestamp_valid_bits > 0;
 
    case PIPE_CAP_TEXTURE_MULTISAMPLE:
-   case PIPE_CAP_SAMPLE_SHADING:
       return 1;
+
+   case PIPE_CAP_SAMPLE_SHADING:
+      return screen->info.feats.features.sampleRateShading;
 
    case PIPE_CAP_TEXTURE_SWIZZLE:
       return 1;