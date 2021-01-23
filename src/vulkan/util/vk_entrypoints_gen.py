984ad76da8cce06d010e4ad3f778f47b55032e9d
Jason Ekstrand <jason.ekstrand@intel.com>
2021-01-23 12:46:02 -0600

    vulkan: Add dispatch table lookup helpers
    These both look up a name by string and check to see if it's actually
    supported given instance and possibly device extension tables.

--- src/vulkan/util/vk_entrypoints_gen.py
+++ src/vulkan/util/vk_entrypoints_gen.py
@@ -49,6 +49,8 @@ TEMPLATE_H = Template(COPYRIGHT + """\
 #include "vulkan/vulkan.h"
 #include "vulkan/vulkan_intel.h"
 
+#include "vk_extensions.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -107,6 +109,40 @@ vk_device_dispatch_table_load(struct vk_device_dispatch_table *table,
                               PFN_vkGetDeviceProcAddr gpa,
                               VkDevice device);
 
+PFN_vkVoidFunction
+vk_instance_dispatch_table_get(const struct vk_instance_dispatch_table *table,
+                               const char *name);
+
+PFN_vkVoidFunction
+vk_physical_device_dispatch_table_get(const struct vk_physical_device_dispatch_table *table,
+                                      const char *name);
+
+PFN_vkVoidFunction
+vk_device_dispatch_table_get(const struct vk_device_dispatch_table *table,
+                             const char *name);
+
+PFN_vkVoidFunction
+vk_instance_dispatch_table_get_if_supported(
+    const struct vk_instance_dispatch_table *table,
+    const char *name,
+    uint32_t core_version,
+    const struct vk_instance_extension_table *instance_exts);
+
+PFN_vkVoidFunction
+vk_physical_device_dispatch_table_get_if_supported(
+    const struct vk_physical_device_dispatch_table *table,
+    const char *name,
+    uint32_t core_version,
+    const struct vk_instance_extension_table *instance_exts);
+
+PFN_vkVoidFunction
+vk_device_dispatch_table_get_if_supported(
+    const struct vk_device_dispatch_table *table,
+    const char *name,
+    uint32_t core_version,
+    const struct vk_instance_extension_table *instance_exts,
+    const struct vk_device_extension_table *device_exts);
+
 #ifdef __cplusplus
 }
 #endif
@@ -118,6 +154,9 @@ TEMPLATE_C = Template(COPYRIGHT + """\
 /* This file generated from ${filename}, don't edit directly. */
 
 #include "vk_entrypoints.h"
+
+#include "util/macros.h"
+#include "string.h"
 
 <%def name="load_dispatch_table(type, VkType, ProcAddr, entrypoints)">
 void
@@ -157,7 +196,321 @@ ${load_dispatch_table('physical_device', 'VkInstance', 'GetInstanceProcAddr',
 ${load_dispatch_table('device', 'VkDevice', 'GetDeviceProcAddr',
                       device_entrypoints)}
 
+
+struct string_map_entry {
+   uint32_t name;
+   uint32_t hash;
+   uint32_t num;
+};
+
+/* We use a big string constant to avoid lots of reloctions from the entry
+ * point table to lots of little strings. The entries in the entry point table
+ * store the index into this big string.
+ */
+
+<%def name="strmap(strmap, prefix)">
+static const char ${prefix}_strings[] =
+% for s in strmap.sorted_strings:
+    "${s.string}\\0"
+% endfor
+;
+
+static const struct string_map_entry ${prefix}_string_map_entries[] = {
+% for s in strmap.sorted_strings:
+    { ${s.offset}, ${'{:0=#8x}'.format(s.hash)}, ${s.num} }, /* ${s.string} */
+% endfor
+};
+
+/* Hash table stats:
+ * size ${len(strmap.sorted_strings)} entries
+ * collisions entries:
+% for i in range(10):
+ *     ${i}${'+' if i == 9 else ' '}     ${strmap.collisions[i]}
+% endfor
+ */
+
+#define none 0xffff
+static const uint16_t ${prefix}_string_map[${strmap.hash_size}] = {
+% for e in strmap.mapping:
+    ${ '{:0=#6x}'.format(e) if e >= 0 else 'none' },
+% endfor
+};
+
+static int
+${prefix}_string_map_lookup(const char *str)
+{
+    static const uint32_t prime_factor = ${strmap.prime_factor};
+    static const uint32_t prime_step = ${strmap.prime_step};
+    const struct string_map_entry *e;
+    uint32_t hash, h;
+    uint16_t i;
+    const char *p;
+
+    hash = 0;
+    for (p = str; *p; p++)
+        hash = hash * prime_factor + *p;
+
+    h = hash;
+    while (1) {
+        i = ${prefix}_string_map[h & ${strmap.hash_mask}];
+        if (i == none)
+           return -1;
+        e = &${prefix}_string_map_entries[i];
+        if (e->hash == hash && strcmp(str, ${prefix}_strings + e->name) == 0)
+            return e->num;
+        h += prime_step;
+    }
+
+    return -1;
+}
+</%def>
+
+${strmap(instance_strmap, 'instance')}
+${strmap(physical_device_strmap, 'physical_device')}
+${strmap(device_strmap, 'device')}
+
+static bool
+vk_instance_entrypoint_is_enabled(int index, uint32_t core_version,
+                                  const struct vk_instance_extension_table *instance)
+{
+   switch (index) {
+% for e in instance_entrypoints:
+   case ${e.num}:
+      /* ${e.name} */
+   % if e.core_version:
+      return ${e.core_version.c_vk_version()} <= core_version;
+   % elif e.extensions:
+     % for ext in e.extensions:
+        % if ext.type == 'instance':
+      if (instance->${ext.name[3:]}) return true;
+        % else:
+      /* All device extensions are considered enabled at the instance level */
+      return true;
+        % endif
+     % endfor
+      return false;
+   % else:
+      return true;
+   % endif
+% endfor
+   default:
+      return false;
+   }
+}
+
+/** Return true if the core version or extension in which the given entrypoint
+ * is defined is enabled.
+ *
+ * If device is NULL, all device extensions are considered enabled.
+ */
+static bool
+vk_physical_device_entrypoint_is_enabled(int index, uint32_t core_version,
+                                         const struct vk_instance_extension_table *instance)
+{
+   switch (index) {
+% for e in physical_device_entrypoints:
+   case ${e.num}:
+      /* ${e.name} */
+   % if e.core_version:
+      return ${e.core_version.c_vk_version()} <= core_version;
+   % elif e.extensions:
+     % for ext in e.extensions:
+        % if ext.type == 'instance':
+      if (instance->${ext.name[3:]}) return true;
+        % else:
+      /* All device extensions are considered enabled at the instance level */
+      return true;
+        % endif
+     % endfor
+      return false;
+   % else:
+      return true;
+   % endif
+% endfor
+   default:
+      return false;
+   }
+}
+
+/** Return true if the core version or extension in which the given entrypoint
+ * is defined is enabled.
+ *
+ * If device is NULL, all device extensions are considered enabled.
+ */
+static bool
+vk_device_entrypoint_is_enabled(int index, uint32_t core_version,
+                                const struct vk_instance_extension_table *instance,
+                                const struct vk_device_extension_table *device)
+{
+   switch (index) {
+% for e in device_entrypoints:
+   case ${e.num}:
+      /* ${e.name} */
+   % if e.core_version:
+      return ${e.core_version.c_vk_version()} <= core_version;
+   % elif e.extensions:
+     % for ext in e.extensions:
+        % if ext.type == 'instance':
+      if (instance->${ext.name[3:]}) return true;
+        % else:
+      if (!device || device->${ext.name[3:]}) return true;
+        % endif
+     % endfor
+      return false;
+   % else:
+      return true;
+   % endif
+% endfor
+   default:
+      return false;
+   }
+}
+
+PFN_vkVoidFunction
+vk_instance_dispatch_table_get(
+    const struct vk_instance_dispatch_table *table, const char *name)
+{
+    int map_num = instance_string_map_lookup(name);
+    if (map_num < 0)
+        return NULL;
+
+    return ((PFN_vkVoidFunction *)table)[map_num & 0xffff];
+}
+
+PFN_vkVoidFunction
+vk_physical_device_dispatch_table_get(
+    const struct vk_physical_device_dispatch_table *table, const char *name)
+{
+    int map_num = physical_device_string_map_lookup(name);
+    if (map_num < 0)
+        return NULL;
+
+    return ((PFN_vkVoidFunction *)table)[map_num & 0xffff];
+}
+
+PFN_vkVoidFunction
+vk_device_dispatch_table_get(
+    const struct vk_device_dispatch_table *table, const char *name)
+{
+    int map_num = device_string_map_lookup(name);
+    if (map_num < 0)
+        return NULL;
+
+    return ((PFN_vkVoidFunction *)table)[map_num & 0xffff];
+}
+
+PFN_vkVoidFunction
+vk_instance_dispatch_table_get_if_supported(
+    const struct vk_instance_dispatch_table *table,
+    const char *name,
+    uint32_t core_version,
+    const struct vk_instance_extension_table *instance_exts)
+{
+    int map_num = instance_string_map_lookup(name);
+    if (map_num < 0)
+        return NULL;
+
+    if (!vk_instance_entrypoint_is_enabled(map_num >> 16, core_version,
+                                           instance_exts))
+        return NULL;
+
+    return ((PFN_vkVoidFunction *)table)[map_num & 0xffff];
+}
+
+PFN_vkVoidFunction
+vk_physical_device_dispatch_table_get_if_supported(
+    const struct vk_physical_device_dispatch_table *table,
+    const char *name,
+    uint32_t core_version,
+    const struct vk_instance_extension_table *instance_exts)
+{
+    int map_num = physical_device_string_map_lookup(name);
+    if (map_num < 0)
+        return NULL;
+
+    if (!vk_physical_device_entrypoint_is_enabled(map_num >> 16, core_version,
+                                                  instance_exts))
+        return NULL;
+
+    return ((PFN_vkVoidFunction *)table)[map_num & 0xffff];
+}
+
+PFN_vkVoidFunction
+vk_device_dispatch_table_get_if_supported(
+    const struct vk_device_dispatch_table *table,
+    const char *name,
+    uint32_t core_version,
+    const struct vk_instance_extension_table *instance_exts,
+    const struct vk_device_extension_table *device_exts)
+{
+    int map_num = device_string_map_lookup(name);
+    if (map_num < 0)
+        return NULL;
+
+    if (!vk_device_entrypoint_is_enabled(map_num >> 16, core_version,
+                                         instance_exts, device_exts))
+        return NULL;
+
+    return ((PFN_vkVoidFunction *)table)[map_num & 0xffff];
+}
 """, output_encoding='utf-8')
+
+U32_MASK = 2**32 - 1
+
+PRIME_FACTOR = 5024183
+PRIME_STEP = 19
+
+class StringIntMapEntry(object):
+    def __init__(self, string, num):
+        self.string = string
+        self.num = num
+
+        # Calculate the same hash value that we will calculate in C.
+        h = 0
+        for c in string:
+            h = ((h * PRIME_FACTOR) + ord(c)) & U32_MASK
+        self.hash = h
+
+        self.offset = None
+
+def round_to_pow2(x):
+    return 2**int(math.ceil(math.log(x, 2)))
+
+class StringIntMap(object):
+    def __init__(self):
+        self.baked = False
+        self.strings = dict()
+
+    def add_string(self, string, num):
+        assert not self.baked
+        assert string not in self.strings
+        assert 0 <= num < 2**31
+        self.strings[string] = StringIntMapEntry(string, num)
+
+    def bake(self):
+        self.sorted_strings = \
+            sorted(self.strings.values(), key=lambda x: x.string)
+        offset = 0
+        for entry in self.sorted_strings:
+            entry.offset = offset
+            offset += len(entry.string) + 1
+
+        # Save off some values that we'll need in C
+        self.hash_size = round_to_pow2(len(self.strings) * 1.25)
+        self.hash_mask = self.hash_size - 1
+        self.prime_factor = PRIME_FACTOR
+        self.prime_step = PRIME_STEP
+
+        self.mapping = [-1] * self.hash_size
+        self.collisions = [0] * 10
+        for idx, s in enumerate(self.sorted_strings):
+            level = 0
+            h = s.hash
+            while self.mapping[h & self.hash_mask] >= 0:
+                h = h + PRIME_STEP
+                level = level + 1
+            self.collisions[min(level, 9)] += 1
+            self.mapping[h & self.hash_mask] = idx
 
 EntrypointParam = namedtuple('EntrypointParam', 'type name decl')
 
@@ -182,6 +535,7 @@ class Entrypoint(EntrypointBase):
         self.params = params
         self.guard = guard
         self.aliases = []
+        self.disp_table_index = None
 
     def is_physical_device_entrypoint(self):
         return self.params[0].type in ('VkPhysicalDevice', )
@@ -217,6 +571,10 @@ class EntrypointAlias(EntrypointBase):
     @property
     def return_type(self):
         return self.alias.return_type
+
+    @property
+    def disp_table_index(self):
+        return self.alias.disp_table_index
 
     def decl_params(self):
         return self.alias.decl_params()
@@ -332,6 +690,39 @@ def main():
         else:
             instance_entrypoints.append(e)
 
+    for i, e in enumerate(e for e in device_entrypoints if not e.alias):
+        e.disp_table_index = i
+
+    device_strmap = StringIntMap()
+    for i, e in enumerate(device_entrypoints):
+        e.num = i
+        assert i < 2**15 and e.disp_table_index < 2**15
+        map_num = (i << 16) | e.disp_table_index
+        device_strmap.add_string("vk" + e.name, map_num)
+    device_strmap.bake()
+
+    for i, e in enumerate(e for e in physical_device_entrypoints if not e.alias):
+        e.disp_table_index = i
+
+    physical_device_strmap = StringIntMap()
+    for i, e in enumerate(physical_device_entrypoints):
+        e.num = i
+        assert i < 2**15 and e.disp_table_index < 2**15
+        map_num = (i << 16) | e.disp_table_index
+        physical_device_strmap.add_string("vk" + e.name, map_num)
+    physical_device_strmap.bake()
+
+    for i, e in enumerate(e for e in instance_entrypoints if not e.alias):
+        e.disp_table_index = i
+
+    instance_strmap = StringIntMap()
+    for i, e in enumerate(instance_entrypoints):
+        e.num = i
+        assert i < 2**15 and e.disp_table_index < 2**15
+        map_num = (i << 16) | e.disp_table_index
+        instance_strmap.add_string("vk" + e.name, map_num)
+    instance_strmap.bake()
+
     # For outputting entrypoints.h we generate a anv_EntryPoint() prototype
     # per entry point.
     try:
@@ -341,10 +732,14 @@ def main():
                                           physical_device_entrypoints=physical_device_entrypoints,
                                           device_entrypoints=device_entrypoints,
                                           filename=os.path.basename(__file__)))
+        if args.out_c:
             with open(args.out_c, 'wb') as f:
                 f.write(TEMPLATE_C.render(instance_entrypoints=instance_entrypoints,
                                           physical_device_entrypoints=physical_device_entrypoints,
                                           device_entrypoints=device_entrypoints,
+                                          instance_strmap=instance_strmap,
+                                          physical_device_strmap=physical_device_strmap,
+                                          device_strmap=device_strmap,
                                           filename=os.path.basename(__file__)))
     except Exception:
         # In the event there's an error, this imports some helpers from mako
