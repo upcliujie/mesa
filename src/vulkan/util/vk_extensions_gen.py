ebfec2e6d8f001cb3121d6ffa071d235dac8e958
Jason Ekstrand <jason.ekstrand@intel.com>
2021-01-23 05:12:43 -0600

    vulkan: Add common extension tables
    Unlike the per-driver tables, these contain every extension ever and
    assume the maximum extension version.  This later assumption is ok
    because Vulkan extension versions have only been used for something
    interesting once in the history of the API.  If it happens again, we can
    afford a special case.

--- src/vulkan/util/vk_extensions_gen.py
+++ src/vulkan/util/vk_extensions_gen.py
@@ -35,6 +35,8 @@ _TEMPLATE_H = Template(COPYRIGHT + """
 #ifndef ${driver.upper()}_EXTENSIONS_H
 #define ${driver.upper()}_EXTENSIONS_H
 
+#include <stdbool.h>
+
 %for include in includes:
 #include "${include}"
 %endfor
@@ -74,16 +76,37 @@ struct ${driver}_device_extension_table {
 
 struct ${driver}_physical_device;
 
+%if driver != 'vk':
 void
 ${driver}_physical_device_get_supported_extensions(const struct ${driver}_physical_device *device,
                                              struct ${driver}_device_extension_table *extensions);
+%endif
 
 #endif /* ${driver.upper()}_EXTENSIONS_H */
 """)
 
 _TEMPLATE_C = Template(COPYRIGHT + """
+%if driver == 'vk':
+#include "vk_object.h"
+%else:
 #include "${driver}_private.h"
+%endif
 
+#include "${driver}_extensions.h"
+
+const VkExtensionProperties ${driver}_instance_extensions[${driver.upper()}_INSTANCE_EXTENSION_COUNT] = {
+%for ext in instance_extensions:
+   {"${ext.name}", ${ext.ext_version}},
+%endfor
+};
+
+const VkExtensionProperties ${driver}_device_extensions[${driver.upper()}_DEVICE_EXTENSION_COUNT] = {
+%for ext in device_extensions:
+   {"${ext.name}", ${ext.ext_version}},
+%endfor
+};
+
+%if driver != 'vk':
 #include "vk_util.h"
 
 /* Convert the VK_USE_PLATFORM_* defines to booleans */
@@ -119,12 +142,6 @@ VkResult ${driver}_EnumerateInstanceVersion(
     return VK_SUCCESS;
 }
 
-const VkExtensionProperties ${driver}_instance_extensions[${driver.upper()}_INSTANCE_EXTENSION_COUNT] = {
-%for ext in instance_extensions:
-   {"${ext.name}", ${ext.ext_version}},
-%endfor
-};
-
 const struct ${driver}_instance_extension_table ${driver}_instance_extensions_supported = {
 %for ext in instance_extensions:
    .${ext.name[3:]} = ${get_extension_condition(ext.name, ext.enable)},
@@ -149,12 +166,6 @@ ${driver}_physical_device_api_version(struct ${driver}_physical_device *device)
     return version;
 }
 
-const VkExtensionProperties ${driver}_device_extensions[${driver.upper()}_DEVICE_EXTENSION_COUNT] = {
-%for ext in device_extensions:
-   {"${ext.name}", ${ext.ext_version}},
-%endfor
-};
-
 void
 ${driver}_physical_device_get_supported_extensions(const struct ${driver}_physical_device *device,
                                                    struct ${driver}_device_extension_table *extensions)
@@ -165,6 +176,7 @@ ${driver}_physical_device_get_supported_extensions(const struct ${driver}_physic
 %endfor
    };
 }
+%endif
 """)
 
 def gen_extensions(driver, xml_files, api_versions, max_api_version, extensions, out_c, out_h, includes = []):
@@ -193,3 +205,22 @@ def gen_extensions(driver, xml_files, api_versions, max_api_version, extensions,
     if out_c:
         with open(out_c, 'w') as f:
             f.write(_TEMPLATE_C.render(**template_env))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--out-c', help='Output C file.')
+    parser.add_argument('--out-h', help='Output H file.')
+    parser.add_argument('--xml',
+                        help='Vulkan API XML file.',
+                        required=True,
+                        action='append',
+                        dest='xml_files')
+    args = parser.parse_args()
+
+    extensions = []
+    for filename in args.xml_files:
+        extensions += get_all_exts_from_xml(filename)
+
+    gen_extensions('vk', args.xml_files, None, None,
+                   extensions, args.out_c, args.out_h, [])
