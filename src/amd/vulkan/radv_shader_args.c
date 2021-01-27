a358bb0a525634552139a21c2dbc2582807e0b36
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-01-27 17:06:24 -0500

    radv: don't emit basevertex and drawid if neither is used

--- src/amd/vulkan/radv_shader_args.c
+++ src/amd/vulkan/radv_shader_args.c
@@ -412,10 +412,12 @@ set_vs_specific_input_locs(struct radv_shader_args *args,
 					   user_sgpr_idx);
 		}
 
-		unsigned vs_num = 2;
+		unsigned vs_num = 1;
 		if (args->shader_info->vs.needs_draw_id)
 			vs_num++;
 
+		if (args->shader_info->needs_base_instance)
+			vs_num++;
 		set_loc_shader(args, AC_UD_VS_BASE_VERTEX_START_INSTANCE,
 			       user_sgpr_idx, vs_num);
 	}
