32a482648d51119bac3fc24af07d30df59192914
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2021-01-27 15:17:28 +0100

    CI: windows: split the layers to meet new registry requirements
    The new registry caching in place for registry.fd.o can not handle layers
    bigger than 5 GB. The last layer we used to build on windows was 5.2 GB,
    meaning that the upload would fail.
    
    Split the layers by calling multiple `RUN`, hoping that the size will be
    roughly split between those steps if we have a special layer for VS2019.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- .gitlab-ci/windows/mesa_deps.ps1
+++ .gitlab-ci/windows/mesa_deps.ps1
@@ -43,30 +43,17 @@ if (!$?) {
 # downloads so must be done after Chocolatey use
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
-# VS16.x is 2019
-$msvc_2019_url = 'https://aka.ms/vs/16/release/vs_buildtools.exe'
-
-Get-Date
-Write-Host "Downloading Visual Studio 2019 build tools"
-Invoke-WebRequest -Uri $msvc_2019_url -OutFile C:\vs_buildtools.exe
-
-Get-Date
-Write-Host "Installing Visual Studio 2019"
-Start-Process -NoNewWindow -Wait C:\vs_buildtools.exe -ArgumentList '--wait --quiet --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATL --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Graphics.Tools --add Microsoft.VisualStudio.Component.Windows10SDK.18362 --includeRecommended'
-if (!$?) {
-  Write-Host "Failed to install Visual Studio tools"
-  Exit 1
-}
-Remove-Item C:\vs_buildtools.exe -Force
-
 Get-Date
 Write-Host "Cloning LLVM master"
-git clone -b master --depth=1 https://github.com/llvm/llvm-project llvm-project
+git clone -b release/12.x --depth=1 https://github.com/llvm/llvm-project llvm-project
 if (!$?) {
   Write-Host "Failed to clone LLVM repository"
   Exit 1
 }
 
+# ideally we need to use a tag here instead of master
+# but as of today, SPIRV-LLVM-Translator doesn't have
+# a tag matching LLVM 12.0.0, which is not yet out TBH
 Get-Date
 Write-Host "Cloning SPIRV-LLVM-Translator"
 git clone -b master https://github.com/KhronosGroup/SPIRV-LLVM-Translator llvm-project/llvm/projects/SPIRV-LLVM-Translator
