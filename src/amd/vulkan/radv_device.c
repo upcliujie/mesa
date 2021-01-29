Grima04/mesa-turnip-kgsl

commit 9b1138e3f0e960119a46dc08794132719c93173e

radv: implement VK_EXT_private_data
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4886>

src/amd/vulkan/radv_device.c
@@ -1274,6 +1274,12 @@ void radv_GetPhysicalDeviceFeatures2(
 			features->nullDescriptor = true;
 			break;
 		}
+		case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PRIVATE_DATA_FEATURES_EXT: {
+			VkPhysicalDevicePrivateDataFeaturesEXT *features =
+				(VkPhysicalDevicePrivateDataFeaturesEXT *)ext;
+			features->privateData = true;
+			break;
+		}
 		default:
 			break;
 		}
@@ -7777,4 +7783,49 @@ void radv_GetPhysicalDeviceMultisamplePropertiesEXT(
 	} else {
 		pMultisampleProperties->maxSampleLocationGridSize = (VkExtent2D){ 0, 0 };
 	}
+}
+
+VkResult radv_CreatePrivateDataSlotEXT(
+    VkDevice                                    _device,
+    const VkPrivateDataSlotCreateInfoEXT*       pCreateInfo,
+    const VkAllocationCallbacks*                pAllocator,
+    VkPrivateDataSlotEXT*                       pPrivateDataSlot)
+{
+	RADV_FROM_HANDLE(radv_device, device, _device);
+	return vk_private_data_slot_create(&device->vk, pCreateInfo, pAllocator,
+					   pPrivateDataSlot);
+}
+
+void radv_DestroyPrivateDataSlotEXT(
+    VkDevice                                    _device,
+    VkPrivateDataSlotEXT                        privateDataSlot,
+    const VkAllocationCallbacks*                pAllocator)
+{
+	RADV_FROM_HANDLE(radv_device, device, _device);
+	vk_private_data_slot_destroy(&device->vk, privateDataSlot, pAllocator);
+}
+
+VkResult radv_SetPrivateDataEXT(
+    VkDevice                                    _device,
+    VkObjectType                                objectType,
+    uint64_t                                    objectHandle,
+    VkPrivateDataSlotEXT                        privateDataSlot,
+    uint64_t                                    data)
+{
+	RADV_FROM_HANDLE(radv_device, device, _device);
+	return vk_object_base_set_private_data(&device->vk, objectType,
+					       objectHandle, privateDataSlot,
+					       data);
+}
+
+void radv_GetPrivateDataEXT(
+    VkDevice                                    _device,
+    VkObjectType                                objectType,
+    uint64_t                                    objectHandle,
+    VkPrivateDataSlotEXT                        privateDataSlot,
+    uint64_t*                                   pData)
+{
+	RADV_FROM_HANDLE(radv_device, device, _device);
+	vk_object_base_get_private_data(&device->vk, objectType, objectHandle,
+					privateDataSlot, pData);
 }

src/amd/vulkan/radv_extensions.py
@@ -159,6 +159,7 @@ EXTENSIONS = [
     Extension('VK_EXT_pci_bus_info',                      2, True),
     Extension('VK_EXT_pipeline_creation_feedback',        1, True),
     Extension('VK_EXT_post_depth_coverage',               1, 'device->rad_info.chip_class >= GFX10'),
+    Extension('VK_EXT_private_data',                      1, True),
     Extension('VK_EXT_queue_family_foreign',              1, True),
     Extension('VK_EXT_robustness2',                       1, True),
     # Disable sample locations on GFX10 until the CTS failures have been resolved.

upcliujie/mesa

commit ff0b5248875258c933e05a0f4453b3fd0edb46d4

radv: move queue object to a common base object
This is needed to use the new dispatch layer code.

Fixes: 9b1138e3f0e96 "radv: implement VK_EXT_private_data"
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/amd/vulkan/radv_device.c
@@ -2430,12 +2430,13 @@ radv_queue_init(struct radv_device *device, struct radv_queue *queue,
 		VkDeviceQueueCreateFlags flags,
 		const VkDeviceQueueGlobalPriorityCreateInfoEXT *global_priority)
 {
-	queue->_loader_data.loaderMagic = ICD_LOADER_MAGIC;
 	queue->device = device;
 	queue->queue_family_index = queue_family_index;
 	queue->queue_idx = idx;
 	queue->priority = radv_get_queue_global_priority(global_priority);
 	queue->flags = flags;
+
+	vk_object_base_init(&device->vk, &queue->base, VK_OBJECT_TYPE_QUEUE);
 
 	VkResult result = device->ws->ctx_create(device->ws, queue->priority, &queue->hw_ctx);
 	if (result != VK_SUCCESS)
@@ -2457,6 +2458,7 @@ radv_queue_init(struct radv_device *device, struct radv_queue *queue,
 static void
 radv_queue_finish(struct radv_queue *queue)
 {
+	vk_object_base_finish(&queue->base);
 	if (queue->hw_ctx) {
 		if (queue->cond_created) {
 			if (queue->thread_running) {

src/amd/vulkan/radv_private.h
@@ -696,7 +696,7 @@ struct radv_deferred_queue_submission;
 enum ring_type radv_queue_family_to_ring(int f);
 
 struct radv_queue {
-	VK_LOADER_DATA                              _loader_data;
+	struct vk_object_base                       base;
 	struct radv_device *                         device;
 	struct radeon_winsys_ctx                    *hw_ctx;
 	enum radeon_ctx_priority                     priority;