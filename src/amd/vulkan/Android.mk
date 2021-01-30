b176f53692df4e9322cf2b71bf67a99df9dfa5e5
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2021-01-30 22:19:55 +0100

    radv: Remove the format table.
    Kill some code!

--- src/amd/vulkan/Android.mk
+++ src/amd/vulkan/Android.mk
@@ -79,15 +79,11 @@ LOCAL_GENERATED_SOURCES += $(intermediates)/radv_entrypoints.c
 LOCAL_GENERATED_SOURCES += $(intermediates)/radv_entrypoints.h
 LOCAL_GENERATED_SOURCES += $(intermediates)/radv_extensions.c
 LOCAL_GENERATED_SOURCES += $(intermediates)/radv_extensions.h
-LOCAL_GENERATED_SOURCES += $(intermediates)/vk_format_table.c
 
 RADV_ENTRYPOINTS_SCRIPT := $(MESA_TOP)/src/amd/vulkan/radv_entrypoints_gen.py
 RADV_EXTENSIONS_SCRIPT := $(MESA_TOP)/src/amd/vulkan/radv_extensions.py
-VK_FORMAT_TABLE_SCRIPT := $(MESA_TOP)/src/amd/vulkan/vk_format_table.py
-VK_FORMAT_PARSE_SCRIPT := $(MESA_TOP)/src/amd/vulkan/vk_format_parse.py
 
 vulkan_api_xml = $(MESA_TOP)/src/vulkan/registry/vk.xml
-vk_format_layout_csv = $(MESA_TOP)/src/amd/vulkan/vk_format_layout.csv
 
 $(intermediates)/radv_entrypoints.c: $(RADV_ENTRYPOINTS_SCRIPT) \
 					$(RADV_EXTENSIONS_SCRIPT) \
@@ -107,12 +103,6 @@ $(intermediates)/radv_extensions.c: $(RADV_EXTENSIONS_SCRIPT) $(vulkan_api_xml)
 		--out-h $(addsuffix .h,$(basename $@))
 
 $(intermediates)/radv_extensions.h: $(intermediates)/radv_extensions.c
-
-$(intermediates)/vk_format_table.c: $(VK_FORMAT_TABLE_SCRIPT) \
-					$(VK_FORMAT_PARSE_SCRIPT) \
-					$(vk_format_layout_csv)
-	@mkdir -p $(dir $@)
-	$(MESA_PYTHON2) $(VK_FORMAT_TABLE_SCRIPT) $(vk_format_layout_csv) > $@
 
 LOCAL_SHARED_LIBRARIES += $(RADV_SHARED_LIBRARIES)
 
