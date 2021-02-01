078184b8061997b5ec9e6a621520e554ea9718e2
Jason Ekstrand <jason.ekstrand@intel.com>
2021-02-01 16:55:33 -0600

    spirv: Create acceleration structure and shader record variables
    spirv_to_nir now requires NIR variables to be created for everything.
    
    Fixes: 10b3eecd361af "spirv: Don't remove variables used by resource..."
    Reviewed-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/compiler/spirv/vtn_variables.c
+++ src/compiler/spirv/vtn_variables.c
@@ -1828,6 +1828,8 @@ vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
    case vtn_variable_mode_ubo:
    case vtn_variable_mode_ssbo:
    case vtn_variable_mode_push_constant:
+   case vtn_variable_mode_accel_struct:
+   case vtn_variable_mode_shader_record:
       var->var = rzalloc(b->shader, nir_variable);
       var->var->name = ralloc_strdup(var->var, val->name);
 
@@ -1932,11 +1934,6 @@ vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
                              var_decoration_cb, var);
       break;
    }
-
-   case vtn_variable_mode_accel_struct:
-   case vtn_variable_mode_shader_record:
-      /* These don't need actual variables. */
-      break;
 
    case vtn_variable_mode_image:
    case vtn_variable_mode_phys_ssbo:
