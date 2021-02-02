Grima04/mesa-turnip-kgsl

commit 35f59e14f833944bff7df9ab99cf87bfb402c52f

ci: Use GNU time as meson test wrapper
This will hopefully give us more information about why some tests are
intermittently timing out.

Only in build jobs using the x86_build docker image for now, since
those are where we're currently seeing most such timeouts. But may
expand this later if it provides the expected benefits.

v2:
* Add comment about why we test for and use /usr/bin/time explicitly.

Acked-by: Eric Engestrom <eric@engestrom.ch> # v1
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8776>

.gitlab-ci.yml
@@ -277,7 +277,7 @@ x86_build:
   extends:
     - .use-x86_build-base
   variables:
-    MESA_IMAGE_TAG: &x86_build "2021-1-4-directx-headers"
+    MESA_IMAGE_TAG: &x86_build "2021-01-29-time"
 
 .use-x86_build:
   variables:

.gitlab-ci/container/x86_build.sh
@@ -32,6 +32,7 @@ apt-get install -y --no-remove \
       liblua5.3-dev \
       libxml2-dev \
       ocl-icd-opencl-dev \
+      time \
       wine-development \
       wine32-development
 

.gitlab-ci/meson/test-wrapper.sh
@@ -1,0 +1,8 @@
+#!/bin/sh
+
+# Only use GNU time if available, not any shell built-in command
+if test -f /usr/bin/time; then
+    exec /usr/bin/time -v "$@"
+fi
+
+exec "$@"

.gitlab-ci/meson-build.sh
@@ -59,6 +59,6 @@ meson _build --native-file=native.file \
 cd _build
 meson configure
 ninja
-LC_ALL=C.UTF-8 meson test --num-processes ${FDO_CI_CONCURRENT:-4}
+LC_ALL=C.UTF-8 meson test --num-processes ${FDO_CI_CONCURRENT:-4} --wrapper=$PWD/../.gitlab-ci/meson/test-wrapper.sh
 ninja install
 cd ..

upcliujie/mesa

commit be08576067806dbf2688ff39eb0cd2b26864fac8

ci: Run 'time' in the background and propagate signals to test process
Simply exec'ing time didn't produce any output from it when a test
timed out.

Fixes: 35f59e14f833 "ci: Use GNU time as meson test wrapper"

.gitlab-ci.yml
@@ -277,7 +277,7 @@ x86_build:
   extends:
     - .use-x86_build-base
   variables:
-    MESA_IMAGE_TAG: &x86_build "2021-01-29-time"
+    MESA_IMAGE_TAG: &x86_build "2021-02-02-procps"
 
 .use-x86_build:
   variables:

.gitlab-ci/container/x86_build.sh
@@ -32,6 +32,7 @@ apt-get install -y --no-remove \
       liblua5.3-dev \
       libxml2-dev \
       ocl-icd-opencl-dev \
+      procps \
       time \
       wine-development \
       wine32-development

.gitlab-ci/meson/test-wrapper.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
 # Only use GNU time if available, not any shell built-in command
-if test -f /usr/bin/time; then
-    exec /usr/bin/time -v "$@"
+if ! test -f /usr/bin/time; then
+    exec "$@"
 fi
 
-exec "$@"
+
+# If the test times out, meson sends SIGINT & SIGTERM signals to this process.
+# Simply exec'ing "time" would result in no output from that in this case.
+# Instead, we need to run "time" in the background, catch the signals and
+# propagate them to the actual test process.
+
+/usr/bin/time -v "$@" &
+TIMEPID=$!
+TESTPID=$(ps --ppid $TIMEPID -o pid=)
+
+if test "x$TESTPID" != x; then
+    trap 'kill -INT $TESTPID; wait $TIMEPID; exit $?' INT
+    trap 'kill -TERM $TESTPID; wait $TIMEPID; exit $?' TERM
+fi
+
+wait $TIMEPID
+exit $?