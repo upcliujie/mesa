c0ef3a9fd8f5463197e3237d20206867c23c7191
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-02-04 16:11:00 +0100

    zink: pre-populate locations in variables
    These locations should match what we get out of the pre-existing pass,
    and are currently unused. They will be used in a later commit in this
    series, but is left like this because this code is fairly fragile.

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -499,6 +499,7 @@ emit_input(struct ntv_context *ctx, struct nir_variable *var)
 
       default:
          slot = handle_slot(ctx, slot, glsl_count_vec4_slots(var->type, false, false));
+         assert(var->data.driver_location == slot);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
       if (var->data.centroid)
@@ -525,6 +526,7 @@ emit_input(struct ntv_context *ctx, struct nir_variable *var)
 
       default:
          slot = handle_handle_slot(ctx, var, false);
+         assert(var->data.driver_location == slot);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
    }
@@ -583,6 +585,7 @@ emit_output(struct ntv_context *ctx, struct nir_variable *var)
 
       default:
          slot = handle_handle_slot(ctx, var, true);
+         assert(var->data.driver_location == slot);
          spirv_builder_emit_location(&ctx->builder, var_id, slot);
       }
       /* tcs can't do xfb */
