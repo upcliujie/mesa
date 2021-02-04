Grima04/mesa-turnip-kgsl

commit 6ae726b79988addc50c1ed149151a351d0343d9d

freedreno: clamp scissor bounds
We don't have negative scissor coords and maximum dimensions are:
 a3xx - 4096
 a4xx+ - 16384

Fixes piglit tests:
 fbo-viewport
 viewport-clamp

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/30
Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8209>

src/gallium/drivers/freedreno/freedreno_state.c
@@ -331,14 +331,13 @@ fd_set_viewport_states(struct pipe_context *pctx,
 		swap(miny, maxy);
 	}
 
-	debug_assert(miny >= 0);
-	debug_assert(maxy >= 0);
+	const float max_dims = is_a3xx(ctx->screen) ? 4096.f : 16384.f;
 
-	/* Convert to integer and round up the max bounds. */
-	scissor->minx = minx;
-	scissor->miny = miny;
-	scissor->maxx = ceilf(maxx);
-	scissor->maxy = ceilf(maxy);
+	/* Clamp, convert to integer and round up the max bounds. */
+	scissor->minx = CLAMP(minx, 0.f, max_dims);
+	scissor->miny = CLAMP(miny, 0.f, max_dims);
+	scissor->maxx = CLAMP(ceilf(maxx), 0.f, max_dims);
+	scissor->maxy = CLAMP(ceilf(maxy), 0.f, max_dims);
 
 	ctx->dirty |= FD_DIRTY_VIEWPORT;
 }

upcliujie/mesa

commit b58a0472d5f27cabf9bf38bbb63a5dc61bba51c2

freedreno/a2xx: fix scissors clamp bounds
a2xx has maximum viewport dimensions of 4096

Fixes: 6ae726b79988addc50c1ed149151a351d0343d9d ("freedreno: clamp scissor bounds")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

src/gallium/drivers/freedreno/freedreno_state.c
@@ -331,7 +331,7 @@ fd_set_viewport_states(struct pipe_context *pctx,
 		swap(miny, maxy);
 	}
 
-	const float max_dims = is_a3xx(ctx->screen) ? 4096.f : 16384.f;
+	const float max_dims = ctx->screen->gpu_id >= 400 ? 16384.f : 4096.f;
 
 	/* Clamp, convert to integer and round up the max bounds. */
 	scissor->minx = CLAMP(minx, 0.f, max_dims);