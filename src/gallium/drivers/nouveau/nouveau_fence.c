Grima04/mesa-turnip-kgsl

commit 28a781323fba87e6e338cfecb0b6fe25a08f61a4

nouveau: change fence destruction logic on screen destroy
With the drm shim, we can't actually wait for the hardware to do
anything. But why wait for it at all? We just need to make sure to
execute all the work and clean up any resources. Add a helper to do
that.

Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
Reviewed-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8406>

src/gallium/drivers/nouveau/nouveau_fence.c
@@ -109,6 +109,21 @@ nouveau_fence_del(struct nouveau_fence *fence)
 }
 
 void
+nouveau_fence_cleanup(struct nouveau_screen *screen)
+{
+   struct nouveau_fence *fence, *next;
+
+   for (fence = screen->fence.head; fence; fence = next) {
+      next = fence->next;
+      nouveau_fence_trigger_work(fence);
+      nouveau_fence_ref(NULL, &fence);
+   }
+   screen->fence.head = NULL;
+   screen->fence.tail = NULL;
+   nouveau_fence_ref(NULL, &screen->fence.current);
+}
+
+void
 nouveau_fence_update(struct nouveau_screen *screen, bool flushed)
 {
    struct nouveau_fence *fence;

src/gallium/drivers/nouveau/nouveau_fence.h
@@ -33,6 +33,7 @@ void nouveau_fence_emit(struct nouveau_fence *);
 void nouveau_fence_del(struct nouveau_fence *);
 
 bool nouveau_fence_new(struct nouveau_screen *, struct nouveau_fence **);
+void nouveau_fence_cleanup(struct nouveau_screen *);
 bool nouveau_fence_work(struct nouveau_fence *, void (*)(void *), void *);
 void nouveau_fence_update(struct nouveau_screen *, bool flushed);
 void nouveau_fence_next(struct nouveau_screen *);

src/gallium/drivers/nouveau/nv30/nv30_screen.c
@@ -495,17 +495,7 @@ nv30_screen_destroy(struct pipe_screen *pscreen)
    if (!nouveau_drm_screen_unref(&screen->base))
       return;
 
-   if (screen->base.fence.current) {
-      struct nouveau_fence *current = NULL;
-
-      /* nouveau_fence_wait will create a new current fence, so wait on the
-       * _current_ one, and remove both.
-       */
-      nouveau_fence_ref(screen->base.fence.current, &current);
-      nouveau_fence_wait(current, NULL);
-      nouveau_fence_ref(NULL, &current);
-      nouveau_fence_ref(NULL, &screen->base.fence.current);
-   }
+   nouveau_fence_cleanup(&screen->base);
 
    nouveau_bo_ref(NULL, &screen->notify);
 

src/gallium/drivers/nouveau/nv50/nv50_screen.c
@@ -555,17 +555,8 @@ nv50_screen_destroy(struct pipe_screen *pscreen)
    if (!nouveau_drm_screen_unref(&screen->base))
       return;
 
-   if (screen->base.fence.current) {
-      struct nouveau_fence *current = NULL;
+   nouveau_fence_cleanup(&screen->base);
 
-      /* nouveau_fence_wait will create a new current fence, so wait on the
-       * _current_ one, and remove both.
-       */
-      nouveau_fence_ref(screen->base.fence.current, &current);
-      nouveau_fence_wait(current, NULL);
-      nouveau_fence_ref(NULL, &current);
-      nouveau_fence_ref(NULL, &screen->base.fence.current);
-   }
    if (screen->base.pushbuf)
       screen->base.pushbuf->user_priv = NULL;
 

src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -673,17 +673,8 @@ nvc0_screen_destroy(struct pipe_screen *pscreen)
    if (!nouveau_drm_screen_unref(&screen->base))
       return;
 
-   if (screen->base.fence.current) {
-      struct nouveau_fence *current = NULL;
+   nouveau_fence_cleanup(&screen->base);
 
-      /* nouveau_fence_wait will create a new current fence, so wait on the
-       * _current_ one, and remove both.
-       */
-      nouveau_fence_ref(screen->base.fence.current, &current);
-      nouveau_fence_wait(current, NULL);
-      nouveau_fence_ref(NULL, &current);
-      nouveau_fence_ref(NULL, &screen->base.fence.current);
-   }
    if (screen->base.pushbuf)
       screen->base.pushbuf->user_priv = NULL;
 

upcliujie/mesa

commit a85a5a64ab324c242fb8c9fb200a21a66b5af5e9

nouveau: reinstate fencing on screen destroy
As it turns out, the wait is required as the driver expects for
rendering to be quiesced on exit. This can trigger channel failures,
which in turn trigger recovery. This can fail and destroy the whole
system.

Fixes: 28a781323fb ("nouveau: change fence destruction logic on screen destroy")
References: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4223
Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/gallium/drivers/nouveau/nouveau_fence.c
@@ -109,24 +109,6 @@ nouveau_fence_del(struct nouveau_fence *fence)
 }
 
 void
-nouveau_fence_cleanup(struct nouveau_screen *screen)
-{
-   struct nouveau_fence *fence, *next;
-
-   for (fence = screen->fence.head; fence; fence = next) {
-      next = fence->next;
-      nouveau_fence_trigger_work(fence);
-      nouveau_fence_ref(NULL, &fence);
-   }
-   screen->fence.head = NULL;
-   screen->fence.tail = NULL;
-   if (screen->fence.current) {
-      nouveau_fence_trigger_work(screen->fence.current);
-      nouveau_fence_ref(NULL, &screen->fence.current);
-   }
-}
-
-void
 nouveau_fence_update(struct nouveau_screen *screen, bool flushed)
 {
    struct nouveau_fence *fence;

src/gallium/drivers/nouveau/nouveau_fence.h
@@ -33,7 +33,6 @@ void nouveau_fence_emit(struct nouveau_fence *);
 void nouveau_fence_del(struct nouveau_fence *);
 
 bool nouveau_fence_new(struct nouveau_screen *, struct nouveau_fence **);
-void nouveau_fence_cleanup(struct nouveau_screen *);
 bool nouveau_fence_work(struct nouveau_fence *, void (*)(void *), void *);
 void nouveau_fence_update(struct nouveau_screen *, bool flushed);
 void nouveau_fence_next(struct nouveau_screen *);

src/gallium/drivers/nouveau/nv30/nv30_screen.c
@@ -495,7 +495,17 @@ nv30_screen_destroy(struct pipe_screen *pscreen)
    if (!nouveau_drm_screen_unref(&screen->base))
       return;
 
-   nouveau_fence_cleanup(&screen->base);
+   if (screen->base.fence.current) {
+      struct nouveau_fence *current = NULL;
+
+      /* nouveau_fence_wait will create a new current fence, so wait on the
+       * _current_ one, and remove both.
+       */
+      nouveau_fence_ref(screen->base.fence.current, &current);
+      nouveau_fence_wait(current, NULL);
+      nouveau_fence_ref(NULL, &current);
+      nouveau_fence_ref(NULL, &screen->base.fence.current);
+   }
 
    nouveau_bo_ref(NULL, &screen->notify);
 

src/gallium/drivers/nouveau/nv50/nv50_screen.c
@@ -567,8 +567,17 @@ nv50_screen_destroy(struct pipe_screen *pscreen)
    if (!nouveau_drm_screen_unref(&screen->base))
       return;
 
-   nouveau_fence_cleanup(&screen->base);
+   if (screen->base.fence.current) {
+      struct nouveau_fence *current = NULL;
 
+      /* nouveau_fence_wait will create a new current fence, so wait on the
+       * _current_ one, and remove both.
+       */
+      nouveau_fence_ref(screen->base.fence.current, &current);
+      nouveau_fence_wait(current, NULL);
+      nouveau_fence_ref(NULL, &current);
+      nouveau_fence_ref(NULL, &screen->base.fence.current);
+   }
    if (screen->base.pushbuf)
       screen->base.pushbuf->user_priv = NULL;
 

src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -685,8 +685,17 @@ nvc0_screen_destroy(struct pipe_screen *pscreen)
    if (!nouveau_drm_screen_unref(&screen->base))
       return;
 
-   nouveau_fence_cleanup(&screen->base);
+   if (screen->base.fence.current) {
+      struct nouveau_fence *current = NULL;
 
+      /* nouveau_fence_wait will create a new current fence, so wait on the
+       * _current_ one, and remove both.
+       */
+      nouveau_fence_ref(screen->base.fence.current, &current);
+      nouveau_fence_wait(current, NULL);
+      nouveau_fence_ref(NULL, &current);
+      nouveau_fence_ref(NULL, &screen->base.fence.current);
+   }
    if (screen->base.pushbuf)
       screen->base.pushbuf->user_priv = NULL;
 
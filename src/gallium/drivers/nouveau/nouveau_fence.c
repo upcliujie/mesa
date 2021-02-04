a85a5a64ab324c242fb8c9fb200a21a66b5af5e9
Ilia Mirkin <imirkin@alum.mit.edu>
2021-02-04 12:15:43 -0500

    nouveau: reinstate fencing on screen destroy
    As it turns out, the wait is required as the driver expects for
    rendering to be quiesced on exit. This can trigger channel failures,
    which in turn trigger recovery. This can fail and destroy the whole
    system.
    
    Fixes: 28a781323fb ("nouveau: change fence destruction logic on screen destroy")
    References: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4223
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/gallium/drivers/nouveau/nouveau_fence.c
+++ src/gallium/drivers/nouveau/nouveau_fence.c
@@ -109,24 +109,6 @@ nouveau_fence_del(struct nouveau_fence *fence)
 }
 
 void
-nouveau_fence_cleanup(struct nouveau_screen *screen)
-{
-   struct nouveau_fence *fence, *next;
-
-   for (fence = screen->fence.head; fence; fence = next) {
-      next = fence->next;
-      nouveau_fence_trigger_work(fence);
-      nouveau_fence_ref(NULL, &fence);
-   }
-   screen->fence.head = NULL;
-   screen->fence.tail = NULL;
-   if (screen->fence.current) {
-      nouveau_fence_trigger_work(screen->fence.current);
-      nouveau_fence_ref(NULL, &screen->fence.current);
-   }
-}
-
-void
 nouveau_fence_update(struct nouveau_screen *screen, bool flushed)
 {
    struct nouveau_fence *fence;
