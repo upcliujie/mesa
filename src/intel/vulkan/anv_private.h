e79857f8196004305cd0b7f8bacf90defadda6ee
Chad Versace <chad@kiwitree.net>
2021-02-05 12:07:07 -0800

    anv/image: Replace bo_is_owned with from_gralloc (v2)
    The name anv_image_plane::bo_is_owned will be made ambiguous by the
    implementation of VK_EXT_image_drm_format_modifier, which may bind the
    plane to multiple bo's.
    
    Also, bo_is_owned was set if and only if the image was imported from
    gralloc, and it was set only on the first plane. Therefore, let's rename
    the field to from_gralloc, and move it to the toplevel of anv_image.
    
    v2: Fix build in anv_android.c.
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -3727,6 +3727,12 @@ struct anv_image {
    bool external_format;
 
    /**
+    * Image was imported from gralloc with VkNativeBufferANDROID. The gralloc bo
+    * must be released when the image is destroyed.
+    */
+   bool from_gralloc;
+
+   /**
     * Image subsurfaces
     *
     * For each foo, anv_image::planes[x].surface is valid if and only if
@@ -3800,11 +3806,6 @@ struct anv_image {
        * BO associated with this plane, set when bound.
        */
       struct anv_address address;
-
-      /**
-       * When destroying the image, also free the bo.
-       * */
-      bool bo_is_owned;
    } planes[3];
 };
 
