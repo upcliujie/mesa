Grima04/mesa-turnip-kgsl

commit 0278d1fa323cf1f289a2c5f4cd803c4203d4a48a

gallium: add unbind_num_trailing_slots to set_vertex_buffers
Instead of calling this functions again to unbind trailing slots,
extend it to do it as part of the call that sets vertex buffers.
This reduces CPU overhead. Only st/mesa benefits from this.

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8298>

src/gallium/drivers/nouveau/nv50/nv50_state.c
@@ -1075,6 +1075,7 @@ nv50_set_window_rectangles(struct pipe_context *pipe,
 static void
 nv50_set_vertex_buffers(struct pipe_context *pipe,
                         unsigned start_slot, unsigned count,
+                        unsigned unbind_num_trailing_slots,
                         const struct pipe_vertex_buffer *vb)
 {
    struct nv50_context *nv50 = nv50_context(pipe);
@@ -1084,7 +1085,8 @@ nv50_set_vertex_buffers(struct pipe_context *pipe,
    nv50->dirty_3d |= NV50_NEW_3D_ARRAYS;
 
    util_set_vertex_buffers_count(nv50->vtxbuf, &nv50->num_vtxbufs, vb,
-                                 start_slot, count);
+                                 start_slot, count,
+                                 unbind_num_trailing_slots);
 
    if (!vb) {
       nv50->vbo_user &= ~(((1ull << count) - 1) << start_slot);

src/gallium/drivers/nouveau/nvc0/nvc0_state.c
@@ -996,6 +996,7 @@ nvc0_set_tess_state(struct pipe_context *pipe,
 static void
 nvc0_set_vertex_buffers(struct pipe_context *pipe,
                         unsigned start_slot, unsigned count,
+                        unsigned unbind_num_trailing_slots,
                         const struct pipe_vertex_buffer *vb)
 {
     struct nvc0_context *nvc0 = nvc0_context(pipe);
@@ -1005,7 +1006,8 @@ nvc0_set_vertex_buffers(struct pipe_context *pipe,
     nvc0->dirty_3d |= NVC0_NEW_3D_ARRAYS;
 
     util_set_vertex_buffers_count(nvc0->vtxbuf, &nvc0->num_vtxbufs, vb,
-                                  start_slot, count);
+                                  start_slot, count,
+                                  unbind_num_trailing_slots);
 
     if (!vb) {
        nvc0->vbo_user &= ~(((1ull << count) - 1) << start_slot);

upcliujie/mesa

commit 316017b7d1519416476bdac25eae20916935b242

nv50,nvc0: clear internal vbo masks based on the trailing slots
Fixes: 0278d1fa323c (gallium: add unbind_num_trailing_slots to set_vertex_buffers)
Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/gallium/drivers/nouveau/nv50/nv50_state.c
@@ -1091,6 +1091,11 @@ nv50_set_vertex_buffers(struct pipe_context *pipe,
                                  unbind_num_trailing_slots,
                                  take_ownership);
 
+    unsigned clear_mask = ~(((1ull << unbind_num_trailing_slots) - 1) << (start_slot + count));
+    nv50->vbo_user &= clear_mask;
+    nv50->vbo_constant &= clear_mask;
+    nv50->vtxbufs_coherent &= clear_mask;
+
    if (!vb) {
       nv50->vbo_user &= ~(((1ull << count) - 1) << start_slot);
       nv50->vbo_constant &= ~(((1ull << count) - 1) << start_slot);

src/gallium/drivers/nouveau/nvc0/nvc0_state.c
@@ -1012,6 +1012,11 @@ nvc0_set_vertex_buffers(struct pipe_context *pipe,
                                   unbind_num_trailing_slots,
                                   take_ownership);
 
+    unsigned clear_mask = ~(((1ull << unbind_num_trailing_slots) - 1) << (start_slot + count));
+    nvc0->vbo_user &= clear_mask;
+    nvc0->constant_vbos &= clear_mask;
+    nvc0->vtxbufs_coherent &= clear_mask;
+
     if (!vb) {
        nvc0->vbo_user &= ~(((1ull << count) - 1) << start_slot);
        nvc0->constant_vbos &= ~(((1ull << count) - 1) << start_slot);