0ec9113b9964b6ed056932ea7b2fc66e2fde9420
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-02-10 02:24:39 +0200

    vulkan: implement legacy entrypoints on top of VK_KHR_synchronization2
    v2: fix common vkQueueSubmit() pNext generation
        fix potential leak of perf_query_submit_info
        fix comment about casting VkQueue to vk_object_base
        add helper for barrier struct upgrades (Jason)
        reuse __vk_append_struct() (Jason)
        optimize vk_common_GetQueueCheckpointDataNV
        use multialloc for vk_common_QueueSubmit (Dave)
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

---
+++ src/vulkan/util/vk_synchronization2.c
@@ -0,0 +1,422 @@
+/*
+ * Copyright © 2021 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "vk_alloc.h"
+#include "vk_common_entrypoints.h"
+#include "vk_device.h"
+#include "vk_util.h"
+
+VKAPI_ATTR void VKAPI_CALL
+vk_common_CmdWriteTimestamp(
+   VkCommandBuffer                             commandBuffer,
+   VkPipelineStageFlagBits                     pipelineStage,
+   VkQueryPool                                 queryPool,
+   uint32_t                                    query)
+{
+   /* We don't have a vk_command_buffer object but we can assume, since we're
+    * using common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)commandBuffer;
+
+   disp->device->dispatch_table.CmdWriteTimestamp2KHR(commandBuffer,
+                                                      (VkPipelineStageFlags2KHR) pipelineStage,
+                                                      queryPool,
+                                                      query);
+}
+
+static VkMemoryBarrier2KHR
+upgrade_memory_barrier(const VkMemoryBarrier *barrier,
+                       VkPipelineStageFlags2KHR src_stage_mask2,
+                       VkPipelineStageFlags2KHR dst_stage_mask2)
+{
+   return (VkMemoryBarrier2KHR) {
+      .sType         = VK_STRUCTURE_TYPE_MEMORY_BARRIER_2_KHR,
+      .srcStageMask  = src_stage_mask2,
+      .srcAccessMask = (VkAccessFlags2KHR) barrier->srcAccessMask,
+      .dstStageMask  = dst_stage_mask2,
+      .dstAccessMask = (VkAccessFlags2KHR) barrier->dstAccessMask,
+   };
+}
+
+static VkBufferMemoryBarrier2KHR
+upgrade_buffer_memory_barrier(const VkBufferMemoryBarrier *barrier,
+                              VkPipelineStageFlags2KHR src_stage_mask2,
+                              VkPipelineStageFlags2KHR dst_stage_mask2)
+{
+   return (VkBufferMemoryBarrier2KHR) {
+      .sType                = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER_2_KHR,
+      .srcStageMask         = src_stage_mask2,
+      .srcAccessMask        = (VkAccessFlags2KHR) barrier->srcAccessMask,
+      .dstStageMask         = dst_stage_mask2,
+      .dstAccessMask        = (VkAccessFlags2KHR) barrier->dstAccessMask,
+      .srcQueueFamilyIndex  = barrier->srcQueueFamilyIndex,
+      .dstQueueFamilyIndex  = barrier->dstQueueFamilyIndex,
+      .buffer               = barrier->buffer,
+      .offset               = barrier->offset,
+      .size                 = barrier->size,
+   };
+}
+
+static VkImageMemoryBarrier2KHR
+upgrade_image_memory_barrier(const VkImageMemoryBarrier *barrier,
+                             VkPipelineStageFlags2KHR src_stage_mask2,
+                             VkPipelineStageFlags2KHR dst_stage_mask2)
+{
+   return (VkImageMemoryBarrier2KHR) {
+      .sType                = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2_KHR,
+      .srcStageMask         = src_stage_mask2,
+      .srcAccessMask        = (VkAccessFlags2KHR) barrier->srcAccessMask,
+      .dstStageMask         = dst_stage_mask2,
+      .dstAccessMask        = (VkAccessFlags2KHR) barrier->dstAccessMask,
+      .oldLayout            = barrier->oldLayout,
+      .newLayout            = barrier->newLayout,
+      .srcQueueFamilyIndex  = barrier->srcQueueFamilyIndex,
+      .dstQueueFamilyIndex  = barrier->dstQueueFamilyIndex,
+      .image                = barrier->image,
+      .subresourceRange     = barrier->subresourceRange,
+   };
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vk_common_CmdPipelineBarrier(
+    VkCommandBuffer                             commandBuffer,
+    VkPipelineStageFlags                        srcStageMask,
+    VkPipelineStageFlags                        dstStageMask,
+    VkDependencyFlags                           dependencyFlags,
+    uint32_t                                    memoryBarrierCount,
+    const VkMemoryBarrier*                      pMemoryBarriers,
+    uint32_t                                    bufferMemoryBarrierCount,
+    const VkBufferMemoryBarrier*                pBufferMemoryBarriers,
+    uint32_t                                    imageMemoryBarrierCount,
+    const VkImageMemoryBarrier*                 pImageMemoryBarriers)
+{
+   /* We don't have a vk_command_buffer object but we can assume, since we're
+    * using common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)commandBuffer;
+
+   STACK_ARRAY(VkMemoryBarrier2KHR, memory_barriers, memoryBarrierCount);
+   STACK_ARRAY(VkBufferMemoryBarrier2KHR, buffer_barriers, bufferMemoryBarrierCount);
+   STACK_ARRAY(VkImageMemoryBarrier2KHR, image_barriers, imageMemoryBarrierCount);
+
+   VkPipelineStageFlags2KHR src_stage_mask2 = (VkPipelineStageFlags2KHR) srcStageMask;
+   VkPipelineStageFlags2KHR dst_stage_mask2 = (VkPipelineStageFlags2KHR) dstStageMask;
+
+   for (uint32_t i = 0; i < memoryBarrierCount; i++) {
+      memory_barriers[i] = upgrade_memory_barrier(&pMemoryBarriers[i],
+                                                  src_stage_mask2,
+                                                  dst_stage_mask2);
+   }
+   for (uint32_t i = 0; i < bufferMemoryBarrierCount; i++) {
+      buffer_barriers[i] = upgrade_buffer_memory_barrier(&pBufferMemoryBarriers[i],
+                                                         src_stage_mask2,
+                                                         dst_stage_mask2);
+   }
+   for (uint32_t i = 0; i < imageMemoryBarrierCount; i++) {
+      image_barriers[i] = upgrade_image_memory_barrier(&pImageMemoryBarriers[i],
+                                                       src_stage_mask2,
+                                                       dst_stage_mask2);
+   }
+
+   VkDependencyInfoKHR dep_info = {
+      .sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO_KHR,
+      .memoryBarrierCount = memoryBarrierCount,
+      .pMemoryBarriers = memory_barriers,
+      .bufferMemoryBarrierCount = bufferMemoryBarrierCount,
+      .pBufferMemoryBarriers = buffer_barriers,
+      .imageMemoryBarrierCount = imageMemoryBarrierCount,
+      .pImageMemoryBarriers = image_barriers,
+   };
+
+   disp->device->dispatch_table.CmdPipelineBarrier2KHR(commandBuffer, &dep_info);
+
+   STACK_ARRAY_FINISH(memory_barriers);
+   STACK_ARRAY_FINISH(buffer_barriers);
+   STACK_ARRAY_FINISH(image_barriers);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vk_common_CmdSetEvent(
+    VkCommandBuffer                             commandBuffer,
+    VkEvent                                     event,
+    VkPipelineStageFlags                        stageMask)
+{
+   /* We don't have a vk_command_buffer object but we can assume, since we're
+    * using common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)commandBuffer;
+
+   VkMemoryBarrier2KHR mem_barrier = {
+      .sType = VK_STRUCTURE_TYPE_MEMORY_BARRIER_2_KHR,
+      .srcStageMask = (VkPipelineStageFlags2KHR) stageMask,
+      .dstStageMask = (VkPipelineStageFlags2KHR) stageMask,
+   };
+   VkDependencyInfoKHR dep_info = {
+      .sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO_KHR,
+      .memoryBarrierCount = 1,
+      .pMemoryBarriers = &mem_barrier,
+   };
+
+   disp->device->dispatch_table.CmdSetEvent2KHR(commandBuffer, event, &dep_info);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vk_common_CmdResetEvent(
+    VkCommandBuffer                             commandBuffer,
+    VkEvent                                     event,
+    VkPipelineStageFlags                        stageMask)
+{
+   /* We don't have a vk_command_buffer object but we can assume, since we're
+    * using common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)commandBuffer;
+
+   disp->device->dispatch_table.CmdResetEvent2KHR(commandBuffer,
+                                                  event,
+                                                  (VkPipelineStageFlags2KHR) stageMask);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vk_common_CmdWaitEvents(
+    VkCommandBuffer                             commandBuffer,
+    uint32_t                                    eventCount,
+    const VkEvent*                              pEvents,
+    VkPipelineStageFlags                        srcStageMask,
+    VkPipelineStageFlags                        destStageMask,
+    uint32_t                                    memoryBarrierCount,
+    const VkMemoryBarrier*                      pMemoryBarriers,
+    uint32_t                                    bufferMemoryBarrierCount,
+    const VkBufferMemoryBarrier*                pBufferMemoryBarriers,
+    uint32_t                                    imageMemoryBarrierCount,
+    const VkImageMemoryBarrier*                 pImageMemoryBarriers)
+{
+   /* We don't have a vk_command_buffer object but we can assume, since we're
+    * using common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)commandBuffer;
+
+   STACK_ARRAY(VkMemoryBarrier2KHR, memory_barriers, memoryBarrierCount);
+   STACK_ARRAY(VkBufferMemoryBarrier2KHR, buffer_barriers, bufferMemoryBarrierCount);
+   STACK_ARRAY(VkImageMemoryBarrier2KHR, image_barriers, imageMemoryBarrierCount);
+
+   VkPipelineStageFlags2KHR src_stage_mask2 = (VkPipelineStageFlags2KHR) srcStageMask;
+   VkPipelineStageFlags2KHR dst_stage_mask2 = (VkPipelineStageFlags2KHR) destStageMask;
+
+   for (uint32_t i = 0; i < memoryBarrierCount; i++) {
+      memory_barriers[i] = upgrade_memory_barrier(&pMemoryBarriers[i],
+                                                  src_stage_mask2,
+                                                  dst_stage_mask2);
+   }
+   for (uint32_t i = 0; i < bufferMemoryBarrierCount; i++) {
+      buffer_barriers[i] = upgrade_buffer_memory_barrier(&pBufferMemoryBarriers[i],
+                                                         src_stage_mask2,
+                                                         dst_stage_mask2);
+   }
+   for (uint32_t i = 0; i < imageMemoryBarrierCount; i++) {
+      image_barriers[i] = upgrade_image_memory_barrier(&pImageMemoryBarriers[i],
+                                                       src_stage_mask2,
+                                                       dst_stage_mask2);
+   }
+
+   VkDependencyInfoKHR dep_info = {
+      .sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO_KHR,
+      .memoryBarrierCount = memoryBarrierCount,
+      .pMemoryBarriers = memory_barriers,
+      .bufferMemoryBarrierCount = bufferMemoryBarrierCount,
+      .pBufferMemoryBarriers = buffer_barriers,
+      .imageMemoryBarrierCount = imageMemoryBarrierCount,
+      .pImageMemoryBarriers = image_barriers,
+   };
+
+   disp->device->dispatch_table.CmdWaitEvents2KHR(commandBuffer,
+                                                  eventCount,
+                                                  pEvents,
+                                                  &dep_info);
+
+   STACK_ARRAY_FINISH(memory_barriers);
+   STACK_ARRAY_FINISH(buffer_barriers);
+   STACK_ARRAY_FINISH(image_barriers);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vk_common_CmdWriteBufferMarkerAMD(
+    VkCommandBuffer                             commandBuffer,
+    VkPipelineStageFlagBits                     pipelineStage,
+    VkBuffer                                    dstBuffer,
+    VkDeviceSize                                dstOffset,
+    uint32_t                                    marker)
+{
+   /* We don't have a vk_command_buffer object but we can assume, since we're
+    * using common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)commandBuffer;
+
+   disp->device->dispatch_table.CmdWriteBufferMarker2AMD(commandBuffer,
+                                                         (VkPipelineStageFlags2KHR) pipelineStage,
+                                                         dstBuffer,
+                                                         dstOffset,
+                                                         marker);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+vk_common_GetQueueCheckpointDataNV(
+    VkQueue                                     queue,
+    uint32_t*                                   pCheckpointDataCount,
+    VkCheckpointDataNV*                         pCheckpointData)
+{
+   /* We don't have a vk_command_buffer object but we can assume, since we're
+    * using common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)queue;
+
+   uint32_t n_checkpoint_data_2 = 0;
+   disp->device->dispatch_table.GetQueueCheckpointData2NV(queue, &n_checkpoint_data_2, NULL);
+
+   if (!pCheckpointData)
+      return;
+
+   STACK_ARRAY(VkCheckpointData2NV, checkpoint_data_2, n_checkpoint_data_2);
+   disp->device->dispatch_table.GetQueueCheckpointData2NV(queue, &n_checkpoint_data_2, checkpoint_data_2);
+
+   VK_OUTARRAY_MAKE(out, pCheckpointData, pCheckpointDataCount);
+
+   for (uint32_t i = 0; i < n_checkpoint_data_2; i++) {
+      vk_outarray_append(&out, check_point) {
+         check_point->stage = VK_PIPELINE_STAGE_FLAG_BITS_MAX_ENUM & checkpoint_data_2[i].stage;
+         check_point->pCheckpointMarker = checkpoint_data_2[i].pCheckpointMarker;
+      }
+   }
+
+   STACK_ARRAY_FINISH(checkpoint_data_2);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+vk_common_QueueSubmit(
+    VkQueue                                     queue,
+    uint32_t                                    submitCount,
+    const VkSubmitInfo*                         pSubmits,
+    VkFence                                     fence)
+{
+   /* We don't have a vk_queue object but we can assume, since we're using
+    * common dispatch, that it's a vk_object of some sort.
+    */
+   struct vk_object_base *disp = (struct vk_object_base *)queue;
+
+   STACK_ARRAY(VkSubmitInfo2KHR, submit_info_2, submitCount);
+   STACK_ARRAY(VkPerformanceQuerySubmitInfoKHR, perf_query_submit_info, submitCount);
+
+   VK_MULTIALLOC(ma);
+   uint32_t n_wait_semaphores = 0;
+   uint32_t n_command_buffers = 0;
+   uint32_t n_signal_semaphores = 0;
+   for (uint32_t s = 0; s < submitCount; s++) {
+      n_wait_semaphores += pSubmits[s].waitSemaphoreCount;
+      n_command_buffers += pSubmits[s].commandBufferCount;
+      n_signal_semaphores += pSubmits[s].signalSemaphoreCount;
+   }
+
+   VK_MULTIALLOC_DECL(&ma, VkSemaphoreSubmitInfoKHR, wait_semaphores, n_wait_semaphores);
+   VK_MULTIALLOC_DECL(&ma, VkCommandBufferSubmitInfoKHR, command_buffers, n_command_buffers);
+   VK_MULTIALLOC_DECL(&ma, VkSemaphoreSubmitInfoKHR, signal_semaphores, n_signal_semaphores);
+
+   if (ma.size > 0 &&
+       !vk_multialloc_alloc(&ma, &disp->device->alloc, VK_SYSTEM_ALLOCATION_SCOPE_COMMAND))
+      return VK_ERROR_OUT_OF_HOST_MEMORY;
+
+   for (uint32_t s = 0; s < submitCount; s++) {
+      const VkTimelineSemaphoreSubmitInfoKHR *timeline_info =
+         vk_find_struct_const(pSubmits[s].pNext,
+                              TIMELINE_SEMAPHORE_SUBMIT_INFO_KHR);
+      const uint64_t *wait_values =
+         timeline_info && timeline_info->waitSemaphoreValueCount ?
+         timeline_info->pWaitSemaphoreValues : NULL;
+      const uint64_t *signal_values =
+         timeline_info && timeline_info->signalSemaphoreValueCount ?
+         timeline_info->pSignalSemaphoreValues : NULL;
+
+      const VkDeviceGroupSubmitInfo *group_info =
+         vk_find_struct_const(pSubmits[s].pNext, DEVICE_GROUP_SUBMIT_INFO);
+
+      for (uint32_t i = 0; i < pSubmits[s].waitSemaphoreCount; i++) {
+         wait_semaphores[i] = (VkSemaphoreSubmitInfoKHR) {
+            .sType       = VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO_KHR,
+            .semaphore   = pSubmits[s].pWaitSemaphores[i],
+            .value       = wait_values ? wait_values[i] : 0,
+            .stageMask   = pSubmits[s].pWaitDstStageMask[i],
+            .deviceIndex = group_info ? group_info->pWaitSemaphoreDeviceIndices[i] : 0,
+         };
+      }
+      for (uint32_t i = 0; i < pSubmits[s].commandBufferCount; i++) {
+         command_buffers[i] = (VkCommandBufferSubmitInfoKHR) {
+            .sType         = VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO_KHR,
+            .commandBuffer = pSubmits[s].pCommandBuffers[i],
+            .deviceMask    = group_info ? group_info->pCommandBufferDeviceMasks[i] : 0,
+         };
+      }
+      for (uint32_t i = 0; i < pSubmits[s].signalSemaphoreCount; i++) {
+         signal_semaphores[i] = (VkSemaphoreSubmitInfoKHR) {
+            .sType     = VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO_KHR,
+            .semaphore = pSubmits[s].pSignalSemaphores[i],
+            .value     = signal_values ? signal_values[i] : 0,
+            .stageMask = 0,
+            .deviceIndex = group_info ? group_info->pSignalSemaphoreDeviceIndices[i] : 0,
+         };
+      }
+
+      const VkProtectedSubmitInfo *protected_info =
+         vk_find_struct_const(pSubmits[s].pNext, PROTECTED_SUBMIT_INFO);
+
+      submit_info_2[s] = (VkSubmitInfo2KHR) {
+         .sType                    = VK_STRUCTURE_TYPE_SUBMIT_INFO_2_KHR,
+         .flags                    = ((protected_info && protected_info->protectedSubmit) ?
+                                      VK_SUBMIT_PROTECTED_BIT_KHR : 0),
+         .waitSemaphoreInfoCount   = pSubmits[s].waitSemaphoreCount,
+         .pWaitSemaphoreInfos      = wait_semaphores,
+         .commandBufferInfoCount   = pSubmits[s].commandBufferCount,
+         .pCommandBufferInfos      = command_buffers,
+         .signalSemaphoreInfoCount = pSubmits[s].signalSemaphoreCount,
+         .pSignalSemaphoreInfos    = signal_semaphores,
+      };
+
+      const VkPerformanceQuerySubmitInfoKHR *query_info =
+         vk_find_struct_const(pSubmits[s].pNext,
+                              PERFORMANCE_QUERY_SUBMIT_INFO_KHR);
+      if (query_info)
+         __vk_append_struct(&submit_info_2[s], &perf_query_submit_info[s]);
+
+      wait_semaphores += pSubmits[s].waitSemaphoreCount;
+      command_buffers += pSubmits[s].commandBufferCount;
+      signal_semaphores += pSubmits[s].signalSemaphoreCount;
+   }
+
+   VkResult result = disp->device->dispatch_table.QueueSubmit2KHR(queue,
+                                                                  submitCount,
+                                                                  submit_info_2,
+                                                                  fence);
+
+   VK_MULTIALLOC_FREE(ma);
+   STACK_ARRAY_FINISH(submit_info_2);
+   STACK_ARRAY_FINISH(perf_query_submit_info);
+
+   return result;
+}
