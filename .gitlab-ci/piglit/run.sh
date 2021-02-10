b0c4a98b1e555878da3c37c6534b88f1e02dfe3c
Antonio Caggiano <antonio.caggiano@collabora.com>
2021-02-10 19:07:32 +0100

    ci/freedreno: Test with non-redistributable traces
    Add a job to test with traces that we cannot redistribute, listed in a
    separate file. Since those traces might not be accessible by everyone,
    this job is created only when the pipeline is triggered by `marge-bot`.
    
    This job is optional because otherwise it could be blocking a merge
    request of someone who cannot really debug the issue due to lack of
    access to these traces.
    
    The documentation available under `docs/ci` goes into more details
    explaining the rationale behind optional traces.
    
    Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6441>

--- .gitlab-ci/piglit/run.sh
+++ .gitlab-ci/piglit/run.sh
@@ -202,6 +202,8 @@ fi
 
 FAILURE_MESSAGE=$(printf "%s" "Unexpected change in results:")
 
+ci-fairy minio login $CI_JOB_JWT
+
 eval $RUN_CMD
 
 if [ $? -ne 0 ]; then
@@ -226,8 +228,6 @@ mkdir -p .gitlab-ci/piglit
 
 if [ "x$PIGLIT_PROFILES" = "xreplay" ] \
        && [ ${PIGLIT_REPLAY_UPLOAD_TO_MINIO:-0} -eq 1 ]; then
-
-    ci-fairy minio login $CI_JOB_JWT
 
     __PREFIX="trace/$PIGLIT_REPLAY_DEVICE_NAME"
     __MINIO_PATH="$PIGLIT_REPLAY_ARTIFACTS_BASE_URL"
