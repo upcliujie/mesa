226201c99002895ab14db0fdfa621279a43ea45b
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-02-10 18:40:55 -0500

    zink: enforce device lost status
    many apps don't request device-lost notification, so just calling the reset
    callback isn't enough; once the device has been lost, no more cmdbufs should
    be submitted and the queue should not be waited on
    
    Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -290,7 +290,7 @@ zink_context_destroy(struct pipe_context *pctx)
    struct zink_context *ctx = zink_context(pctx);
    struct zink_screen *screen = zink_screen(pctx->screen);
 
-   if (ctx->batch.queue && vkQueueWaitIdle(ctx->batch.queue) != VK_SUCCESS)
+   if (ctx->batch.queue && !screen->device_lost && vkQueueWaitIdle(ctx->batch.queue) != VK_SUCCESS)
       debug_printf("vkQueueWaitIdle failed\n");
 
    util_blitter_destroy(ctx->blitter);
@@ -2002,6 +2002,8 @@ timeline_wait(struct zink_context *ctx, uint32_t batch_id, uint64_t timeout)
    uint64_t batch_id64 = batch_id;
    wi.pValues = &batch_id64;
    bool success = false;
+   if (screen->device_lost)
+      return true;
    switch (screen->vk_WaitSemaphores(screen->dev, &wi, timeout)) {
    case VK_SUCCESS:
       success = true;
@@ -2009,6 +2011,7 @@ timeline_wait(struct zink_context *ctx, uint32_t batch_id, uint64_t timeout)
    case VK_ERROR_DEVICE_LOST:
       if (ctx->reset.reset)
          ctx->reset.reset(ctx->reset.data, PIPE_GUILTY_CONTEXT_RESET);
+      screen->device_lost = true;
       break;
    default:
       break;
