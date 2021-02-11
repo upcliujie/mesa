Grima04/mesa-turnip-kgsl

commit fc7770b1ddac0f26489925ded8002bd9a8886095

pan/bi: Add trivial rewrite helpers
We need to do certain rewrites during scheduling before RA runs in order
to satsify scheduler post-conditions.

Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8354>

src/panfrost/bifrost/bi_schedule.c
@@ -890,6 +890,32 @@ bi_rewrite_passthrough(bi_tuple prec, bi_tuple succ)
         }
 }
 
+static void
+bi_rewrite_fau_to_pass(bi_tuple *tuple)
+{
+        bi_foreach_instr_and_src_in_tuple(tuple, ins, s) {
+                if (ins->src[s].type != BI_INDEX_FAU) continue;
+
+                ins->src[s] = bi_passthrough(ins->src[s].offset ?
+                                BIFROST_SRC_FAU_HI : BIFROST_SRC_FAU_LO);
+        }
+}
+
+static void
+bi_rewrite_zero(bi_instr *ins, bool fma)
+{
+        bi_foreach_src(ins, s) {
+                bi_index src = ins->src[s];
+                unsigned swizzle = src.swizzle;
+
+                if (src.type == BI_INDEX_CONSTANT && src.value == 0) {
+                        assert(!src.abs && !src.neg);
+                        ins->src[s] = bi_passthrough(
+                                        fma ? BIFROST_SRC_STAGE : BIFROST_SRC_FAU_LO);
+                        ins->src[s].swizzle = swizzle;
+                }
+        }
+}
 
 #ifndef NDEBUG
 

upcliujie/mesa

commit efb88a1c8cce676f94a39146341158efa488d535

pan/bi: Handle modifiers in rewrite_fau_to_pass
Will prevent failures when we start using FAU together with modifiers in
a few commits.

Fixes: fc7770b1dda ("pan/bi: Add trivial rewrite helpers")
Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

src/panfrost/bifrost/bi_schedule.c
@@ -852,8 +852,10 @@ bi_rewrite_fau_to_pass(bi_tuple *tuple)
         bi_foreach_instr_and_src_in_tuple(tuple, ins, s) {
                 if (ins->src[s].type != BI_INDEX_FAU) continue;
 
-                ins->src[s] = bi_passthrough(ins->src[s].offset ?
+                bi_index pass = bi_passthrough(ins->src[s].offset ?
                                 BIFROST_SRC_FAU_HI : BIFROST_SRC_FAU_LO);
+
+                ins->src[s] = bi_replace_index(ins->src[s], pass);
         }
 }
 