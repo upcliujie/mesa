c297e59c95a62aa587aeb42b7b2f67282c6505a1
Marek Olšák <marek.olsak@amd.com>
2021-02-12 09:44:04 -0500

    glsl: lower mediump uniforms to FP16 based on an option
    Everything else is done - just allow it.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -3287,6 +3287,7 @@ struct gl_shader_compiler_options
    GLboolean LowerPrecisionFloat16;
    GLboolean LowerPrecisionInt16;
    GLboolean LowerPrecisionDerivatives;
+   GLboolean LowerPrecisionFloat16Uniforms;
 
    /**
     * This enables lowering of 16b constants.  Some drivers may not
