Grima04/mesa-turnip-kgsl

commit 2891e0b74e6c6dc4a93bc73536949dbb93c2bb50

zink: pull xfb info from tess shader when applicable
if it's the last vertex stage then it does the xfb

Reviewed-by: Erik Faye-Lund <kusmabite@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8152>

src/gallium/drivers/zink/zink_program.c
@@ -304,6 +304,7 @@ update_shader_modules(struct zink_context *ctx, struct zink_shader *stages[ZINK_
       if (dirty[i]) {
          struct zink_shader_module *zm;
          dirty[i]->has_geometry_shader = dirty[MESA_SHADER_GEOMETRY] || stages[PIPE_SHADER_GEOMETRY];
+         dirty[i]->has_tess_shader = dirty[MESA_SHADER_TESS_EVAL] || stages[PIPE_SHADER_TESS_EVAL];
          zm = get_shader_module_for_stage(ctx, dirty[i], prog);
          zink_shader_module_reference(zink_screen(ctx->base.screen), &prog->modules[type], zm);
          /* we probably need a new pipeline when we switch shader modules */

src/gallium/drivers/zink/zink_compiler.c
@@ -331,8 +331,16 @@ zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, struct z
    void *streamout = NULL;
    nir_shader *nir = zs->nir;
    /* TODO: use a separate mem ctx here for ralloc */
-   if (zs->streamout.so_info_slots && (zs->nir->info.stage != MESA_SHADER_VERTEX || !zs->has_geometry_shader))
+   if (zs->has_geometry_shader) {
+      if (zs->nir->info.stage == MESA_SHADER_GEOMETRY)
+         streamout = &zs->streamout;
+   } else if (zs->has_tess_shader) {
+      if (zs->nir->info.stage == MESA_SHADER_TESS_EVAL)
+         streamout = &zs->streamout;
+   } else
       streamout = &zs->streamout;
+   if (!zs->streamout.so_info_slots)
+       streamout = NULL;
    if (zs->nir->info.stage == MESA_SHADER_FRAGMENT) {
       nir = nir_shader_clone(NULL, nir);
       if (!zink_fs_key(key)->samples &&

src/gallium/drivers/zink/zink_compiler.h
@@ -72,6 +72,7 @@ struct zink_shader {
    size_t num_bindings;
    struct set *programs;
 
+   bool has_tess_shader; // vertex shaders need to know if a tesseval shader exists
    bool has_geometry_shader; // vertex shaders need to know if a geometry shader exists
 };
 

upcliujie/mesa

commit d854569883249c524a5c828e75df20c40764d6aa

zink: fix streamout for tess stage
the tess shader needs to actually emit xfb stuff in order for it to work

Fixes: 2891e0b74e6 ("zink: pull xfb info from tess shader when applicable")

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -3791,8 +3791,8 @@ nir_to_spirv(struct nir_shader *s, const struct zink_so_info *so_info,
 
    emit_cf_list(&ctx, &entry->body);
 
-   /* vertex shader emits copied xfb outputs at the end of the shader */
-   if (so_info && ctx.stage == MESA_SHADER_VERTEX)
+   /* vertex/tess shader emits copied xfb outputs at the end of the shader */
+   if (so_info && (ctx.stage == MESA_SHADER_VERTEX || ctx.stage == MESA_SHADER_TESS_EVAL))
       emit_so_outputs(&ctx, so_info);
 
    spirv_builder_return(&ctx.builder); // doesn't belong here, but whatevz

src/gallium/drivers/zink/zink_program.c
@@ -847,7 +847,7 @@ zink_create_tes_state(struct pipe_context *pctx,
    else
       nir = (struct nir_shader *)shader->ir.nir;
 
-   return zink_shader_create(zink_screen(pctx->screen), nir, NULL);
+   return zink_shader_create(zink_screen(pctx->screen), nir, &shader->stream_output);
 }
 
 static void