860e29da85e6beae3d4f930148c969053b7b7e31
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-02-15 13:51:54 +0200

    turnip: implement VK_KHR_vulkan_memory_model
    No handling of Acquire/Release because at the moment scheduler
    works as if any barrier is Acq+Rel.
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/freedreno/vulkan/tu_extensions.py
+++ src/freedreno/vulkan/tu_extensions.py
@@ -103,6 +103,7 @@ EXTENSIONS = [
     Extension('VK_KHR_depth_stencil_resolve',             1, True),
     Extension('VK_KHR_performance_query',                 1, 'device->instance->debug_flags & TU_DEBUG_PERFC'),
     Extension('VK_EXT_memory_budget',                     1, True),
+    Extension('VK_KHR_vulkan_memory_model',               3, True),
 ]
 
 MAX_API_VERSION = VkVersion(MAX_API_VERSION)
