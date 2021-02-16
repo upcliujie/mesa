c6a2e89313be1ce9c3965a4a5da285df1c073c7e
Timur Kristóf <timur.kristof@gmail.com>
2021-02-16 18:21:26 +0100

    nir: Constant-fold no_unsigned_wrap additions into shared load/store.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/compiler/nir/nir_opt_constant_folding.c
+++ src/compiler/nir/nir_opt_constant_folding.c
@@ -179,6 +179,133 @@ fail:
    return NULL;
 }
 
+static uint32_t
+max_upper_bound(nir_src *src)
+{
+   /* Cheap alternative to the real range analysis.
+    * This helps with various address offset calculations.
+    */
+
+   if (nir_src_is_const(*src))
+      return nir_src_as_uint(*src);
+
+   if (src->ssa->parent_instr->type == nir_instr_type_intrinsic) {
+      nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(src->ssa->parent_instr);
+      switch (intrin->intrinsic) {
+      case nir_intrinsic_load_tess_rel_patch_id_gcn:
+      case nir_intrinsic_load_tcs_num_patches_gcn:
+         /* A very generous maximum of 128 x wave64 TCS with a patch size of 1.
+          * (No current HW can actually have that many waves in a workgroup.)
+          */
+         return 128u * 64u * 1u;
+      default:
+         return UINT32_MAX;
+      }
+   }
+
+   if (src->ssa->parent_instr->type == nir_instr_type_alu) {
+      nir_alu_instr *alu = nir_instr_as_alu(src->ssa->parent_instr);
+      uint32_t src0 = max_upper_bound(&alu->src[0].src);
+      uint32_t src1 = UINT32_MAX;
+      if (nir_op_infos[alu->op].num_inputs > 1)
+         src1 = max_upper_bound(&alu->src[1].src);
+
+      switch (alu->op) {
+      case nir_op_iadd:
+         if ((UINT32_MAX - src0) < src1)
+            return UINT32_MAX;
+         return src0 + src1;
+      case nir_op_imul:
+         if (src0 != 0 && (src0 * src1) / src0 != src1)
+            return UINT32_MAX;
+         return src0 * src1;
+      case nir_op_ishl:
+         if (util_last_bit64(src0) + src1 > src->ssa->bit_size)
+            return UINT32_MAX;
+         return src0 << MIN2(src1, src->ssa->bit_size - 1u);
+      case nir_op_umin:
+         return MIN2(src0, src1);
+      case nir_op_umax:
+         return MAX2(src0, src1);
+      default:
+         return UINT32_MAX;
+      }
+   }
+
+   return UINT32_MAX;
+}
+
+static nir_ssa_def *
+try_extract_const_addition(nir_builder *b, nir_instr *instr, unsigned *out_const)
+{
+   if (instr->type != nir_instr_type_alu)
+      return NULL;
+
+   nir_alu_instr *alu = nir_instr_as_alu(instr);
+   if (alu->op != nir_op_iadd ||
+       !nir_alu_src_is_trivial_ssa(alu, 0) ||
+       !nir_alu_src_is_trivial_ssa(alu, 1))
+      return NULL;
+
+   if (!alu->no_unsigned_wrap) {
+      /* Check if there really can be an unsigned wrap. */
+      unsigned upper_bound_0 = max_upper_bound(&alu->src[0].src);
+      unsigned upper_bound_1 = max_upper_bound(&alu->src[1].src);
+
+      if ((UINT_MAX - upper_bound_0) < upper_bound_1)
+         return NULL;
+   }
+
+   for (unsigned i = 0; i < 2; ++i) {
+      if (nir_src_is_const(alu->src[i].src)) {
+         *out_const += nir_src_as_uint(alu->src[i].src);
+         return alu->src[1 - i].src.ssa;
+      }
+
+      nir_ssa_def *replace_src = try_extract_const_addition(b, alu->src[0].src.ssa->parent_instr, out_const);
+      if (replace_src) {
+         b->cursor = nir_before_instr(&alu->instr);
+         return nir_iadd(b, replace_src, alu->src[1 - i].src.ssa);
+      }
+   }
+
+   return NULL;
+}
+
+static bool
+try_fold_load_store(nir_builder *b,
+                    nir_intrinsic_instr *intrin,
+                    UNUSED struct constant_fold_state *state,
+                    unsigned offset_src_idx)
+{
+   /* Assume that BASE is the constant offset of a load/store.
+    * Try to constant-fold additions to the offset source
+    * into the actual const offset of the instruction.
+    */
+
+   unsigned off_const = nir_intrinsic_base(intrin);
+   nir_src *off_src = &intrin->src[offset_src_idx];
+   nir_ssa_def *replace_src = NULL;
+
+   if (!off_src->is_ssa || off_src->ssa->bit_size != 32)
+      return false;
+
+   if (!nir_src_is_const(*off_src)) {
+      replace_src = try_extract_const_addition(b, off_src->ssa->parent_instr, &off_const);
+   } else if (nir_src_as_uint(*off_src)) {
+      off_const += nir_src_as_uint(*off_src);
+      b->cursor = nir_before_instr(&intrin->instr);
+      replace_src = nir_imm_zero(b, off_src->ssa->num_components, off_src->ssa->bit_size);
+   }
+
+   if (!replace_src)
+      return false;
+
+   nir_instr_rewrite_src(&intrin->instr, &intrin->src[offset_src_idx], nir_src_for_ssa(replace_src));
+   nir_intrinsic_set_base(intrin, off_const);
+   return true;
+}
+
 static bool
 try_fold_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
                    struct constant_fold_state *state)
@@ -226,6 +353,11 @@ try_fold_intrinsic(nir_builder *b, nir_intrinsic_instr *intrin,
       }
       return false;
    }
+
+   case nir_intrinsic_load_shared:
+      return try_fold_load_store(b, intrin, state, 0);
+   case nir_intrinsic_store_shared:
+      return try_fold_load_store(b, intrin, state, 1);
 
    case nir_intrinsic_load_constant: {
       state->has_load_constant = true;
