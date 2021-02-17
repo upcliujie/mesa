9702fe47c6d70d51b098a20228ca7e74bfd1f305
Andres Gomez <agomez@igalia.com>
2021-02-17 18:23:30 +0200

    ci: add libdrm to the x86_test-vk container
    Fixes: 5d0ba8b1834 (ci: Split building of libdrm to its own script)
    Signed-off-by: Andres Gomez <agomez@igalia.com>

--- .gitlab-ci/container/x86_test-vk.sh
+++ .gitlab-ci/container/x86_test-vk.sh
@@ -27,6 +27,7 @@ STABLE_EPHEMERAL=" \
       pkg-config \
       python3-distutils \
       wget \
+      xz-utils \
       "
 
 # Unfortunately, gfxreconstruct needs the -dev packages:
@@ -135,6 +136,10 @@ rm -rf /root/.rustup /root/.cargo
 ############### Build VulkanTools
 
 . .gitlab-ci/container/build-vulkantools.sh
+
+############### Build libdrm
+
+. .gitlab-ci/container/build-libdrm.sh
 
 ############### Uninstall the build software
 
