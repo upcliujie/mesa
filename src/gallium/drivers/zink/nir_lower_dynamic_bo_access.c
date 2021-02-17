Grima04/mesa-turnip-kgsl

commit 902b3182133061f1bebe55888c8884e8148878e9

zink: break up dynamic access lowering
this is gross and slow, but to handle dominance properly, now we just load all
the ubos and bcsel away

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7199>

src/gallium/drivers/zink/nir_lower_dynamic_bo_access.c
@@ -42,22 +42,7 @@ bool nir_lower_dynamic_bo_access(nir_shader *shader);
  * was previously loaded by the load_ubo conditional chain
  */
 static nir_ssa_def *
-recursive_generate_bo_ssa_def(nir_builder *b, nir_intrinsic_instr *instr, nir_ssa_def *index, unsigned start, unsigned end, nir_ssa_def **dests)
-{
-   if (start == end - 1)
-      return dests[start];
-
-   unsigned mid = start + (end - start) / 2;
-   return nir_build_alu(b, nir_op_bcsel, nir_build_alu(b, nir_op_ilt, index, nir_imm_int(b, mid), NULL, NULL),
-      recursive_generate_bo_ssa_def(b, instr, index, start, mid, dests),
-      recursive_generate_bo_ssa_def(b, instr, index, mid, end, dests),
-      NULL
-   );
-}
-
-/* generate a chain of conditionals which reduce to a single, constant value to pass to load_ubo */
-static void
-recursive_un_dynamic_bo_access(nir_builder *b, nir_intrinsic_instr *instr, nir_ssa_def *index, unsigned start, unsigned end, nir_ssa_def **dests)
+recursive_generate_bo_ssa_def(nir_builder *b, nir_intrinsic_instr *instr, nir_ssa_def *index, unsigned start, unsigned end)
 {
    if (start == end - 1) {
       /* block index src is 1 for this op */
@@ -79,16 +64,15 @@ recursive_un_dynamic_bo_access(nir_builder *b, nir_intrinsic_instr *instr, nir_s
                            nir_dest_num_components(instr->dest),
                            nir_dest_bit_size(instr->dest), NULL);
       nir_builder_instr_insert(b, &new_instr->instr);
-      dests[start] = &new_instr->dest.ssa;
-      return;
+      return &new_instr->dest.ssa;
    }
 
    unsigned mid = start + (end - start) / 2;
-   nir_push_if(b, nir_ilt(b, index, nir_imm_int(b, mid)));
-   recursive_un_dynamic_bo_access(b, instr, index, start, mid, dests);
-   nir_push_else(b, NULL);
-   recursive_un_dynamic_bo_access(b, instr, index, mid, end, dests);
-   nir_pop_if(b, NULL);
+   return nir_build_alu(b, nir_op_bcsel, nir_build_alu(b, nir_op_ilt, index, nir_imm_int(b, mid), NULL, NULL),
+      recursive_generate_bo_ssa_def(b, instr, index, start, mid),
+      recursive_generate_bo_ssa_def(b, instr, index, mid, end),
+      NULL
+   );
 }
 
 static bool
@@ -105,7 +89,6 @@ lower_dynamic_bo_access_instr(nir_intrinsic_instr *instr, nir_builder *b)
    if (nir_src_is_const(instr->src[block_idx]))
       return false;
    b->cursor = nir_after_instr(&instr->instr);
-   nir_ssa_def *dests[16];
    bool ssbo_mode = instr->intrinsic != nir_intrinsic_load_ubo && instr->intrinsic != nir_intrinsic_load_ubo_vec4;
    unsigned first_idx = UINT_MAX, last_idx;
    if (ssbo_mode) {
@@ -122,13 +105,10 @@ lower_dynamic_bo_access_instr(nir_intrinsic_instr *instr, nir_builder *b)
       last_idx = first_idx + b->shader->info.num_ubos;
    }
 
-   /* first create the conditional chains to generate all the dests */
-   recursive_un_dynamic_bo_access(b, instr, instr->src[block_idx].ssa,
-                                  first_idx, last_idx, dests);
    /* now create the composite dest with a bcsel chain based on the original value */
    nir_ssa_def *new_dest = recursive_generate_bo_ssa_def(b, instr,
                                                        instr->src[block_idx].ssa,
-                                                       first_idx, last_idx, dests);
+                                                       first_idx, last_idx);
 
    if (instr->intrinsic != nir_intrinsic_store_ssbo)
       /* now use the composite dest in all cases where the original dest (from the dynamic index)

upcliujie/mesa

commit 501a24c7f6437fc71f1290861de3549ebfd0d151

zink: fix dynamic bo lowering for ssbo stores
for stores, there is no dest, so the pass needs to just iterate over the
indices and dump a bunch of conditionals

there are no piglit tests for this, so naturally it would not have worked

fixes KHR-GL46.compute_shader.resources-max

Fixes: 902b3182133 ("zink: break up dynamic access lowering")

src/gallium/drivers/zink/nir_lower_dynamic_bo_access.c
@@ -45,12 +45,10 @@ static nir_ssa_def *
 recursive_generate_bo_ssa_def(nir_builder *b, nir_intrinsic_instr *instr, nir_ssa_def *index, unsigned start, unsigned end)
 {
    if (start == end - 1) {
-      /* block index src is 1 for this op */
-      unsigned block_idx = instr->intrinsic == nir_intrinsic_store_ssbo;
       nir_intrinsic_instr *new_instr = nir_intrinsic_instr_create(b->shader, instr->intrinsic);
-      new_instr->src[block_idx] = nir_src_for_ssa(nir_imm_int(b, start));
+      new_instr->src[0] = nir_src_for_ssa(nir_imm_int(b, start));
       for (unsigned i = 0; i < nir_intrinsic_infos[instr->intrinsic].num_srcs; i++) {
-         if (i != block_idx)
+         if (i)
             nir_src_copy(&new_instr->src[i], &instr->src[i], &new_instr->instr);
       }
       if (instr->intrinsic != nir_intrinsic_load_ubo_vec4) {
@@ -59,10 +57,9 @@ recursive_generate_bo_ssa_def(nir_builder *b, nir_intrinsic_instr *instr, nir_ss
             nir_intrinsic_set_range(new_instr, nir_intrinsic_range(instr));
       }
       new_instr->num_components = instr->num_components;
-      if (instr->intrinsic != nir_intrinsic_store_ssbo)
-         nir_ssa_dest_init(&new_instr->instr, &new_instr->dest,
-                           nir_dest_num_components(instr->dest),
-                           nir_dest_bit_size(instr->dest), NULL);
+      nir_ssa_dest_init(&new_instr->instr, &new_instr->dest,
+                        nir_dest_num_components(instr->dest),
+                        nir_dest_bit_size(instr->dest), NULL);
       nir_builder_instr_insert(b, &new_instr->instr);
       return &new_instr->dest.ssa;
    }
@@ -73,6 +70,21 @@ recursive_generate_bo_ssa_def(nir_builder *b, nir_intrinsic_instr *instr, nir_ss
       recursive_generate_bo_ssa_def(b, instr, index, mid, end),
       NULL
    );
+}
+
+static void
+generate_store_ssbo_ssa_def(nir_builder *b, nir_intrinsic_instr *instr, nir_ssa_def *index, nir_ssa_def *idx)
+{
+   nir_intrinsic_instr *new_instr = nir_intrinsic_instr_create(b->shader, instr->intrinsic);
+   new_instr->src[1] = nir_src_for_ssa(idx);
+   for (unsigned i = 0; i < nir_intrinsic_infos[instr->intrinsic].num_srcs; i++) {
+      if (i != 1)
+         nir_src_copy(&new_instr->src[i], &instr->src[i], &new_instr->instr);
+   }
+   nir_intrinsic_set_align(new_instr, nir_intrinsic_align_mul(instr), nir_intrinsic_align_offset(instr));
+   nir_intrinsic_set_write_mask(new_instr, 0xff);
+   new_instr->num_components = instr->num_components;
+   nir_builder_instr_insert(b, &new_instr->instr);
 }
 
 static bool
@@ -105,18 +117,26 @@ lower_dynamic_bo_access_instr(nir_intrinsic_instr *instr, nir_builder *b)
       last_idx = first_idx + b->shader->info.num_ubos;
    }
 
-   /* now create the composite dest with a bcsel chain based on the original value */
-   nir_ssa_def *new_dest = recursive_generate_bo_ssa_def(b, instr,
-                                                       instr->src[block_idx].ssa,
-                                                       first_idx, last_idx);
+   if (instr->intrinsic != nir_intrinsic_store_ssbo) {
+      /* now create the composite dest with a bcsel chain based on the original value */
+      nir_ssa_def *new_dest = recursive_generate_bo_ssa_def(b, instr,
+                                                          instr->src[block_idx].ssa,
+                                                          first_idx, last_idx);
 
-   if (instr->intrinsic != nir_intrinsic_store_ssbo)
       /* now use the composite dest in all cases where the original dest (from the dynamic index)
        * was used and remove the dynamically-indexed load_*bo instruction
        */
       nir_ssa_def_rewrite_uses_after(&instr->dest.ssa, nir_src_for_ssa(new_dest), &instr->instr);
-
+   } else {
+      for (unsigned i = first_idx; i < last_idx; i++) {
+         nir_ssa_def *idx = nir_imm_int(b, i);
+         nir_if *if_cond = nir_push_if(b, nir_build_alu(b, nir_op_ieq, instr->src[block_idx].ssa, idx, NULL, NULL));
+         generate_store_ssbo_ssa_def(b, instr, instr->src[block_idx].ssa, idx);
+         nir_pop_if(b, if_cond);
+      }
+   }
    nir_instr_remove(&instr->instr);
+
    return true;
 }
 