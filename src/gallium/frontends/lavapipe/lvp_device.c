ec764ebdd9573538621fa4a0b76734175eaef205
Dave Airlie <airlied@redhat.com>
2021-02-17 17:06:39 -0800

    lavapipe: fix some void ptr arithmetic
    msvc disagrees with it, and they are trivial to fix.

--- src/gallium/frontends/lavapipe/lvp_device.c
+++ src/gallium/frontends/lavapipe/lvp_device.c
@@ -1063,7 +1063,7 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_MapMemory(
 
    map = device->pscreen->map_memory(device->pscreen, mem->pmem);
 
-   *ppData = map + offset;
+   *ppData = (char *)map + offset;
    return VK_SUCCESS;
 }
 
