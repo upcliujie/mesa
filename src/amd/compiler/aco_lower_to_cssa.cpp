fbf929672f03da4a60cba398ba6e71c52e75a986
Daniel Schürmann <daniel@schuermann.dev>
2021-02-19 15:30:04 +0100

    aco/cssa: rewrite lower_to_cssa pass (3)

--- src/amd/compiler/aco_lower_to_cssa.cpp
+++ src/amd/compiler/aco_lower_to_cssa.cpp
@@ -22,7 +22,6 @@
  *
  */
 
-#include <map>
 #include <unordered_map>
 #include "aco_ir.h"
 #include "aco_builder.h"
@@ -42,8 +41,6 @@
 namespace aco {
 namespace {
 
-typedef std::map<uint32_t, std::vector<std::pair<Definition, Operand>>> phi_info;
-
 typedef std::vector<Temp> merge_set;
 
 struct copy {
@@ -60,8 +57,6 @@ struct merge_node {
 struct cssa_ctx {
    Program* program;
    live& live_vars;
-   phi_info logical_phi_info {};
-   phi_info linear_phi_info {};
    std::vector<std::vector<copy>> parallelcopies; // copies per block
    std::vector<merge_set> merge_sets; // each vector is one (ordered) merge set
    std::unordered_map<uint32_t, merge_node> merge_node_table; // tempid -> merge set index
@@ -397,161 +392,14 @@ void emit_parallelcopies(cssa_ctx& ctx)
    assert(renames.empty());
 }
 
-bool collect_phi_info(cssa_ctx& ctx)
-{
-   bool progress = false;
-   for (Block& block : ctx.program->blocks) {
-      for (aco_ptr<Instruction>& phi : block.instructions) {
-         bool is_logical;
-         if (phi->opcode == aco_opcode::p_phi)
-            is_logical = true;
-         else if (phi->opcode == aco_opcode::p_linear_phi)
-            is_logical = false;
-         else
-            break;
-
-         /* no CSSA for the exec mask as we don't spill it anyway */
-         if (phi->definitions[0].isFixed() && phi->definitions[0].physReg() == exec)
-            continue;
-         std::vector<unsigned>& preds = is_logical ? block.logical_preds : block.linear_preds;
-
-         /* collect definition's block per Operand */
-         std::vector<unsigned> def_points(phi->operands.size());
-         for (unsigned i = 0; i < phi->operands.size(); i++) {
-            Operand& op = phi->operands[i];
-            if (op.isUndefined()) {
-               def_points[i] = preds[i];
-            } else if (op.isConstant()) {
-               /* in theory, we could insert the definition there... */
-               def_points[i] = 0;
-            } else if (op.isTemp()) {
-               unsigned pred = preds[i];
-               do {
-                  def_points[i] = pred;
-                  pred = is_logical ?
-                         ctx.program->blocks[pred].logical_idom :
-                         ctx.program->blocks[pred].linear_idom;
-               } while (def_points[i] != pred &&
-                        ctx.live_vars.live_out[pred].count(op.tempId()));
-            } else {
-               /* no need to insert a copy of the exec mask */
-               assert(op.isFixed() && op.physReg() == exec);
-               def_points[i] = preds[i];
-            }
-         }
-
-         /* check live-range intersections */
-         for (unsigned i = 0; i < phi->operands.size(); i++) {
-            Operand op = phi->operands[i];
-            if (op.isUndefined())
-               continue;
-            /* check if the operand comes from the exec mask of a predecessor */
-            if (op.isFixed() && op.physReg() == exec)
-               continue;
-
-            bool interferes = false;
-            unsigned idom = is_logical ?
-                            ctx.program->blocks[def_points[i]].logical_idom :
-                            ctx.program->blocks[def_points[i]].linear_idom;
-            /* live-through operands definitely interfere */
-            if (op.isTemp() && !op.isKill()) {
-               interferes = true;
-            /* create copies for constants to ease spilling */
-            } else if (op.isConstant()) {
-               interferes = true;
-            /* create copies for SGPR -> VGPR moves */
-            } else if (op.regClass() != phi->definitions[0].regClass()) {
-               interferes = true;
-            /* operand might interfere with any phi-def*/
-            } else if (def_points[i] == block.index) {
-               interferes = true;
-            /* operand might interfere with phi-def */
-            } else if (ctx.live_vars.live_out[idom].count(phi->definitions[0].tempId())) {
-               interferes = true;
-            /* else check for interferences with other operands */
-            } else {
-               for (unsigned j = 0; !interferes && j < phi->operands.size(); j++) {
-                  /* don't care about other register classes */
-                  if (!phi->operands[j].isTemp() || phi->operands[j].regClass() != phi->definitions[0].regClass())
-                     continue;
-                  /* same operands cannot interfere */
-                  if (op.getTemp() == phi->operands[j].getTemp())
-                     continue;
-                  /* if def_points[i] dominates any other def_point, assume they interfere.
-                   * As live-through operands are checked above, only test up the current block. */
-                  unsigned other_def_point = def_points[j];
-                  while (def_points[i] < other_def_point && other_def_point != block.index)
-                     other_def_point = is_logical ?
-                                       ctx.program->blocks[other_def_point].logical_idom :
-                                       ctx.program->blocks[other_def_point].linear_idom;
-                  interferes = def_points[i] == other_def_point;
-               }
-            }
-
-            if (!interferes)
-               continue;
-
-            progress = true;
-
-            /* create new temporary and rename operands */
-            Temp new_tmp = ctx.program->allocateTmp(phi->definitions[0].regClass());
-            if (is_logical)
-               ctx.logical_phi_info[preds[i]].emplace_back(Definition(new_tmp), op);
-            else
-               ctx.linear_phi_info[preds[i]].emplace_back(Definition(new_tmp), op);
-            phi->operands[i] = Operand(new_tmp);
-            phi->operands[i].setKill(true);
-            def_points[i] = preds[i];
-         }
-      }
-   }
-   return progress;
-}
-
-void insert_parallelcopies(cssa_ctx& ctx)
-{
-   /* insert the parallelcopies from logical phis before p_logical_end */
-   for (auto&& entry : ctx.logical_phi_info) {
-      Block& block = ctx.program->blocks[entry.first];
-      unsigned idx = block.instructions.size() - 1;
-      while (block.instructions[idx]->opcode != aco_opcode::p_logical_end) {
-         assert(idx > 0);
-         idx--;
-      }
-
-      Builder bld(ctx.program);
-      bld.reset(&block.instructions, std::next(block.instructions.begin(), idx));
-      for (std::pair<Definition, Operand>& pair : entry.second)
-         bld.pseudo(aco_opcode::p_parallelcopy, pair.first, pair.second);
-   }
-
-   /* insert parallelcopies for the linear phis at the end of blocks just before the branch */
-   for (auto&& entry : ctx.linear_phi_info) {
-      Block& block = ctx.program->blocks[entry.first];
-      std::vector<aco_ptr<Instruction>>::iterator it = block.instructions.end();
-      --it;
-      assert((*it)->isBranch());
-
-      Builder bld(ctx.program);
-      bld.reset(&block.instructions, it);
-      for (std::pair<Definition, Operand>& pair : entry.second)
-         bld.pseudo(aco_opcode::p_parallelcopy, pair.first, pair.second);
-   }
-}
-
 } /* end namespace */
 
 
 void lower_to_cssa(Program* program, live& live_vars)
 {
    cssa_ctx ctx = {program, live_vars};
-   /* collect information about all interfering phi operands */
-   bool progress = collect_phi_info(ctx);
-
-   if (!progress)
-      return;
-
-   insert_parallelcopies(ctx);
+   collect_parallelcopies(ctx);
+   emit_parallelcopies(ctx);
 
    /* update live variable information */
    live_vars = live_var_analysis(program);
