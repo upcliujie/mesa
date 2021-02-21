f4edae914881fa9b9d7d718b714a9de5abae0eb1
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-02-21 12:04:20 -0500

    zink: set conformant ubo/ssbo size limits
    the vulkan spec only says that these sizes must be >0 and imposes no other
    limitations on sizes, nor is there any mechanism by which to detect a driver's
    hardcoded max size
    
    u_screen sets ssbo max size to 1<<27 (and weird clamping occurs above that), while
    1<<31 seems to be a reasonable maximum for ubos based on reading some vk driver values
    
    fixes KHR-GL46.shader_storage_buffer_object.basic-max

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -430,7 +430,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return 0;
 
    case PIPE_CAP_MAX_SHADER_BUFFER_SIZE:
-      return 65536;
+      return 1 << 27;
 
    case PIPE_CAP_TGSI_FS_COORD_ORIGIN_UPPER_LEFT:
    case PIPE_CAP_TGSI_FS_COORD_PIXEL_CENTER_HALF_INTEGER:
@@ -596,7 +596,7 @@ zink_get_shader_param(struct pipe_screen *pscreen,
    }
 
    case PIPE_SHADER_CAP_MAX_CONST_BUFFER_SIZE:
-      return 65536;
+      return 1 << 31;
 
    case PIPE_SHADER_CAP_MAX_CONST_BUFFERS:
       return  MIN2(screen->info.props.limits.maxPerStageDescriptorUniformBuffers,
