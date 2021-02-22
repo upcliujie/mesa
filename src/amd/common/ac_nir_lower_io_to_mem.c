e8948462926d24591980475f79564bbd77dc7f50
Timur Kristóf <timur.kristof@gmail.com>
2021-02-22 20:38:06 +0100

    ac: Add NIR lowering of ES->GS I/O.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/amd/common/ac_nir_lower_io_to_mem.c
+++ src/amd/common/ac_nir_lower_io_to_mem.c
@@ -773,3 +773,171 @@ ac_nir_lower_tess_to_const(nir_shader *shader,
                                  lower_tess_intrinsics_to_const,
                                  &st);
 }
+
+typedef struct {
+   /* Which hardware generation we're dealing with */
+   enum chip_class chip_class;
+
+   /* Number of ES outputs for which memory should be reserved.
+    * When compacted, this should be the number of linked ES outputs.
+    */
+   unsigned num_reserved_es_outputs;
+} lower_esgs_io_state;
+
+static bool
+lower_es_output_store(nir_builder *b,
+                      nir_instr *instr,
+                      void *state)
+{
+   if (!filter_store_output(instr, state))
+      return false;
+
+   b->cursor = nir_before_instr(instr);
+
+   lower_esgs_io_state *st = (lower_esgs_io_state *) state;
+   nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+
+   unsigned write_mask = nir_intrinsic_write_mask(intrin);
+   nir_ssa_def *io_off = offset_from_io_intrinsic_with_stride(b, intrin, 4u, 4u);
+
+   if (st->chip_class <= GFX8) {
+      /* GFX6-8: ES is a separate HW stage, data is passed from ES to GS in VRAM. */
+      nir_ssa_def *ring = nir_build_load_ring_esgs_gcn(b);
+      nir_ssa_def *es2gs_off = nir_build_load_ring_es2gs_offset_gcn(b);
+      nir_build_store_mubuf_gcn(b, intrin->src[0].ssa, io_off, ring, es2gs_off,
+                                .stride = 4u, .gcn_slc = true, .memory_modes = nir_var_shader_out);
+   } else {
+      /* GFX9+: ES is merged into GS, data is passed through LDS. */
+      unsigned esgs_itemsize = st->num_reserved_es_outputs * 16u;
+      nir_ssa_def *vertex_idx = nir_build_load_local_invocation_index(b);
+      nir_ssa_def *off = nir_iadd(b, nir_umul24_imm(b, vertex_idx, esgs_itemsize), io_off);
+      nir_build_store_shared(b, intrin->src[0].ssa, off, .write_mask = write_mask, .align_mul = 16u);
+   }
+
+   nir_instr_remove(instr);
+   return true;
+}
+
+static bool
+filter_load_gs_per_vertex_input(const nir_instr *instr,
+                                UNUSED const void *state)
+{
+   if (instr->type != nir_instr_type_intrinsic)
+      return false;
+
+   nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+   return intrin->intrinsic == nir_intrinsic_load_per_vertex_input;
+}
+
+static nir_ssa_def *
+gs_per_vertex_input_vertex_offset_gfx6(nir_builder *b, nir_src *vertex_src)
+{
+   /* TODO: gs_vtx_offset works slightly differently between RADV and RadeonSI. This is only for RADV. */
+
+   if (nir_src_is_const(*vertex_src))
+      return nir_build_load_gs_vertex_offset_gcn(b, .base = nir_src_as_uint(*vertex_src));
+
+   nir_ssa_def *vertex_offset = nir_build_load_gs_vertex_offset_gcn(b, .base = 0);
+
+   for (unsigned i = 1; i < b->shader->info.gs.vertices_in; ++i) {
+      nir_ssa_def *cond = nir_ieq_imm(b, vertex_src->ssa, i);
+      nir_ssa_def *elem = nir_build_load_gs_vertex_offset_gcn(b, .base = i);
+      vertex_offset = nir_bcsel(b, cond, elem, vertex_offset);
+   }
+
+   return vertex_offset;
+}
+
+static nir_ssa_def *
+gs_per_vertex_input_vertex_offset_gfx9(nir_builder *b, nir_src *vertex_src)
+{
+   /* TODO: gs_vtx_offset works slightly differently between RADV and RadeonSI. This is only for RADV. */
+
+   if (nir_src_is_const(*vertex_src)) {
+      unsigned vertex = nir_src_as_uint(*vertex_src);
+      return nir_ubfe(b, nir_build_load_gs_vertex_offset_gcn(b, .base = vertex / 2u * 2u),
+                      nir_imm_int(b, (vertex % 2u) * 16u), nir_imm_int(b, 16u));
+   }
+
+   nir_ssa_def *vertex_offset = nir_build_load_gs_vertex_offset_gcn(b, .base = 0);
+
+   for (unsigned i = 1; i < b->shader->info.gs.vertices_in; i++) {
+      nir_ssa_def *cond = nir_ieq_imm(b, vertex_src->ssa, i);
+      nir_ssa_def *elem = nir_build_load_gs_vertex_offset_gcn(b, .base = i / 2u * 2u);
+      if (i % 2u)
+         elem = nir_ishr_imm(b, elem, 16u);
+
+      vertex_offset = nir_bcsel(b, cond, elem, vertex_offset);
+   }
+
+   return nir_iand_imm(b, vertex_offset, 0xffffu);
+}
+
+static nir_ssa_def *
+gs_per_vertex_input_offset(nir_builder *b,
+                           lower_esgs_io_state *st,
+                           nir_intrinsic_instr *instr)
+{
+   nir_src *vertex_src = nir_get_io_vertex_index_src(instr);
+   nir_ssa_def *vertex_offset = st->chip_class >= GFX9
+                                ? gs_per_vertex_input_vertex_offset_gfx9(b, vertex_src)
+                                : gs_per_vertex_input_vertex_offset_gfx6(b, vertex_src);
+
+   unsigned base_stride = st->chip_class >= GFX9 ? 1 : 64 /* Wave size on GFX6-8 */;
+   nir_ssa_def *io_off = offset_from_io_intrinsic_with_stride(b, instr, base_stride, base_stride);
+   nir_ssa_def *off = nir_iadd(b, io_off, vertex_offset);
+   return nir_umul24_imm(b, off, 4u);
+}
+
+static nir_ssa_def *
+lower_gs_per_vertex_input_load(nir_builder *b,
+                               nir_instr *instr,
+                               void *state)
+{
+   lower_esgs_io_state *st = (lower_esgs_io_state *) state;
+   nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+   nir_ssa_def *off = gs_per_vertex_input_offset(b, st, intrin);
+
+   if (st->chip_class >= GFX9)
+      return nir_build_load_shared(b, intrin->dest.ssa.num_components, intrin->dest.ssa.bit_size, off, .align_mul = 16u);
+
+   nir_ssa_def *ring = nir_build_load_ring_esgs_gcn(b);
+   nir_ssa_def *scalar_offset = nir_ssa_undef(b, 1, 32); /* Scalar offset not needed here. */
+   unsigned wave_size = 64u; /* GFX6-8 only support wave64 */
+
+   return nir_build_load_mubuf_gcn(b, intrin->dest.ssa.num_components, intrin->dest.ssa.bit_size,
+                                   ring, off, scalar_offset,
+                                   .stride = 4u * wave_size, .memory_modes = nir_var_shader_in);
+}
+
+void
+ac_nir_lower_es_outputs_to_mem(nir_shader *shader,
+                               enum chip_class chip_class,
+                               unsigned num_reserved_es_outputs)
+{
+   lower_esgs_io_state state = {
+      .chip_class = chip_class,
+      .num_reserved_es_outputs = num_reserved_es_outputs,
+   };
+
+   nir_shader_instructions_pass(shader,
+                                lower_es_output_store,
+                                nir_metadata_all,
+                                &state);
+}
+
+void
+ac_nir_lower_gs_inputs_to_mem(nir_shader *shader,
+                              enum chip_class chip_class,
+                              unsigned num_reserved_es_outputs)
+{
+   lower_esgs_io_state state = {
+      .chip_class = chip_class,
+      .num_reserved_es_outputs = num_reserved_es_outputs,
+   };
+
+   nir_shader_lower_instructions(shader,
+                                 filter_load_gs_per_vertex_input,
+                                 lower_gs_per_vertex_input_load,
+                                 &state);
+}
