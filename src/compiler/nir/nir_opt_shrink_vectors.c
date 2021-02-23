Grima04/mesa-turnip-kgsl

commit e4d75c22beba5533b499f7f8f8be6ab0a110ecbc

nir/opt_shrink_vectors: shrink image stores using the format
fossil-db (Navi):
Totals from 657 (0.48% of 135946) affected shaders:
VGPRs: 26076 -> 25520 (-2.13%); split: -2.15%, +0.02%
CodeSize: 3033016 -> 3014472 (-0.61%); split: -0.64%, +0.03%
MaxWaves: 9386 -> 9420 (+0.36%)
Instrs: 590109 -> 585502 (-0.78%); split: -0.82%, +0.04%

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5772>

src/compiler/nir/nir_opt_shrink_vectors.c
@@ -110,6 +110,30 @@ opt_shrink_vectors_alu(nir_builder *b, nir_alu_instr *instr)
 }
 
 static bool
+opt_shrink_vectors_image_store(nir_builder *b, nir_intrinsic_instr *instr)
+{
+   enum pipe_format format;
+   if (instr->intrinsic == nir_intrinsic_image_deref_store) {
+      nir_deref_instr *deref = nir_src_as_deref(instr->src[0]);
+      format = nir_deref_instr_get_variable(deref)->data.image.format;
+   } else {
+      format = nir_intrinsic_format(instr);
+   }
+   if (format == PIPE_FORMAT_NONE)
+      return false;
+
+   unsigned components = util_format_get_nr_components(format);
+   if (components >= instr->num_components)
+      return false;
+
+   nir_ssa_def *data = nir_channels(b, instr->src[3].ssa, BITSET_MASK(components));
+   nir_instr_rewrite_src(&instr->instr, &instr->src[3], nir_src_for_ssa(data));
+   instr->num_components = components;
+
+   return true;
+}
+
+static bool
 opt_shrink_vectors_intrinsic(nir_builder *b, nir_intrinsic_instr *instr)
 {
    switch (instr->intrinsic) {
@@ -133,6 +157,10 @@ opt_shrink_vectors_intrinsic(nir_builder *b, nir_intrinsic_instr *instr)
    case nir_intrinsic_store_global:
    case nir_intrinsic_store_scratch:
       break;
+   case nir_intrinsic_bindless_image_store:
+   case nir_intrinsic_image_deref_store:
+   case nir_intrinsic_image_store:
+      return opt_shrink_vectors_image_store(b, instr);
    default:
       return false;
    }

upcliujie/mesa

commit 542cdfa96650602dbc8ccd7040c63bdf0edf6d00

nir/opt_shrink_vectors: avoid shrinking image stores with wrong format
The Surge 2 and old versions of Shadow of the Tomb Raider specify a format
with fewer components than they actually pass to the image store.
Workaround this by assuming they gave the wrong format by looking for the
last component that does not match the first.

fossil-db (GFX10.3):
Totals from 383 (0.26% of 146267) affected shaders:
VGPRs: 19960 -> 20048 (+0.44%)
CodeSize: 2878428 -> 2885360 (+0.24%); split: -0.01%, +0.25%
Instrs: 546654 -> 547963 (+0.24%); split: -0.01%, +0.24%
VClause: 9290 -> 9322 (+0.34%); split: -0.01%, +0.36%
SClause: 18736 -> 18738 (+0.01%)
Copies: 28967 -> 30195 (+4.24%); split: -0.06%, +4.30%
Branches: 14235 -> 14245 (+0.07%)
PreVGPRs: 17726 -> 17746 (+0.11%)
Cycles: 6870512 -> 6876240 (+0.08%); split: -0.02%, +0.11%
VMEM: 52821 -> 52986 (+0.31%); split: +0.33%, -0.02%
SMEM: 19525 -> 19773 (+1.27%); split: +1.31%, -0.04%

I think some of the regressions are fine actually, but we can't know
for certain. This optimization still seems beneficial for some shaders
at least.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Fixes: e4d75c22beb ("nir/opt_shrink_vectors: shrink image stores using the format")

src/compiler/nir/nir_opt_shrink_vectors.c
@@ -123,6 +123,25 @@ opt_shrink_vectors_image_store(nir_builder *b, nir_intrinsic_instr *instr)
       return false;
 
    unsigned components = util_format_get_nr_components(format);
+
+   /* workaround game bugs where the format is too small */
+   nir_alu_instr *vec = nir_src_as_alu_instr(instr->src[3]);
+   if (vec && vec->op == nir_op_vec(instr->num_components)) {
+      for (unsigned i = 1; i < instr->num_components; i++) {
+         if ((vec->src[i].src.ssa != vec->src[0].src.ssa ||
+              vec->src[i].swizzle[0] != vec->src[0].swizzle[0]) &&
+             vec->src[i].src.ssa->parent_instr->type != nir_instr_type_ssa_undef)
+            components = MAX2(components, i + 1);
+      }
+   } else if (nir_src_is_const(instr->src[3])) {
+      for (unsigned i = 1; i < instr->num_components; i++) {
+         if (nir_src_comp_as_uint(instr->src[3], i) != nir_src_comp_as_uint(instr->src[3], 0))
+            components = MAX2(components, i + 1);
+      }
+   } else {
+      return false;
+   }
+
    if (components >= instr->num_components)
       return false;
 