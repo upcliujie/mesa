Grima04/mesa-turnip-kgsl

commit b6f8f3a3ba4739338c75bf39f9ec3f77e20f544e

zink: fix streamout for clipdistance
the assert added here during review broke this since clipdistance is 8 components

Fixes: 9eec52c67e6 ("zink: tweak xfb slot mapping in ntv")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8673>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -1249,7 +1249,7 @@ emit_so_outputs(struct ntv_context *ctx,
       while (!output)
          output = ctx->outputs[location--];
       location++;
-      assert(orig_location - location < 4);
+      assert(orig_location - location < 8);
       SpvId output_type = ctx->so_output_types[location];
       const struct glsl_type *out_type = ctx->so_output_gl_types[location];
 

upcliujie/mesa

commit f7a20503c339cdd1265145a46a16990257c6521a

zink: remove ntv streamout assert
this was added during review, but it was never correct and just crashes
valid cases like streamout from a mat3x4 type

Fixes: b6f8f3a3ba4 ("zink: fix streamout for clipdistance")

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -1304,7 +1304,6 @@ emit_so_outputs(struct ntv_context *ctx,
       while (!output)
          output = ctx->outputs[location--];
       location++;
-      assert(orig_location - location < 8);
       SpvId output_type = ctx->so_output_types[location];
       const struct glsl_type *out_type = ctx->so_output_gl_types[location];
 