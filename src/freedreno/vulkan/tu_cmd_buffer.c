5983aee97b44927f95295b5fbb54c952dd00870c
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-03-01 17:31:56 +0200

    turnip,ir3: account for dispatch group offsets
    Fixes tests:
     dEQP-VK.compute.device_group.dispatch_base
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/freedreno/vulkan/tu_cmd_buffer.c
+++ src/freedreno/vulkan/tu_cmd_buffer.c
@@ -3809,10 +3809,13 @@ tu_emit_compute_driver_params(struct tu_cmd_buffer *cmd,
       return;
 
    if (!info->indirect) {
-      uint32_t driver_params[4] = {
+      uint32_t driver_params[8] = {
          [IR3_DP_NUM_WORK_GROUPS_X] = info->blocks[0],
          [IR3_DP_NUM_WORK_GROUPS_Y] = info->blocks[1],
          [IR3_DP_NUM_WORK_GROUPS_Z] = info->blocks[2],
+         [IR3_DP_BASE_GROUP_X] = info->offsets[0],
+         [IR3_DP_BASE_GROUP_Y] = info->offsets[1],
+         [IR3_DP_BASE_GROUP_Z] = info->offsets[2],
       };
 
       uint32_t num_consts = MIN2(const_state->num_driver_params,
@@ -3863,6 +3866,18 @@ tu_emit_compute_driver_params(struct tu_cmd_buffer *cmd,
                   CP_LOAD_STATE6_0_STATE_BLOCK(tu6_stage2shadersb(type)) |
                   CP_LOAD_STATE6_0_NUM_UNIT(1));
       tu_cs_emit_qw(cs, global_iova(cmd, cs_indirect_xyz[0]));
+   }
+
+   /* Zeroing of IR3_DP_BASE_GROUP_X/Y/Z for indirect dispatch */
+   if (info->indirect) {
+      tu_cs_emit_pkt7(cs, tu6_stage2opcode(type), 6);
+      tu_cs_emit(cs, CP_LOAD_STATE6_0_DST_OFF(offset + (IR3_DP_BASE_GROUP_X / 4)) |
+                 CP_LOAD_STATE6_0_STATE_TYPE(ST6_CONSTANTS) |
+                 CP_LOAD_STATE6_0_STATE_SRC(SS6_DIRECT) |
+                 CP_LOAD_STATE6_0_STATE_BLOCK(tu6_stage2shadersb(type)) |
+                 CP_LOAD_STATE6_0_NUM_UNIT(1));
+      for (uint32_t i = 0; i < 5; i++)
+         tu_cs_emit(cs, 0);
    }
 }
 
