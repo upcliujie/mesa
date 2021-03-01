Grima04/mesa-turnip-kgsl

commit 4d47b22bf007413274cabac264122ab7e428fe7d

glsl/float64: make this compatible with glsl 330
just a minor tweak

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7483>

src/compiler/glsl/float64.glsl
@@ -40,7 +40,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#version 430
+#version 330
 #extension GL_ARB_gpu_shader_int64 : enable
 #extension GL_ARB_shader_bit_encoding : enable
 #extension GL_EXT_shader_integer_mix : enable
@@ -1655,7 +1655,7 @@ __fround64(uint64_t __a)
    if (unbiasedExp < 20) {
       if (unbiasedExp < 0) {
          if ((aHi & 0x80000000u) != 0u && aLo == 0u) {
-            return 0;
+            return 0ul;
          }
          aHi &= 0x80000000u;
          if ((a.y & 0x000FFFFFu) == 0u && a.x == 0u) {

upcliujie/mesa

commit 818e37dfbbf2441c0f9c1308d50da4a383fe1925

glsl/float64: Bump #version to 400
An earlier commit tried to make this shader compatible with GLSL 3.30,
but it requires, GL_ARB_gpu_shader_int64, which requires GLSL 4.00 and
GL 4.0 according to the extension spec.  So we were failing to enable
the required extension, breaking compilation of this shader.

The original intention of that patch was to get this working on zink,
which at the time only supported GL 3.3.  But now it supports later
OpenGL versions, so we don't need to do this any longer.  Rather than
revert the patch and raise the version all the way back to 430, just
bump it to the require 400 at Ian Romanick's suggestion.

Fixes: 4d47b22bf00 ("glsl/float64: make this compatible with glsl 330")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3991

src/compiler/glsl/float64.glsl
@@ -40,7 +40,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#version 330
+#version 400
 #extension GL_ARB_gpu_shader_int64 : enable
 #extension GL_ARB_shader_bit_encoding : enable
 #extension GL_EXT_shader_integer_mix : enable