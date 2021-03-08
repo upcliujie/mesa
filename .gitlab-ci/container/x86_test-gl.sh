a193cb5befc64860591fca5a791986bc3d1f618c
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-03-08 14:05:08 +0100

    kernel

--- .gitlab-ci/container/x86_test-gl.sh
+++ .gitlab-ci/container/x86_test-gl.sh
@@ -9,9 +9,11 @@ export DEBIAN_FRONTEND=noninteractive
 STABLE_EPHEMERAL=" \
       autoconf \
       automake \
+      bison \
       ccache \
       clang-10 \
       cmake \
+      flex \
       g++ \
       libcap-dev \
       libclang-cpp10-dev \
@@ -56,6 +58,16 @@ apt-get install -y --no-remove \
 
 . .gitlab-ci/container/container_pre_build.sh
 
+
+############### Build kernel
+
+export DEFCONFIG="arch/x86/configs/x86_64_defconfig"
+export KERNEL_IMAGE_NAME=bzImage
+export KERNEL_ARCH=x86_64
+export DEBIAN_ARCH=amd64
+export KERNEL_URL="https://gitlab.freedesktop.org/tomeu/linux/-/archive/v5.10-rc2-for-mesa-ci/linux-v5.10-rc2-for-mesa-ci.tar.gz"
+
+. .gitlab-ci/container/build-kernel.sh
 
 ############### Build spirv-tools (debian too old)
 
