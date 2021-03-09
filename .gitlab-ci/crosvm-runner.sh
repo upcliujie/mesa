987dbd84a1cb0222a325cbc74e10301e67e36db8
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-03-09 13:51:40 +0100

    crosvm build

--- .gitlab-ci/crosvm-runner.sh
+++ .gitlab-ci/crosvm-runner.sh
@@ -2,10 +2,12 @@
 
 set -x
 
-export LD_LIBRARY_PATH=$CI_PROJECT_DIR/install/lib/
+ln -sf $CI_PROJECT_DIR/install /install
+
+export LD_LIBRARY_PATH=/install/lib/
 export EGL_PLATFORM=surfaceless
 
-export -p > $CI_PROJECT_DIR/install/crosvm-env.sh
+export -p > /install/crosvm-env.sh
 
 sleep 2
 
@@ -34,7 +36,7 @@ crosvm run \
   --disable-sandbox \
   --shared-dir /:my_root:type=fs:writeback=true:timeout=60:cache=always \
   --host_ip=192.168.30.1 --netmask=255.255.255.0 --mac "AA:BB:CC:00:00:12" \
-  -p "root=my_root rw rootfstype=virtiofs loglevel=3 init=$CI_PROJECT_DIR/install/crosvm-init.sh ip=192.168.30.2::192.168.30.1:255.255.255.0:crosvm:eth0" \
+  -p "root=my_root rw rootfstype=virtiofs loglevel=3 init=/install/crosvm-init.sh ip=192.168.30.2::192.168.30.1:255.255.255.0:crosvm:eth0" \
   /lava-files/bzImage
 
 ls -l $CI_PROJECT_DIR/results
