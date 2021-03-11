c7b016cd7f5727f36195c209c4b8312f00d4af34
Ian Romanick <ian.d.romanick@intel.com>
2021-03-10 19:49:05 -0800

    nir/lower_tex: Add support for lowering Y41x formats
    These are similar to AYUV, but the channel ordering is different... in
    such a way that there's no RGBA format that will make the channels line
    up right.
    
    v2: Rebase on bc438c91d9b ("nir/lower_tex: ignore texture_index if
    tex_instr has deref src")

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -4710,6 +4710,7 @@ typedef struct nir_lower_tex_options {
    unsigned lower_ayuv_external;
    unsigned lower_xyuv_external;
    unsigned lower_yuv_external;
+   unsigned lower_y41x_external;
    unsigned bt709_external;
    unsigned bt2020_external;
 
