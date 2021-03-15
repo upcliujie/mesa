317b8af751522255ce67a3b8941894cf4ac9f61c
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-03-15 19:38:50 +0100

    radv: implement dynamic rasterizer discard enable
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -1216,6 +1216,7 @@ struct radv_dynamic_state {
 
    bool depth_bias_enable;
    bool primitive_restart_enable;
+   bool rasterizer_discard_enable;
 };
 
 extern const struct radv_dynamic_state default_dynamic_state;
@@ -1722,6 +1723,7 @@ struct radv_pipeline {
          unsigned tess_patch_control_points;
          unsigned pa_su_sc_mode_cntl;
          unsigned db_depth_control;
+         unsigned pa_cl_clip_cntl;
          bool uses_dynamic_stride;
 
          /* Used for rbplus */
