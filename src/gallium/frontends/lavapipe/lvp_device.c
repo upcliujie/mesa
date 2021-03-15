577e4c3b0a1a1fd77a7a0c8b87423265a55980da
Dave Airlie <airlied@redhat.com>
2021-03-15 11:44:17 +1000

    lavapipe: enable KHR_shader_atomic_int64

--- src/gallium/frontends/lavapipe/lvp_device.c
+++ src/gallium/frontends/lavapipe/lvp_device.c
@@ -110,6 +110,7 @@ static const struct vk_device_extension_table lvp_device_extensions_supported = 
    .KHR_push_descriptor                   = true,
    .KHR_relaxed_block_layout              = true,
    .KHR_sampler_mirror_clamp_to_edge      = true,
+   .KHR_shader_atomic_int64               = true,
    .KHR_shader_draw_parameters            = true,
    .KHR_storage_buffer_storage_class      = true,
 #ifdef LVP_USE_WSI_PLATFORM
@@ -523,6 +524,12 @@ VKAPI_ATTR void VKAPI_CALL lvp_GetPhysicalDeviceFeatures2(
          VkPhysicalDeviceHostQueryResetFeaturesEXT *features =
             (VkPhysicalDeviceHostQueryResetFeaturesEXT *)ext;
          features->hostQueryReset = true;
+         break;
+      }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SHADER_ATOMIC_INT64_FEATURES_KHR: {
+         VkPhysicalDeviceShaderAtomicInt64FeaturesKHR *features = (void *)ext;
+         features->shaderBufferInt64Atomics = true;
+         features->shaderSharedInt64Atomics = true;
          break;
       }
       default:
