1cfa408ff8a8993492576dc5d7a30ff7601b4a4e
Alejandro Piñeiro <apinheiro@igalia.com>
2021-03-16 23:07:12 +0000

    v3dv: avoid some maybe-uninitialized warnings

--- src/broadcom/vulkan/v3dv_device.c
+++ src/broadcom/vulkan/v3dv_device.c
@@ -256,6 +256,7 @@ create_display_fd_xcb(VkIcdSurfaceBase *surface)
    int fd = -1;
 
    xcb_connection_t *conn;
+   xcb_dri3_open_reply_t *reply = NULL;
    if (surface) {
       if (surface->platform == VK_ICD_WSI_PLATFORM_XLIB)
          conn = XGetXCBConnection(((VkIcdSurfaceXlib *)surface)->dpy);
@@ -273,7 +274,6 @@ create_display_fd_xcb(VkIcdSurfaceBase *surface)
    xcb_screen_t *screen = iter.data;
 
    xcb_dri3_open_cookie_t cookie;
-   xcb_dri3_open_reply_t *reply;
    cookie = xcb_dri3_open(conn, screen->root, None);
    reply = xcb_dri3_open_reply(conn, cookie, NULL);
    if (!reply)
