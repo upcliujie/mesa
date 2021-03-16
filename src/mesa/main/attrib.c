Grima04/mesa-turnip-kgsl

commit 7fa9d9d06c44e9cf3d39b2ce5126bf1b0abff586

mesa: add a fast path for restoring light attributes in glPopAttrib
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6946>

src/mesa/main/attrib.c
@@ -879,50 +879,63 @@ _mesa_PopAttrib(void)
       if (_math_matrix_is_dirty(ctx->ModelviewMatrixStack.Top))
          _math_matrix_analyse(ctx->ModelviewMatrixStack.Top);
 
-      for (i = 0; i < ctx->Const.MaxLights; i++) {
-         const struct gl_light_uniforms *lu = &attr->Light.LightSource[i];
-         const struct gl_light *l = &attr->Light.Light[i];
-         _mesa_set_enable(ctx, GL_LIGHT0 + i, l->Enabled);
-         _mesa_light(ctx, i, GL_AMBIENT, lu->Ambient);
-         _mesa_light(ctx, i, GL_DIFFUSE, lu->Diffuse);
-         _mesa_light(ctx, i, GL_SPECULAR, lu->Specular);
-         _mesa_light(ctx, i, GL_POSITION, lu->EyePosition);
-         _mesa_light(ctx, i, GL_SPOT_DIRECTION, lu->SpotDirection);
-         {
-            GLfloat p[4] = { 0 };
-            p[0] = lu->SpotExponent;
-            _mesa_light(ctx, i, GL_SPOT_EXPONENT, p);
+      if (ctx->Driver.Lightfv) {
+         /* Legacy slow path for some classic drivers. */
+         for (i = 0; i < ctx->Const.MaxLights; i++) {
+            const struct gl_light_uniforms *lu = &attr->Light.LightSource[i];
+            const struct gl_light *l = &attr->Light.Light[i];
+            _mesa_set_enable(ctx, GL_LIGHT0 + i, l->Enabled);
+            _mesa_light(ctx, i, GL_AMBIENT, lu->Ambient);
+            _mesa_light(ctx, i, GL_DIFFUSE, lu->Diffuse);
+            _mesa_light(ctx, i, GL_SPECULAR, lu->Specular);
+            _mesa_light(ctx, i, GL_POSITION, lu->EyePosition);
+            _mesa_light(ctx, i, GL_SPOT_DIRECTION, lu->SpotDirection);
+            {
+               GLfloat p[4] = { 0 };
+               p[0] = lu->SpotExponent;
+               _mesa_light(ctx, i, GL_SPOT_EXPONENT, p);
+            }
+            {
+               GLfloat p[4] = { 0 };
+               p[0] = lu->SpotCutoff;
+               _mesa_light(ctx, i, GL_SPOT_CUTOFF, p);
+            }
+            {
+               GLfloat p[4] = { 0 };
+               p[0] = lu->ConstantAttenuation;
+               _mesa_light(ctx, i, GL_CONSTANT_ATTENUATION, p);
+            }
+            {
+               GLfloat p[4] = { 0 };
+               p[0] = lu->LinearAttenuation;
+               _mesa_light(ctx, i, GL_LINEAR_ATTENUATION, p);
+            }
+            {
+               GLfloat p[4] = { 0 };
+               p[0] = lu->QuadraticAttenuation;
+               _mesa_light(ctx, i, GL_QUADRATIC_ATTENUATION, p);
+            }
          }
-         {
-            GLfloat p[4] = { 0 };
-            p[0] = lu->SpotCutoff;
-            _mesa_light(ctx, i, GL_SPOT_CUTOFF, p);
-         }
-         {
-            GLfloat p[4] = { 0 };
-            p[0] = lu->ConstantAttenuation;
-            _mesa_light(ctx, i, GL_CONSTANT_ATTENUATION, p);
-         }
-         {
-            GLfloat p[4] = { 0 };
-            p[0] = lu->LinearAttenuation;
-            _mesa_light(ctx, i, GL_LINEAR_ATTENUATION, p);
-         }
-         {
-            GLfloat p[4] = { 0 };
-            p[0] = lu->QuadraticAttenuation;
-            _mesa_light(ctx, i, GL_QUADRATIC_ATTENUATION, p);
-         }
+         /* light model */
+         _mesa_LightModelfv(GL_LIGHT_MODEL_AMBIENT,
+                            attr->Light.Model.Ambient);
+         _mesa_LightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER,
+                           (GLfloat) attr->Light.Model.LocalViewer);
+         _mesa_LightModelf(GL_LIGHT_MODEL_TWO_SIDE,
+                           (GLfloat) attr->Light.Model.TwoSide);
+         _mesa_LightModelf(GL_LIGHT_MODEL_COLOR_CONTROL,
+                           (GLfloat) attr->Light.Model.ColorControl);
+      } else {
+         /* Fast path for other drivers. */
+         FLUSH_VERTICES(ctx, _NEW_LIGHT);
+
+         memcpy(ctx->Light.LightSource, attr->Light.LightSource,
+                sizeof(attr->Light.LightSource));
+         memcpy(&ctx->Light.Light, &attr->Light.Light,
+                sizeof(attr->Light.Light));
+         memcpy(&ctx->Light.Model, &attr->Light.Model,
+                sizeof(attr->Light.Model));
       }
-      /* light model */
-      _mesa_LightModelfv(GL_LIGHT_MODEL_AMBIENT,
-                         attr->Light.Model.Ambient);
-      _mesa_LightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER,
-                        (GLfloat) attr->Light.Model.LocalViewer);
-      _mesa_LightModelf(GL_LIGHT_MODEL_TWO_SIDE,
-                        (GLfloat) attr->Light.Model.TwoSide);
-      _mesa_LightModelf(GL_LIGHT_MODEL_COLOR_CONTROL,
-                        (GLfloat) attr->Light.Model.ColorControl);
       /* shade model */
       _mesa_ShadeModel(attr->Light.ShadeModel);
       /* color material */

upcliujie/mesa

commit 9f9adf4dfacb38b58187b9d171c3da967a3b9945

mesa: set states in fast path for restoring light attributes
Since states were not updated in fast path for restoring light attributes,
seen darker images in solidworks2012_viewprt.trace

Fixes regression seen with solidworks2012_viewport.trace

Fixes: 7fa9d9d06c4 ("mesa: add a fast path for restoring light attributes")

Reviewed-by: Brian Paul <brianp@vmware.com>

src/mesa/main/attrib.c
@@ -999,10 +999,16 @@ _mesa_PopAttrib(void)
 
          memcpy(ctx->Light.LightSource, attr->Light.LightSource,
                 sizeof(attr->Light.LightSource));
-         memcpy(&ctx->Light.Light, &attr->Light.Light,
-                sizeof(attr->Light.Light));
          memcpy(&ctx->Light.Model, &attr->Light.Model,
                 sizeof(attr->Light.Model));
+
+         for (i = 0; i < ctx->Const.MaxLights; i++) {
+            TEST_AND_UPDATE(ctx->Light.Light[i].Enabled,
+                            attr->Light.Light[i].Enabled,
+                            GL_LIGHT0 + i);
+            memcpy(&ctx->Light.Light[i], &attr->Light.Light[i],
+                   sizeof(struct gl_light));
+         }
       }
       /* shade model */
       TEST_AND_CALL1(Light.ShadeModel, ShadeModel);