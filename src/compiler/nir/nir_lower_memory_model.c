10cc636b1a2ba9373541a1e57dbc81e71ad74dc5
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2021-03-17 13:33:10 -0700

    SQUASH nir missing passes
    Added a note to make lower multiview use the info collected from
    gatehr_info
    
    we dont use nir_lower_memory_model in ANV, but I think given this is a
    src/compiler/nir pass, we should just ahndled there too.
    
    fadd was missing from both, so added it too.

--- src/compiler/nir/nir_lower_memory_model.c
+++ src/compiler/nir/nir_lower_memory_model.c
@@ -45,10 +45,13 @@ get_intrinsic_info(nir_intrinsic_instr *intrin, nir_variable_mode *modes,
       *writes = true;
       break;
    case nir_intrinsic_image_deref_atomic_add:
+   case nir_intrinsic_image_deref_atomic_fadd:
    case nir_intrinsic_image_deref_atomic_umin:
    case nir_intrinsic_image_deref_atomic_imin:
    case nir_intrinsic_image_deref_atomic_umax:
    case nir_intrinsic_image_deref_atomic_imax:
+   case nir_intrinsic_image_deref_atomic_fmin:
+   case nir_intrinsic_image_deref_atomic_fmax:
    case nir_intrinsic_image_deref_atomic_and:
    case nir_intrinsic_image_deref_atomic_or:
    case nir_intrinsic_image_deref_atomic_xor:
--- src/compiler/nir/nir_lower_multiview.c
+++ src/compiler/nir/nir_lower_multiview.c
@@ -89,10 +89,13 @@ shader_writes_to_memory(nir_shader *shader)
          case nir_intrinsic_shared_atomic_comp_swap:
          case nir_intrinsic_image_deref_store:
          case nir_intrinsic_image_deref_atomic_add:
+         case nir_intrinsic_image_deref_atomic_fadd:
          case nir_intrinsic_image_deref_atomic_umin:
          case nir_intrinsic_image_deref_atomic_umax:
          case nir_intrinsic_image_deref_atomic_imin:
          case nir_intrinsic_image_deref_atomic_imax:
+         case nir_intrinsic_image_deref_atomic_fmin:
+         case nir_intrinsic_image_deref_atomic_fmax:
          case nir_intrinsic_image_deref_atomic_and:
          case nir_intrinsic_image_deref_atomic_or:
          case nir_intrinsic_image_deref_atomic_xor:
