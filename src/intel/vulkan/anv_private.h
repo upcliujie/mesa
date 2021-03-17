ubc-aamodt-group/mesa-vulkan-sim

anv: Make anv_address::offset 64-bit
This allows us to convert a 64-bit address to an anv_address which is
useful for working with device addresses.

src/intel/vulkan/anv_private.h
@@ -1340,7 +1340,7 @@ anv_device_upload_nir(struct anv_device *device,
 
 struct anv_address {
    struct anv_bo *bo;
-   uint32_t offset;
+   uint64_t offset;
 };
 
 struct anv_device {
@@ -1730,6 +1730,16 @@ anv_batch_has_error(struct anv_batch *batch)
 
 #define ANV_NULL_ADDRESS ((struct anv_address) { NULL, 0 })
 
+static inline struct anv_address
+anv_address_from_u64(uint64_t addr_u64)
+{
+   assert(addr_u64 == gen_canonical_address(addr_u64));
+   return (struct anv_address) {
+      .bo = NULL,
+      .offset = addr_u64,
+   };
+}
+
 static inline bool
 anv_address_is_null(struct anv_address addr)
 {
@@ -1776,7 +1786,7 @@ _anv_combine_address(struct anv_batch *batch, void *location,
       return address.offset + delta;
    } else {
       assert(batch->start <= location && location < batch->end);
-
+      assert(address.offset < INT32_MAX);
       return anv_batch_emit_reloc(batch, location, address.bo, address.offset + delta);
    }
 }

upcliujie/mesa

src/intel/vulkan/anv_private.h
@@ -1176,7 +1176,7 @@ anv_device_upload_nir(struct anv_device *device,
 
 struct anv_address {
    struct anv_bo *bo;
-   uint32_t offset;
+   uint64_t offset;
 };
 
 struct anv_device {
@@ -1590,6 +1590,16 @@ anv_batch_has_error(struct anv_batch *batch)
 
 #define ANV_NULL_ADDRESS ((struct anv_address) { NULL, 0 })
 
+static inline struct anv_address
+anv_address_from_u64(uint64_t addr_u64)
+{
+   assert(addr_u64 == intel_canonical_address(addr_u64));
+   return (struct anv_address) {
+      .bo = NULL,
+      .offset = addr_u64,
+   };
+}
+
 static inline bool
 anv_address_is_null(struct anv_address addr)
 {
@@ -1639,7 +1649,7 @@ _anv_combine_address(struct anv_batch *batch, void *location,
       return anv_address_physical(anv_address_add(address, delta));
    } else {
       assert(batch->start <= location && location < batch->end);
-
+      assert(address.offset < INT32_MAX);
       return anv_batch_emit_reloc(batch, location, address.bo, address.offset + delta);
    }
 }