a146e7940498c8341c97e9c2fe43a99afe836e18
Antonio Caggiano <antonio.caggiano@collabora.com>
2021-03-18 18:30:45 +0100

    pps: Gfx-pps v0.3.0
    Add the gfx-pps backbone in `src/pps`.
    
    Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>

---
+++ src/tool/pps/pps_counter.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright © 2020 Collabora, Ltd.
+ * Author: Antonio Caggiano <antonio.caggiano@collabora.com>
+ * Author: Rohan Garg <rohan.garg@collabora.com>
+ * Author: Robert Beckett <bob.beckett@collabora.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <functional>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace pps
+{
+struct CounterGroup {
+   std::string name;
+
+   uint32_t id;
+
+   /// List of counters ID belonging to this group
+   std::vector<int32_t> counters;
+
+   std::vector<CounterGroup> subgroups;
+};
+
+class Driver;
+
+class Counter
+{
+   public:
+   /// @brief A counter value can be of different types depending on what it represents:
+   /// cycles, cycles-per-instruction, percentages, bytes, and so on.
+   enum class Units {
+      Percent,
+      Byte,
+      Hertz,
+      None,
+   };
+
+   using Value = std::variant<int64_t, double>;
+
+   /// @param c Counter which we want to retrieve a value
+   /// @param d Driver used to sample performance counters
+   /// @return The value of the counter
+   using Getter = Value(const Counter &c, const Driver &d);
+
+   /// @brief The default getter is used by non-derived counters to retrieve
+   /// their values from a device's performance counter memory dump
+   static Value default_getter(const Counter &c, const Driver &d);
+
+   Counter() = default;
+   virtual ~Counter() = default;
+
+   /// @param id ID of the counter
+   /// @param name Name of the counter
+   /// @param group Group ID this counter belongs to
+   Counter(int32_t id, const std::string &name, int32_t group);
+
+   bool operator==(const Counter &c) const;
+
+   /// @param get New getter function for this counter
+   void set_getter(const std::function<Getter> &get)
+   {
+      getter = get;
+   }
+
+   /// @brief d Driver used to sample performance counters
+   /// @return Last sampled value for this counter
+   Value get_value(const Driver &d) const
+   {
+      return getter(*this, d);
+   }
+
+   /// Id of the counter
+   int32_t id = -1;
+
+   /// Name of the counter
+   std::string name = "";
+
+   /// ID of the group this counter belongs to
+   int32_t group = -1;
+
+   /// Offset of this counter within GPU counter list
+   /// For derived counters it is negative and remains unused
+   int32_t offset = -1;
+
+   /// Whether it is a derived counter or not
+   bool derived = false;
+
+   /// Returns the value of this counter within counters memory
+   /// Derived counters must use getter different than default
+   std::function<Getter> getter = default_getter;
+
+   /// The unit of the counter
+   Units units;
+};
+
+/// @return The underlying u32 value
+template<typename T> constexpr uint32_t to_u32(T &&elem)
+{
+   return static_cast<uint32_t>(elem);
+}
+
+} // namespace pps
---
+++ src/tool/pps/pps_driver.cc
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2019-2020 Collabora, Ltd.
+ * Author: Antonio Caggiano <antonio.caggiano@collabora.com>
+ * Author: Rohan Garg <rohan.garg@collabora.com>
+ * Author: Robert Beckett <bob.beckett@collabora.com>
+ * Author: Corentin Noël <corentin.noel@collabora.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "pps_driver.h"
+
+#include <iterator>
+#include <sstream>
+
+#include "pps.h"
+#include "pps_algorithm.h"
+
+namespace pps
+{
+const std::vector<std::string> &Driver::supported_device_names()
+{
+   static std::vector<std::string> supported_device_names = {};
+   return supported_device_names;
+}
+
+std::unique_ptr<Driver> Driver::create(DrmDevice &&drm_device)
+{
+   std::unique_ptr<Driver> driver;
+
+   if (!driver) {
+      PERFETTO_ELOG("Failed to find a driver for DRM device %s", drm_device.name.c_str());
+      return nullptr;
+   }
+
+   driver->drm_device = std::move(drm_device);
+   return driver;
+}
+
+std::string Driver::default_driver_name()
+{
+   auto supported_devices = Driver::supported_device_names();
+   auto devices = DrmDevice::create_all();
+   for (auto &device : devices) {
+      if (CONTAINS(supported_devices, device.name)) {
+         PPS_LOG_IMPORTANT("Driver selected: %s", device.name.c_str());
+         return device.name;
+      }
+   }
+   PPS_LOG_FATAL("Failed to find any driver");
+}
+
+std::string Driver::find_driver_name(const char *requested)
+{
+   auto supported_devices = Driver::supported_device_names();
+   auto devices = DrmDevice::create_all();
+   for (auto &device : devices) {
+      if (device.name == requested) {
+         PPS_LOG_IMPORTANT("Driver selected: %s", device.name.c_str());
+         return device.name;
+      }
+   }
+
+   std::ostringstream drivers_os;
+   std::copy(supported_devices.begin(),
+      supported_devices.end() - 1,
+      std::ostream_iterator<std::string>(drivers_os, ", "));
+   drivers_os << supported_devices.back();
+
+   PPS_LOG_ERROR(
+      "Device '%s' not found (supported drivers: %s)", requested, drivers_os.str().c_str());
+
+   return default_driver_name();
+}
+
+} // namespace pps
