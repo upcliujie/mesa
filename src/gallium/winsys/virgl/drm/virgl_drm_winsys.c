74c81797a39b7a626156b706624fe830d68f0d16
Lepton Wu <lepton@chromium.org>
2021-03-18 23:05:19 -0700

    virgl: Don't destroy resource while it's in use.
    This is the race condition: thread 1 check reference count of resource
    and then find out out it's zero and then it begin to destroy it. Around
    the same time, thread 2 gets the lock and get the resource from the hash
    table and plan to use it. Then this resource gets destroyed while it's
    still in use.
    
    Signed-off-by: Lepton Wu <lepton@chromium.org>

--- src/gallium/winsys/virgl/drm/virgl_drm_winsys.c
+++ src/gallium/winsys/virgl/drm/virgl_drm_winsys.c
@@ -72,6 +72,15 @@ static void virgl_hw_res_destroy(struct virgl_drm_winsys *qdws,
       struct drm_gem_close args;
 
       mtx_lock(&qdws->bo_handles_mutex);
+
+      /* Double check the reference count is zero. We already checked it
+       * in virgl_drm_resource_reference. Check here with lock again and
+       * if it's not zero, we have to abandon the destroy. */
+      if (pipe_is_referenced(&res->reference)) {
+         mtx_unlock(&qdws->bo_handles_mutex);
+         return;
+      }
+
       _mesa_hash_table_remove_key(qdws->bo_handles,
                              (void *)(uintptr_t)res->bo_handle);
       if (res->flink_name)
