fbabc706d820355c183d1dfeb79d64819c2b9d02
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-03-23 18:39:32 +0200

    turnip: enable VK_KHR_16bit_storage on A650
    A650 can use the same SSBO descriptor for both 32-bit and 16-bit access,
    which makes it easy to enable this extension.
    
    Passes tests that run under:
    
    dEQP-VK.spirv_assembly.instruction.*.16bit_storage.*
    
    Rebased and modified commit from Jonathan Marek.
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/freedreno/vulkan/tu_shader.c
+++ src/freedreno/vulkan/tu_shader.c
@@ -76,6 +76,7 @@ tu_spirv_to_nir(struct tu_device *dev,
          .physical_storage_buffer_address = true,
          .float_controls = true,
          .float16 = true,
+         .storage_16bit = dev->physical_device->gpu_id >= 650,
       },
    };
 
