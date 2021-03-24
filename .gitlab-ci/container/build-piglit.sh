9cb47436da922b1cec114e4ad4ab4ebf38c6cb97
Eric Anholt <eric@anholt.net>
2021-03-24 10:16:26 -0700

    ci: Uprev piglit to 5751f7d8f9c4 ("gbm: Add test for multi-planar GBM BOs")
    Along with other new tests, brings in the perf improvement for
    gl-1.3-texture-env so we can stop skipping it.

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -8,7 +8,7 @@ fi
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
-git checkout b0bbeb876a506e0ee689dd7e17cee374c8284058
+git checkout 5751f7d8f9c4972a5ec9dfa47188f6e8a283cbde
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
