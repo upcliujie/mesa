Grima04/mesa-turnip-kgsl

commit 4a3b42a717ce4fa4450066352f04f2e1ef78ad9a

drisw: Prefer hardware-layered sw-winsys drivers over pure sw
Reviewed-by: Adam Jackson <ajax@redhat.com>
Reviewed-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8128>

src/gallium/auxiliary/target-helpers/inline_sw_helper.h
@@ -78,6 +78,12 @@ sw_screen_create(struct sw_winsys *winsys)
 {
    const char *drivers[] = {
       debug_get_option("GALLIUM_DRIVER", ""),
+#if defined(GALLIUM_ZINK)
+      "zink",
+#endif
+#if defined(GALLIUM_D3D12)
+      "d3d12",
+#endif
 #if defined(GALLIUM_LLVMPIPE)
       "llvmpipe",
 #endif
@@ -86,12 +92,6 @@ sw_screen_create(struct sw_winsys *winsys)
 #endif
 #if defined(GALLIUM_SWR)
       "swr",
-#endif
-#if defined(GALLIUM_ZINK)
-      "zink",
-#endif
-#if defined(GALLIUM_D3D12)
-      "d3d12",
 #endif
    };
 

src/gallium/auxiliary/target-helpers/sw_helper.h
@@ -84,6 +84,12 @@ sw_screen_create(struct sw_winsys *winsys)
 {
    const char *drivers[] = {
       debug_get_option("GALLIUM_DRIVER", ""),
+#if defined(GALLIUM_ZINK)
+      "zink",
+#endif
+#if defined(GALLIUM_D3D12)
+      "d3d12",
+#endif
 #if defined(GALLIUM_LLVMPIPE)
       "llvmpipe",
 #endif
@@ -92,12 +98,6 @@ sw_screen_create(struct sw_winsys *winsys)
 #endif
 #if defined(GALLIUM_SWR)
       "swr",
-#endif
-#if defined(GALLIUM_ZINK)
-      "zink",
-#endif
-#if defined(GALLIUM_D3D12)
-      "d3d12",
 #endif
    };
 

upcliujie/mesa

commit 70259f75a5546d331b0d687227341f653a4bf544

drisw: move zink down the list below the sw drivers.
We don't ever want drisw path picking zink as the driver,
we can revisit this when the penny wrapper work gets further
along.

This selection causes systems with nvidia/intel dual-gpus
to try and pick the intel gpu for rendering in the nvidia
context if there is no nvidia GL driver or accel doesn't work.

This is a partial revert of the original commit.

Fixes: 4a3b42a717ce ("drisw: Prefer hardware-layered sw-winsys drivers over pure sw")

src/gallium/auxiliary/target-helpers/inline_sw_helper.h
@@ -81,9 +81,6 @@ sw_screen_create(struct sw_winsys *winsys)
    UNUSED bool only_sw = env_var_as_boolean("LIBGL_ALWAYS_SOFTWARE", false);
    const char *drivers[] = {
       debug_get_option("GALLIUM_DRIVER", ""),
-#if defined(GALLIUM_ZINK)
-      only_sw ? "" : "zink",
-#endif
 #if defined(GALLIUM_D3D12)
       only_sw ? "" : "d3d12",
 #endif
@@ -95,6 +92,9 @@ sw_screen_create(struct sw_winsys *winsys)
 #endif
 #if defined(GALLIUM_SWR)
       "swr",
+#endif
+#if defined(GALLIUM_ZINK)
+      only_sw ? "" : "zink",
 #endif
    };
 

src/gallium/auxiliary/target-helpers/sw_helper.h
@@ -86,9 +86,6 @@ sw_screen_create(struct sw_winsys *winsys)
    UNUSED bool only_sw = env_var_as_boolean("LIBGL_ALWAYS_SOFTWARE", false);
    const char *drivers[] = {
       debug_get_option("GALLIUM_DRIVER", ""),
-#if defined(GALLIUM_ZINK)
-      only_sw ? "" : "zink",
-#endif
 #if defined(GALLIUM_D3D12)
       only_sw ? "" : "d3d12",
 #endif
@@ -100,6 +97,9 @@ sw_screen_create(struct sw_winsys *winsys)
 #endif
 #if defined(GALLIUM_SWR)
       "swr",
+#endif
+#if defined(GALLIUM_ZINK)
+      only_sw ? "" : "zink",
 #endif
    };
 