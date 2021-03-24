Grima04/mesa-turnip-kgsl

commit 10a7682413785a0b3667ab077971cd132f43281b

util: add _mesa_set_create_u32_keys where keys are not pointers
the only limitation is that key=0 is not allowed

Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6955>

src/util/set.c
@@ -148,6 +148,26 @@ _mesa_set_create(void *mem_ctx,
    return ht;
 }
 
+static uint32_t
+key_u32_hash(const void *key)
+{
+   uint32_t u = (uint32_t)(uintptr_t)key;
+   return _mesa_hash_uint(&u);
+}
+
+static bool
+key_u32_equals(const void *a, const void *b)
+{
+   return (uint32_t)(uintptr_t)a == (uint32_t)(uintptr_t)b;
+}
+
+/* key == 0 and key == deleted_key are not allowed */
+struct set *
+_mesa_set_create_u32_keys(void *mem_ctx)
+{
+   return _mesa_set_create(NULL, key_u32_hash, key_u32_equals);
+}
+
 struct set *
 _mesa_set_clone(struct set *set, void *dst_mem_ctx)
 {

src/util/set.h
@@ -61,6 +61,9 @@ _mesa_set_create(void *mem_ctx,
                  bool (*key_equals_function)(const void *a,
                                              const void *b));
 struct set *
+_mesa_set_create_u32_keys(void *mem_ctx);
+
+struct set *
 _mesa_set_clone(struct set *set, void *dst_mem_ctx);
 
 void

upcliujie/mesa

commit 7aaaa5f9454aa9e9ba7bc4571abcf829d82e83c9

util/set: stop leaking u32 key sets which pass a mem ctx
Fixes: 10a76824137 ("util: add _mesa_set_create_u32_keys where keys are not pointers")

src/util/set.c
@@ -174,7 +174,7 @@ key_u32_equals(const void *a, const void *b)
 struct set *
 _mesa_set_create_u32_keys(void *mem_ctx)
 {
-   return _mesa_set_create(NULL, key_u32_hash, key_u32_equals);
+   return _mesa_set_create(mem_ctx, key_u32_hash, key_u32_equals);
 }
 
 struct set *