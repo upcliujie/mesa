Grima04/mesa-turnip-kgsl

commit 6fa56273be734dd04d4a8c0d342dd65bc8602a30

anv/image: Drop duplicate 'format' in anv_image_create()
Reduces the chance of misusing unitialized 'n_planes' and 'format'
during image creation.

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8097>

src/intel/vulkan/anv_image.c
@@ -796,6 +796,7 @@ anv_image_create(VkDevice _device,
                                              pCreateInfo->extent);
    image->vk_format = pCreateInfo->format;
    image->format = anv_get_format(pCreateInfo->format);
+   image->n_planes = image->format->n_planes;
    image->aspects = vk_format_aspects(image->vk_format);
    image->levels = pCreateInfo->mipLevels;
    image->array_size = pCreateInfo->arrayLayers;
@@ -829,14 +830,9 @@ anv_image_create(VkDevice _device,
       return VK_SUCCESS;
    }
 
-   const struct anv_format *format = anv_get_format(image->vk_format);
-   assert(format != NULL);
-
    const isl_tiling_flags_t isl_tiling_flags =
       choose_isl_tiling_flags(&device->info, create_info, isl_mod_info,
                               image->needs_set_tiling);
-
-   image->n_planes = format->n_planes;
 
    const VkImageFormatListCreateInfoKHR *fmt_list =
       vk_find_struct_const(pCreateInfo->pNext,

upcliujie/mesa

commit 9c3a79869a9cedb05c06abfe7aa2f70c367e70af

anv/android: fix image creation with external format
This fixes crashes seen with following group of tests:
   android.graphics.cts.BasicVulkanGpuTest

Fixes: 6fa56273be7 ("anv/image: Drop duplicate 'format' in anv_image_create()")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>

src/intel/vulkan/anv_image.c
@@ -930,7 +930,6 @@ anv_image_create(VkDevice _device,
                                              pCreateInfo->extent);
    image->vk_format = pCreateInfo->format;
    image->format = anv_get_format(pCreateInfo->format);
-   image->n_planes = image->format->n_planes;
    image->aspects = vk_format_aspects(image->vk_format);
    image->levels = pCreateInfo->mipLevels;
    image->array_size = pCreateInfo->arrayLayers;
@@ -941,6 +940,17 @@ anv_image_create(VkDevice _device,
    image->needs_set_tiling = wsi_info && wsi_info->scanout;
    image->drm_format_mod = isl_mod_info ? isl_mod_info->modifier :
                                           DRM_FORMAT_MOD_INVALID;
+
+   /* In case of external format, We don't know format yet,
+    * so skip the rest for now.
+    */
+   if (create_info->external_format) {
+      image->external_format = true;
+      *pImage = anv_image_to_handle(image);
+      return VK_SUCCESS;
+   }
+
+   image->n_planes = image->format->n_planes;
 
    /* The Vulkan 1.2.165 glossary says:
     *
@@ -966,15 +976,6 @@ anv_image_create(VkDevice _device,
       image->bindings[i] = (struct anv_image_binding) {
          .memory_range = { .binding = i },
       };
-   }
-
-   /* In case of external format, We don't know format yet,
-    * so skip the rest for now.
-    */
-   if (create_info->external_format) {
-      image->external_format = true;
-      *pImage = anv_image_to_handle(image);
-      return VK_SUCCESS;
    }
 
    const isl_tiling_flags_t isl_tiling_flags =