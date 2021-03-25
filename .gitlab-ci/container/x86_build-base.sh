Grima04/mesa-turnip-kgsl

commit 6e6cd7d93cc54fc8c279377c2d552761f4457174

scons: Remove.
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9720>

meson.build
@@ -980,7 +980,7 @@ elif host_machine.system() == 'windows'
   pre_args += [
     '-D_WINDOWS', '-D_WIN32_WINNT=0x0A00', '-DWINVER=0x0A00',
     '-DPIPE_SUBSYSTEM_WINDOWS_USER',
-    '-D_USE_MATH_DEFINES',  # XXX: scons doesn't use this for mingw
+    '-D_USE_MATH_DEFINES',  # XXX: scons didn't use this for mingw
   ]
   if cc.get_id() == 'msvc'
     pre_args += [

.travis.yml
@@ -13,16 +13,11 @@ matrix:
   include:
     - env:
       - BUILD=meson
-    - env:
-      - BUILD=scons
 
 before_install:
   - HOMEBREW_NO_AUTO_UPDATE=1 brew install expat gettext
   - if test "x$BUILD" = xmeson; then
       HOMEBREW_NO_AUTO_UPDATE=1 brew install ninja;
-    fi
-  - if test "x$BUILD" = xscons; then
-      HOMEBREW_NO_AUTO_UPDATE=1 brew install scons;
     fi
 
   # Set PATH for homebrew pip3 installs
@@ -47,9 +42,6 @@ install:
       pip3 install --user meson;
       pip3 install --user mako;
     fi
-  - if test "x$BUILD" = xscons; then
-      pip2 install --user mako;
-    fi
 
 script:
   - if test "x$BUILD" = xmeson; then
@@ -57,8 +49,4 @@ script:
       ninja -C _build || travis_terminate 1;
       ninja -C _build test || travis_terminate 1;
       ninja -C _build install || travis_terminate 1;
-    fi
-  - if test "x$BUILD" = xscons; then
-      scons force_scons=1 || travis_terminate 1;
-      scons force_scons=1 check || travis_terminate 1;
     fi

REVIEWERS
@@ -67,11 +67,6 @@ GALLIUM TARGETS
 R: Emil Velikov <emil.l.velikov@gmail.com>
 F: src/gallium/targets/
 
-SCONS BUILD
-F: scons/
-F: */SConscript*
-F: */Makefile.sources
-
 ANDROID BUILD
 R: Emil Velikov <emil.l.velikov@gmail.com>
 R: Rob Herring <robh@kernel.org>

SConstruct
@@ -1,187 +1,0 @@
-#######################################################################
-# Top-level SConstruct
-#
-# For example, invoke scons as
-#
-#   scons build=debug llvm=yes machine=x86
-#
-# to set configuration variables. Or you can write those options to a file
-# named config.py:
-#
-#   # config.py
-#   build='debug'
-#   llvm=True
-#   machine='x86'
-#
-# Invoke
-#
-#   scons -h
-#
-# to get the full list of options. See scons manpage for more info.
-#
-
-from __future__ import print_function
-import os
-import os.path
-import sys
-import SCons.Util
-
-import common
-
-#######################################################################
-# Minimal scons version
-
-EnsureSConsVersion(2, 4)
-EnsurePythonVersion(2, 7)
-
-
-#######################################################################
-# Configuration options
-
-opts = Variables('config.py')
-common.AddOptions(opts)
-
-env = Environment(
-	options = opts,
-	tools = ['gallium'],
-	toolpath = ['#scons'],
-	ENV = os.environ,
-)
-
-# XXX: This creates a many problems as it saves...
-#opts.Save('config.py', env)
-
-# Backwards compatability with old target configuration variable
-try:
-    targets = ARGUMENTS['targets']
-except KeyError:
-    pass
-else:
-    targets = targets.split(',')
-    print('scons: warning: targets option is deprecated; pass the targets on their own such as')
-    print()
-    print('  scons %s' % ' '.join(targets))
-    print()
-    COMMAND_LINE_TARGETS.append(targets)
-
-
-Help(opts.GenerateHelpText(env))
-
-
-#######################################################################
-# Print a deprecation warning for using scons on non-windows
-
-if common.host_platform != 'windows' and env['platform'] != 'windows':
-    if env['force_scons']:
-        print("WARNING: Scons is deprecated for non-windows platforms (including cygwin) "
-              "please use meson instead.", file=sys.stderr)
-    else:
-        print("ERROR: Scons is deprecated for non-windows platforms (including cygwin) "
-              "please use meson instead. If you really need to use scons you "
-              "can add `force_scons=1` to the scons command line.", file=sys.stderr)
-        sys.exit(1)
-else:
-    print("WARNING: Scons support is in the process of being deprecated on "
-          "on windows platforms (including mingw). If you haven't already "
-          "please try using meson for windows builds. Be sure to report any "
-          "issues you run into", file=sys.stderr)
-
-
-#######################################################################
-# Environment setup
-
-with open("VERSION") as f:
-  mesa_version = f.read().strip()
-env.Append(CPPDEFINES = [
-    ('PACKAGE_VERSION', '\\"%s\\"' % mesa_version),
-    ('PACKAGE_BUGREPORT', '\\"https://gitlab.freedesktop.org/mesa/mesa/-/issues\\"'),
-])
-
-# Includes
-env.Prepend(CPPPATH = [
-	'#/include',
-])
-env.Append(CPPPATH = [
-	'#/src/gallium/include',
-	'#/src/gallium/auxiliary',
-	'#/src/gallium/drivers',
-	'#/src/gallium/winsys',
-])
-
-# for debugging
-#print env.Dump()
-
-
-# Add a check target for running tests
-check = env.Alias('check')
-env.AlwaysBuild(check)
-
-
-#######################################################################
-# Invoke host SConscripts
-#
-# For things that are meant to be run on the native host build machine, instead
-# of the target machine.
-#
-
-# Create host environent
-if env['crosscompile'] and not env['embedded']:
-    host_env = Environment(
-        options = opts,
-        # no tool used
-        tools = [],
-        toolpath = ['#scons'],
-        ENV = os.environ,
-    )
-
-    # Override options
-    host_env['platform'] = common.host_platform
-    host_env['machine'] = common.host_machine
-    host_env['toolchain'] = 'default'
-    host_env['llvm'] = False
-
-    host_env.Tool('gallium')
-
-    host_env['hostonly'] = True
-    assert host_env['crosscompile'] == False
-
-    target_env = env
-    env = host_env
-    Export('env')
-
-    SConscript(
-        'src/SConscript',
-        variant_dir = host_env['build_dir'],
-        duplicate = 0, # http://www.scons.org/doc/0.97/HTML/scons-user/x2261.html
-    )
-
-    env = target_env
-
-Export('env')
-
-#######################################################################
-# Invoke SConscripts
-
-# TODO: Build several variants at the same time?
-# http://www.scons.org/wiki/SimultaneousVariantBuilds
-
-SConscript(
-	'src/SConscript',
-	variant_dir = env['build_dir'],
-	duplicate = 0 # http://www.scons.org/doc/0.97/HTML/scons-user/x2261.html
-)
-
-
-########################################################################
-# List all aliases
-
-try:
-    from SCons.Node.Alias import default_ans
-except ImportError:
-    pass
-else:
-    aliases = sorted(default_ans.keys())
-    env.Help('\n')
-    env.Help('Recognized targets:\n')
-    for alias in aliases:
-        env.Help('    %s\n' % alias)

common.py
@@ -1,124 +1,0 @@
-#######################################################################
-# Common SCons code
-
-import os
-import os.path
-import re
-import subprocess
-import sys
-import platform as _platform
-
-import SCons.Script.SConscript
-
-
-#######################################################################
-# Defaults
-
-host_platform = _platform.system().lower()
-if host_platform.startswith('cygwin'):
-    host_platform = 'cygwin'
-# MSYS2 default platform selection.
-if host_platform.startswith('mingw'):
-    host_platform = 'windows'
-
-# Search sys.argv[] for a "platform=foo" argument since we don't have
-# an 'env' variable at this point.
-if 'platform' in SCons.Script.ARGUMENTS:
-    target_platform = SCons.Script.ARGUMENTS['platform']
-else:
-    target_platform = host_platform
-
-_machine_map = {
-    'x86': 'x86',
-    'i386': 'x86',
-    'i486': 'x86',
-    'i586': 'x86',
-    'i686': 'x86',
-    'BePC': 'x86',
-    'Intel': 'x86',
-    'ppc': 'ppc',
-    'BeBox': 'ppc',
-    'BeMac': 'ppc',
-    'AMD64': 'x86_64',
-    'x86_64': 'x86_64',
-    'sparc': 'sparc',
-    'sun4u': 'sparc',
-}
-
-
-# find host_machine value
-if 'PROCESSOR_ARCHITECTURE' in os.environ:
-    host_machine = os.environ['PROCESSOR_ARCHITECTURE']
-else:
-    host_machine = _platform.machine()
-host_machine = _machine_map.get(host_machine, 'generic')
-# MSYS2 default machine selection.
-if _platform.system().lower().startswith('mingw') and 'MSYSTEM' in os.environ:
-    if os.environ['MSYSTEM'] == 'MINGW32':
-        host_machine = 'x86'
-    if os.environ['MSYSTEM'] == 'MINGW64':
-        host_machine = 'x86_64'
-
-default_machine = host_machine
-default_toolchain = 'default'
-# MSYS2 default toolchain selection.
-if _platform.system().lower().startswith('mingw'):
-    default_toolchain = 'mingw'
-
-if target_platform == 'windows' and host_platform != 'windows':
-    default_machine = 'x86'
-    default_toolchain = 'crossmingw'
-
-
-# find default_llvm value
-if 'LLVM' in os.environ or 'LLVM_CONFIG' in os.environ:
-    default_llvm = 'yes'
-else:
-    default_llvm = 'no'
-    try:
-        if target_platform != 'windows' and \
-           subprocess.call(['llvm-config', '--version'],
-                           stdout=subprocess.PIPE) == 0:
-            default_llvm = 'yes'
-    except:
-        pass
-
-
-#######################################################################
-# Common options
-
-def AddOptions(opts):
-    try:
-        from SCons.Variables.BoolVariable import BoolVariable as BoolOption
-    except ImportError:
-        from SCons.Options.BoolOption import BoolOption
-    try:
-        from SCons.Variables.EnumVariable import EnumVariable as EnumOption
-    except ImportError:
-        from SCons.Options.EnumOption import EnumOption
-    opts.Add(EnumOption('build', 'build type', 'debug',
-                        allowed_values=('debug', 'checked', 'profile',
-                                        'release')))
-    opts.Add(BoolOption('verbose', 'verbose output', 'no'))
-    opts.Add(EnumOption('machine', 'use machine-specific assembly code',
-                        default_machine,
-                        allowed_values=('generic', 'ppc', 'x86', 'x86_64')))
-    opts.Add(EnumOption('platform', 'target platform', host_platform,
-                        allowed_values=('cygwin', 'darwin', 'freebsd', 'haiku',
-                                        'linux', 'sunos', 'windows')))
-    opts.Add(BoolOption('embedded', 'embedded build', 'no'))
-    opts.Add(BoolOption('analyze',
-                        'enable static code analysis where available', 'no'))
-    opts.Add(BoolOption('asan', 'enable Address Sanitizer', 'no'))
-    opts.Add('toolchain', 'compiler toolchain', default_toolchain)
-    opts.Add(BoolOption('llvm', 'use LLVM', default_llvm))
-    opts.Add(BoolOption('force_scons', 'Force enable scons on deprecated platforms', 'false'))
-    opts.Add(BoolOption('openmp', 'EXPERIMENTAL: compile with openmp (swrast)',
-                        'no'))
-    opts.Add(BoolOption('debug', 'DEPRECATED: debug build', 'yes'))
-    opts.Add(BoolOption('profile', 'DEPRECATED: profile build', 'no'))
-    opts.Add(BoolOption('quiet', 'DEPRECATED: profile build', 'yes'))
-    opts.Add(BoolOption('swr', 'Build OpenSWR', 'no'))
-    if host_platform == 'windows':
-        opts.Add('MSVC_VERSION', 'Microsoft Visual C/C++ version')
-        opts.Add('MSVC_USE_SCRIPT', 'Microsoft Visual C/C++ vcvarsall script', True)

docs/README.WIN32
@@ -1,64 +1,0 @@
-File: docs/README.WIN32
-
-Last updated: 21 June 2013
-
-
-Quick Start
------ -----
-
-Windows drivers are build with SCons.  Makefiles or Visual Studio projects are
-no longer shipped or supported.
-
-Run
-
-  scons libgl-gdi
-
-to build gallium based GDI driver.
-
-This will work both with MSVS or Mingw.
-
-
-Windows Drivers
-------- -------
-
-At this time, only the gallium GDI driver is known to work.
-
-Source code also exists in the tree for other drivers in
-src/mesa/drivers/windows, but the status of this code is unknown.
-
-Recipe
-------
-
-Building on windows requires several open-source packages. These are
-steps that work as of this writing.
-
-- install python 2.7
-- install scons (latest)
-- install mingw, flex, and bison
-- install pywin32 from here: http://www.lfd.uci.edu/~gohlke/pythonlibs
-  get pywin32-218.4.win-amd64-py2.7.exe
-- install git
-- download mesa from git
-  see https://www.mesa3d.org/repository.html
-- run scons
-
-General
--------
-
-After building, you can copy the above DLL files to a place in your
-PATH such as $SystemRoot/SYSTEM32.  If you don't like putting things
-in a system directory, place them in the same directory as the
-executable(s).  Be careful about accidentially overwriting files of
-the same name in the SYSTEM32 directory.
-
-The DLL files are built so that the external entry points use the
-stdcall calling convention.
-
-Static LIB files are not built.  The LIB files that are built with are
-the linker import files associated with the DLL files.
-
-The si-glu sources are used to build the GLU libs.  This was done
-mainly to get the better tessellator code.
-
-If you have a Windows-related build problem or question, please post
-to the mesa-dev or mesa-users list.

scons/crossmingw.py
@@ -1,178 +1,0 @@
-"""SCons.Tool.gcc
-
-Tool-specific initialization for MinGW (http://www.mingw.org/)
-
-There normally shouldn't be any need to import this module directly.
-It will usually be imported through the generic SCons.Tool.Tool()
-selection method.
-
-See also http://www.scons.org/wiki/CrossCompilingMingw
-"""
-
-#
-# Copyright (c) 2001, 2002, 2003, 2004 The SCons Foundation
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
-# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-import os
-import os.path
-import string
-
-import SCons.Action
-import SCons.Builder
-import SCons.Tool
-import SCons.Util
-
-# These are the mingw toolchain prefixes we search for:
-# (We only search for the mingw-w64 toolchain, and not the mingw.org one.)
-prefixes32 = SCons.Util.Split("""
-    i686-w64-mingw32-
-""")
-prefixes64 = SCons.Util.Split("""
-    x86_64-w64-mingw32-
-""")
-
-def find(env):
-    if env['machine'] == 'x86_64':
-        prefixes = prefixes64
-    else:
-        prefixes = prefixes32
-    for prefix in prefixes:
-        # First search in the SCons path and then the OS path:
-        if env.WhereIs(prefix + 'gcc') or SCons.Util.WhereIs(prefix + 'gcc'):
-            return prefix
-
-    return ''
-
-def shlib_generator(target, source, env, for_signature):
-    cmd = SCons.Util.CLVar(['$SHLINK', '$SHLINKFLAGS']) 
-
-    dll = env.FindIxes(target, 'SHLIBPREFIX', 'SHLIBSUFFIX')
-    if dll: cmd.extend(['-o', dll])
-
-    cmd.extend(['$SOURCES', '$_LIBDIRFLAGS', '$_LIBFLAGS'])
-
-    implib = env.FindIxes(target, 'LIBPREFIX', 'LIBSUFFIX')
-    if implib: cmd.append('-Wl,--out-implib,'+implib.get_string(for_signature))
-
-    def_target = env.FindIxes(target, 'WIN32DEFPREFIX', 'WIN32DEFSUFFIX')
-    if def_target: cmd.append('-Wl,--output-def,'+def_target.get_string(for_signature))
-
-    return [cmd]
-
-def shlib_emitter(target, source, env):
-    dll = env.FindIxes(target, 'SHLIBPREFIX', 'SHLIBSUFFIX')
-    no_import_lib = env.get('no_import_lib', 0)
-
-    if not dll:
-        raise SCons.Errors.UserError("A shared library should have exactly one target with the suffix: %s" % env.subst("$SHLIBSUFFIX"))
-    
-    if not no_import_lib and \
-       not env.FindIxes(target, 'LIBPREFIX', 'LIBSUFFIX'):
-
-        # Append an import library to the list of targets.
-        target.append(env.ReplaceIxes(dll,  
-                                      'SHLIBPREFIX', 'SHLIBSUFFIX',
-                                      'LIBPREFIX', 'LIBSUFFIX'))
-
-    # Append a def file target if there isn't already a def file target
-    # or a def file source. There is no option to disable def file
-    # target emitting, because I can't figure out why someone would ever
-    # want to turn it off.
-    def_source = env.FindIxes(source, 'WIN32DEFPREFIX', 'WIN32DEFSUFFIX')
-    def_target = env.FindIxes(target, 'WIN32DEFPREFIX', 'WIN32DEFSUFFIX')
-    if not def_source and not def_target:
-        target.append(env.ReplaceIxes(dll,  
-                                      'SHLIBPREFIX', 'SHLIBSUFFIX',
-                                      'WIN32DEFPREFIX', 'WIN32DEFSUFFIX'))
-    
-    return (target, source)
-                         
-
-shlib_action = SCons.Action.Action(shlib_generator, '$SHLINKCOMSTR', generator=1)
-
-res_action = SCons.Action.Action('$RCCOM', '$RCCOMSTR')
-
-res_builder = SCons.Builder.Builder(action=res_action, suffix='.o',
-                                    source_scanner=SCons.Tool.SourceFileScanner)
-SCons.Tool.SourceFileScanner.add_scanner('.rc', SCons.Defaults.CScan)
-
-
-
-def generate(env):
-    mingw_prefix = find(env)
-
-    if mingw_prefix:
-        dir = os.path.dirname(env.WhereIs(mingw_prefix + 'gcc') or SCons.Util.WhereIs(mingw_prefix + 'gcc'))
-
-        # The mingw bin directory must be added to the path:
-        path = env['ENV'].get('PATH', [])
-        if not path: 
-            path = []
-        if SCons.Util.is_String(path):
-            path = str.split(path, os.pathsep)
-
-        env['ENV']['PATH'] = str.join(os.pathsep, [dir] + path)
-
-    # Most of mingw is the same as gcc and friends...
-    gnu_tools = ['gcc', 'g++', 'gnulink', 'ar', 'gas']
-    for tool in gnu_tools:
-        SCons.Tool.Tool(tool)(env)
-
-    #... but a few things differ:
-    env['CC'] = mingw_prefix + 'gcc'
-    env['SHCCFLAGS'] = SCons.Util.CLVar('$CCFLAGS')
-    env['CXX'] = mingw_prefix + 'g++'
-    env['SHCXXFLAGS'] = SCons.Util.CLVar('$CXXFLAGS')
-    env['SHLINKFLAGS'] = SCons.Util.CLVar('$LINKFLAGS -shared')
-    env['SHLINKCOM']   = shlib_action
-    env.Append(SHLIBEMITTER = [shlib_emitter])
-    env['LINK'] = mingw_prefix + 'g++'
-    env['AR'] = mingw_prefix + 'ar'
-    env['RANLIB'] = mingw_prefix + 'ranlib'
-    env['LINK'] = mingw_prefix + 'g++'
-    env['AS'] = mingw_prefix + 'as'
-    env['WIN32DEFPREFIX']        = ''
-    env['WIN32DEFSUFFIX']        = '.def'
-    env['SHOBJSUFFIX'] = '.o'
-    env['STATIC_AND_SHARED_OBJECTS_ARE_THE_SAME'] = 1
-
-    env['RC'] = mingw_prefix + 'windres'
-    env['RCFLAGS'] = SCons.Util.CLVar('')
-    env['RCCOM'] = '$RC $_CPPDEFFLAGS $_CPPINCFLAGS ${INCPREFIX}${SOURCE.dir} $RCFLAGS -i $SOURCE -o $TARGET'
-    env['BUILDERS']['RES'] = res_builder
-    
-    # Some setting from the platform also have to be overridden:
-    env['OBJPREFIX']      = ''
-    env['OBJSUFFIX']      = '.o'
-    env['SHOBJPREFIX']    = '$OBJPREFIX'
-    env['SHOBJSUFFIX']    = '$OBJSUFFIX'
-    env['PROGPREFIX']     = ''
-    env['PROGSUFFIX']     = '.exe'
-    env['LIBPREFIX']      = 'lib'
-    env['LIBSUFFIX']      = '.a'
-    env['SHLIBPREFIX']    = ''
-    env['SHLIBSUFFIX']    = '.dll'
-    env['LIBPREFIXES']    = [ 'lib', '' ]
-    env['LIBSUFFIXES']    = [ '.a', '.lib' ]
-
-def exists(env):
-    return find(env)

upcliujie/mesa

commit de4678edb30ef50a7cadcadf74fb48942f6e74fc

Remove Scons leftovers
Fixes: 6e6cd7d93cc54fc8c279 ("scons: Remove.")
Reviewed-by: Eric Engestrom <eric@engestrom.ch>

.gitlab-ci.yml
@@ -296,7 +296,7 @@ x86_build-base:
     - .fdo.container-build@debian
     - .container
   variables:
-    MESA_IMAGE_TAG: &x86_build-base "2021-02-08-container-reorg"
+    MESA_IMAGE_TAG: &x86_build-base "2021-03-25-no-scons"
 
 .use-x86_build-base:
   extends:
@@ -492,7 +492,7 @@ armhf_test:
 
 # Native Windows docker builds
 #
-# Unlike the above Linux-based builds - including MinGW/SCons builds which
+# Unlike the above Linux-based builds - including MinGW builds which
 # cross-compile for Windows - which use the freedesktop ci-templates, we
 # cannot use the same scheme here. As Windows lacks support for
 # Docker-in-Docker, and Podman does not run natively on Windows, we have
@@ -589,8 +589,6 @@ sanity:
     paths:
       - _build/meson-logs/*.txt
       - _build/meson-logs/strace
-      # scons:
-      - build/*/config.log
       - shader-db
 
 # Just Linux
@@ -629,14 +627,6 @@ sanity:
     LLVM_VERSION: 10
   script:
     - .gitlab-ci/meson/build.sh
-
-.scons-build:
-  extends:
-    - .build-linux
-    - .use-x86_build
-  stage: build-misc
-  script:
-    - env SCONSFLAGS="-j${FDO_CI_CONCURRENT:-4}" .gitlab-ci/scons-build.sh
 
 meson-testing:
   extends:
@@ -924,13 +914,6 @@ test-d3d12-windows:
     name: "mesa_${CI_JOB_NAME}"
     paths:
       - summary/
-
-.scons-win64:
-  extends: .scons-build
-  variables:
-    SCONS_TARGET: platform=windows machine=x86_64 debug=1
-    SCONS_CHECK_COMMAND: "true"
-  allow_failure: true
 
 meson-clover:
   extends: .meson-build

.gitlab-ci/container/x86_build-base.sh
@@ -70,7 +70,6 @@ apt-get install -y --no-remove \
         python3-pil \
         python3-requests \
         qemu-user \
-        scons \
         valgrind \
         wget \
         wine64-development \

.gitlab-ci/scons-build.sh
@@ -1,12 +1,0 @@
-#!/bin/bash
-
-set -e
-set -o xtrace
-
-if test -n "$LLVM_VERSION"; then
-    export LLVM_CONFIG="llvm-config-${LLVM_VERSION}"
-fi
-
-rm -rf build
-scons $SCONS_TARGET force_scons=on
-eval $SCONS_CHECK_COMMAND 

docs/conf.py
@@ -132,7 +132,6 @@ html_extra_path = [
   'libGL.txt',
   'README.UVD',
   'README.VCE',
-  'README.WIN32',
 ]
 
 html_redirects = [

include/c99_compat.h
@@ -135,49 +135,4 @@ test_c99_compat_h(const void * restrict a,
 #endif
 
 
-/* Fallback definitions, for scons which doesn't auto-detect these things. */
-#ifdef HAVE_SCONS
-
-#  ifndef _WIN32
-#    define HAVE_PTHREAD
-#    define HAVE_POSIX_MEMALIGN
-#  endif
-
-#  ifdef __GNUC__
-#    if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 2)
-#      error "GCC version 4.2 or higher required"
-#    endif
-
-     /* https://gcc.gnu.org/onlinedocs/gcc-4.2.4/gcc/Other-Builtins.html */
-#    define HAVE___BUILTIN_CLZ 1
-#    define HAVE___BUILTIN_CLZLL 1
-#    define HAVE___BUILTIN_CTZ 1
-#    define HAVE___BUILTIN_EXPECT 1
-#    define HAVE___BUILTIN_FFS 1
-#    define HAVE___BUILTIN_FFSLL 1
-#    define HAVE___BUILTIN_POPCOUNT 1
-#    define HAVE___BUILTIN_POPCOUNTLL 1
-     /* https://gcc.gnu.org/onlinedocs/gcc-4.2.4/gcc/Function-Attributes.html */
-#    define HAVE_FUNC_ATTRIBUTE_FLATTEN 1
-#    define HAVE_FUNC_ATTRIBUTE_UNUSED 1
-#    define HAVE_FUNC_ATTRIBUTE_FORMAT 1
-#    define HAVE_FUNC_ATTRIBUTE_PACKED 1
-#    define HAVE_FUNC_ATTRIBUTE_ALIAS 1
-#    define HAVE_FUNC_ATTRIBUTE_NORETURN 1
-
-#    if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3)
-       /* https://gcc.gnu.org/onlinedocs/gcc-4.3.6/gcc/Other-Builtins.html */
-#      define HAVE___BUILTIN_BSWAP32 1
-#      define HAVE___BUILTIN_BSWAP64 1
-#    endif
-
-#    if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5)
-#      define HAVE___BUILTIN_UNREACHABLE 1
-#    endif
-
-#  endif /* __GNUC__ */
-
-#endif /* HAVE_SCONS */
-
-
 #endif /* _C99_COMPAT_H_ */

meson.build
@@ -980,7 +980,7 @@ elif host_machine.system() == 'windows'
   pre_args += [
     '-D_WINDOWS', '-D_WIN32_WINNT=0x0A00', '-DWINVER=0x0A00',
     '-DPIPE_SUBSYSTEM_WINDOWS_USER',
-    '-D_USE_MATH_DEFINES',  # XXX: scons didn't use this for mingw
+    '-D_USE_MATH_DEFINES',
   ]
   if cc.get_id() == 'msvc'
     pre_args += [