4136899fe6f08fee70fe5a78f52d1bf815f23f2b
Ian Romanick <ian.d.romanick@intel.com>
2021-03-25 10:12:00 -0700

    anv: Use same primary surface memory range for both "planes" of subsampled formats
    The Y plane and the UV plane of Y210, Y212, and Y216 formats occupy the
    same memory.  Ensure that the memory range for the two planes is the
    same.

--- src/intel/vulkan/anv_image.c
+++ src/intel/vulkan/anv_image.c
@@ -668,7 +668,10 @@ add_primary_surface(struct anv_device *device,
 
    image->planes[plane].aux_usage = ISL_AUX_USAGE_NONE;
 
-   add_surface(image, ANV_IMAGE_MEMORY_BINDING_PLANE_0 + plane, anv_surf);
+   if (plane == 1 && anv_is_y_plane_and_uv_plane_same_memory(image->vk_format))
+      anv_surf->memory_range = image->planes[0].primary_surface.memory_range;
+   else
+      add_surface(image, ANV_IMAGE_MEMORY_BINDING_PLANE_0 + plane, anv_surf);
 
    return VK_SUCCESS;
 }
@@ -2380,6 +2383,12 @@ anv_CreateImageView(VkDevice _device,
    anv_foreach_image_aspect_bit(iaspect_bit, image, expanded_aspects) {
       uint32_t iplane =
          anv_image_aspect_to_plane(image->aspects, 1UL << iaspect_bit);
+
+      if (conv_format != NULL &&
+          anv_is_y_plane_and_uv_plane_same_memory(conv_format->vk_format)) {
+         iplane = 0;
+      }
+
       VkImageAspectFlags vplane_aspect =
          anv_plane_to_aspect(iview->aspect_mask, vplane);
       struct anv_format_plane format =
