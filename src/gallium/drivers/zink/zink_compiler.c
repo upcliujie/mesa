98ab6dbe99a3e8e2a1024d1fa458e7486a13af0c
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-03-25 08:17:14 -0400

    zink: disable spirv15 creation when renderdoc is active
    this breaks some (most) versions of renderdoc

--- src/gallium/drivers/zink/zink_compiler.c
+++ src/gallium/drivers/zink/zink_compiler.c
@@ -640,7 +640,8 @@ zink_shader_compile(struct zink_screen *screen, struct zink_shader *zs, struct z
 
    assign_io_locations(nir, shader_slot_map, shader_slots_reserved);
 
-   struct spirv_shader *spirv = nir_to_spirv(nir, streamout, screen->vk_version >= VK_MAKE_VERSION(1, 2, 0));
+   bool spirv_15 = !screen->renderdoc_api && screen->vk_version >= VK_MAKE_VERSION(1, 2, 0);
+   struct spirv_shader *spirv = nir_to_spirv(nir, streamout, spirv_15);
    if (!spirv)
       goto done;
 
