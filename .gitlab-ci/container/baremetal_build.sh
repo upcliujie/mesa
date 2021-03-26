c38db011656c0bacd9643c78370bb7e20bd871dc
Eric Anholt <eric@anholt.net>
2021-03-26 10:12:18 -0700

    ci: Switch baremetal to using an arm-built kernel+rootfs.
    A recent change on the runners has caused the debootstrap step to start
    failing, meaning we are unable to rebuild our containers.  By switching to
    arm64 building the arm64/armhf rootfses, we can avoid qemu troubles (and
    the slow speed of debian install under emulation).
    
    We retain the rootfs in the container, since it's important to the job
    runtimes to not actually have to move the data around.  gitlab can't
    handle intra-stage dependencies, so the arm64/armhf test containers move
    to the build-misc stage, which is still before any of the test stages.

--- .gitlab-ci/container/baremetal_build.sh
+++ .gitlab-ci/container/baremetal_build.sh
@@ -3,62 +3,16 @@
 set -e
 set -o xtrace
 
-ROOTFS=/lava-files/rootfs-${arch}
+# Fetch the arm-built rootfs image and unpack it in our x86 container (saves
+# network transfer, disk usage, and runtime on test jobs)
 
-INCLUDE_PIGLIT=1
+ARTIFACTS_PREFIX="https://${MINIO_HOST}/mesa-lava/"
+if wget -q --method=HEAD "${ARTIFACTS_PREFIX}/${FDO_UPSTREAM_REPO}/${DISTRIBUTION_TAG}/${ARCH}/done"; then
+  ARTIFACTS_URL="${ARTIFACTS_PREFIX}/mesa/mesa/${DISTRIBUTION_TAG}/${ARCH}"
+else
+  ARTIFACTS_URL="${ARTIFACTS_PREFIX}/${CI_PROJECT_PATH}/${DISTRIBUTION_TAG}/${ARCH}"
+fi
 
-dpkg --add-architecture $arch
-apt-get update
-
-# Cross-build test deps
-BAREMETAL_EPHEMERAL=" \
-        autoconf \
-        automake \
-        crossbuild-essential-$arch \
-        git-lfs \
-        libboost-dev:$arch \
-        libdrm-dev:$arch \
-        libegl1-mesa-dev:$arch \
-        libelf-dev:$arch \
-        libexpat1-dev:$arch \
-        libffi-dev:$arch \
-        libgbm-dev:$arch \
-        libgl1-mesa-dev:$arch \
-        libgles2-mesa-dev:$arch \
-        libpciaccess-dev:$arch \
-        libpcre3-dev:$arch \
-        libpng-dev:$arch \
-        libpython3-dev:$arch \
-        libstdc++6:$arch \
-        libtinfo-dev:$arch \
-        libudev-dev:$arch \
-        libvulkan-dev:$arch \
-        libwaffle-dev:$arch \
-        libxcb-keysyms1-dev:$arch \
-        libxkbcommon-dev:$arch \
-        python3-dev \
-        qt5-default \
-        qt5-qmake \
-        qtbase5-dev:$arch \
-        "
-
-apt-get install -y --no-remove $BAREMETAL_EPHEMERAL
-
-mkdir /var/cache/apt/archives/$arch
-
-############### Create cross-files
-
-. .gitlab-ci/container/create-cross-file.sh $arch
-
-. .gitlab-ci/container/container_pre_build.sh
-
-############### Create rootfs
-KERNEL_URL=https://github.com/anholt/linux/archive/mesa-ci-2021-02-17-5.11.tar.gz
-
-DEBIAN_ARCH=$arch . .gitlab-ci/container/lava_build.sh
-
-############### Uninstall the build software
-
-apt-get purge -y $BAREMETAL_EPHEMERAL
-
-. .gitlab-ci/container/container_post_build.sh
+wget $ARTIFACTS_URL -O rootfs.tgz
+mkdir -p /rootfs
+tar -C /rootfs -zxvf rootfs.tgz
