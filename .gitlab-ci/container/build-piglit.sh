a448958eedef3c8742e40eb7fb75417810e00918
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-03-26 08:09:54 +0100

    Use my piglit branch

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -6,9 +6,9 @@ if [ -n "$INCLUDE_OPENCL_TESTS" ]; then
     PIGLIT_OPTS="-DPIGLIT_BUILD_CL_TESTS=ON"
 fi
 
-git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
+git clone https://gitlab.freedesktop.org/tomeu/piglit.git --no-checkout /piglit
 pushd /piglit
-git checkout b0bbeb876a506e0ee689dd7e17cee374c8284058
+git checkout replay-minio
 patch -p1 <$OLDPWD/.gitlab-ci/piglit/disable-vs_in.diff
 cmake -S . -B . -G Ninja -DCMAKE_BUILD_TYPE=Release $PIGLIT_OPTS $EXTRA_CMAKE_ARGS
 ninja $PIGLIT_BUILD_TARGETS
