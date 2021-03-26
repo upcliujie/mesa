Grima04/mesa-turnip-kgsl

commit bcc61a01d49a9125fd350f005d0e95ad33349935

mesa: don't overallocate ParameterValues 4 times (v2)
The additional memory was never used.

v2: rework

Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9360>

src/mesa/program/prog_parameter.c
@@ -199,10 +199,11 @@ _mesa_reserve_parameter_storage(struct gl_program_parameter_list *paramList,
 {
    const GLuint oldNum = paramList->NumParameters;
    const unsigned oldValNum = paramList->NumParameterValues;
+   const unsigned needSizeValues = oldValNum + reserve_values * 4;
 
    if (paramList->DisallowRealloc &&
        (oldNum + reserve_params > paramList->Size ||
-        oldValNum + reserve_values > paramList->SizeValues)) {
+        needSizeValues > paramList->SizeValues)) {
       _mesa_problem(NULL, "Parameter storage reallocation disallowed. This "
               "is a Mesa bug. Increase the reservation size in the code.");
       abort();
@@ -218,17 +219,17 @@ _mesa_reserve_parameter_storage(struct gl_program_parameter_list *paramList,
                  paramList->Size * sizeof(struct gl_program_parameter));
    }
 
-   if (oldValNum + reserve_values > paramList->SizeValues) {
-      paramList->SizeValues += 4 * reserve_values;
+   if (needSizeValues > paramList->SizeValues) {
+      paramList->SizeValues = needSizeValues + 16; /* alloc some extra */
 
       paramList->ParameterValues = (gl_constant_value *)
          align_realloc(paramList->ParameterValues,         /* old buf */
-                       oldValNum * 4 * sizeof(gl_constant_value),/* old sz */
+                       oldValNum * sizeof(gl_constant_value),/* old sz */
                        /* Overallocate the size by 12 because matrix rows can
                         * be allocated partially but fetch_state always writes
                         * 4 components (16 bytes).
                         */
-                       paramList->SizeValues * 4 * sizeof(gl_constant_value) +
+                       paramList->SizeValues * sizeof(gl_constant_value) +
                        12, 16);
    }
 }
@@ -347,6 +348,9 @@ _mesa_add_parameter(struct gl_program_parameter_list *paramList,
    } else {
       unreachable("invalid parameter type");
    }
+
+   assert(paramList->NumParameters <= paramList->Size);
+   assert(paramList->NumParameterValues <= paramList->SizeValues);
 
    return (GLint) oldNum;
 }

upcliujie/mesa

commit 56fc56ff86d0d23bf39b3b27600b1843d3de841b

mesa: Allocate vec4 parameters, not scalars
gl_program_parameter_list::Size and friends keep track of the number of
vec4 elements.  This means that the allocation size in bytes must be
that value multiplied by `4 * sizeof(gl_constant_value)`.

Fixes: bcc61a01d49 ("mesa: don't overallocate ParameterValues 4 times (v2)")
Closes: #4505

src/mesa/program/prog_parameter.c
@@ -216,7 +216,7 @@ _mesa_reserve_parameter_storage(struct gl_program_parameter_list *paramList,
       /* realloc arrays */
       paramList->Parameters =
          realloc(paramList->Parameters,
-                 paramList->Size * sizeof(struct gl_program_parameter));
+                 paramList->Size * 4 * sizeof(struct gl_program_parameter));
    }
 
    if (needSizeValues > paramList->SizeValues) {
@@ -225,16 +225,16 @@ _mesa_reserve_parameter_storage(struct gl_program_parameter_list *paramList,
 
       paramList->ParameterValues = (gl_constant_value *)
          align_realloc(paramList->ParameterValues,         /* old buf */
-                       oldValNum * sizeof(gl_constant_value),/* old sz */
+                       oldValNum * 4 * sizeof(gl_constant_value),/* old sz */
                        /* Overallocate the size by 12 because matrix rows can
                         * be allocated partially but fetch_state always writes
                         * 4 components (16 bytes).
                         */
-                       paramList->SizeValues * sizeof(gl_constant_value) +
+                       paramList->SizeValues * 4 * sizeof(gl_constant_value) +
                        12, 16);
       /* The values are written to the shader cache, so clear them. */
       memset(paramList->ParameterValues + oldSize, 0,
-             (paramList->SizeValues - oldSize) * sizeof(gl_constant_value));
+             (paramList->SizeValues - oldSize) * 4 * sizeof(gl_constant_value));
    }
 }
 