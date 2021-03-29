fc29b3f04abf26e8ca10f2973c706e522ac6eebf
Rhys Perry <pendingchaos02@gmail.com>
2021-03-29 13:13:09 +0100

    radv: perform FCE instead of decompression for signed<->unsigned copies
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -6445,7 +6445,7 @@ static void radv_handle_color_image_transition(struct radv_cmd_buffer *cmd_buffe
 		                                      src_render_loop, src_queue_mask) &&
 		           !radv_layout_can_fast_clear(cmd_buffer->device, image, dst_layout,
 		                                       dst_render_loop, dst_queue_mask)) {
-			radv_fast_clear_flush_image_inplace(cmd_buffer, image, range);
+			radv_fast_clear_flush_image_inplace(cmd_buffer, image, range, false);
 			fast_clear_flushed = true;
 		}
 
@@ -6456,7 +6456,7 @@ static void radv_handle_color_image_transition(struct radv_cmd_buffer *cmd_buffe
 		                               src_render_loop, src_queue_mask) &&
 		    !radv_layout_can_fast_clear(cmd_buffer->device, image, dst_layout,
 		                                dst_render_loop, dst_queue_mask)) {
-			radv_fast_clear_flush_image_inplace(cmd_buffer, image, range);
+			radv_fast_clear_flush_image_inplace(cmd_buffer, image, range, false);
 			fast_clear_flushed = true;
 		}
 	}
@@ -6482,7 +6482,7 @@ static void radv_handle_color_image_transition(struct radv_cmd_buffer *cmd_buffe
 			/* A FMASK decompress is required before expanding
 			 * FMASK.
 			 */
-			radv_fast_clear_flush_image_inplace(cmd_buffer, image, range);
+			radv_fast_clear_flush_image_inplace(cmd_buffer, image, range, false);
 		}
 
 		struct radv_barrier_data barrier = {0};
