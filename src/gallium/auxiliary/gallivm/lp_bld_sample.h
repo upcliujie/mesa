Grima04/mesa-turnip-kgsl

commit 0ce7c4a7c98122996af849b0f1199da13cd7efa2

gallivm: Use the proper enum for the texture target bitfield.
This gets you nice output in gdb when trying to figure out why your 2D
sample shader is hitting the cube paths.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8118>

src/gallium/auxiliary/gallivm/lp_bld_sample.h
@@ -169,7 +169,7 @@ struct lp_static_texture_state
    unsigned swizzle_a:3;
 
    /* pipe_texture's state */
-   unsigned target:4;        /**< PIPE_TEXTURE_* */
+   enum pipe_texture_target target:4;        /**< PIPE_TEXTURE_* */
    unsigned pot_width:1;     /**< is the width a power of two? */
    unsigned pot_height:1;
    unsigned pot_depth:1;

upcliujie/mesa

commit 31b14cfb1323d78de471d84f88392431e75a3ad6

gallivm: increase size of texture target enum bitfield
Need to bump up the size of texture target bitfield for MSVC.

Fixes: 0ce7c4a7c981 ("gallivm: Use the proper enum for the texture target bitfield.")

src/gallium/auxiliary/gallivm/lp_bld_sample.h
@@ -169,7 +169,7 @@ struct lp_static_texture_state
    unsigned swizzle_a:3;
 
    /* pipe_texture's state */
-   enum pipe_texture_target target:4;        /**< PIPE_TEXTURE_* */
+   enum pipe_texture_target target:5;        /**< PIPE_TEXTURE_* */
    unsigned pot_width:1;     /**< is the width a power of two? */
    unsigned pot_height:1;
    unsigned pot_depth:1;