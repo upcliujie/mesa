1af08cb4d4a59d2a0bb299fdddfb34c51df15a9b
Dave Airlie <airlied@redhat.com>
2021-03-30 13:44:34 +1000

    st/mesa: also disable other int textures

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -1267,6 +1267,7 @@ void st_init_extensions(struct pipe_screen *screen,
 
       /* Integer textures make no sense before GLSL 1.30 */
       extensions->EXT_texture_integer = GL_FALSE;
+      extensions->ARB_texture_rgb10_a2ui = GL_FALSE;
    }
 
    if (options->glsl_zero_init) {
