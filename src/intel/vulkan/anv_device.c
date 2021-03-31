dbf8ee713b3b4fca4f8473a29a01138c1e310706
Jason Ekstrand <jason@jlekstrand.net>
2021-03-31 17:45:42 -0500

    anv: Implement VK_EXT_conservative_rasterization
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4480

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -258,6 +258,7 @@ get_device_extensions(const struct anv_physical_device *device,
       .EXT_calibrated_timestamps             = device->has_reg_timestamp,
       .EXT_conditional_rendering             = device->info.gen >= 8 ||
                                                device->info.is_haswell,
+      .EXT_conservative_rasterization        = device->info.gen >= 9,
       .EXT_custom_border_color               = device->info.gen >= 8,
       .EXT_depth_clip_enable                 = true,
       .EXT_descriptor_indexing               = device->has_a64_buffer_access &&
@@ -2120,6 +2121,25 @@ void anv_GetPhysicalDeviceProperties2(
 
    vk_foreach_struct(ext, pProperties->pNext) {
       switch (ext->sType) {
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_CONSERVATIVE_RASTERIZATION_PROPERTIES_EXT: {
+         /* TODO: Real limits */
+         VkPhysicalDeviceConservativeRasterizationPropertiesEXT *properties =
+            (VkPhysicalDeviceConservativeRasterizationPropertiesEXT *)ext;
+         /* I stole this from the Windows driver.  They claim it's hard-coded
+          * in the hardware.  I can't find it anywhere in the public docs.
+          */
+         properties->primitiveOverestimationSize = 1.0f / 512.0f;
+         properties->maxExtraPrimitiveOverestimationSize = 0.0f;
+         properties->extraPrimitiveOverestimationSizeGranularity = 0.0f;
+         properties->primitiveUnderestimation = false;
+         properties->conservativePointAndLineRasterization = false;
+         properties->degenerateTrianglesRasterized = true;
+         properties->degenerateLinesRasterized = false;
+         properties->fullyCoveredFragmentShaderInputVariable = false;
+         properties->conservativeRasterizationPostDepthCoverage = true;
+         break;
+      }
+
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_CUSTOM_BORDER_COLOR_PROPERTIES_EXT: {
          VkPhysicalDeviceCustomBorderColorPropertiesEXT *properties =
             (VkPhysicalDeviceCustomBorderColorPropertiesEXT *)ext;
