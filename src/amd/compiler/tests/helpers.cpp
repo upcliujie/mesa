3375f2363fd55067c227a56f29c4e7c293cbedd5
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-04-01 14:35:23 +0200

    radv: change RADV_FORCE_FAMILY to use family name instead of LLVM processor name
    gfx1030 doesn't allow us to specific eg. dimgrey.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/compiler/tests/helpers.cpp
+++ src/amd/compiler/tests/helpers.cpp
@@ -261,7 +261,7 @@ VkDevice get_vk_device(enum radeon_family family)
    if (device_cache[family])
       return device_cache[family];
 
-   setenv("RADV_FORCE_FAMILY", ac_get_llvm_processor_name(family), 1);
+   setenv("RADV_FORCE_FAMILY", ac_get_family_name(family), 1);
 
    VkApplicationInfo app_info = {};
    app_info.pApplicationName = "aco_tests";
