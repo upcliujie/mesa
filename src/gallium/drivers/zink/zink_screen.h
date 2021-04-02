67b8415f1569c199a90828440e0de2a672385773
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-04-02 15:16:17 -0400

    zink: populate maxSampleLocationGridSize for all available sample sizes on init

--- src/gallium/drivers/zink/zink_screen.h
+++ src/gallium/drivers/zink/zink_screen.h
@@ -174,6 +174,7 @@ struct zink_screen {
 
    PFN_vkGetPhysicalDeviceMultisamplePropertiesEXT vk_GetPhysicalDeviceMultisamplePropertiesEXT;
    PFN_vkCmdSetSampleLocationsEXT vk_CmdSetSampleLocationsEXT;
+   VkExtent2D maxSampleLocationGridSize[5];
 };
 
 
