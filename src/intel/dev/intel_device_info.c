cc308b248376b1393de631bb2518c68975fd5ad0
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-04-03 13:28:50 +0300

    intel/dev: fix subslice/eu total computations with some fused configurations
    When a device has its first slice/subslice fused off, we can't use the
    number of slices/subslices to iterate the mask array.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reported-by: Matt Roper <matthew.d.roper@intel.com>
    Cc: <mesa-stable@lists.freedesktop.org>

--- src/intel/dev/intel_device_info.c
+++ src/intel/dev/intel_device_info.c
@@ -78,18 +78,18 @@ main(int argc, char *argv[])
       fprintf(stdout, "   revision: %u\n", devinfo.revision);
 
       uint32_t n_s = 0, n_ss = 0, n_eus = 0;
-      for (unsigned s = 0; s < devinfo.num_slices; s++) {
+      for (unsigned s = 0; s < devinfo.max_slices; s++) {
          n_s += (devinfo.slice_masks & (1u << s)) ? 1 : 0;
-         for (unsigned ss = 0; ss < devinfo.num_subslices[s]; ss++) {
+         for (unsigned ss = 0; ss < devinfo.max_subslices_per_slice; ss++) {
             fprintf(stdout, "   slice%u.subslice%u: ", s, ss);
             if (gen_device_info_subslice_available(&devinfo, s, ss)) {
                n_ss++;
-               for (unsigned eu = 0; eu < devinfo.num_eu_per_subslice; eu++) {
+               for (unsigned eu = 0; eu < devinfo.max_eu_per_subslice; eu++) {
                   n_eus += gen_device_info_eu_available(&devinfo, s, ss, eu) ? 1 : 0;
                   fprintf(stdout, "%s", gen_device_info_eu_available(&devinfo, s, ss, eu) ? "1" : "0");
                }
             } else {
-               fprintf(stderr, "fused");
+               fprintf(stdout, "fused");
             }
             fprintf(stdout, "\n");
          }
