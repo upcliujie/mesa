291c924efd38d573e5c6f7b58aecc8bc9ba710bd
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-04-06 09:40:21 -0400

    aux/trace: add GALLIUM_TRACE_TRIGGER mode
    similar to amd/radv driver debug modes for sqtt, this specifies a filename
    which is checked on every flush(PIPE_FLUSH_END_OF_FRAME); when it exists,
    the next frame (and only that frame) is captured into the trace
    
    to use, specify a file with the env var, run your app, and 'touch /path/to/file'
    when you want to capture a trace
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- src/gallium/auxiliary/driver_trace/tr_context.c
+++ src/gallium/auxiliary/driver_trace/tr_context.c
@@ -1334,6 +1334,9 @@ trace_context_flush(struct pipe_context *_pipe,
       trace_dump_ret(ptr, *fence);
 
    trace_dump_call_end();
+
+   if (flags & PIPE_FLUSH_END_OF_FRAME)
+      trace_dump_check_trigger();
 }
 
 
