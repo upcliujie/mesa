70d5898c353d4a46d1c61a7f1a42c6218ac72a00
Yevhenii Kolesnikov <yevhenii.kolesnikov@globallogic.com>
2021-04-06 15:19:12 +0300

    lavapipe: Use a common vk_command_buffer structure
    Switch to using common structure.
    
    Signed-off-by: Yevhenii Kolesnikov <yevhenii.kolesnikov@globallogic.com>

--- src/gallium/frontends/lavapipe/lvp_private.h
+++ src/gallium/frontends/lavapipe/lvp_private.h
@@ -59,6 +59,7 @@ typedef uint32_t xcb_window_t;
 #include "vk_shader_module.h"
 #include "vk_util.h"
 #include "vk_format.h"
+#include "vk_command_buffer.h"
 
 #include "wsi_common.h"
 
@@ -585,7 +586,7 @@ enum lvp_cmd_buffer_status {
 };
 
 struct lvp_cmd_buffer {
-   struct vk_object_base base;
+   struct vk_command_buffer vk;
 
    struct lvp_device *                          device;
 
@@ -604,7 +605,7 @@ struct lvp_cmd_buffer {
 #define LVP_FROM_HANDLE(__lvp_type, __name, __handle) \
    struct __lvp_type *__name = __lvp_type ## _from_handle(__handle)
 
-VK_DEFINE_HANDLE_CASTS(lvp_cmd_buffer, base, VkCommandBuffer,
+VK_DEFINE_HANDLE_CASTS(lvp_cmd_buffer, vk.base, VkCommandBuffer,
                        VK_OBJECT_TYPE_COMMAND_BUFFER)
 VK_DEFINE_HANDLE_CASTS(lvp_device, vk.base, VkDevice, VK_OBJECT_TYPE_DEVICE)
 VK_DEFINE_HANDLE_CASTS(lvp_instance, vk.base, VkInstance, VK_OBJECT_TYPE_INSTANCE)
