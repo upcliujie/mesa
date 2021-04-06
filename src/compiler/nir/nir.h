9aea673b444d524341d51b4fb9fc6fc7b1514832
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-04-06 14:53:03 +0200

    nir: clarify that 8bit ALU is integer
    This just brings this option into the style of the previous commit.

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -3425,8 +3425,8 @@ typedef struct nir_shader_compiler_options {
    /** Lower nir_op_ibfe and nir_op_ubfe that have two constant sources. */
    bool lower_bfe_with_two_constants;
 
-   /** Whether 8-bit ALU is supported. */
-   bool support_8bit_alu;
+   /** Whether 8-bit int ALU is supported. */
+   bool support_8bit_int_alu;
 
    /** Whether 16-bit int ALU is supported. */
    bool support_16bit_int_alu;
