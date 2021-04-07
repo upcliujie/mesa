c7e89ca37feb64a0ce663c5bfdf95b1b77d82a8d
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-04-07 09:08:18 +0200

    radv: add MSAA support to ClearColorImage() on compute queue
    Found by inspection.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -545,7 +545,7 @@ struct radv_meta_state {
 	struct {
 		VkPipelineLayout                          img_p_layout;
 		VkDescriptorSetLayout                     img_ds_layout;
-		VkPipeline pipeline;
+		VkPipeline pipeline[MAX_SAMPLES_LOG2];
 		VkPipeline pipeline_3d;
 	} cleari;
 	struct {
