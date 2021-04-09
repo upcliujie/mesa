df8d390eddbe921f21fefe4474acd0dca566d132
Timur Kristóf <timur.kristof@gmail.com>
2021-04-09 16:55:53 +0200

    nir: Add AMD specific intrinsics for merged shaders and NGG.
    These intrinsics represent what the hardware can actually do.
    Lowering our shaders to use these intrinsics will allow us to
    deal with mapping the classic VS, TES, GS (and the future MS)
    stages to the hardware capabilities using NIR, which makes our
    backend compilers simpler.
    
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -143,6 +143,12 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
    case nir_intrinsic_load_ring_tess_offchip_offset_amd:
    case nir_intrinsic_load_ring_esgs_amd:
    case nir_intrinsic_load_ring_es2gs_offset_amd:
+   case nir_intrinsic_load_workgroup_num_input_vertices_amd:
+   case nir_intrinsic_load_workgroup_num_input_primitives_amd:
+   case nir_intrinsic_load_shader_query_enabled_amd:
+   case nir_intrinsic_load_cull_front_face_enabled_amd:
+   case nir_intrinsic_load_cull_back_face_enabled_amd:
+   case nir_intrinsic_load_cull_any_enabled_amd:
       is_divergent = false;
       break;
 
@@ -488,6 +494,11 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
    case nir_intrinsic_load_tlb_color_v3d:
    case nir_intrinsic_load_tess_rel_patch_id_amd:
    case nir_intrinsic_load_gs_vertex_offset_amd:
+   case nir_intrinsic_has_input_vertex_amd:
+   case nir_intrinsic_has_input_primitive_amd:
+   case nir_intrinsic_load_packed_passthrough_primitive_amd:
+   case nir_intrinsic_load_initial_edgeflag_amd:
+   case nir_intrinsic_gds_atomic_add_amd:
       is_divergent = true;
       break;
 
