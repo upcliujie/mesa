77f6f9f92b27bf57c168bf3f8019f77151f174db
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-04-09 10:13:53 -0400

    zink: use VkImageDrmFormatModifierListCreateInfoEXT for creating from modifier array
    Acked-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/gallium/drivers/zink/zink_resource.c
+++ src/gallium/drivers/zink/zink_resource.c
@@ -540,6 +540,7 @@ resource_object_create(struct zink_screen *screen, const struct pipe_resource *t
       uint64_t mod = create_ici(screen, &ici, templ, templ->bind, ici_modifier_count, ici_modifiers, &success);
       VkExternalMemoryImageCreateInfo emici = {0};
       VkImageDrmFormatModifierExplicitCreateInfoEXT idfmeci = {0};
+      VkImageDrmFormatModifierListCreateInfoEXT idfmlci;
 
       if (!success)
          goto fail1;
@@ -573,6 +574,12 @@ resource_object_create(struct zink_screen *screen, const struct pipe_resource *t
             idfmeci.pPlaneLayouts = &plane_layout;
 
             ici.pNext = &idfmeci;
+         } else if (ici.tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT) {
+            idfmlci.sType = VK_STRUCTURE_TYPE_IMAGE_DRM_FORMAT_MODIFIER_LIST_CREATE_INFO_EXT;
+            idfmlci.pNext = ici.pNext;
+            idfmlci.drmFormatModifierCount = 1;
+            idfmlci.pDrmFormatModifiers = &mod;
+            ici.pNext = &idfmlci;
          } else
 #endif
          if (ici.tiling == VK_IMAGE_TILING_OPTIMAL) {
