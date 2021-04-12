40dbd9a6c96786c60c7d1e28bc64c61325edded0
Icecream95 <ixn@disroot.org>
2021-04-12 23:57:25 +1200

    panfrost: Push constant range combining
    Makes panfrost_emit_const_buf about twice as fast.

--- src/panfrost/lib/pan_indirect_dispatch.c
+++ src/panfrost/lib/pan_indirect_dispatch.c
@@ -105,8 +105,11 @@ get_push_uniforms(struct pan_pool *pool,
         uint32_t *out = push_consts_buf.cpu;
         uint8_t *in = (uint8_t *)inputs;
 
-        for (unsigned i = 0; i < dev->indirect_dispatch.push.count; ++i)
-                memcpy(out + i, in + dev->indirect_dispatch.push.ranges[i].offset, 4);
+        for (unsigned i = 0; i < dev->indirect_dispatch.push.num_ranges; ++i) {
+                struct panfrost_ubo_range src = dev->indirect_dispatch.push.ranges[i];
+                memcpy(out, in + src.offset, src.size * 4);
+                out += src.size;
+        }
 
         return push_consts_buf.gpu;
 }
