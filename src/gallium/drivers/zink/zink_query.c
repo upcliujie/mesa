Grima04/mesa-turnip-kgsl

commit 00fc85a01194e9fc150d4a0ec80826f2f16504ee

zink: reset queries when suspending if >50% of total pool is used
this is a great time for resets since there's never a renderpass active,
so an early reset here may mean avoiding a renderpass split later

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10049>

src/gallium/drivers/zink/zink_query.c
@@ -780,6 +780,8 @@ zink_suspend_queries(struct zink_context *ctx, struct zink_batch *batch)
       }
       if (query->needs_update)
          update_qbo(ctx, query);
+      if (query->curr_query > NUM_QUERIES / 2)
+         reset_pool(ctx, batch, query);
    }
 }
 

upcliujie/mesa

commit 8c94fcf28173415dbc05effcb6725b5c9075c825

zink: only reset query on suspend if the query has previously been stopped
if the query has never been stopped, then doing an implicit reset here does
nothing except give the gpu more work

Fixes: 00fc85a0119 ("zink: reset queries when suspending if >50% of total pool is used")

src/gallium/drivers/zink/zink_query.c
@@ -780,7 +780,7 @@ zink_suspend_queries(struct zink_context *ctx, struct zink_batch *batch)
       }
       if (query->needs_update)
          update_qbo(ctx, query);
-      if (query->curr_query > NUM_QUERIES / 2)
+      if (query->last_start && query->curr_query > NUM_QUERIES / 2)
          reset_pool(ctx, batch, query);
    }
 }