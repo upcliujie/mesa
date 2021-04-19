Grima04/mesa-turnip-kgsl

commit cdb9a4775bff5c6510eb985891120668ac8f5664

zink: set PIPE_CAP_TEXTURE_BORDER_COLOR_QUIRK
non-intel platforms need border colors pre-swizzled

this is an internal khronos spec bug that will (someday) be resolved in
a more detectable manner

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9136>

src/gallium/drivers/zink/zink_screen.c
@@ -273,6 +273,11 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_GL_CLAMP:
       return 0;
 
+   case PIPE_CAP_TEXTURE_BORDER_COLOR_QUIRK:
+      return screen->info.driver_props.driverID == VK_DRIVER_ID_INTEL_OPEN_SOURCE_MESA_KHR ||
+             screen->info.driver_props.driverID == VK_DRIVER_ID_INTEL_PROPRIETARY_WINDOWS_KHR ?
+             0 : PIPE_QUIRK_TEXTURE_BORDER_COLOR_SWIZZLE_NV50;
+
    case PIPE_CAP_MAX_TEXTURE_2D_SIZE:
       return screen->info.props.limits.maxImageDimension2D;
    case PIPE_CAP_MAX_TEXTURE_3D_LEVELS:

upcliujie/mesa

commit 387e947a1573b8000f59a92dfbf6169e18409b5a

zink: remove incorrect border-swizzle assumption
According to the Khronos issue[1], it seems like RADV is in the wrong, so
let's turn the logic around here.

This makes us do the right thing for Lavapipe, and we should probablt
just remove the whole block once !9731 lands.

[1]: https://github.com/KhronosGroup/Vulkan-Docs/issues/1421

Fixes: cdb9a4775bf ("zink: set PIPE_CAP_TEXTURE_BORDER_COLOR_QUIRK")

src/gallium/drivers/zink/zink_screen.c
@@ -319,8 +319,7 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return 0;
 
    case PIPE_CAP_TEXTURE_BORDER_COLOR_QUIRK:
-      return screen->info.driver_props.driverID == VK_DRIVER_ID_INTEL_OPEN_SOURCE_MESA_KHR ||
-             screen->info.driver_props.driverID == VK_DRIVER_ID_INTEL_PROPRIETARY_WINDOWS_KHR ?
+      return screen->info.driver_props.driverID != VK_DRIVER_ID_MESA_RADV ?
              0 : PIPE_QUIRK_TEXTURE_BORDER_COLOR_SWIZZLE_NV50;
 
    case PIPE_CAP_MAX_TEXTURE_2D_SIZE: