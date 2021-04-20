Grima04/mesa-turnip-kgsl

commit 9a937330efbc21de64a85a4e80d1e3e92ef2a750

radeonsi: Only set modifier creation function for GFX9+ & with kernel support.
Fixes: c786150dfa5 ("radeonsi: Add modifier support.")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3963
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8407>

src/amd/common/ac_gpu_info.c
@@ -672,7 +672,8 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    info->has_read_registers_query = true;
    info->has_scheduled_fence_dependency = info->drm_minor >= 28;
    info->mid_command_buffer_preemption_enabled = amdinfo->ids_flags & AMDGPU_IDS_FLAGS_PREEMPTION;
-	info->has_tmz_support = has_tmz_support(dev, info, amdinfo);
+   info->has_tmz_support = has_tmz_support(dev, info, amdinfo);
+   info->kernel_has_modifiers = info->chip_class >= GFX9 && info->drm_minor >= 40;
 
    info->pa_sc_tile_steering_override = device_info.pa_sc_tile_steering_override;
    info->max_render_backends = amdinfo->rb_pipes;

src/amd/common/ac_gpu_info.h
@@ -161,6 +161,7 @@ struct radeon_info {
    /* Whether SR-IOV is enabled or amdgpu.mcbp=1 was set on the kernel command line. */
    bool mid_command_buffer_preemption_enabled;
    bool has_tmz_support;
+   bool kernel_has_modifiers;
 
    /* Shader cores. */
    uint32_t cu_mask[4][2];

src/gallium/drivers/radeonsi/si_texture.c
@@ -2476,13 +2476,20 @@ void si_init_screen_texture_functions(struct si_screen *sscreen)
    sscreen->b.resource_get_param = si_resource_get_param;
    sscreen->b.resource_get_info = si_texture_get_info;
    sscreen->b.resource_from_memobj = si_resource_from_memobj;
-   sscreen->b.resource_create_with_modifiers = si_texture_create_with_modifiers;
    sscreen->b.memobj_create_from_handle = si_memobj_from_handle;
    sscreen->b.memobj_destroy = si_memobj_destroy;
    sscreen->b.check_resource_capability = si_check_resource_capability;
-   sscreen->b.query_dmabuf_modifiers = si_query_dmabuf_modifiers;
-   sscreen->b.is_dmabuf_modifier_supported = si_is_dmabuf_modifier_supported;
-   sscreen->b.get_dmabuf_modifier_planes = si_get_dmabuf_modifier_planes;
+
+   /* By not setting it the frontend will fall back to non-modifier create,
+    * which works around some applications using modifiers that are not
+    * allowed in combination with lack of error reporting in
+    * gbm_dri_surface_create */
+   if (sscreen->info.chip_class >= GFX9 && sscreen->info.kernel_has_modifiers) {
+      sscreen->b.resource_create_with_modifiers = si_texture_create_with_modifiers;
+      sscreen->b.query_dmabuf_modifiers = si_query_dmabuf_modifiers;
+      sscreen->b.is_dmabuf_modifier_supported = si_is_dmabuf_modifier_supported;
+      sscreen->b.get_dmabuf_modifier_planes = si_get_dmabuf_modifier_planes;
+   }
 }
 
 void si_init_context_texture_functions(struct si_context *sctx)

upcliujie/mesa

commit 20ba754015a3455387682792e338ddb02c64e585

amd/common: Use cap to test kernel modifier support.
Turns out both kernel v5.10 and v5.11 have the same amdgpu driver
version and only one has modifiers ... In addition the version check
is kinda annoying for backports.

So lets use the cap. Since the cap is technically about ADDFB2 I
tested that this works on rendernodes (and reading the code there
is no distinction from what kind of node this is called).

Fixes: 9a937330efb ("radeonsi: Only set modifier creation function for GFX9+ & with kernel support.")

src/amd/common/ac_gpu_info.c
@@ -34,6 +34,7 @@
 #include <stdio.h>
 
 #ifdef _WIN32
+#define DRM_CAP_ADDFB2_MODIFIERS 0x10
 #define DRM_CAP_SYNCOBJ 0x13
 #define DRM_CAP_SYNCOBJ_TIMELINE 0x14
 #define AMDGPU_GEM_DOMAIN_GTT 0x2
@@ -274,6 +275,14 @@ static bool has_timeline_syncobj(int fd)
 {
    uint64_t value;
    if (drmGetCap(fd, DRM_CAP_SYNCOBJ_TIMELINE, &value))
+      return false;
+   return value ? true : false;
+}
+
+static bool has_modifiers(int fd)
+{
+   uint64_t value;
+   if (drmGetCap(fd, DRM_CAP_ADDFB2_MODIFIERS, &value))
       return false;
    return value ? true : false;
 }
@@ -705,7 +714,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    info->has_scheduled_fence_dependency = info->drm_minor >= 28;
    info->mid_command_buffer_preemption_enabled = amdinfo->ids_flags & AMDGPU_IDS_FLAGS_PREEMPTION;
    info->has_tmz_support = has_tmz_support(dev, info, amdinfo);
-   info->kernel_has_modifiers = info->chip_class >= GFX9 && info->drm_minor >= 40;
+   info->kernel_has_modifiers = has_modifiers(fd);
    info->has_graphics = gfx.available_rings > 0;
 
    info->pa_sc_tile_steering_override = device_info.pa_sc_tile_steering_override;