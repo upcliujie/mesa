050a098adb1e488e832d83d5bee497236e82936e
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-04-21 12:30:54 +0200

    zink: remove needless shader-info from context
    There's no longer any code reading this, so let's get rid of it. It
    always rubbed me a bit the wrong way, because this repeated some
    information already present in the context.

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -73,8 +73,6 @@ struct ntv_context {
    bool block_started;
    SpvId loop_break, loop_cont;
 
-   struct shader_info *info;
-
    SpvId front_face_var, instance_id_var, vertex_id_var,
          primitive_id_var, invocation_id_var, // geometry
          sample_mask_type, sample_id_var, sample_pos_var, sample_mask_in_var,
@@ -3467,8 +3465,6 @@ nir_to_spirv(struct nir_shader *s, const struct zink_so_info *so_info)
 
    default: ;
    }
-
-   ctx.info = &s->info;
 
    if (s->info.stage < MESA_SHADER_GEOMETRY) {
       if (s->info.outputs_written & BITFIELD64_BIT(VARYING_SLOT_LAYER) ||
