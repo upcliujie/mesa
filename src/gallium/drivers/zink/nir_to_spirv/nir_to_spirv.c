d6b74306c81f18079f8901dad2ab7e860dfac16c
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-04-21 12:09:45 +0200

    zink: always enable fixed shader-caps
    This is required for a bunch of stuff that can occur in any Vulkan
    shader stage, not just these few. So let's always emit this cap.

--- src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
+++ src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -3440,15 +3440,11 @@ nir_to_spirv(struct nir_shader *s, const struct zink_so_info *so_info)
    ctx.mem_ctx = ralloc_context(NULL);
    ctx.builder.mem_ctx = ctx.mem_ctx;
 
-   switch (s->info.stage) {
-   case MESA_SHADER_VERTEX:
-   case MESA_SHADER_FRAGMENT:
-   case MESA_SHADER_COMPUTE:
-      spirv_builder_emit_cap(&ctx.builder, SpvCapabilityShader);
-      spirv_builder_emit_cap(&ctx.builder, SpvCapabilityImageBuffer);
-      spirv_builder_emit_cap(&ctx.builder, SpvCapabilitySampledBuffer);
-      break;
+   spirv_builder_emit_cap(&ctx.builder, SpvCapabilityShader);
+   spirv_builder_emit_cap(&ctx.builder, SpvCapabilityImageBuffer);
+   spirv_builder_emit_cap(&ctx.builder, SpvCapabilitySampledBuffer);
 
+   switch (s->info.stage) {
    case MESA_SHADER_TESS_CTRL:
    case MESA_SHADER_TESS_EVAL:
       spirv_builder_emit_cap(&ctx.builder, SpvCapabilityTessellation);
@@ -3464,9 +3460,6 @@ nir_to_spirv(struct nir_shader *s, const struct zink_so_info *so_info)
       if (s->info.outputs_written & BITFIELD64_BIT(VARYING_SLOT_PSIZ))
          spirv_builder_emit_cap(&ctx.builder, SpvCapabilityGeometryPointSize);
       break;
-
-   default:
-      unreachable("invalid stage");
    }
 
    ctx.info = &s->info;
