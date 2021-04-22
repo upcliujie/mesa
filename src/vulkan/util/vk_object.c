0f0a264349ce1ab706afb10ffdb88ebbc7b696c9
Yevhenii Kolesnikov <yevhenii.kolesnikov@globallogic.com>
2021-04-22 23:32:10 +0300

    vulkan: Add convenience debug message helpers
    Forwards the message to both VK_EXT_debug_utils and
    VK_EXT_debug_report provided callbacks.
    
    Signed-off-by: Yevhenii Kolesnikov <yevhenii.kolesnikov@globallogic.com>

--- src/vulkan/util/vk_object.c
+++ src/vulkan/util/vk_object.c
@@ -325,3 +325,83 @@ vk_common_GetPrivateDataEXT(VkDevice _device,
                                    objectType, objectHandle,
                                    privateDataSlot, pData);
 }
+
+static const char *
+vk_ObjectName_from_ObjectType(VkObjectType type)
+{
+   switch (type) {
+   case VK_OBJECT_TYPE_INSTANCE:              return "VkInstance";
+   case VK_OBJECT_TYPE_PHYSICAL_DEVICE:       return "VkPhysicalDevice";
+   case VK_OBJECT_TYPE_DEVICE:                return "VkDevice";
+   case VK_OBJECT_TYPE_QUEUE:                 return "VkQueue";
+   case VK_OBJECT_TYPE_SEMAPHORE:             return "VkSemaphore";
+   case VK_OBJECT_TYPE_COMMAND_BUFFER:        return "VkCommandBuffer";
+   case VK_OBJECT_TYPE_FENCE:                 return "VkFence";
+   case VK_OBJECT_TYPE_DEVICE_MEMORY:         return "VkDeviceMemory";
+   case VK_OBJECT_TYPE_BUFFER:                return "VkBuffer";
+   case VK_OBJECT_TYPE_IMAGE:                 return "VkImage";
+   case VK_OBJECT_TYPE_EVENT:                 return "VkEvent";
+   case VK_OBJECT_TYPE_QUERY_POOL:            return "VkQueryPool";
+   case VK_OBJECT_TYPE_BUFFER_VIEW:           return "VkBufferView";
+   case VK_OBJECT_TYPE_IMAGE_VIEW:            return "VkImageView";
+   case VK_OBJECT_TYPE_SHADER_MODULE:         return "VkShaderModule";
+   case VK_OBJECT_TYPE_PIPELINE_CACHE:        return "VkPipelineCache";
+   case VK_OBJECT_TYPE_PIPELINE_LAYOUT:       return "VkPipelineLayout";
+   case VK_OBJECT_TYPE_RENDER_PASS:           return "VkRenderPass";
+   case VK_OBJECT_TYPE_PIPELINE:              return "VkPipeline";
+   case VK_OBJECT_TYPE_DESCRIPTOR_SET_LAYOUT: return "VkDescriptorSetLayout";
+   case VK_OBJECT_TYPE_SAMPLER:               return "VkSampler";
+   case VK_OBJECT_TYPE_DESCRIPTOR_POOL:       return "VkDescriptorPool";
+   case VK_OBJECT_TYPE_DESCRIPTOR_SET:        return "VkDescriptorSet";
+   case VK_OBJECT_TYPE_FRAMEBUFFER:           return "VkFramebuffer";
+   case VK_OBJECT_TYPE_COMMAND_POOL:          return "VkCommandPool";
+   case VK_OBJECT_TYPE_SAMPLER_YCBCR_CONVERSION:
+      return "VkSamplerYcbcrConversion";
+   case VK_OBJECT_TYPE_DESCRIPTOR_UPDATE_TEMPLATE:
+      return "VkDescriptorUpdateTemplate";
+   case VK_OBJECT_TYPE_SURFACE_KHR:           return "VkSurfaceKHR";
+   case VK_OBJECT_TYPE_SWAPCHAIN_KHR:         return "VkSwapchainKHR";
+   case VK_OBJECT_TYPE_DISPLAY_KHR:           return "VkDisplayKHR";
+   case VK_OBJECT_TYPE_DISPLAY_MODE_KHR:      return "VkDisplayModeKHR";
+   case VK_OBJECT_TYPE_DEBUG_REPORT_CALLBACK_EXT:
+      return "VkDebugReportCallbackEXT";
+   case VK_OBJECT_TYPE_INDIRECT_COMMANDS_LAYOUT_NV:
+      return "VkIndirectCommandsLayoutNV";
+   case VK_OBJECT_TYPE_DEBUG_UTILS_MESSENGER_EXT:
+      return "VkDebugUtilsMessengerEXT";
+   case VK_OBJECT_TYPE_VALIDATION_CACHE_EXT:
+      return "VkValidationCacheEXT";
+   case VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_NV:
+      return "VkAccelerationStructureNV";
+   case VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_KHR:
+      return "VkAccelerationStructureKHR";
+   case VK_OBJECT_TYPE_PERFORMANCE_CONFIGURATION_INTEL:
+      return "VkPerformanceConfigurationINTEL";
+   case VK_OBJECT_TYPE_DEFERRED_OPERATION_KHR:
+      return "VkDeferredOperationKHR";
+   case VK_OBJECT_TYPE_PRIVATE_DATA_SLOT_EXT:
+      return "VkPrivateDataSlotEXT";
+   default:
+      unreachable("Invalid object type.");
+   }
+}
+
+const char *
+vk_object_base_name(struct vk_object_base *obj)
+{
+   if (obj->object_name)
+      return obj->object_name;
+
+   int len = snprintf(NULL, 0, "%s(0x%"PRIx64")",
+                      vk_ObjectName_from_ObjectType(obj->type),
+                      (uint64_t)obj);
+
+   obj->object_name = vk_alloc(&obj->device->alloc, len + 1, 1,
+                               VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+
+   snprintf(obj->object_name, len + 1, "%s(0x%" PRIx64 ")",
+            vk_ObjectName_from_ObjectType(obj->type),
+            (uint64_t)obj);
+
+   return obj->object_name;
+}
