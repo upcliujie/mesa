6407f7f6330ce0c1c52cb6551886bc951c82a585
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-04-23 14:48:19 +0300

    tu: implement dynamic rasterizer discard enable
    Initialization of pipeline builder had to be moved because it
    now depends on whether rasterizer-discard dynamic state is enabled.
    
    The state which could be omitted with rasterization discard
    enabled - is unconditionally emitted when discard is a dynamic
    state. It's not an optimal way, but does not intruduce much complexity.
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/freedreno/vulkan/tu_private.h
+++ src/freedreno/vulkan/tu_private.h
@@ -462,6 +462,7 @@ enum tu_dynamic_state
    TU_DYNAMIC_STATE_RB_DEPTH_CNTL,
    TU_DYNAMIC_STATE_RB_STENCIL_CNTL,
    TU_DYNAMIC_STATE_VB_STRIDE,
+   TU_DYNAMIC_STATE_RASTERIZER_DISCARD,
    TU_DYNAMIC_STATE_COUNT,
    /* no associated draw state: */
    TU_DYNAMIC_STATE_PRIMITIVE_TOPOLOGY = TU_DYNAMIC_STATE_COUNT,
@@ -682,8 +683,9 @@ enum tu_cmd_dirty_bits
    TU_CMD_DIRTY_COMPUTE_DESC_SETS_LOAD = BIT(6),
    TU_CMD_DIRTY_SHADER_CONSTS = BIT(7),
    TU_CMD_DIRTY_LRZ = BIT(8),
+   TU_CMD_DIRTY_RASTERIZER_DISCARD = BIT(9),
    /* all draw states were disabled and need to be re-enabled: */
-   TU_CMD_DIRTY_DRAW_STATE = BIT(9)
+   TU_CMD_DIRTY_DRAW_STATE = BIT(10)
 };
 
 /* There are only three cache domains we have to care about: the CCU, or
@@ -890,6 +892,7 @@ struct tu_cmd_state
    uint32_t dynamic_stencil_ref;
 
    uint32_t gras_su_cntl, rb_depth_cntl, rb_stencil_cntl;
+   uint32_t pc_raster_cntl, vpc_unknown_9107;
    enum pc_di_primtype primtype;
    bool primitive_restart_enable;
 
@@ -1109,6 +1112,8 @@ struct tu_pipeline
    uint32_t gras_su_cntl, gras_su_cntl_mask;
    uint32_t rb_depth_cntl, rb_depth_cntl_mask;
    uint32_t rb_stencil_cntl, rb_stencil_cntl_mask;
+   uint32_t pc_raster_cntl, pc_raster_cntl_mask;
+   uint32_t vpc_unknown_9107, vpc_unknown_9107_mask;
    uint32_t stencil_wrmask;
 
    bool rb_depth_cntl_disable;
