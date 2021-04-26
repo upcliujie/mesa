Grima04/mesa-turnip-kgsl

commit 01ba8a8d02ba04575860570ce51fc282c9b7866e

glx: Implement GLX_EXT_no_config_context
This is the GLX counterpart to EGL_KHR_no_config_context. Contexts may
now be created without reference to an fbconfig, in which case it is
treated as compatible with any fbconfig (and thus any GLX drawable).

Khronos: https://github.com/KhronosGroup/OpenGL-Registry/pull/102

Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Acked-by: Eric Anholt <eric@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8999>

src/glx/create_context.c
@@ -48,19 +48,10 @@ glXCreateContextAttribsARB(Display *dpy, GLXFBConfig config,
    xcb_void_cookie_t cookie;
    unsigned dummy_err = 0;
    uint32_t xid, share_xid;
+   int screen = -1;
 
-   if (dpy == NULL || cfg == NULL)
+   if (dpy == NULL)
       return NULL;
-
-   /* This means that either the caller passed the wrong display pointer or
-    * one of the internal GLX data structures (probably the fbconfig) has an
-    * error.  There is nothing sensible to do, so return an error.
-    */
-   psc = GetGLXScreenConfigs(dpy, cfg->screen);
-   if (psc == NULL)
-      return NULL;
-
-   assert(cfg->screen == psc->scr);
 
    /* Count the number of attributes specified by the application.  All
     * attributes appear in pairs, except the terminating None.
@@ -69,6 +60,29 @@ glXCreateContextAttribsARB(Display *dpy, GLXFBConfig config,
       for (/* empty */; attrib_list[num_attribs * 2] != 0; num_attribs++)
 	 /* empty */ ;
    }
+
+   if (cfg) {
+      screen = cfg->screen;
+   } else {
+      for (unsigned int i = 0; i < num_attribs; i++) {
+         if (attrib_list[i * 2] == GLX_SCREEN)
+            screen = attrib_list[i * 2 + 1];
+      }
+      if (screen == -1) {
+         __glXSendError(dpy, BadValue, 0, X_GLXCreateContextAttribsARB, True);
+         return NULL;
+      }
+   }
+
+   /* This means that either the caller passed the wrong display pointer or
+    * one of the internal GLX data structures (probably the fbconfig) has an
+    * error.  There is nothing sensible to do, so return an error.
+    */
+   psc = GetGLXScreenConfigs(dpy, screen);
+   if (psc == NULL)
+      return NULL;
+
+   assert(screen == psc->scr);
 
    if (direct && psc->vtable->create_context_attribs) {
       /* GLX drops the error returned by the driver.  The expectation is that
@@ -104,8 +118,8 @@ glXCreateContextAttribsARB(Display *dpy, GLXFBConfig config,
    cookie =
       xcb_glx_create_context_attribs_arb_checked(c,
 						 xid,
-						 cfg->fbconfigID,
-						 cfg->screen,
+						 cfg ? cfg->fbconfigID : 0,
+						 screen,
 						 share_xid,
 						 gc ? gc->isDirect : direct,
 						 num_attribs,

src/glx/applegl_glx.c
@@ -130,6 +130,9 @@ applegl_create_context(struct glx_screen *psc,
    /* TODO: Integrate this with apple_glx_create_context and make
     * struct apple_glx_context inherit from struct glx_context. */
 
+   if (!config)
+      return NULL;
+
    gc = calloc(1, sizeof(*gc));
    if (gc == NULL)
       return NULL;

src/glx/dri2_glx.c
@@ -1082,6 +1082,7 @@ dri2BindExtensions(struct dri2_screen *psc, struct glx_display * priv,
 
       __glXEnableDirectExtension(&psc->base, "GLX_ARB_create_context");
       __glXEnableDirectExtension(&psc->base, "GLX_ARB_create_context_profile");
+      __glXEnableDirectExtension(&psc->base, "GLX_EXT_no_config_context");
 
       if ((mask & ((1 << __DRI_API_GLES) |
                    (1 << __DRI_API_GLES2) |

src/glx/dri3_glx.c
@@ -740,6 +740,7 @@ dri3_bind_extensions(struct dri3_screen *psc, struct glx_display * priv,
 
    __glXEnableDirectExtension(&psc->base, "GLX_ARB_create_context");
    __glXEnableDirectExtension(&psc->base, "GLX_ARB_create_context_profile");
+   __glXEnableDirectExtension(&psc->base, "GLX_EXT_no_config_context");
 
    if ((mask & ((1 << __DRI_API_GLES) |
                 (1 << __DRI_API_GLES2) |

src/glx/dri_common.c
@@ -506,6 +506,10 @@ dri2_convert_glx_attribs(unsigned num_attribs, const uint32_t *attribs,
             return false;
          }
          break;
+      case GLX_SCREEN:
+         /* Implies GLX_EXT_no_config_context */
+         *render_type = GLX_DONT_CARE;
+         break;
       default:
 	 /* If an unknown attribute is received, fail.
 	  */

src/glx/drisw_glx.c
@@ -748,6 +748,7 @@ driswBindExtensions(struct drisw_screen *psc, const __DRIextension **extensions)
    if (psc->swrast->base.version >= 3) {
       __glXEnableDirectExtension(&psc->base, "GLX_ARB_create_context");
       __glXEnableDirectExtension(&psc->base, "GLX_ARB_create_context_profile");
+      __glXEnableDirectExtension(&psc->base, "GLX_EXT_no_config_context");
 
       /* DRISW version >= 2 implies support for OpenGL ES.
        */

src/glx/driwindows_glx.c
@@ -200,7 +200,7 @@ driwindows_create_context_attribs(struct glx_screen *base,
      identical values, so far
    */
 
-   if (!psc->base.driScreen)
+   if (!psc->base.driScreen || !config_base)
       return NULL;
 
    /* Check the renderType value */

src/glx/g_glxglvnddispatchfuncs.c
@@ -161,7 +161,19 @@ static GLXContext dispatch_CreateContextAttribsARB(Display *dpy,
     __GLXvendorInfo *dd;
     GLXContext ret;
 
-    dd = GetDispatchFromFBConfig(dpy, config);
+    if (config) {
+       dd = GetDispatchFromFBConfig(dpy, config);
+    } else if (attrib_list) {
+       int i, screen;
+
+       for (i = 0; attrib_list[i * 2] != None; i++) {
+          if (attrib_list[i * 2] == GLX_SCREEN) {
+             screen = attrib_list[i * 2 + 1];
+             dd = GetDispatchFromDrawable(dpy, RootWindow(dpy, screen));
+             break;
+          }
+       }
+    }
     if (dd == NULL)
         return None;
 

upcliujie/mesa

commit db8b2bf1aff04266976984ad874ded96a494d8ed

glx: fix compilation error when function name not found
Patch workarounds the problem like commit eec904d29c0 did, this fixes
compilation on older distributions that still use old header.

Fixes: 01ba8a8d02b ("glx: Implement GLX_EXT_no_config_context")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>

src/glx/create_context.c
@@ -33,6 +33,14 @@
 #error This code requires sizeof(uint32_t) == sizeof(int).
 #endif
 
+/* An "Atrribs/Attribs" typo was fixed in glxproto.h in Nov 2014.
+ * This is in case we don't have the updated header.
+ */
+#if !defined(X_GLXCreateContextAttribsARB) && \
+     defined(X_GLXCreateContextAtrribsARB)
+#define X_GLXCreateContextAttribsARB X_GLXCreateContextAtrribsARB
+#endif
+
 _X_HIDDEN GLXContext
 glXCreateContextAttribsARB(Display *dpy, GLXFBConfig config,
 			   GLXContext share_context, Bool direct,