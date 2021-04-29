Grima04/mesa-turnip-kgsl

commit bc4a127d6e10318d48fa8b540b9c1ff7d62c8d29

intel/disasm: Label support in shader disassembly for UIP/JIP
Shader instructions which use UIP/JIP now get formatted with a label
in addition with immediate value, labels have "LABEL%d" format.

v2: - Consider brw_jump_scale when calculating label's offset

From: "Lonnberg, Toni" <toni.lonnberg@intel.com>
Signed-off-by: Danylo Piliaiev <danylo.piliaiev@globallogic.com>
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Matt Turner <mattst88@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4245>

src/intel/compiler/brw_eu.cpp
@@ -457,15 +457,84 @@ brw_create_label(struct brw_label **labels, int offset, void *mem_ctx)
    }
 }
 
+const struct brw_label *
+brw_label_assembly(const struct gen_device_info *devinfo,
+                   const void *assembly, int start, int end, void *mem_ctx)
+{
+   struct brw_label *root_label = NULL;
+
+   int to_bytes_scale = sizeof(brw_inst) / brw_jump_scale(devinfo);
+
+   for (int offset = start; offset < end;) {
+      const brw_inst *inst = (const brw_inst *) ((const char *) assembly + offset);
+      brw_inst uncompacted;
+
+      bool is_compact = brw_inst_cmpt_control(devinfo, inst);
+
+      if (is_compact) {
+         brw_compact_inst *compacted = (brw_compact_inst *)inst;
+         brw_uncompact_instruction(devinfo, &uncompacted, compacted);
+         inst = &uncompacted;
+      }
+
+      if (brw_has_uip(devinfo, brw_inst_opcode(devinfo, inst))) {
+         /* Instructions that have UIP also have JIP. */
+         brw_create_label(&root_label,
+            offset + brw_inst_uip(devinfo, inst) * to_bytes_scale, mem_ctx);
+         brw_create_label(&root_label,
+            offset + brw_inst_jip(devinfo, inst) * to_bytes_scale, mem_ctx);
+      } else if (brw_has_jip(devinfo, brw_inst_opcode(devinfo, inst))) {
+         int jip;
+         if (devinfo->gen >= 7) {
+            jip = brw_inst_jip(devinfo, inst);
+         } else {
+            jip = brw_inst_gen6_jump_count(devinfo, inst);
+         }
+
+         brw_create_label(&root_label, offset + jip * to_bytes_scale, mem_ctx);
+      }
+
+      if (is_compact) {
+         offset += sizeof(brw_compact_inst);
+      } else {
+         offset += sizeof(brw_inst);
+      }
+   }
+
+   return root_label;
+}
+
+void
+brw_disassemble_with_labels(const struct gen_device_info *devinfo,
+                            const void *assembly, int start, int end, FILE *out)
+{
+   void *mem_ctx = ralloc_context(NULL);
+   const struct brw_label *root_label =
+      brw_label_assembly(devinfo, assembly, start, end, mem_ctx);
+
+   brw_disassemble(devinfo, assembly, start, end, root_label, out);
+
+   ralloc_free(mem_ctx);
+}
+
 void
 brw_disassemble(const struct gen_device_info *devinfo,
-                const void *assembly, int start, int end, FILE *out)
+                const void *assembly, int start, int end,
+                const struct brw_label *root_label, FILE *out)
 {
    bool dump_hex = (INTEL_DEBUG & DEBUG_HEX) != 0;
 
    for (int offset = start; offset < end;) {
       const brw_inst *insn = (const brw_inst *)((char *)assembly + offset);
       brw_inst uncompacted;
+
+      if (root_label != NULL) {
+        const struct brw_label *label = brw_find_label(root_label, offset);
+        if (label != NULL) {
+           fprintf(out, "\nLABEL%d:\n", label->number);
+        }
+      }
+
       bool compacted = brw_inst_cmpt_control(devinfo, insn);
       if (0)
          fprintf(out, "0x%08x: ", offset);
@@ -490,8 +559,6 @@ brw_disassemble(const struct gen_device_info *devinfo,
 
          brw_uncompact_instruction(devinfo, &uncompacted, compacted);
          insn = &uncompacted;
-         offset += 8;
-      } else {
          if (dump_hex) {
             unsigned char * insn_ptr = ((unsigned char *)&insn[0]);
             for (int i = 0 ; i < 16; i = i + 4) {
@@ -502,10 +569,15 @@ brw_disassemble(const struct gen_device_info *devinfo,
                        insn_ptr[i + 3]);
             }
          }
-         offset += 16;
       }
 
-      brw_disassemble_inst(out, devinfo, insn, compacted);
+      brw_disassemble_inst(out, devinfo, insn, compacted, offset, root_label);
+
+      if (compacted) {
+         offset += sizeof(brw_compact_inst);
+      } else {
+         offset += sizeof(brw_inst);
+      }
    }
 }
 

src/gallium/drivers/iris/iris_program_cache.c
@@ -365,7 +365,7 @@ iris_print_program_cache(struct iris_context *ice)
       const struct keybox *keybox = entry->key;
       struct iris_compiled_shader *shader = entry->data;
       fprintf(stderr, "%s:\n", cache_name(keybox->cache_id));
-      brw_disassemble(devinfo, shader->map, 0,
-                      shader->prog_data->program_size, stderr);
+      brw_disassemble_with_labels(devinfo, shader->map, 0,
+                                  shader->prog_data->program_size, stderr);
    }
 }

src/intel/common/gen_disasm.c
@@ -84,6 +84,10 @@ gen_disasm_disassemble(struct gen_disasm *disasm, const void *assembly,
 
    brw_validate_instructions(devinfo, assembly, start, end, disasm_info);
 
+   void *mem_ctx = ralloc_context(NULL);
+   const struct brw_label *root_label =
+      brw_label_assembly(devinfo, assembly, start, end, mem_ctx);
+
    foreach_list_typed(struct inst_group, group, link,
                       &disasm_info->group_list) {
       struct exec_node *next_node = exec_node_get_next(&group->link);
@@ -96,13 +100,15 @@ gen_disasm_disassemble(struct gen_disasm *disasm, const void *assembly,
       int start_offset = group->offset;
       int end_offset = next->offset;
 
-      brw_disassemble(devinfo, assembly, start_offset, end_offset, out);
+      brw_disassemble(devinfo, assembly, start_offset, end_offset,
+                      root_label, out);
 
       if (group->error) {
          fputs(group->error, out);
       }
    }
 
+   ralloc_free(mem_ctx);
    ralloc_free(disasm_info);
 }
 

src/intel/compiler/brw_compile_clip.c
@@ -87,8 +87,8 @@ brw_compile_clip(const struct brw_compiler *compiler,
 
    if (unlikely(INTEL_DEBUG & DEBUG_CLIP)) {
       fprintf(stderr, "clip:\n");
-      brw_disassemble(compiler->devinfo,
-                      program, 0, *final_assembly_size, stderr);
+      brw_disassemble_with_labels(compiler->devinfo,
+                                  program, 0, *final_assembly_size, stderr);
       fprintf(stderr, "\n");
    }
 

src/intel/compiler/brw_compile_sf.c
@@ -870,8 +870,8 @@ brw_compile_sf(const struct brw_compiler *compiler,
 
    if (unlikely(INTEL_DEBUG & DEBUG_SF)) {
       fprintf(stderr, "sf:\n");
-      brw_disassemble(compiler->devinfo,
-                      program, 0, *final_assembly_size, stderr);
+      brw_disassemble_with_labels(compiler->devinfo,
+                                  program, 0, *final_assembly_size, stderr);
       fprintf(stderr, "\n");
    }
 

src/intel/compiler/brw_disasm.c
@@ -1654,13 +1654,29 @@ brw_disassemble_imm(const struct gen_device_info *devinfo,
    brw_inst inst;
    inst.data[0] = (((uint64_t) dw1) << 32) | ((uint64_t) dw0);
    inst.data[1] = (((uint64_t) dw3) << 32) | ((uint64_t) dw2);
-   return brw_disassemble_inst(stderr, devinfo, &inst, false);
+   return brw_disassemble_inst(stderr, devinfo, &inst, false, 0, NULL);
 }
 #endif
 
+static void
+write_label(FILE *file, const struct gen_device_info *devinfo,
+            const struct brw_label *root_label,
+            int offset, int jump)
+{
+   if (root_label != NULL) {
+      int to_bytes_scale = sizeof(brw_inst) / brw_jump_scale(devinfo);
+      const struct brw_label *label =
+         brw_find_label(root_label, offset + jump * to_bytes_scale);
+      if (label != NULL) {
+         format(file, " LABEL%d", label->number);
+      }
+   }
+}
+
 int
 brw_disassemble_inst(FILE *file, const struct gen_device_info *devinfo,
-                     const brw_inst *inst, bool is_compacted)
+                     const brw_inst *inst, bool is_compacted,
+                     int offset, const struct brw_label *root_label)
 {
    int err = 0;
    int space = 0;
@@ -1736,16 +1752,23 @@ brw_disassemble_inst(FILE *file, const struct gen_device_info *devinfo,
    if (brw_has_uip(devinfo, opcode)) {
       /* Instructions that have UIP also have JIP. */
       pad(file, 16);
-      format(file, "JIP: %d", brw_inst_jip(devinfo, inst));
-      pad(file, 32);
-      format(file, "UIP: %d", brw_inst_uip(devinfo, inst));
+      string(file, "JIP: ");
+      write_label(file, devinfo, root_label, offset, brw_inst_jip(devinfo, inst));
+
+      pad(file, 38);
+      string(file, "UIP: ");
+      write_label(file, devinfo, root_label, offset, brw_inst_uip(devinfo, inst));
    } else if (brw_has_jip(devinfo, opcode)) {
-      pad(file, 16);
+      int jip;
       if (devinfo->gen >= 7) {
-         format(file, "JIP: %d", brw_inst_jip(devinfo, inst));
+         jip = brw_inst_jip(devinfo, inst);
       } else {
-         format(file, "JIP: %d", brw_inst_gen6_jump_count(devinfo, inst));
+         jip = brw_inst_gen6_jump_count(devinfo, inst);
       }
+
+      pad(file, 16);
+      string(file, "JIP: ");
+      write_label(file, devinfo, root_label, offset, jip);
    } else if (devinfo->gen < 6 && (opcode == BRW_OPCODE_BREAK ||
                                    opcode == BRW_OPCODE_CONTINUE ||
                                    opcode == BRW_OPCODE_ELSE)) {

src/intel/compiler/brw_disasm_info.c
@@ -31,12 +31,16 @@ __attribute__((weak)) void nir_print_instr(UNUSED const nir_instr *instr,
                                            UNUSED FILE *fp) {}
 
 void
-dump_assembly(void *assembly, struct disasm_info *disasm,
-              const unsigned *block_latency)
+dump_assembly(void *assembly, int start_offset, int end_offset,
+              struct disasm_info *disasm, const unsigned *block_latency)
 {
    const struct gen_device_info *devinfo = disasm->devinfo;
    const char *last_annotation_string = NULL;
    const void *last_annotation_ir = NULL;
+
+   void *mem_ctx = ralloc_context(NULL);
+   const struct brw_label *root_label =
+      brw_label_assembly(devinfo, assembly, start_offset, end_offset, mem_ctx);
 
    foreach_list_typed(struct inst_group, group, link, &disasm->group_list) {
       struct exec_node *next_node = exec_node_get_next(&group->link);
@@ -77,7 +81,8 @@ dump_assembly(void *assembly, struct disasm_info *disasm,
             fprintf(stderr, "   %s\n", last_annotation_string);
       }
 
-      brw_disassemble(devinfo, assembly, start_offset, end_offset, stderr);
+      brw_disassemble(devinfo, assembly, start_offset, end_offset,
+                      root_label, stderr);
 
       if (group->error) {
          fputs(group->error, stderr);
@@ -94,6 +99,8 @@ dump_assembly(void *assembly, struct disasm_info *disasm,
       }
    }
    fprintf(stderr, "\n");
+
+   ralloc_free(mem_ctx);
 }
 
 struct disasm_info *

src/intel/compiler/brw_disasm_info.h
@@ -65,8 +65,8 @@ struct disasm_info {
 };
 
 void
-dump_assembly(void *assembly, struct disasm_info *disasm,
-              const unsigned *block_latency);
+dump_assembly(void *assembly, int start_offset, int end_offset,
+              struct disasm_info *disasm, const unsigned *block_latency);
 
 struct disasm_info *
 disasm_initialize(const struct gen_device_info *devinfo,

upcliujie/mesa

commit 6067e17cb25de71f8805e70c608691aaf1105e56

intel/compiler: Fix INTEL_DEBUG=hex
With the missing else, this prints the compacted hex followed by hex
for an uncompacted version of the compacted instruction. It also
doesn't print hex for instructions that are not compacted.

Cc: Danylo Piliaiev <dpiliaiev@igalia.com>
Fixes: bc4a127d6e1 ("intel/disasm: Label support in shader disassembly for UIP/JIP")
Fixes: https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4245
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>

src/intel/compiler/brw_eu.cpp
@@ -566,6 +566,7 @@ brw_disassemble(const struct intel_device_info *devinfo,
 
          brw_uncompact_instruction(devinfo, &uncompacted, compacted);
          insn = &uncompacted;
+      } else {
          if (dump_hex) {
             unsigned char * insn_ptr = ((unsigned char *)&insn[0]);
             for (int i = 0 ; i < 16; i = i + 4) {