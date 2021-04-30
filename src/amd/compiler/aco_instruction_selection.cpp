a8424cbdc68e16ca634542e6b583f4466e8b0c88
Timur Kristóf <timur.kristof@gmail.com>
2021-04-30 12:24:55 +0200

    aco: Implement the viewport and small prim precision intrinsics.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/amd/compiler/aco_instruction_selection.cpp
+++ src/amd/compiler/aco_instruction_selection.cpp
@@ -8574,8 +8574,31 @@ void visit_intrinsic(isel_context *ctx, nir_intrinsic_instr *instr)
       visit_bvh64_intersect_ray_amd(ctx, instr);
       break;
    case nir_intrinsic_load_cull_any_enabled_amd: {
-      Builder::Result cull_any_enabled = bld.sop2(aco_opcode::s_and_b32, bld.def(s1), bld.def(s1, scc), get_arg(ctx, ctx->args->ngg_gs_state), Operand(0x6u));
+      Builder::Result cull_any_enabled = bld.sop2(aco_opcode::s_and_b32, bld.def(s1), bld.def(s1, scc), get_arg(ctx, ctx->args->ngg_gs_state), Operand(-2u));
       bld.copy(Definition(get_ssa_temp(ctx, &instr->dest.ssa)), bool_to_vector_condition(ctx, cull_any_enabled.def(1).getTemp()));
+      break;
+   }
+   case nir_intrinsic_load_cull_small_prim_precision_amd: {
+      /* Exponent is 8-bit signed int, move that into a signed 32-bit int. */
+      Temp exponent = bld.sop2(aco_opcode::s_ashr_i32, bld.def(s1), bld.def(s1, scc), get_arg(ctx, ctx->args->ngg_gs_state), Operand(24u));
+      /* small_prim_precision = 1.0 * 2^X */
+      bld.vop3(aco_opcode::v_ldexp_f32, Definition(get_ssa_temp(ctx, &instr->dest.ssa)), Operand(0x3f800000u), Operand(exponent));
+      break;
+   }
+   case nir_intrinsic_load_viewport_x_scale: {
+      bld.copy(Definition(get_ssa_temp(ctx, &instr->dest.ssa)), get_arg(ctx, ctx->args->ngg_viewport_scale_x));
+      break;
+   }
+   case nir_intrinsic_load_viewport_y_scale: {
+      bld.copy(Definition(get_ssa_temp(ctx, &instr->dest.ssa)), get_arg(ctx, ctx->args->ngg_viewport_scale_y));
+      break;
+   }
+   case nir_intrinsic_load_viewport_x_offset: {
+      bld.copy(Definition(get_ssa_temp(ctx, &instr->dest.ssa)), get_arg(ctx, ctx->args->ngg_viewport_translate_x));
+      break;
+   }
+   case nir_intrinsic_load_viewport_y_offset: {
+      bld.copy(Definition(get_ssa_temp(ctx, &instr->dest.ssa)), get_arg(ctx, ctx->args->ngg_viewport_translate_y));
       break;
    }
    case nir_intrinsic_overwrite_vs_arguments_amd: {
