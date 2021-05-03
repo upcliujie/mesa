65a15b92c6cf3aa038004386fa9b0d289e08c38d
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-05-03 18:38:44 +0200

    panvk: add x11 wsi code
    The actual code is just shamelessly copied from ANV, nothing really new
    here.

--- src/panfrost/vulkan/panvk_device.c
+++ src/panfrost/vulkan/panvk_device.c
@@ -148,7 +148,8 @@ static const struct debug_control panvk_debug_options[] = {
    { NULL, 0 }
 };
 
-#if defined(VK_USE_PLATFORM_WAYLAND_KHR)
+#if defined(VK_USE_PLATFORM_WAYLAND_KHR) || \
+    defined(VK_USE_PLATFORM_XCB_KHR)
 #define PANVK_USE_WSI_PLATFORM
 #endif
 
@@ -167,6 +168,9 @@ static const struct vk_instance_extension_table panvk_instance_extensions = {
 #endif
 #ifdef VK_USE_PLATFORM_WAYLAND_KHR
    .KHR_wayland_surface = true,
+#endif
+#ifdef VK_USE_PLATFORM_XCB_KHR
+   .KHR_xcb_surface = true,
 #endif
 };
 
