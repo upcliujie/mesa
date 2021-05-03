eb4dc7459baae65ed2e99c60d1ae504113bfb122
Iago Toral Quiroga <itoral@igalia.com>
2021-05-03 13:35:40 +0200

    v3dv: setup loop unrolling
    We set the maximum at 16 iterations (the GL compiler chooses 32
    iterations for the GLSL front-end loop unrolling pass) because we
    have observed a bunch of shaders from Sascha Willems that spill
    significantly with 32, leading to massive performance degradation,
    while 16 avoids spilling and doesn't seem to cause viisble
    performance degradation compared to cases that unroll 32 without
    spilling.

--- src/broadcom/vulkan/v3dv_pipeline.c
+++ src/broadcom/vulkan/v3dv_pipeline.c
@@ -233,6 +233,7 @@ const nir_shader_compiler_options v3dv_nir_options = {
    .vertex_id_zero_based = false, /* FIXME: to set this to true, the intrinsic
                                    * needs to be supported */
    .lower_interpolate_at = true,
+   .max_unroll_iterations = 16,
 };
 
 const nir_shader_compiler_options *
