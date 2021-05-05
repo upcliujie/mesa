Grima04/mesa-turnip-kgsl

commit 0245b8257066fe786d1999e107ac140b85365e18

vulkan/wsi/x11: lower resize events to VK_SUBOPTIMAL_KHR
Currently, a resized window results in an VK_ERROR_OUT_OF_DATE_KHR. But,
this seems to go against the spec, which suggests returning
VK_SUBOPTIMAL_KHR if the surface extent no longer matches the window
extent. (It does not *require* the WSI actually scale the contents, only
encourages doing so "if possible")

More to the point, VK_SUBOPTIMAL_KHR is the correct status to return if
the client *can* continue presenting to this swapchain, even if the
result will look wrong / "suboptimal". It's the client's decision on
when exactly to recreate the swapchain in this case, and the client may
very well end up wanting to continue presenting for a frame or two, to
prevent flooding the driver with swapchain recreations (which can result
in the window appearing visually "frozen" while resizing).

This change makes live resizing *significantly* smoother in e.g. mpv.

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/3196>

src/vulkan/wsi/wsi_common_x11.c
@@ -893,7 +893,7 @@ x11_handle_dri3_present_event(struct x11_swapchain *chain,
 
       if (config->width != chain->extent.width ||
           config->height != chain->extent.height)
-         return VK_ERROR_OUT_OF_DATE_KHR;
+         return VK_SUBOPTIMAL_KHR;
 
       break;
    }

upcliujie/mesa

commit 1443a19dfb709e513f23c949d3b2caaa65b60fda

vulkan/wsi/x11: return VK_ERROR_OUT_OF_DATE_KHR on increasing resizes
If the surface extend goes beyond the window extend,
this might result in unsupported resolutions in full
screen applications.

Fixes: 0245b8257066fe786d1999e107ac140b85365e18 ('vulkan/wsi/x11: lower resize events to VK_SUBOPTIMAL_KHR')

src/vulkan/wsi/wsi_common_x11.c
@@ -895,6 +895,10 @@ x11_handle_dri3_present_event(struct x11_swapchain *chain,
    case XCB_PRESENT_CONFIGURE_NOTIFY: {
       xcb_present_configure_notify_event_t *config = (void *) event;
 
+      if (config->width > chain->extent.width ||
+          config->height > chain->extent.height)
+         return VK_ERROR_OUT_OF_DATE_KHR;
+
       if (config->width != chain->extent.width ||
           config->height != chain->extent.height)
          return VK_SUBOPTIMAL_KHR;