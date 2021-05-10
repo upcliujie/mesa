Grima04/mesa-turnip-kgsl

commit 81ab9fe2d0c2c2d9961107c88209f2fff1f136c4

etnaviv: handle NULL views in set_sampler_views
Passing NULL for the views parameter should be the same as passing an
array of NULL, according to the documentation. So let's respect that
detail.

This fixes a crash when using GALLIUM_HUD. The wrong handling of views
parameter was causing problems starting with
2813688f8db ("gallium/hud: don't use cso_context to restore VBs, constbuf 0 and sampler views").

Cc: <mesa-stable@lists.freedesktop.org>
Fixes: c9e8b49b885 ("etnaviv: gallium driver for Vivante GPUs")
Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8670>

src/gallium/drivers/etnaviv/etnaviv_texture.c
@@ -247,8 +247,10 @@ set_sampler_views(struct etna_context *ctx, unsigned start, unsigned end,
    uint32_t prev_active_sampler_views = ctx->active_sampler_views;
 
    for (i = start, j = 0; j < nr; i++, j++, mask <<= 1) {
-      pipe_sampler_view_reference(&ctx->sampler_view[i], views[j]);
-      if (views[j]) {
+      struct pipe_sampler_view *view = views ? views[i] : NULL;
+
+      pipe_sampler_view_reference(&ctx->sampler_view[i], view);
+      if (view) {
          ctx->active_sampler_views |= mask;
          ctx->dirty_sampler_views |= mask;
       } else

upcliujie/mesa

commit 7a4693da2e7711b8a704d820a08fbca702781385

etnaviv: fix vertex sampler setup
The start offset of the vertex samplers isn't zero, but the indexing of
the passed in views array is still zero based. Use the correct indexing
variable to fix vertex sampler setup.

Cc: <mesa-stable@lists.freedesktop.org>
Fixes: 81ab9fe2d0c2 ("etnaviv: handle NULL views in set_sampler_views")
Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

src/gallium/drivers/etnaviv/etnaviv_texture.c
@@ -247,7 +247,7 @@ set_sampler_views(struct etna_context *ctx, unsigned start, unsigned end,
    uint32_t prev_active_sampler_views = ctx->active_sampler_views;
 
    for (i = start, j = 0; j < nr; i++, j++, mask <<= 1) {
-      struct pipe_sampler_view *view = views ? views[i] : NULL;
+      struct pipe_sampler_view *view = views ? views[j] : NULL;
 
       pipe_sampler_view_reference(&ctx->sampler_view[i], view);
       if (view) {