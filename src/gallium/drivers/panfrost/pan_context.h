709deaa1afb25bbd8e7b3dcba9ff152f9233bc8e
Alyssa Rosenzweig <alyssa@collabora.com>
2021-05-12 18:32:31 -0400

    panfrost: Pool shaders
    Now we can do so without leaking memory :-)
    
    Likewise use a pan_pool for RSDs, to share the common path.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/gallium/drivers/panfrost/pan_context.h
+++ src/gallium/drivers/panfrost/pan_context.h
@@ -106,10 +106,8 @@ struct panfrost_context {
         /* Gallium context */
         struct pipe_context base;
 
-        /* Upload manager for small resident GPU-internal data structures, like
-         * sampler descriptors. We use an upload manager since the minimum BO
-         * size from the kernel is 4kb */
-        struct u_upload_mgr *state_uploader;
+        /* Unowned pools, so manage yourself. */
+        struct pan_pool descs, shaders;
 
         /* Sync obj used to keep track of in-flight jobs. */
         uint32_t syncobj;
@@ -215,21 +213,13 @@ struct panfrost_shader_state {
         /* Compiled, mapped descriptor, ready for the hardware */
         bool compiled;
 
-        /* Uploaded shader descriptor (TODO: maybe stuff the packed unuploaded
-         * bits in a union to save some memory?) */
-
-        struct {
-                struct pipe_resource *rsrc;
-                uint32_t offset;
-        } upload;
+        /* GPU-executable memory and containing Renderer State Descriptor */
+        struct pan_pool_ref bin, state;
 
         struct pan_shader_info info;
 
         struct pipe_stream_output_info stream_output;
         uint64_t so_mask;
-
-        /* GPU-executable memory */
-        struct panfrost_bo *bo;
 
         /* Variants */
         enum pipe_format rt_formats[8];
