997928e926cfec82fd5f30f4115f9fc64185750c
Rhys Perry <pendingchaos02@gmail.com>
2021-05-12 15:46:53 +0100

    radv: implement VK_EXT_vertex_input_dynamic_state
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Acked-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -1714,6 +1714,12 @@ radv_GetPhysicalDeviceFeatures2(VkPhysicalDevice physicalDevice,
          features->shaderIntegerDotProduct = true;
          break;
       }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VERTEX_INPUT_DYNAMIC_STATE_FEATURES_EXT: {
+         VkPhysicalDeviceVertexInputDynamicStateFeaturesEXT *features =
+            (VkPhysicalDeviceVertexInputDynamicStateFeaturesEXT *)ext;
+         features->vertexInputDynamicState = true;
+         break;
+      }
       default:
          break;
       }
@@ -3164,6 +3170,12 @@ radv_CreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo *pCr
          if (features->shaderImageFloat32AtomicMinMax ||
              features->sparseImageFloat32AtomicMinMax)
             image_float32_atomics = true;
+         break;
+      }
+      case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VERTEX_INPUT_DYNAMIC_STATE_FEATURES_EXT: {
+         const VkPhysicalDeviceVertexInputDynamicStateFeaturesEXT *features = (const void *)ext;
+         if (features->vertexInputDynamicState)
+            vs_prologs = true;
          break;
       }
       default:
