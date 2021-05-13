763f5920481e5497c13b4687f156b217c12f0ad3
Alyssa Rosenzweig <alyssa@collabora.com>
2021-05-13 12:17:28 -0400

    panfrost: Only link varyings once in good conditions
    If the varying descriptors will always be the same for a given shader
    variant (certainly true if none of separable shaders, transform
    feedback, or point sprites are used), we only need to link once. Now
    that pan_pool supports both owned and unowned modes, we have the
    flexibility to reuse the code path for both allocation strategies.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/gallium/drivers/panfrost/pan_context.h
+++ src/gallium/drivers/panfrost/pan_context.h
@@ -203,6 +203,23 @@ struct panfrost_rasterizer {
         struct pipe_rasterizer_state base;
 };
 
+/* Linked varyings */
+struct pan_linkage {
+        /* If the upload is owned by the CSO instead
+         * of the pool, the referenced BO. Else,
+         * NULL. */
+        struct panfrost_bo *bo;
+
+        /* Uploaded attribute descriptors */
+        mali_ptr producer, consumer;
+
+        /* Varyings buffers required */
+        uint32_t present;
+
+        /* Per-vertex stride for general varying buffer */
+        uint32_t stride;
+};
+
 /* Variants bundle together to form the backing CSO, bundling multiple
  * shaders with varying emulated features baked in */
 
@@ -215,6 +232,9 @@ struct panfrost_shader_state {
         struct pan_pool_ref bin, state;
 
         struct pan_shader_info info;
+
+        /* Linked varyings, for non-separable programs */
+        struct pan_linkage linkage;
 
         struct pipe_stream_output_info stream_output;
         uint64_t so_mask;
