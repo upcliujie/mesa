b01cb21902f8d6da34b1633f786b2e47815b7586
Yevhenii Kolesnikov <yevhenii.kolesnikov@globallogic.com>
2021-05-13 21:39:09 +0300

    anv: Switch to new debug message helpers
    Signed-off-by: Yevhenii Kolesnikov <yevhenii.kolesnikov@globallogic.com>

--- src/intel/vulkan/genX_cmd_buffer.c
+++ src/intel/vulkan/genX_cmd_buffer.c
@@ -401,7 +401,7 @@ anv_can_fast_clear_color_view(struct anv_device * device,
    if (isl_color_value_requires_conversion(clear_color,
                                            &iview->image->planes[0].primary_surface.isl,
                                            &iview->planes[0].isl)) {
-      anv_perf_warn(device, &iview->base,
+      anv_perf_warn(VK_LOG_OBJS(&iview->base),
                     "Cannot fast-clear to colors which would require "
                     "format conversion on resolve");
       return false;
@@ -416,7 +416,7 @@ anv_can_fast_clear_color_view(struct anv_device * device,
     */
    if (iview->planes[0].isl.base_level > 0 ||
        iview->planes[0].isl.base_array_layer > 0) {
-      anv_perf_warn(device, &iview->image->base,
+      anv_perf_warn(VK_LOG_OBJS(&iview->image->base),
                     "Rendering with multi-lod or multi-layer framebuffer "
                     "with LOAD_OP_LOAD and baseMipLevel > 0 or "
                     "baseArrayLayer > 0.  Not fast clearing.");
@@ -424,7 +424,7 @@ anv_can_fast_clear_color_view(struct anv_device * device,
    }
 
    if (num_layers > 1) {
-      anv_perf_warn(device, &iview->image->base,
+      anv_perf_warn(VK_LOG_OBJS(&iview->image->base),
                     "Rendering to a multi-layer framebuffer with "
                     "LOAD_OP_CLEAR.  Only fast-clearing the first slice");
    }
@@ -1385,7 +1385,7 @@ transition_color_buffer(struct anv_cmd_buffer *cmd_buffer,
          }
       } else {
          if (image->samples == 4 || image->samples == 16) {
-            anv_perf_warn(cmd_buffer->device, &image->base,
+            anv_perf_warn(VK_LOG_OBJS(&image->base),
                           "Doing a potentially unnecessary fast-clear to "
                           "define an MCS buffer.");
          }
