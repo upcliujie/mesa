Grima04/mesa-turnip-kgsl

commit 8b9033ad0a007ffe76f0f691c5540ce1b7c28499

radv: Support DCC modifiers fully.
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9998>

src/amd/vulkan/radv_image.c
@@ -1360,6 +1360,10 @@ radv_image_alloc_single_sample_cmask(const struct radv_device *device,
 static void
 radv_image_alloc_values(const struct radv_device *device, struct radv_image *image)
 {
+	/* images with modifiers can be potentially imported */
+	if (image->tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT)
+		return;
+
 	if (radv_image_has_cmask(image) || radv_image_has_dcc(image)) {
 		image->fce_pred_offset = image->size;
 		image->size += 8 * image->info.levels;

src/amd/vulkan/radv_formats.c
@@ -1156,10 +1156,7 @@ radv_get_modifier_flags(struct radv_physical_device *dev,
 		return 0;
 
 	if (ac_modifier_has_dcc(modifier)) {
-		features &= ~(VK_FORMAT_FEATURE_TRANSFER_DST_BIT |
-	                      VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT |
-	                      VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT |
-	                      VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT);
+		features &= ~VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT;
 
 		if (dev->instance->debug_flags & (RADV_DEBUG_NO_DCC | RADV_DEBUG_NO_DISPLAY_DCC))
 			return 0;

upcliujie/mesa

commit 1fcb0d11db3db926ab783689756f7b826ab8ab6a

radv: Allow DCC images to be compressed with foreign queues.
Otherwise we would always decompress when transitioning to the
foreign queue.

Fixes: 8b9033ad0a0 ("radv: Support DCC modifiers fully.")

src/amd/vulkan/radv_image.c
@@ -1978,6 +1978,9 @@ bool
 radv_layout_dcc_compressed(const struct radv_device *device, const struct radv_image *image,
                            VkImageLayout layout, bool in_render_loop, unsigned queue_mask)
 {
+   if (image->tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT && queue_mask & (1u << RADV_QUEUE_FOREIGN))
+      return radv_image_has_dcc(image);
+
    /* If the image is read-only, we can always just keep it compressed */
    if (!(image->usage & RADV_IMAGE_USAGE_WRITE_BITS) && radv_image_has_dcc(image))
       return true;