06eafd3a59089f0bed045f79db344e0bc0f2f039
Iago Toral Quiroga <itoral@igalia.com>
2021-05-14 10:05:09 +0200

    nir: V3D TLB load intrinsics can be removed
    Our lowering for logic operations will emit reads for 4 components,
    so we want this to remove component reads that are not used (for
    example with a clear operation) or don't exist in the underlying RT
    format.
    
    Fixes CTS tests like these:
    dEQP-VK.pipeline.logic_op.r8_uint.clear
    dEQP-VK.pipeline.logic_op.r8_uint.and
    dEQP-VK.pipeline.logic_op.r8_uint.and_reverse
    dEQP-VK.pipeline.logic_op.r8_uint.copy
    dEQP-VK.pipeline.logic_op.r8_uint.and_inverted
    dEQP-VK.pipeline.logic_op.r8_uint.no_op
    dEQP-VK.pipeline.logic_op.r8_uint.xor
    dEQP-VK.pipeline.logic_op.r8_uint.or

--- src/compiler/nir/nir_intrinsics.py
+++ src/compiler/nir/nir_intrinsics.py
@@ -1156,7 +1156,7 @@ intrinsic("alloc_vertices_and_primitives_amd", src_comp=[1, 1], indices=[])
 #
 # src[] = { render_target }
 # BASE = sample index
-load("tlb_color_v3d", [1], [BASE, COMPONENT], [])
+load("tlb_color_v3d", [1], [BASE, COMPONENT], [CAN_ELIMINATE])
 
 # V3D-specific instrinc for per-sample tile buffer color writes.
 #
@@ -1165,7 +1165,7 @@ load("tlb_color_v3d", [1], [BASE, COMPONENT], [])
 #
 # src[] = { value, render_target }
 # BASE = sample index
-store("tlb_sample_color_v3d", [1], [BASE, COMPONENT, SRC_TYPE], [])
+store("tlb_sample_color_v3d", [1], [BASE, COMPONENT, SRC_TYPE], [CAN_ELIMINATE])
 
 # V3D-specific intrinsic to load the number of layers attached to
 # the target framebuffer
