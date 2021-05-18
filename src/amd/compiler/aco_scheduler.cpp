9eb5508389c7d26ce9558528fcb2094fd024f4ea
Daniel Schürmann <daniel@schuermann.dev>
2021-05-18 22:33:30 +0200

    aco: stop scheduling if clause-forming fails
    This avoids unintended reordering of VMEM instructions.
    It is also highly unlikely that we find more independent
    instructions before previous clause-related instructions.
    
    Totals from 1376 (0.92% of 149839) affected shaders (Sienna Cichlid):
    VGPRs: 79712 -> 79608 (-0.13%); split: -0.15%, +0.02%
    CodeSize: 6746132 -> 6756488 (+0.15%); split: -0.03%, +0.19%
    Instrs: 1296942 -> 1299522 (+0.20%); split: -0.04%, +0.24%
    Latency: 23015052 -> 22878257 (-0.59%); split: -0.76%, +0.16%
    InvThroughput: 6470647 -> 6448582 (-0.34%); split: -0.44%, +0.09%
    VClause: 38561 -> 38252 (-0.80%); split: -0.93%, +0.13%
    SClause: 35200 -> 35213 (+0.04%); split: -0.08%, +0.11%
    Copies: 72793 -> 72429 (-0.50%); split: -0.73%, +0.23%
    Branches: 29636 -> 29641 (+0.02%)

--- src/amd/compiler/aco_scheduler.cpp
+++ src/amd/compiler/aco_scheduler.cpp
@@ -797,6 +797,8 @@ void schedule_VMEM(sched_ctx& ctx, Block* block,
          break;
 
       if (!can_move_down) {
+         if (part_of_clause)
+            break;
          add_to_hazard_query(&indep_hq, candidate.get());
          add_to_hazard_query(&clause_hq, candidate.get());
          ctx.mv.downwards_skip(cursor);
@@ -806,6 +808,8 @@ void schedule_VMEM(sched_ctx& ctx, Block* block,
       Instruction *candidate_ptr = candidate.get();
       MoveResult res = ctx.mv.downwards_move(cursor, part_of_clause);
       if (res == move_fail_ssa || res == move_fail_rar) {
+         if (part_of_clause)
+            break;
          add_to_hazard_query(&indep_hq, candidate.get());
          add_to_hazard_query(&clause_hq, candidate.get());
          ctx.mv.downwards_skip(cursor);
