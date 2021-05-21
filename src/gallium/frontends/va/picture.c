Grima04/mesa-turnip-kgsl

commit 81be8b3c2f2785932b63d4b82ee33cc3d6ac2765

va/picture: make sure destination buffer is protected if needed
Reviewed-by: Boyuan Zhang <boyuan.zhang@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7006>

src/gallium/frontends/va/picture.c
@@ -702,6 +702,15 @@ vlVaEndPicture(VADriverContextP ctx, VAContextID context_id)
       }
    }
 
+   if ((surf->templat.bind & PIPE_BIND_PROTECTED) != context->desc.base.protected_playback) {
+      if (context->desc.base.protected_playback) {
+         surf->templat.bind |= PIPE_BIND_PROTECTED;
+      }
+      else
+         surf->templat.bind &= ~PIPE_BIND_PROTECTED;
+      realloc = true;
+   }
+
    if (realloc) {
       struct pipe_video_buffer *old_buf = surf->buffer;
 

upcliujie/mesa

commit d0f25f5d34712ed275c2c66cb7d9ffbdca092db3

frontends/va/picture:Fix wrong reallocation even surface is protected
This patch will avoid reallocation,if surface is already protected.
Fixing the comparision logic of boolean value(true \ flase) with
PIPE_BIND_PROTECTED.

Fixes: 81be8b3c2f2 ("va/picture: make sure destination buffer is protected if needed")

Signed-off-by: SureshGuttula <suresh.guttula@amd.corp-partner.google.com>
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>

src/gallium/frontends/va/picture.c
@@ -698,7 +698,7 @@ vlVaEndPicture(VADriverContextP ctx, VAContextID context_id)
       }
    }
 
-   if ((surf->templat.bind & PIPE_BIND_PROTECTED) != context->desc.base.protected_playback) {
+   if ((bool)(surf->templat.bind & PIPE_BIND_PROTECTED) != context->desc.base.protected_playback) {
       if (context->desc.base.protected_playback) {
          surf->templat.bind |= PIPE_BIND_PROTECTED;
       }