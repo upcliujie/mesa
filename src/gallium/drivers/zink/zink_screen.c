f6f6a2771f5e729b759d119e776c056fda56d420
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-21 07:36:53 -0400

    zink: export supported primitive restart types
    this is now handled by gallium, so the codepath can be dropped

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -229,6 +229,15 @@ zink_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return screen->info.feats.features.samplerAnisotropy;
    case PIPE_CAP_REWRITE_UNFIXED_RESTART_INDICES:
       return 1;
+   case PIPE_CAP_SUPPORTED_RESTART_MODES: {
+      uint32_t modes = BITFIELD_BIT(PIPE_PRIM_LINE_STRIP) |
+                       BITFIELD_BIT(PIPE_PRIM_TRIANGLE_STRIP) |
+                       BITFIELD_BIT(PIPE_PRIM_LINE_STRIP_ADJACENCY) |
+                       BITFIELD_BIT(PIPE_PRIM_TRIANGLE_STRIP_ADJACENCY);
+      if (screen->have_triangle_fans)
+         modes |= BITFIELD_BIT(PIPE_PRIM_TRIANGLE_FAN);
+      return modes;
+   }
    case PIPE_CAP_SUPPORTED_PRIM_MODES: {
       uint32_t modes = BITFIELD_MASK(PIPE_PRIM_MAX);
       modes &= ~BITFIELD_BIT(PIPE_PRIM_QUADS);
