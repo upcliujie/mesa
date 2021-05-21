8c37406b8cdcdb24e19a08e6a2aeb744d5984f87
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2021-05-21 10:28:36 -0400

    llvmpipe: stop exporting PIPE_CAP_PRIMITIVE_RESTART_FIXED_INDEX
    this is not accurate and breaks things

--- src/gallium/drivers/llvmpipe/lp_screen.c
+++ src/gallium/drivers/llvmpipe/lp_screen.c
@@ -160,7 +160,6 @@ llvmpipe_get_param(struct pipe_screen *screen, enum pipe_cap param)
    case PIPE_CAP_TGSI_FS_COORD_PIXEL_CENTER_HALF_INTEGER:
       return 1;
    case PIPE_CAP_PRIMITIVE_RESTART:
-   case PIPE_CAP_PRIMITIVE_RESTART_FIXED_INDEX:
       return 1;
    case PIPE_CAP_DEPTH_CLIP_DISABLE:
       return 1;
