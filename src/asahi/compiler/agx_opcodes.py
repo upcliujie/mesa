a033261533fa0dde56b566c80cb9a709b37eaaad
Alyssa Rosenzweig <alyssa@collabora.com>
2021-05-23 17:17:16 -0400

    agx: Add invert_cond (ccn) to IR
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/asahi/compiler/agx_opcodes.py
+++ src/asahi/compiler/agx_opcodes.py
@@ -86,6 +86,7 @@ DIM = immediate("dim", "enum agx_dim")
 SCOREBOARD = immediate("scoreboard")
 ICOND = immediate("icond")
 FCOND = immediate("fcond")
+INVERT_COND = immediate("invert_cond")
 
 FUNOP = lambda x: (x << 28)
 FUNOP_MASK = FUNOP((1 << 14) - 1)
