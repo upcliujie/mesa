9892b8a9565b5d6758ffe833e9b9fa8064f4ab4a
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-05-24 19:54:47 +0300

    turnip: implement basic perfetto support
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/freedreno/vulkan/tu_clear_blit.c
+++ src/freedreno/vulkan/tu_clear_blit.c
@@ -16,6 +16,8 @@
 #include "util/format_srgb.h"
 #include "util/half_float.h"
 
+#include "tu_tracepoints.h"
+
 static uint32_t
 tu_pack_float32_for_unorm(float val, int bits)
 {
@@ -1166,6 +1168,8 @@ tu6_blit_image(struct tu_cmd_buffer *cmd,
          unreachable("unexpected D32_S8 aspect mask in blit_image");
    }
 
+   trace_start_blit(&cmd->trace);
+
    ops->setup(cmd, cs, format, info->dstSubresource.aspectMask,
               blit_param, false, dst_image->layout[0].ubwc);
 
@@ -1213,6 +1217,12 @@ tu6_blit_image(struct tu_cmd_buffer *cmd,
    }
 
    ops->teardown(cmd, cs);
+
+   trace_end_blit(&cmd->trace,
+                  ops == &r3d_ops,
+                  src_image->vk_format,
+                  dst_image->vk_format,
+                  layers);
 }
 
 void
@@ -1819,6 +1829,8 @@ resolve_sysmem(struct tu_cmd_buffer *cmd,
 {
    const struct blit_ops *ops = &r2d_ops;
 
+   trace_start_resolve(&cmd->trace);
+
    ops->setup(cmd, cs, format, VK_IMAGE_ASPECT_COLOR_BIT,
               0, false, dst->ubwc_enabled);
    ops->coords(cs, &rect->offset, &rect->offset, &rect->extent);
@@ -1835,6 +1847,8 @@ resolve_sysmem(struct tu_cmd_buffer *cmd,
    }
 
    ops->teardown(cmd, cs);
+
+   trace_end_resolve(&cmd->trace);
 }
 
 void
