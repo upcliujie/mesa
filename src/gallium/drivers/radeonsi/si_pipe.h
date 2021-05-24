7c57e588f6f2f02d2287b0915391df2bdf9c4198
Marek Olšák <marek.olsak@amd.com>
2021-05-24 15:43:54 -0400

    radeonsi: don't compile TES and GS draw_vbo variants for the prim discard CS
    This also fixes the incorrect emit_draw_packets template argument.
    The condition should be inverted.

--- src/gallium/drivers/radeonsi/si_pipe.h
+++ src/gallium/drivers/radeonsi/si_pipe.h
@@ -2008,12 +2008,22 @@ static inline unsigned si_get_shader_wave_size(struct si_shader *shader)
 
 static inline void si_select_draw_vbo(struct si_context *sctx)
 {
+   bool has_prim_discard_cs = si_compute_prim_discard_enabled(sctx) &&
+                              !sctx->shader.tes.cso && !sctx->shader.gs.cso;
    sctx->b.draw_vbo = sctx->draw_vbo[sctx->chip_class - GFX6]
                                     [!!sctx->shader.tes.cso]
                                     [!!sctx->shader.gs.cso]
                                     [sctx->ngg]
-                                    [si_compute_prim_discard_enabled(sctx)];
+                                    [has_prim_discard_cs];
    assert(sctx->b.draw_vbo);
+
+   if (!has_prim_discard_cs) {
+      /* Reset this to false if prim discard CS is disabled because draw_vbo doesn't reset it. */
+      if (sctx->prim_discard_cs_instancing) {
+         sctx->do_update_shaders = true;
+         sctx->prim_discard_cs_instancing = false;
+      }
+   }
 }
 
 /* Return the number of samples that the rasterizer uses. */
