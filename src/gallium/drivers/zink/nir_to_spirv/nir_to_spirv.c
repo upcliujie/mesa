upcliujie/mesa

commit e963d35efe1c1c1e0f5b0faea37480fdc6073f6a

zink: use ConstOffset for nir_tex_src_offset
Quote from the OpenGL Shading Language spec, version 4.40, section 8.9.2
"Texel Lookup Functions":

> The offset value must be a constant expression.

So, until we start consuming SPIR-V shaders, it seems we don't need to
deal with non-constant offsets.

This means we can avoid lowering this away in some cases.

Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -2151,7 +2151,7 @@ emit_tex(struct ntv_context *ctx, nir_tex_instr *tex)
    assert(tex->texture_index == tex->sampler_index);
 
    SpvId coord = 0, proj = 0, bias = 0, lod = 0, dref = 0, dx = 0, dy = 0,
-         offset = 0, sample = 0, tex_offset = 0;
+         const_offset = 0, sample = 0, tex_offset = 0;
    unsigned coord_components = 0;
    for (unsigned i = 0; i < tex->num_srcs; i++) {
       switch (tex->src[i].src_type) {
@@ -2171,7 +2171,8 @@ emit_tex(struct ntv_context *ctx, nir_tex_instr *tex)
          break;
 
       case nir_tex_src_offset:
-         offset = get_src_int(ctx, &tex->src[i].src);
+         assert(nir_src_is_const(tex->src[i].src));
+         const_offset = get_src_int(ctx, &tex->src[i].src);
          break;
 
       case nir_tex_src_bias:
@@ -2312,22 +2313,23 @@ emit_tex(struct ntv_context *ctx, nir_tex_instr *tex)
        tex->op == nir_texop_txf_ms ||
        tex->op == nir_texop_tg4) {
       SpvId image = spirv_builder_emit_image(&ctx->builder, image_type, load);
-      if (offset)
-         spirv_builder_emit_cap(&ctx->builder, SpvCapabilityImageGatherExtended);
-      if (tex->op == nir_texop_tg4)
+
+      if (tex->op == nir_texop_tg4) {
+         if (const_offset)
+            spirv_builder_emit_cap(&ctx->builder, SpvCapabilityImageGatherExtended);
          result = spirv_builder_emit_image_gather(&ctx->builder, dest_type,
                                                  load, coord, emit_uint_const(ctx, 32, tex->component),
-                                                 lod, sample, offset, dref);
-      else
+                                                 lod, sample, const_offset, dref);
+      } else
          result = spirv_builder_emit_image_fetch(&ctx->builder, dest_type,
-                                                 image, coord, lod, sample, offset);
+                                                 image, coord, lod, sample, const_offset);
    } else {
       result = spirv_builder_emit_image_sample(&ctx->builder,
                                                actual_dest_type, load,
                                                coord,
                                                proj != 0,
                                                lod, bias, dref, dx, dy,
-                                               offset);
+                                               const_offset);
    }
 
    spirv_builder_emit_decoration(&ctx->builder, result,

src/gallium/drivers/zink/nir_to_spirv/spirv_builder.c
@@ -646,7 +646,7 @@ spirv_builder_emit_image_sample(struct spirv_builder *b,
                                 SpvId dref,
                                 SpvId dx,
                                 SpvId dy,
-                                SpvId offset)
+                                SpvId const_offset)
 {
    SpvId result = spirv_builder_new_id(b);
 
@@ -676,9 +676,9 @@ spirv_builder_emit_image_sample(struct spirv_builder *b,
       extra_operands[++num_extra_operands] = dy;
       operand_mask |= SpvImageOperandsGradMask;
    }
-   if (offset) {
-      extra_operands[++num_extra_operands] = offset;
-      operand_mask |= SpvImageOperandsOffsetMask;
+   if (const_offset) {
+      extra_operands[++num_extra_operands] = const_offset;
+      operand_mask |= SpvImageOperandsConstOffsetMask;
    }
 
    /* finalize num_extra_operands / extra_operands */
@@ -721,7 +721,7 @@ spirv_builder_emit_image_gather(struct spirv_builder *b,
                                SpvId component,
                                SpvId lod,
                                SpvId sample,
-                               SpvId offset,
+                               SpvId const_offset,
                                SpvId dref)
 {
    SpvId result = spirv_builder_new_id(b);
@@ -738,9 +738,9 @@ spirv_builder_emit_image_gather(struct spirv_builder *b,
       extra_operands[++num_extra_operands] = sample;
       operand_mask |= SpvImageOperandsSampleMask;
    }
-   if (offset) {
-      extra_operands[++num_extra_operands] = offset;
-      operand_mask |= SpvImageOperandsOffsetMask;
+   if (const_offset) {
+      extra_operands[++num_extra_operands] = const_offset;
+      operand_mask |= SpvImageOperandsConstOffsetMask;
    }
    if (dref)
       op = SpvOpImageDrefGather;
@@ -773,7 +773,7 @@ spirv_builder_emit_image_fetch(struct spirv_builder *b,
                                SpvId coordinate,
                                SpvId lod,
                                SpvId sample,
-                               SpvId offset)
+                               SpvId const_offset)
 {
    SpvId result = spirv_builder_new_id(b);
 
@@ -788,9 +788,9 @@ spirv_builder_emit_image_fetch(struct spirv_builder *b,
       extra_operands[++num_extra_operands] = sample;
       operand_mask |= SpvImageOperandsSampleMask;
    }
-   if (offset) {
-      extra_operands[++num_extra_operands] = offset;
-      operand_mask |= SpvImageOperandsOffsetMask;
+   if (const_offset) {
+      extra_operands[++num_extra_operands] = const_offset;
+      operand_mask |= SpvImageOperandsConstOffsetMask;
    }
 
    /* finalize num_extra_operands / extra_operands */

src/gallium/drivers/zink/zink_compiler.c
@@ -407,7 +407,6 @@ zink_shader_create(struct zink_screen *screen, struct nir_shader *nir,
    if (!screen->info.feats.features.shaderImageGatherExtended) {
       nir_lower_tex_options tex_opts = {};
       tex_opts.lower_tg4_offsets = true;
-      tex_opts.lower_txf_offset = true;
       NIR_PASS_V(nir, nir_lower_tex, &tex_opts);
    }
 

commit 62d5a77b17953bfd97d752996ea10c99d8315bbd

zink: use actual const for const offset
When we emit constants, we don't know what type they'll be used as, so
we just emit them as uint, and then bitcast them to whatever we need.

But this isn't a good idea for ConstOffset, which needs to actually be a
const value, and not a const value bitcasted. So we sadly have to
open-code the const emitting here to avoid the problem.

Fixes: e963d35efe1 ("zink: use ConstOffset for nir_tex_src_offset")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4831

src/gallium/drivers/zink/nir_to_spirv/nir_to_spirv.c
@@ -2896,9 +2896,28 @@ emit_tex(struct ntv_context *ctx, nir_tex_instr *tex)
          break;
 
       case nir_tex_src_offset:
-         if (nir_src_is_const(tex->src[i].src))
-            const_offset = get_src_int(ctx, &tex->src[i].src);
-         else
+         if (nir_src_is_const(tex->src[i].src)) {
+            nir_const_value *v = nir_src_as_const_value(tex->src[i].src);
+            unsigned bit_size = nir_src_bit_size(tex->src[i].src);
+            unsigned num_components = nir_src_num_components(tex->src[i].src);
+
+            SpvId components[NIR_MAX_VEC_COMPONENTS];
+            for (int i = 0; i < num_components; ++i) {
+               int64_t tmp = nir_const_value_as_int(*v, bit_size);
+               components[i] = emit_int_const(ctx, bit_size, tmp);
+            }
+
+            if (num_components > 1) {
+               SpvId type = get_ivec_type(ctx, bit_size, num_components);
+               const_offset = spirv_builder_const_composite(&ctx->builder,
+                                                            type,
+                                                            components,
+                                                            num_components);
+            } else {
+               assert(num_components == 1);
+               const_offset = components[0];
+            }
+         } else
             offset = get_src_int(ctx, &tex->src[i].src);
          break;
 