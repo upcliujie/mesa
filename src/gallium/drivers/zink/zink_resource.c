aerisarn/mesa-uwp

commit 1887ff2ebb78a1e0ca27d4626a29a3f0cb170d31

zink: mark 2d-arrays as cube-compatible
OpenGL allows reinterpreting 2D array textures as cubemaps,
so we need to always set the cube-compatible flag for them.

This fixes a piglit test on Lavapipe.

Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10913>

src/gallium/drivers/zink/zink_resource.c
@@ -384,7 +384,8 @@ create_ici(struct zink_screen *screen, const struct pipe_resource *templ, unsign
       debug_printf("ZINK: failed to validate image creation\n");
 
    if (templ->target == PIPE_TEXTURE_CUBE ||
-       templ->target == PIPE_TEXTURE_CUBE_ARRAY) {
+       templ->target == PIPE_TEXTURE_CUBE_ARRAY ||
+       templ->target == PIPE_TEXTURE_2D_ARRAY) {
       VkImageFormatProperties props;
       if (vkGetPhysicalDeviceImageFormatProperties(screen->pdev, ici.format,
                                                    ici.imageType, ici.tiling,

upcliujie/mesa

commit 5bc65a2d0e9cffa6999c8da3a839fa43bd65f735

zink: limit images we mark as cube-compatible
The Vulkan spec says the following:

> If imageType is VK_IMAGE_TYPE_2D and flags contains
> VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT, extent.width and extent.height
> must be equal and arrayLayers must be greater than or equal to 6

This makes a lot of sense, as these are also requirements for being able
to create cubemaps from them in the first place.

Let's thread a bit more careful, and only set this bit in these cases.
This matters in the new case of setting this flag on 2D array textures.
In the other cases, this should already be the case.

I haven't seen this trigger any issues, I just realized this while
reading the Vulkan-spec.

Fixes: 1887ff2ebb7 ("zink: mark 2d-arrays as cube-compatible")

src/gallium/drivers/zink/zink_resource.c
@@ -383,9 +383,11 @@ create_ici(struct zink_screen *screen, const struct pipe_resource *templ, unsign
    if (!good)
       debug_printf("ZINK: failed to validate image creation\n");
 
-   if (templ->target == PIPE_TEXTURE_CUBE ||
-       templ->target == PIPE_TEXTURE_CUBE_ARRAY ||
-       templ->target == PIPE_TEXTURE_2D_ARRAY) {
+   if ((templ->target == PIPE_TEXTURE_CUBE ||
+        templ->target == PIPE_TEXTURE_CUBE_ARRAY ||
+        templ->target == PIPE_TEXTURE_2D_ARRAY) &&
+        ici.extent.width == ici.extent.height &&
+        ici.arrayLayers >= 6) {
       VkImageFormatProperties props;
       if (vkGetPhysicalDeviceImageFormatProperties(screen->pdev, ici.format,
                                                    ici.imageType, ici.tiling,