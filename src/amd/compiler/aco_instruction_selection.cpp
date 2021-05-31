c580520292b68aff44c7dcb997b04b689dad8333
Rhys Perry <pendingchaos02@gmail.com>
2021-05-31 18:18:24 +0100

    aco: fix usage of dst_hint in lds_load_callback
    For ds_read_u8 and ds_read_u16, we use a 32-bit regclass, but don't load
    32 bits.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4863
    Cc: mesa-stable

--- src/amd/compiler/aco_instruction_selection.cpp
+++ src/amd/compiler/aco_instruction_selection.cpp
@@ -3644,7 +3644,7 @@ Temp lds_load_callback(Builder& bld, const LoadEmitInfo &info,
    const_offset /= const_offset_unit;
 
    RegClass rc = RegClass(RegType::vgpr, DIV_ROUND_UP(size, 4));
-   Temp val = rc == info.dst.regClass() && dst_hint.id() ? dst_hint : bld.tmp(rc);
+   Temp val = size == info.dst.bytes() && info.dst.type() == RegType::vgpr && dst_hint.id() ? dst_hint : bld.tmp(rc);
    Instruction *instr;
    if (read2)
       instr = bld.ds(op, Definition(val), offset, m, const_offset, const_offset + 1);
