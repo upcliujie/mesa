dd0a083d02869249c4bbd2b871119531e748f643
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-06-07 17:52:05 +0200

    squash! llvmpipe: do not always use pixel-rounded coordinates for points
    This fixes the correction for the fill-rule.

--- src/gallium/drivers/llvmpipe/lp_setup_point.c
+++ src/gallium/drivers/llvmpipe/lp_setup_point.c
@@ -533,6 +533,13 @@ try_setup_point( struct lp_setup_context *setup,
       plane[3].dcdy = ~0U << 8;
       plane[3].c = y[1];
       plane[3].eo = 0;
+
+      /* adjust for fill-rule*/
+      plane[0].c++; /* left */
+      if (pixel_offset == 0)
+         plane[2].c++; /* top-left */
+      else
+         plane[3].c++; /* bottom-left */
    }
 
    return lp_setup_bin_triangle(setup, point, &bbox, &bbox, nr_planes, viewport_index);
