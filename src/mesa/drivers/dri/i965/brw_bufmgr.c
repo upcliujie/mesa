96ba682d6fbbf9a7aa005ab151c989c5e6b986f3
Eleni Maria Stea <elene.mst@gmail.com>
2021-06-07 09:22:20 +0300

    intel: PAGE_SIZE used in allocators shouldn't be defined on FreeBSD
    iris, i965, and anv define the PAGE_SIZE in anv_allocator and bufmgr
    files. As on FreeBSD the page size is defined in machine/param.h that is
    indirectly included by those files, we'd rather define it only when the
    system is not FreeBSD to avoid compile errors.
    
    Signed-off-by: Eleni Maria Stea <elene.mst@gmail.com>

--- src/mesa/drivers/dri/i965/brw_bufmgr.c
+++ src/mesa/drivers/dri/i965/brw_bufmgr.c
@@ -95,7 +95,10 @@
 #define VG_DEFINED(ptr, size) VG(VALGRIND_MAKE_MEM_DEFINED(ptr, size))
 #define VG_NOACCESS(ptr, size) VG(VALGRIND_MAKE_MEM_NOACCESS(ptr, size))
 
+/* On FreeBSD PAGE_SIZE is defined in machine/param.h */
+#ifndef PAGE_SIZE
 #define PAGE_SIZE 4096
+#endif
 
 #define FILE_DEBUG_FLAG DEBUG_BUFMGR
 
