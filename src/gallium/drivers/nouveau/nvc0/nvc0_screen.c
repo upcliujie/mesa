688d3dc536c1dc2e1fa7a594799ee44050da6d57
Karol Herbst <kherbst@redhat.com>
2021-06-09 21:48:17 +0200

    nouveau/fence: per context fence

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -701,8 +701,6 @@ nvc0_screen_destroy(struct pipe_screen *pscreen)
    if (!nouveau_drm_screen_unref(&screen->base))
       return;
 
-   nouveau_fence_cleanup(&screen->base.fence);
-
    if (screen->base.pushbuf)
       screen->base.pushbuf->user_priv = NULL;
 
@@ -1560,8 +1558,6 @@ nvc0_screen_create(struct nouveau_device *dev)
 
    if (!nvc0_blitter_create(screen))
       goto fail;
-
-   nouveau_fence_new(&screen->base, &screen->base.fence.current);
 
    return &screen->base;
 
