Grima04/mesa-turnip-kgsl

commit 787412b7eb8a25f92b62fbd9e04d48b1c5fb0023

zink: break out region overlap testing function into helper
this is useful for a lot of copying operations

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9206>

src/gallium/drivers/zink/zink_blit.c
@@ -4,6 +4,7 @@
 #include "zink_screen.h"
 
 #include "util/u_blitter.h"
+#include "util/u_rect.h"
 #include "util/u_surface.h"
 #include "util/format/u_format.h"
 
@@ -259,4 +260,21 @@ zink_blit_begin(struct zink_context *ctx, enum zink_blit_flags flags)
                                                ctx->num_sampler_views[PIPE_SHADER_FRAGMENT],
                                                ctx->sampler_views[PIPE_SHADER_FRAGMENT]);
    }
+}
+
+bool
+zink_blit_region_fills(struct u_rect region, unsigned width, unsigned height)
+{
+   struct u_rect intersect = {0, width, 0, height};
+
+   if (!u_rect_test_intersection(&region, &intersect))
+      /* is this even a thing? */
+      return false;
+
+    u_rect_find_intersection(&region, &intersect);
+    if (intersect.x0 != 0 || intersect.y0 != 0 ||
+        intersect.x1 != width || intersect.y1 != height)
+       return false;
+
+   return false;
 }

src/gallium/drivers/zink/zink_clear.c
@@ -162,22 +162,7 @@ clear_zs_no_rp(struct zink_context *ctx, struct zink_resource *res, VkImageAspec
    vkCmdClearDepthStencilImage(batch->cmdbuf, res->image, res->layout, &zs_value, 1, &range);
 }
 
-static bool
-clear_needs_rp(unsigned width, unsigned height, struct u_rect *region)
-{
-   struct u_rect intersect = {0, width, 0, height};
 
-   if (!u_rect_test_intersection(region, &intersect))
-      /* is this even a thing? */
-      return true;
-
-    u_rect_find_intersection(region, &intersect);
-    if (intersect.x0 != 0 || intersect.y0 != 0 ||
-        intersect.x1 != width || intersect.y1 != height)
-       return true;
-
-   return false;
-}
 
 static struct zink_framebuffer_clear_data *
 get_clear_data(struct zink_context *ctx, struct zink_framebuffer_clear *fb_clear, const struct pipe_scissor_state *scissor_state)
@@ -212,7 +197,7 @@ zink_clear(struct pipe_context *pctx,
 
    if (scissor_state) {
       struct u_rect scissor = {scissor_state->minx, scissor_state->maxx, scissor_state->miny, scissor_state->maxy};
-      needs_rp = clear_needs_rp(fb->width, fb->height, &scissor);
+      needs_rp = !zink_blit_region_fills(scissor, fb->width, fb->height);
    }
 
 
@@ -373,7 +358,7 @@ zink_clear_texture(struct pipe_context *pctx,
    struct zink_resource *res = zink_resource(pres);
    struct pipe_screen *pscreen = pctx->screen;
    struct u_rect region = {box->x, box->x + box->width, box->y, box->y + box->height};
-   bool needs_rp = clear_needs_rp(pres->width0, pres->height0, &region) || ctx->render_condition_active;
+   bool needs_rp = !zink_blit_region_fills(region, pres->width0, pres->height0) || ctx->render_condition_active;
    struct zink_batch *batch = zink_curr_batch(ctx);
    struct pipe_surface *surf = NULL;
 

src/gallium/drivers/zink/zink_context.h
@@ -38,6 +38,7 @@
 struct blitter_context;
 struct primconvert_context;
 struct list_head;
+struct u_rect;
 
 struct zink_blend_state;
 struct zink_depth_stencil_alpha_state;
@@ -279,6 +280,9 @@ zink_blit_begin(struct zink_context *ctx, enum zink_blit_flags flags);
 void
 zink_blit(struct pipe_context *pctx,
           const struct pipe_blit_info *info);
+
+bool
+zink_blit_region_fills(struct u_rect region, unsigned width, unsigned height);
 
 void
 zink_clear(struct pipe_context *pctx,

upcliujie/mesa

commit 82de988e3338ae36290c5c21823fcc8d1acfe713

zink: fix typo that's definitely not at all embarrassing or anything like that
and also adjust some formatting to pad out the diff and really make sure nobody
notices that anything was ever amiss here

Fixes: 787412b7eb8 ("zink: break out region overlap testing function into helper")

src/gallium/drivers/zink/zink_blit.c
@@ -330,12 +330,12 @@ zink_blit_region_fills(struct u_rect region, unsigned width, unsigned height)
       /* is this even a thing? */
       return false;
 
-    u_rect_find_intersection(&region, &intersect);
-    if (intersect.x0 != 0 || intersect.y0 != 0 ||
-        intersect.x1 != width || intersect.y1 != height)
-       return false;
+   u_rect_find_intersection(&region, &intersect);
+   if (intersect.x0 != 0 || intersect.y0 != 0 ||
+       intersect.x1 != width || intersect.y1 != height)
+      return false;
 
-   return false;
+   return true;
 }
 
 bool