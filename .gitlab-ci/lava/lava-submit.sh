dcfcbdfcc2d7d744710e963accef260aba561858
Daniel Stone <daniels@collabora.com>
2021-06-11 17:14:21 +0100

    ci/lava: Use common stage-2 init
    Now that our job-execution scripts look very similar for LAVA and
    bare-metal, we can just tell LAVA to use the same stage-2 init we use
    for bare-metal and delete a bunch of duplication.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>

--- .gitlab-ci/lava/lava-submit.sh
+++ .gitlab-ci/lava/lava-submit.sh
@@ -19,6 +19,7 @@ mkdir -p results/job-rootfs-overlay/
 export PIGLIT_NO_WINDOW=1
 export PIGLIT_REPLAY_UPLOAD_TO_MINIO=1
 cp artifacts/ci-common/capture-devcoredump.sh results/job-rootfs-overlay/
+cp artifacts/ci-common/init-*.sh results/job-rootfs-overlay/
 artifacts/ci-common/generate-env.sh > results/job-rootfs-overlay/set-job-env-vars.sh
 
 tar zcf job-rootfs-overlay.tar.gz -C results/job-rootfs-overlay/ .
