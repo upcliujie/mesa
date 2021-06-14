f37c2b339b144886bbcec00490ef4548bf97e9f7
Daniel Schürmann <daniel@schuermann.dev>
2021-06-14 19:19:23 +0200

    [SQUASHED] !11411
    nir: consider write_mask in nir_ssa_def_components_read()
    nir/opt_shrink_vectors: remove explicit shrinking of store srcs
    nir/opt_shrink_vectors: reverse iteration order

--- src/compiler/nir/nir_opt_shrink_vectors.c
+++ src/compiler/nir/nir_opt_shrink_vectors.c
@@ -58,6 +58,11 @@ shrink_dest_to_read_mask(nir_ssa_def *def)
 
    if (def->num_components > last_bit) {
       def->num_components = last_bit;
+      nir_foreach_use_safe(use_src, def) {
+         if (use_src->parent_instr->type == nir_instr_type_intrinsic)
+            nir_instr_as_intrinsic(use_src->parent_instr)->num_components = last_bit;
+      }
+
       return true;
    }
 
@@ -95,6 +100,11 @@ opt_shrink_vectors_alu(nir_builder *b, nir_alu_instr *instr)
                srcs[i] = nir_ssa_for_alu_src(b, instr, i);
 
             nir_ssa_def *new_vec = nir_vec(b, srcs, last_bit);
+
+            nir_foreach_use_safe(use_src, def) {
+               if (use_src->parent_instr->type == nir_instr_type_intrinsic)
+                  nir_instr_as_intrinsic(use_src->parent_instr)->num_components = last_bit;
+            }
             nir_ssa_def_rewrite_uses(def, new_vec);
             return true;
          }
@@ -151,12 +161,6 @@ opt_shrink_vectors_intrinsic(nir_builder *b, nir_intrinsic_instr *instr, bool sh
    case nir_intrinsic_load_global_constant:
    case nir_intrinsic_load_kernel_input:
    case nir_intrinsic_load_scratch:
-   case nir_intrinsic_store_output:
-   case nir_intrinsic_store_per_vertex_output:
-   case nir_intrinsic_store_ssbo:
-   case nir_intrinsic_store_shared:
-   case nir_intrinsic_store_global:
-   case nir_intrinsic_store_scratch:
       break;
    case nir_intrinsic_bindless_image_store:
    case nir_intrinsic_image_deref_store:
@@ -168,30 +172,12 @@ opt_shrink_vectors_intrinsic(nir_builder *b, nir_intrinsic_instr *instr, bool sh
 
    /* Must be a vectorized intrinsic that we can resize. */
    assert(instr->num_components != 0);
+   assert(nir_intrinsic_infos[instr->intrinsic].has_dest);
 
-   if (nir_intrinsic_infos[instr->intrinsic].has_dest) {
-      /* loads: Trim the dest to the used channels */
-
-      if (shrink_dest_to_read_mask(&instr->dest.ssa)) {
-         instr->num_components = instr->dest.ssa.num_components;
-         return true;
-      }
-   } else {
-      /* Stores: trim the num_components stored according to the write
-       * mask.
-       */
-      unsigned write_mask = nir_intrinsic_write_mask(instr);
-      unsigned last_bit = util_last_bit(write_mask);
-      if (last_bit < instr->num_components && instr->src[0].is_ssa) {
-         nir_ssa_def *def = nir_channels(b, instr->src[0].ssa,
-                                         BITSET_MASK(last_bit));
-         nir_instr_rewrite_src(&instr->instr,
-                               &instr->src[0],
-                               nir_src_for_ssa(def));
-         instr->num_components = last_bit;
-
-         return true;
-      }
+   /* Trim the dest to the used channels */
+   if (shrink_dest_to_read_mask(&instr->dest.ssa)) {
+      instr->num_components = instr->dest.ssa.num_components;
+      return true;
    }
 
    return false;
@@ -246,8 +232,8 @@ nir_opt_shrink_vectors(nir_shader *shader, bool shrink_image_store)
       nir_builder b;
       nir_builder_init(&b, function->impl);
 
-      nir_foreach_block(block, function->impl) {
-         nir_foreach_instr(instr, block) {
+      nir_foreach_block_reverse(block, function->impl) {
+         nir_foreach_instr_reverse(instr, block) {
             progress |= opt_shrink_vectors_instr(&b, instr, shrink_image_store);
          }
       }
