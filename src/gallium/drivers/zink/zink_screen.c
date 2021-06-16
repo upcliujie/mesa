Grima04/mesa-turnip-kgsl

commit 79c0023d196c183bca4a5a66522e1432f8f95f10

zink: use the new extension infrastructure in device creation
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6789>

src/gallium/drivers/zink/zink_screen.c
@@ -927,21 +927,6 @@ zink_internal_create_screen(struct sw_winsys *winsys, int fd)
       goto fail;
    }
 
-   VkDeviceQueueCreateInfo qci = {};
-   float dummy = 0.0f;
-   qci.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-   qci.queueFamilyIndex = screen->gfx_queue;
-   qci.queueCount = 1;
-   qci.pQueuePriorities = &dummy;
-
-   VkDeviceCreateInfo dci = {};
-   dci.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-   dci.queueCreateInfoCount = 1;
-   dci.pQueueCreateInfos = &qci;
-   /* extensions don't have bool members in pEnabledFeatures.
-    * this requires us to pass the whole VkPhysicalDeviceFeatures2 struct
-    */
-   dci.pNext = &feats;
    const char *extensions[9] = {
       VK_KHR_MAINTENANCE1_EXTENSION_NAME,
    };
@@ -973,9 +958,6 @@ zink_internal_create_screen(struct sw_winsys *winsys, int fd)
       extensions[num_extensions++] = VK_EXT_CALIBRATED_TIMESTAMPS_EXTENSION_NAME;
    assert(num_extensions <= ARRAY_SIZE(extensions));
 
-   dci.ppEnabledExtensionNames = extensions;
-   dci.enabledExtensionCount = num_extensions;
-
    if (!zink_get_physical_device_info(screen)) {
       debug_printf("ZINK: failed to detect features\n");
       goto fail;
@@ -985,6 +967,25 @@ zink_internal_create_screen(struct sw_winsys *winsys, int fd)
       debug_printf("ZINK: KHR_external_memory_fd required!\n");
       goto fail;
    }
+   
+   VkDeviceQueueCreateInfo qci = {};
+   float dummy = 0.0f;
+   qci.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+   qci.queueFamilyIndex = screen->gfx_queue;
+   qci.queueCount = 1;
+   qci.pQueuePriorities = &dummy;
+
+   VkDeviceCreateInfo dci = {};
+   dci.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+   dci.queueCreateInfoCount = 1;
+   dci.pQueueCreateInfos = &qci;
+   /* extensions don't have bool members in pEnabledFeatures.
+    * this requires us to pass the whole VkPhysicalDeviceFeatures2 struct
+    */
+   dci.pNext = &screen->info.feats;
+
+   dci.ppEnabledExtensionNames = screen->info.extensions;
+   dci.enabledExtensionCount = screen->info.num_extensions;
 
    if (vkCreateDevice(screen->pdev, &dci, NULL, &screen->dev) != VK_SUCCESS)
       goto fail;

upcliujie/mesa

commit 87dc7c2ccbc272054dff2c805d390f151d7617db

zink: do not forward unwanted junk to device-creation
We don't want to forward whatever ended up in the pNext member when
we called GetPhysicalDeviceFeatures2 to the vkCreateDevice call.

In this particular case, it lead to the Intel Windows Vulkan driver
dropping everything apart from the first draw-call we ever gave it on
the floor. Pretty amazing, but thanks to the validator I was told what
was wrong.

Fixes: 79c0023d196 ("zink: use the new extension infrastructure in device creation")

src/gallium/drivers/zink/zink_screen.c
@@ -1519,6 +1519,7 @@ zink_create_logical_device(struct zink_screen *screen)
     */
    if (screen->info.feats.sType == VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2) {
       dci.pNext = &screen->info.feats;
+      screen->info.feats.pNext = NULL;
    } else {
       dci.pEnabledFeatures = &screen->info.feats.features;
    }