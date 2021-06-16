38df8ec6266155f147a4978c397b288b0bf78bc6
Karol Herbst <kherbst@redhat.com>
2021-06-16 19:07:09 +0200

    nouveau: use the contexts pushbuf and client where possible
    At the moment the context uses the screens object, but in order to fix
    threading without having to lock too much we will have to allocate those
    objects per context later.
    
    Cc: mesa-stable
    Signed-off-by: Karol Herbst <kherbst@redhat.com>

--- src/gallium/drivers/nouveau/nv50/nv50_context.c
+++ src/gallium/drivers/nouveau/nv50/nv50_context.c
@@ -315,12 +315,12 @@ nv50_create(struct pipe_screen *pscreen, void *priv, unsigned ctxflags)
    nv50->base.pushbuf = screen->base.pushbuf;
    nv50->base.client = screen->base.client;
 
-   ret = nouveau_bufctx_new(screen->base.client, 2, &nv50->bufctx);
+   ret = nouveau_bufctx_new(nv50->base.client, 2, &nv50->bufctx);
    if (!ret)
-      ret = nouveau_bufctx_new(screen->base.client, NV50_BIND_3D_COUNT,
+      ret = nouveau_bufctx_new(nv50->base.client, NV50_BIND_3D_COUNT,
                                &nv50->bufctx_3d);
    if (!ret)
-      ret = nouveau_bufctx_new(screen->base.client, NV50_BIND_CP_COUNT,
+      ret = nouveau_bufctx_new(nv50->base.client, NV50_BIND_CP_COUNT,
                                &nv50->bufctx_cp);
    if (ret)
       goto out_err;
