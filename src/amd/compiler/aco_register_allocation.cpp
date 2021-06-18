4e5cbd30e0e5ae5dc33480fdd90a486827065d45
Daniel Schürmann <daniel@schuermann.dev>
2021-06-18 13:33:08 +0200

    aco/ra: call get_reg_impl() less often in case of p_create_vector
    Totals from 114463 (76.39% of 149839) affected shaders: (GFX10.3)
    VGPRs: 4372816 -> 4372400 (-0.01%); split: -0.01%, +0.00%
    CodeSize: 252779140 -> 252632552 (-0.06%); split: -0.08%, +0.02%
    MaxWaves: 3007352 -> 3007426 (+0.00%)
    Instrs: 48229468 -> 48199141 (-0.06%); split: -0.09%, +0.03%
    Latency: 826992307 -> 827212894 (+0.03%); split: -0.01%, +0.04%
    InvThroughput: 205106828 -> 205121409 (+0.01%); split: -0.00%, +0.01%
    VClause: 1025117 -> 1025190 (+0.01%); split: -0.00%, +0.01%
    SClause: 1908277 -> 1908658 (+0.02%); split: -0.02%, +0.04%
    Copies: 2791053 -> 2759853 (-1.12%); split: -1.58%, +0.46%
    Branches: 1093752 -> 1092833 (-0.08%); split: -0.13%, +0.05%

--- src/amd/compiler/aco_register_allocation.cpp
+++ src/amd/compiler/aco_register_allocation.cpp
@@ -1706,8 +1706,15 @@ get_reg_create_vector(ra_ctx& ctx, RegisterFile& reg_file, Temp temp,
       correct_pos_mask = correct_pos_mask_new;
    }
 
-   if (num_moves >= bytes)
+   /* too many moves: try the generic get_reg() function */
+   if (num_moves >= 2 * bytes) {
       return get_reg(ctx, reg_file, temp, parallelcopies, instr);
+   } else if (num_moves > bytes) {
+      DefInfo info(ctx, instr, rc, -1);
+      std::pair<PhysReg, bool> res = get_reg_simple(ctx, reg_file, info);
+      if (res.second)
+         return res.first;
+   }
 
    /* re-enable killed operands which are in the wrong position */
    RegisterFile tmp_file(reg_file);
