28882470ce67d984761e2bbea31f70056f05efae
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-06-18 11:52:31 +0300

    nir: change intel dss_id intrinsic to topology_id
    This will allow to reuse the same intrinsic for various topology based
    ID.
    
    v2: fix intrinsic comment (Caio)
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>

--- src/intel/compiler/brw_compiler.h
+++ src/intel/compiler/brw_compiler.h
@@ -1994,6 +1994,21 @@ brw_compute_first_urb_slot_required(uint64_t inputs_read,
    return 0;
 }
 
+/**
+ * This enum is used as the base indice of the nir_load_topology_id_intel
+ * intrinsic. This is used to return different values based on some aspect of
+ * the topology of the device.
+ */
+enum brw_topology_id
+{
+   /* A value based of the DSS identifier the shader is currently running on.
+    * Be mindful that the DSS ID can be higher than the total number of DSS on
+    * the device. This is because of the fusing that can occur on different
+    * parts.
+    */
+   BRW_TOPOLOGY_ID_DSS,
+};
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
