8dc836a92d182eba790ec5057d2cde914b28cf8c
Jason Ekstrand <jason@jlekstrand.net>
2021-06-18 17:12:32 -0500

    isl: Move all the extra_aux stuff to iris
    CCS isn't really a surface starting with Tigerlake.  It's no longer laid
    out as an independent surface and is no longer referenced by Auxiliary
    Surface Base Address.  Instead, it's a blob of memory that's a 1:256
    scale-down from the main surface that's attached side-band via a second
    set of page tables.
    
    In spite of this, it's sometimes useful to think of it as being a linear
    buffer-like surface, at least for the purposes of allocation.  Iris has
    a concept of the aux surface on a resource and the extra_aux surface.
    When a combined compression scheme is used like HiZ+CCS or MCS+CCS, the
    CCS goes in this second surface.  However, this makes the ISL interface
    a bit messy because now it takes two aux surfaces in spite of the fact
    that only iris really uses this.  In ANV, we deal with CCS separately
    for combined compression forms.
    
    This commit pulls this extra_aux logic out into an iris-specific
    iris_get_ccs_surf helper to clean up the ISL interface.  This does mean
    that the check for CCS+depth+!HiZ is moved out of ISL into iris but
    that's considered an acceptable loss for having a clean interface.  It's
    pretty clear that this case is invalid anyway because there is no
    ISL_AUX_USAGE_DEPTH_CCS and we'll assert in depth/stencil emit if we
    ever try to use compression without HiZ.

--- src/intel/isl/isl.h
+++ src/intel/isl/isl.h
@@ -2219,8 +2219,7 @@ isl_surf_get_mcs_surf(const struct isl_device *dev,
 bool
 isl_surf_get_ccs_surf(const struct isl_device *dev,
                       const struct isl_surf *surf,
-                      struct isl_surf *aux_surf,
-                      struct isl_surf *extra_aux_surf,
+                      struct isl_surf *ccs_surf,
                       uint32_t row_pitch_B /**< Ignored if 0 */);
 
 #define isl_surf_fill_state(dev, state, ...) \
