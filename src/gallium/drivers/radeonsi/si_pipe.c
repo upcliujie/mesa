e56f011ace8b5692cc2a2295f1ba27e3433e5606
Marek Olšák <marek.olsak@amd.com>
2021-06-20 01:42:18 -0400

    radeonsi: don't use NGG culling on 1 RB chips

--- src/gallium/drivers/radeonsi/si_pipe.c
+++ src/gallium/drivers/radeonsi/si_pipe.c
@@ -1240,7 +1240,9 @@ static struct pipe_screen *radeonsi_screen_create_impl(struct radeon_winsys *ws,
                       sscreen->info.chip_class >= GFX10 &&
                       (sscreen->info.family != CHIP_NAVI14 ||
                        sscreen->info.is_pro_graphics);
-   sscreen->use_ngg_culling = sscreen->use_ngg && !(sscreen->debug_flags & DBG(NO_NGG_CULLING));
+   sscreen->use_ngg_culling = sscreen->use_ngg &&
+                              sscreen->info.max_render_backends >= 2 &&
+                              !(sscreen->debug_flags & DBG(NO_NGG_CULLING));
    sscreen->use_ngg_streamout = false;
 
    /* Only set this for the cases that are known to work, which are:
