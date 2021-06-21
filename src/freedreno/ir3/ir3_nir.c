005d7231924148b50864b22bc57657306c8fcd81
Rob Clark <robdclark@chromium.org>
2021-06-21 14:21:00 -0700

    freedreno+ir3: Enable INT16
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- src/freedreno/ir3/ir3_nir.c
+++ src/freedreno/ir3/ir3_nir.c
@@ -223,6 +223,7 @@ ir3_optimize_loop(struct ir3_compiler *compiler, nir_shader *s)
 			progress |= OPT(s, nir_opt_gcm, false);
 		progress |= OPT(s, nir_opt_peephole_select, 16, true, true);
 		progress |= OPT(s, nir_opt_intrinsics);
+		progress |= OPT(s, nir_opt_phi_precision);
 		progress |= OPT(s, nir_opt_algebraic);
 		progress |= OPT(s, nir_lower_alu);
 		progress |= OPT(s, nir_lower_pack);
