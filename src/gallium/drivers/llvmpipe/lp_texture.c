Grima04/mesa-turnip-kgsl

commit 2dcc9c7f54edbff075665ebe5d50f2499dc12163

llvmpipe: add resource get param support.
This implements this resource get param callback and uses it
to return image information for lavapipe.

Reviewed-by: Roland Scheidegger <sroland@vmware.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6639>

src/gallium/drivers/llvmpipe/lp_texture.c
@@ -896,6 +896,44 @@ llvmpipe_get_resource_info(struct pipe_screen *screen,
    *offset = 0;
 }
 
+static bool
+llvmpipe_resource_get_param(struct pipe_screen *screen,
+                            struct pipe_context *context,
+                            struct pipe_resource *resource,
+                            unsigned plane,
+                            unsigned layer,
+                            unsigned level,
+                            enum pipe_resource_param param,
+                            unsigned handle_usage,
+                            uint64_t *value)
+{
+   struct llvmpipe_resource *lpr = llvmpipe_resource(resource);
+
+   switch (param) {
+   case PIPE_RESOURCE_PARAM_NPLANES:
+      *value = 1;
+      return true;
+   case PIPE_RESOURCE_PARAM_STRIDE:
+      *value = lpr->row_stride[level];
+      return true;
+   case PIPE_RESOURCE_PARAM_OFFSET:
+      *value = lpr->mip_offsets[level] + (lpr->img_stride[level] * layer);
+      return true;
+   case PIPE_RESOURCE_PARAM_LAYER_STRIDE:
+      *value = lpr->img_stride[level];
+      return true;
+   case PIPE_RESOURCE_PARAM_MODIFIER:
+   case PIPE_RESOURCE_PARAM_HANDLE_TYPE_SHARED:
+   case PIPE_RESOURCE_PARAM_HANDLE_TYPE_KMS:
+   case PIPE_RESOURCE_PARAM_HANDLE_TYPE_FD:
+   default:
+      break;
+   }
+   assert(0);
+   *value = 0;
+   return false;
+}
+
 void
 llvmpipe_init_screen_resource_funcs(struct pipe_screen *screen)
 {
@@ -921,6 +959,7 @@ llvmpipe_init_screen_resource_funcs(struct pipe_screen *screen)
    screen->resource_create_unbacked = llvmpipe_resource_create_unbacked;
 
    screen->resource_get_info = llvmpipe_get_resource_info;
+   screen->resource_get_param = llvmpipe_resource_get_param;
    screen->allocate_memory = llvmpipe_allocate_memory;
    screen->free_memory = llvmpipe_free_memory;
    screen->map_memory = llvmpipe_map_memory;

upcliujie/mesa

commit ae2eabf0e8ef58a294c418ade9b89212b2d91b58

llvmpipe: Add handle export for resource_get_param
mesa/mesa@2dcc9c7f54ed from mesa/mesa!6639 added a resource_get_param
hook for llvmpipe, which was nice since it gave lavapipe more features.
One of those features was not exporting llvmpipe textures, so those
parts were stubbed out and landed in an assert(0).

This completely broke kms_swrast (llvmpipe+GBM) on non-release builds,
since that definitely does need to export llvmpipe textures.

The query codepath which caused this explosion does fall back to
resource_get_handle() - which is how it worked previously - but not all
callers do this, so just do what all other drivers implementing
resource_get_param() do and open-code the translation.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Reported-by: Jonas Ådahl <jadahl@gmail.com>
Fixes: 2dcc9c7f54ed ("llvmpipe: add resource get param support.")
Ref: https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6639

src/gallium/drivers/llvmpipe/lp_texture.c
@@ -53,6 +53,8 @@
 
 #include "frontend/sw_winsys.h"
 
+#include "drm-uapi/drm_fourcc.h"
+
 
 #ifdef DEBUG
 static struct llvmpipe_resource resource_list;
@@ -904,6 +906,7 @@ llvmpipe_resource_get_param(struct pipe_screen *screen,
                             uint64_t *value)
 {
    struct llvmpipe_resource *lpr = llvmpipe_resource(resource);
+   struct winsys_handle whandle;
 
    switch (param) {
    case PIPE_RESOURCE_PARAM_NPLANES:
@@ -919,9 +922,26 @@ llvmpipe_resource_get_param(struct pipe_screen *screen,
       *value = lpr->img_stride[level];
       return true;
    case PIPE_RESOURCE_PARAM_MODIFIER:
+      *value = DRM_FORMAT_MOD_INVALID;
+      return true;
    case PIPE_RESOURCE_PARAM_HANDLE_TYPE_SHARED:
    case PIPE_RESOURCE_PARAM_HANDLE_TYPE_KMS:
    case PIPE_RESOURCE_PARAM_HANDLE_TYPE_FD:
+      if (!lpr->dt)
+         return false;
+
+      memset(&whandle, 0, sizeof(whandle));
+      if (param == PIPE_RESOURCE_PARAM_HANDLE_TYPE_SHARED)
+         whandle.type = WINSYS_HANDLE_TYPE_SHARED;
+      else if (param == PIPE_RESOURCE_PARAM_HANDLE_TYPE_KMS)
+         whandle.type = WINSYS_HANDLE_TYPE_KMS;
+      else if (param == PIPE_RESOURCE_PARAM_HANDLE_TYPE_FD)
+         whandle.type = WINSYS_HANDLE_TYPE_FD;
+
+      if (!screen->resource_get_handle(screen, context, resource, &whandle, handle_usage))
+         return false;
+      *value = whandle.handle;
+      return true;
    default:
       break;
    }