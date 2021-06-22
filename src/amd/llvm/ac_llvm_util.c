Grima04/mesa-turnip-kgsl

commit 3bb65c06706a58b124e4a600eeb35a7b3c5c3a23

ac: force enable -structurizecfg-skip-uniform-regions for LLVM 11
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/4696>

src/amd/llvm/ac_llvm_util.c
@@ -70,6 +70,10 @@ static void ac_init_llvm_target()
 		/* Atomic optimizations require LLVM 10.0 for gfx10 support. */
 		"-amdgpu-atomic-optimizations=true",
 #endif
+#if LLVM_VERSION_MAJOR >= 11
+		/* This was disabled by default in: https://reviews.llvm.org/D77228 */
+		"-structurizecfg-skip-uniform-regions",
+#endif
 	};
 	LLVMParseCommandLineOptions(ARRAY_SIZE(argv), argv, NULL);
 }

upcliujie/mesa

commit 0d45e714218427d17da947fd1b10ae1b4f687788

ac/llvm: don't set skip-uniform-regions to fix atomic.cmpswap
This fixes SSBO atomic cmpswap tests and transform feedback order piglit tests.

Fixes: 3bb65c06706 - ac: force enable -structurizecfg-skip-uniform-regions for LLVM 11
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3884

src/amd/llvm/ac_llvm_util.c
@@ -68,7 +68,10 @@ static void ac_init_llvm_target(void)
       "-simplifycfg-sink-common=false",
       "-global-isel-abort=2",
       "-amdgpu-atomic-optimizations=true",
+#if LLVM_VERSION_MAJOR == 11
+      /* This fixes variable indexing on LLVM 11. It also breaks atomic.cmpswap on LLVM >= 12. */
       "-structurizecfg-skip-uniform-regions",
+#endif
    };
    LLVMParseCommandLineOptions(ARRAY_SIZE(argv), argv, NULL);
 }