172beacc8c2157e62d60681589a1f92d7346f257
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-06-23 18:15:44 +0300

    intel/devinfo: store the different kind of DG2
    Those are marketing terms :
    
       G10: ~512 EUs
       G11: ~128 EUs
    
    We'll use those to select the right performance metric
    programming/equations and also apply workaround accordingly.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/intel/dev/intel_device_info.c
+++ src/intel/dev/intel_device_info.c
@@ -1039,8 +1039,14 @@ static const struct intel_device_info intel_device_info_sg1 = {
    .platform = INTEL_PLATFORM_DG2,                              \
    .has_lsc = true
 
-static const struct intel_device_info intel_device_info_dg2 = {
+static const struct intel_device_info intel_device_info_dg2_g10 = {
    DG2_FEATURES,
+   .dg2_sku = INTEL_DEVICE_DG2_G10,
+};
+
+static const struct intel_device_info intel_device_info_dg2_g11 = {
+   DG2_FEATURES,
+   .dg2_sku = INTEL_DEVICE_DG2_G11,
 };
 
 static void
