Grima04/mesa-turnip-kgsl

commit f7c58559f5731790f4e68f1b1cb38c10818efa96

radeonsi: refine fast clears for small buffers, always use them for large HTILE
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10003>

src/gallium/drivers/radeonsi/si_clear.c
@@ -538,6 +538,7 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
    struct si_clear_info info[8 * 2 + 1]; /* MRTs * (CMASK + DCC) + ZS */
    unsigned num_clears = 0;
    unsigned clear_types = 0;
+   bool fb_too_small = fb->width * fb->height * fb->layers <= 512 * 512;
 
    /* This function is broken in BE, so just disable this path for now */
 #if UTIL_ARCH_BIG_ENDIAN
@@ -591,8 +592,7 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
        *
        * This helps on both dGPUs and APUs, even small APUs like Mullins.
        */
-      bool too_small = tex->buffer.b.b.nr_samples <= 1 &&
-                       tex->buffer.b.b.width0 * tex->buffer.b.b.height0 <= 512 * 512;
+      bool too_small = tex->buffer.b.b.nr_samples <= 1 && fb_too_small;
       bool eliminate_needed = false;
       bool fmask_decompress_needed = false;
 
@@ -843,12 +843,13 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
                                  zstex->surface.meta_offset, zstex->surface.meta_size, clear_value);
             clear_types |= SI_CLEAR_TYPE_HTILE;
          }
-      } else if (num_clears) {
+      } else if (num_clears || !fb_too_small) {
          /* This is where the HTILE buffer clear is done.
           *
-          * If there is no clear scheduled, we should use the draw-based clear that is without
-          * waits. If there is some other clear scheduled, we will have to wait anyway, so add
-          * the HTILE buffer clear to the batch here.
+          * If there is no clear scheduled and the framebuffer size is too small, we should use
+          * the draw-based clear that is without waits. If there is some other clear scheduled,
+          * we will have to wait anyway, so add the HTILE buffer clear to the batch here.
+          * If the framebuffer size is large enough, use this codepath too.
           */
          uint64_t htile_offset = zstex->surface.meta_offset;
          unsigned htile_size = 0;

upcliujie/mesa

commit 74a53a48ae713068adddee27e0ffbbf403e5c1f9

radeonsi: fix fb_too_small condition
fb->num_layers can be 0 so use util_num_layers(tex) instead.

Fixes: f7c58559f57 ("radeonsi: refine fast clears for small buffers, always use them for large HTILE")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4764

src/gallium/drivers/radeonsi/si_clear.c
@@ -541,7 +541,7 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
    struct si_clear_info info[8 * 2 + 1]; /* MRTs * (CMASK + DCC) + ZS */
    unsigned num_clears = 0;
    unsigned clear_types = 0;
-   bool fb_too_small = fb->width * fb->height * fb->layers <= 512 * 512;
+   unsigned num_pixels = fb->width * fb->height;
 
    /* This function is broken in BE, so just disable this path for now */
 #if UTIL_ARCH_BIG_ENDIAN
@@ -595,6 +595,7 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
        *
        * This helps on both dGPUs and APUs, even small APUs like Mullins.
        */
+      bool fb_too_small = num_pixels * num_layers <= 512 * 512;
       bool too_small = tex->buffer.b.b.nr_samples <= 1 && fb_too_small;
       bool eliminate_needed = false;
       bool fmask_decompress_needed = false;
@@ -768,6 +769,7 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
       unsigned level = zsbuf->u.tex.level;
       bool update_db_depth_clear = false;
       bool update_db_stencil_clear = false;
+      bool fb_too_small = num_pixels * zs_num_layers <= 512 * 512;
 
       /* Transition from TC-incompatible to TC-compatible HTILE if requested. */
       if (zstex->enable_tc_compatible_htile_next_clear) {