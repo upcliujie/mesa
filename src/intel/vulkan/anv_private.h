Grima04/mesa-turnip-kgsl

commit 829699ba632b2b78e4de372baf42ae01095158a7

anv: implement shareable timeline semaphores
This implements timeline semaphores using a new type of dma-fence
stored into drm-syncobjs. We use a thread to implement delayed
submissions.

v2: Drop cloning of temporary semaphores and just transfer their ownership (Jason)
    Drain queue when dealing with binary semaphore
    Ensure we don't submit to the thread as long as we don't need to

v3: Use __u64 not uintptr_t for kernel pointers
    Fix commented code for INTEL_DEBUG=bat
    Set DRM_I915_GEM_EXECBUFFER_EXT_TIMELINE_FENCES in timeline fence execbuf extension
    Add new anv_queue_set_lost()
    Drop multi queue stuff meant for the fake multi queue patch
    Rework temporary syncobj handling
    Don't use syncobj when not available (DeviceWaitIdle/CreateDevice)
    Use ANV_MULTIALLOC
    And a few more tweaks...

v4: Drop drained condition helper (Lionel)
    Fix missing EXEC_OBJECT_WRITE on BOs we want to wait on (Jason)

v5: Add missing device->lost_reported in _anv_device_report_lost (Lionel)
    Fix missing free on submit->simple_bo (Lionel)
    Don't drop setting the device in lost state on QueueSubmit error (Jason)
    Store submit->fence_bos as an array of uintptr_t (Jason)

v6: condition device->has_thread_submit to i915 & core DRM support (Jason)

v7: Fix submit->in_fence leakage on error (Jason)
    Keep dummy semaphore with no thread submission (Jason)

v8: Move ownership of submit->out_fence to submit (Jason)

v9: Don't forget to read the VkFence's syncobj binary payload (Lionel)

v10: Take the mutex lock on anv_gem_close() (Jason/Lionel)

v11: Fix void* -> u64 cast on 32bit (Lionel)

v12: Rebase after BO backed timeline semaphore (Lionel)

v13: Fix missing snippets lost after rebase (Lionel)

v14: Drop update_binary usage (Lionel)

v15: Use ANV_MULTIALLOC (Lionel)

v16: Fix some realloc issues (Ivan)

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net> (v8)

src/intel/vulkan/anv_private.h
@@ -1082,6 +1082,7 @@ struct anv_physical_device {
     bool                                        has_syncobj_wait_available;
     bool                                        has_context_priority;
     bool                                        has_context_isolation;
+    bool                                        has_thread_submit;
     bool                                        has_mem_available;
     bool                                        has_mmap_offset;
     uint64_t                                    gtt_size;
@@ -1183,6 +1184,7 @@ struct anv_queue_submit {
    uint32_t                                  fence_count;
    uint32_t                                  fence_array_length;
    struct drm_i915_gem_exec_fence *          fences;
+   uint64_t *                                fence_values;
 
    uint32_t                                  temporary_semaphore_count;
    uint32_t                                  temporary_semaphore_array_length;
@@ -1194,7 +1196,10 @@ struct anv_queue_submit {
    uint32_t                                  sync_fd_semaphore_array_length;
 
    /* Allocated only with non shareable timelines. */
-   struct anv_timeline **                    wait_timelines;
+   union {
+      struct anv_timeline **                 wait_timelines;
+      uint32_t *                             wait_timeline_syncobjs;
+   };
    uint32_t                                  wait_timeline_count;
    uint32_t                                  wait_timeline_array_length;
    uint64_t *                                wait_timeline_values;
@@ -1229,14 +1234,34 @@ struct anv_queue_submit {
 struct anv_queue {
     struct vk_object_base                       base;
 
-    struct anv_device *                         device;
+   struct anv_device *                       device;
 
-    /*
-     * A list of struct anv_queue_submit to be submitted to i915.
-     */
-    struct list_head                            queued_submits;
+   VkDeviceQueueCreateFlags                  flags;
 
-    VkDeviceQueueCreateFlags                    flags;
+   /* Set once from the device api calls. */
+   bool                                      lost_signaled;
+
+   /* Only set once atomically by the queue */
+   int                                       lost;
+   int                                       error_line;
+   const char *                              error_file;
+   char                                      error_msg[80];
+
+   /*
+    * This mutext protects the variables below.
+    */
+   pthread_mutex_t                           mutex;
+
+   pthread_t                                 thread;
+   pthread_cond_t                            cond;
+
+   /*
+    * A list of struct anv_queue_submit to be submitted to i915.
+    */
+   struct list_head                          queued_submits;
+
+   /* Set to true to stop the submission thread */
+   bool                                      quit;
 };
 
 struct anv_pipeline_cache {
@@ -1330,6 +1355,7 @@ struct anv_device {
     int                                         fd;
     bool                                        can_chain_batches;
     bool                                        robust_buffer_access;
+    bool                                        has_thread_submit;
     struct anv_device_extension_table           enabled_extensions;
     struct anv_device_dispatch_table            dispatch;
 
@@ -1382,6 +1408,7 @@ struct anv_device {
     pthread_mutex_t                             mutex;
     pthread_cond_t                              queue_submit;
     int                                         _lost;
+    int                                         lost_reported;
 
     struct gen_batch_decode_ctx                 decoder_ctx;
     /*
@@ -1439,7 +1466,7 @@ anv_mocs_for_bo(const struct anv_device *device, const struct anv_bo *bo)
 void anv_device_init_blorp(struct anv_device *device);
 void anv_device_finish_blorp(struct anv_device *device);
 
-void _anv_device_set_all_queue_lost(struct anv_device *device);
+void _anv_device_report_lost(struct anv_device *device);
 VkResult _anv_device_set_lost(struct anv_device *device,
                               const char *file, int line,
                               const char *msg, ...)
@@ -1451,12 +1478,17 @@ VkResult _anv_queue_set_lost(struct anv_queue *queue,
 #define anv_device_set_lost(dev, ...) \
    _anv_device_set_lost(dev, __FILE__, __LINE__, __VA_ARGS__)
 #define anv_queue_set_lost(queue, ...) \
-   _anv_queue_set_lost(queue, __FILE__, __LINE__, __VA_ARGS__)
+   (queue)->device->has_thread_submit ? \
+   _anv_queue_set_lost(queue, __FILE__, __LINE__, __VA_ARGS__) : \
+   _anv_device_set_lost(queue->device, __FILE__, __LINE__, __VA_ARGS__)
 
 static inline bool
 anv_device_is_lost(struct anv_device *device)
 {
-   return unlikely(p_atomic_read(&device->_lost));
+   int lost = p_atomic_read(&device->_lost);
+   if (unlikely(lost && !device->lost_reported))
+      _anv_device_report_lost(device);
+   return lost;
 }
 
 VkResult anv_device_query_status(struct anv_device *device);
@@ -3176,6 +3208,7 @@ enum anv_semaphore_type {
    ANV_SEMAPHORE_TYPE_SYNC_FILE,
    ANV_SEMAPHORE_TYPE_DRM_SYNCOBJ,
    ANV_SEMAPHORE_TYPE_TIMELINE,
+   ANV_SEMAPHORE_TYPE_DRM_SYNCOBJ_TIMELINE,
 };
 
 struct anv_timeline_point {

src/intel/vulkan/anv_batch_chain.c
@@ -1091,6 +1091,8 @@ anv_cmd_buffer_add_secondary(struct anv_cmd_buffer *primary,
 struct anv_execbuf {
    struct drm_i915_gem_execbuffer2           execbuf;
 
+   struct drm_i915_gem_execbuffer_ext_timeline_fences timeline_fences;
+
    struct drm_i915_gem_exec_object2 *        objects;
    uint32_t                                  bo_count;
    struct anv_bo **                          bos;
@@ -1117,6 +1119,24 @@ anv_execbuf_finish(struct anv_execbuf *exec)
 {
    vk_free(exec->alloc, exec->objects);
    vk_free(exec->alloc, exec->bos);
+}
+
+static void
+anv_execbuf_add_ext(struct anv_execbuf *exec,
+                    uint32_t ext_name,
+                    struct i915_user_extension *ext)
+{
+   __u64 *iter = &exec->execbuf.cliprects_ptr;
+
+   exec->execbuf.flags |= I915_EXEC_USE_EXTENSIONS;
+
+   while (*iter != 0) {
+      iter = (__u64 *) &((struct i915_user_extension *)(uintptr_t)*iter)->next_extension;
+   }
+
+   ext->name = ext_name;
+
+   *iter = (uintptr_t) ext;
 }
 
 static VkResult
@@ -1754,18 +1774,30 @@ anv_queue_execbuf_locked(struct anv_queue *queue,
 
    if (submit->fence_count > 0) {
       assert(device->physical->has_syncobj);
-      execbuf.execbuf.flags |= I915_EXEC_FENCE_ARRAY;
-      execbuf.execbuf.num_cliprects = submit->fence_count;
-      execbuf.execbuf.cliprects_ptr = (uintptr_t)submit->fences;
+      if (device->has_thread_submit) {
+         execbuf.timeline_fences.fence_count = submit->fence_count;
+         execbuf.timeline_fences.handles_ptr = (uintptr_t)submit->fences;
+         execbuf.timeline_fences.values_ptr = (uintptr_t)submit->fence_values;
+         anv_execbuf_add_ext(&execbuf,
+                             DRM_I915_GEM_EXECBUFFER_EXT_TIMELINE_FENCES,
+                             &execbuf.timeline_fences.base);
+      } else {
+         execbuf.execbuf.flags |= I915_EXEC_FENCE_ARRAY;
+         execbuf.execbuf.num_cliprects = submit->fence_count;
+         execbuf.execbuf.cliprects_ptr = (uintptr_t)submit->fences;
+      }
    }
 
    if (submit->in_fence != -1) {
+      assert(!device->has_thread_submit);
       execbuf.execbuf.flags |= I915_EXEC_FENCE_IN;
       execbuf.execbuf.rsvd2 |= (uint32_t)submit->in_fence;
    }
 
-   if (submit->need_out_fence)
+   if (submit->need_out_fence) {
+      assert(!device->has_thread_submit);
       execbuf.execbuf.flags |= I915_EXEC_FENCE_OUT;
+   }
 
    if (has_perf_query) {
       struct anv_query_pool *query_pool = submit->cmd_buffer->perf_query_pool;

src/intel/vulkan/anv_device.c
@@ -460,6 +460,9 @@ anv_physical_device_try_create(struct anv_instance *instance,
    if (env_var_as_boolean("ANV_QUEUE_THREAD_DISABLE", false))
       device->has_exec_timeline = false;
 
+   device->has_thread_submit =
+      device->has_syncobj_wait_available && device->has_exec_timeline;
+
    device->always_use_bindless =
       env_var_as_boolean("ANV_ALWAYS_BINDLESS", false);
 
@@ -2821,6 +2824,8 @@ VkResult anv_CreateDevice(
       goto fail_fd;
    }
 
+   device->has_thread_submit = physical_device->has_thread_submit;
+
    result = anv_queue_init(device, &device->queue);
    if (result != VK_SUCCESS)
       goto fail_context_id;
@@ -3111,11 +3116,11 @@ void anv_DestroyDevice(
    if (!device)
       return;
 
+   anv_queue_finish(&device->queue);
+
    anv_device_finish_blorp(device);
 
    anv_pipeline_cache_finish(&device->default_pipeline_cache);
-
-   anv_queue_finish(&device->queue);
 
 #ifdef HAVE_VALGRIND
    /* We only need to free these to prevent valgrind errors.  The backing
@@ -3228,6 +3233,22 @@ void anv_GetDeviceQueue2(
       *pQueue = NULL;
 }
 
+void
+_anv_device_report_lost(struct anv_device *device)
+{
+   assert(p_atomic_read(&device->_lost) > 0);
+
+   device->lost_reported = true;
+
+   struct anv_queue *queue = &device->queue;
+
+   __vk_errorf(device->physical->instance, device,
+               VK_DEBUG_REPORT_OBJECT_TYPE_DEVICE_EXT,
+               VK_ERROR_DEVICE_LOST,
+               queue->error_file, queue->error_line,
+               "%s", queue->error_msg);
+}
+
 VkResult
 _anv_device_set_lost(struct anv_device *device,
                      const char *file, int line,
@@ -3236,7 +3257,11 @@ _anv_device_set_lost(struct anv_device *device,
    VkResult err;
    va_list ap;
 
+   if (p_atomic_read(&device->_lost) > 0)
+      return VK_ERROR_DEVICE_LOST;
+
    p_atomic_inc(&device->_lost);
+   device->lost_reported = true;
 
    va_start(ap, msg);
    err = __vk_errorv(device->physical->instance, device,
@@ -3252,24 +3277,29 @@ _anv_device_set_lost(struct anv_device *device,
 
 VkResult
 _anv_queue_set_lost(struct anv_queue *queue,
-                    const char *file, int line,
-                    const char *msg, ...)
+                     const char *file, int line,
+                     const char *msg, ...)
 {
-   VkResult err;
    va_list ap;
 
-   p_atomic_inc(&queue->device->_lost);
+   if (queue->lost)
+      return VK_ERROR_DEVICE_LOST;
 
+   queue->lost = true;
+
+   queue->error_file = file;
+   queue->error_line = line;
    va_start(ap, msg);
-   err = __vk_errorv(queue->device->physical->instance, queue->device,
-                     VK_DEBUG_REPORT_OBJECT_TYPE_DEVICE_EXT,
-                     VK_ERROR_DEVICE_LOST, file, line, msg, ap);
+   vsnprintf(queue->error_msg, sizeof(queue->error_msg),
+             msg, ap);
    va_end(ap);
+
+   p_atomic_inc(&queue->device->_lost);
 
    if (env_var_as_boolean("ANV_ABORT_ON_DEVICE_LOSS", false))
       abort();
 
-   return err;
+   return VK_ERROR_DEVICE_LOST;
 }
 
 VkResult

src/intel/vulkan/anv_wsi.c
@@ -299,10 +299,62 @@ VkResult anv_QueuePresentKHR(
       }
    }
 
-   return wsi_common_queue_present(&queue->device->physical->wsi_device,
-                                   anv_device_to_handle(queue->device),
-                                   _queue, 0,
-                                   pPresentInfo);
+   if (device->has_thread_submit &&
+       pPresentInfo->waitSemaphoreCount > 0) {
+      /* Make sure all of the dependency semaphores have materialized when
+       * using a threaded submission.
+       */
+      uint32_t *syncobjs = vk_alloc(&device->vk.alloc,
+                                    sizeof(*syncobjs) * pPresentInfo->waitSemaphoreCount, 8,
+                                    VK_SYSTEM_ALLOCATION_SCOPE_COMMAND);
+
+      if (!syncobjs)
+         return vk_error(VK_ERROR_OUT_OF_HOST_MEMORY);
+
+      uint32_t wait_count = 0;
+      for (uint32_t i = 0; i < pPresentInfo->waitSemaphoreCount; i++) {
+         ANV_FROM_HANDLE(anv_semaphore, semaphore, pPresentInfo->pWaitSemaphores[i]);
+         struct anv_semaphore_impl *impl =
+            semaphore->temporary.type != ANV_SEMAPHORE_TYPE_NONE ?
+            &semaphore->temporary : &semaphore->permanent;
+
+         if (impl->type == ANV_SEMAPHORE_TYPE_DUMMY)
+            continue;
+         assert(impl->type == ANV_SEMAPHORE_TYPE_DRM_SYNCOBJ);
+         syncobjs[wait_count++] = impl->syncobj;
+      }
+
+      int ret = 0;
+      if (wait_count > 0) {
+         ret =
+            anv_gem_syncobj_wait(device, syncobjs, wait_count,
+                                 anv_get_absolute_timeout(INT64_MAX),
+                                 true /* wait_all */);
+      }
+
+      vk_free(&device->vk.alloc, syncobjs);
+
+      if (ret)
+         return vk_error(VK_ERROR_DEVICE_LOST);
+   }
+
+   VkResult result = wsi_common_queue_present(&device->physical->wsi_device,
+                                              anv_device_to_handle(queue->device),
+                                              _queue, 0,
+                                              pPresentInfo);
+
+   for (uint32_t i = 0; i < pPresentInfo->waitSemaphoreCount; i++) {
+      ANV_FROM_HANDLE(anv_semaphore, semaphore, pPresentInfo->pWaitSemaphores[i]);
+      /* From the Vulkan 1.0.53 spec:
+       *
+       *    "If the import is temporary, the implementation must restore the
+       *    semaphore to its prior permanent state after submitting the next
+       *    semaphore wait operation."
+       */
+      anv_semaphore_reset_temporary(queue->device, semaphore);
+   }
+
+   return result;
 }
 
 VkResult anv_GetDeviceGroupPresentCapabilitiesKHR(

upcliujie/mesa

commit 6f0dfad88d810766e7a5776ea1f946d343733bb7

anv: ensure i915 submission of binary semaphores before QueueSubmit returns
The delayed submission, due to the submission thread, can lead to a
syncobj semaphore used both for wait and signal operations to be reset
before the submission thread has sent the syncobj to i915.

This leads to a failure in i915 because we're passing an empty syncobj
to wait on.

v2: Fix up error cases (Lionel)
    Document things better (Lionel)

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4945
Fixes: 829699ba632b ("anv: implement shareable timeline semaphores")

src/intel/vulkan/anv_private.h
@@ -1051,6 +1051,9 @@ struct anv_queue_submit {
    uint32_t                                  signal_timeline_array_length;
    uint64_t *                                signal_timeline_values;
 
+   /* Whether or not to submit this from the submission thread. */
+   bool                                      no_submit_thread;
+
    int                                       in_fence;
    bool                                      need_out_fence;
    int                                       out_fence;
@@ -1107,6 +1110,8 @@ struct anv_queue {
     * thread when the queued_submits list is empty.
     */
    pthread_cond_t                            consumer_cond;
+
+   bool                                      drained;
 
    /*
     * A list of struct anv_queue_submit to be submitted to i915.

src/intel/vulkan/anv_queue.c
@@ -472,12 +472,29 @@ anv_queue_task(void *_queue)
    return NULL;
 }
 
+static void
+anv_queue_drain(struct anv_queue *queue)
+{
+   if (queue->drained)
+      return;
+
+   pthread_mutex_lock(&queue->mutex);
+   if (!list_is_empty(&queue->queued_submits))
+      pthread_cond_wait(&queue->consumer_cond, &queue->mutex);
+   assert(list_is_empty(&queue->queued_submits));
+   pthread_mutex_unlock(&queue->mutex);
+
+   queue->drained = true;
+}
+
 static VkResult
 anv_queue_submit_post(struct anv_queue *queue,
                       struct anv_queue_submit **_submit,
                       bool flush_queue)
 {
    struct anv_queue_submit *submit = *_submit;
+   VkResult result;
+
 
    /* Wait before signal behavior means we might keep alive the
     * anv_queue_submit object a bit longer, so transfer the ownership to the
@@ -486,10 +503,19 @@ anv_queue_submit_post(struct anv_queue *queue,
    *_submit = NULL;
    if (queue->device->has_thread_submit) {
       pthread_mutex_lock(&queue->mutex);
-      pthread_cond_broadcast(&queue->producer_cond);
-      list_addtail(&submit->link, &queue->queued_submits);
+      if (submit->no_submit_thread) {
+         /* Things should have been drained before. */
+         assert(queue->drained);
+         assert(list_is_empty(&queue->queued_submits));
+         result = anv_queue_submit_process_and_free(queue, submit);
+      } else {
+         pthread_cond_broadcast(&queue->producer_cond);
+         list_addtail(&submit->link, &queue->queued_submits);
+         queue->drained = false;
+         result = VK_SUCCESS;
+      }
       pthread_mutex_unlock(&queue->mutex);
-      return VK_SUCCESS;
+      return result;
    } else {
       pthread_mutex_lock(&queue->device->mutex);
       list_addtail(&submit->link, &queue->queued_submits);
@@ -1124,11 +1150,12 @@ anv_queue_submit_add_in_semaphores(struct anv_queue_submit *submit,
 
 static VkResult
 anv_queue_submit_add_out_semaphores(struct anv_queue_submit *submit,
-                                    struct anv_device *device,
+                                    struct anv_queue *queue,
                                     const VkSemaphore *out_semaphores,
                                     const uint64_t *out_values,
                                     uint32_t num_out_semaphores)
 {
+   struct anv_device *device = queue->device;
    ASSERTED struct anv_physical_device *pdevice = device->physical;
    VkResult result;
 
@@ -1170,7 +1197,13 @@ anv_queue_submit_add_out_semaphores(struct anv_queue_submit *submit,
           * Reset the content of the syncobj so it doesn't contain a
           * previously signaled dma-fence, until one is added by EXECBUFFER by
           * the submission thread.
+          *
+          * In order to do this, we must ensure all previous submissions on
+          * this syncobj have been processed by i915 and we must flag the
+          * submission to not go through the submission thread.
           */
+         anv_queue_drain(queue);
+         submit->no_submit_thread = true;
          anv_gem_syncobj_reset(device, impl->syncobj);
 
          result = anv_queue_submit_add_syncobj(submit, device, impl->syncobj,
@@ -1489,7 +1522,7 @@ VkResult anv_QueueSubmit(
 
       /* Signal semaphores */
       result = anv_queue_submit_add_out_semaphores(submit,
-                                                   device,
+                                                   queue,
                                                    pSubmits[i].pSignalSemaphores,
                                                    signal_values,
                                                    pSubmits[i].signalSemaphoreCount);