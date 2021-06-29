af94b47a57b2c9029532afc63f737429d126ddc8
Rhys Perry <pendingchaos02@gmail.com>
2021-06-29 16:30:00 +0100

    radv: adjust num_records when offset>stride
    If an attribute's offset is larger than the stride, the compiler will
    increase the vertex index and use offset%stride instead as the offset.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5007

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -1727,6 +1727,7 @@ struct radv_pipeline {
 
    uint8_t attrib_bindings[MAX_VERTEX_ATTRIBS];
    uint32_t attrib_ends[MAX_VERTEX_ATTRIBS];
+   uint32_t attrib_index_offset[MAX_VERTEX_ATTRIBS];
 
    bool use_per_attribute_vb_descs;
    uint32_t vb_desc_usage_mask;
