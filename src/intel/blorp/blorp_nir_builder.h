Grima04/mesa-turnip-kgsl

commit ef5bce9253e2221ef8df0764447cb02c02fa2a16

intel: Drop the last uses of a mem_ctx in nir_builder_init_simple_shader().
These two consumers were the only ones out of the ~65 calls to
init_simple_shader, so there's a pretty clear consensus on how to allocate
simple shaders.  I suspect that actually these would be just fine with
b.shader being the mem_ctx, but that would take a bit more rework.

Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7323>

src/intel/blorp/blorp_nir_builder.h
@@ -29,7 +29,8 @@ blorp_nir_init_shader(nir_builder *b,
                       gl_shader_stage stage,
                       const char *name)
 {
-   *b = nir_builder_init_simple_shader(mem_ctx, stage, NULL);
+   *b = nir_builder_init_simple_shader(NULL, stage, NULL);
+   ralloc_adopt(mem_ctx, b->shader);
    if (name != NULL)
       b->shader->info.name = ralloc_strdup(b->shader, name);
    if (stage == MESA_SHADER_FRAGMENT)

src/intel/compiler/brw_nir.c
@@ -1475,8 +1475,9 @@ brw_nir_create_passthrough_tcs(void *mem_ctx, const struct brw_compiler *compile
                                const nir_shader_compiler_options *options,
                                const struct brw_tcs_prog_key *key)
 {
-   nir_builder b = nir_builder_init_simple_shader(mem_ctx, MESA_SHADER_TESS_CTRL,
+   nir_builder b = nir_builder_init_simple_shader(NULL, MESA_SHADER_TESS_CTRL,
                                                   options);
+   ralloc_adopt(mem_ctx, b.shader);
    nir_shader *nir = b.shader;
    nir_variable *var;
    nir_intrinsic_instr *load;

upcliujie/mesa

commit 62d415c47e50508ba93fac2370756fcf1e07027f

intel: fix leaking memory on shader creation
ralloc_adopt takes care of all the shader's children, but shader itsel ends up
orphaned and never gets free'd.

Fixes: ef5bce9253e ("intel: Drop the last uses of a mem_ctx in nir_builder_init_simple_shader().")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4951

Signed-off-by: Yevhenii Kolesnikov <yevhenii.kolesnikov@globallogic.com>

src/intel/blorp/blorp_nir_builder.h
@@ -30,7 +30,7 @@ blorp_nir_init_shader(nir_builder *b,
                       const char *name)
 {
    *b = nir_builder_init_simple_shader(stage, NULL, "%s", name ? name : "");
-   ralloc_adopt(mem_ctx, b->shader);
+   ralloc_steal(mem_ctx, b->shader);
    if (stage == MESA_SHADER_FRAGMENT)
       b->shader->info.fs.origin_upper_left = true;
 }

src/intel/compiler/brw_nir.c
@@ -1523,7 +1523,7 @@ brw_nir_create_passthrough_tcs(void *mem_ctx, const struct brw_compiler *compile
 {
    nir_builder b = nir_builder_init_simple_shader(MESA_SHADER_TESS_CTRL,
                                                   options, "passthrough TCS");
-   ralloc_adopt(mem_ctx, b.shader);
+   ralloc_steal(mem_ctx, b.shader);
    nir_shader *nir = b.shader;
    nir_variable *var;
    nir_ssa_def *load;