c8220d8ce308a62b1a622b44969d03dadf440cd1
Karol Herbst <kherbst@redhat.com>
2021-06-30 10:26:35 +0200

    nouveau: make fencing race free

--- src/gallium/drivers/nouveau/nv50/nv50_vbo.c
+++ src/gallium/drivers/nouveau/nv50/nv50_vbo.c
@@ -751,7 +751,7 @@ nv50_draw_vbo_kick_notify(struct nouveau_context *context)
 {
    struct nv50_context *nv50 = nv50_context(&context->pipe);
 
-   nouveau_fence_update(context->screen, true);
+   _nouveau_fence_update(context->screen, true);
    nv50_bufctx_fence(nv50, nv50->bufctx_3d, true);
 }
 
