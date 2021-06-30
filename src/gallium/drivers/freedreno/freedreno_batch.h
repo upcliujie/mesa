a719e7a889b7e895fc183dd93c896dfcbcb7ee36
Emma Anholt <emma@anholt.net>
2021-06-30 16:33:22 -0700

    WIP: freedreno: Use a bitset for faster checks for resource already referenced.
    Worth about 2% on the UBOs drawoverhead test, now just -1.46751% +/-
    0.706231%.  This is somewhat redundant with the bos list in the
    ringbuffer, perhaps we could move this there and reduce some append_bo
    cost to get us all the way to a win?
    
    WIP because I need to look at the replace and shadow paths, and maybe do
    some validation that our tables match up..

--- src/gallium/drivers/freedreno/freedreno_batch.h
+++ src/gallium/drivers/freedreno/freedreno_batch.h
@@ -251,6 +251,9 @@ struct fd_batch {
     */
    struct set *resources;
 
+   BITSET_WORD *bos;
+   uint32_t bos_size;
+
    /** key in batch-cache (if not null): */
    struct fd_batch_key *key;
    uint32_t hash;
