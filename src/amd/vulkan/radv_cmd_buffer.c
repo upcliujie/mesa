Grima04/zink-wip-xlib

radv: emit drawid for multidraws

src/amd/vulkan/radv_cmd_buffer.c
@@ -5347,6 +5347,21 @@ radv_emit_userdata_vertex(struct radv_cmd_buffer *cmd_buffer, const struct radv_
 }
 
 ALWAYS_INLINE static void
+radv_emit_userdata_vertex_drawid(struct radv_cmd_buffer *cmd_buffer, uint32_t vertex_offset, uint32_t drawid)
+{
+   const struct radv_cmd_state *state = &cmd_buffer->state;
+   struct radeon_cmdbuf *cs = cmd_buffer->cs;
+   if (!drawid)
+      radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr, vertex_offset);
+   else {
+      radeon_set_sh_reg_seq(cs, state->pipeline->graphics.vtx_base_sgpr, 2);
+
+      radeon_emit(cs, vertex_offset);
+      radeon_emit(cs, drawid);
+   }
+}
+
+ALWAYS_INLINE static void
 radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
                                const struct radv_draw_info *info,
                                uint32_t drawCount, const VkMultiDrawIndexedInfoEXT *minfo,
@@ -5368,6 +5383,9 @@ radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
          if (!remaining_indexes &&
              cmd_buffer->device->physical_device->rad_info.has_zero_index_buffer_bug)
             continue;
+
+         if (i > 0 && uses_drawid)
+            radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr + sizeof(uint32_t), i);
 
          const uint64_t index_va = state->index_va + draw->first * index_size;
 
@@ -5393,10 +5411,10 @@ radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
          const VkMultiDrawIndexedInfoEXT *next = (void*)(i < drawCount - 1 ? ((uint8_t*)draw + stride) : NULL);
          const bool offset_changes = next && next->offset != draw->offset;
          if (i > 0) {
-            if (state->last_vertex_offset != draw->offset) {
-               radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr, draw->offset);
-               state->last_vertex_offset = draw->offset;
-            }
+            if (state->last_vertex_offset != draw->offset)
+               radv_emit_userdata_vertex_drawid(cmd_buffer, draw->offset, uses_drawid ? i : 0);
+            else if (uses_drawid)
+               radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr + sizeof(uint32_t), i);
          } else
             radv_emit_userdata_vertex(cmd_buffer, info, draw->offset);
 
@@ -5413,6 +5431,9 @@ radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
          }
       );
    }
+   if (drawCount > 1 && uses_drawid) {
+      state->last_drawid = drawCount - 1;
+   }
 }
 
 ALWAYS_INLINE static void
@@ -5420,27 +5441,32 @@ radv_emit_direct_draw_packets(struct radv_cmd_buffer *cmd_buffer, const struct r
                               uint32_t drawCount, const VkMultiDrawInfoEXT *minfo,
                               uint32_t use_opaque, uint32_t stride)
 {
-   struct radv_cmd_state *state = &cmd_buffer->state;
-   struct radeon_cmdbuf *cs = cmd_buffer->cs;
+   const uint32_t view_mask = cmd_buffer->state.subpass->view_mask;
+   const bool uses_drawid = cmd_buffer->state.pipeline->graphics.uses_drawid;
+   uint32_t last_start = 0;
 
    vk_foreach_multi_draw(draw, i, minfo, drawCount, stride,
-      if (i > 0) {
-         if (state->last_vertex_offset != draw->first) {
-            radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr, draw->first);
-            state->last_vertex_offset = draw->first;
-         }
-      } else
+      if (!i)
          radv_emit_userdata_vertex(cmd_buffer, info, draw->first);
+      else
+         radv_emit_userdata_vertex_drawid(cmd_buffer, draw->first, uses_drawid ? i : 0);
 
-      if (!state->subpass->view_mask) {
+      if (!view_mask) {
          radv_cs_emit_draw_packet(cmd_buffer, draw->count, use_opaque);
       } else {
-         u_foreach_bit(view, state->subpass->view_mask) {
+         u_foreach_bit(view, view_mask) {
             radv_emit_view_index(cmd_buffer, view);
             radv_cs_emit_draw_packet(cmd_buffer, draw->count, use_opaque);
          }
       }
+      last_start = draw->first;
    );
+   if (drawCount > 1) {
+       struct radv_cmd_state *state = &cmd_buffer->state;
+       state->last_vertex_offset = last_start;
+       if (uses_drawid)
+           state->last_drawid = drawCount - 1;
+   }
 }
 
 static void

upcliujie/mesa

src/amd/vulkan/radv_cmd_buffer.c
@@ -5297,6 +5297,21 @@ radv_emit_userdata_vertex(struct radv_cmd_buffer *cmd_buffer, const struct radv_
 }
 
 ALWAYS_INLINE static void
+radv_emit_userdata_vertex_drawid(struct radv_cmd_buffer *cmd_buffer, uint32_t vertex_offset, uint32_t drawid)
+{
+   const struct radv_cmd_state *state = &cmd_buffer->state;
+   struct radeon_cmdbuf *cs = cmd_buffer->cs;
+   if (!drawid)
+      radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr, vertex_offset);
+   else {
+      radeon_set_sh_reg_seq(cs, state->pipeline->graphics.vtx_base_sgpr, 2);
+
+      radeon_emit(cs, vertex_offset);
+      radeon_emit(cs, drawid);
+   }
+}
+
+ALWAYS_INLINE static void
 radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
                                const struct radv_draw_info *info,
                                uint32_t drawCount, const VkMultiDrawIndexedInfoEXT *minfo,
@@ -5319,6 +5334,9 @@ radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
          if (!remaining_indexes &&
              cmd_buffer->device->physical_device->rad_info.has_zero_index_buffer_bug)
             continue;
+
+         if (i > 0 && uses_drawid)
+            radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr + sizeof(uint32_t), i);
 
          const uint64_t index_va = state->index_va + draw->firstIndex * index_size;
 
@@ -5344,10 +5362,10 @@ radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
          const VkMultiDrawIndexedInfoEXT *next = (void*)(i < drawCount - 1 ? ((uint8_t*)draw + stride) : NULL);
          const bool offset_changes = next && next->vertexOffset != draw->vertexOffset;
          if (i > 0) {
-            if (state->last_vertex_offset != draw->vertexOffset) {
-               radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr, draw->vertexOffset);
-               state->last_vertex_offset = draw->vertexOffset;
-            }
+            if (state->last_vertex_offset != draw->vertexOffset)
+               radv_emit_userdata_vertex_drawid(cmd_buffer, draw->vertexOffset, uses_drawid ? i : 0);
+            else if (uses_drawid)
+               radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr + sizeof(uint32_t), i);
          } else
             radv_emit_userdata_vertex(cmd_buffer, info, draw->vertexOffset);
 
@@ -5364,6 +5382,9 @@ radv_emit_draw_packets_indexed(struct radv_cmd_buffer *cmd_buffer,
          }
       }
    }
+   if (drawCount > 1 && uses_drawid) {
+      state->last_drawid = drawCount - 1;
+   }
 }
 
 ALWAYS_INLINE static void
@@ -5371,27 +5392,32 @@ radv_emit_direct_draw_packets(struct radv_cmd_buffer *cmd_buffer, const struct r
                               uint32_t drawCount, const VkMultiDrawInfoEXT *minfo,
                               uint32_t use_opaque, uint32_t stride)
 {
-   struct radv_cmd_state *state = &cmd_buffer->state;
-   struct radeon_cmdbuf *cs = cmd_buffer->cs;
    unsigned i = 0;
+   const uint32_t view_mask = cmd_buffer->state.subpass->view_mask;
+   const bool uses_drawid = cmd_buffer->state.pipeline->graphics.uses_drawid;
+   uint32_t last_start = 0;
 
    vk_foreach_multi_draw(draw, i, minfo, drawCount, stride) {
-      if (i > 0) {
-         if (state->last_vertex_offset != draw->firstVertex) {
-            radeon_set_sh_reg(cs, state->pipeline->graphics.vtx_base_sgpr, draw->firstVertex);
-            state->last_vertex_offset = draw->firstVertex;
-         }
-      } else
+      if (!i)
          radv_emit_userdata_vertex(cmd_buffer, info, draw->firstVertex);
+      else
+         radv_emit_userdata_vertex_drawid(cmd_buffer, draw->firstVertex, uses_drawid ? i : 0);
 
-      if (!state->subpass->view_mask) {
+      if (!view_mask) {
          radv_cs_emit_draw_packet(cmd_buffer, draw->vertexCount, use_opaque);
       } else {
-         u_foreach_bit(view, state->subpass->view_mask) {
+         u_foreach_bit(view, view_mask) {
             radv_emit_view_index(cmd_buffer, view);
             radv_cs_emit_draw_packet(cmd_buffer, draw->vertexCount, use_opaque);
          }
       }
+      last_start = draw->firstVertex;
+   }
+   if (drawCount > 1) {
+       struct radv_cmd_state *state = &cmd_buffer->state;
+       state->last_vertex_offset = last_start;
+       if (uses_drawid)
+           state->last_drawid = drawCount - 1;
    }
 }
 