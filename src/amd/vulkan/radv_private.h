1b7a2963afd38fc13cc2deb66a9a176685dd3954
Timur Kristóf <timur.kristof@gmail.com>
2021-07-05 08:51:09 +0200

    radv: Add last_vgt_api_stage and use it to simplify some code.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -1765,6 +1765,9 @@ struct radv_pipeline {
 
          /* Whether the pipeline uses NGG (GFX10+). */
          bool is_ngg;
+
+         /* Last pre-PS API stage */
+         gl_shader_stage last_vgt_api_stage;
       } graphics;
    };
 
