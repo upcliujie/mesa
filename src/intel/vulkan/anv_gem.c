Grima04/mesa-turnip-kgsl

commit c0d07c838a9fcd67e4ae8cf948ced2daa3edf8c6

anv: Support i915 query (DRM_IOCTL_I915_QUERY) from Linux v4.17
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8667>

src/intel/vulkan/anv_gem.c
@@ -660,3 +660,24 @@ anv_gem_syncobj_timeline_query(struct anv_device *device,
 
    return gen_ioctl(device->fd, DRM_IOCTL_SYNCOBJ_QUERY, &args);
 }
+
+int
+anv_i915_query(int fd, uint64_t query_id, void *buffer,
+               int32_t *buffer_len)
+{
+   struct drm_i915_query_item item = {
+      .query_id = query_id,
+      .length = *buffer_len,
+      .data_ptr = (uintptr_t)buffer,
+   };
+
+   struct drm_i915_query args = {
+      .num_items = 1,
+      .flags = 0,
+      .items_ptr = (uintptr_t)&item,
+   };
+
+   int ret = gen_ioctl(fd, DRM_IOCTL_I915_QUERY, &args);
+   *buffer_len = item.length;
+   return ret;
+}

src/intel/vulkan/anv_gem_stubs.c
@@ -246,6 +246,13 @@ anv_gem_supports_syncobj_wait(int fd)
 }
 
 int
+anv_i915_query(int fd, uint64_t query_id, void *buffer,
+               int32_t *buffer_len)
+{
+   unreachable("Unused");
+}
+
+int
 anv_gem_syncobj_wait(struct anv_device *device,
                      const uint32_t *handles, uint32_t num_handles,
                      int64_t abs_timeout_ns, bool wait_all)

src/intel/vulkan/anv_private.h
@@ -1660,6 +1660,8 @@ int anv_gem_syncobj_timeline_signal(struct anv_device *device,
 int anv_gem_syncobj_timeline_query(struct anv_device *device,
                                    const uint32_t *handles, uint64_t *points,
                                    uint32_t num_items);
+int anv_i915_query(int fd, uint64_t query_id, void *buffer,
+                   int32_t *buffer_len);
 
 uint64_t anv_vma_alloc(struct anv_device *device,
                        uint64_t size, uint64_t align,

upcliujie/mesa

commit 91bc9a3d572de9a4870b10f79dbed479942427c6

anv: Handle errors properly in anv_i915_query
DRM_IOCTL_I915_QUERY is a multi-query.  The most egregious errors are
returned via the usual ioctl error mechanism but there are also
per-query errors that are indicated by item.length < 0.  We need to
handle those as well.  While we're at it, scrape errno so we can return
a proper integer error.

Fixes: c0d07c838a9f "anv: Support i915 query (DRM_IOCTL_I915_QUERY)..."

src/intel/vulkan/anv_gem.c
@@ -787,8 +787,13 @@ anv_i915_query(int fd, uint64_t query_id, void *buffer,
    };
 
    int ret = intel_ioctl(fd, DRM_IOCTL_I915_QUERY, &args);
+   if (ret != 0)
+      return -errno;
+   else if (item.length < 0)
+      return item.length;
+
    *buffer_len = item.length;
-   return ret;
+   return 0;
 }
 
 struct drm_i915_query_engine_info *