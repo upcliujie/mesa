2a22ca44fdfe6a05480d80b6e27594a2da325332
Neil Roberts <nroberts@igalia.com>
2021-07-08 18:09:32 +0200

    dri: Extend the image extension to add invalidateUnsynchronizedImage
    This will be used to implement EGL_MESA_unsynchronized_image.

--- include/GL/internal/dri_interface.h
+++ include/GL/internal/dri_interface.h
@@ -1328,7 +1328,7 @@ struct __DRIdri2ExtensionRec {
  * extensions.
  */
 #define __DRI_IMAGE "DRI_IMAGE"
-#define __DRI_IMAGE_VERSION 19
+#define __DRI_IMAGE_VERSION 20
 
 /**
  * These formats correspond to the similarly named MESA_FORMAT_*
@@ -1827,6 +1827,18 @@ struct __DRIimageExtensionRec {
                                             const unsigned int modifier_count,
                                             unsigned int use,
                                             void *loaderPrivate);
+
+   /**
+    * Marks that the image doesn’t need to be automatically synchronized to
+    * external updates and also that an external update has occured. This is
+    * useful if the driver is maintaining a shadow copy of the contents. If
+    * this is called at least once then the driver can assume that it doesn’t
+    * need to update the shadow copy unless this function is called.
+    *
+    * \since 20
+    */
+   void (*invalidateUnsynchronizedImage)(__DRIscreen *screen,
+                                         __DRIimage *image);
 };
 
 
@@ -1992,6 +2004,8 @@ typedef struct __DRIDriverVtableExtensionRec {
 #define   __DRI2_RENDERER_HAS_CONTEXT_PRIORITY_HIGH           (1 << 2)
 
 #define __DRI2_RENDERER_HAS_PROTECTED_CONTENT                 0x000e
+
+#define __DRI2_RENDERER_HAS_UNSYNCHRONIZED_IMAGE              0x000f
 
 typedef struct __DRI2rendererQueryExtensionRec __DRI2rendererQueryExtension;
 struct __DRI2rendererQueryExtensionRec {
