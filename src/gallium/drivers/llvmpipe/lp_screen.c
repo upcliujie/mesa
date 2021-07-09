1191c751f7624071c507ceb12be425aaec92058f
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-07-09 15:19:09 +0200

    llvmpipe: use nir-lowering for fpow
    While gallivm has lp_build_pow, this isn't precise enough for what we
    need for highp fpow on Vulkan. This seems to be mostly about
    lp_build_exp2 / lp_build_log2 vs lp_build_exp2_safe /
    lp_build_log2_safe, which handles more special cases. This means that
    it's *probably* a good idea even for the non-highp case, as the special
    cases aren't really about precision, but about correctness. It's
    probably just that only the highp test-cases trigger them.
    
    I left the support for the nir-opcode around in case some other gallivm
    users depend on it. We'll simply not trigger the code from LLVMpipe.
    
    This fixes a few Vulkan-CTS failures on Lavapipe.

--- src/gallium/drivers/llvmpipe/lp_screen.c
+++ src/gallium/drivers/llvmpipe/lp_screen.c
@@ -560,6 +560,7 @@ static const struct nir_shader_compiler_options gallivm_nir_options = {
    .lower_ffma16 = true,
    .lower_ffma32 = true,
    .lower_ffma64 = true,
+   .lower_fpow = true,
    .lower_fmod = true,
    .lower_hadd = true,
    .lower_add_sat = true,
