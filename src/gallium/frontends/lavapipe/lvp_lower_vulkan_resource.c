Grima04/mesa-turnip-kgsl

commit c18ff60087ef655b9a584c4586859df5e2bf7d28

lavapipe: emit correct textures_used for texture-arrays
When we lower a texture-lookup with a dynamic index, we need to mark the
entire array as used, because we don't know better.

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10487>

src/gallium/frontends/lavapipe/lvp_lower_vulkan_resource.c
@@ -100,15 +100,16 @@ static nir_ssa_def *lower_vri_intrin_lvd(struct nir_builder *b,
    return nir_vec2(b, index, nir_imm_int(b, 0));
 }
 
-static int lower_vri_instr_tex_deref(nir_tex_instr *tex,
-                                     nir_tex_src_type deref_src_type,
-                                     gl_shader_stage stage,
-                                     struct lvp_pipeline_layout *layout)
+static unsigned
+lower_vri_instr_tex_deref(nir_tex_instr *tex,
+                          nir_tex_src_type deref_src_type,
+                          gl_shader_stage stage,
+                          struct lvp_pipeline_layout *layout)
 {
    int deref_src_idx = nir_tex_instr_src_index(tex, deref_src_type);
 
    if (deref_src_idx < 0)
-      return -1;
+      return 0;
 
    nir_deref_instr *deref_instr = nir_src_as_deref(tex->src[deref_src_idx].src);
    nir_variable *var = nir_deref_instr_get_variable(deref_instr);
@@ -142,19 +143,28 @@ static int lower_vri_instr_tex_deref(nir_tex_instr *tex,
       tex->sampler_index = value;
    else
       tex->texture_index = value;
-   return value;
+
+   if (deref_instr->deref_type == nir_deref_type_array) {
+      assert(glsl_type_is_array(var->type));
+      assert(value >= 0);
+      unsigned size = glsl_get_aoa_size(var->type);
+      return u_bit_consecutive(value, size);
+   } else
+      return 1u << value;
 }
 
 static void lower_vri_instr_tex(struct nir_builder *b,
                                 nir_tex_instr *tex, void *data_cb)
 {
    struct lvp_pipeline_layout *layout = data_cb;
-   int tex_value = 0;
+   unsigned textures_used;
 
    lower_vri_instr_tex_deref(tex, nir_tex_src_sampler_deref, b->shader->info.stage, layout);
-   tex_value = lower_vri_instr_tex_deref(tex, nir_tex_src_texture_deref, b->shader->info.stage, layout);
-   if (tex_value >= 0)
-      BITSET_SET(b->shader->info.textures_used, tex_value);
+   textures_used = lower_vri_instr_tex_deref(tex, nir_tex_src_texture_deref, b->shader->info.stage, layout);
+   while (textures_used) {
+      int i = u_bit_scan(&textures_used);
+      BITSET_SET(b->shader->info.textures_used, i);
+   }
 }
 
 static nir_ssa_def *lower_vri_instr(struct nir_builder *b,

upcliujie/mesa

commit 802c5e59bbb7b1684d4745d768f8acc8b254e037

lavapipe: do not assert on more than 32 samplers
We can have more than 32 samplers, but the code below will assert in that
case. The return value is not used for samplers, so let's just return
zero early and be done with it.

Fixes: c18ff60087e ("lavapipe: emit correct textures_used for texture-arrays")

src/gallium/frontends/lavapipe/lvp_lower_vulkan_resource.c
@@ -144,6 +144,9 @@ lower_vri_instr_tex_deref(nir_tex_instr *tex,
    else
       tex->texture_index = value;
 
+   if (deref_src_type == nir_tex_src_sampler_deref)
+      return 0;
+
    if (deref_instr->deref_type == nir_deref_type_array) {
       assert(glsl_type_is_array(var->type));
       assert(value >= 0);