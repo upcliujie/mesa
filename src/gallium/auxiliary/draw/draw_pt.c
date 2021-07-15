Grima04/mesa-turnip-kgsl

commit ce0b1f23c30e1943be67d81231722e76e074f823

aux/draw: track increment_draw_id value from draw info
need this in order to accurately update drawid

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9844>

src/gallium/auxiliary/draw/draw_pt.c
@@ -541,6 +541,7 @@ draw_vbo(struct draw_context *draw,
    draw->pt.user.max_index = use_info->index_bounds_valid ? use_info->max_index : ~0;
    draw->pt.user.eltSize = use_info->index_size ? draw->pt.user.eltSizeIB : 0;
    draw->pt.user.drawid = use_info->drawid;
+   draw->pt.user.increment_draw_id = use_info->increment_draw_id;
    draw->pt.user.viewid = 0;
    draw->pt.vertices_per_patch = use_info->vertices_per_patch;
 

src/gallium/auxiliary/draw/draw_private.h
@@ -198,6 +198,7 @@ struct draw_context
          unsigned min_index;
          unsigned max_index;
          unsigned drawid;
+         bool increment_draw_id;
          unsigned viewid;
          
          /** vertex arrays */

upcliujie/mesa

commit a740c3b41e7bda9167c834ef428c1a1505432794

draw: handle resetting draw_id between instances.
If we have a multi draw then the draw_id needs to get reset
between each instance.

Fixes the rest of the VK CTS multidraw tests

Fixes: ce0b1f23c30e ("aux/draw: track increment_draw_id value from draw info")

src/gallium/auxiliary/draw/draw_pt.c
@@ -455,6 +455,7 @@ resolve_draw_info(const struct pipe_draw_info *raw_info,
  */
 static void
 draw_instances(struct draw_context *draw,
+               unsigned drawid_offset,
                const struct pipe_draw_info *info,
                const struct pipe_draw_start_count_bias *draws,
                unsigned num_draws)
@@ -473,6 +474,7 @@ draw_instances(struct draw_context *draw,
          draw->instance_id = 0xffffffff;
       }
 
+      draw->pt.user.drawid = drawid_offset;
       draw_new_instance(draw);
 
       if (info->primitive_restart) {
@@ -601,10 +603,10 @@ draw_vbo(struct draw_context *draw,
    if (use_info->view_mask) {
       u_foreach_bit(i, use_info->view_mask) {
          draw->pt.user.viewid = i;
-         draw_instances(draw, use_info, use_draws, num_draws);
+         draw_instances(draw, drawid_offset, use_info, use_draws, num_draws);
       }
    } else
-      draw_instances(draw, use_info, use_draws, num_draws);
+      draw_instances(draw, drawid_offset, use_info, use_draws, num_draws);
 
    /* If requested emit the pipeline statistics for this run */
    if (draw->collect_statistics) {