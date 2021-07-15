aerisarn/mesa-uwp

commit 790ff1ceac8a406467b38c929e0ae75200da59f0

anv: fixup physical device properties of fragment shading rate
We've only enabled the extension on Gfx11+ so no need to care about
prior values.

Also fixup values of (min|max)FragmentShadingRateAttachmentTexelSize.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 231651fd89fb00 ("anv: implement VK_KHR_fragment_shading_rate")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10607>

src/intel/vulkan/anv_device.c
@@ -2218,43 +2218,27 @@ void anv_GetPhysicalDeviceProperties2(
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FRAGMENT_SHADING_RATE_PROPERTIES_KHR: {
          VkPhysicalDeviceFragmentShadingRatePropertiesKHR *props =
             (VkPhysicalDeviceFragmentShadingRatePropertiesKHR *)ext;
-         if (pdevice->info.ver < 11) {
-            props->minFragmentShadingRateAttachmentTexelSize = (VkExtent2D) { 0, 0 };
-            props->maxFragmentShadingRateAttachmentTexelSize = (VkExtent2D) { 0, 0 };
-            props->maxFragmentShadingRateAttachmentTexelSizeAspectRatio = 0;
-            props->primitiveFragmentShadingRateWithMultipleViewports = false;
-            props->layeredShadingRateAttachments = false;
-            props->fragmentShadingRateNonTrivialCombinerOps = true;
-            props->maxFragmentSize = (VkExtent2D) { 1, 1 };
-            props->maxFragmentSizeAspectRatio = 1;
-            props->maxFragmentShadingRateCoverageSamples = 0;
-            props->maxFragmentShadingRateRasterizationSamples = 0;
-            props->fragmentShadingRateWithShaderDepthStencilWrites = false;
-            props->fragmentShadingRateWithSampleMask = false;
-            props->fragmentShadingRateWithShaderSampleMask = false;
-            props->fragmentShadingRateWithConservativeRasterization = true;
-            props->fragmentShadingRateWithFragmentShaderInterlock = false;
-            props->fragmentShadingRateWithCustomSampleLocations = false;
-            props->fragmentShadingRateStrictMultiplyCombiner = false;
-         } else {
-            props->minFragmentShadingRateAttachmentTexelSize = (VkExtent2D) { 1, 1 };
-            props->maxFragmentShadingRateAttachmentTexelSize = (VkExtent2D) { 4, 4 };
-            props->maxFragmentShadingRateAttachmentTexelSizeAspectRatio = 0;
-            props->primitiveFragmentShadingRateWithMultipleViewports = pdevice->info.ver >= 12;
-            props->layeredShadingRateAttachments = false;
-            props->fragmentShadingRateNonTrivialCombinerOps = true;
-            props->maxFragmentSize = (VkExtent2D) { 4, 4 };
-            props->maxFragmentSizeAspectRatio = 4;
-            props->maxFragmentShadingRateCoverageSamples = 4 * 4;
-            props->maxFragmentShadingRateRasterizationSamples = VK_SAMPLE_COUNT_16_BIT;
-            props->fragmentShadingRateWithShaderDepthStencilWrites = false;
-            props->fragmentShadingRateWithSampleMask = true;
-            props->fragmentShadingRateWithShaderSampleMask = false;
-            props->fragmentShadingRateWithConservativeRasterization = true;
-            props->fragmentShadingRateWithFragmentShaderInterlock = true;
-            props->fragmentShadingRateWithCustomSampleLocations = true;
-            props->fragmentShadingRateStrictMultiplyCombiner = false;
-         }
+         /* Those must be 0 if attachmentFragmentShadingRate is not
+          * supported.
+          */
+         props->minFragmentShadingRateAttachmentTexelSize = (VkExtent2D) { 0, 0 };
+         props->maxFragmentShadingRateAttachmentTexelSize = (VkExtent2D) { 0, 0 };
+         props->maxFragmentShadingRateAttachmentTexelSizeAspectRatio = 0;
+
+         props->primitiveFragmentShadingRateWithMultipleViewports = pdevice->info.ver >= 12;
+         props->layeredShadingRateAttachments = false;
+         props->fragmentShadingRateNonTrivialCombinerOps = true;
+         props->maxFragmentSize = (VkExtent2D) { 4, 4 };
+         props->maxFragmentSizeAspectRatio = 4;
+         props->maxFragmentShadingRateCoverageSamples = 4 * 4;
+         props->maxFragmentShadingRateRasterizationSamples = VK_SAMPLE_COUNT_16_BIT;
+         props->fragmentShadingRateWithShaderDepthStencilWrites = false;
+         props->fragmentShadingRateWithSampleMask = true;
+         props->fragmentShadingRateWithShaderSampleMask = false;
+         props->fragmentShadingRateWithConservativeRasterization = true;
+         props->fragmentShadingRateWithFragmentShaderInterlock = true;
+         props->fragmentShadingRateWithCustomSampleLocations = true;
+         props->fragmentShadingRateStrictMultiplyCombiner = false;
          break;
       }
 

upcliujie/mesa

commit 4427b9b2b5b38c9a8b9b003cf431dbb67d77c13f

anv: fix feature/property/sizes reported for fragment shading rate
Fixes: 790ff1ceac8 ("anv: fixup physical device properties of fragment shading rate")

src/intel/vulkan/anv_device.c
@@ -1550,7 +1550,9 @@ void anv_GetPhysicalDeviceFeatures2(
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FRAGMENT_SHADING_RATE_FEATURES_KHR: {
          VkPhysicalDeviceFragmentShadingRateFeaturesKHR *features =
             (VkPhysicalDeviceFragmentShadingRateFeaturesKHR *)ext;
+         features->attachmentFragmentShadingRate = false;
          features->pipelineFragmentShadingRate = true;
+         features->primitiveFragmentShadingRate = pdevice->info.ver >= 12;
          break;
       }
 
@@ -2349,7 +2351,7 @@ void anv_GetPhysicalDeviceProperties2(
          props->fragmentShadingRateNonTrivialCombinerOps = true;
          props->maxFragmentSize = (VkExtent2D) { 4, 4 };
          props->maxFragmentSizeAspectRatio = 4;
-         props->maxFragmentShadingRateCoverageSamples = 4 * 4;
+         props->maxFragmentShadingRateCoverageSamples = 4 * 4 * 16;
          props->maxFragmentShadingRateRasterizationSamples = VK_SAMPLE_COUNT_16_BIT;
          props->fragmentShadingRateWithShaderDepthStencilWrites = false;
          props->fragmentShadingRateWithSampleMask = true;
@@ -4952,11 +4954,14 @@ VkResult anv_GetPhysicalDeviceFragmentShadingRatesKHR(
    VkSampleCountFlags sample_counts =
       isl_device_get_sample_counts(&physical_device->isl_dev);
 
-   for (uint32_t x = 4; x >= 1; x /= 2) {
-       for (uint32_t y = 4; y >= 1; y /= 2) {
+   for (uint32_t x = 4; x > 1; x /= 2) {
+       for (uint32_t y = 4; y > 1; y /= 2) {
          append_rate(sample_counts, x, y);
       }
    }
+
+   /* For size {1, 1}, the sample count must be ~0 */
+   append_rate(~0, 1, 1);
 
 #undef append_rate
 