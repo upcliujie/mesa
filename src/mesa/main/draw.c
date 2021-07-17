Grima04/mesa-turnip-kgsl

commit 4566383ae4bf28b99576ae9d293c50decc771888

gallium: move pipe_draw_info::index_bias to pipe_draw_start_count_bias
this moves index_bias into the multidraw struct, enabling draws where the value
changes to be merged; the draw_info struct member is renamed and moved to the end
of the struct for tc use

u_vbuf still has some checks to split draws if index_bias changes, maybe
this can be removed at some point?

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10166>

src/mesa/main/draw.c
@@ -993,7 +993,7 @@ _mesa_draw_gallium_fallback(struct gl_context *ctx,
          prim.end = 1;
          prim.start = index_size && info->has_user_indices ? 0 : draws[i].start;
          prim.count = draws[i].count;
-         prim.basevertex = index_size ? info->index_bias : 0;
+         prim.basevertex = index_size ? draws[i].index_bias : 0;
          prim.draw_id = info->drawid + (info->increment_draw_id ? i : 0);
 
          if (!index_size) {
@@ -1027,7 +1027,7 @@ _mesa_draw_gallium_fallback(struct gl_context *ctx,
       prim[num_prims].end = 1;
       prim[num_prims].start = draws[i].start;
       prim[num_prims].count = draws[i].count;
-      prim[num_prims].basevertex = info->index_size ? info->index_bias : 0;
+      prim[num_prims].basevertex = info->index_size ? draws[i].index_bias : 0;
       prim[num_prims].draw_id = info->drawid + (info->increment_draw_id ? i : 0);
 
       if (!index_size) {
@@ -1068,58 +1068,20 @@ _mesa_draw_gallium_complex_fallback(struct gl_context *ctx,
                                     struct pipe_draw_info *info,
                                     const struct pipe_draw_start_count_bias *draws,
                                     const unsigned char *mode,
-                                    const int *base_vertex,
                                     unsigned num_draws)
 {
-   enum {
-      MODE = 1,
-      BASE_VERTEX = 2,
-   };
-   unsigned mask = (mode ? MODE : 0) | (base_vertex ? BASE_VERTEX : 0);
    unsigned i, first;
-
+ 
    /* Find consecutive draws where mode and base_vertex don't vary. */
-   switch (mask) {
-   case MODE:
-      for (i = 0, first = 0; i <= num_draws; i++) {
-         if (i == num_draws || mode[i] != mode[first]) {
-            info->mode = mode[first];
-            ctx->Driver.DrawGallium(ctx, info, &draws[first], i - first);
-            first = i;
-         }
+   for (i = 0, first = 0; i <= num_draws; i++) {
+      if (i == num_draws || mode[i] != mode[first]) {
+         info->mode = mode[first];
+         ctx->Driver.DrawGallium(ctx, info, &draws[first], i - first);
+         first = i;
       }
-      break;
-
-   case BASE_VERTEX:
-      for (i = 0, first = 0; i <= num_draws; i++) {
-         if (i == num_draws || base_vertex[i] != base_vertex[first]) {
-            info->index_bias = base_vertex[first];
-            ctx->Driver.DrawGallium(ctx, info, &draws[first], i - first);
-            first = i;
-         }
-      }
-      break;
-
-   case MODE | BASE_VERTEX:
-      for (i = 0, first = 0; i <= num_draws; i++) {
-         if (i == num_draws ||
-             mode[i] != mode[first] ||
-             base_vertex[i] != base_vertex[first]) {
-            info->mode = mode[first];
-            info->index_bias = base_vertex[first];
-            ctx->Driver.DrawGallium(ctx, info, &draws[first], i - first);
-            first = i;
-         }
-      }
-      break;
-
-   default:
-      assert(!"invalid parameters in DrawGalliumComplex");
-      break;
    }
 }
-
-
+ 
 /**
  * Check that element 'j' of the array has reasonable data.
  * Map VBO if needed.
@@ -1331,8 +1293,9 @@ _mesa_draw_arrays(struct gl_context *ctx, GLenum mode, GLint start,
    info.index_bounds_valid = true;
    info.increment_draw_id = false;
    info.take_index_buffer_ownership = false;
-   info._pad = 0;
+   info.index_bias_varies = false;
    /* Packed section end. */
+   info._pad2 = 0;
    info.start_instance = baseInstance;
    info.instance_count = numInstances;
    info.drawid = 0;
@@ -1660,8 +1623,9 @@ _mesa_MultiDrawArrays(GLenum mode, const GLint *first,
    info.index_bounds_valid = false;
    info.increment_draw_id = primcount > 1;
    info.take_index_buffer_ownership = false;
-   info._pad = 0;
+   info.index_bias_varies = false;
    /* Packed section end. */
+   info._pad2 = 0;
    info.start_instance = 0;
    info.instance_count = 1;
    info.drawid = 0;
@@ -1775,13 +1739,13 @@ _mesa_validated_drawrangeelements(struct gl_context *ctx, GLenum mode,
    info.index_bounds_valid = index_bounds_valid;
    info.increment_draw_id = false;
    info.take_index_buffer_ownership = false;
-   info._pad = 0;
+   info.index_bias_varies = false;
    /* Packed section end. */
+   info._pad2 = 0;
    info.start_instance = baseInstance;
    info.instance_count = numInstances;
    info.drawid = 0;
    info.view_mask = 0;
-   info.index_bias = basevertex;
    info.restart_index = ctx->Array._RestartIndex[index_size_shift];
 
    if (info.has_user_indices) {
@@ -1791,6 +1755,7 @@ _mesa_validated_drawrangeelements(struct gl_context *ctx, GLenum mode,
       info.index.gl_bo = index_bo;
       draw.start = (uintptr_t)indices >> index_size_shift;
    }
+   draw.index_bias = basevertex;
 
    info.min_index = start;
    info.max_index = end;
@@ -2151,17 +2116,6 @@ _mesa_validated_multidrawelements(struct gl_context *ctx, GLenum mode,
       }
    }
 
-   /* See if BaseVertex is constant across all draws. */
-   bool basevertex_is_constant = true;
-   if (basevertex) {
-      for (int i = 1; i < primcount; i++) {
-         if (basevertex[i] != basevertex[0]) {
-            basevertex_is_constant = false;
-            break;
-         }
-      }
-   }
-
    struct gl_buffer_object *index_bo = ctx->Array.VAO->IndexBufferObj;
    struct pipe_draw_info info;
 
@@ -2174,8 +2128,9 @@ _mesa_validated_multidrawelements(struct gl_context *ctx, GLenum mode,
    info.index_bounds_valid = false;
    info.increment_draw_id = primcount > 1;
    info.take_index_buffer_ownership = false;
-   info._pad = 0;
+   info.index_bias_varies = !!basevertex;
    /* Packed section end. */
+   info._pad2 = 0;
    info.start_instance = 0;
    info.instance_count = 1;
    info.drawid = 0;
@@ -2206,21 +2161,17 @@ _mesa_validated_multidrawelements(struct gl_context *ctx, GLenum mode,
             draw[i].start =
                ((uintptr_t)indices[i] - min_index_ptr) >> index_size_shift;
             draw[i].count = count[i];
+            draw[i].index_bias = basevertex ? basevertex[i] : 0;
          }
       } else {
          for (int i = 0; i < primcount; i++) {
             draw[i].start = (uintptr_t)indices[i] >> index_size_shift;
             draw[i].count = count[i];
+            draw[i].index_bias = basevertex ? basevertex[i] : 0;
          }
       }
 
-      if (basevertex_is_constant) {
-         info.index_bias = basevertex ? basevertex[0] : 0;
-         ctx->Driver.DrawGallium(ctx, &info, draw, primcount);
-      } else {
-         ctx->Driver.DrawGalliumComplex(ctx, &info, draw, NULL, basevertex,
-                                        primcount);
-      }
+      ctx->Driver.DrawGallium(ctx, &info, draw, primcount);
       FREE_PRIMS(draw, primcount);
    } else {
       /* draw[i].start would overflow. Draw one at a time. */
@@ -2235,10 +2186,10 @@ _mesa_validated_multidrawelements(struct gl_context *ctx, GLenum mode,
 
          /* Reset these, because the callee can change them. */
          info.index_bounds_valid = false;
-         info.index_bias = basevertex ? basevertex[i] : 0;
          info.drawid = i;
          info.index.user = indices[i];
          draw.start = 0;
+         draw.index_bias = basevertex ? basevertex[i] : 0;
          draw.count = count[i];
 
          ctx->Driver.DrawGallium(ctx, &info, &draw, 1);

src/mesa/state_tracker/st_draw.c
@@ -191,7 +191,6 @@ st_draw_gallium_complex(struct gl_context *ctx,
                         struct pipe_draw_info *info,
                         const struct pipe_draw_start_count_bias *draws,
                         const unsigned char *mode,
-                        const int *base_vertex,
                         unsigned num_draws)
 {
    struct st_context *st = st_context(ctx);
@@ -201,67 +200,21 @@ st_draw_gallium_complex(struct gl_context *ctx,
    if (!prepare_indexed_draw(st, ctx, info, draws, num_draws))
       return;
 
-   enum {
-      MODE = 1,
-      BASE_VERTEX = 2,
-   };
-   unsigned mask = (mode ? MODE : 0) | (base_vertex ? BASE_VERTEX : 0);
    unsigned i, first;
    struct cso_context *cso = st->cso_context;
 
    /* Find consecutive draws where mode and base_vertex don't vary. */
-   switch (mask) {
-   case MODE:
-      for (i = 0, first = 0; i <= num_draws; i++) {
-         if (i == num_draws || mode[i] != mode[first]) {
-            info->mode = mode[first];
-            cso_multi_draw(cso, info, &draws[first], i - first);
-            first = i;
+   for (i = 0, first = 0; i <= num_draws; i++) {
+      if (i == num_draws || mode[i] != mode[first]) {
+         info->mode = mode[first];
+         cso_multi_draw(cso, info, &draws[first], i - first);
+         first = i;
 
-            /* We can pass the reference only once. st_buffer_object keeps
-             * the reference alive for later draws.
-             */
-            info->take_index_buffer_ownership = false;
-         }
+         /* We can pass the reference only once. st_buffer_object keeps
+          * the reference alive for later draws.
+          */
+         info->take_index_buffer_ownership = false;
       }
-      break;
-
-   case BASE_VERTEX:
-      for (i = 0, first = 0; i <= num_draws; i++) {
-         if (i == num_draws || base_vertex[i] != base_vertex[first]) {
-            info->index_bias = base_vertex[first];
-            cso_multi_draw(cso, info, &draws[first], i - first);
-            first = i;
-
-            /* We can pass the reference only once. st_buffer_object keeps
-             * the reference alive for later draws.
-             */
-            info->take_index_buffer_ownership = false;
-         }
-      }
-      break;
-
-   case MODE | BASE_VERTEX:
-      for (i = 0, first = 0; i <= num_draws; i++) {
-         if (i == num_draws ||
-             mode[i] != mode[first] ||
-             base_vertex[i] != base_vertex[first]) {
-            info->mode = mode[first];
-            info->index_bias = base_vertex[first];
-            cso_multi_draw(cso, info, &draws[first], i - first);
-            first = i;
-
-            /* We can pass the reference only once. st_buffer_object keeps
-             * the reference alive for later draws.
-             */
-            info->take_index_buffer_ownership = false;
-         }
-      }
-      break;
-
-   default:
-      assert(!"invalid parameters in DrawGalliumComplex");
-      break;
    }
 }
 

src/gallium/auxiliary/cso_cache/cso_context.c
@@ -1469,6 +1469,7 @@ cso_draw_arrays(struct cso_context *cso, uint mode, uint start, uint count)
 
    draw.start = start;
    draw.count = count;
+   draw.index_bias = 0;
 
    cso_draw_vbo(cso, &info, NULL, draw);
 }
@@ -1492,6 +1493,7 @@ cso_draw_arrays_instanced(struct cso_context *cso, uint mode,
 
    draw.start = start;
    draw.count = count;
+   draw.index_bias = 0;
 
    cso_draw_vbo(cso, &info, NULL, draw);
 }

src/gallium/auxiliary/draw/draw_pt.c
@@ -153,6 +153,7 @@ draw_pt_arrays(struct draw_context *draw,
       } else
          draw_pt_split_prim(prim, &first, &incr);
       count = draw_pt_trim_count(draw_info[i].count, first, incr);
+      draw->pt.user.eltBias = draw->pt.user.eltSize ? draw_info[i].index_bias : 0;
       if (count >= first)
          frontend->run( frontend, draw_info[i].start, count );
 
@@ -245,7 +246,7 @@ void draw_pt_destroy( struct draw_context *draw )
  * Debug- print the first 'count' vertices.
  */
 static void
-draw_print_arrays(struct draw_context *draw, uint prim, int start, uint count)
+draw_print_arrays(struct draw_context *draw, uint prim, int start, uint count, int index_bias)
 {
    uint i;
 
@@ -282,9 +283,9 @@ draw_print_arrays(struct draw_context *draw, uint prim, int start, uint count)
             assert(0);
             return;
          }
-         ii += draw->pt.user.eltBias;
+         ii += index_bias;
          debug_printf("Element[%u + %u] + %i -> Vertex %u:\n", start, i,
-                      draw->pt.user.eltBias, ii);
+                      index_bias, ii);
       }
       else {
          /* non-indexed arrays */
@@ -358,22 +359,20 @@ draw_print_arrays(struct draw_context *draw, uint prim, int start, uint count)
 static inline void
 prim_restart_loop(struct draw_context *draw,
                   const struct pipe_draw_info *info,
-                  unsigned start,
-                  unsigned count,
+                  const struct pipe_draw_start_count_bias *draw_info,
                   const void *elements)
 {
    const unsigned elt_max = draw->pt.user.eltMax;
-   struct pipe_draw_start_count_bias cur;
-   cur.start = start;
+   struct pipe_draw_start_count_bias cur = *draw_info;
    cur.count = 0;
 
    /* The largest index within a loop using the i variable as the index.
     * Used for overflow detection */
    const unsigned MAX_LOOP_IDX = 0xffffffff;
 
-   for (unsigned j = 0; j < count; j++) {
+   for (unsigned j = 0; j < draw_info->count; j++) {
       unsigned restart_idx = 0;
-      unsigned i = draw_overflow_uadd(start, j, MAX_LOOP_IDX);
+      unsigned i = draw_overflow_uadd(draw_info->start, j, MAX_LOOP_IDX);
       switch (draw->pt.user.eltSize) {
       case 1:
          restart_idx = ((const uint8_t*)elements)[i];
@@ -424,7 +423,7 @@ draw_pt_arrays_restart(struct draw_context *draw,
    if (draw->pt.user.eltSize) {
       /* indexed prims (draw_elements) */
       for (unsigned i = 0; i < num_draws; i++)
-         prim_restart_loop(draw, info, draw_info[i].start, draw_info[i].count, draw->pt.user.elts);
+         prim_restart_loop(draw, info, &draw_info[i], draw->pt.user.elts);
    }
    else {
       /* Non-indexed prims (draw_arrays).
@@ -536,7 +535,6 @@ draw_vbo(struct draw_context *draw,
    if (info->index_size)
       assert(draw->pt.user.elts);
 
-   draw->pt.user.eltBias = use_info->index_size ? use_info->index_bias : 0;
    draw->pt.user.min_index = use_info->index_bounds_valid ? use_info->min_index : 0;
    draw->pt.user.max_index = use_info->index_bounds_valid ? use_info->max_index : ~0;
    draw->pt.user.eltSize = use_info->index_size ? draw->pt.user.eltSizeIB : 0;
@@ -578,7 +576,7 @@ draw_vbo(struct draw_context *draw,
 
    if (0) {
       for (unsigned i = 0; i < num_draws; i++)
-         draw_print_arrays(draw, use_info->mode, use_draws[i].start, MIN2(use_draws[i].count, 20));
+         draw_print_arrays(draw, use_info->mode, use_draws[i].start, MIN2(use_draws[i].count, 20), use_draws[i].index_bias);
    }
 
    index_limit = util_draw_max_index(draw->pt.vertex_buffer,

src/gallium/auxiliary/driver_ddebug/dd_draw.c
@@ -358,7 +358,7 @@ dd_dump_draw_vbo(struct dd_draw_state *dstate, struct pipe_draw_info *info,
    int sh, i;
 
    DUMP(draw_info, info);
-   DUMP(draw_start_count, draw);
+   DUMP(draw_start_count_bias, draw);
    if (indirect) {
       if (indirect->buffer)
          DUMP_M(resource, indirect, buffer);

src/gallium/auxiliary/driver_trace/tr_dump_state.c
@@ -800,7 +800,6 @@ void trace_dump_draw_info(const struct pipe_draw_info *state)
 
    trace_dump_member(uint, state, vertices_per_patch);
 
-   trace_dump_member(int,  state, index_bias);
    trace_dump_member(uint, state, min_index);
    trace_dump_member(uint, state, max_index);
 
@@ -819,6 +818,7 @@ void trace_dump_draw_start_count(const struct pipe_draw_start_count_bias *state)
    trace_dump_struct_begin("pipe_draw_start_count_bias");
    trace_dump_member(uint, state, start);
    trace_dump_member(uint, state, count);
+   trace_dump_member(int,  state, index_bias);
    trace_dump_struct_end();
 }
 

src/gallium/auxiliary/indices/u_primconvert.c
@@ -141,7 +141,6 @@ util_primconvert_draw_vbo(struct primconvert_context *pc,
    new_info.index_bounds_valid = info->index_bounds_valid;
    new_info.min_index = info->min_index;
    new_info.max_index = info->max_index;
-   new_info.index_bias = info->index_size ? info->index_bias : 0;
    new_info.start_instance = info->start_instance;
    new_info.instance_count = info->instance_count;
    new_info.primitive_restart = info->primitive_restart;
@@ -181,6 +180,7 @@ util_primconvert_draw_vbo(struct primconvert_context *pc,
    u_upload_alloc(pc->pipe->stream_uploader, 0, new_info.index_size * new_draw.count, 4,
                   &ib_offset, &new_info.index.resource, &dst);
    new_draw.start = ib_offset / new_info.index_size;
+   new_draw.index_bias = info->index_size ? draw->index_bias : 0;
 
    if (info->index_size) {
       trans_func(src, draw->start, draw->count, new_draw.count, info->restart_index, dst);

src/gallium/auxiliary/util/u_draw.c
@@ -181,7 +181,7 @@ util_draw_indirect(struct pipe_context *pipe,
       draw.count = params[0];
       info.instance_count = params[1];
       draw.start = params[2];
-      info.index_bias = info_in->index_size ? params[3] : 0;
+      draw.index_bias = info_in->index_size ? params[3] : 0;
       info.start_instance = info_in->index_size ? params[4] : params[3];
       info.drawid = i;
 

upcliujie/mesa

commit c12815717a34c813ec02d1dd958244d3bbb5b04b

mesa: fix incorrect comment in draw_gallium_multimode
Fixes: 4566383ae4b ("gallium: move pipe_draw_info::index_bias to pipe_draw_start_count_bias")

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>

src/mesa/main/draw.c
@@ -1073,7 +1073,7 @@ _mesa_draw_gallium_multimode_fallback(struct gl_context *ctx,
 {
    unsigned i, first;
  
-   /* Find consecutive draws where mode and base_vertex don't vary. */
+   /* Find consecutive draws where mode doesn't vary. */
    for (i = 0, first = 0; i <= num_draws; i++) {
       if (i == num_draws || mode[i] != mode[first]) {
          info->mode = mode[first];

src/mesa/state_tracker/st_draw.c
@@ -204,7 +204,7 @@ st_draw_gallium_multimode(struct gl_context *ctx,
    unsigned i, first;
    struct cso_context *cso = st->cso_context;
 
-   /* Find consecutive draws where mode and base_vertex don't vary. */
+   /* Find consecutive draws where mode doesn't vary. */
    for (i = 0, first = 0; i <= num_draws; i++) {
       if (i == num_draws || mode[i] != mode[first]) {
          unsigned current_num_draws = i - first;