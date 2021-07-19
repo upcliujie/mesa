09dbd608503d1174f69de776f6e51ee7b460b194
Erik Faye-Lund <erik.faye-lund@collabora.com>
2021-07-19 15:32:19 +0200

    vulkan/wsi: add transition to/from transfer-src state
    Without these barriers, we'll try to blit from the image when it's in
    the incorrect layout, which is illegal.

--- src/vulkan/wsi/wsi_common.h
+++ src/vulkan/wsi/wsi_common.h
@@ -169,6 +169,7 @@ struct wsi_device {
    WSI_CB(BindBufferMemory);
    WSI_CB(BindImageMemory);
    WSI_CB(BeginCommandBuffer);
+   WSI_CB(CmdPipelineBarrier);
    WSI_CB(CmdCopyImageToBuffer);
    WSI_CB(CreateBuffer);
    WSI_CB(CreateCommandPool);
