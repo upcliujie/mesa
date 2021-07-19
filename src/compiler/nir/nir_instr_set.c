f25200da821146b57081a6e4a4b69d0b3ce396bf
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-07-19 19:33:12 +0300

    nir: consider bit size of dest of phi instructions
    We can't replace a phi with another one if the destination size if
    different between the 2 instructions.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable

--- src/compiler/nir/nir_instr_set.c
+++ src/compiler/nir/nir_instr_set.c
@@ -703,6 +703,9 @@ nir_instrs_equal(const nir_instr *instr1, const nir_instr *instr2)
       if (phi1->instr.block != phi2->instr.block)
          return false;
 
+      if (phi1->dest.ssa.bit_size != phi2->dest.ssa.bit_size)
+         return false;
+
       nir_foreach_phi_src(src1, phi1) {
          nir_foreach_phi_src(src2, phi2) {
             if (src1->pred == src2->pred) {
