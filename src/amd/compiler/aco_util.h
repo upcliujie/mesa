70ac280884646ebdf2437f93634ebf760227c123
Daniel Schürmann <daniel@schuermann.dev>
2021-07-19 09:11:36 +0200

    aco: remove unused declaration
    Fixes: 59fdaa1985f9269c40f0ca4704fbeb3fb05c4c2f ('aco: reorder and cleanup #includes')
    Closes: #5076

--- src/amd/compiler/aco_util.h
+++ src/amd/compiler/aco_util.h
@@ -51,7 +51,6 @@ public:
    using reverse_iterator = std::reverse_iterator<iterator>;
    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
    using size_type = uint16_t;
-   using difference_type = ptrdiff_t;
 
    /*! \brief                  Compiler generated default constructor
     */
