2a076971440bd7b09102f89fe13164805c5d5338
Jason Ekstrand <jason@jlekstrand.net>
2021-07-20 17:14:56 -0500

    nir/lower_subgroups: Handle down-casts in uint_to_ballot_type
    This is required for Zink where the API ballot type is a uint64_t and
    the "hardware" ballot type is uvec4.
    
    Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>

--- src/compiler/nir/nir_lower_subgroups.c
+++ src/compiler/nir/nir_lower_subgroups.c
@@ -81,15 +81,19 @@ uint_to_ballot_type(nir_builder *b, nir_ssa_def *value,
    assert(util_is_power_of_two_nonzero(num_components));
    assert(util_is_power_of_two_nonzero(value->num_components));
 
-   /* The ballot type must always have enough bits */
    unsigned total_bits = bit_size * num_components;
-   assert(total_bits >= value->bit_size * value->num_components);
 
    /* If the source doesn't have enough bits, zero-pad */
    if (total_bits > value->bit_size * value->num_components)
       value = nir_pad_vector_imm_int(b, value, 0, total_bits / value->bit_size);
 
-   return nir_bitcast_vector(b, value, bit_size);
+   value = nir_bitcast_vector(b, value, bit_size);
+
+   /* If the source has too many components, truncate */
+   if (value->num_components > num_components)
+      value = nir_channels(b, value, BITFIELD_MASK(num_components));
+
+   return value;
 }
 
 static nir_ssa_def *
