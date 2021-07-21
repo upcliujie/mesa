5f71a0dc78329b1fdb431e327e50a68c0ce1a08c
Jason Ekstrand <jason@jlekstrand.net>
2021-07-21 18:11:39 -0500

    anv: Delete anv_image::format

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -3949,7 +3949,6 @@ struct anv_image {
     * of the actual surface formats.
     */
    VkFormat vk_format;
-   const struct anv_format *format;
 
    VkImageAspectFlags aspects;
    VkExtent3D extent;
