0dad9e8f1849733ebed6acc245e870c9f0e7fd47
Alyssa Rosenzweig <alyssa@collabora.com>
2021-07-21 20:29:54 -0400

    pan/va: Optimize add with imm to ADD_IMM
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/panfrost/bifrost/bifrost_compile.c
+++ src/panfrost/bifrost/bifrost_compile.c
@@ -4122,6 +4122,10 @@ bi_compile_variant_nir(nir_shader *nir,
                 bi_lower_opt_instruction(I);
         }
 
+        if (ctx->arch >= 9) {
+                va_optimizer(ctx);
+        }
+
         bi_foreach_block(ctx, block) {
                 bi_lower_branch(ctx, block);
         }
