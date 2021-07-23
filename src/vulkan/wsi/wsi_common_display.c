aa8f9bf0741ec75fb166574a3d475c97e68c7756
Jason Ekstrand <jason@jlekstrand.net>
2021-07-22 23:49:09 -0500

    vulkan/wsi/display: Split image creation
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>

--- src/vulkan/wsi/wsi_common_display.c
+++ src/vulkan/wsi/wsi_common_display.c
@@ -1054,9 +1054,8 @@ wsi_display_image_init(VkDevice device_h,
    if (drm_format == 0)
       return VK_ERROR_DEVICE_LOST;
 
-   VkResult result = wsi_create_native_image(&chain->base, create_info,
-                                             0, NULL, NULL, NULL,
-                                             &image->base);
+   VkResult result = wsi_create_image(&chain->base, &chain->base.image_info,
+                                      &image->base);
    if (result != VK_SUCCESS)
       return result;
 
@@ -1130,6 +1129,7 @@ wsi_display_swapchain_destroy(struct wsi_swapchain *drv_chain,
 
    for (uint32_t i = 0; i < chain->base.image_count; i++)
       wsi_display_image_finish(drv_chain, allocator, &chain->images[i]);
+   wsi_destroy_image_info(&chain->base, &chain->base.image_info);
 
    wsi_swapchain_finish(&chain->base);
    vk_free(allocator, chain);
@@ -1924,6 +1924,15 @@ wsi_display_surface_create_swapchain(
 
    chain->surface = (VkIcdSurfaceDisplay *) icd_surface;
 
+   result = wsi_configure_native_image(&chain->base, create_info,
+                                       0, NULL, NULL,
+                                       NULL /* alloc_shm */,
+                                       &chain->base.image_info);
+   if (result != VK_SUCCESS) {
+      vk_free(allocator, chain);
+      goto fail_init_images;
+   }
+
    for (uint32_t image = 0; image < chain->base.image_count; image++) {
       result = wsi_display_image_init(device, &chain->base,
                                       create_info, allocator,
@@ -1934,6 +1943,7 @@ wsi_display_surface_create_swapchain(
             wsi_display_image_finish(&chain->base, allocator,
                                      &chain->images[image]);
          }
+         wsi_destroy_image_info(&chain->base, &chain->base.image_info);
          vk_free(allocator, chain);
          goto fail_init_images;
       }
