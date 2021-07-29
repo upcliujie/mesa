5badbc7a0628d83b60c52c8acf04a9f424dda69e
Rhys Perry <pendingchaos02@gmail.com>
2021-07-29 17:29:43 +0100

    radv: increase inline push constant limit if we can inline all constants
    fossil-db (Sienna Cichlid):
    Totals from 665 (0.49% of 134627) affected shaders:
    CodeSize: 4519620 -> 4491724 (-0.62%); split: -0.62%, +0.01%
    Instrs: 842745 -> 837313 (-0.64%); split: -0.66%, +0.01%
    Latency: 7289925 -> 7279661 (-0.14%); split: -0.30%, +0.16%
    InvThroughput: 1240770 -> 1240639 (-0.01%); split: -0.01%, +0.00%
    VClause: 15799 -> 15772 (-0.17%)
    SClause: 33773 -> 32604 (-3.46%); split: -3.66%, +0.20%
    Copies: 67695 -> 64992 (-3.99%); split: -4.49%, +0.50%
    PreSGPRs: 38597 -> 38640 (+0.11%); split: -0.14%, +0.25%
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/common/ac_shader_args.h
+++ src/amd/common/ac_shader_args.h
@@ -27,7 +27,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define AC_MAX_INLINE_PUSH_CONSTS 8
+#define AC_MAX_MIXED_INLINE_PUSH_CONSTS 8
+#define AC_MAX_INLINE_PUSH_CONSTS       32
 
 enum ac_arg_regfile
 {
