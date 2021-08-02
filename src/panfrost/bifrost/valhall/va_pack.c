dbc20ff90a7b935c5ee1a4e762ba6ea282a0b5a0
Alyssa Rosenzweig <alyssa@collabora.com>
2021-08-02 11:48:02 -0400

    [HACK] Stub out packing for varyings and texturing
    Want to see how far shader-db gets.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/panfrost/bifrost/valhall/va_pack.c
+++ src/panfrost/bifrost/valhall/va_pack.c
@@ -428,6 +428,44 @@ va_pack_instr(const bi_instr *I, unsigned action)
       break;
    }
 
+   case BI_OPCODE_LD_VAR_IMM_F16:
+   case BI_OPCODE_LD_VAR_IMM_F32:
+   {
+      hex |= va_pack_alu(I);
+
+      /* Staging register - destination */
+      hex |= (uint64_t) va_pack_reg(I->dest[0]) << 40;
+      hex |= (0x80ull << 40);
+
+      hex |= va_pack_varying_format(I) << 24;
+      hex |= ((uint64_t) I->vecsize << 28);
+
+      hex |= ((uint64_t) I->index) << 16;
+      hex |= ((uint64_t) I->update) << 36;
+      hex |= ((uint64_t) I->sample) << 38;
+      break;
+   }
+
+   case BI_OPCODE_LD_ATTR_IMM:
+   {
+      hex |= va_pack_typed_load(I);
+
+      hex |= ((uint64_t) I->table) << 16;
+      hex |= ((uint64_t) I->attribute_index) << 20;
+      break;
+   }
+
+   case BI_OPCODE_LD_VAR_SPECIAL:
+   case BI_OPCODE_LEA_ATTR_TEX:
+   case BI_OPCODE_ST_CVT:
+   case BI_OPCODE_VAR_TEX_F32:
+   case BI_OPCODE_VAR_TEX_F16:
+   case BI_OPCODE_ZS_EMIT:
+   case BI_OPCODE_CUBEFACE:
+      /* TODO: Pack thse ops. For now, nop them out */
+      hex |= (0xc0ull << 40);
+      break;
+
    default:
       if (!info.exact && I->op != BI_OPCODE_NOP) {
          bi_print_instr(I, stdout);
