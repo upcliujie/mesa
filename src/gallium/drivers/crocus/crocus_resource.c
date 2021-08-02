aerisarn/mesa-uwp

commit 5bf6ec31ccf6c24a407df9443a534f8036d26861

crocus/gen4: restrict memcpy mapping to gen5
This is due to gen4 + 4.5 having some rather strange swizzling
that we can't actually detect properly in userspace

Fixes: f3630548f1da ("crocus: initial gallium driver for Intel gfx 4-7")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12058>

src/gallium/drivers/crocus/crocus_resource.c
@@ -1577,6 +1577,7 @@ crocus_transfer_map(struct pipe_context *ctx,
    struct crocus_context *ice = (struct crocus_context *)ctx;
    struct crocus_resource *res = (struct crocus_resource *)resource;
    struct isl_surf *surf = &res->surf;
+   struct crocus_screen *screen = (struct crocus_screen *)ctx->screen;
 
    if (usage & PIPE_MAP_DISCARD_WHOLE_RESOURCE) {
       /* Replace the backing storage with a fresh buffer for non-async maps */
@@ -1690,7 +1691,7 @@ crocus_transfer_map(struct pipe_context *ctx,
       if (surf->tiling == ISL_TILING_W) {
          /* TODO: Teach crocus_map_tiled_memcpy about W-tiling... */
          crocus_map_s8(map);
-      } else if (surf->tiling != ISL_TILING_LINEAR) {
+      } else if (surf->tiling != ISL_TILING_LINEAR && screen->devinfo.ver > 4) {
          crocus_map_tiled_memcpy(map);
       } else {
          crocus_map_direct(map);

upcliujie/mesa

commit 07ae6179a765d27719a933745d945e115664c37d

crocus/gen45: fix mapping compressed textures
I don't think iris ever hits this path, but probably has the same bug if
it did.

Fixes texsubimage on gfx4 + gfx4.5

Fixes: 5bf6ec31ccf6 ("crocus/gen4: restrict memcpy mapping to gen5")

src/gallium/drivers/crocus/crocus_resource.c
@@ -1541,13 +1541,19 @@ crocus_map_direct(struct crocus_transfer *map)
          isl_format_get_layout(surf->format);
       const unsigned cpp = fmtl->bpb / 8;
       unsigned x0_el, y0_el;
+      uint32_t x = box->x;
+      uint32_t y = box->y;
 
+      x /= util_format_get_blockwidth(res->base.b.format);
+      y /= util_format_get_blockheight(res->base.b.format);
       get_image_offset_el(surf, xfer->level, box->z, &x0_el, &y0_el);
 
+      x += x0_el;
+      y += y0_el;
       xfer->stride = isl_surf_get_row_pitch_B(surf);
       xfer->layer_stride = isl_surf_get_array_pitch(surf);
 
-      map->ptr = ptr + (y0_el + box->y) * xfer->stride + (x0_el + box->x) * cpp;
+      map->ptr = ptr + y * xfer->stride + x * cpp;
    }
 }
 