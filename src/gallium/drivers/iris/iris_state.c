ed57d25c51697fa7dc0c8dc0f9be0a1f8bd811e9
Kenneth Graunke <kenneth@whitecape.org>
2021-08-02 14:50:19 -0700

    iris: Extend the cache tracker to handle L3 flushes and invalidates
    Most clients are L3-coherent these days.  However, there are some
    notable exceptions, such as push constants, stream output, and command
    streamer memory reads and writes.
    
    With the advent of the tile cache, flushing the render or depth caches
    alone are no longer sufficient for memory to become globally-observable.
    For those, we need to flush the tile cache as well.  However, we'd like
    to avoid that for L3-coherent clients, as it shouldn't be necessary,
    and is expensive.

--- src/gallium/drivers/iris/iris_state.c
+++ src/gallium/drivers/iris/iris_state.c
@@ -7581,8 +7581,24 @@ batch_mark_sync_for_pipe_control(struct iris_batch *batch, uint32_t flags)
       if ((flags & PIPE_CONTROL_DEPTH_CACHE_FLUSH))
          iris_batch_mark_flush_sync(batch, IRIS_DOMAIN_DEPTH_WRITE);
 
-      if ((flags & PIPE_CONTROL_DATA_CACHE_FLUSH))
+      if ((flags & PIPE_CONTROL_TILE_CACHE_FLUSH)) {
+         /* A tile cache flush makes any C/Z data in L3 visible to memory. */
+         const unsigned c = IRIS_DOMAIN_RENDER_WRITE;
+         const unsigned z = IRIS_DOMAIN_DEPTH_WRITE;
+         batch->coherent_seqnos[c][c] = batch->l3_coherent_seqnos[c];
+         batch->coherent_seqnos[z][z] = batch->l3_coherent_seqnos[z];
+      }
+
+      if (flags & (PIPE_CONTROL_FLUSH_HDC | PIPE_CONTROL_DATA_CACHE_FLUSH)) {
+         /* HDC and DC flushes both flush the data cache out to L3 */
          iris_batch_mark_flush_sync(batch, IRIS_DOMAIN_DATA_WRITE);
+      }
+
+      if ((flags & PIPE_CONTROL_DATA_CACHE_FLUSH)) {
+         /* A DC flush also flushes L3 data cache lines out to memory. */
+         const unsigned i = IRIS_DOMAIN_DATA_WRITE;
+         batch->coherent_seqnos[i][i] = batch->l3_coherent_seqnos[i];
+      }
 
       if ((flags & PIPE_CONTROL_FLUSH_ENABLE))
          iris_batch_mark_flush_sync(batch, IRIS_DOMAIN_OTHER_WRITE);
@@ -7594,6 +7610,15 @@ batch_mark_sync_for_pipe_control(struct iris_batch *batch, uint32_t flags)
          iris_batch_mark_flush_sync(batch, IRIS_DOMAIN_SAMPLER_READ);
          iris_batch_mark_flush_sync(batch, IRIS_DOMAIN_OTHER_READ);
       }
+   }
+
+   if ((flags & PIPE_CONTROL_VF_CACHE_INVALIDATE)) {
+      /* This is actually due to the "L3 Read Only Cache Invalidate"
+       * bit, but we always set that when doing a VF cache invalidate
+       * for now.
+       */
+      const unsigned i = IRIS_DOMAIN_VF_READ;
+      batch->l3_coherent_seqnos[i] = batch->coherent_seqnos[i][i];
    }
 
    if ((flags & PIPE_CONTROL_RENDER_TARGET_FLUSH))
