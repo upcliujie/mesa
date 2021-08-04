aerisarn/mesa-uwp

commit 73f6bff07f1f3a5aa31e906cd69eeef58d95104c

zink: fix mem info query to be more permissive
airlied was right, this was way too conservative

Fixes: ff4ba3d4a77 ("zink: support PIPE_CAP_QUERY_MEMORY_INFO")

Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11803>

src/gallium/drivers/zink/zink_screen.c
@@ -1625,11 +1625,11 @@ zink_query_memory_info(struct pipe_screen *pscreen, struct pipe_memory_info *inf
       screen->vk.GetPhysicalDeviceMemoryProperties2(screen->pdev, &mem);
 
       for (unsigned i = 0; i < mem.memoryProperties.memoryHeapCount; i++) {
-         if (mem.memoryProperties.memoryHeaps[i].flags == VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) {
+         if (mem.memoryProperties.memoryHeaps[i].flags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) {
             /* VRAM */
             info->total_device_memory = mem.memoryProperties.memoryHeaps[i].size / 1024;
             info->avail_device_memory = (budget.heapBudget[i] - budget.heapUsage[i]) / 1024;
-         } else if (mem.memoryProperties.memoryHeaps[i].flags == (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) {
+         } else if (mem.memoryProperties.memoryHeaps[i].flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
             /* GART */
             info->total_staging_memory = mem.memoryProperties.memoryHeaps[i].size / 1024;
             info->avail_staging_memory = (budget.heapBudget[i] - budget.heapUsage[i]) / 1024;
@@ -1638,12 +1638,12 @@ zink_query_memory_info(struct pipe_screen *pscreen, struct pipe_memory_info *inf
       /* evictions not yet supported in vulkan */
    } else {
       for (unsigned i = 0; i < screen->info.mem_props.memoryHeapCount; i++) {
-         if (screen->info.mem_props.memoryHeaps[i].flags == VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) {
+         if (screen->info.mem_props.memoryHeaps[i].flags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) {
             /* VRAM */
             info->total_device_memory = screen->info.mem_props.memoryHeaps[i].size / 1024;
             /* free real estate! */
             info->avail_device_memory = info->total_device_memory;
-         } else if (screen->info.mem_props.memoryHeaps[i].flags == (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) {
+         } else if (screen->info.mem_props.memoryHeaps[i].flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
             /* GART */
             info->total_staging_memory = screen->info.mem_props.memoryHeaps[i].size / 1024;
             /* free real estate! */

upcliujie/mesa

commit 9c0db82f4be4ebb76d9b16420b88439d841c67a1

zink: simplify else clause for mem info gathering
it doesn't matter if this is host-visible, it just has to not be device-local

Fixes: 73f6bff07f1 ("zink: fix mem info query to be more permissive")

src/gallium/drivers/zink/zink_screen.c
@@ -1652,7 +1652,7 @@ zink_query_memory_info(struct pipe_screen *pscreen, struct pipe_memory_info *inf
             /* VRAM */
             info->total_device_memory += mem.memoryProperties.memoryHeaps[i].size / 1024;
             info->avail_device_memory += (budget.heapBudget[i] - budget.heapUsage[i]) / 1024;
-         } else if (mem.memoryProperties.memoryHeaps[i].flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+         } else {
             /* GART */
             info->total_staging_memory += mem.memoryProperties.memoryHeaps[i].size / 1024;
             info->avail_staging_memory += (budget.heapBudget[i] - budget.heapUsage[i]) / 1024;
@@ -1666,7 +1666,7 @@ zink_query_memory_info(struct pipe_screen *pscreen, struct pipe_memory_info *inf
             info->total_device_memory += screen->info.mem_props.memoryHeaps[i].size / 1024;
             /* free real estate! */
             info->avail_device_memory += info->total_device_memory;
-         } else if (screen->info.mem_props.memoryHeaps[i].flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+         } else {
             /* GART */
             info->total_staging_memory += screen->info.mem_props.memoryHeaps[i].size / 1024;
             /* free real estate! */