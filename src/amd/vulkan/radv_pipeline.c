3addf1257ee91ea9aac7565a65d6817fa3310bfb
Rhys Perry <pendingchaos02@gmail.com>
2021-08-04 12:59:26 +0100

    radv,aco: lower vulkan_resource_index in NIR
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -3026,7 +3026,8 @@ radv_fill_shader_info(struct radv_pipeline *pipeline,
 static void
 radv_declare_pipeline_args(struct radv_device *device, struct radv_shader_args *args,
                            nir_shader **nir, struct radv_shader_info *infos,
-                           const struct radv_pipeline_key *pipeline_key)
+                           const struct radv_pipeline_key *pipeline_key,
+                           struct radv_pipeline_layout *layout)
 {
    unsigned active_stages = 0;
 
@@ -3042,6 +3043,7 @@ radv_declare_pipeline_args(struct radv_device *device, struct radv_shader_args *
       args[i].explicit_scratch_args = !radv_use_llvm_for_stage(device, i);
       args[i].remap_spi_ps_input = !radv_use_llvm_for_stage(device, i);
       args[i].key = pipeline_key;
+      args[i].layout = layout;
    }
 
    if (device->physical_device->rad_info.chip_class >= GFX9 && nir[MESA_SHADER_TESS_CTRL]) {
@@ -3551,7 +3553,7 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
    radv_determine_ngg_settings(pipeline, pipeline_key, infos, nir);
 
    struct radv_shader_args args[MESA_SHADER_STAGES] = {{{{{0}}}}};
-   radv_declare_pipeline_args(device, args, nir, infos, pipeline_key);
+   radv_declare_pipeline_args(device, args, nir, infos, pipeline_key, pipeline->layout);
 
    for (int i = 0; i < MESA_SHADER_STAGES; ++i) {
       if (nir[i]) {
@@ -3571,8 +3573,6 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
          }
          NIR_PASS_V(nir[i], nir_lower_memory_model);
 
-         bool lower_to_scalar = false;
-
          nir_load_store_vectorize_options vectorize_opts = {
             .modes = nir_var_mem_ssbo | nir_var_mem_ubo | nir_var_mem_push_const |
                      nir_var_mem_shared | nir_var_mem_global,
@@ -3587,17 +3587,16 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
 
          if (nir_opt_load_store_vectorize(nir[i], &vectorize_opts)) {
             NIR_PASS_V(nir[i], nir_copy_prop);
-            lower_to_scalar = true;
 
             /* Gather info again, to update whether 8/16-bit are used. */
             nir_shader_gather_info(nir[i], nir_shader_get_entrypoint(nir[i]));
          }
 
-         lower_to_scalar |=
-            nir_opt_shrink_vectors(nir[i], !device->instance->disable_shrink_image_store);
+         NIR_PASS_V(nir[i], radv_nir_apply_pipeline_layout, device, &args[i]);
 
-         if (lower_to_scalar)
-            nir_lower_alu_to_scalar(nir[i], NULL, NULL);
+         nir_opt_shrink_vectors(nir[i], !device->instance->disable_shrink_image_store);
+
+         nir_lower_alu_to_scalar(nir[i], NULL, NULL);
 
          /* lower ALU operations */
          nir_lower_int64(nir[i]);
