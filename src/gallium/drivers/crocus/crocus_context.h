b65fc3154b6f1d8a046069814136890848ce3f8a
Jordan Justen <jordan.l.justen@intel.com>
2021-08-04 01:53:23 -0700

    crocus/blorp: Support compute for slow clears with INTEL_DEBUG=blocs
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/gallium/drivers/crocus/crocus_context.h
+++ src/gallium/drivers/crocus/crocus_context.h
@@ -615,6 +615,9 @@ struct crocus_context {
       /** Bitfield of whether color blending is enabled for RT[i] */
       uint8_t blend_enables;
 
+      /** Bitfield of whether color writes are enabled for RT[i] */
+      uint8_t color_write_enables;
+
       /** Are depth writes enabled?  (Depth buffer may or may not exist.) */
       bool depth_writes_enabled;
 
