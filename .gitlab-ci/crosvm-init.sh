c0f47ec3fba0c917547af57a1f5b90c3415669ce
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-08-05 09:14:43 +0200

    stats

--- .gitlab-ci/crosvm-init.sh
+++ .gitlab-ci/crosvm-init.sh
@@ -14,6 +14,17 @@ mount -t tmpfs tmpfs /tmp
 # / is ro
 export PIGLIT_REPLAY_EXTRA_ARGS="$PIGLIT_REPLAY_EXTRA_ARGS --db-path /tmp/replayer-db"
 
+sleep 4
+bash -c 'while :; do
+  echo "------------- Guest -------------"
+  top -n1 | grep Cpu
+  ps aux | sort -nrk 3,3 | head -n 4 | tr -s " " | cut -d " " -f 3,11-
+
+  sleep 10
+done' &
+
+while [ 1 ] ; do cat /proc/loadavg ; sleep 60; done &
+
 if sh $CROSVM_TEST_SCRIPT; then
     touch /results/success
 fi
