8a0277fda19db72d93bfd39fae760c016ccf9276
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-08-05 09:14:43 +0200

    stats

--- .gitlab-ci/crosvm-runner.sh
+++ .gitlab-ci/crosvm-runner.sh
@@ -30,6 +30,35 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 # Crosvm wants this
 syslogd > /dev/null
 
+bash -c 'while :; do
+  # Get the first line with aggregate of all CPUs 
+  cpu_now=($(head -n1 /proc/stat)) 
+  # Get all columns but skip the first (which is the "cpu" string) 
+  cpu_sum="${cpu_now[@]:1}" 
+  # Replace the column seperator (space) with + 
+  cpu_sum=$((${cpu_sum// /+})) 
+  # Get the delta between two reads 
+  cpu_delta=$((cpu_sum - cpu_last_sum)) 
+  # Get the idle time Delta 
+  cpu_idle=$((cpu_now[4]- cpu_last[4])) 
+  # Calc time spent working 
+  cpu_used=$((cpu_delta - cpu_idle)) 
+  # Calc percentage 
+  cpu_usage=$((100 * cpu_used / cpu_delta)) 
+  
+  # Keep this as last for our next read 
+  cpu_last=("${cpu_now[@]}") 
+  cpu_last_sum=$cpu_sum 
+  
+  echo "CPU usage at $cpu_usage%" 
+  ps aux | sort -nrk 3,3 | head -n 10 | tr -s " " | cut -d " " -f 3,11
+  
+  # Wait a second before the next read 
+  sleep 10
+done' &
+
+while [ 1 ] ; do cat /proc/loadavg ; sleep 60; done &
+
 crosvm run \
   --gpu "$CROSVM_GPU_ARGS" \
   -m 4096 \
