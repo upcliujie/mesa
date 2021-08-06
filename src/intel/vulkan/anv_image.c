aerisarn/mesa-uwp

commit bc3c71b87ae0b8e053f45c26c1179560e462db04

anv: don't try to access Android swapchains
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Cc: mesa-stable
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5180
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12217>

src/intel/vulkan/anv_image.c
@@ -1477,11 +1477,17 @@ anv_CreateImage(VkDevice device,
       return anv_image_from_gralloc(device, pCreateInfo, gralloc_info,
                                     pAllocator, pImage);
 
+#ifndef VK_USE_PLATFORM_ANDROID_KHR
+   /* Ignore swapchain creation info on Android. Since we don't have an
+    * implementation in Mesa, we're guaranteed to access an Android object
+    * incorrectly.
+    */
    const VkImageSwapchainCreateInfoKHR *swapchain_info =
       vk_find_struct_const(pCreateInfo->pNext, IMAGE_SWAPCHAIN_CREATE_INFO_KHR);
    if (swapchain_info && swapchain_info->swapchain != VK_NULL_HANDLE)
       return anv_image_from_swapchain(device, pCreateInfo, swapchain_info,
                                       pAllocator, pImage);
+#endif
 
    return anv_image_create(device,
       &(struct anv_image_create_info) {

upcliujie/mesa

commit 1a8caed6700992dc0136cd63fb433d5cc35ca2b6

anv: refuse to create images out of swapchains on Android
This is not something we can support in Mesa because we don't have
access to the Swapchain code from the Android system.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: bc3c71b87ae0b8 ("anv: don't try to access Android swapchains")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5180

src/intel/vulkan/anv_image.c
@@ -1477,17 +1477,20 @@ anv_CreateImage(VkDevice device,
       return anv_image_from_gralloc(device, pCreateInfo, gralloc_info,
                                     pAllocator, pImage);
 
-#ifndef VK_USE_PLATFORM_ANDROID_KHR
-   /* Ignore swapchain creation info on Android. Since we don't have an
-    * implementation in Mesa, we're guaranteed to access an Android object
-    * incorrectly.
-    */
    const VkImageSwapchainCreateInfoKHR *swapchain_info =
       vk_find_struct_const(pCreateInfo->pNext, IMAGE_SWAPCHAIN_CREATE_INFO_KHR);
-   if (swapchain_info && swapchain_info->swapchain != VK_NULL_HANDLE)
+   if (swapchain_info && swapchain_info->swapchain != VK_NULL_HANDLE) {
+#ifdef VK_USE_PLATFORM_ANDROID_KHR
+      /* Fail image creation out of swapchains on Android. Since we don't have
+       * an implementation in Mesa of the swapchain in Mesa, we're guaranteed
+       * to access an Android object incorrectly.
+       */
+      return vk_error(VK_ERROR_INITIALIZATION_FAILED);
+#else
       return anv_image_from_swapchain(device, pCreateInfo, swapchain_info,
                                       pAllocator, pImage);
 #endif
+   }
 
    return anv_image_create(device,
       &(struct anv_image_create_info) {