Grima04/mesa-turnip-kgsl

commit ba11f673a245a00d03c901d32ddc3489cb3ad287

i965: support only color formats with memory objects
There are issues with depth support and lack of support for
combined depth+stencil, let's support only color formats for now.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4712
Fixes: f73aeca0ce3 ("i965: Initial implementation for EXT_memory_object_*")
Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: Rohan Garg <rohan.garg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10646>

src/mesa/drivers/dri/i965/brw_tex.c
@@ -353,6 +353,10 @@ intel_texture_for_memory_object(struct gl_context *ctx,
    struct gl_texture_image *image = tex_obj->Image[0][0];
    struct isl_surf surf;
 
+   /* Only color formats are supported. */
+   if (!_mesa_is_format_color_format(image->TexFormat))
+      return GL_FALSE;
+
    isl_tiling_flags_t tiling_flags = ISL_TILING_ANY_MASK;
    if (tex_obj->TextureTiling == GL_LINEAR_TILING_EXT)
       tiling_flags = ISL_TILING_LINEAR_BIT;

src/mesa/drivers/dri/i965/brw_surface_formats.c
@@ -197,7 +197,6 @@ brw_isl_format_for_mesa_format(mesa_format mesa_format)
       [MESA_FORMAT_RGBX_UNORM16] = ISL_FORMAT_R16G16B16X16_UNORM,
       [MESA_FORMAT_RGBX_FLOAT16] = ISL_FORMAT_R16G16B16X16_FLOAT,
       [MESA_FORMAT_RGBX_FLOAT32] = ISL_FORMAT_R32G32B32X32_FLOAT,
-      [MESA_FORMAT_Z_UNORM16] = ISL_FORMAT_R16_UNORM,
    };
 
    assert(mesa_format < MESA_FORMAT_COUNT);

upcliujie/mesa

commit c0926f9ca064f29531324ea4c6b1c5965719e1eb

intel: Create a separate stencil miptree for combined depth and stencil formats
Handle combined depth and stencil formats by creating and setting a
separate stencil miptree.

Simplify the memory object import code by assuming we're running on
Gen 7 and above ( and below Gen 9 ).

Signed-off-by: Rohan Garg <rohan.garg@collabora.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Fixes: ba11f673a245a00d03c901d32ddc3489cb3ad287 ("i965: support only color formats with memory objects")

src/mesa/drivers/dri/i965/brw_tex.c
@@ -322,23 +322,6 @@ brw_texture_barrier(struct gl_context *ctx)
    }
 }
 
-/* Return the usual surface usage flags for the given format. */
-static isl_surf_usage_flags_t
-isl_surf_usage(mesa_format format)
-{
-   switch(_mesa_get_format_base_format(format)) {
-   case GL_DEPTH_COMPONENT:
-      return ISL_SURF_USAGE_DEPTH_BIT | ISL_SURF_USAGE_TEXTURE_BIT;
-   case GL_DEPTH_STENCIL:
-      return ISL_SURF_USAGE_DEPTH_BIT | ISL_SURF_USAGE_STENCIL_BIT |
-             ISL_SURF_USAGE_TEXTURE_BIT;
-   case GL_STENCIL_INDEX:
-      return ISL_SURF_USAGE_STENCIL_BIT | ISL_SURF_USAGE_TEXTURE_BIT;
-   default:
-      return ISL_SURF_USAGE_RENDER_TARGET_BIT | ISL_SURF_USAGE_TEXTURE_BIT;
-   }
-}
-
 static GLboolean
 intel_texture_for_memory_object(struct gl_context *ctx,
                                           struct gl_texture_object *tex_obj,
@@ -351,30 +334,11 @@ intel_texture_for_memory_object(struct gl_context *ctx,
    struct brw_memory_object *intel_memobj = brw_memory_object(mem_obj);
    struct brw_texture_object *intel_texobj = brw_texture_object(tex_obj);
    struct gl_texture_image *image = tex_obj->Image[0][0];
-   struct isl_surf surf;
+   enum isl_tiling tiling = ISL_TILING_Y0;
+   uint64_t s_offset;
 
-   /* Only color formats are supported. */
-   if (!_mesa_is_format_color_format(image->TexFormat))
-      return GL_FALSE;
-
-   isl_tiling_flags_t tiling_flags = ISL_TILING_ANY_MASK;
    if (tex_obj->TextureTiling == GL_LINEAR_TILING_EXT)
-      tiling_flags = ISL_TILING_LINEAR_BIT;
-
-   UNUSED const bool isl_surf_created_successfully =
-      isl_surf_init(&brw->screen->isl_dev, &surf,
-                    .dim = get_isl_surf_dim(tex_obj->Target),
-                    .format = brw_isl_format_for_mesa_format(image->TexFormat),
-                    .width = width,
-                    .height = height,
-                    .depth = depth,
-                    .levels = levels,
-                    .array_len = tex_obj->Target == GL_TEXTURE_3D ? 1 : depth,
-                    .samples = MAX2(image->NumSamples, 1),
-                    .usage = isl_surf_usage(image->TexFormat),
-                    .tiling_flags = tiling_flags);
-
-   assert(isl_surf_created_successfully);
+      tiling = ISL_TILING_LINEAR;
 
    intel_texobj->mt = brw_miptree_create_for_bo(brw,
                                                 intel_memobj->bo,
@@ -383,10 +347,30 @@ intel_texture_for_memory_object(struct gl_context *ctx,
                                                 width,
                                                 height,
                                                 depth,
-                                                surf.row_pitch_B,
-                                                surf.tiling,
+                                                0,
+                                                tiling,
                                                 MIPTREE_CREATE_NO_AUX);
    assert(intel_texobj->mt);
+
+   if (util_format_is_depth_and_stencil(image->TexFormat)) {
+      s_offset = offset +
+         ALIGN(intel_texobj->mt->surf.size_B,
+               intel_texobj->mt->surf.alignment_B);
+
+      intel_texobj->mt->stencil_mt =
+         brw_miptree_create_for_bo(brw,
+                                   intel_memobj->bo,
+                                   MESA_FORMAT_S_UINT8,
+                                   s_offset,
+                                   width,
+                                   height,
+                                   depth,
+                                   0,
+                                   ISL_TILING_W,
+                                   MIPTREE_CREATE_NO_AUX);
+      assert(intel_texobj->mt->stencil_mt);
+   }
+
    brw_alloc_texture_image_buffer(ctx, image);
 
    intel_texobj->needs_validate = false;