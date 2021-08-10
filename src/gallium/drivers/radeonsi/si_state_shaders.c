2168853474c1a0798a3697bb987d896a9c5c47cd
Marek Olšák <marek.olsak@amd.com>
2021-08-10 08:23:50 -0400

    radeonsi: unset SI_PREFETCH_* only when we unbind pm4 shader states

--- src/gallium/drivers/radeonsi/si_state_shaders.c
+++ src/gallium/drivers/radeonsi/si_state_shaders.c
@@ -4207,9 +4207,12 @@ bool si_update_shaders(struct si_context *sctx)
          }
       }
    } else {
-      if (sctx->chip_class <= GFX8)
+      if (sctx->chip_class <= GFX8) {
          si_pm4_bind_state(sctx, ls, NULL);
+         sctx->prefetch_L2_mask &= ~SI_PREFETCH_LS;
+      }
       si_pm4_bind_state(sctx, hs, NULL);
+      sctx->prefetch_L2_mask &= ~SI_PREFETCH_HS;
    }
 
    /* Update GS. */
@@ -4225,12 +4228,16 @@ bool si_update_shaders(struct si_context *sctx)
             return false;
       } else {
          si_pm4_bind_state(sctx, vs, NULL);
+         sctx->prefetch_L2_mask &= ~SI_PREFETCH_VS;
       }
    } else {
       if (!sctx->ngg) {
          si_pm4_bind_state(sctx, gs, NULL);
-         if (sctx->chip_class <= GFX8)
+         sctx->prefetch_L2_mask &= ~SI_PREFETCH_GS;
+         if (sctx->chip_class <= GFX8) {
             si_pm4_bind_state(sctx, es, NULL);
+            sctx->prefetch_L2_mask &= ~SI_PREFETCH_ES;
+         }
       }
    }
 
@@ -4244,6 +4251,7 @@ bool si_update_shaders(struct si_context *sctx)
          if (sctx->ngg) {
             si_pm4_bind_state(sctx, gs, sctx->shader.vs.current->pm4);
             si_pm4_bind_state(sctx, vs, NULL);
+            sctx->prefetch_L2_mask &= ~SI_PREFETCH_VS;
          } else {
             si_pm4_bind_state(sctx, vs, sctx->shader.vs.current->pm4);
          }
@@ -4364,33 +4372,21 @@ bool si_update_shaders(struct si_context *sctx)
    if (sctx->chip_class >= GFX7) {
       if (si_pm4_state_enabled_and_changed(sctx, ls))
          sctx->prefetch_L2_mask |= SI_PREFETCH_LS;
-      else if (!sctx->queued.named.ls)
-         sctx->prefetch_L2_mask &= ~SI_PREFETCH_LS;
 
       if (si_pm4_state_enabled_and_changed(sctx, hs))
          sctx->prefetch_L2_mask |= SI_PREFETCH_HS;
-      else if (!sctx->queued.named.hs)
-         sctx->prefetch_L2_mask &= ~SI_PREFETCH_HS;
 
       if (si_pm4_state_enabled_and_changed(sctx, es))
          sctx->prefetch_L2_mask |= SI_PREFETCH_ES;
-      else if (!sctx->queued.named.es)
-         sctx->prefetch_L2_mask &= ~SI_PREFETCH_ES;
 
       if (si_pm4_state_enabled_and_changed(sctx, gs))
          sctx->prefetch_L2_mask |= SI_PREFETCH_GS;
-      else if (!sctx->queued.named.gs)
-         sctx->prefetch_L2_mask &= ~SI_PREFETCH_GS;
 
       if (si_pm4_state_enabled_and_changed(sctx, vs))
          sctx->prefetch_L2_mask |= SI_PREFETCH_VS;
-      else if (!sctx->queued.named.vs)
-         sctx->prefetch_L2_mask &= ~SI_PREFETCH_VS;
 
       if (si_pm4_state_enabled_and_changed(sctx, ps))
          sctx->prefetch_L2_mask |= SI_PREFETCH_PS;
-      else if (!sctx->queued.named.ps)
-         sctx->prefetch_L2_mask &= ~SI_PREFETCH_PS;
    }
 
    sctx->do_update_shaders = false;
