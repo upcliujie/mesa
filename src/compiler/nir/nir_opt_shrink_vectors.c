e6309876c27d22365816f10b7dcc387f2380c3a7
Daniel Schürmann <daniel@schuermann.dev>
2021-08-17 13:23:22 +0200

    nir/opt_shrink_vectors: shrink load_const properly
    This patch enables removal of arbitrary channels in
    load_const instructions, if they are either unused or
    duplicates of other channels and only used by ALU.
    
    Totals from 869 (0.58% of 150170) affected shaders: (GFX10.3)
    VGPRs: 26904 -> 26568 (-1.25%); split: -1.40%, +0.15%
    CodeSize: 1562048 -> 1550980 (-0.71%); split: -0.72%, +0.01%
    MaxWaves: 24510 -> 24538 (+0.11%)
    Instrs: 291466 -> 289777 (-0.58%); split: -0.59%, +0.01%
    Latency: 2078995 -> 2081686 (+0.13%); split: -0.11%, +0.24%
    InvThroughput: 484515 -> 481206 (-0.68%); split: -0.69%, +0.01%
    VClause: 5589 -> 5598 (+0.16%)
    SClause: 17962 -> 17996 (+0.19%); split: -0.03%, +0.22%
    Copies: 10898 -> 10886 (-0.11%); split: -0.17%, +0.06%
    Branches: 2864 -> 2861 (-0.10%)
    PreVGPRs: 19371 -> 19253 (-0.61%); split: -0.87%, +0.26%

--- src/compiler/nir/nir_opt_shrink_vectors.c
+++ src/compiler/nir/nir_opt_shrink_vectors.c
@@ -70,6 +70,20 @@ shrink_dest_to_read_mask(nir_ssa_def *def)
    return false;
 }
 
+static void
+reswizzle_alu_uses(nir_ssa_def *def, uint8_t *reswizzle)
+{
+   nir_foreach_use(use_src, def) {
+      /* all uses must be ALU instructions */
+      assert(use_src->parent_instr->type == nir_instr_type_alu);
+      nir_alu_src *alu_src = (nir_alu_src*)use_src;
+
+      /* reswizzle ALU sources */
+      for (unsigned i = 0; i < NIR_MAX_VEC_COMPONENTS; i++)
+         alu_src->swizzle[i] = reswizzle[alu_src->swizzle[i]];
+   }
+}
+
 static bool
 opt_shrink_vectors_alu(nir_builder *b, nir_alu_instr *instr)
 {
@@ -156,12 +170,7 @@ opt_shrink_vectors_alu(nir_builder *b, nir_alu_instr *instr)
    assert(index == num_components);
 
    /* update uses */
-   nir_foreach_use(use_src, def) {
-      assert(use_src->parent_instr->type == nir_instr_type_alu);
-      nir_alu_src *alu_src = (nir_alu_src*)use_src;
-      for (unsigned i = 0; i < NIR_MAX_VEC_COMPONENTS; i++)
-         alu_src->swizzle[i] = reswizzle[alu_src->swizzle[i]];
-   }
+   reswizzle_alu_uses(def, reswizzle);
 
    return true;
 }
@@ -257,7 +266,53 @@ opt_shrink_vectors_intrinsic(nir_builder *b, nir_intrinsic_instr *instr, bool sh
 static bool
 opt_shrink_vectors_load_const(nir_load_const_instr *instr)
 {
-   return shrink_dest_to_read_mask(&instr->def);
+   nir_ssa_def *def = &instr->def;
+
+   /* early out if there's nothing to do. */
+   if (def->num_components == 1)
+      return false;
+
+   /* don't remove any channels if used by non-ALU */
+   nir_foreach_use(use_src, def) {
+      if (use_src->parent_instr->type != nir_instr_type_alu)
+         return false;
+   }
+
+   unsigned mask = nir_ssa_def_components_read(def);
+
+   /* If nothing was read, leave it up to DCE. */
+   if (!mask)
+      return false;
+
+   uint8_t reswizzle[NIR_MAX_VEC_COMPONENTS] = { 0 };
+   unsigned index = 0;
+   for (unsigned i = 0; i < def->num_components; i++) {
+      /* skip unused components */
+      if (!((mask >> i) & 0x1))
+         continue;
+
+      /* remove duplicate constants */
+      for (unsigned j = i + 1; j < def->num_components; j++) {
+         if (instr->value[i].u64 == instr->value[j].u64) {
+            reswizzle[j] = index;
+            mask &= ~(1 << j);
+         }
+      }
+
+      instr->value[index] = instr->value[i];
+      reswizzle[i] = index++;
+   }
+
+   /* return if no component was removed */
+   if (mask == BITFIELD_MASK(def->num_components))
+       return false;
+
+   def->num_components = util_bitcount(mask);
+
+   /* update uses */
+   reswizzle_alu_uses(def, reswizzle);
+
+   return true;
 }
 
 static bool
