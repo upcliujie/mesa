aerisarn/mesa-uwp

commit a48fc88571f9fbaaf82390187bc748fed0dfa02a

freedreno/a6xx: Apply the cube image size lowering to GL, too.
Fixes KHR-GLES31.core.texture_cube_map_array.texture_size_compute_sh.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12256>

src/freedreno/ir3/ir3_nir.c
@@ -352,6 +352,11 @@ ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s)
       .lower_tg4_offsets = true,
    };
 
+   nir_lower_image_options lower_image_opts = {
+      .lower_cube_size = true,
+   };
+   NIR_PASS_V(s, nir_lower_image, &lower_image_opts);
+
    if (compiler->gen >= 4) {
       /* a4xx seems to have *no* sam.p */
       tex_options.lower_txp = ~0; /* lower all txp */

src/freedreno/vulkan/tu_shader.c
@@ -781,11 +781,6 @@ tu_shader_create(struct tu_device *dev,
 
    NIR_PASS_V(nir, tu_lower_io, shader, layout);
 
-   nir_lower_image_options lower_image_opts = {
-      .lower_cube_size = true,
-   };
-   NIR_PASS_V(nir, nir_lower_image, &lower_image_opts);
-
    nir_shader_gather_info(nir, nir_shader_get_entrypoint(nir));
 
    ir3_finalize_nir(dev->compiler, nir);

upcliujie/mesa

commit 13e81da23dbf7c7d5c8c61048a2fb5a43449f6e2

freedreno/ir3: Only lower cube image sizes once.
shader variants can cause ir3_nir_finalize() to run more than once, which
would make us keep dividing the size by 6.

Fixes: a48fc88571f9 ("freedreno/a6xx: Apply the cube image size lowering to GL, too.")

src/freedreno/ir3/ir3_nir.c
@@ -352,11 +352,6 @@ ir3_finalize_nir(struct ir3_compiler *compiler, nir_shader *s)
       .lower_tg4_offsets = true,
    };
 
-   nir_lower_image_options lower_image_opts = {
-      .lower_cube_size = true,
-   };
-   NIR_PASS_V(s, nir_lower_image, &lower_image_opts);
-
    if (compiler->gen >= 4) {
       /* a4xx seems to have *no* sam.p */
       tex_options.lower_txp = ~0; /* lower all txp */
@@ -477,6 +472,11 @@ ir3_nir_lower_subgroup_id_cs(nir_shader *shader)
                                         lower_subgroup_id, NULL);
 }
 
+static const nir_lower_idiv_options idiv_options = {
+   .imprecise_32bit_lowering = true,
+   .allow_fp16 = true,
+};
+
 /**
  * Late passes that need to be done after pscreen->finalize_nir()
  */
@@ -525,9 +525,15 @@ ir3_nir_post_finalize(struct ir3_compiler *compiler, nir_shader *s)
    }
 
    /* we cannot ensure that ir3_finalize_nir() is only called once, so
-    * we also need to do trig workarounds here:
+    * we also need to do any run-once workarounds here:
     */
    OPT_V(s, ir3_nir_apply_trig_workarounds);
+
+   nir_lower_image_options lower_image_opts = {
+      .lower_cube_size = true,
+   };
+   NIR_PASS_V(s, nir_lower_image, &lower_image_opts);
+   NIR_PASS_V(s, nir_lower_idiv, &idiv_options); /* idiv generated by cube lowering */
 
    ir3_optimize_loop(compiler, s);
 }