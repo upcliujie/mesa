48f6c656ee748e45432ca85ab01b222c8e6da954
Rhys Perry <pendingchaos02@gmail.com>
2021-08-18 13:50:58 +0100

    nir: allow NIR_PASS(_, )
    If a user wants to skip printing the shader if no changes were made
    without declaring a dummy variable for the progress.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -4496,6 +4496,7 @@ static inline bool should_print_nir(nir_shader *shader) { return false; }
       printf("%s\n", #pass);                                         \
    if (pass(nir, ##__VA_ARGS__)) {                                   \
       nir_validate_shader(nir, "after " #pass);                      \
+      UNUSED bool _;                                                 \
       progress = true;                                               \
       if (should_print_nir(nir))                                        \
          nir_print_shader(nir, stdout);                              \
