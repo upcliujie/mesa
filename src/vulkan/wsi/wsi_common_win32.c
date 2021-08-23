d4a72e9634d45e996349f3b21ef3ca544489695f
Dave Airlie <airlied@redhat.com>
2021-08-23 10:03:20 +1000

    vulkan/wsi/sw: add support for using host_ptr for shm pixmaps.
    This pipes the allocation of the MIT-SHM pixmap into the wsi common
    code to callback to the x11 path to allocate things in the right place.
    
    Acked-by: Roland Scheidegger <sroland@vmware.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- src/vulkan/wsi/wsi_common_win32.c
+++ src/vulkan/wsi/wsi_common_win32.c
@@ -301,6 +301,7 @@ wsi_create_native_image(const struct wsi_swapchain *chain,
                         uint32_t num_modifier_lists,
                         const uint32_t *num_modifiers,
                         const uint64_t *const *modifiers,
+                        uint8_t *(alloc_shm)(struct wsi_image *image, unsigned size),
                         struct wsi_image *image)
 {
    const struct wsi_device *wsi = chain->wsi;
