ab1cf9df97f44ad43257522760f6c27e06917ad8
Rhys Perry <pendingchaos02@gmail.com>
2021-08-30 14:02:38 +0100

    ac/llvm,radv: implement uadd_sat/iadd_sat
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -3497,9 +3497,7 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
          if (!radv_use_llvm_for_stage(device, i))
             nir_lower_int64(nir[i]);
 
-         /* TODO: Implement nir_op_uadd_sat with LLVM. */
-         if (!radv_use_llvm_for_stage(device, i))
-            nir_opt_idiv_const(nir[i], 8);
+         nir_opt_idiv_const(nir[i], 8);
 
          nir_lower_idiv(nir[i],
                         &(nir_lower_idiv_options){
