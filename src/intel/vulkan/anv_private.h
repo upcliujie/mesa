aerisarn/mesa-uwp

commit ac6d7a1758cbee91caf6812333bb3416d22ad7e4

anv: Make anv_address::offset 64-bit
This allows us to convert a 64-bit address to an anv_address which is
useful for working with device addresses.

v2: switch to int64_t to keep state pool relative relocation working
    on non-softpin platforms

v3: Update assert to reflect relative offsets (Jason)

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8637>

src/intel/vulkan/anv_private.h
@@ -1177,7 +1177,7 @@ anv_device_upload_nir(struct anv_device *device,
 
 struct anv_address {
    struct anv_bo *bo;
-   uint32_t offset;
+   int64_t offset;
 };
 
 struct anv_device {
@@ -1639,6 +1639,16 @@ anv_batch_emit_reloc(struct anv_batch *batch,
 
 #define ANV_NULL_ADDRESS ((struct anv_address) { NULL, 0 })
 
+static inline struct anv_address
+anv_address_from_u64(uint64_t addr_u64)
+{
+   assert(addr_u64 == intel_canonical_address(addr_u64));
+   return (struct anv_address) {
+      .bo = NULL,
+      .offset = addr_u64,
+   };
+}
+
 static inline bool
 anv_address_is_null(struct anv_address addr)
 {
@@ -1691,7 +1701,8 @@ _anv_combine_address(struct anv_batch *batch, void *location,
       return anv_address_physical(anv_address_add(address, delta));
    } else {
       assert(batch->start <= location && location < batch->end);
-
+      /* i915 relocations are signed. */
+      assert(INT32_MIN <= address.offset && address.offset <= INT32_MAX);
       return anv_batch_emit_reloc(batch, location, address.bo, address.offset + delta);
    }
 }

upcliujie/mesa

commit 8f2b8ee379f6a1d9727819360d588d3f7077a8ef

anv: allow offset > 2Gb from BO start with softpin
Ran into this with a really large perf query pool > 2Gb. This isn't
strictly a fix because the behavior before ac6d7a1758cbee was probably
that we would wrap around offsets on 32bit which is broken.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: ac6d7a1758cbee ("anv: Make anv_address::offset 64-bit")

src/intel/vulkan/anv_private.h
@@ -1714,8 +1714,9 @@ _anv_combine_address(struct anv_batch *batch, void *location,
       return anv_address_physical(anv_address_add(address, delta));
    } else {
       assert(batch->start <= location && location < batch->end);
-      /* i915 relocations are signed. */
-      assert(INT32_MIN <= address.offset && address.offset <= INT32_MAX);
+      /* i915 relocations are signed. Skip if we're softpinning. */
+      assert(ANV_ALWAYS_SOFTPIN ||
+             (INT32_MIN <= address.offset && address.offset <= INT32_MAX));
       return anv_batch_emit_reloc(batch, location, address.bo, address.offset + delta);
    }
 }