07778f54826767f8772564777dec45db9dca617d
Boris Brezillon <boris.brezillon@collabora.com>
2021-09-03 10:20:56 +0200

    pan/midg: Remove spurious printf() in print_vector_constants()
    Also tried to replace that one by an fprintf(fp, ...), but it pollutes
    the dump too.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- src/panfrost/midgard/disassemble.c
+++ src/panfrost/midgard/disassemble.c
@@ -623,8 +623,6 @@ print_vector_constants(FILE *fp, unsigned src_binary,
                         /* We work on twos, actually */
                         if (i & 1)
                                 c++;
-                } else {
-                        printf(" (%u)", src->expand_mode);
                 }
 
                 if (first)
