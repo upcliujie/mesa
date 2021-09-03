ff0ec93a425b22e7d5dee24861f333450eb3ef21
Matthieu Herrb <matthieu@herrb.eu>
2021-09-03 10:00:28 +0200

    replace uses of printf("...%n...", .. &len) -> len = printf(...)
    Signed-off-by: Matthieu Herrb <matthieu@herrb.eu>

--- src/freedreno/rnn/headergen2.c
+++ src/freedreno/rnn/headergen2.c
@@ -97,9 +97,9 @@ static void printdef (char *name, char *suf, int type, uint64_t val, char *file)
 	FILE *dst = findfout(file);
 	int len;
 	if (suf)
-		fprintf (dst, "#define %s__%s%n", name, suf, &len);
+		len = fprintf (dst, "#define %s__%s", name, suf);
 	else
-		fprintf (dst, "#define %s%n", name, &len);
+		len = fprintf (dst, "#define %s", name);
 	if (type == 0 && val > 0xffffffffull)
 		seekcol (dst, len, startcol-8);
 	else
@@ -311,7 +311,7 @@ static void printdelem (struct rnndelem *elem, uint64_t offset, const char *str)
 					fprintf(dst, "enum %s ", elems[i]->index->name);
 				else
 					fprintf(dst, "uint32_t ");
-				fprintf (dst, "i%d%n", i, &len);
+				len = fprintf (dst, "i%d", i);
 			}
 			fprintf (dst, ") { return ");
 			fprintf (dst, "0x%08"PRIx64"", offset + elem->offset);
