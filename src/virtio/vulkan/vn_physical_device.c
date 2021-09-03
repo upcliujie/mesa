aerisarn/mesa-uwp

commit 7dfac808b029a887023c2c24d08b408f517ce507

venus: no supported device is not an error
Signed-off-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Ryan Neph <ryanneph@google.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12653>

src/virtio/vulkan/vn_physical_device.c
@@ -1336,16 +1336,20 @@ vn_instance_enumerate_physical_devices(struct vn_instance *instance)
    }
 
    count = supported_count;
-   if (!count)
-      goto out;
-
-   result = vn_instance_enumerate_physical_device_groups_locked(
-      instance, physical_devs, count);
-   if (result != VK_SUCCESS) {
-      for (uint32_t i = 0; i < count; i++)
-         vn_physical_device_fini(&physical_devs[i]);
-      count = 0;
-      goto out;
+   if (count) {
+      result = vn_instance_enumerate_physical_device_groups_locked(
+         instance, physical_devs, count);
+      if (result != VK_SUCCESS) {
+         for (uint32_t i = 0; i < count; i++)
+            vn_physical_device_fini(&physical_devs[i]);
+         count = 0;
+         goto out;
+      }
+   } else {
+      /* no supported physical device is not an error */
+      result = VK_SUCCESS;
+      vk_free(alloc, physical_devs);
+      physical_devs = NULL;
    }
 
    instance->physical_device.devices = physical_devs;

upcliujie/mesa

commit 9a663544eebc41b187b22e06886111555755e136

venus: OOM in device enumeration should be reported
Fixed
dEQP-VK.api.device_init.create_instance_device_intentional_alloc_fail.

Fixes: 7dfac808b02 ("venus: no supported device is not an error")
Signed-off-by: Chia-I Wu <olvaffe@gmail.com>

src/virtio/vulkan/vn_physical_device.c
@@ -1261,6 +1261,9 @@ vn_instance_enumerate_physical_device_groups_locked(
 static VkResult
 vn_instance_enumerate_physical_devices(struct vn_instance *instance)
 {
+   /* TODO enumerate physical device groups without enumerating physical
+    * devices first
+    */
    const VkAllocationCallbacks *alloc = &instance->base.base.alloc;
    struct vn_physical_device *physical_devs = NULL;
    VkResult result;
@@ -1346,11 +1349,13 @@ vn_instance_enumerate_physical_devices(struct vn_instance *instance)
          count = 0;
          goto out;
       }
-   } else {
+   } else if (result == VK_ERROR_INITIALIZATION_FAILED) {
       /* no supported physical device is not an error */
       result = VK_SUCCESS;
       vk_free(alloc, physical_devs);
       physical_devs = NULL;
+   } else {
+      goto out;
    }
 
    instance->physical_device.devices = physical_devs;