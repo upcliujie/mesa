Grima04/mesa-turnip-kgsl

commit bde08ce4d7bf2e59054ea1c522e82037c9dd147b

llvmpipe: handle compute shader launch with 0 threads
If you set LP_NUM_THREADS=0 compute shaders would hang,
just execute the workloads in sequence if we have no threads
in the pool.

Fixes: 1b24e3ba75 ("llvmpipe: add compute threadpool + mutex")
Reviewed-by: Roland Scheidegger <sroland@vmware.com>

src/gallium/drivers/llvmpipe/lp_cs_tpool.c
@@ -114,6 +114,15 @@ lp_cs_tpool_queue_task(struct lp_cs_tpool *pool,
 {
    struct lp_cs_tpool_task *task;
 
+   if (pool->num_threads == 0) {
+      struct lp_cs_local_mem lmem;
+
+      memset(&lmem, 0, sizeof(lmem));
+      for (unsigned t = 0; t < num_iters; t++) {
+         work(data, t, &lmem);
+      }
+      return NULL;
+   }
    task = CALLOC_STRUCT(lp_cs_tpool_task);
    if (!task) {
       return NULL;

upcliujie/mesa

commit 90019313838de92ea5b3609bde0ae2c3be450ccf

llvmpipe: Fix leak of CS local memory with 0 threads.
Fixes: bde08ce4d7bf ("llvmpipe: handle compute shader launch with 0 threads")

src/gallium/drivers/llvmpipe/lp_cs_tpool.c
@@ -121,6 +121,7 @@ lp_cs_tpool_queue_task(struct lp_cs_tpool *pool,
       for (unsigned t = 0; t < num_iters; t++) {
          work(data, t, &lmem);
       }
+      FREE(lmem.local_mem_ptr);
       return NULL;
    }
    task = CALLOC_STRUCT(lp_cs_tpool_task);