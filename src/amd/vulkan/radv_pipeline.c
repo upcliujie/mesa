aerisarn/mesa-uwp

commit 7e664a538355018a16ef60ecf8aaef304a25aa59

radv: Don't generate GS copy shader when the pipeline has NGG.
Previously the code used radv_pipeline_has_ngg but that always
returned false because the pipeline->shaders was all NULL at the
time when the GS copy shader was created.

Fixes: ca783612e721bc66af545075d76dc578ddbf7666
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11092>

src/amd/vulkan/radv_pipeline.c
@@ -3363,8 +3363,10 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
    radv_fill_shader_keys(device, keys, pipeline_key, nir);
    radv_fill_shader_info(pipeline, pStages, keys, infos, nir);
 
-   if ((nir[MESA_SHADER_VERTEX] && keys[MESA_SHADER_VERTEX].vs_common_out.as_ngg) ||
-       (nir[MESA_SHADER_TESS_EVAL] && keys[MESA_SHADER_TESS_EVAL].vs_common_out.as_ngg)) {
+   bool pipeline_has_ngg = (nir[MESA_SHADER_VERTEX] && keys[MESA_SHADER_VERTEX].vs_common_out.as_ngg) ||
+                           (nir[MESA_SHADER_TESS_EVAL] && keys[MESA_SHADER_TESS_EVAL].vs_common_out.as_ngg);
+
+   if (pipeline_has_ngg) {
       struct gfx10_ngg_info *ngg_info;
 
       if (nir[MESA_SHADER_GEOMETRY])
@@ -3512,7 +3514,7 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
 
    if (modules[MESA_SHADER_GEOMETRY]) {
       struct radv_shader_binary *gs_copy_binary = NULL;
-      if (!radv_pipeline_has_ngg(pipeline)) {
+      if (!pipeline_has_ngg) {
          struct radv_shader_info info = {0};
          struct radv_shader_variant_key key = {0};
 

upcliujie/mesa

commit 8bf80b9bd07e1ca1e906659dacd6f8f3b30d6892

radv: don't require a GS copy shader to use the cache with NGG VS+GS
Fixes dEQP-VK.pipeline.cache.no_cache_control.readwrite_cache.pipeline_from_get_data.vertex_stage_geometry_stage_fragment_stage

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Fixes: 7e664a53835 ("radv: Don't generate GS copy shader when the pipeline has NGG.")

src/amd/vulkan/radv_pipeline.c
@@ -3355,7 +3355,8 @@ radv_create_shaders(struct radv_pipeline *pipeline, struct radv_device *device,
    if (!keep_executable_info &&
        radv_create_shader_variants_from_pipeline_cache(device, cache, hash, pipeline->shaders,
                                                        &found_in_application_cache) &&
-       (!modules[MESA_SHADER_GEOMETRY] || pipeline->gs_copy_shader)) {
+       (!modules[MESA_SHADER_GEOMETRY] || pipeline->gs_copy_shader ||
+        pipeline->shaders[MESA_SHADER_GEOMETRY]->info.is_ngg)) {
       radv_stop_feedback(pipeline_feedback, found_in_application_cache);
       return VK_SUCCESS;
    }