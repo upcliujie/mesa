41f5c8899a924e3d69d1e6a57fb971f753cd4810
Nanley Chery <nanley.g.chery@intel.com>
2021-09-08 06:47:33 -0700

    iris: Drop a lowered aux import case
    Iris no longer has to handle importing a YUYV or UYVY resource with 3
    planes thanks to commit f57c074270e ("gallium/dri: Allow use of
    R8G8_R8B8 for YUYV and G8R8_B8R8 for UYVY").

--- src/gallium/drivers/iris/iris_resource.c
+++ src/gallium/drivers/iris/iris_resource.c
@@ -949,18 +949,12 @@ iris_resource_finish_aux_import(struct pipe_screen *pscreen,
       r[0]->aux.clear_color_bo = r[2]->aux.clear_color_bo;
       r[0]->aux.clear_color_offset = r[2]->aux.clear_color_offset;
       r[0]->aux.clear_color_unknown = true;
-   } else if (num_main_planes == 2 && num_planes == 4) {
+   } else {
+      assert(num_main_planes == 2 && num_planes == 4);
       import_aux_info(r[0], r[2]);
       import_aux_info(r[1], r[3]);
       map_aux_addresses(screen, r[0], format, 0);
       map_aux_addresses(screen, r[1], format, 1);
-   } else {
-      /* Gallium has lowered a single main plane into two. */
-      assert(num_main_planes == 2 && num_planes == 3);
-      assert(isl_format_is_yuv(format) && !isl_format_is_planar(format));
-      import_aux_info(r[0], r[2]);
-      import_aux_info(r[1], r[2]);
-      map_aux_addresses(screen, r[0], format, 0);
    }
 }
 
