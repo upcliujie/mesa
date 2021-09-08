92edc30d80649f7a0769e8146e79b3617efcfef7
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-09-08 07:52:58 +0200

    virgl/ci: Run each dEQP instance in its own VM
    Currently we run deqp-runner inside a single VM, which makes very poor
    use of the available CPUs because Virgl has a bottleneck in the VMM that
    serializes everything.
    
    With this change, we can run several Crosvm instances in a runner and
    make full use of the CPUs.
    
    With this change, we can get the same coverage with 3 runners instead of
    6. Use the freed capacity to also test with GLES on the host.
    
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

--- .gitlab-ci/crosvm-init.sh
+++ .gitlab-ci/crosvm-init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-set -ex
+set -e
+
+export DEQP_TEMP_DIR=$1
 
 mount -t proc none /proc
 mount -t sysfs none /sys
@@ -9,14 +11,19 @@ mkdir -p /dev/pts
 mount -t devpts devpts /dev/pts
 mount -t tmpfs tmpfs /tmp
 
-. /crosvm-env.sh
+. $DEQP_TEMP_DIR/crosvm-env.sh
 
-# / is ro
-export PIGLIT_REPLAY_EXTRA_ARGS="$PIGLIT_REPLAY_EXTRA_ARGS --db-path /tmp/replayer-db"
+# Remove this once deqp-runner does the renderer check
+export LIBGL_ALWAYS_SOFTWARE=false
+export GALLIUM_DRIVER=virgl
 
-if sh $CROSVM_TEST_SCRIPT; then
-    touch /results/success
+cd $PWD
+
+set +e
+if sh $DEQP_TEMP_DIR/crosvm-script.sh; then
+    touch $CI_PROJECT_DIR/results/success
 fi
+set -e
 
 sleep 5   # Leave some time to get the last output flushed out
 
