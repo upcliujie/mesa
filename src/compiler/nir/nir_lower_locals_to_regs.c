3d8e4e963ef2f6e6d04da85384fcf796ae13bde0
Rhys Perry <pendingchaos02@gmail.com>
2021-09-08 16:41:00 +0100

    nir: adjust nir_src_copy signature to take a nir_instr *
    This is almost always a nir_instr and updating the src of a nir_if will
    have to work slightly differently in the future.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/compiler/nir/nir_lower_locals_to_regs.c
+++ src/compiler/nir/nir_lower_locals_to_regs.c
@@ -227,7 +227,7 @@ lower_locals_to_regs_block(nir_block *block,
          nir_src reg_src = get_deref_reg_src(deref, state);
 
          nir_alu_instr *mov = nir_alu_instr_create(b->shader, nir_op_mov);
-         nir_src_copy(&mov->src[0].src, &intrin->src[1], mov);
+         nir_src_copy(&mov->src[0].src, &intrin->src[1], &mov->instr);
          mov->dest.write_mask = nir_intrinsic_write_mask(intrin);
          mov->dest.dest.is_ssa = false;
          mov->dest.dest.reg.reg = reg_src.reg.reg;
