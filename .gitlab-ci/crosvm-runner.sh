0b04971ecdbfc287ebeabfad0b8ddf3654545bba
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-09-08 07:52:58 +0200

    virgl/ci: Run each dEQP instance in its own VM
    Currently we run deqp-runner inside a single VM, which makes very poor
    use of the available CPUs because Virgl has a bottleneck in the VMM that
    serializes everything.
    
    With this change, we can run several Crosvm instances in a runner and
    make full use of the CPUs.
    
    With this change, we can get the same coverage with 3 runners instead of
    6. Use the freed capacity to also test with GLES on the host.
    
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

--- .gitlab-ci/crosvm-runner.sh
+++ .gitlab-ci/crosvm-runner.sh
@@ -1,49 +1,50 @@
 #!/bin/sh
 
-set -x
+set -e
 
-ln -sf $CI_PROJECT_DIR/install /install
+# This script can be called concurrently, pass arguments and env in a per-instance tmp dir
+export DEQP_TEMP_DIR=`mktemp -d /tmp.XXXXXXXXXX`
 
-export LD_LIBRARY_PATH=$CI_PROJECT_DIR/install/lib/
-export EGL_PLATFORM=surfaceless
+# The dEQP binary needs to run from the directory it's in
+if [ -z "${1##*"deqp"*}" ]; then
+  PWD=`dirname $1`
+fi
 
-export -p > /crosvm-env.sh
-export GALLIUM_DRIVER="$CROSVM_GALLIUM_DRIVER"
-export GALLIVM_PERF="nopt"
-export LIBGL_ALWAYS_SOFTWARE="true"
+export -p > $DEQP_TEMP_DIR/crosvm-env.sh
 
-CROSVM_KERNEL_ARGS="root=my_root rw rootfstype=virtiofs loglevel=3 init=$CI_PROJECT_DIR/install/crosvm-init.sh ip=192.168.30.2::192.168.30.1:255.255.255.0:crosvm:eth0"
+CROSVM_KERNEL_ARGS="root=my_root rw rootfstype=virtiofs loglevel=3 init=$CI_PROJECT_DIR/install/crosvm-init.sh ip=192.168.30.2::192.168.30.1:255.255.255.0:crosvm:eth0 -- $DEQP_TEMP_DIR"
 
-# Temporary results dir because from the guest we cannot write to /
-mkdir -p /results
-mount -t tmpfs tmpfs /results
-
-mkdir -p /piglit/.gitlab-ci/piglit
-mount -t tmpfs tmpfs /piglit/.gitlab-ci/piglit
+echo $@ > $DEQP_TEMP_DIR/crosvm-script.sh
 
 unset DISPLAY
 unset XDG_RUNTIME_DIR
 
-/usr/sbin/iptables-legacy  -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# Crosvm wants this
-syslogd > /dev/null
-
 # We aren't testing LLVMPipe here, so we don't need to validate NIR on the host
 export NIR_VALIDATE=0
+
+export LIBGL_ALWAYS_SOFTWARE="true"
+export GALLIUM_DRIVER="$CROSVM_GALLIUM_DRIVER"
+
+# One-time setup
+(
+  flock 9
+  if [ -z "`pidof syslogd`" ]; then
+      /usr/sbin/iptables-legacy  -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+      echo 1 > /proc/sys/net/ipv4/ip_forward
+
+      syslogd > /dev/null &  # Crosvm requires syslogd
+      sleep 1
+  fi
+) 9>/var/lock/crosvm_lock
 
 crosvm run \
   --gpu "$CROSVM_GPU_ARGS" \
   -m 4096 \
-  -c $((FDO_CI_CONCURRENT > 1 ? FDO_CI_CONCURRENT - 1 : 1)) \
+  -c 1 \
   --disable-sandbox \
   --shared-dir /:my_root:type=fs:writeback=true:timeout=60:cache=always \
   --host_ip=192.168.30.1 --netmask=255.255.255.0 --mac "AA:BB:CC:00:00:12" \
   -p "$CROSVM_KERNEL_ARGS" \
-  /lava-files/bzImage
-
-mkdir -p $CI_PROJECT_DIR/results
-mv /results/* $CI_PROJECT_DIR/results/.
+  /lava-files/bzImage 2>&1 | LC_ALL=C tr -dc '\0-\177'
 
 test -f $CI_PROJECT_DIR/results/success
