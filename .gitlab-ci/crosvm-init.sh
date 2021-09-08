420645e3e3b6f7ca9c77c799c98da777814bd3bc
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-09-08 07:52:58 +0200

    virgl/ci: Run each dEQP instance in its own VM
    Currently we run deqp-runner inside a single VM, which makes very poor
    use of the available CPUs because Virgl has a bottleneck in the VMM that
    serializes everything.
    
    With this change, we can run several Crosvm instances in a runner and
    make full use of the CPUs. Getting the same coverage with 3 runners
    instead of 6.
    
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

--- .gitlab-ci/crosvm-init.sh
+++ .gitlab-ci/crosvm-init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-set -ex
+set -e
+
+export DEQP_TEMP_DIR=$1
 
 mount -t proc none /proc
 mount -t sysfs none /sys
@@ -9,19 +11,19 @@ mkdir -p /dev/pts
 mount -t devpts devpts /dev/pts
 mount -t tmpfs tmpfs /tmp
 
-. /crosvm-env.sh
+. $DEQP_TEMP_DIR/crosvm-env.sh
 
-# / is ro
-export PIGLIT_REPLAY_EXTRA_ARGS="$PIGLIT_REPLAY_EXTRA_ARGS --db-path /tmp/replayer-db"
+cd $PWD
 
-if sh $CROSVM_TEST_SCRIPT; then
-    touch /results/success
-fi
+sleep 1   # Give some time to Crosvm to initialize virtio-gpu
 
-sleep 5   # Leave some time to get the last output flushed out
+set +e
+stdbuf -oL sh $DEQP_TEMP_DIR/crosvm-script.sh
+echo $? > $DEQP_TEMP_DIR/exit_code
+set -e
+
+sleep 1   # Leave some time to get the last output flushed out
 
 poweroff -d -n -f || true
 
-sleep 10   # Just in case init would exit before the kernel shuts down the VM
-
-exit 1
+sleep 1   # Just in case init would exit before the kernel shuts down the VM
