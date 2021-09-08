016f6e18a9fadb474a632b4cd39355bdc34d8863
Jordan Justen <jordan.l.justen@intel.com>
2021-09-08 16:20:24 -0700

    intel: Move subslice_total into devinfo
    Suggested-by: Kenneth Graunke <kenneth@whitecape.org>
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/mesa/drivers/dri/i965/brw_screen.c
+++ src/mesa/drivers/dri/i965/brw_screen.c
@@ -2611,8 +2611,7 @@ __DRIconfig **brw_init_screen(__DRIscreen *dri_screen)
    isl_device_init(&screen->isl_dev, &screen->devinfo,
                    screen->hw_has_swizzling);
 
-   /* GENs prior to 8 do not support EU/Subslice info */
-   screen->subslice_total = intel_device_info_subslice_total(devinfo);
+   assert(screen->devinfo.subslice_total >= 1);
 
    /* Gfx7-7.5 kernel requirements / command parser saga:
     *
