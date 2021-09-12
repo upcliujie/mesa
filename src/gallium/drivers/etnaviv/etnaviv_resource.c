aerisarn/mesa-uwp

commit 3b3cd5128672ab2dd2541b1789a40a4dd2652cc1

etnaviv: fix renderonly check in etna_resource_alloc
When the driver hasn't been initialized via renderonly, screen->ro
will be NULL. This fixes a crash when passing USE_SCANOUT to etnaviv
when it's missing renderonly.

Signed-off-by: Simon Ser <contact@emersion.fr>
Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12074>

src/gallium/drivers/etnaviv/etnaviv_resource.c
@@ -272,7 +272,7 @@ etna_resource_alloc(struct pipe_screen *pscreen, unsigned layout,
 
    size = setup_miptree(rsc, paddingX, paddingY, msaa_xscale, msaa_yscale);
 
-   if (unlikely(templat->bind & PIPE_BIND_SCANOUT) && screen->ro->kms_fd >= 0) {
+   if (unlikely(templat->bind & PIPE_BIND_SCANOUT) && screen->ro) {
       struct pipe_resource scanout_templat = *templat;
       struct winsys_handle handle;
 

upcliujie/mesa

commit 8fe163d3f7b5a98225a453d2851e395e2f002b78

etnaviv: improve renderonly check in etna_resource_alloc
Fixes a crash I when running piglit with PIGLIT_PLATFORM="gbm".
In that specific case screen->ro->kms_fd is -1.

Fixes: 3b3cd512867 ("etnaviv: fix renderonly check in etna_resource_alloc")
Cc: mesa-stable
Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

src/gallium/drivers/etnaviv/etnaviv_resource.c
@@ -272,7 +272,7 @@ etna_resource_alloc(struct pipe_screen *pscreen, unsigned layout,
 
    size = setup_miptree(rsc, paddingX, paddingY, msaa_xscale, msaa_yscale);
 
-   if (unlikely(templat->bind & PIPE_BIND_SCANOUT) && screen->ro) {
+   if (unlikely(templat->bind & PIPE_BIND_SCANOUT) && screen->ro && screen->ro->kms_fd >= 0) {
       struct pipe_resource scanout_templat = *templat;
       struct winsys_handle handle;
 