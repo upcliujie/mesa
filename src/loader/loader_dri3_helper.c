5737861d58f1f5967f97cb0263551d2226681bae
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2021-09-13 16:30:48 +0200

    add a cap

--- src/loader/loader_dri3_helper.c
+++ src/loader/loader_dri3_helper.c
@@ -373,6 +373,7 @@ loader_dri3_drawable_init(xcb_connection_t *conn,
                           __DRIscreen *dri_screen,
                           bool is_different_gpu,
                           bool multiplanes_available,
+                          bool avoid_back_buffer_reuse,
                           const __DRIconfig *dri_config,
                           struct loader_dri3_extensions *ext,
                           const struct loader_dri3_vtable *vtable,
@@ -392,6 +393,7 @@ loader_dri3_drawable_init(xcb_connection_t *conn,
    draw->dri_screen = dri_screen;
    draw->is_different_gpu = is_different_gpu;
    draw->multiplanes_available = multiplanes_available;
+   draw->avoid_back_buffer_reuse = avoid_back_buffer_reuse;
 
    draw->have_back = 0;
    draw->have_fake_front = 0;
@@ -1932,7 +1934,7 @@ dri3_get_buffer(__DRIdrawable *driDrawable,
    if (buffer_type == loader_dri3_buffer_back) {
       draw->back_format = format;
 
-      buf_id = dri3_find_back(draw, draw->is_different_gpu);
+      buf_id = dri3_find_back(draw, draw->avoid_back_buffer_reuse);
 
       if (buf_id < 0)
          return NULL;
