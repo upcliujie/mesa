51361080df62ca1ba14efae8574c211e67b83793
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2021-09-16 13:11:30 +0200

    glsl: rework gl_ClipVertex / gl_CullDistance checks
    The GLSL spec says it's an error if a shader statically writes to these
    2 variables.
    
    Until this commit, Mesa refused to link a shader if it had an unused
    function writing to one of these variables while another (used) function
    wrote to the other.
    
    This commit changes Mesa's behavior and perform dead function elimination
    after the intra-stage linking step but before performing these checks.
    
    I'm not sure a write from an unused function should be qualified as a
    static write to this variable but:
    - the amdgpu-pro driver tolerates this and doesn't emit an error
    - Mesa already does something similar: if the write to one of these var
      is considered dead code, it gets removed before the check and
      doesn't cause an error
    
    This fixes one application.

--- src/compiler/glsl/linker.cpp
+++ src/compiler/glsl/linker.cpp
@@ -103,6 +103,8 @@ struct find_variable {
 
 /**
  * Visitor that determines whether or not a variable is ever written.
+ * Note: this is only considering if the variable is statically written
+ * (= regardless of the runtime flow of control)
  *
  * Use \ref find_assignments for convenience.
  */
@@ -622,6 +624,11 @@ analyze_clip_cull_usage(struct gl_shader_program *prog,
                         struct gl_context *ctx,
                         struct shader_info *info)
 {
+   /* Remove dead functions to avoid raising an error (eg: dead function
+    * writes to gl_ClipVertex, and main() writes to gl_ClipDistance).
+    */
+   do_dead_functions(shader->ir);
+
    info->clip_distance_array_size = 0;
    info->cull_distance_array_size = 0;
 
