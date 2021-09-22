45eb43d054f54caa95b5b792937bf09d4493873b
Jason Ekstrand <jason@jlekstrand.net>
2021-09-22 14:13:00 -0500

    vulkan: Add a common vk_render_pass struct
    This basically contains everything in pCreateInfo plus one or two extra
    bits that might be useful.

---
+++ src/vulkan/runtime/vk_render_pass.h
@@ -0,0 +1,112 @@
+/*
+ * Copyright © 2021 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#ifndef VK_RENDER_PASS_H
+#define VK_RENDER_PASS_H
+
+#include "vk_object.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct vk_subpass_attachment {
+   uint32_t attachment;
+   VkImageAspectFlags aspects;
+   VkImageUsageFlagBits usage;
+   VkImageLayout layout;
+   VkImageLayout stencil_layout;
+
+   struct vk_subpass_attachment *resolve;
+};
+
+struct vk_subpass {
+   uint32_t attachment_count;
+   struct vk_subpass_attachment *attachments;
+
+   uint32_t input_count;
+   struct vk_subpass_attachment *input_attachments;
+
+   uint32_t color_count;
+   struct vk_subpass_attachment *color_attachments;
+
+   uint32_t color_resolve_count;
+   struct vk_subpass_attachment *color_resolve_attachments;
+
+   struct vk_subpass_attachment *depth_stencil_attachment;
+   struct vk_subpass_attachment *depth_stencil_resolve_attachment;
+
+   /** VkSubpassDescription2::viewMask */
+   uint32_t view_mask;
+
+   VkResolveModeFlagBitsKHR depth_resolve_mode;
+   VkResolveModeFlagBitsKHR stencil_resolve_mode;
+};
+
+struct vk_render_pass_attachment {
+   VkFormat format;
+   uint32_t samples;
+
+   VkAttachmentLoadOp load_op;
+   VkAttachmentStoreOp store_op;
+   VkAttachmentLoadOp stencil_load_op;
+   VkAttachmentLoadOp stencil_store_op;
+
+   VkImageLayout initial_layout;
+   VkImageLayout final_layout;
+   VkImageLayout initial_stencil_layout;
+   VkImageLayout final_stencil_layout;
+};
+
+struct vk_subpass_dependency {
+   VkDependencyFlags flags;
+
+   uint32_t src_subpass;
+   uint32_t dst_subpass;
+   VkPipelineStageFlags2 src_stage_mask;
+   VkPipelineStageFlags2 dst_stage_mask;
+   VkAccessFlags2 src_access_mask;
+   VkAccessFlags2 dst_access_mask;
+   int32_t view_offset;
+};
+
+struct vk_render_pass {
+   struct vk_object_base base;
+
+   uint32_t attachment_count;
+   struct vk_render_pass_attachment *attachments;
+
+   uint32_t subpass_count;
+   struct vk_subpass *subpasses;
+
+   uint32_t dependency_count;
+   struct vk_subpass_dependency *dependencies;
+};
+
+VK_DEFINE_NONDISP_HANDLE_CASTS(vk_render_pass, base, VkRenderPass,
+                               VK_OBJECT_TYPE_RENDER_PASS)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VK_RENDER_PASS_H */
