fbf1d9c991c7a8ed11f88e9c31206f8c334e87d6
Jason Ekstrand <jason@jlekstrand.net>
2021-09-24 16:04:51 -0500

    vulkan/device: Use vk_error
    Tested-by: Boris Brezillon <boris.brezillon@collabora.com>

--- src/vulkan/util/vk_device.c
+++ src/vulkan/util/vk_device.c
@@ -25,6 +25,7 @@
 
 #include "vk_common_entrypoints.h"
 #include "vk_instance.h"
+#include "vk_log.h"
 #include "vk_physical_device.h"
 #include "vk_queue.h"
 #include "vk_util.h"
@@ -62,14 +63,14 @@ vk_device_init(struct vk_device *device,
       }
 
       if (idx >= VK_DEVICE_EXTENSION_COUNT)
-         return VK_ERROR_EXTENSION_NOT_PRESENT;
+         return vk_error(physical_device, VK_ERROR_EXTENSION_NOT_PRESENT);
 
       if (!physical_device->supported_extensions.extensions[idx])
-         return VK_ERROR_EXTENSION_NOT_PRESENT;
+         return vk_error(physical_device, VK_ERROR_EXTENSION_NOT_PRESENT);
 
 #ifdef ANDROID
       if (!vk_android_allowed_device_extensions.extensions[idx])
-         return VK_ERROR_EXTENSION_NOT_PRESENT;
+         return vk_error(physical_device, VK_ERROR_EXTENSION_NOT_PRESENT);
 #endif
 
       device->enabled_extensions.extensions[idx] = true;
