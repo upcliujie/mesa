Grima04/mesa-turnip-kgsl

commit c4f2297f007202ce868b7f5297c7418fe88ad9bb

spirv: Recognize zero initializers in Workgroup variables
This will be used to implement
VK_KHR_zero_initialize_workgroup_memory.

Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8708>

src/compiler/spirv/spirv_to_nir.c
@@ -2167,6 +2167,7 @@ vtn_handle_constant(struct vtn_builder *b, SpvOp opcode,
 
    case SpvOpConstantNull:
       val->constant = vtn_null_constant(b, val->type);
+      val->is_null_constant = true;
       break;
 
    default:

src/compiler/shader_info.h
@@ -368,6 +368,8 @@ typedef struct shader_info {
           */
          enum gl_derivative_group derivative_group:2;
 
+         bool zero_initialize_shared_memory;
+
          /**
           * Size of shared variables accessed by the compute shader.
           */

src/compiler/spirv/vtn_private.h
@@ -603,6 +603,9 @@ struct vtn_value {
     * the existence of a NonUniform decoration on this value.*/
    uint32_t propagated_non_uniform : 1;
 
+   /* Valid for vtn_value_type_constant to indicate the value is OpConstantNull. */
+   bool is_null_constant:1;
+
    const char *name;
    struct vtn_decoration *decoration;
    struct vtn_type *type;

src/compiler/spirv/vtn_variables.c
@@ -1977,6 +1977,26 @@ vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
    }
 
    if (initializer) {
+      switch (var->mode) {
+      case vtn_variable_mode_workgroup:
+         /* VK_KHR_zero_initialize_workgroup_memory. */
+         vtn_fail_if(b->options->environment != NIR_SPIRV_VULKAN,
+                     "Only Vulkan supports variable initializer "
+                     "for Workgroup variable %u",
+                     vtn_id_for_value(b, val));
+         vtn_fail_if(initializer->value_type != vtn_value_type_constant ||
+                     !initializer->is_null_constant,
+                     "Workgroup variable %u can only have OpConstantNull "
+                     "as initializer, but have %u instead",
+                     vtn_id_for_value(b, val),
+                     vtn_id_for_value(b, initializer));
+         b->shader->info.cs.zero_initialize_shared_memory = true;
+         break;
+      default:
+         /* Nothing to check. */
+         break;
+      }
+
       switch (initializer->value_type) {
       case vtn_value_type_constant:
          var->var->constant_initializer =

upcliujie/mesa

commit 708e3badb5a3363a9e58765541a657724fd7fbdf

spirv: propagate is_null_constant through ConstantComposite
If all members are null, then the whole composite can be considered
null as well.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: c4f2297f007202 ("spirv: Recognize zero initializers in Workgroup variables")

src/compiler/spirv/spirv_to_nir.c
@@ -1964,17 +1964,21 @@ vtn_handle_constant(struct vtn_builder *b, SpvOp opcode,
                   spirv_op_to_string(opcode), elem_count, val->type->length);
 
       nir_constant **elems = ralloc_array(b, nir_constant *, elem_count);
+      val->is_null_constant = true;
       for (unsigned i = 0; i < elem_count; i++) {
          struct vtn_value *eval = vtn_untyped_value(b, w[i + 3]);
 
          if (eval->value_type == vtn_value_type_constant) {
             elems[i] = eval->constant;
+            val->is_null_constant = val->is_null_constant &&
+                                    eval->is_null_constant;
          } else {
             vtn_fail_if(eval->value_type != vtn_value_type_undef,
                         "only constants or undefs allowed for "
                         "SpvOpConstantComposite");
             /* to make it easier, just insert a NULL constant for now */
             elems[i] = vtn_null_constant(b, eval->type);
+            val->is_null_constant = false;
          }
       }
 