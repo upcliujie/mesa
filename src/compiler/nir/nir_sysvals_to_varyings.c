db5bcac0d199df3870c2f932cf56b4b031d2ff19
Boris Brezillon <boris.brezillon@collabora.com>
2021-09-27 14:20:20 +0200

    nir: Add a nir_sysvals_to_varyings() helper
    Allow backends to turn some sysvals into input varyings so the frontend
    (in our case spirv_to_nir()) doesn't have to bother selecting which
    one is expected.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

---
+++ src/compiler/nir/nir_sysvals_to_varyings.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2021 Collabora Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "nir.h"
+
+void
+nir_sysvals_to_varyings(nir_shader *shader,
+                        const struct nir_sysvals_to_varyings_options *options)
+{
+   nir_foreach_variable_with_modes(var, shader, nir_var_system_value) {
+      switch (var->data.location) {
+      case SYSTEM_VALUE_FRAG_COORD:
+         if (options->frag_coord) {
+            var->data.mode = nir_var_shader_in;
+            var->data.location = VARYING_SLOT_POS;
+         }
+         break;
+      case SYSTEM_VALUE_FRONT_FACE:
+         if (options->front_face) {
+            var->data.mode = nir_var_shader_in;
+            var->data.location = VARYING_SLOT_FACE;
+         }
+         break;
+      case SYSTEM_VALUE_POINT_COORD:
+         if (options->point_coord) {
+            var->data.mode = nir_var_shader_in;
+            var->data.location = VARYING_SLOT_PNTC;
+         }
+         break;
+      default:
+         break;
+      }
+   }
+}
