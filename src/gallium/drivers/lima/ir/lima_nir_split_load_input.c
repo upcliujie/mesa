Grima04/mesa-turnip-kgsl

commit 6dd0ad66dedc4cbd79574053f0b5023172935657

lima/ppir: add NIR pass to split varying loads
NIR may emit a single instrinsic to load several packed varyings,
but that's suboptimal for Utgard PP for several reasons:
- varyings that are used as sampler inputs can be passed using
  pipeline register with increased precision
- we have small number of regs, so using a vec4 regs for storing
  two vec2 varyings increases reg pressure.

Add NIR pass to split a single load into several loads and utilize
it in lima.

Reviewed-by: Qiang Yu <yuq825@gmail.com>
Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

src/gallium/drivers/lima/ir/lima_nir_split_load_input.c
@@ -1,0 +1,123 @@
+/*
+ * Copyright © 2019 Vasily Khoruzhick <anarsoul@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "nir.h"
+#include "nir_builder.h"
+
+#include "lima_ir.h"
+
+static bool
+lima_nir_split_load_input_block(nir_block *block, nir_builder *b)
+{
+   bool progress = false;
+
+   nir_foreach_instr_safe(instr, block) {
+      if (instr->type != nir_instr_type_alu)
+         continue;
+
+      nir_alu_instr *alu = nir_instr_as_alu(instr);
+      if (alu->op != nir_op_mov)
+         continue;
+
+      if (!alu->dest.dest.is_ssa)
+         continue;
+
+      if (!alu->src[0].src.is_ssa)
+         continue;
+
+      nir_ssa_def *ssa = alu->src[0].src.ssa;
+      if (ssa->parent_instr->type != nir_instr_type_intrinsic)
+         continue;
+
+      nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(ssa->parent_instr);
+      if (intrin->intrinsic != nir_intrinsic_load_input)
+         continue;
+
+      uint8_t swizzle = alu->src[0].swizzle[0];
+      int i;
+
+      for (i = 1; i < nir_dest_num_components(alu->dest.dest); i++)
+         if (alu->src[0].swizzle[i] != (swizzle + i))
+            break;
+
+      if (i != nir_dest_num_components(alu->dest.dest))
+         continue;
+
+      b->cursor = nir_before_instr(&intrin->instr);
+      nir_intrinsic_instr *new_intrin = nir_intrinsic_instr_create(
+                                             b->shader,
+                                             intrin->intrinsic);
+      nir_ssa_dest_init(&new_intrin->instr, &new_intrin->dest,
+                        nir_dest_num_components(alu->dest.dest),
+                        ssa->bit_size,
+                        NULL);
+      new_intrin->num_components = nir_dest_num_components(alu->dest.dest);
+      nir_intrinsic_set_base(new_intrin, nir_intrinsic_base(intrin));
+      nir_intrinsic_set_component(new_intrin, nir_intrinsic_component(intrin) + swizzle);
+      nir_intrinsic_set_type(new_intrin, nir_intrinsic_type(intrin));
+
+      /* offset */
+      nir_src_copy(&new_intrin->src[0], &intrin->src[0], new_intrin);
+
+      nir_builder_instr_insert(b, &new_intrin->instr);
+      nir_ssa_def_rewrite_uses(&alu->dest.dest.ssa,
+                               nir_src_for_ssa(&new_intrin->dest.ssa));
+      nir_instr_remove(&alu->instr);
+      progress = true;
+   }
+
+   return progress;
+}
+
+static bool
+lima_nir_split_load_input_impl(nir_function_impl *impl)
+{
+   bool progress = false;
+   nir_builder builder;
+   nir_builder_init(&builder, impl);
+
+   nir_foreach_block(block, impl) {
+      progress |= lima_nir_split_load_input_block(block, &builder);
+   }
+
+   nir_metadata_preserve(impl, nir_metadata_block_index |
+                               nir_metadata_dominance);
+   return progress;
+}
+
+/* Replaces a single load of several packed varyings and number of movs with
+ * a number of loads of smaller size
+ */
+bool
+lima_nir_split_load_input(nir_shader *shader)
+{
+   bool progress = false;
+
+   nir_foreach_function(function, shader) {
+      if (function->impl)
+         progress |= lima_nir_split_load_input_impl(function->impl);
+   }
+
+   return progress;
+}
+

src/gallium/drivers/lima/Android.mk
@@ -36,6 +36,7 @@ LOCAL_SRC_FILES := \
 	ir/gp/scheduler.c \
 	ir/lima_ir.h \
 	ir/lima_nir_lower_uniform_to_scalar.c \
+	ir/lima_nir_split_load_input.c \
 	ir/pp/codegen.c \
 	ir/pp/codegen.h \
 	ir/pp/disasm.c \

src/gallium/drivers/lima/ir/lima_ir.h
@@ -65,5 +65,6 @@ struct ra_regs *ppir_regalloc_init(void *mem_ctx);
 
 void lima_nir_lower_uniform_to_scalar(nir_shader *shader);
 bool lima_nir_scale_trig(nir_shader *shader);
+bool lima_nir_split_load_input(nir_shader *shader);
 
 #endif

src/gallium/drivers/lima/lima_program.c
@@ -224,6 +224,7 @@ lima_program_optimize_fs_nir(struct nir_shader *s)
                nir_var_shader_in |
                nir_var_shader_out |
                nir_var_function_temp);
+      NIR_PASS(progress, s, lima_nir_split_load_input);
    } while (progress);
 
    NIR_PASS_V(s, nir_lower_int_to_float);

src/gallium/drivers/lima/meson.build
@@ -46,6 +46,7 @@ files_lima = files(
   'ir/pp/disasm.c',
 
   'ir/lima_nir_lower_uniform_to_scalar.c',
+  'ir/lima_nir_split_load_input.c',
 
   'ir/lima_ir.h',
 

upcliujie/mesa

commit e2252fd44884e6fb362cac67b83b83aa218897bf

lima: split_load_input: don't split unaligned vec2
Mali4x0 can't fetch unaligned vec2 (i.e. .yz), so don't split it.

Fixes: 6dd0ad66dedc ("lima/ppir: add NIR pass to split varying loads")
Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

src/gallium/drivers/lima/ir/lima_nir_split_load_input.c
@@ -67,6 +67,11 @@ lima_nir_split_load_input_block(nir_block *block, nir_builder *b)
       if (nir_dest_num_components(alu->dest.dest) == 3 && swizzle > 0)
          continue;
 
+      /* mali4xx can't access unaligned vec2, don't split load input */
+      if (nir_dest_num_components(alu->dest.dest) == 2 &&
+          swizzle != 0 && swizzle != 2)
+         continue;
+
       b->cursor = nir_before_instr(&intrin->instr);
       nir_intrinsic_instr *new_intrin = nir_intrinsic_instr_create(
                                              b->shader,