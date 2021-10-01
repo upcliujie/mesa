269be0b91aa03d04887422824443bbcfa3e63a99
Emma Anholt <emma@anholt.net>
2021-09-30 21:12:00 -0700

    i915g: Make sure we consider negates/swizzles on shadow compare values.
    The num_coords arg is "how many of xyzw do we need to check the
    negates/swizzles on to see if we need to move them to a temp?"
    
    Caught by imirkin while debugging #4986.

--- src/gallium/drivers/i915/i915_fpc_translate.c
+++ src/gallium/drivers/i915/i915_fpc_translate.c
@@ -333,12 +333,12 @@ i915_num_coords(uint32_t tex)
    case TGSI_TEXTURE_1D:
       return 1;
 
-   case TGSI_TEXTURE_SHADOW2D:
    case TGSI_TEXTURE_2D:
-   case TGSI_TEXTURE_SHADOWRECT:
    case TGSI_TEXTURE_RECT:
       return 2;
 
+   case TGSI_TEXTURE_SHADOW2D:
+   case TGSI_TEXTURE_SHADOWRECT:
    case TGSI_TEXTURE_3D:
    case TGSI_TEXTURE_CUBE:
       return 3;
