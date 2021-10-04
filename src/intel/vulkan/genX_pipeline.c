719a2ba7cd12e34ca07e3866b7bfe40e3620ddad
Jason Ekstrand <jason@jlekstrand.net>
2021-10-04 13:38:19 -0500

    anv: Switch to the new common pipeline cache
    This patch is intended to be somewhat minimal.  There's a lot of cleanup
    work that can be done but we'll leave that to later patches.

--- src/intel/vulkan/genX_pipeline.c
+++ src/intel/vulkan/genX_pipeline.c
@@ -2746,7 +2746,7 @@ emit_mesh_state(struct anv_graphics_pipeline *pipeline)
 static VkResult
 genX(graphics_pipeline_create)(
     VkDevice                                    _device,
-    struct anv_pipeline_cache *                 cache,
+    struct vk_pipeline_cache *                  cache,
     const VkGraphicsPipelineCreateInfo*         pCreateInfo,
     const VkAllocationCallbacks*                pAllocator,
     VkPipeline*                                 pPipeline)
@@ -2758,8 +2758,8 @@ genX(graphics_pipeline_create)(
    assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO);
 
    /* Use the default pipeline cache if none is specified */
-   if (cache == NULL && device->physical->instance->pipeline_cache_enabled)
-      cache = &device->default_pipeline_cache;
+   if (cache == NULL)
+      cache = device->default_pipeline_cache;
 
    pipeline = vk_zalloc2(&device->vk.alloc, pAllocator, sizeof(*pipeline), 8,
                          VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
@@ -3034,7 +3034,7 @@ emit_compute_state(struct anv_compute_pipeline *pipeline,
 static VkResult
 compute_pipeline_create(
     VkDevice                                    _device,
-    struct anv_pipeline_cache *                 cache,
+    struct vk_pipeline_cache *                  cache,
     const VkComputePipelineCreateInfo*          pCreateInfo,
     const VkAllocationCallbacks*                pAllocator,
     VkPipeline*                                 pPipeline)
@@ -3046,8 +3046,8 @@ compute_pipeline_create(
    assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO);
 
    /* Use the default pipeline cache if none is specified */
-   if (cache == NULL && device->physical->instance->pipeline_cache_enabled)
-      cache = &device->default_pipeline_cache;
+   if (cache == NULL)
+      cache = device->default_pipeline_cache;
 
    pipeline = vk_zalloc2(&device->vk.alloc, pAllocator, sizeof(*pipeline), 8,
                          VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
@@ -3093,7 +3093,7 @@ VkResult genX(CreateGraphicsPipelines)(
     const VkAllocationCallbacks*                pAllocator,
     VkPipeline*                                 pPipelines)
 {
-   ANV_FROM_HANDLE(anv_pipeline_cache, pipeline_cache, pipelineCache);
+   VK_FROM_HANDLE(vk_pipeline_cache, pipeline_cache, pipelineCache);
 
    VkResult result = VK_SUCCESS;
 
@@ -3132,7 +3132,7 @@ VkResult genX(CreateComputePipelines)(
     const VkAllocationCallbacks*                pAllocator,
     VkPipeline*                                 pPipelines)
 {
-   ANV_FROM_HANDLE(anv_pipeline_cache, pipeline_cache, pipelineCache);
+   VK_FROM_HANDLE(vk_pipeline_cache, pipeline_cache, pipelineCache);
 
    VkResult result = VK_SUCCESS;
 
@@ -3180,7 +3180,7 @@ assert_rt_stage_index_valid(const VkRayTracingPipelineCreateInfoKHR* pCreateInfo
 static VkResult
 ray_tracing_pipeline_create(
     VkDevice                                    _device,
-    struct anv_pipeline_cache *                 cache,
+    struct vk_pipeline_cache *                  cache,
     const VkRayTracingPipelineCreateInfoKHR*    pCreateInfo,
     const VkAllocationCallbacks*                pAllocator,
     VkPipeline*                                 pPipeline)
@@ -3191,8 +3191,8 @@ ray_tracing_pipeline_create(
    assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_RAY_TRACING_PIPELINE_CREATE_INFO_KHR);
 
    /* Use the default pipeline cache if none is specified */
-   if (cache == NULL && device->physical->instance->pipeline_cache_enabled)
-      cache = &device->default_pipeline_cache;
+   if (cache == NULL)
+      cache = device->default_pipeline_cache;
 
    VK_MULTIALLOC(ma);
    VK_MULTIALLOC_DECL(&ma, struct anv_ray_tracing_pipeline, pipeline, 1);
@@ -3316,7 +3316,7 @@ genX(CreateRayTracingPipelinesKHR)(
     const VkAllocationCallbacks*                pAllocator,
     VkPipeline*                                 pPipelines)
 {
-   ANV_FROM_HANDLE(anv_pipeline_cache, pipeline_cache, pipelineCache);
+   VK_FROM_HANDLE(vk_pipeline_cache, pipeline_cache, pipelineCache);
 
    VkResult result = VK_SUCCESS;
 
