aerisarn/mesa-uwp

commit 5116388e0ba98f080de91e37249e195421e78c0b

turnip: Expose a device name similar to the blob.
We add "Turnip" so that users (and vulkan.gpuinfo.org) can distinguish us
without requiring VK_KHR_driver_properties.  This will be a lot more
user-friendly than "FD618", though.

I made some little vk_asprintf helpers, because I figure other drivers
setting up deviceName's will want them too.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13041>

src/freedreno/vulkan/tu_device.c
@@ -202,7 +202,16 @@ tu_physical_device_init(struct tu_physical_device *device,
 {
    VkResult result = VK_SUCCESS;
 
-   device->name = fd_dev_name(&device->dev_id);
+   const char *fd_name = fd_dev_name(&device->dev_id);
+   if (strncmp(fd_name, "FD", 2) == 0) {
+      device->name = vk_asprintf(&instance->vk.alloc,
+                                 VK_SYSTEM_ALLOCATION_SCOPE_INSTANCE,
+                                 "Turnip Adreno (TM) %s", &fd_name[2]);
+   } else {
+      device->name = vk_strdup(&instance->vk.alloc, fd_name,
+                               VK_SYSTEM_ALLOCATION_SCOPE_INSTANCE);
+
+   }
 
    const struct fd_dev_info *info = fd_dev_info(&device->dev_id);
    if (!info) {
@@ -276,6 +285,8 @@ tu_physical_device_finish(struct tu_physical_device *device)
    close(device->local_fd);
    if (device->master_fd != -1)
       close(device->master_fd);
+
+   vk_free(&device->instance->vk.alloc, (void *)device->name);
 
    vk_physical_device_finish(&device->vk);
 }

src/freedreno/ci/gitlab-ci.yml
@@ -24,6 +24,7 @@
   variables:
     DEQP_VER: vk
     VK_DRIVER: freedreno
+    DEQP_EXPECTED_RENDERER: "Turnip Adreno (TM) 630"
     MESA_VK_IGNORE_CONFORMANCE_WARNING: 1
 
 .freedreno-test-traces:
@@ -163,8 +164,8 @@ a630_gles_asan:
 
 a630_vk:
   extends:
-    - .baremetal-deqp-test-freedreno-vk
     - .a630-test
+    - .baremetal-deqp-test-freedreno-vk
   parallel: 3
   variables:
     DEQP_VER: vk
@@ -183,8 +184,8 @@ a630_vk_full:
 # Clicking play can show you some useful areas for fixing turnip, though.
 a630_vk_asan:
   extends:
-    - .baremetal-deqp-test-freedreno-vk
     - .a630-test
+    - .baremetal-deqp-test-freedreno-vk
     - .baremetal-arm64-asan-test
     - .test-manual
   variables:

upcliujie/mesa

commit bdb98baa0d6b973162f41f05bffe9402787bd7fd

turnip: Fix allocation failure handling around device->name.
Fixes regressions in dEQP-VK.api.device_init.create_instance_device_intentional_alloc_fail

Fixes: 5116388e0ba9 ("turnip: Expose a device name similar to the blob.")

src/freedreno/vulkan/tu_device.c
@@ -212,12 +212,16 @@ tu_physical_device_init(struct tu_physical_device *device,
                                VK_SYSTEM_ALLOCATION_SCOPE_INSTANCE);
 
    }
+   if (!device->name) {
+      return vk_startup_errorf(instance, VK_ERROR_OUT_OF_HOST_MEMORY,
+                               "device name alloc fail");
+   }
 
    const struct fd_dev_info *info = fd_dev_info(&device->dev_id);
    if (!info) {
       result = vk_startup_errorf(instance, VK_ERROR_INITIALIZATION_FAILED,
                                  "device %s is unsupported", device->name);
-      return result;
+      goto fail;
    }
    switch (fd_dev_gen(&device->dev_id)) {
    case 6:
@@ -229,12 +233,12 @@ tu_physical_device_init(struct tu_physical_device *device,
    default:
       result = vk_startup_errorf(instance, VK_ERROR_INITIALIZATION_FAILED,
                                  "device %s is unsupported", device->name);
-      return result;
+      goto fail;
    }
    if (tu_device_get_cache_uuid(fd_dev_gpu_id(&device->dev_id), device->cache_uuid)) {
       result = vk_startup_errorf(instance, VK_ERROR_INITIALIZATION_FAILED,
                                  "cannot generate UUID");
-      return result;
+      goto fail;
    }
 
    /* The gpu id is already embedded in the uuid so we just pass "tu"
@@ -260,18 +264,22 @@ tu_physical_device_init(struct tu_physical_device *device,
                                     &supported_extensions,
                                     &dispatch_table);
    if (result != VK_SUCCESS)
-      return result;
+      goto fail;
 
 #if TU_HAS_SURFACE
    result = tu_wsi_init(device);
    if (result != VK_SUCCESS) {
       vk_startup_errorf(instance, result, "WSI init failure");
       vk_physical_device_finish(&device->vk);
-      return result;
+      goto fail;
    }
 #endif
 
    return VK_SUCCESS;
+
+fail:
+   vk_free(&instance->vk.alloc, (void *)device->name);
+   return result;
 }
 
 static void