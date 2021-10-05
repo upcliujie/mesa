ec4b70a669397aef1b1a438ce664d26906d94873
Chia-I Wu <olvaffe@gmail.com>
2021-10-04 18:01:06 -0700

    vulkan/wsi/wayland: fix an invalid u_vector_init call
    u_vector_init requires size to be power-of-two.
    
    Fixes: 151b65b2119 ("vulkan/wsi/wayland: generalize modifier handling")
    Signed-off-by: Chia-I Wu <olvaffe@gmail.com>

--- src/vulkan/wsi/wsi_common_wayland.c
+++ src/vulkan/wsi/wsi_common_wayland.c
@@ -455,8 +455,10 @@ wsi_wl_display_init(struct wsi_wayland *wsi_wl,
    VkResult result = VK_SUCCESS;
    memset(display, 0, sizeof(*display));
 
+   const size_t size_pow2 =
+      util_next_power_of_two(sizeof(struct wsi_wl_format));
    if (!u_vector_init(&display->formats, sizeof(struct wsi_wl_format),
-                      8 * sizeof(struct wsi_wl_format)))
+                      8 * size_pow2))
       return VK_ERROR_OUT_OF_HOST_MEMORY;
 
    display->wsi_wl = wsi_wl;
