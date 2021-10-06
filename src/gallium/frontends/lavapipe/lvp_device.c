aerisarn/mesa-uwp

commit 028591954a2d7e1c3cf8ecec04f1555564dd8076

lvp/fence: quick fix to previous commit.
This fixes last of xcb cts issues.

Fixes: 8a294b6f975e1 ("lavapipe: Fix vkWaitForFences for initially-signalled fences")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13212>

src/gallium/frontends/lavapipe/lvp_device.c
@@ -2089,7 +2089,7 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_WaitForFences(
          struct lvp_fence *f = lvp_fence_from_handle(pFences[i]);
 
          /* this is an unsubmitted fence: immediately bail out */
-         if (!f->timeline && !f->signalled)
+         if (!f->timeline && !f->signalled && !f->handle)
             return VK_TIMEOUT;
          if (!fence || f->timeline > fence->timeline)
             fence = f;

upcliujie/mesa

commit 4b027d8fb5e0a090522af4a4a50d0f774a3d47d5

lavapipe: fix fence handling around wsi submission
My previous fix was incorrect, properly fix things so that
fences in acquire get a proper timeline set.

Fixes: 028591954a2d7 ("lvp/fence: quick fix to previous commit.")

src/gallium/frontends/lavapipe/lvp_device.c
@@ -2089,7 +2089,7 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_WaitForFences(
          struct lvp_fence *f = lvp_fence_from_handle(pFences[i]);
 
          /* this is an unsubmitted fence: immediately bail out */
-         if (!f->timeline && !f->signalled && !f->handle)
+         if (!f->timeline && !f->signalled)
             return VK_TIMEOUT;
          if (!fence || f->timeline > fence->timeline)
             fence = f;

src/gallium/frontends/lavapipe/lvp_wsi.c
@@ -240,6 +240,7 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_AcquireNextImage2KHR(
    LVP_FROM_HANDLE(lvp_fence, fence, pAcquireInfo->fence);
 
    if (fence && (result == VK_SUCCESS || result == VK_SUBOPTIMAL_KHR)) {
+      fence->timeline = p_atomic_inc_return(&device->queue.timeline);
       util_queue_add_job(&device->queue.queue, fence, &fence->fence, queue_thread_noop, NULL, 0);
    }
    return result;