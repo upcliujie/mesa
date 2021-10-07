1cb8f0d8ded7fb21a81d8ed1e46d15f300415f67
Yiwei Zhang <zzyiwei@chromium.org>
2021-10-07 18:46:49 +0000

    android_stub: update platform headers to include atrace
    1. Add tracing headers
    2. Update to reflect relocated headers
    3. Remove redundant apex/window.h
    
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>

--- src/android_stub/cutils_stub.cpp
+++ src/android_stub/cutils_stub.cpp
@@ -1,10 +1,76 @@
 #include <cutils/properties.h>
+#include <cutils/trace.h>
 
 extern "C" {
 
-int property_get(const char* key, char* value, const char* default_value)
+atomic_bool atrace_is_ready = ATOMIC_VAR_INIT(true);
+int atrace_marker_fd = -1;
+uint64_t atrace_enabled_tags = 0;
+
+void
+atrace_set_debuggable(bool /*debuggable*/)
+{
+}
+
+void
+atrace_set_tracing_enabled(bool /*enabled*/)
+{
+}
+
+void
+atrace_update_tags()
+{
+}
+
+void
+atrace_setup()
+{
+}
+
+void
+atrace_begin_body(const char * /*name*/)
+{
+}
+
+void
+atrace_end_body()
+{
+}
+
+void
+atrace_async_begin_body(const char * /*name*/, int32_t /*cookie*/)
+{
+}
+
+void
+atrace_async_end_body(const char * /*name*/, int32_t /*cookie*/)
+{
+}
+
+void
+atrace_int_body(const char * /*name*/, int32_t /*value*/)
+{
+}
+
+void
+atrace_int64_body(const char * /*name*/, int64_t /*value*/)
+{
+}
+
+void
+atrace_init()
+{
+}
+
+uint64_t
+atrace_get_enabled_tags()
+{
+   return ATRACE_TAG_NOT_READY;
+}
+
+int
+property_get(const char *key, char *value, const char *default_value)
 {
    return 0;
 }
-
 }
