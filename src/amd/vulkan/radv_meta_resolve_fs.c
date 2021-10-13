45c60692dd9314fe54be623b555a6cbfa69d8ad9
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2021-10-13 02:52:11 +0200

    radv: Add hooks after in-renderpass meta operations.
    So that we can mark the rendertargets dirty.

--- src/amd/vulkan/radv_meta_resolve_fs.c
+++ src/amd/vulkan/radv_meta_resolve_fs.c
@@ -1108,7 +1108,7 @@ radv_cmd_buffer_resolve_subpass_fs(struct radv_cmd_buffer *cmd_buffer)
                    &(VkExtent2D){fb->width, fb->height});
    }
 
-   radv_cmd_buffer_set_subpass(cmd_buffer, subpass);
+   radv_cmd_buffer_restore_subpass(cmd_buffer, subpass);
 
    radv_meta_restore(&saved_state, cmd_buffer);
 }
