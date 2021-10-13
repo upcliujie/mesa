Grima04/mesa-turnip-kgsl

commit 98c622a96e28020640517cb5499a1d7bd56553b0

vulkan: Update dispatch table gen for Windows
Update entry null test to deal with MSVC not supporting weak functions.

Also fix warnings for returning from void functions.

Acked-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9121>

src/vulkan/util/vk_dispatch_table_gen.py
@@ -446,7 +446,12 @@ void vk_${type}_dispatch_table_from_entrypoints(
     if (overwrite) {
         memset(dispatch_table, 0, sizeof(*dispatch_table));
         for (unsigned i = 0; i < ARRAY_SIZE(${type}_compaction_table); i++) {
+#ifdef _MSC_VER
+            const uintptr_t zero = 0;
+            if (entry[i] == NULL || memcmp(entry[i], &zero, sizeof(zero)) == 0)
+#else
             if (entry[i] == NULL)
+#endif
                 continue;
             unsigned disp_index = ${type}_compaction_table[i];
             assert(disp[disp_index] == NULL);
@@ -559,7 +564,11 @@ ${e.prefixed_name('vk_tramp')}(${e.decl_params()})
 {
     <% assert e.params[0].type == 'VkPhysicalDevice' %>
     VK_FROM_HANDLE(vk_physical_device, vk_physical_device, ${e.params[0].name});
+  % if e.return_type == 'void':
+    vk_physical_device->dispatch_table.${e.name}(${e.call_params()});
+  % else:
     return vk_physical_device->dispatch_table.${e.name}(${e.call_params()});
+  % endif
 }
   % if e.guard is not None:
 #endif
@@ -593,10 +602,18 @@ ${e.prefixed_name('vk_tramp')}(${e.decl_params()})
 {
   % if e.params[0].type == 'VkDevice':
     VK_FROM_HANDLE(vk_device, vk_device, ${e.params[0].name});
+    % if e.return_type == 'void':
+    vk_device->dispatch_table.${e.name}(${e.call_params()});
+    % else:
     return vk_device->dispatch_table.${e.name}(${e.call_params()});
+    % endif
   % elif e.params[0].type in ('VkCommandBuffer', 'VkQueue'):
     struct vk_object_base *vk_object = (struct vk_object_base *)${e.params[0].name};
+    % if e.return_type == 'void':
+    vk_object->device->dispatch_table.${e.name}(${e.call_params()});
+    % else:
     return vk_object->device->dispatch_table.${e.name}(${e.call_params()});
+    % endif
   % else:
     assert(!"Unhandled device child trampoline case: ${e.params[0].type}");
   % endif

upcliujie/mesa

commit 5211d1493bdbcc2a2dac1b5b53a5402ba2d894a3

vulkan: Fix weak symbol emulation when compiling with MSVC
Mapping unimplemented entrypoints to a global function pointer variable
initialized to NULL is a bit cumbersome, and actually led to a bug
in the vk_xxx_dispatch_table_from_entrypoints() template: the !override
case didn't have the right check on the source table entries. Instead of
fixing that case, let's simplify the logic by creating a stub function
and making the alternatename pragma point to this stub. This way we get
rid of all those uneeded xxx_Null symbols/variables and simplify the
tests in vk_xxxx_dispatch_table_from_entrypoints().

Cc: mesa-stable
Fixes: 98c622a96e28 ("vulkan: Update dispatch table gen for Windows")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>

src/vulkan/util/vk_dispatch_table_gen.py
@@ -464,6 +464,10 @@ vk_device_entrypoint_is_enabled(int index, uint32_t core_version,
    }
 }
 
+#ifdef _MSC_VER
+void vk_entrypoint_stub(void);
+#endif
+
 <%def name="dispatch_table_from_entrypoints(type)">
 void vk_${type}_dispatch_table_from_entrypoints(
     struct vk_${type}_dispatch_table *dispatch_table,
@@ -477,8 +481,7 @@ void vk_${type}_dispatch_table_from_entrypoints(
         memset(dispatch_table, 0, sizeof(*dispatch_table));
         for (unsigned i = 0; i < ARRAY_SIZE(${type}_compaction_table); i++) {
 #ifdef _MSC_VER
-            const uintptr_t zero = 0;
-            if (entry[i] == NULL || memcmp(entry[i], &zero, sizeof(zero)) == 0)
+            if (entry[i] == vk_entrypoint_stub)
 #else
             if (entry[i] == NULL)
 #endif
@@ -490,7 +493,11 @@ void vk_${type}_dispatch_table_from_entrypoints(
     } else {
         for (unsigned i = 0; i < ARRAY_SIZE(${type}_compaction_table); i++) {
             unsigned disp_index = ${type}_compaction_table[i];
+#ifdef _MSC_VER
+            if (disp[disp_index] == NULL && entry[i] != vk_entrypoint_stub)
+#else
             if (disp[disp_index] == NULL)
+#endif
                 disp[disp_index] = entry[i];
         }
     }

src/vulkan/util/vk_entrypoints_gen.py
@@ -123,13 +123,12 @@ TEMPLATE_C = Template(COPYRIGHT + """
     % endif
     % for p in prefixes:
 #ifdef _MSC_VER
-    ${e.return_type} (*${p}_${e.name}_Null)(${e.decl_params()}) = 0;
 #ifdef _M_IX86
       % for args_size in [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 60, 104]:
-    #pragma comment(linker, "/alternatename:_${p}_${e.name}@${args_size}=_${p}_${e.name}_Null")
+    #pragma comment(linker, "/alternatename:_${p}_${e.name}@${args_size}=_vk_entrypoint_stub")
       % endfor
 #else
-    #pragma comment(linker, "/alternatename:${p}_${e.name}=${p}_${e.name}_Null")
+    #pragma comment(linker, "/alternatename:${p}_${e.name}=vk_entrypoint_stub")
 #endif
 #else
     VKAPI_ATTR ${e.return_type} VKAPI_CALL ${p}_${e.name}(${e.decl_params()}) __attribute__ ((weak));

src/vulkan/util/vk_util.c
@@ -31,6 +31,13 @@
 
 #include "compiler/spirv/nir_spirv.h"
 
+#ifdef _MSC_VER
+void vk_entrypoint_stub(void)
+{
+   unreachable(!"Entrypoint not implemented\n");
+}
+#endif
+
 uint32_t vk_get_driver_version(void)
 {
    const char *minor_string = strchr(PACKAGE_VERSION, '.');