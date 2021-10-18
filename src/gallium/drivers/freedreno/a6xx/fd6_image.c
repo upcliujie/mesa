3a49f3d830f6a872d7c1515a6547bedc25816f87
Emma Anholt <emma@anholt.net>
2021-10-18 15:18:14 -0700

    freedreno/a6xx: Set up ssbo/image tex/ibo descriptors in set_shader_*().
    There's no CSO state to hang these off of, but this seems like the right
    place to compute these.  It means that we don't have to redo this work
    when changing other textures or dispatching another CS.

--- src/gallium/drivers/freedreno/a6xx/fd6_image.c
+++ src/gallium/drivers/freedreno/a6xx/fd6_image.c
@@ -330,12 +330,48 @@ fd6_build_ibo_state(struct fd_context *ctx, const struct ir3_shader_variant *v,
 }
 
 static void
+fd6_set_shader_buffers(struct pipe_context *pctx, enum pipe_shader_type shader,
+                       unsigned start, unsigned count,
+                       const struct pipe_shader_buffer *buffers,
+                       unsigned writable_bitmask) in_dt
+{
+   struct fd_context *ctx = fd_context(pctx);
+   struct fd6_context *fd6_ctx = fd6_context(ctx);
+
+   fd_set_shader_buffers(pctx, shader, start, count, buffers, writable_bitmask);
+
+   if (!buffers)
+      return;
+
+   for (unsigned i = 0; i < count; i++) {
+      unsigned n = i + start;
+      const struct pipe_shader_buffer *buf = &buffers[i];
+      uint32_t *descriptor = fd6_ctx->ssbo_descriptors[shader][n];
+
+      if (!buf->buffer)
+         continue;
+
+      static const uint8_t swiz[4] = {PIPE_SWIZZLE_X, PIPE_SWIZZLE_Y,
+                                      PIPE_SWIZZLE_Z, PIPE_SWIZZLE_W};
+
+      fdl6_buffer_view_init(descriptor,
+                            ctx->screen->info->a6xx.storage_16bit
+                               ? PIPE_FORMAT_R16_UINT
+                               : PIPE_FORMAT_R32_UINT,
+                            swiz,
+                            buf->buffer_offset, /* Using relocs for addresses */
+                            buf->buffer_size);
+   }
+}
+
+static void
 fd6_set_shader_images(struct pipe_context *pctx, enum pipe_shader_type shader,
                       unsigned start, unsigned count,
                       unsigned unbind_num_trailing_slots,
                       const struct pipe_image_view *images) in_dt
 {
    struct fd_context *ctx = fd_context(pctx);
+   struct fd6_context *fd6_ctx = fd6_context(ctx);
    struct fd_shaderimg_stateobj *so = &ctx->shaderimg[shader];
 
    fd_set_shader_images(pctx, shader, start, count, unbind_num_trailing_slots,
@@ -347,16 +383,63 @@ fd6_set_shader_images(struct pipe_context *pctx, enum pipe_shader_type shader,
    for (unsigned i = 0; i < count; i++) {
       unsigned n = i + start;
       struct pipe_image_view *buf = &so->si[n];
+      struct fdl6_view *view = &fd6_ctx->image_views[shader][n];
 
       if (!buf->resource)
          continue;
 
-      fd6_validate_format(ctx, fd_resource(buf->resource), buf->format);
+      struct fd_resource *rsc = fd_resource(buf->resource);
+      fd6_validate_format(ctx, rsc, buf->format);
+
+      if (buf->resource->target == PIPE_BUFFER) {
+         static const uint8_t swiz[4] = {PIPE_SWIZZLE_X, PIPE_SWIZZLE_Y,
+                                         PIPE_SWIZZLE_Z, PIPE_SWIZZLE_W};
+         fdl6_buffer_view_init(view->descriptor,
+                           buf->format,
+                           swiz,
+                           buf->u.buf.offset, /* Using relocs for addresses */
+                           buf->u.buf.size);
+
+         /* buffer descriptor is the same for TEX and IBO */
+         memcpy(view->storage_descriptor, view->descriptor,
+                sizeof(view->descriptor));
+      } else {
+         struct fdl_view_args args = {
+            /* Using relocs for addresses */
+            .iova = 0,
+
+            .base_miplevel = buf->u.tex.level,
+            .level_count = 1,
+
+            .base_array_layer = buf->u.tex.first_layer,
+            .layer_count = buf->u.tex.last_layer - buf->u.tex.first_layer + 1,
+
+            .format = buf->format,
+            .swiz = {PIPE_SWIZZLE_X, PIPE_SWIZZLE_Y, PIPE_SWIZZLE_Z,
+                     PIPE_SWIZZLE_W},
+
+            .type = fdl_type_from_pipe_target(buf->resource->target),
+            .chroma_offsets = {FDL_CHROMA_LOCATION_COSITED_EVEN,
+                              FDL_CHROMA_LOCATION_COSITED_EVEN},
+         };
+
+         /* fdl6_view makes the storage descriptor treat cubes like a 2D array (so
+          * you can reference a specific layer), but we need to do that for the
+          * texture descriptor as well to get our layer.
+          */
+         if (args.type == FDL_VIEW_TYPE_CUBE)
+            args.type = FDL_VIEW_TYPE_2D;
+
+         const struct fdl_layout *layouts[3] = {&rsc->layout, NULL, NULL};
+         fdl6_view_init(view, layouts, &args,
+                        ctx->screen->info->a6xx.has_z24uint_s8uint);
+      }
    }
 }
 
 void
 fd6_image_init(struct pipe_context *pctx)
 {
+   pctx->set_shader_buffers = fd6_set_shader_buffers;
    pctx->set_shader_images = fd6_set_shader_images;
 }
