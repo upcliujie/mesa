413d8d1ddcf45bcd4102cc71e450bb72171596da
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-10-20 08:43:01 +0200

    radv: move freeing the trigger SQTT file at a better place
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -3254,7 +3254,6 @@ fail_meta:
    radv_device_finish_meta(device);
 fail:
    radv_thread_trace_finish(device);
-   free(device->thread_trace.trigger_file);
 
    radv_trap_handler_finish(device);
    radv_finish_trace(device);
@@ -3323,7 +3322,6 @@ radv_DestroyDevice(VkDevice _device, const VkAllocationCallbacks *pAllocator)
 
    u_cnd_monotonic_destroy(&device->timeline_cond);
 
-   free(device->thread_trace.trigger_file);
    radv_thread_trace_finish(device);
 
    vk_device_finish(&device->vk);
