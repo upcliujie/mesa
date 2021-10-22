Grima04/mesa-turnip-kgsl

commit d399c3e861a7c459501413e8d675db8ca1a40712

intel/dev: Add device info for ADL-S
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/7322>

include/pci_ids/iris_pci_ids.h
@@ -4,6 +4,17 @@ CHIPSET(0x4c8c, rkl_gt05, "RKL GT0.5", "Intel(R) Graphics")
 CHIPSET(0x4c90, rkl_gt1, "RKL GT1", "Intel(R) Graphics")
 CHIPSET(0x4c9a, rkl_gt1, "RKL GT1", "Intel(R) Graphics")
 
+CHIPSET(0x4680, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4681, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4682, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4683, adl_gt05, "ADL-S GT0.5", "Intel(R) Graphics")
+CHIPSET(0x4690, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4691, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4692, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4693, adl_gt05, "ADL-S GT0.5", "Intel(R) Graphics")
+CHIPSET(0x4698, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+CHIPSET(0x4699, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
+
 CHIPSET(0x9A40, tgl_gt2, "TGL GT2", "Intel(R) Xe Graphics")
 CHIPSET(0x9A49, tgl_gt2, "TGL GT2", "Intel(R) Xe Graphics")
 CHIPSET(0x9A59, tgl_gt2, "TGL GT2", "Intel(R) Graphics")

src/intel/dev/gen_device_info.c
@@ -63,6 +63,7 @@ static const struct {
    { "tgl", 0x9a49 },
    { "rkl", 0x4c8a },
    { "dg1", 0x4905 },
+   { "adl", 0x4680 },
 };
 
 /**
@@ -956,6 +957,14 @@ static const struct gen_device_info gen_device_info_rkl_gt05 = {
 };
 
 static const struct gen_device_info gen_device_info_rkl_gt1 = {
+   GEN12_GT_FEATURES(1),
+};
+
+static const struct gen_device_info gen_device_info_adl_gt05 = {
+   GEN12_GT05_FEATURES,
+};
+
+static const struct gen_device_info gen_device_info_adl_gt1 = {
    GEN12_GT_FEATURES(1),
 };
 

upcliujie/mesa

commit ffcbf823be6057bfcd77b5f508b1921a669201ab

intel: remove 2 preproduction pci-id for ADLS
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: d399c3e861a7c4 ("intel/dev: Add device info for ADL-S")

include/pci_ids/iris_pci_ids.h
@@ -14,8 +14,6 @@ CHIPSET(0x4690, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
 CHIPSET(0x4691, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
 CHIPSET(0x4692, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
 CHIPSET(0x4693, adl_gt05, "ADL-S GT0.5", "Intel(R) Graphics")
-CHIPSET(0x4698, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
-CHIPSET(0x4699, adl_gt1, "ADL-S GT1", "Intel(R) Graphics")
 
 CHIPSET(0x4626, adl_gt2, "ADL GT2", "Intel(R) Graphics")
 CHIPSET(0x4628, adl_gt2, "ADL GT2", "Intel(R) Graphics")