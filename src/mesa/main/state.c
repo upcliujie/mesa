Grima04/mesa-turnip-kgsl

commit 10c75ae41c4e914a0160f9d93bbe1e65b5e14517

mesa: move _mesa_update_pixel out of _mesa_update_state
This only has to be called in a few places and not in normal draw calls.

egl_image_target_texture doesn't call  _mesa_update_pixel because it only
assigns an EGL image to a texture object.

Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8850>

src/mesa/main/state.c
@@ -465,9 +465,6 @@ _mesa_update_state_locked( struct gl_context *ctx )
       if (new_state & _NEW_LIGHT_CONSTANTS)
          _mesa_update_lighting(ctx);
 
-      if (new_state & _NEW_PIXEL)
-         _mesa_update_pixel( ctx );
-
       /* ctx->_NeedEyeCoords is determined here.
        *
        * If the truth value of this variable has changed, update for the

src/mesa/main/drawpix.c
@@ -32,6 +32,7 @@
 #include "framebuffer.h"
 #include "image.h"
 #include "pbo.h"
+#include "pixel.h"
 #include "state.h"
 #include "glformats.h"
 #include "fbobject.h"
@@ -71,6 +72,9 @@ _mesa_DrawPixels( GLsizei width, GLsizei height,
     * its own.  Note: this may dirty some state.
     */
    _mesa_set_vp_override(ctx, GL_TRUE);
+
+   if (ctx->NewState & _NEW_PIXEL)
+      _mesa_update_pixel(ctx);
 
    if (ctx->NewState)
       _mesa_update_state(ctx);
@@ -241,6 +245,9 @@ _mesa_CopyPixels( GLint srcx, GLint srcy, GLsizei width, GLsizei height,
     */
    _mesa_set_vp_override(ctx, GL_TRUE);
 
+   if (ctx->NewState & _NEW_PIXEL)
+      _mesa_update_pixel(ctx);
+
    if (ctx->NewState)
       _mesa_update_state(ctx);
 
@@ -326,6 +333,9 @@ _mesa_Bitmap( GLsizei width, GLsizei height,
    if (!ctx->Current.RasterPosValid) {
       return;    /* do nothing */
    }
+
+   if (ctx->NewState & _NEW_PIXEL)
+      _mesa_update_pixel(ctx);
 
    if (ctx->NewState)
       _mesa_update_state(ctx);

src/mesa/main/readpix.c
@@ -36,6 +36,7 @@
 #include "mtypes.h"
 #include "pack.h"
 #include "pbo.h"
+#include "pixel.h"
 #include "state.h"
 #include "glformats.h"
 #include "fbobject.h"
@@ -1047,6 +1048,9 @@ read_pixels(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format,
                    "glReadPixels(width=%d height=%d)", width, height );
       return;
    }
+
+   if (ctx->NewState & _NEW_PIXEL)
+      _mesa_update_pixel(ctx);
 
    if (ctx->NewState)
       _mesa_update_state(ctx);

src/mesa/main/teximage.c
@@ -42,6 +42,7 @@
 #include "macros.h"
 #include "mipmap.h"
 #include "multisample.h"
+#include "pixel.h"
 #include "pixelstore.h"
 #include "state.h"
 #include "texcompress.h"
@@ -3127,7 +3128,7 @@ teximage(struct gl_context *ctx, GLboolean compressed, GLuint dims,
       }
 
       if (ctx->NewState & _NEW_PIXEL)
-         _mesa_update_state(ctx);
+         _mesa_update_pixel(ctx);
 
       _mesa_lock_texture(ctx, texObj);
       {
@@ -3423,9 +3424,6 @@ egl_image_target_texture(struct gl_context *ctx,
       return;
    }
 
-   if (ctx->NewState & _NEW_PIXEL)
-      _mesa_update_state(ctx);
-
    _mesa_lock_texture(ctx, texObj);
 
    if (texObj->Immutable) {
@@ -3565,7 +3563,7 @@ texture_sub_image(struct gl_context *ctx, GLuint dims,
    FLUSH_VERTICES(ctx, 0, 0);
 
    if (ctx->NewState & _NEW_PIXEL)
-      _mesa_update_state(ctx);
+      _mesa_update_pixel(ctx);
 
    _mesa_lock_texture(ctx, texObj);
    {
@@ -4248,6 +4246,9 @@ copy_texture_sub_image_err(struct gl_context *ctx, GLuint dims,
                   _mesa_enum_to_string(target),
                   level, xoffset, yoffset, zoffset, x, y, width, height);
 
+   if (ctx->NewState & _NEW_PIXEL)
+      _mesa_update_pixel(ctx);
+
    if (ctx->NewState & NEW_COPY_TEX_STATE)
       _mesa_update_state(ctx);
 
@@ -4270,6 +4271,9 @@ copy_texture_sub_image_no_error(struct gl_context *ctx, GLuint dims,
                                 GLint x, GLint y, GLsizei width, GLsizei height)
 {
    FLUSH_VERTICES(ctx, 0, 0);
+
+   if (ctx->NewState & _NEW_PIXEL)
+      _mesa_update_pixel(ctx);
 
    if (ctx->NewState & NEW_COPY_TEX_STATE)
       _mesa_update_state(ctx);
@@ -4299,6 +4303,9 @@ copyteximage(struct gl_context *ctx, GLuint dims, struct gl_texture_object *texO
                   _mesa_enum_to_string(target), level,
                   _mesa_enum_to_string(internalFormat),
                   x, y, width, height, border);
+
+   if (ctx->NewState & _NEW_PIXEL)
+      _mesa_update_pixel(ctx);
 
    if (ctx->NewState & NEW_COPY_TEX_STATE)
       _mesa_update_state(ctx);

upcliujie/mesa

commit 76362f1a91676127d0b96266db1b9e0d2ce5e71b

mesa: don't clear _NEW_PIXEL if it wasn't applied
10c75ae41 moved handling of this state to the functions that
depend on ctx->_ImageTransferState.

We shouldn't clear the NEW_PIXEL bit in _mesa_update_state_locked
or we might end up not applying a new state, eg:

  glPixelTransferi(...)
  glClear(...)
  glTexSubImage2D(...) <-- won't use the new value set by
                           glPixelTransferi because glClear caused
                           _NEW_PIXEL to be cleared.

Fixes: 10c75ae41c4 ("mesa: move _mesa_update_pixel out of _mesa_update_state")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5273

src/mesa/main/state.c
@@ -488,7 +488,7 @@ _mesa_update_state_locked( struct gl_context *ctx )
     * active modules (such as swrast_setup, swrast, tnl, etc).
     */
    ctx->Driver.UpdateState(ctx);
-   ctx->NewState = 0;
+   ctx->NewState = new_state & _NEW_PIXEL;
 }
 
 

src/mesa/state_tracker/st_cb_drawpixels.c
@@ -1321,7 +1321,7 @@ st_DrawPixels(struct gl_context *ctx, GLint x, GLint y,
    struct pipe_resource *pt;
 
    /* Mesa state should be up to date by now */
-   assert(ctx->NewState == 0x0);
+   assert((ctx->NewState & ~_NEW_PIXEL) == 0x0);
 
    _mesa_update_draw_buffer_bounds(ctx, ctx->DrawBuffer);
 

src/mesa/state_tracker/st_draw.c
@@ -91,7 +91,7 @@ prepare_draw(struct st_context *st, struct gl_context *ctx, uint64_t state_mask,
              enum st_pipeline pipeline)
 {
    /* Mesa core state should have been validated already */
-   assert(ctx->NewState == 0x0);
+   assert((ctx->NewState & ~_NEW_PIXEL) == 0x0);
 
    if (unlikely(!st->bitmap.cache.empty))
       st_flush_bitmap_cache(st);

src/mesa/tnl/t_draw.c
@@ -445,7 +445,7 @@ void _tnl_draw_prims(struct gl_context *ctx,
                              false, 0);
 
    /* Mesa core state should have been validated already */
-   assert(ctx->NewState == 0x0);
+   assert((ctx->NewState & ~_NEW_PIXEL) == 0x0);
 
    if (!_mesa_check_conditional_render(ctx))
       return; /* don't draw */

src/mesa/vbo/vbo_exec_draw.c
@@ -324,7 +324,7 @@ vbo_exec_vtx_flush(struct vbo_exec_context *exec)
          if (!persistent_mapping)
             vbo_exec_vtx_unmap(exec);
 
-         assert(ctx->NewState == 0);
+         assert((ctx->NewState & ~_NEW_PIXEL) == 0x0);
 
          if (0)
             printf("%s %d %d\n", __func__, exec->vtx.prim_count,

src/mesa/vbo/vbo_save_draw.c
@@ -328,7 +328,7 @@ vbo_save_playback_vertex_list(struct gl_context *ctx, void *data, bool copy_to_c
       return;
    }
 
-   assert(ctx->NewState == 0);
+   assert((ctx->NewState & ~_NEW_PIXEL) == 0x0);
 
    struct pipe_draw_info *info = (struct pipe_draw_info *) &node->cold->info;
    void *gl_bo = info->index.gl_bo;