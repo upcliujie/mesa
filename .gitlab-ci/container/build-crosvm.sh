93177a6141c58799f9f87c87bf4325f76881692b
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2021-10-29 11:00:32 +0200

    ci: Uprev Crosvm
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>

--- .gitlab-ci/container/build-crosvm.sh
+++ .gitlab-ci/container/build-crosvm.sh
@@ -10,14 +10,14 @@ AOSP_EXTERNAL_ROOT=$CROS_ROOT/aosp/external
 mkdir -p $AOSP_EXTERNAL_ROOT
 PLATFORM2_ROOT=/platform2
 
-PLATFORM2_COMMIT=72e56e66ccf3d2ea48f5686bd1f772379c43628b
+PLATFORM2_COMMIT=418aa4f600349859421e494e52d85865e927a85c
 git clone --single-branch --no-checkout https://chromium.googlesource.com/chromiumos/platform2 $PLATFORM2_ROOT
 pushd $PLATFORM2_ROOT
 git checkout $PLATFORM2_COMMIT
 popd
 
 # minijail does not exist in upstream linux distros.
-MINIJAIL_COMMIT=debdf5de5a0ae3b667bee2f8fb1f755b0b3f5a6c
+MINIJAIL_COMMIT=d01c60272ab9bd2c467f2586cd67137d04eb8938
 git clone --single-branch --no-checkout https://android.googlesource.com/platform/external/minijail $AOSP_EXTERNAL_ROOT/minijail
 pushd $AOSP_EXTERNAL_ROOT/minijail
 git checkout $MINIJAIL_COMMIT
@@ -26,21 +26,21 @@ cp libminijail.so /usr/lib/x86_64-linux-gnu/
 popd
 
 # Pull the cras library for audio access.
-ADHD_COMMIT=a1e0869b95c845c4fe6234a7b92fdfa6acc1e809
+ADHD_COMMIT=e20968929240bc9ae6c842f4cbf9135e483b64a2
 git clone --single-branch --no-checkout https://chromium.googlesource.com/chromiumos/third_party/adhd $THIRD_PARTY_ROOT/adhd
 pushd $THIRD_PARTY_ROOT/adhd
 git checkout $ADHD_COMMIT
 popd
 
 # Pull vHost (dataplane for virtio backend drivers)
-VHOST_COMMIT=3091854e27242d09453004b011f701fa29c0b8e8
+VHOST_COMMIT=7c95b4a2c1e378f7328d8bc0510bbb6998f54581
 git clone --single-branch --no-checkout https://chromium.googlesource.com/chromiumos/third_party/rust-vmm/vhost $THIRD_PARTY_ROOT/rust-vmm/vhost
 pushd $THIRD_PARTY_ROOT/rust-vmm/vhost
 git checkout $VHOST_COMMIT
 popd
 
-CROSVM_VERSION=e42a43d880b0364b55559dbeade3af174f929001
-git clone --single-branch --no-checkout https://chromium.googlesource.com/chromiumos/platform/crosvm /platform/crosvm
+CROSVM_VERSION=bf37e81e42f768bd833f7ecdcde6fee3a0b576ab
+git clone --no-checkout https://gitlab.freedesktop.org/tomeu/crosvm.git /platform/crosvm
 pushd /platform/crosvm
 git checkout "$CROSVM_VERSION"
 
