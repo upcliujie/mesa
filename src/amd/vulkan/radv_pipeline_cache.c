8e3bbffac17b97504ada8611d1d2cfdf74359fda
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-11-05 13:57:35 +0100

    radv: pass a pointer to a pipeline for the create/insert cache functions
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_pipeline_cache.c
+++ src/amd/vulkan/radv_pipeline_cache.c
@@ -293,7 +293,7 @@ radv_is_cache_disabled(struct radv_device *device)
 bool
 radv_create_shaders_from_pipeline_cache(
    struct radv_device *device, struct radv_pipeline_cache *cache, const unsigned char *sha1,
-   struct radv_shader **shaders, struct radv_pipeline_shader_stack_size **stack_sizes,
+   struct radv_pipeline *pipeline, struct radv_pipeline_shader_stack_size **stack_sizes,
    uint32_t *num_stack_sizes, bool *found_in_application_cache)
 {
    struct cache_entry *entry;
@@ -367,7 +367,7 @@ radv_create_shaders_from_pipeline_cache(
       }
    }
 
-   memcpy(shaders, entry->shaders, sizeof(entry->shaders));
+   memcpy(pipeline->shaders, entry->shaders, sizeof(entry->shaders));
 
    if (num_stack_sizes) {
       *num_stack_sizes = entry->num_stack_sizes;
@@ -391,7 +391,7 @@ radv_create_shaders_from_pipeline_cache(
 
 void
 radv_pipeline_cache_insert_shaders(struct radv_device *device, struct radv_pipeline_cache *cache,
-                                   const unsigned char *sha1, struct radv_shader **shaders,
+                                   const unsigned char *sha1, struct radv_pipeline *pipeline,
                                    struct radv_shader_binary *const *binaries,
                                    const struct radv_pipeline_shader_stack_size *stack_sizes,
                                    uint32_t num_stack_sizes)
@@ -404,13 +404,13 @@ radv_pipeline_cache_insert_shaders(struct radv_device *device, struct radv_pipel
    if (entry) {
       for (int i = 0; i < MESA_VULKAN_SHADER_STAGES; ++i) {
          if (entry->shaders[i]) {
-            radv_shader_destroy(cache->device, shaders[i]);
-            shaders[i] = entry->shaders[i];
+            radv_shader_destroy(cache->device, pipeline->shaders[i]);
+            pipeline->shaders[i] = entry->shaders[i];
          } else {
-            entry->shaders[i] = shaders[i];
+            entry->shaders[i] = pipeline->shaders[i];
          }
-         if (shaders[i])
-            p_atomic_inc(&shaders[i]->ref_count);
+         if (pipeline->shaders[i])
+            p_atomic_inc(&pipeline->shaders[i]->ref_count);
       }
       radv_pipeline_cache_unlock(cache);
       return;
@@ -426,7 +426,7 @@ radv_pipeline_cache_insert_shaders(struct radv_device *device, struct radv_pipel
 
    size_t size = sizeof(*entry) + sizeof(*stack_sizes) * num_stack_sizes;
    for (int i = 0; i < MESA_VULKAN_SHADER_STAGES; ++i)
-      if (shaders[i])
+      if (pipeline->shaders[i])
          size += binaries[i]->total_size;
    const size_t size_without_align = size;
    size = align(size_without_align, alignof(struct cache_entry));
@@ -443,7 +443,7 @@ radv_pipeline_cache_insert_shaders(struct radv_device *device, struct radv_pipel
    char *p = entry->code;
 
    for (int i = 0; i < MESA_VULKAN_SHADER_STAGES; ++i) {
-      if (!shaders[i])
+      if (!pipeline->shaders[i])
          continue;
 
       entry->binary_sizes[i] = binaries[i]->total_size;
@@ -487,11 +487,11 @@ radv_pipeline_cache_insert_shaders(struct radv_device *device, struct radv_pipel
     * items.
     */
    for (int i = 0; i < MESA_VULKAN_SHADER_STAGES; ++i) {
-      if (!shaders[i])
+      if (!pipeline->shaders[i])
          continue;
 
-      entry->shaders[i] = shaders[i];
-      p_atomic_inc(&shaders[i]->ref_count);
+      entry->shaders[i] = pipeline->shaders[i];
+      p_atomic_inc(&pipeline->shaders[i]->ref_count);
    }
 
    radv_pipeline_cache_add_entry(cache, entry);
