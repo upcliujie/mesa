377ce79097681c97f30fa4f288dc259b61a06c73
Dave Airlie <airlied@redhat.com>
2021-11-09 13:50:06 +1000

    vulkan/video: add a h264 slice header decoder.
    This uses the util code to parse rbsp, and is modelled on the code
    in the omx frontend, but also from reading the spec.
    
    Some sps/pps params aren't supported in vulkan video, so aren't
    parsed.
    
    The slice header details are used by the Intel driver to program the
    hw.

--- src/vulkan/runtime/vk_video.c
+++ src/vulkan/runtime/vk_video.c
@@ -25,6 +25,7 @@
 #include "vk_object.h"
 #include "vk_video.h"
 #include "vk_util.h"
+#include "vl_rbsp.h"
 
 #ifdef VK_ENABLE_BETA_EXTENSIONS
 
@@ -179,6 +180,278 @@ void vk_video_session_parameters_object_finish(struct vk_video_session_parameter
       break;
    }
    vk_object_base_finish(&params->base);
+}
+
+static void ref_pic_list_mod(struct vl_rbsp *rbsp,
+                             StdVideoH264SliceType slice_type)
+{
+   unsigned modification_of_pic_nums_idc;
+
+   if (slice_type != STD_VIDEO_H264_SLICE_TYPE_I &&
+       slice_type != STD_VIDEO_H264_SLICE_TYPE_SI) {
+      /* ref_pic_list_modification_flag_l0 */
+      if (vl_rbsp_u(rbsp, 1)) {
+         do {
+            modification_of_pic_nums_idc = vl_rbsp_ue(rbsp);
+            if (modification_of_pic_nums_idc == 0 ||
+                modification_of_pic_nums_idc == 1)
+               /* abs_diff_pic_num_minus1 */
+               vl_rbsp_ue(rbsp);
+            else if (modification_of_pic_nums_idc == 2)
+               /* long_term_pic_num */
+               vl_rbsp_ue(rbsp);
+         } while (modification_of_pic_nums_idc != 3);
+      }
+   }
+
+   if (slice_type == STD_VIDEO_H264_SLICE_TYPE_B) {
+      /* ref_pic_list_modification_flag_l1 */
+      if (vl_rbsp_u(rbsp, 1)) {
+         do {
+            modification_of_pic_nums_idc = vl_rbsp_ue(rbsp);
+            if (modification_of_pic_nums_idc == 0 ||
+                modification_of_pic_nums_idc == 1)
+               /* abs_diff_pic_num_minus1 */
+               vl_rbsp_ue(rbsp);
+            else if (modification_of_pic_nums_idc == 2)
+               /* long_term_pic_num */
+               vl_rbsp_ue(rbsp);
+         } while (modification_of_pic_nums_idc != 3);
+      }
+   }
+}
+
+static void pred_weight_table(struct vk_video_h264_slice_params *params,
+                              struct vl_rbsp *rbsp,
+                              const StdVideoH264SequenceParameterSet *sps,
+                              StdVideoH264SliceType slice_type)
+{
+   unsigned ChromaArrayType = sps->chroma_format_idc;//sps->separate_colour_plane_flag ? 0 : sps->chroma_format_idc;
+   unsigned i, j;
+
+   /* luma_log2_weight_denom */
+   params->luma_log2_weight_denom = vl_rbsp_ue(rbsp);
+
+   if (ChromaArrayType != 0)
+      /* chroma_log2_weight_denom */
+      params->chroma_log2_weight_denom = vl_rbsp_ue(rbsp);
+
+   for (i = 0; i <= params->num_ref_idx_l0_active_minus1; ++i) {
+      /* luma_weight_l0_flag */
+      params->luma_weight_l0_flag[i] = vl_rbsp_u(rbsp, 1);
+      if (params->luma_weight_l0_flag[i]) {
+         /* luma_weight_l0[i] */
+         params->luma_weight_l0[i] = vl_rbsp_se(rbsp);
+         /* luma_offset_l0[i] */
+         params->luma_offset_l0[i] = vl_rbsp_se(rbsp);
+      }
+      if (ChromaArrayType != 0) {
+         /* chroma_weight_l0_flag */
+         params->chroma_weight_l0_flag[i] = vl_rbsp_u(rbsp, 1);
+         if (params->chroma_weight_l0_flag[i]) {
+            for (j = 0; j < 2; ++j) {
+               /* chroma_weight_l0[i][j] */
+               params->chroma_weight_l0[i][j] = vl_rbsp_se(rbsp);
+               /* chroma_offset_l0[i][j] */
+               params->chroma_offset_l0[i][j] = vl_rbsp_se(rbsp);
+            }
+         }
+      }
+   }
+
+   if (slice_type == STD_VIDEO_H264_SLICE_TYPE_B) {
+      for (i = 0; i <= params->num_ref_idx_l1_active_minus1; ++i) {
+         /* luma_weight_l1_flag */
+         params->luma_weight_l1_flag[i] = vl_rbsp_u(rbsp, 1);
+         if (params->luma_weight_l1_flag[i]) {
+            /* luma_weight_l1[i] */
+            params->luma_weight_l1[i] = vl_rbsp_se(rbsp);
+            /* luma_offset_l1[i] */
+            params->luma_offset_l1[i] = vl_rbsp_se(rbsp);
+         }
+         if (ChromaArrayType != 0) {
+            /* chroma_weight_l1_flag */
+            params->chroma_weight_l1_flag[i] = vl_rbsp_u(rbsp, 1);
+            if (params->chroma_weight_l1_flag[i]) {
+               for (j = 0; j < 2; ++j) {
+                  /* chroma_weight_l1[i][j] */
+                  params->chroma_weight_l1[i][j] = vl_rbsp_se(rbsp);
+                  /* chroma_offset_l1[i][j] */
+                  params->chroma_offset_l1[i][j] = vl_rbsp_se(rbsp);
+               }
+            }
+         }
+      }
+   }
+}
+
+static void dec_ref_pic_marking(struct vl_rbsp *rbsp,
+                                bool IdrPicFlag)
+{
+   unsigned memory_management_control_operation;
+
+   if (IdrPicFlag) {
+      /* no_output_of_prior_pics_flag */
+      vl_rbsp_u(rbsp, 1);
+      /* long_term_reference_flag */
+      vl_rbsp_u(rbsp, 1);
+   } else {
+      /* adaptive_ref_pic_marking_mode_flag */
+      if (vl_rbsp_u(rbsp, 1)) {
+         do {
+            memory_management_control_operation = vl_rbsp_ue(rbsp);
+
+            if (memory_management_control_operation == 1 ||
+                memory_management_control_operation == 3)
+               /* difference_of_pic_nums_minus1 */
+               vl_rbsp_ue(rbsp);
+
+            if (memory_management_control_operation == 2)
+               /* long_term_pic_num */
+               vl_rbsp_ue(rbsp);
+
+            if (memory_management_control_operation == 3 ||
+                memory_management_control_operation == 6)
+               /* long_term_frame_idx */
+               vl_rbsp_ue(rbsp);
+
+            if (memory_management_control_operation == 4)
+               /* max_long_term_frame_idx_plus1 */
+               vl_rbsp_ue(rbsp);
+         } while (memory_management_control_operation != 0);
+      }
+   }
+}
+
+void vk_video_parse_h264_slice_header(const struct VkVideoDecodeInfoKHR *frame_info,
+                                      const StdVideoH264SequenceParameterSet *sps,
+                                      const StdVideoH264PictureParameterSet *pps,
+                                      void *slice_hdr,
+                                      struct vk_video_h264_slice_params *params)
+{
+   struct vl_vlc vlc;
+   unsigned sizes = frame_info->srcBufferRange;
+   const void *slice_hdrs[1] = { slice_hdr };
+   vl_vlc_init(&vlc, 1, slice_hdrs, &sizes);
+
+   assert(vl_vlc_peekbits(&vlc, 24) == 0x000001);
+
+   vl_vlc_eatbits(&vlc, 24);
+
+   /* forbidden_zero_bit */
+   vl_vlc_eatbits(&vlc, 1);
+
+   unsigned nal_ref_idc = vl_vlc_get_uimsbf(&vlc, 2);
+   unsigned nal_unit_type = vl_vlc_get_uimsbf(&vlc, 5);
+
+   assert(nal_unit_type == 1 || nal_unit_type == 5);
+
+   const uint8_t *orig_ptr = vlc.data;
+   struct vl_rbsp rbsp;
+   vl_rbsp_init(&rbsp, &vlc, 128);
+
+   memset(params, 0, sizeof(*params));
+   /* first_mb_in_slice */
+   params->first_mb_in_slice = vl_rbsp_ue(&rbsp);
+   params->slice_type = vl_rbsp_ue(&rbsp) % 5;
+   vl_rbsp_ue(&rbsp);//pps id
+
+   if (sps->flags.separate_colour_plane_flag)
+      vl_rbsp_u(&rbsp, 2);
+
+   vl_rbsp_u(&rbsp, sps->log2_max_frame_num_minus4 + 4);
+   unsigned field_pic_flag = 0;
+   if (!sps->flags.frame_mbs_only_flag) {
+      field_pic_flag = vl_rbsp_u(&rbsp, 1);
+      if (field_pic_flag)
+         /*unsigned bottom_field_flag =*/ vl_rbsp_u(&rbsp, 1);
+   }
+
+   if (nal_unit_type == 5) {
+      /*unsigned idr_pic_id =*/ vl_rbsp_ue(&rbsp);
+   }
+   if (sps->pic_order_cnt_type == 0) {
+      unsigned log2_max_pic_order_cnt_lsb = sps->log2_max_pic_order_cnt_lsb_minus4 + 4;
+      /* unsigned pic_order_cnt_lsb */ vl_rbsp_u(&rbsp, log2_max_pic_order_cnt_lsb);
+      if (0) {//pps->flags.bottom_field_pic_order_in_frame_present_flag && !field_pic_flag) {
+         /* delta_pic-order_cnt[1] */ vl_rbsp_se(&rbsp);
+      } else
+         assert(0);
+   }
+
+   if (pps->flags.redundant_pic_cnt_present_flag)
+      /* redundant_pic_cnt */
+     vl_rbsp_ue(&rbsp);
+
+   if (params->slice_type == STD_VIDEO_H264_SLICE_TYPE_B)
+      /* direct_spatial_mv_pred_flag */
+      params->direct_spatial_mv_pred_flag = vl_rbsp_u(&rbsp, 1);
+
+   params->num_ref_idx_l0_active_minus1 = pps->num_ref_idx_l0_default_active_minus1;
+   params->num_ref_idx_l1_active_minus1 = pps->num_ref_idx_l1_default_active_minus1;
+
+   if (params->slice_type != STD_VIDEO_H264_SLICE_TYPE_B)
+      params->num_ref_idx_l1_active_minus1 = 0;
+   if (params->slice_type != STD_VIDEO_H264_SLICE_TYPE_I) {
+      /* num_ref_idx_active_override_flag */
+      if (vl_rbsp_u(&rbsp, 1)) {
+         params->num_ref_idx_l0_active_minus1 = vl_rbsp_ue(&rbsp);
+
+         if (params->slice_type == STD_VIDEO_H264_SLICE_TYPE_B)
+            params->num_ref_idx_l1_active_minus1 = vl_rbsp_ue(&rbsp);
+      }
+   } else
+      params->num_ref_idx_l0_active_minus1 = 0;
+
+   if (nal_unit_type == 20 || nal_unit_type == 21)
+      assert(0);
+   else
+      ref_pic_list_mod(&rbsp, params->slice_type);
+
+   if ((pps->flags.weighted_pred_flag && (params->slice_type == STD_VIDEO_H264_SLICE_TYPE_P || params->slice_type == STD_VIDEO_H264_SLICE_TYPE_SP)) ||
+       (pps->weighted_bipred_idc == 1 && params->slice_type == STD_VIDEO_H264_SLICE_TYPE_B))
+      pred_weight_table(params, &rbsp, sps, params->slice_type);
+
+   if (nal_ref_idc != 0)
+      dec_ref_pic_marking(&rbsp, nal_unit_type == 5);
+
+   if (pps->flags.entropy_coding_mode_flag &&
+       params->slice_type != STD_VIDEO_H264_SLICE_TYPE_I &&
+       params->slice_type != STD_VIDEO_H264_SLICE_TYPE_SI)
+      /* cabac_init_idc */
+      params->cabac_init_idc = vl_rbsp_ue(&rbsp);
+
+   /* slice_qp_delta */
+   params->slice_qp_delta = vl_rbsp_se(&rbsp);
+
+   if (params->slice_type == STD_VIDEO_H264_SLICE_TYPE_SP ||
+       params->slice_type == STD_VIDEO_H264_SLICE_TYPE_SI) {
+      if (params->slice_type == STD_VIDEO_H264_SLICE_TYPE_SP)
+         /* sp_for_switch_flag */
+         vl_rbsp_u(&rbsp, 1);
+
+      /*slice_qs_delta */
+      vl_rbsp_se(&rbsp);
+   }
+
+   if (pps->flags.deblocking_filter_control_present_flag) {
+      params->disable_deblocking_filter_idc = vl_rbsp_ue(&rbsp);
+
+      if (params->disable_deblocking_filter_idc != 1) {
+         /* slice_alpha_c0_offset_div2 */
+         params->slice_alpha_c0_offset_div2 = vl_rbsp_se(&rbsp);
+
+         /* slice_beta_offset_div2 */
+         params->slice_beta_offset_div2 = vl_rbsp_se(&rbsp);
+      }
+   }
+
+   /* Vulkan will never give us num_slice_group_minus1 > 0 so no need to worry about slice_group_change_cycle */
+
+   /* this is a bit horrible. - the rbsp overfetches, so workout how much data it has consumed, then depending
+      on the direction, remove or add back the bits it hasn't consumed. Add back the 8-bits from the original
+      NAL header. */
+   params->slice_data_bit_offset = ((rbsp.nal.data - orig_ptr) * 8) + rbsp.nal.invalid_bits - ((rbsp.nal.invalid_bits < 0) ? 32 : 0) + 8;
 }
 
 #endif
