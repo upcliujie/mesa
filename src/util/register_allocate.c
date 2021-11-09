aerisarn/mesa-uwp

commit 95d41a3525d97986b4925153a36828b1a6e65523

ra: Use struct ra_class in the public API.
All these unsigned ints are awful to keep track of.  Use pointers so we
get some type checking.

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9437>

src/util/register_allocate.c
@@ -214,7 +214,7 @@ ra_make_reg_conflicts_transitive(struct ra_regs *regs, unsigned int r)
    }
 }
 
-unsigned int
+struct ra_class *
 ra_alloc_reg_class(struct ra_regs *regs)
 {
    struct ra_class *class;
@@ -223,20 +223,33 @@ ra_alloc_reg_class(struct ra_regs *regs)
                             regs->class_count + 1);
 
    class = rzalloc(regs, struct ra_class);
-   regs->classes[regs->class_count] = class;
+   class->regset = regs;
+
+   /* Users may rely on the class index being allocated in order starting from 0. */
+   class->index = regs->class_count++;
+   regs->classes[class->index] = class;
 
    class->regs = rzalloc_array(class, BITSET_WORD, BITSET_WORDS(regs->count));
 
-   /* Users may rely on the class index being allocated in order starting from 0. */
-   return regs->class_count++;
+   return class;
+}
+
+struct ra_class *
+ra_get_class_from_index(struct ra_regs *regs, unsigned int class)
+{
+   return regs->classes[class];
+}
+
+unsigned int
+ra_class_index(struct ra_class *c)
+{
+   return c->index;
 }
 
 void
-ra_class_add_reg(struct ra_regs *regs, unsigned int c, unsigned int r)
+ra_class_add_reg(struct ra_class *class, unsigned int r)
 {
-   struct ra_class *class = regs->classes[c];
-
-   assert(r < regs->count);
+   assert(r < class->regset->count);
 
    BITSET_SET(class->regs, r);
    class->p++;
@@ -475,20 +488,20 @@ void ra_set_select_reg_callback(struct ra_graph *g,
 
 void
 ra_set_node_class(struct ra_graph *g,
-                  unsigned int n, unsigned int class)
+                  unsigned int n, struct ra_class *class)
 {
-   g->nodes[n].class = class;
+   g->nodes[n].class = class->index;
 }
 
-unsigned int
+struct ra_class *
 ra_get_node_class(struct ra_graph *g,
                   unsigned int n)
 {
-   return g->nodes[n].class;
+   return g->regs->classes[g->nodes[n].class];
 }
 
 unsigned int
-ra_add_node(struct ra_graph *g, unsigned int class)
+ra_add_node(struct ra_graph *g, struct ra_class *class)
 {
    unsigned int n = g->count;
    ra_resize_interference_graph(g, g->count + 1);

src/util/register_allocate_test.cpp
@@ -53,27 +53,27 @@ TEST_F(ra_test, thumb)
    int next_vreg = 16;
 
    /* reg32low is any of the low 8 registers. */
-   int reg32low = ra_alloc_reg_class(regs);
+   struct ra_class *reg32low = ra_alloc_reg_class(regs);
    for (int i = 0; i < 8; i++) {
       int vreg = next_vreg++;
-      ra_class_add_reg(regs, reg32low, vreg);
+      ra_class_add_reg(reg32low, vreg);
       ra_add_transitive_reg_conflict(regs, i, vreg);
    }
 
    /* reg64low is pairs of the low 8 registers (with wraparound!) */
-   int reg64low = ra_alloc_reg_class(regs);
+   struct ra_class *reg64low = ra_alloc_reg_class(regs);
    for (int i = 0; i < 8; i++) {
       int vreg = next_vreg++;
-      ra_class_add_reg(regs, reg64low, vreg);
+      ra_class_add_reg(reg64low, vreg);
       ra_add_transitive_reg_conflict(regs, i, vreg);
       ra_add_transitive_reg_conflict(regs, (i + 1) % 8, vreg);
    }
 
    /* reg96 is one of either r[0..2] or r[1..3] */
-   int reg96 = ra_alloc_reg_class(regs);
+   struct ra_class *reg96 = ra_alloc_reg_class(regs);
    for (int i = 0; i < 2; i++) {
       int vreg = next_vreg++;
-      ra_class_add_reg(regs, reg96, vreg);
+      ra_class_add_reg(reg96, vreg);
       for (int j = 0; j < 3; j++)
          ra_add_transitive_reg_conflict(regs, i + j, vreg);
    }
@@ -81,16 +81,16 @@ TEST_F(ra_test, thumb)
    ra_set_finalize(regs, NULL);
 
    /* Table 4.1 */
-   ASSERT_EQ(regs->classes[reg32low]->p, 8);
-   ASSERT_EQ(regs->classes[reg32low]->q[reg32low], 1);
-   ASSERT_EQ(regs->classes[reg32low]->q[reg64low], 2);
-   ASSERT_EQ(regs->classes[reg32low]->q[reg96], 3);
-   ASSERT_EQ(regs->classes[reg64low]->p, 8);
-   ASSERT_EQ(regs->classes[reg64low]->q[reg32low], 2);
-   ASSERT_EQ(regs->classes[reg64low]->q[reg64low], 3);
-   ASSERT_EQ(regs->classes[reg64low]->q[reg96], 4);
-   ASSERT_EQ(regs->classes[reg96]->p, 2);
-   ASSERT_EQ(regs->classes[reg96]->q[reg96], 2);
-   ASSERT_EQ(regs->classes[reg96]->q[reg64low], 2);
-   ASSERT_EQ(regs->classes[reg96]->q[reg96], 2);
+   ASSERT_EQ(reg32low->p, 8);
+   ASSERT_EQ(reg32low->q[reg32low->index], 1);
+   ASSERT_EQ(reg32low->q[reg64low->index], 2);
+   ASSERT_EQ(reg32low->q[reg96->index], 3);
+   ASSERT_EQ(reg64low->p, 8);
+   ASSERT_EQ(reg64low->q[reg32low->index], 2);
+   ASSERT_EQ(reg64low->q[reg64low->index], 3);
+   ASSERT_EQ(reg64low->q[reg96->index], 4);
+   ASSERT_EQ(reg96->p, 2);
+   ASSERT_EQ(reg96->q[reg96->index], 2);
+   ASSERT_EQ(reg96->q[reg64low->index], 2);
+   ASSERT_EQ(reg96->q[reg96->index], 2);
 }

src/broadcom/compiler/v3d_compiler.h
@@ -549,10 +549,10 @@ enum v3d_compilation_result {
 struct v3d_compiler {
         const struct v3d_device_info *devinfo;
         struct ra_regs *regs;
-        unsigned int reg_class_any[3];
-        unsigned int reg_class_r5[3];
-        unsigned int reg_class_phys[3];
-        unsigned int reg_class_phys_or_acc[3];
+        struct ra_class *reg_class_any[3];
+        struct ra_class *reg_class_r5[3];
+        struct ra_class *reg_class_phys[3];
+        struct ra_class *reg_class_phys_or_acc[3];
 };
 
 /**

src/broadcom/compiler/vir_register_allocate.c
@@ -517,28 +517,21 @@ vir_init_reg_sets(struct v3d_compiler *compiler)
 
                 for (int i = PHYS_INDEX;
                      i < PHYS_INDEX + (PHYS_COUNT >> threads); i++) {
-                        ra_class_add_reg(compiler->regs,
-                                         compiler->reg_class_phys_or_acc[threads], i);
-                        ra_class_add_reg(compiler->regs,
-                                         compiler->reg_class_phys[threads], i);
-                        ra_class_add_reg(compiler->regs,
-                                         compiler->reg_class_any[threads], i);
+                        ra_class_add_reg(compiler->reg_class_phys_or_acc[threads], i);
+                        ra_class_add_reg(compiler->reg_class_phys[threads], i);
+                        ra_class_add_reg(compiler->reg_class_any[threads], i);
                 }
 
                 for (int i = ACC_INDEX + 0; i < ACC_INDEX + ACC_COUNT - 1; i++) {
-                        ra_class_add_reg(compiler->regs,
-                                         compiler->reg_class_phys_or_acc[threads], i);
-                        ra_class_add_reg(compiler->regs,
-                                         compiler->reg_class_any[threads], i);
+                        ra_class_add_reg(compiler->reg_class_phys_or_acc[threads], i);
+                        ra_class_add_reg(compiler->reg_class_any[threads], i);
                 }
                 /* r5 can only store a single 32-bit value, so not much can
                  * use it.
                  */
-                ra_class_add_reg(compiler->regs,
-                                 compiler->reg_class_r5[threads],
+                ra_class_add_reg(compiler->reg_class_r5[threads],
                                  ACC_INDEX + 5);
-                ra_class_add_reg(compiler->regs,
-                                 compiler->reg_class_any[threads],
+                ra_class_add_reg(compiler->reg_class_any[threads],
                                  ACC_INDEX + 5);
         }
 

src/freedreno/ir3/ir3_ra.c
@@ -398,7 +398,8 @@ static unsigned int
 ra_select_reg_merged(unsigned int n, BITSET_WORD *regs, void *data)
 {
 	struct ir3_ra_ctx *ctx = data;
-	unsigned int class = ra_get_node_class(ctx->g, n);
+	struct ra_class *classp = ra_get_node_class(ctx->g, n);
+	unsigned int class = ra_class_index(classp);
 	bool half, shared;
 	int sz = ra_class_to_size(class, &half, &shared);
 
@@ -501,10 +502,10 @@ ra_select_reg_merged(unsigned int n, BITSET_WORD *regs, void *data)
 		return ra_select_reg_merged(n, regs, data);
 	}
 
-	if (class == ctx->set->half_classes[0]) {
+	if (classp == ctx->set->half_classes[0]) {
 		int n = r - base;
 		ctx->start_search_reg = (n + 1) % ctx->max_target;
-	} else if (class == ctx->set->classes[0]) {
+	} else if (classp == ctx->set->classes[0]) {
 		int n = (r - base) * 2;
 		ctx->start_search_reg = (n + 1) % ctx->max_target;
 	}

src/freedreno/ir3/ir3_ra.h
@@ -87,16 +87,16 @@ static inline unsigned SHARED_CLASS_REGS(unsigned i)
 /* register-set, created one time, used for all shaders: */
 struct ir3_ra_reg_set {
 	struct ra_regs *regs;
-	unsigned int classes[class_count];
-	unsigned int half_classes[half_class_count];
-	unsigned int shared_classes[shared_class_count];
+	struct ra_class *classes[class_count];
+	struct ra_class *half_classes[half_class_count];
+	struct ra_class *shared_classes[shared_class_count];
 
 	/* pre-fetched tex dst is limited, on current gens to regs
 	 * 0x3f and below.  An additional register class, with one
 	 * vreg, that is setup to conflict with any regs above that
 	 * limit.
 	 */
-	unsigned prefetch_exclude_class;
+	struct ra_class *prefetch_exclude_class;
 	unsigned prefetch_exclude_reg;
 
 	/* The virtual register space flattens out all the classes,

src/freedreno/ir3/ir3_ra_regset.c
@@ -134,7 +134,7 @@ ir3_ra_alloc_reg_set(struct ir3_compiler *compiler, bool mergedregs)
 		set->gpr_to_ra_reg[i] = ralloc_array(set, uint16_t, CLASS_REGS(i));
 
 		for (unsigned j = 0; j < CLASS_REGS(i); j++) {
-			ra_class_add_reg(set->regs, set->classes[i], reg);
+			ra_class_add_reg(set->classes[i], reg);
 
 			set->ra_reg_to_gpr[reg] = j;
 			set->gpr_to_ra_reg[i][j] = reg;
@@ -153,7 +153,7 @@ ir3_ra_alloc_reg_set(struct ir3_compiler *compiler, bool mergedregs)
 				ralloc_array(set, uint16_t, HALF_CLASS_REGS(i));
 
 		for (unsigned j = 0; j < HALF_CLASS_REGS(i); j++) {
-			ra_class_add_reg(set->regs, set->half_classes[i], reg);
+			ra_class_add_reg(set->half_classes[i], reg);
 
 			set->ra_reg_to_gpr[reg] = j;
 			set->gpr_to_ra_reg[base + i][j] = reg;
@@ -172,7 +172,7 @@ ir3_ra_alloc_reg_set(struct ir3_compiler *compiler, bool mergedregs)
 				ralloc_array(set, uint16_t, SHARED_CLASS_REGS(i));
 
 		for (unsigned j = 0; j < SHARED_CLASS_REGS(i); j++) {
-			ra_class_add_reg(set->regs, set->shared_classes[i], reg);
+			ra_class_add_reg(set->shared_classes[i], reg);
 
 			set->ra_reg_to_gpr[reg] = j;
 			set->gpr_to_ra_reg[base + i][j] = reg;
@@ -188,7 +188,7 @@ ir3_ra_alloc_reg_set(struct ir3_compiler *compiler, bool mergedregs)
 	 * knows to allocate prefetch dst regs below the limit:
 	 */
 	set->prefetch_exclude_class = ra_alloc_reg_class(set->regs);
-	ra_class_add_reg(set->regs, set->prefetch_exclude_class, reg);
+	ra_class_add_reg(set->prefetch_exclude_class, reg);
 	set->prefetch_exclude_reg = reg++;
 
 	/*

src/gallium/drivers/etnaviv/etnaviv_compiler_nir_ra.c
@@ -90,11 +90,12 @@ etna_ra_setup(void *mem_ctx)
    /* classes always be created from index 0, so equal to the class enum
     * which represents a register with (c+1) components
     */
+   struct ra_class *classes[NUM_REG_CLASSES];
    for (int c = 0; c < NUM_REG_CLASSES; c++)
-      ra_alloc_reg_class(regs);
+      classes[c] = ra_alloc_reg_class(regs);
    /* add each register of each class */
    for (int r = 0; r < NUM_REG_TYPES * ETNA_MAX_TEMPS; r++)
-      ra_class_add_reg(regs, reg_get_class(r), r);
+      ra_class_add_reg(classes[reg_get_class(r)], r);
    /* set conflicts */
    for (int r = 0; r < ETNA_MAX_TEMPS; r++) {
       for (int i = 0; i < NUM_REG_TYPES; i++) {
@@ -172,7 +173,7 @@ etna_ra_assign(struct etna_compile *c, nir_shader *shader)
          }
       }
 
-      ra_set_node_class(g, i, comp);
+      ra_set_node_class(g, i, ra_get_class_from_index(regs, comp));
    }
 
    nir_foreach_block(block, impl) {
@@ -197,7 +198,7 @@ etna_ra_assign(struct etna_compile *c, nir_shader *shader)
                 deref->var->data.location == FRAG_RESULT_DEPTH) {
                ra_set_node_reg(g, index, REG_FRAG_DEPTH);
             } else {
-               ra_set_node_class(g, index, REG_CLASS_VEC4);
+               ra_set_node_class(g, index, ra_get_class_from_index(regs, REG_CLASS_VEC4));
             }
          } continue;
          case nir_intrinsic_load_input:

upcliujie/mesa

commit 6cbac3ccfb9c7e37d848d63a9e8951ae5bfbe254

util/ra: Fix deserialization of register sets
Set reg_class::regset when deserializing.

Fixes: 95d41a3525d ("ra: Use struct ra_class in the public API.")

src/util/register_allocate.c
@@ -454,6 +454,7 @@ ra_set_deserialize(void *mem_ctx, struct blob_reader *blob)
    for (unsigned int c = 0; c < class_count; c++) {
       struct ra_class *class = rzalloc(regs, struct ra_class);
       regs->classes[c] = class;
+      class->regset = regs;
 
       class->regs = ralloc_array(class, BITSET_WORD, BITSET_WORDS(reg_count));
       blob_copy_bytes(blob, class->regs, BITSET_WORDS(reg_count) *

src/util/tests/register_allocate_test.cpp
@@ -212,7 +212,7 @@ TEST_F(ra_test, aligned_contigregs)
    }
 }
 
-TEST_F(ra_test, DISABLED_serialization_roundtrip)
+TEST_F(ra_test, serialization_roundtrip)
 {
    struct blob blob;
    blob_init(&blob);