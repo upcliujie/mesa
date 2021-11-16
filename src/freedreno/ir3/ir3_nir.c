7388cbe55d0d1684aadc0da32fc5a7b7c8b600a9
Danylo Piliaiev <dpiliaiev@igalia.com>
2021-11-16 14:53:32 +0200

    ir3,turnip: Enable subgroup ops support in all stages on gen4
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/freedreno/ir3/ir3_nir.c
+++ src/freedreno/ir3/ir3_nir.c
@@ -528,7 +528,8 @@ ir3_nir_post_finalize(struct ir3_compiler *compiler, nir_shader *s)
    }
 
    if ((s->info.stage == MESA_SHADER_COMPUTE) ||
-       (s->info.stage == MESA_SHADER_KERNEL)) {
+       (s->info.stage == MESA_SHADER_KERNEL) ||
+       compiler->has_getfiberid) {
       bool progress = false;
       NIR_PASS(progress, s, nir_lower_subgroups,
                &(nir_lower_subgroups_options){
@@ -540,8 +541,11 @@ ir3_nir_post_finalize(struct ir3_compiler *compiler, nir_shader *s)
                   .lower_subgroup_masks = true,
                   .lower_read_invocation_to_cond = true,
                });
+   }
 
-      progress = false;
+   if ((s->info.stage == MESA_SHADER_COMPUTE) ||
+       (s->info.stage == MESA_SHADER_KERNEL)) {
+      bool progress = false;
       NIR_PASS(progress, s, ir3_nir_lower_subgroup_id_cs);
 
       /* ir3_nir_lower_subgroup_id_cs creates extra compute intrinsics which
