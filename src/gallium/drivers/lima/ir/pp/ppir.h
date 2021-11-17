a4a6c6ed7734f7b4257ce13963cd8fced84ea692
Vasily Khoruzhick <anarsoul@gmail.com>
2021-11-16 22:43:52 -0800

    lima/ppir: implement gl_FragDepth support
    Mali4x0 supports writing depth and stencil from fragment shader
    and we've been using it quite a while for depth/stencil buffer reload.
    
    The missing part was specifying output register for depth/stencil.
    To figure it out, I changed reload shader to use register $4 as output
    and poked RSW bits (or rather consecutive 4 bit groups) until tests
    that rely on reload started to pass again.
    
    It turns out that register number for gl_FragDepth/gl_FragStencil is in
    rsw->depth_test and register number for gl_FragColor is in
    rsw->multi_sample and it's repeated 4 times for some reason (likely for
    MSAA?)
    
    With this knowledge we now can modify ppir compiler to support multiple
    store_output intrinsics.
    
    To do that just add destination SSA for store_output to the registers
    list for regalloc and mark them explicitly as output. Since it's never
    read in shader we have to take care about it in liveness analysis -
    basically just mark it alive from the time when it's written to the end
    of the block. If it's live only in the last instruction, mark it as
    live_internal, so regalloc doesn't clobber it.
    
    Then just let regalloc do its job, and then copy register number to the
    shader state and program it in RSW.
    
    The tricky part is gl_FragStencil, since it resides in the same register
    as gl_FragDepth and with the current design of the compiler it's hard to
    merge them. However gl_FragStencil doesn't seem to be part of GL2
    or GLES2, so we can just leave it not implemented.
    
    Also we need to take care of stop bit for instructions - now we can't
    just set it in every instruction that stores output, since there may be
    several outputs. So if there's any store_output instructions in the
    block just mark that block has a stop, and set stop bit in the last
    instruction in the block. The only exception is discard - we always need
    to set stop bit in discard instruction.
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- src/gallium/drivers/lima/ir/pp/ppir.h
+++ src/gallium/drivers/lima/ir/pp/ppir.h
@@ -161,7 +161,7 @@ typedef struct ppir_node {
    struct ppir_instr *instr;
    int instr_pos;
    struct ppir_block *block;
-   bool is_end;
+   bool is_out;
    bool succ_different_block;
 
    /* for scheduler */
@@ -179,9 +179,30 @@ typedef enum {
    ppir_pipeline_reg_discard, /* varying load */
 } ppir_pipeline;
 
+typedef enum {
+   ppir_output_color,
+   ppir_output_depth,
+   ppir_output_num,
+   ppir_output_invalid = -1,
+} ppir_output_type;
+
+static inline ppir_output_type ppir_nir_output_to_ppir(gl_frag_result res)
+{
+   switch (res) {
+   case FRAG_RESULT_COLOR:
+   case FRAG_RESULT_DATA0:
+      return ppir_output_color;
+   case FRAG_RESULT_DEPTH:
+      return ppir_output_depth;
+   default:
+      return ppir_output_invalid;
+   }
+}
+
 typedef struct ppir_reg {
    struct list_head list;
    int index;
+   ppir_output_type out_type;
    int regalloc_index;
    int num_components;
 
@@ -191,6 +212,7 @@ typedef struct ppir_reg {
    bool is_head;
    bool spilled;
    bool undef;
+   bool out_reg;
 } ppir_reg;
 
 typedef enum {
@@ -316,7 +338,7 @@ typedef struct ppir_instr {
 
    ppir_node *slots[PPIR_INSTR_SLOT_NUM];
    ppir_const constant[2];
-   bool is_end;
+   bool stop;
 
    /* for scheduler */
    struct list_head succ_list;
@@ -340,6 +362,7 @@ typedef struct ppir_block {
    struct list_head list;
    struct list_head node_list;
    struct list_head instr_list;
+   bool stop;
 
    struct ppir_block *successors[2];
 
@@ -370,6 +393,8 @@ typedef struct ppir_compiler {
    struct hash_table_u64 *blocks;
    int cur_index;
    int cur_instr_index;
+   int *nir_out_to_type;
+   int *out_type_to_reg;
 
    struct list_head reg_list;
    int reg_num;
