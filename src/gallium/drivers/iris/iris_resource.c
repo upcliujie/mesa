f179b700658cacc66a7d285bc5391e78f2199dd6
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-11-17 13:31:57 +0200

    iris/bufmgr: consider external buffer information at creation
    Because the mmap mode is picked at creation time, marking the BO as
    external has no effect later.
    
    v2: Don't mix conditions on EXPORTED flag (Nanley)
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: f62724ccacffb2 ("iris: Pick a single mmap mode (WB/WC) at BO allocation time")
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5646

--- src/gallium/drivers/iris/iris_resource.c
+++ src/gallium/drivers/iris/iris_resource.c
@@ -447,6 +447,9 @@ iris_resource_alloc_flags(const struct iris_screen *screen,
        util_format_get_num_planes(templ->format) > 1)
       flags |= BO_ALLOC_NO_SUBALLOC;
 
+   if (templ->bind & PIPE_BIND_SHARED)
+      flags |= BO_ALLOC_EXPORTED;
+
    return flags;
 }
 
@@ -1028,10 +1031,8 @@ iris_resource_create_for_buffer(struct pipe_screen *pscreen,
       return NULL;
    }
 
-   if (templ->bind & PIPE_BIND_SHARED) {
-      iris_bo_mark_exported(res->bo);
+   if (templ->bind & PIPE_BIND_SHARED)
       res->base.is_shared = true;
-   }
 
    return &res->base.b;
 }
@@ -1122,10 +1123,8 @@ iris_resource_create_with_modifiers(struct pipe_screen *pscreen,
       map_aux_addresses(screen, res, res->surf.format, 0);
    }
 
-   if (templ->bind & PIPE_BIND_SHARED) {
-      iris_bo_mark_exported(res->bo);
+   if (templ->bind & PIPE_BIND_SHARED)
       res->base.is_shared = true;
-   }
 
    return &res->base.b;
 
