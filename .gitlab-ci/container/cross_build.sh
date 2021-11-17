1491521dc5c627d25d4110dd4a9ffcaf381a75cb
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-11-17 14:22:04 +0200

    ci/cross_build: add libclc/spirv-tools dependencies for coming intel_clc
    We unfortunately have to disable the overlay layer because installing
    spirv-tools:i386 removes glslang-tools:amd64 which is needed for the
    overlay shaders.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- .gitlab-ci/container/cross_build.sh
+++ .gitlab-ci/container/cross_build.sh
@@ -5,15 +5,47 @@ set -o xtrace
 
 export DEBIAN_FRONTEND=noninteractive
 
+if [[ $arch == "s390x" ]]; then
+    LLVM=9
+else
+    LLVM=11
+fi
+
 # Ephemeral packages (installed for this script and removed again at the end)
 STABLE_EPHEMERAL=" \
-        "
-
+      "
+HOST_STABLE_EPHEMERAL=" \
+      cmake \
+      clang \
+      clang-${LLVM} \
+      libclang-cpp${LLVM} \
+      libncurses-dev \
+      libpfm4 \
+      libtinfo-dev \
+      libyaml-0-2 \
+      libz3-dev \
+      llvm-${LLVM} \
+      llvm-${LLVM}-dev \
+      llvm-${LLVM}-runtime \
+      llvm-${LLVM}-tools \
+      llvm-spirv \
+      libllvmspirvlib${LLVM} \
+      python3-pygments \
+      python3-yaml
+      "
 dpkg --add-architecture $arch
 apt-get update
 
 apt-get install -y --no-remove \
-        $STABLE_EPHEMERAL \
+        $HOST_STABLE_EPHEMERAL
+
+# Generate .spv files
+. .gitlab-ci/container/build-libclc.sh
+
+# Now remove host version of clang
+apt-get remove -y $HOST_STABLE_EPHEMERAL
+
+apt-get install -y --no-remove \
         crossbuild-essential-$arch \
         libelf-dev:$arch \
         libexpat1-dev:$arch \
@@ -37,11 +69,7 @@ apt-get install -y --no-remove \
         wget
 
 if [[ $arch != "armhf" ]]; then
-    if [[ $arch == "s390x" ]]; then
-        LLVM=9
-    else
-        LLVM=11
-    fi
+    apt-get remove -y glslang-tools spirv-tools
 
     # llvm-*-tools:$arch conflicts with python3:amd64. Install dependencies only
     # with apt-get, then force-install llvm-*-{dev,tools}:$arch with dpkg to get
@@ -53,6 +81,8 @@ if [[ $arch != "armhf" ]]; then
             libtinfo-dev:$arch \
             libz3-dev:$arch \
             llvm-${LLVM}:$arch \
+            libllvmspirvlib-dev:$arch \
+            spirv-tools:$arch \
             zlib1g
 fi
 
@@ -73,7 +103,7 @@ apt-get purge -y \
 
 # This needs to be done after container_post_build.sh, or apt-get breaks in there
 if [[ $arch != "armhf" ]]; then
-    apt-get download llvm-${LLVM}-{dev,tools}:$arch
-    dpkg -i --force-depends llvm-${LLVM}-*_${arch}.deb
-    rm llvm-${LLVM}-*_${arch}.deb
+    apt-get download llvm-${LLVM}-{dev,tools}:$arch libclc-dev:all
+    dpkg -i --force-depends llvm-${LLVM}-*_${arch}.deb libclc-dev*_all.deb
+    rm llvm-${LLVM}-*_${arch}.deb libclc-dev*_all.deb
 fi
