bbbd70ee7dd9bf348be8ca5ab8c755d6dc2f481c
Ilia Mirkin <imirkin@alum.mit.edu>
2021-11-21 03:18:25 -0500

    nir: remove bogus comment about gl_ClipDistance reading in GL
    gl_ClipDistance most definitely can be read in fragment shaders since
    GLSL 1.30. This is also accessible in ES with EXT_clip_cull_distance.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- src/compiler/nir/nir_lower_clip.c
+++ src/compiler/nir/nir_lower_clip.c
@@ -490,10 +490,6 @@ nir_lower_clip_fs(nir_shader *shader, unsigned ucp_enables,
    if (!ucp_enables)
       return false;
 
-   /* Fragment shaders can't read gl_ClipDistance[] in OpenGL so it will not
-    * have the variable defined, but Vulkan allows this, in which case the
-    * SPIR-V compiler would have already added it as a compact array.
-    */
    if (!fs_has_clip_dist_input_var(shader, in, &ucp_enables))
       create_clipdist_vars(shader, in, ucp_enables, false, use_clipdist_array);
    else
