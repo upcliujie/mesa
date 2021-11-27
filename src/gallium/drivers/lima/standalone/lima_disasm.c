Grima04/mesa-turnip-kgsl

commit 9660427ab7b3fd18a250189667b826086529160e

lima: Print usage if --help is any of the arguments.
Fix defect reported by Coverity Scan.

Structurally dead code (UNREACHABLE)
unreachable: Since the loop increment n++; is unreachable, the loop body
will never execute more than once.

Signed-off-by: Vinson Lee <vlee@freedesktop.org>
Reviewed-by: Vasily Khoruzhick <anarsoul@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6906>

src/gallium/drivers/lima/standalone/lima_disasm.c
@@ -163,8 +163,6 @@ main(int argc, char **argv)
          print_usage();
          return 1;
       }
-
-      break;
    }
 
    char *filename = NULL;

upcliujie/mesa

commit e50836591888882049a7b76f92ba305590e1b335

lima: disasm: use last argument as a filename
Otherwise it fails to open a file.

Fixes: 9660427ab7b3 ("lima: Print usage if --help is any of the arguments.")
Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

src/gallium/drivers/lima/standalone/lima_disasm.c
@@ -166,7 +166,7 @@ main(int argc, char **argv)
    }
 
    char *filename = NULL;
-   filename = argv[n];
+   filename = argv[argc - 1];
 
    uint32_t size = 0;
    uint32_t *prog = extract_shader_binary(filename, &size, &is_frag);