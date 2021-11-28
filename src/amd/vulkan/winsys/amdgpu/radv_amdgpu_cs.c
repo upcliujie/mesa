24bdf197c14b31f8adfe1bf7501390682ffa9bf1
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2021-11-28 17:51:23 +0100

    radv: Add new cs_submit2 winsys call.
    A bit more runtime dependence so we can remove all the syncobj stuff
    in radv itself.

--- src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.c
+++ src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.c
@@ -36,6 +36,9 @@
 #include "radv_debug.h"
 #include "radv_radeon_winsys.h"
 #include "sid.h"
+#include "vk_alloc.h"
+#include "vk_drm_syncobj.h"
+#include "vk_sync.h"
 
 #define GFX6_MAX_CS_SIZE 0xffff8 /* in dwords */
 
@@ -1190,6 +1193,57 @@ radv_amdgpu_winsys_cs_submit(struct radeon_winsys_ctx *_ctx, int queue_idx,
    return result;
 }
 
+static VkResult
+radv_amdgpu_winsys_cs_submit2(struct radeon_winsys_ctx *_ctx, int queue_idx,
+                              struct radeon_cmdbuf **cs_array, unsigned cs_count,
+                              struct radeon_cmdbuf *initial_preamble_cs,
+                              struct radeon_cmdbuf *continue_preamble_cs, uint32_t wait_count,
+                              const struct vk_sync_wait *waits, uint32_t signal_count,
+                              const struct vk_sync_signal *signals, bool can_patch)
+{
+   struct radv_amdgpu_winsys *ws = radv_amdgpu_ctx(_ctx)->ws;
+   struct radv_winsys_sem_info sem_info;
+   memset(&sem_info, 0, sizeof(sem_info));
+
+   VK_MULTIALLOC(ma);
+   vk_multialloc_add(&ma, &sem_info.wait.points, uint64_t, wait_count);
+   vk_multialloc_add(&ma, &sem_info.wait.syncobj, uint32_t, wait_count);
+   vk_multialloc_add(&ma, &sem_info.signal.points, uint64_t, signal_count);
+   vk_multialloc_add(&ma, &sem_info.signal.syncobj, uint32_t, signal_count);
+
+   if (!vk_multialloc_alloc(&ma, vk_default_allocator(), VK_SYSTEM_ALLOCATION_SCOPE_COMMAND))
+      return VK_ERROR_OUT_OF_HOST_MEMORY;
+
+   for (uint32_t i = 0; i < wait_count; ++i) {
+      assert(waits[i].sync->type == &ws->syncobj_sync_type);
+      sem_info.wait.syncobj[i] = ((struct vk_drm_syncobj *)waits[i].sync)->syncobj;
+      sem_info.wait.points[i] = waits[i].wait_value;
+   }
+
+   for (uint32_t i = 0; i < signal_count; ++i) {
+      assert(signals[i].sync->type == &ws->syncobj_sync_type);
+      sem_info.signal.syncobj[i] = ((struct vk_drm_syncobj *)signals[i].sync)->syncobj;
+      sem_info.signal.points[i] = signals[i].signal_value;
+   }
+
+   sem_info.wait.timeline_syncobj_count =
+      (ws->syncobj_sync_type.features & VK_SYNC_FEATURE_TIMELINE) ? wait_count : 0;
+   sem_info.wait.syncobj_count = wait_count - sem_info.wait.timeline_syncobj_count;
+   sem_info.cs_emit_wait = wait_count != 0;
+
+   sem_info.signal.timeline_syncobj_count =
+      (ws->syncobj_sync_type.features & VK_SYNC_FEATURE_TIMELINE) ? signal_count : 0;
+   sem_info.signal.syncobj_count = signal_count - sem_info.signal.timeline_syncobj_count;
+   sem_info.cs_emit_signal = signal_count != 0;
+
+   VkResult result =
+      radv_amdgpu_winsys_cs_submit(_ctx, queue_idx, cs_array, cs_count, initial_preamble_cs,
+                                   continue_preamble_cs, &sem_info, can_patch);
+
+   free(sem_info.wait.points ? sem_info.wait.points : sem_info.signal.points);
+   return result;
+}
+
 static void *
 radv_amdgpu_winsys_get_cpu_addr(void *_cs, uint64_t addr)
 {
@@ -1788,6 +1842,7 @@ radv_amdgpu_cs_init_functions(struct radv_amdgpu_winsys *ws)
    ws->base.cs_add_buffer = radv_amdgpu_cs_add_buffer;
    ws->base.cs_execute_secondary = radv_amdgpu_cs_execute_secondary;
    ws->base.cs_submit = radv_amdgpu_winsys_cs_submit;
+   ws->base.cs_submit2 = radv_amdgpu_winsys_cs_submit2;
    ws->base.cs_dump = radv_amdgpu_winsys_cs_dump;
    ws->base.create_syncobj = radv_amdgpu_create_syncobj;
    ws->base.destroy_syncobj = radv_amdgpu_destroy_syncobj;
