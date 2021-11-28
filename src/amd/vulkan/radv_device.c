59b88c40997d1c8fb2a4e0bafcb82cf6f28849bb
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2021-11-28 17:59:26 +0100

    radv: Use vulkan runtime for device lost.
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -2914,22 +2914,6 @@ radv_device_finish_vrs_image(struct radv_device *device)
                      &device->meta_state.alloc);
 }
 
-VkResult
-_radv_device_set_lost(struct radv_device *device, const char *file, int line, const char *msg, ...)
-{
-   VkResult err;
-   va_list ap;
-
-   p_atomic_inc(&device->lost);
-
-   va_start(ap, msg);
-   err =
-      __vk_errorv(device, VK_ERROR_DEVICE_LOST, file, line, msg, ap);
-   va_end(ap);
-
-   return err;
-}
-
 VKAPI_ATTR VkResult VKAPI_CALL
 radv_CreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo *pCreateInfo,
                   const VkAllocationCallbacks *pAllocator, VkDevice *pDevice)
@@ -4813,7 +4797,7 @@ fail:
        * VK_ERROR_DEVICE_LOST to ensure the clients do not attempt
        * to submit the same job again to this device.
        */
-      result = radv_device_set_lost(queue->device, "vkQueueSubmit() failed");
+      result = vk_device_set_lost(&queue->device->vk, "vkQueueSubmit() failed");
    }
 
    radv_free_temp_syncobjs(queue->device, submission->temporary_semaphore_part_count,
@@ -5032,7 +5016,7 @@ radv_QueueSubmit2KHR(VkQueue _queue, uint32_t submitCount, const VkSubmitInfo2KH
    uint32_t fence_idx = 0;
    bool flushed_caches = false;
 
-   if (radv_device_is_lost(queue->device))
+   if (vk_device_is_lost(&queue->device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    if (fence != VK_NULL_HANDLE) {
@@ -5105,7 +5089,7 @@ radv_QueueWaitIdle(VkQueue _queue)
 {
    RADV_FROM_HANDLE(radv_queue, queue, _queue);
 
-   if (radv_device_is_lost(queue->device))
+   if (vk_device_is_lost(&queue->device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    mtx_lock(&queue->pending_mutex);
@@ -5117,10 +5101,10 @@ radv_QueueWaitIdle(VkQueue _queue)
    if (!queue->device->ws->ctx_wait_idle(
           queue->hw_ctx, radv_queue_family_to_ring(queue->vk.queue_family_index),
           queue->vk.index_in_family)) {
-      return radv_device_set_lost(queue->device,
-                                  "Failed to wait for a '%s' queue "
-                                  "to be idle. GPU hang ?",
-                                  radv_get_queue_family_name(queue));
+      return vk_device_set_lost(&queue->device->vk,
+                                "Failed to wait for a '%s' queue "
+                                "to be idle. GPU hang ?",
+                                radv_get_queue_family_name(queue));
    }
 
    return VK_SUCCESS;
@@ -5717,7 +5701,7 @@ radv_QueueBindSparse(VkQueue _queue, uint32_t bindInfoCount, const VkBindSparseI
    RADV_FROM_HANDLE(radv_queue, queue, _queue);
    uint32_t fence_idx = 0;
 
-   if (radv_device_is_lost(queue->device))
+   if (vk_device_is_lost(&queue->device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    if (fence != VK_NULL_HANDLE) {
@@ -5866,7 +5850,7 @@ radv_WaitForFences(VkDevice _device, uint32_t fenceCount, const VkFence *pFences
    RADV_FROM_HANDLE(radv_device, device, _device);
    uint32_t *handles;
 
-   if (radv_device_is_lost(device))
+   if (vk_device_is_lost(&device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    timeout = radv_get_absolute_timeout(timeout);
@@ -5924,7 +5908,7 @@ radv_GetFenceStatus(VkDevice _device, VkFence _fence)
    struct radv_fence_part *part =
       fence->temporary.kind != RADV_FENCE_NONE ? &fence->temporary : &fence->permanent;
 
-   if (radv_device_is_lost(device))
+   if (vk_device_is_lost(&device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    bool success = device->ws->wait_syncobj(device->ws, &part->syncobj, 1, true, 0);
@@ -6194,7 +6178,7 @@ radv_GetSemaphoreCounterValue(VkDevice _device, VkSemaphore _semaphore, uint64_t
    RADV_FROM_HANDLE(radv_device, device, _device);
    RADV_FROM_HANDLE(radv_semaphore, semaphore, _semaphore);
 
-   if (radv_device_is_lost(device))
+   if (vk_device_is_lost(&device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    struct radv_semaphore_part *part = semaphore->temporary.kind != RADV_SEMAPHORE_NONE
@@ -6253,7 +6237,7 @@ radv_WaitSemaphores(VkDevice _device, const VkSemaphoreWaitInfo *pWaitInfo, uint
 {
    RADV_FROM_HANDLE(radv_device, device, _device);
 
-   if (radv_device_is_lost(device))
+   if (vk_device_is_lost(&device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    uint64_t abs_timeout = radv_get_absolute_timeout(timeout);
@@ -6404,7 +6388,7 @@ radv_GetEventStatus(VkDevice _device, VkEvent _event)
    RADV_FROM_HANDLE(radv_device, device, _device);
    RADV_FROM_HANDLE(radv_event, event, _event);
 
-   if (radv_device_is_lost(device))
+   if (vk_device_is_lost(&device->vk))
       return VK_ERROR_DEVICE_LOST;
 
    if (*event->map == 1)
