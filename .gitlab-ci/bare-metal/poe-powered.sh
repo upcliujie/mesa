cecbb293898e4b757fe17dc6d3ae4c9f9e9c48e2
Emma Anholt <emma@anholt.net>
2021-12-02 16:14:30 -0800

    ci/nouveau: Add a manual run for the Jetson Nano (GM20B).
    It's got some serious issues with boot reliability (MMU faults during
    power-on), and the test suite is full of flakes around transform feedback,
    atomics, and tess.  But, I hope it can be useful for regression testing
    core Mesa reworks.
    
    Acked-by: Ilia Mirkin <imirkin@alum.mit.edu>

--- .gitlab-ci/bare-metal/poe-powered.sh
+++ .gitlab-ci/bare-metal/poe-powered.sh
@@ -99,7 +99,27 @@ fi
 [ -d $BM_BOOTFS/lib/modules ] && rsync -a --delete $BM_BOOTFS/lib/modules/ /nfs/usr/lib/modules/
 
 # Install kernel image + bootloader files
-rsync -a --delete $BM_BOOTFS/boot/ /tftp/
+rsync -aL --delete $BM_BOOTFS/ /tftp/
+
+# XXX HAX
+#wget https://people.freedesktop.org/~anholt/kernel-test/Image -O /tftp/Image
+#mkdir -p /nfs/lib/modules/5.16.0-00011-ge42f4ed25bfa
+#wget https://people.freedesktop.org/~anholt/kernel-test/nouveau.ko -O /nfs/lib/modules/5.16.0-00011-ge42f4ed25bfa/nouveau.ko
+#wget https://people.freedesktop.org/~anholt/kernel-test/tegra-drm.ko -O /nfs/lib/modules/5.16.0-00011-ge42f4ed25bfa/tegra-drm.ko
+
+# Set up the pxelinux config for Jetson Nano
+mkdir -p /tftp/pxelinux.cfg
+cat <<EOF >/tftp/pxelinux.cfg/default-arm-tegra210-p3450-0000
+PROMPT 0
+TIMEOUT 30
+DEFAULT primary
+MENU TITLE jetson nano boot options
+LABEL primary
+      MENU LABEL CI kernel on TFTP
+      LINUX Image
+      FDT tegra210-p3450-0000.dtb
+      APPEND \${cbootargs} $BM_CMDLINE
+EOF
 
 # Create the rootfs in the NFS directory
 mkdir -p /nfs/results
