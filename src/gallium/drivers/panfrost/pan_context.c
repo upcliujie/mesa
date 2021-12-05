a0b4d3e7325ba4387db98fe69068fe3163938a8d
Alyssa Rosenzweig <alyssa@collabora.com>
2021-12-05 15:20:30 -0500

    panfrost: Set DIRTY_VERTEX
    I believe we can dirty track this state meaningfully on Valhall.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/gallium/drivers/panfrost/pan_context.c
+++ src/gallium/drivers/panfrost/pan_context.c
@@ -287,6 +287,7 @@ panfrost_bind_vertex_elements_state(
 {
         struct panfrost_context *ctx = pan_context(pctx);
         ctx->vertex = hwcso;
+        ctx->dirty |= PAN_DIRTY_VERTEX;
 }
 
 static void *
@@ -581,6 +582,8 @@ panfrost_set_vertex_buffers(
         util_set_vertex_buffers_mask(ctx->vertex_buffers, &ctx->vb_mask, buffers,
                                      start_slot, num_buffers, unbind_num_trailing_slots,
                                      take_ownership);
+
+        ctx->dirty |= PAN_DIRTY_VERTEX;
 }
 
 static void
