6c7c2a602fb4d0fc4f654c487da605c13e83c525
Emma Anholt <emma@anholt.net>
2021-12-06 12:11:43 -0800

    r300: Request NIR shaders from mesa/st and use NIR-to-TGSI.
    This brings us into parity on state tracker paths with most other
    supported drivers, and a lot of additional optimization on our shaders.

--- src/gallium/drivers/r300/r300_screen.h
+++ src/gallium/drivers/r300/r300_screen.h
@@ -103,6 +103,7 @@ radeon_winsys(struct pipe_screen *screen) {
 #define DBG_NO_ZMASK    (1 << 21)
 #define DBG_NO_HIZ      (1 << 22)
 #define DBG_NO_CMASK    (1 << 23)
+#define DBG_USE_TGSI    (1 << 24)
 /* Statistics. */
 #define DBG_P_STAT      (1 << 25)
 /*@}*/
