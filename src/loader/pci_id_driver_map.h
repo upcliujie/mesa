aerisarn/mesa-uwp

commit 0cad451f007f366d94e058b7c717b96723a3e6cd

classic/i915: Remove driver
This is only going to be supported in the Amber branch

Reviewed-by: Emma Anholt <emma@anholt.net>
Acked-by: Jason Ekstrand <jason@jlekstrand.net>
Acked-by: Kenneth Graunke <kenneth@whitecape.org>
Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10153>

src/loader/pci_id_driver_map.h
@@ -8,18 +8,6 @@
 #  error "Only include from loader.c"
 #endif
 
-static const int i830_chip_ids[] = {
-#define CHIPSET(chip, desc, name) chip,
-#include "pci_ids/i830_pci_ids.h"
-#undef CHIPSET
-};
-
-static const int i915_chip_ids[] = {
-#define CHIPSET(chip, desc, name) chip,
-#include "pci_ids/i915_pci_ids.h"
-#undef CHIPSET
-};
-
 static const int i965_chip_ids[] = {
 #define CHIPSET(chip, family, family_str, name) chip,
 #include "pci_ids/i965_pci_ids.h"
@@ -65,8 +53,6 @@ static const struct {
    int num_chips_ids;
    bool (*predicate)(int fd);
 } driver_map[] = {
-   { 0x8086, "i830", i830_chip_ids, ARRAY_SIZE(i830_chip_ids) },
-   { 0x8086, "i915", i915_chip_ids, ARRAY_SIZE(i915_chip_ids) },
    { 0x8086, "i965", i965_chip_ids, ARRAY_SIZE(i965_chip_ids) },
    { 0x8086, "crocus", crocus_chip_ids, ARRAY_SIZE(crocus_chip_ids) },
    { 0x8086, "iris", NULL, -1, is_kernel_i915 },

upcliujie/mesa

commit e4fe124ecf12d67079987c21f1466ab03e2d2dd5

loader: Restore i915g support.
The cleanup of i915c cleaned up our PCI ID list.

Fixes: 0cad451f007f ("classic/i915: Remove driver")

src/loader/pci_id_driver_map.h
@@ -8,6 +8,12 @@
 #  error "Only include from loader.c"
 #endif
 
+static const int i915_chip_ids[] = {
+#define CHIPSET(chip, desc, name) chip,
+#include "pci_ids/i915_pci_ids.h"
+#undef CHIPSET
+};
+
 static const int crocus_chip_ids[] = {
 #define CHIPSET(chip, family, family_str, name) chip,
 #include "pci_ids/crocus_pci_ids.h"
@@ -47,6 +53,7 @@ static const struct {
    int num_chips_ids;
    bool (*predicate)(int fd);
 } driver_map[] = {
+   { 0x8086, "i915", i915_chip_ids, ARRAY_SIZE(i915_chip_ids) },
    { 0x8086, "crocus", crocus_chip_ids, ARRAY_SIZE(crocus_chip_ids) },
    { 0x8086, "iris", NULL, -1, is_kernel_i915 },
    { 0x1002, "r300", r300_chip_ids, ARRAY_SIZE(r300_chip_ids) },