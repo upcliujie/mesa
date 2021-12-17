2447a57e3b76a71134088e499eec80e8c1e316c0
Caio Oliveira <caio.oliveira@intel.com>
2021-12-17 11:20:10 -0800

    ci/windows: Remove line numbers of SPIR-V errors in spirv2dxil tests

--- .gitlab-ci/windows/spirv2dxil_check.ps1
+++ .gitlab-ci/windows/spirv2dxil_check.ps1
@@ -47,7 +47,7 @@ foreach ($spvasm in $spvasm_files) {
         }
         else {
             Write-Output "$subtest Fail"
-            $sanitized_output = $s2d_output -replace ', file .+, line \d+'
+            $sanitized_output = $s2d_output -replace ', file .+, line \d+' | Select-String -Pattern '^    In file ' -NotMatch
             Write-Output "$sanitized_output`n"
         }
     }
