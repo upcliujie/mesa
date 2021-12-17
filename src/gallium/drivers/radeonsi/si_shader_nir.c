aerisarn/mesa-uwp

commit b59bb9c07ae47f9f59bd7fcad6af77c591bb3611

radeonsi: force flat for PrimID early in si_nir_scan_shader
Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12343>

src/gallium/drivers/radeonsi/si_shader_nir.c
@@ -114,7 +114,11 @@ static void scan_io_usage(struct si_shader_info *info, nir_intrinsic_instr *intr
          unsigned loc = driver_location + i;
 
          info->input_semantic[loc] = semantic + i;
-         info->input_interpolate[loc] = interp;
+
+         if (semantic == SYSTEM_VALUE_PRIMITIVE_ID)
+            info->input_interpolate[loc] = INTERP_MODE_FLAT;
+         else
+            info->input_interpolate[loc] = interp;
 
          if (mask) {
             info->input_usage_mask[loc] |= mask;

src/gallium/drivers/radeonsi/si_state_shaders.c
@@ -3531,8 +3531,7 @@ static unsigned si_get_ps_input_cntl(struct si_context *sctx, struct si_shader *
    unsigned offset, ps_input_cntl = 0;
 
    if (interpolate == INTERP_MODE_FLAT ||
-       (interpolate == INTERP_MODE_COLOR && rs->flatshade) ||
-       semantic == VARYING_SLOT_PRIMITIVE_ID)
+       (interpolate == INTERP_MODE_COLOR && rs->flatshade))
       ps_input_cntl |= S_028644_FLAT_SHADE(1);
 
    if (semantic == VARYING_SLOT_PNTC ||

upcliujie/mesa

commit 34f1cdfd4499020a4988c01a2519da4d06fd74f2

radeonsi/nir: Check for VARYING_SLOT_PRIMITIVE_ID not SYSTEM_VALUE
This function is called on load/store_input/output.  It makes no sense
for it to get a SYSTEM_VALUE enum.  This only doesn't explode because
SYSTEM_VALUE_PRIMITIVE_ID happens to be below VARYING_SLOT_VAR0 so it
doesn't interact with any actual varyings.  The next commit is going to
add another system value which will push SYSTEM_VALUE_PRIMITIVE_ID up by
one so it will equal VARYING_SLOT_VAR0 and then the first FS input will
always get smashed to flat which isn't what we want.

Fixes: b59bb9c07ae4 ("radeonsi: force flat for PrimID early in si_nir_scan_shader")

src/gallium/drivers/radeonsi/si_shader_nir.c
@@ -148,7 +148,7 @@ static void scan_io_usage(struct si_shader_info *info, nir_intrinsic_instr *intr
 
          info->input[loc].semantic = semantic + i;
 
-         if (semantic == SYSTEM_VALUE_PRIMITIVE_ID)
+         if (semantic == VARYING_SLOT_PRIMITIVE_ID)
             info->input[loc].interpolate = INTERP_MODE_FLAT;
          else
             info->input[loc].interpolate = interp;