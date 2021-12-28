96cbf3301cef5f15b7a8fd23ea65029aa63578a9
Qiang Yu <yuq825@gmail.com>
2021-12-28 22:14:11 +0800

    glsl: ir_texture support sprase texture
    Sparse ir_texture will set is_sparse and use struct type to
    hold both residency code and sampled texel.
    
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/compiler/glsl/ir.h
+++ src/compiler/glsl/ir.h
@@ -1878,30 +1878,30 @@ enum ir_texture_opcode {
  * selected from \c ir_texture_opcodes.  In the printed IR, these will
  * appear as:
  *
- *                                    Texel offset (0 or an expression)
- *                                    | Projection divisor
- *                                    | |  Shadow comparator
- *                                    | |  |
- *                                    v v  v
- * (tex <type> <sampler> <coordinate> 0 1 ( ))
- * (txb <type> <sampler> <coordinate> 0 1 ( ) <bias>)
- * (txl <type> <sampler> <coordinate> 0 1 ( ) <lod>)
- * (txd <type> <sampler> <coordinate> 0 1 ( ) (dPdx dPdy))
- * (txf <type> <sampler> <coordinate> 0       <lod>)
+ *                                             Texel offset (0 or an expression)
+ *                                             | Projection divisor
+ *                                             | |  Shadow comparator
+ *                                             | |  |
+ *                                             v v  v
+ * (tex <sparse> <type> <sampler> <coordinate> 0 1 ( ))
+ * (txb <sparse> <type> <sampler> <coordinate> 0 1 ( ) <bias>)
+ * (txl <sparse> <type> <sampler> <coordinate> 0 1 ( ) <lod>)
+ * (txd <sparse> <type> <sampler> <coordinate> 0 1 ( ) (dPdx dPdy))
+ * (txf <sparse> <type> <sampler> <coordinate> 0       <lod>)
  * (txf_ms
- *      <type> <sampler> <coordinate>         <sample_index>)
- * (txs <type> <sampler> <lod>)
- * (lod <type> <sampler> <coordinate>)
- * (tg4 <type> <sampler> <coordinate> <offset> <component>)
- * (query_levels <type> <sampler>)
- * (samples_identical <sampler> <coordinate>)
+ *      <sparse> <type> <sampler> <coordinate>         <sample_index>)
+ * (txs <sparse> <type> <sampler> <lod>)
+ * (lod <sparse> <type> <sampler> <coordinate>)
+ * (tg4 <sparse> <type> <sampler> <coordinate> <offset> <component>)
+ * (query_levels <sparse> <type> <sampler>)
+ * (samples_identical <sparse> <sampler> <coordinate>)
  */
 class ir_texture : public ir_rvalue {
 public:
-   ir_texture(enum ir_texture_opcode op)
+   ir_texture(enum ir_texture_opcode op, bool sparse = false)
       : ir_rvalue(ir_type_texture),
         op(op), sampler(NULL), coordinate(NULL), projector(NULL),
-        shadow_comparator(NULL), offset(NULL)
+        shadow_comparator(NULL), offset(NULL), is_sparse(sparse)
    {
       memset(&lod_info, 0, sizeof(lod_info));
    }
@@ -1972,6 +1972,9 @@ public:
 	 ir_rvalue *dPdy;	/**< Partial derivative of coordinate wrt Y */
       } grad;
    } lod_info;
+
+   /* Whether a sparse texture */
+   bool is_sparse;
 };
 
 
