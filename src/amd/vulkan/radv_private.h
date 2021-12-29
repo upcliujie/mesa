c1b05cd311d85150689c5eb82002371ea2e09cf3
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2021-12-29 10:24:49 +0100

    radv: remove set but unused radv_buffer::shareable
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -1000,8 +1000,6 @@ struct radv_buffer {
    /* Set when bound */
    struct radeon_winsys_bo *bo;
    VkDeviceSize offset;
-
-   bool shareable;
 };
 
 void radv_buffer_init(struct radv_buffer *buffer, struct radv_device *device,
