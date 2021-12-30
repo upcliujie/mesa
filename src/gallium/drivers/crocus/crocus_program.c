aerisarn/mesa-uwp

commit 3077d968568213bfe2990ddc4ada2763d9701418

crocus: Clamp VS point sizes to the HW limits as required.
Fixes piglit vs-point-size-zero.

Reviewed-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14238>

src/gallium/drivers/crocus/crocus_program.c
@@ -2701,6 +2701,9 @@ crocus_create_uncompiled_shader(struct pipe_context *ctx,
    NIR_PASS_V(nir, brw_nir_lower_storage_image, devinfo);
    NIR_PASS_V(nir, crocus_lower_storage_image_derefs);
 
+   if (nir->info.stage != MESA_SHADER_FRAGMENT && nir->info.stage != MESA_SHADER_COMPUTE)
+      NIR_PASS_V(nir, nir_lower_point_size, 1.0, 255.0);
+
    nir_sweep(nir);
 
    ish->program_id = get_new_program_id(screen);

upcliujie/mesa

commit 169adabcffb97a67d89e9436bfc7d8b89777dc55

crocus: only clamp point size on last stage.
This fixes a regression in tests that pass unclamped point size
values between stages.

Fixes: 3077d9685682 ("crocus: Clamp VS point sizes to the HW limits as required.")

src/gallium/drivers/crocus/crocus_program.c
@@ -1200,6 +1200,9 @@ crocus_compile_vs(struct crocus_context *ice,
       nir_shader_gather_info(nir, impl);
    }
 
+   if (key->clamp_pointsize)
+      nir_lower_point_size(nir, 1.0, 255.0);
+
    prog_data->use_alt_mode = nir->info.is_arb_asm;
 
    crocus_setup_uniforms(compiler, mem_ctx, nir, prog_data, &system_values,
@@ -1583,6 +1586,9 @@ crocus_compile_tes(struct crocus_context *ice,
       nir_shader_gather_info(nir, impl);
    }
 
+   if (key->clamp_pointsize)
+      nir_lower_point_size(nir, 1.0, 255.0);
+
    crocus_setup_uniforms(compiler, mem_ctx, nir, prog_data, &system_values,
                          &num_system_values, &num_cbufs);
    crocus_lower_swizzles(nir, &key->base.tex);
@@ -1719,6 +1725,9 @@ crocus_compile_gs(struct crocus_context *ice,
       nir_lower_vars_to_ssa(nir);
       nir_shader_gather_info(nir, impl);
    }
+
+   if (key->clamp_pointsize)
+      nir_lower_point_size(nir, 1.0, 255.0);
 
    crocus_setup_uniforms(compiler, mem_ctx, nir, prog_data, &system_values,
                          &num_system_values, &num_cbufs);
@@ -2700,9 +2709,6 @@ crocus_create_uncompiled_shader(struct pipe_context *ctx,
 
    NIR_PASS_V(nir, brw_nir_lower_storage_image, devinfo);
    NIR_PASS_V(nir, crocus_lower_storage_image_derefs);
-
-   if (nir->info.stage != MESA_SHADER_FRAGMENT && nir->info.stage != MESA_SHADER_COMPUTE)
-      NIR_PASS_V(nir, nir_lower_point_size, 1.0, 255.0);
 
    nir_sweep(nir);
 

src/gallium/drivers/crocus/crocus_state.c
@@ -4689,6 +4689,10 @@ crocus_populate_vs_key(const struct crocus_context *ice,
        last_stage == MESA_SHADER_VERTEX)
       key->nr_userclip_plane_consts = cso_rast->num_clip_plane_consts;
 
+   if (last_stage == MESA_SHADER_VERTEX &&
+       info->outputs_written & (VARYING_BIT_PSIZ))
+      key->clamp_pointsize = 1;
+
 #if GFX_VER <= 5
    key->copy_edgeflag = (cso_rast->cso.fill_back != PIPE_POLYGON_MODE_FILL ||
                          cso_rast->cso.fill_front != PIPE_POLYGON_MODE_FILL);
@@ -4732,6 +4736,10 @@ crocus_populate_tes_key(const struct crocus_context *ice,
        (info->outputs_written & (VARYING_BIT_POS | VARYING_BIT_CLIP_VERTEX)) &&
        last_stage == MESA_SHADER_TESS_EVAL)
       key->nr_userclip_plane_consts = cso_rast->num_clip_plane_consts;
+
+   if (last_stage == MESA_SHADER_TESS_EVAL &&
+       info->outputs_written & (VARYING_BIT_PSIZ))
+      key->clamp_pointsize = 1;
 }
 
 /**
@@ -4749,6 +4757,10 @@ crocus_populate_gs_key(const struct crocus_context *ice,
        (info->outputs_written & (VARYING_BIT_POS | VARYING_BIT_CLIP_VERTEX)) &&
        last_stage == MESA_SHADER_GEOMETRY)
       key->nr_userclip_plane_consts = cso_rast->num_clip_plane_consts;
+
+   if (last_stage == MESA_SHADER_GEOMETRY &&
+       info->outputs_written & (VARYING_BIT_PSIZ))
+      key->clamp_pointsize = 1;
 }
 
 static inline GLenum