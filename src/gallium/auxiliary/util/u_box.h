4274fdcd9607fac3867ca96329921757ae959672
Alyssa Rosenzweig <alyssa@collabora.com>
2022-01-04 16:16:06 -0500

    gallium/util: Add pixel->blocks box helper
    There is a lot of unit confusion in Gallium due to pixels versus blocks
    matching only with uncompressed textures. Add a helper to do a common
    pixels->blocks unit conversion required in multiple drivers.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/gallium/auxiliary/util/u_box.h
+++ src/gallium/auxiliary/util/u_box.h
@@ -239,4 +239,22 @@ u_box_minify_3d(struct pipe_box *dst,
    dst->depth = MAX2(src->depth >> l, 1);
 }
 
+/* Converts a box @src specified in pixels to an equivalent box @dst specified
+ * in blocks, where the boxes represent a region-of-interest of an image with
+ * format @format. This is trivial (a copy) for uncompressed formats.
+ */
+static inline void
+u_box_pixels_to_blocks(struct pipe_box *dst,
+                       const struct pipe_box *src, enum pipe_format format)
+{
+   u_box_3d(
+         src->x / util_format_get_blockwidth(format),
+         src->y / util_format_get_blockheight(format),
+         src->z,
+         DIV_ROUND_UP(src->width, util_format_get_blockwidth(format)),
+         DIV_ROUND_UP(src->height, util_format_get_blockheight(format)),
+         src->depth,
+         dst);
+}
+
 #endif
