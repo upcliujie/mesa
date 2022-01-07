19a0443c1a43f6f0041428fb1d015ee0a5b1e24e
Dave Airlie <airlied@redhat.com>
2022-01-07 12:47:08 +1000

    glsl/nir: don't pass gl_context to the convertor routine.

--- src/compiler/glsl/glsl_to_nir.h
+++ src/compiler/glsl/glsl_to_nir.h
@@ -37,7 +37,7 @@ extern "C" {
 struct gl_context;
 struct gl_shader_program;
 
-nir_shader *glsl_to_nir(struct gl_context *ctx,
+nir_shader *glsl_to_nir(const struct gl_constants *consts,
                         const struct gl_shader_program *shader_prog,
                         gl_shader_stage stage,
                         const nir_shader_compiler_options *options);
