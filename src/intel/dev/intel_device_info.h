30e8149d3c127f673cce702cce2dc501fc6a7a28
Jordan Justen <jordan.l.justen@intel.com>
2022-01-08 00:01:55 -0800

    intel/devinfo: Add apply_hwconfig to conditionally use hwconfig values
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- src/intel/dev/intel_device_info.h
+++ src/intel/dev/intel_device_info.h
@@ -362,6 +362,11 @@ struct intel_device_info
     * no_hw is true when the chipset_id pci device id has been overridden
     */
    bool no_hw;
+
+   /**
+    * apply_hwconfig is true when the platform should apply hwconfig values
+    */
+   bool apply_hwconfig;
    /** @} */
 };
 
