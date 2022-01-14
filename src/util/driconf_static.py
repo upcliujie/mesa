Grima04/mesa-turnip-kgsl

commit 5740ac370142aa626d7a03b16d63fa3fb160f100

xmlconfig: Add static driconfig support
For platforms which do not have support for parsing driconf from xml
files on the filesystem, build in driconf tables generated from
00-mesa-defaults.conf at compile time and use that for option matching.

This allows us to have game/engine specific overrides built in to mesa.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9179>

src/util/xmlconfig.c
@@ -1016,6 +1016,80 @@ parseConfigDir(struct OptConfData *data, const char *dirname)
 
    free(entries);
 }
+#else
+#  include "driconf_static.h"
+
+static void
+parseStaticOptions(struct OptConfData *data, const struct driconf_option *options,
+                   unsigned num_options)
+{
+   if (data->ignoringDevice || data->ignoringApp)
+      return;
+   for (unsigned i = 0; i < num_options; i++) {
+      const char *optattr[] = {
+         "name", options[i].name,
+         "value", options[i].value,
+         NULL
+      };
+      parseOptConfAttr(data, optattr);
+   }
+}
+
+static void
+parseStaticConfig(struct OptConfData *data)
+{
+   data->ignoringDevice = 0;
+   data->ignoringApp = 0;
+   data->inDriConf = 0;
+   data->inDevice = 0;
+   data->inApp = 0;
+   data->inOption = 0;
+
+   for (unsigned i = 0; i < ARRAY_SIZE(driconf); i++) {
+      const struct driconf_device *d = driconf[i];
+      const char *devattr[] = {
+         "driver", d->driver,
+         NULL
+      };
+
+      data->ignoringDevice = 0;
+      data->inDevice++;
+      parseDeviceAttr(data, devattr);
+      data->inDevice--;
+
+      data->inApp++;
+
+      for (unsigned j = 0; j < d->num_engines; j++) {
+         const struct driconf_engine *e = &d->engines[j];
+         const char *engattr[] = {
+            "engine_name_match", e->engine_name_match,
+            "engine_versions", e->engine_versions,
+            NULL
+         };
+
+         data->ignoringApp = 0;
+         parseEngineAttr(data, engattr);
+         parseStaticOptions(data, e->options, e->num_options);
+      }
+
+      for (unsigned j = 0; j < d->num_applications; j++) {
+         const struct driconf_application *a = &d->applications[j];
+         const char *appattr[] = {
+            "name", a->name,
+            "executable", a->executable,
+            "application_name_match", a->application_name_match,
+            "application_versions", a->application_versions,
+            NULL
+         };
+
+         data->ignoringApp = 0;
+         parseAppAttr(data, appattr);
+         parseStaticOptions(data, a->options, a->num_options);
+      }
+
+      data->inApp--;
+   }
+}
 #endif /* WITH_XMLCONFIG */
 
 /** \brief Initialize an option cache based on info */
@@ -1093,6 +1167,8 @@ driParseConfigFiles(driOptionCache *cache, const driOptionCache *info,
       snprintf(filename, PATH_MAX, "%s/.drirc", home);
       parseOneConfigFile(&userData, filename);
    }
+#else
+   parseStaticConfig(&userData);
 #endif /* WITH_XMLCONFIG */
 }
 

upcliujie/mesa

commit c985b3524f8509a16f18b72ccce0e6e9c2964ef8

driconf: Fix unhandled tags in static conf
A rule with executable_regexp tag would match every executable
without this fix and force_glsl_extensions_warn would be always
set to true which breaks some dEQP tests.

Fixes: 5740ac37014 ("xmlconfig: Add static driconfig support")

src/util/driconf_static.py
@@ -46,6 +46,7 @@ class Application(object):
         self.cname = cname('application')
         self.name = xml.attrib['name']
         self.executable = xml.attrib.get('executable', None)
+        self.executable_regexp = xml.attrib.get('executable_regexp', None)
         self.sha1 = xml.attrib.get('sha1', None)
         self.application_name_match = xml.attrib.get('application_name_match', None)
         self.application_versions = xml.attrib.get('application_versions', None)
@@ -118,6 +119,7 @@ struct driconf_option {
 struct driconf_application {
     const char *name;
     const char *executable;
+    const char *executable_regexp;
     const char *sha1;
     const char *application_name_match;
     const char *application_versions;
@@ -178,6 +180,9 @@ static const struct driconf_application ${device.cname}_applications[] = {
     { .name = "${application.name}",
 %        if application.executable:
       .executable = "${application.executable}",
+%        endif
+%        if application.executable_regexp:
+      .executable_regexp = "${application.executable_regexp}",
 %        endif
 %        if application.sha1:
       .sha1 = "${application.sha1}",

src/util/xmlconfig.c
@@ -1123,6 +1123,8 @@ parseStaticConfig(struct OptConfData *data)
          const char *appattr[] = {
             "name", a->name,
             "executable", a->executable,
+            "executable_regexp", a->executable_regexp,
+            "sha1", a->sha1,
             "application_name_match", a->application_name_match,
             "application_versions", a->application_versions,
             NULL