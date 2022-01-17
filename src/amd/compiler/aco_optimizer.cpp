f2c8c37c3e71fe3b3813e4e892496d8047f9e748
Rhys Perry <pendingchaos02@gmail.com>
2022-01-17 16:52:10 +0000

    aco: use v_fma_mix to combine mul/add/fma output conversions
    fossil-db (Sienna Cichlid):
    Totals from 42 (0.03% of 134913) affected shaders:
    CodeSize: 596904 -> 596332 (-0.10%); split: -0.10%, +0.00%
    Instrs: 110194 -> 109902 (-0.26%)
    Latency: 1205239 -> 1204915 (-0.03%); split: -0.03%, +0.00%
    InvThroughput: 189697 -> 189375 (-0.17%)
    VClause: 1365 -> 1366 (+0.07%)
    Copies: 5429 -> 5414 (-0.28%); split: -0.33%, +0.06%
    Branches: 4034 -> 4026 (-0.20%)
    
    fossil-db (Navi):
    Totals from 42 (0.03% of 134913) affected shaders:
    CodeSize: 596044 -> 595488 (-0.09%); split: -0.10%, +0.00%
    Instrs: 110845 -> 110540 (-0.28%)
    Latency: 1206131 -> 1205747 (-0.03%)
    InvThroughput: 190178 -> 189809 (-0.19%)
    VClause: 1372 -> 1370 (-0.15%); split: -0.29%, +0.15%
    Copies: 5671 -> 5641 (-0.53%); split: -0.56%, +0.04%
    Branches: 4033 -> 4025 (-0.20%)
    
    fossil-db (Vega):
    Totals from 42 (0.03% of 135048) affected shaders:
    CodeSize: 605824 -> 605352 (-0.08%); split: -0.08%, +0.00%
    Instrs: 115975 -> 115706 (-0.23%)
    Latency: 1399845 -> 1398912 (-0.07%)
    InvThroughput: 489901 -> 489442 (-0.09%)
    VClause: 1314 -> 1311 (-0.23%); split: -0.38%, +0.15%
    Copies: 9673 -> 9666 (-0.07%); split: -0.12%, +0.05%
    Branches: 4025 -> 4024 (-0.02%)
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>

--- src/amd/compiler/aco_optimizer.cpp
+++ src/amd/compiler/aco_optimizer.cpp
@@ -124,6 +124,8 @@ enum Label {
    label_dpp8 = 1ull << 36,
    label_f2f16_in = 1ull << 37,
    label_f2f32_in = 1ull << 38,
+   label_f2f16_out = 1ull << 39,
+   label_f2f32_out = 1ull << 40,
 };
 
 static constexpr uint64_t instr_usedef_labels =
@@ -131,7 +133,8 @@ static constexpr uint64_t instr_usedef_labels =
    label_uniform_bitwise | label_minmax | label_vopc | label_usedef | label_extract | label_dpp16 |
    label_dpp8 | label_f2f16_in | label_f2f32_in;
 static constexpr uint64_t instr_mod_labels =
-   label_omod2 | label_omod4 | label_omod5 | label_clamp | label_insert;
+   label_omod2 | label_omod4 | label_omod5 | label_clamp | label_insert | label_f2f16_out |
+   label_f2f32_out;
 
 static constexpr uint64_t instr_labels = instr_usedef_labels | instr_mod_labels;
 static constexpr uint64_t temp_labels = label_abs | label_neg | label_temp | label_vcc | label_b2f |
@@ -326,6 +329,22 @@ struct ssa_info {
    }
 
    bool is_clamp() { return label & label_clamp; }
+
+   void set_f2f16_out(Instruction* conv)
+   {
+      add_label(label_f2f16_out);
+      instr = conv;
+   }
+
+   bool is_f2f16_out() { return label & label_f2f16_out; }
+
+   void set_f2f32_out(Instruction* conv)
+   {
+      add_label(label_f2f32_out);
+      instr = conv;
+   }
+
+   bool is_f2f32_out() { return label & label_f2f32_out; }
 
    void set_undefined() { add_label(label_undefined); }
 
@@ -1894,13 +1913,18 @@ label_instruction(opt_ctx& ctx, aco_ptr<Instruction>& instr)
       break;
    }
    case aco_opcode::v_cvt_f16_f32: {
-      if (instr->operands[0].isTemp())
+      if (instr->operands[0].isTemp()) {
          ctx.info[instr->definitions[0].tempId()].set_f2f16_in(instr.get());
+         ctx.info[instr->operands[0].tempId()].set_f2f16_out(instr.get());
+      }
       break;
    }
    case aco_opcode::v_cvt_f32_f16: {
-      if (instr->operands[0].isTemp())
+      if (instr->operands[0].isTemp()) {
          ctx.info[instr->definitions[0].tempId()].set_f2f32_in(instr.get());
+         if (!(ctx.info[instr->operands[0].tempId()].label & label_extract))
+            ctx.info[instr->operands[0].tempId()].set_f2f32_out(instr.get());
+      }
       break;
    }
    default: break;
@@ -3068,7 +3092,8 @@ apply_omod_clamp(opt_ctx& ctx, aco_ptr<Instruction>& instr)
    }
 
    instr->definitions[0].swapTemp(def_info.instr->definitions[0]);
-   ctx.info[instr->definitions[0].tempId()].label &= label_clamp | label_insert;
+   ctx.info[instr->definitions[0].tempId()].label &=
+      label_clamp | label_insert | label_f2f32_out | label_f2f16_out;
    ctx.uses[def_info.instr->definitions[0].tempId()]--;
 
    return true;
@@ -3529,13 +3554,57 @@ to_mad_mix(opt_ctx& ctx, aco_ptr<Instruction>& instr)
    instr = std::move(vop3p);
 
    if (is_32bit)
-      ctx.info[instr->definitions[0].tempId()].label &= label_clamp | label_mul;
+      ctx.info[instr->definitions[0].tempId()].label &= label_f2f16_out | label_clamp | label_mul;
    else
-      ctx.info[instr->definitions[0].tempId()].label &= label_clamp | label_mul;
+      ctx.info[instr->definitions[0].tempId()].label &= label_f2f32_out | label_clamp | label_mul;
    if (ctx.info[instr->definitions[0].tempId()].label & label_mul)
       ctx.info[instr->definitions[0].tempId()].instr = instr.get();
 
    return is_add ? 1 : 0;
+}
+
+bool
+combine_output_conversion(opt_ctx& ctx, aco_ptr<Instruction>& instr)
+{
+   ssa_info& def_info = ctx.info[instr->definitions[0].tempId()];
+   if (!(def_info.label & (label_f2f32_out | label_f2f16_out)))
+      return false;
+   Instruction* conv = def_info.instr;
+
+   if (!can_use_mad_mix(ctx, instr) || ctx.uses[instr->definitions[0].tempId()] != 1)
+      return false;
+
+   if (!ctx.uses[conv->definitions[0].tempId()])
+      return false;
+
+   if (conv->definitions[0].bytes() == 4 &&
+       (instr->definitions[0].isPrecise() || conv->definitions[0].isPrecise()))
+      return false;
+
+   if (get_operand_size(conv, 0) != instr->definitions[0].bytes() * 8 || conv->usesModifiers())
+      return false;
+
+   if (!instr->isVOP3P())
+      to_mad_mix(ctx, instr);
+
+   if (conv->opcode == aco_opcode::v_cvt_f16_f32)
+      instr->opcode = aco_opcode::v_fma_mixlo_f16;
+   else
+      instr->opcode = aco_opcode::v_fma_mix_f32;
+   instr->definitions[0].swapTemp(conv->definitions[0]);
+   if (conv->definitions[0].isPrecise())
+      instr->definitions[0].setPrecise(true);
+   ctx.info[instr->definitions[0].tempId()].label &= label_clamp;
+   ctx.uses[conv->definitions[0].tempId()]--;
+
+   if ((instr->opcode == aco_opcode::v_fma_mixlo_f16 ||
+        instr->opcode == aco_opcode::v_fma_mix_f32) &&
+       instr->operands[2].constantEquals(0) && instr->vop3p().neg_lo[2] &&
+       !ctx.info[instr->definitions[0].tempId()].is_clamp()) {
+      ctx.info[instr->definitions[0].tempId()].set_mul(instr.get());
+   }
+
+   return true;
 }
 
 void
@@ -3637,7 +3706,7 @@ combine_instruction(opt_ctx& ctx, aco_ptr<Instruction>& instr)
       if (can_apply_sgprs(ctx, instr))
          apply_sgprs(ctx, instr);
       combine_mad_mix(ctx, instr);
-      while (apply_omod_clamp(ctx, instr))
+      while (apply_omod_clamp(ctx, instr) | combine_output_conversion(ctx, instr))
          ;
       apply_insert(ctx, instr);
    }
