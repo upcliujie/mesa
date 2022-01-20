76413aaeed5a97b771f574017175d45aa971e18b
Georg Lehmann <dadschoorse@gmail.com>
2022-01-20 20:50:15 +0100

    radv, aco: Packed usub_sat/isub_sat.
    Signed-off-by: Georg Lehmann <dadschoorse@gmail.com>

--- src/amd/vulkan/radv_pipeline.c
+++ src/amd/vulkan/radv_pipeline.c
@@ -3672,6 +3672,8 @@ opt_vectorize_callback(const nir_instr *instr, void *_)
    case nir_op_iadd_sat:
    case nir_op_uadd_sat:
    case nir_op_isub:
+   case nir_op_isub_sat:
+   case nir_op_usub_sat:
    case nir_op_imul:
    case nir_op_imin:
    case nir_op_imax:
