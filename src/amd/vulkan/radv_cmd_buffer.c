1e726ac18c7927bd0f20c8cb8181bd53ed0885f9
Timur Kristóf <timur.kristof@gmail.com>
2022-01-22 10:11:36 +0100

    radv: Mark task rings needed for task and mesh shaders.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -5135,6 +5135,10 @@ radv_CmdBindPipeline(VkCommandBuffer commandBuffer, VkPipelineBindPoint pipeline
 
       cmd_buffer->state.compute_pipeline = pipeline;
       cmd_buffer->push_constant_stages |= VK_SHADER_STAGE_COMPUTE_BIT;
+      cmd_buffer->task_rings_needed |=
+         pipeline &&
+         pipeline->shaders[MESA_SHADER_COMPUTE] &&
+         pipeline->shaders[MESA_SHADER_COMPUTE]->info.cs.uses_task_rings;
       break;
    case VK_PIPELINE_BIND_POINT_RAY_TRACING_KHR:
       if (cmd_buffer->state.rt_pipeline == pipeline)
@@ -5222,6 +5226,7 @@ radv_CmdBindPipeline(VkCommandBuffer commandBuffer, VkPipelineBindPoint pipeline
                               compute_pipeline);
 
          cmd_buffer->record_result = cmd_buffer->ace_internal_cmdbuf->record_result;
+         cmd_buffer->task_rings_needed = true;
       }
 
       break;
