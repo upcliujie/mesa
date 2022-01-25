cf20ad2c920baff4d1b4358c3584e0210645580b
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2022-01-25 17:20:54 +0100

    radv: enable RADV_FORCE_VRS by default on Vangogh and default to 1x1
    This allows to dynamically change the per-vertex VRS on Vangogh by
    using RADV_FORCE_VRS_CONFIG_FILE.
    
    The overhead with RADV_FORCE_VRS=1x1 seems really negligible in my
    test spot.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -3369,9 +3369,13 @@ radv_CreateDevice(VkPhysicalDevice physicalDevice, const VkDeviceCreateInfo *pCr
       }
    }
 
+
+   device->force_vrs_enabled = device->physical_device->rad_info.family == CHIP_VANGOGH ||
+                               (device->physical_device->rad_info.chip_class >= GFX10_3 &&
+                                device->force_vrs != RADV_FORCE_VRS_1x1);
+
    device->adjust_frag_coord_z =
-      (device->vk.enabled_extensions.KHR_fragment_shading_rate ||
-       device->force_vrs != RADV_FORCE_VRS_1x1) &&
+      (device->vk.enabled_extensions.KHR_fragment_shading_rate || device->force_vrs_enabled) &&
       (device->physical_device->rad_info.family == CHIP_SIENNA_CICHLID ||
        device->physical_device->rad_info.family == CHIP_NAVY_FLOUNDER ||
        device->physical_device->rad_info.family == CHIP_VANGOGH);
