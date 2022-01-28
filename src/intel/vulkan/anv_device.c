aerisarn/mesa-uwp

commit f40a08d25c91256cd3dff0211b8e10d5bbb3734e

anv: Don't advertise unsupported shader stages
We don't currently support mesh or ray tracing, so don't advertise their
shader stages as supported for subgroups.

Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11893>

src/intel/vulkan/anv_device.c
@@ -2057,9 +2057,17 @@ anv_get_physical_device_properties_1_1(struct anv_physical_device *pdevice,
 
    p->subgroupSize = BRW_SUBGROUP_SIZE;
    VkShaderStageFlags scalar_stages = 0;
-   for (unsigned stage = 0; stage < MESA_VULKAN_SHADER_STAGES; stage++) {
+   for (unsigned stage = 0; stage < MESA_SHADER_STAGES; stage++) {
       if (pdevice->compiler->scalar_stage[stage])
          scalar_stages |= mesa_to_vk_shader_stage(stage);
+   }
+   if (pdevice->vk.supported_extensions.KHR_ray_tracing_pipeline) {
+      scalar_stages |= MESA_SHADER_RAYGEN |
+                       MESA_SHADER_ANY_HIT |
+                       MESA_SHADER_CLOSEST_HIT |
+                       MESA_SHADER_MISS |
+                       MESA_SHADER_INTERSECTION |
+                       MESA_SHADER_CALLABLE;
    }
    p->subgroupSupportedStages = scalar_stages;
    p->subgroupSupportedOperations = VK_SUBGROUP_FEATURE_BASIC_BIT |

upcliujie/mesa

commit a2d8b452babc05556da2bac8d7e4fd5157130b7e

anv: Fix subgroupSupportedStages physical property
Use the proper Vulkan values that can be combined into a bitmask.

Fixes: f40a08d25c9 ("anv: Don't advertise unsupported shader stages")

src/intel/vulkan/anv_device.c
@@ -1960,12 +1960,9 @@ anv_get_physical_device_properties_1_1(struct anv_physical_device *pdevice,
          scalar_stages |= mesa_to_vk_shader_stage(stage);
    }
    if (pdevice->vk.supported_extensions.KHR_ray_tracing_pipeline) {
-      scalar_stages |= MESA_SHADER_RAYGEN |
-                       MESA_SHADER_ANY_HIT |
-                       MESA_SHADER_CLOSEST_HIT |
-                       MESA_SHADER_MISS |
-                       MESA_SHADER_INTERSECTION |
-                       MESA_SHADER_CALLABLE;
+      for (unsigned stage = MESA_SHADER_RAYGEN; stage < MESA_SHADER_CALLABLE; stage++) {
+         scalar_stages |= mesa_to_vk_shader_stage(stage);
+      }
    }
    p->subgroupSupportedStages = scalar_stages;
    p->subgroupSupportedOperations = VK_SUBGROUP_FEATURE_BASIC_BIT |