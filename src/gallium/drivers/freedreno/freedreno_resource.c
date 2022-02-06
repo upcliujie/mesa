581980b255ab37696558e45f14cc6549df7d0341
Rob Clark <robdclark@chromium.org>
2022-02-06 09:22:59 -0800

    freedreno/drm: Add FD_BO_SHARED hint
    With the virtio backend we will need to pass an extra flag when
    allocating buffers that will be shared cross-device (such as with
    virtio-wl for passing between host and guest)
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- src/gallium/drivers/freedreno/freedreno_resource.c
+++ src/gallium/drivers/freedreno/freedreno_resource.c
@@ -197,6 +197,7 @@ realloc_bo(struct fd_resource *rsc, uint32_t size)
    struct fd_screen *screen = fd_screen(rsc->b.b.screen);
    uint32_t flags =
       COND(rsc->layout.tile_mode, FD_BO_NOMAP) |
+      COND(prsc->bind & PIPE_BIND_SHARED, FD_BO_SHARED) |
       COND(prsc->usage & PIPE_USAGE_STAGING, FD_BO_CACHED_COHERENT) |
       COND(prsc->bind & PIPE_BIND_SCANOUT, FD_BO_SCANOUT);
    /* TODO other flags? */
