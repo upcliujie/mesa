aerisarn/mesa-uwp

commit 382f6ccda8869f72134dbfa9c3cc68a229e01138

anv: Require the local heap for CCS on XeHP
This platform doesn't support CCS in system memory.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14431>

src/intel/vulkan/anv_image.c
@@ -1611,10 +1611,25 @@ anv_image_get_memory_requirements(struct anv_device *device,
     *    supported memory type for the resource. The bit `1<<i` is set if and
     *    only if the memory type `i` in the VkPhysicalDeviceMemoryProperties
     *    structure for the physical device is supported.
-    *
-    * All types are currently supported for images.
     */
-   uint32_t memory_types = (1ull << device->physical->memory.type_count) - 1;
+   uint32_t memory_types = 0;
+   for (int i = 0; i < device->physical->memory.type_count; i++) {
+      const uint32_t heap_index = device->physical->memory.types[i].heapIndex;
+
+      bool memory_type_supported = true;
+      u_foreach_bit(b, aspects) {
+         VkImageAspectFlagBits aspect = 1 << b;
+         const uint32_t plane = anv_image_aspect_to_plane(image, aspect);
+
+         if (device->info.verx10 >= 125 &&
+             isl_aux_usage_has_ccs(image->planes[plane].aux_usage) &&
+             !device->physical->memory.heaps[heap_index].is_local_mem)
+            memory_type_supported = false;
+      }
+
+      if (memory_type_supported)
+         memory_types |= 1 << i;
+   }
 
    vk_foreach_struct(ext, pMemoryRequirements->pNext) {
       switch (ext->sType) {

upcliujie/mesa

commit e5baa2d4c7efcd3346ea5c22fe611f9db663c124

anv: Require LMEM for optimal tiling on XeHP
The spec requires that all color images created with the same tiling
(and a few other properties) support the same memoryTypeBits. Make anv
check the properties allowed by the spec instead of some driver-internal
details.

With this, anv passes the tests in
dEQP-VK.memory.requirements.*tiling_optimal

Fixes: 382f6ccda88 ("anv: Require the local heap for CCS on XeHP")

src/intel/vulkan/anv_image.c
@@ -1574,12 +1574,31 @@ anv_image_get_memory_requirements(struct anv_device *device,
                                   VkImageAspectFlags aspects,
                                   VkMemoryRequirements2 *pMemoryRequirements)
 {
-   /* The Vulkan spec (git aaed022) says:
+   /* There are a couple restrictions around filling the memoryTypeBits
+    * member. The Vulkan spec says:
     *
-    *    memoryTypeBits is a bitfield and contains one bit set for every
-    *    supported memory type for the resource. The bit `1<<i` is set if and
-    *    only if the memory type `i` in the VkPhysicalDeviceMemoryProperties
-    *    structure for the physical device is supported.
+    *    memoryTypeBits is a bitmask and contains one bit set for every
+    *    supported memory type for the resource. Bit i is set if and only if
+    *    the memory type i in the VkPhysicalDeviceMemoryProperties structure
+    *    for the physical device is supported for the resource.
+    *
+    * This means that we must construct memoryTypeBits from the types
+    * supported by the physical device. It also says,
+    *
+    *    For images created with a color format, the memoryTypeBits member
+    *    is identical for all VkImage objects created with the same
+    *    combination of values for the tiling member, the
+    *    VK_IMAGE_CREATE_SPARSE_BINDING_BIT bit of the flags member, the
+    *    VK_IMAGE_CREATE_SPLIT_INSTANCE_BIND_REGIONS_BIT bit of the flags
+    *    member, handleTypes member of VkExternalMemoryImageCreateInfo, and
+    *    the VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT of the usage member in
+    *    the VkImageCreateInfo structure passed to vkCreateImage.
+    *
+    *    [There's a similar section for depth/stencil images that additionally
+    *     allows the bits to depend on the format].
+    *
+    * This means that we can only rely on some generic properties of the image
+    * to determine if the memory type is supported.
     */
    uint32_t memory_types = 0;
    for (int i = 0; i < device->physical->memory.type_count; i++) {
@@ -1590,10 +1609,21 @@ anv_image_get_memory_requirements(struct anv_device *device,
          VkImageAspectFlagBits aspect = 1 << b;
          const uint32_t plane = anv_image_aspect_to_plane(image, aspect);
 
+         /* Using CCS on XeHP requires a local memory heap. We'd like to check
+          * if the image is using CCS to require local memory, but the only
+          * relevant property the spec allows us to check is the tiling.
+          */
          if (device->info.verx10 >= 125 &&
-             isl_aux_usage_has_ccs(image->planes[plane].aux_usage) &&
-             !device->physical->memory.heaps[heap_index].is_local_mem)
+             image->vk.tiling == VK_IMAGE_TILING_OPTIMAL &&
+             !device->physical->memory.heaps[heap_index].is_local_mem) {
+
+            if (!isl_aux_usage_has_ccs(image->planes[plane].aux_usage)) {
+               anv_perf_warn(VK_LOG_OBJS(&image->vk.base),
+                             "Requiring LMEM for image lacking CCS");
+            }
+
             memory_type_supported = false;
+         }
       }
 
       if (memory_type_supported)