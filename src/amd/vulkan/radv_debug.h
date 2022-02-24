8d312ba040b695d9226a7d224b7baa6ce8992113
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2022-02-24 02:45:20 +0100

    radv/amdgpu: Add basic BO cache.
    Just a basic system of bucketizing and putting stuff in a free list.
    
    Safeguards:
    
    1) Only use up to 20% of memory in the cache.
    2) Anything that can fail due to lack of buffer memory, we retry
       after freeing buffers in the cache.
    
    Open risks:
    
    1) We use a single free list which could be a problem with a ton
       of buffers.
    2) There is no good tracking across memory domains, which means we
       could cause extra spilling from VRAM on dGPUs.
    3) The bucketizing could cause significant extra memory overhead.
    
    I haven't had the time to do thorough testing on this, so this puts
    it behind a perftest flag.
    
    v2: Only do it for cmdbuffers & upload buffers becuase if mystery hangs
        in Elden Ring.

--- src/amd/vulkan/radv_debug.h
+++ src/amd/vulkan/radv_debug.h
@@ -82,6 +82,7 @@ enum {
    RADV_PERFTEST_FORCE_EMULATE_RT = 1u << 10,
    RADV_PERFTEST_NV_MS = 1u << 11,
    RADV_PERFTEST_RT_WAVE_64 = 1u << 12,
+   RADV_PERFTEST_BO_CACHE = 1u << 13,
 };
 
 bool radv_init_trace(struct radv_device *device);
