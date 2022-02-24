aerisarn/mesa-uwp

commit 604d97671b663501195644f2a5f12535e88c563f

iris: Add support for flushing the blitter (hackily)
To flush the blitter, we need to use MI_FLUSH_DW rather than the usual
PIPE_CONTROL we use on the 3D engine.  Most of our code is set up to
suggest flushes via PIPE_CONTROL commands, however, so we hackily just
emit MI_FLUSH_DW when they ask for any kind of PIPE_CONTROL flush.

Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14912>

src/gallium/drivers/iris/iris_state.c
@@ -7651,6 +7651,29 @@ iris_emit_raw_pipe_control(struct iris_batch *batch,
    enum pipe_control_flags non_lri_post_sync_flags =
       post_sync_flags & ~PIPE_CONTROL_LRI_POST_SYNC_OP;
 
+#if GFX_VER >= 12
+   if (batch->name == IRIS_BATCH_BLITTER) {
+      batch_mark_sync_for_pipe_control(batch, flags);
+      iris_batch_sync_region_start(batch);
+
+      /* The blitter doesn't actually use PIPE_CONTROL; rather it uses the
+       * MI_FLUSH_DW command.  However, all of our code is set up to flush
+       * via emitting a pipe control, so we just translate it at this point,
+       * even if it is a bit hacky.
+       */
+      iris_emit_cmd(batch, GENX(MI_FLUSH_DW), fd) {
+         fd.Address = rw_bo(bo, offset, IRIS_DOMAIN_OTHER_WRITE);
+         fd.ImmediateData = imm;
+#if GFX_VERx10 >= 125
+         /* TODO: This may not always be necessary */
+         fd.FlushCCS = true;
+#endif
+      }
+      iris_batch_sync_region_end(batch);
+      return;
+   }
+#endif
+
    /* Recursive PIPE_CONTROL workarounds --------------------------------
     * (http://knowyourmeme.com/memes/xzibit-yo-dawg)
     *

upcliujie/mesa

commit 928752522373a8098e09fdd4b5b4daa1747a5f7c

iris: Set MI_FLUSH_DW::PostSyncOperation correctly
The MI_FLUSH_DW post-sync operation uses the same encoding as the
PIPE_CONTROL one so we can use the same helper.  Write PS Depth Count
is not supported, of course, as the blitter has no depth pipeline.

This means that we can write the timestamp register from the blitter.

Fixes: 604d97671b6 ("iris: Add support for flushing the blitter (hackily)")

src/gallium/drivers/iris/iris_state.c
@@ -7666,6 +7666,8 @@ iris_emit_raw_pipe_control(struct iris_batch *batch,
       batch_mark_sync_for_pipe_control(batch, flags);
       iris_batch_sync_region_start(batch);
 
+      assert(!(flags & PIPE_CONTROL_WRITE_DEPTH_COUNT));
+
       /* The blitter doesn't actually use PIPE_CONTROL; rather it uses the
        * MI_FLUSH_DW command.  However, all of our code is set up to flush
        * via emitting a pipe control, so we just translate it at this point,
@@ -7674,6 +7676,7 @@ iris_emit_raw_pipe_control(struct iris_batch *batch,
       iris_emit_cmd(batch, GENX(MI_FLUSH_DW), fd) {
          fd.Address = rw_bo(bo, offset, IRIS_DOMAIN_OTHER_WRITE);
          fd.ImmediateData = imm;
+         fd.PostSyncOperation = flags_to_post_sync_op(flags);
 #if GFX_VERx10 >= 125
          /* TODO: This may not always be necessary */
          fd.FlushCCS = true;