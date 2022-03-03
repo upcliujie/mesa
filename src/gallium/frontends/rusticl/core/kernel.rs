242cea4f2fcbb5a42b9d6a34951964a548702a7b
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-03-03 17:24:06 -0600

    Convert Kernel to the new refcount framework

--- src/gallium/frontends/rusticl/core/kernel.rs
+++ src/gallium/frontends/rusticl/core/kernel.rs
@@ -1,25 +1,24 @@
 extern crate mesa_rust;
 extern crate rusticl_opencl_gen;
 
-use crate::decl_cl_type;
-use crate::init_cl_type;
+use crate::api::icd::*;
+use crate::impl_cl_type_trait;
 
 use self::rusticl_opencl_gen::*;
 
-use std::ptr;
+use std::sync::Arc;
 
-decl_cl_type!(_cl_kernel, CLKernel, CLKernelRef, CL_INVALID_KERNEL);
-
-pub struct CLKernel {
-    pub cl: cl_kernel,
+#[repr(C)]
+pub struct Kernel {
+    pub base: CLObjectBase<CL_INVALID_KERNEL>,
 }
 
-impl CLKernel {
-    pub fn new() -> CLKernelRef {
-        let c = Self {
-            cl: ptr::null_mut(),
-        };
+impl_cl_type_trait!(cl_kernel, Kernel, CL_INVALID_KERNEL);
 
-        init_cl_type!(c, _cl_kernel)
+impl Kernel {
+    pub fn new() -> Arc<Kernel> {
+        Arc::new(Self {
+            base: CLObjectBase::new(),
+        })
     }
 }
