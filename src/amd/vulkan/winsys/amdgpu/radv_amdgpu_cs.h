4a39652e72e396e120d433432db2f25b9353f2e6
Dave Airlie <airlied@redhat.com>
2022-03-07 10:49:05 +1000

    radv/ws: add support for queues without user fences.
    The video queues don't have user fence support.

--- src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.h
+++ src/amd/vulkan/winsys/amdgpu/radv_amdgpu_cs.h
@@ -46,7 +46,7 @@ struct radv_amdgpu_fence {
 struct radv_amdgpu_ctx {
    struct radv_amdgpu_winsys *ws;
    amdgpu_context_handle ctx;
-   struct radv_amdgpu_fence last_submission[AMDGPU_HW_IP_DMA + 1][MAX_RINGS_PER_TYPE];
+   struct radv_amdgpu_fence last_submission[AMDGPU_HW_IP_NUM + 1][MAX_RINGS_PER_TYPE];
 
    struct radeon_winsys_bo *fence_bo;
 
