2d70e573e8c3f06c5c6c6a4fecd6fd43eb3c095a
Qiang Yu <yuq825@gmail.com>
2022-03-07 15:37:03 +0800

    mesa: add hardware accelerated select constant
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/mesa/state_tracker/st_extensions.c
+++ src/mesa/state_tracker/st_extensions.c
@@ -648,6 +648,11 @@ void st_init_limits(struct pipe_screen *screen,
       screen->get_param(screen, PIPE_CAP_MAX_SPARSE_ARRAY_TEXTURE_LAYERS);
    c->SparseTextureFullArrayCubeMipmaps =
       screen->get_param(screen, PIPE_CAP_SPARSE_TEXTURE_FULL_ARRAY_CUBE_MIPMAPS);
+
+   c->HardwareAcceleratedSelect =
+      getenv("MESA_HW_ACCEL_SELECT") &&
+      !c->ShaderCompilerOptions[MESA_SHADER_GEOMETRY].EmitNoIndirectTemp &&
+      c->Program[MESA_SHADER_GEOMETRY].MaxShaderStorageBlocks;
 }
 
 
