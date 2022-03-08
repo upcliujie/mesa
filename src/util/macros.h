e83538dae79d01230d618811e5d0d5ccaa01e243
Icecream95 <ixn@disroot.org>
2022-03-09 12:17:26 +1300

    util: Add a mul_overflow function
    Defined as a macro which uses __builtin_mul_overflow if available,
    otherwise emulates it with a division.

--- src/util/macros.h
+++ src/util/macros.h
@@ -155,6 +155,17 @@ do {                       \
 #define assume(expr) assert(expr)
 #endif
 
+static inline int
+mul_overflow(size_t size, unsigned count, size_t *res)
+{
+#ifdef HAVE___BUILTIN_MUL_OVERFLOW
+        return __builtin_mul_overflow(size, count, res);
+#else
+        *res = size * count;
+        return count > (SIZE_MAX / size);
+#endif
+}
+
 /* Attribute const is used for functions that have no effects other than their
  * return value, and only rely on the argument values to compute the return
  * value.  As a result, calls to it can be CSEed.  Note that using memory
