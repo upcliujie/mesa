42b1c7ceb37fb306d9d6c4039aedb62ab4fc0b9f
Karol Herbst <kherbst@redhat.com>
2022-03-11 18:18:44 +0100

    rusticl: translate spirv to nir and first steps to kernel arg handling

--- src/gallium/frontends/rusticl/rusticl_mesa_inline_bindings_wrapper.c
+++ src/gallium/frontends/rusticl/rusticl_mesa_inline_bindings_wrapper.c
@@ -5,3 +5,15 @@ pipe_resource_reference(struct pipe_resource **dst, struct pipe_resource *src)
 {
    __pipe_resource_reference_wraped(dst, src);
 }
+
+nir_intrinsic_instr *
+nir_instr_as_intrinsic(const nir_instr *instr)
+{
+    return __nir_instr_as_intrinsic_wraped(instr);
+}
+
+nir_ssa_def *
+nir_load_var(nir_builder *build, nir_variable *var)
+{
+    return __nir_load_var(build, var);
+}
