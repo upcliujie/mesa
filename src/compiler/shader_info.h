d4bb6d1809ee9d8a3e09b0f64867bf7deeffb62c
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-03-12 14:21:24 -0500

    spirv: add and set shader_info flags for subgroup sizing using spirv version
    spirv 1.6 allows these by default, so drivers should have a method
    to easily determine availability

--- src/compiler/shader_info.h
+++ src/compiler/shader_info.h
@@ -471,6 +471,15 @@ typedef struct shader_info {
           * Uses subgroup intrinsics which can communicate across a quad.
           */
          bool uses_wide_subgroup_intrinsics;
+
+         /**
+          * requires all invocations within a subgroup to be active
+          */
+         bool require_full_subgroups;
+         /**
+          * all invocations within a subgroup may be active
+          */
+         bool varying_subgroups;
       } cs;
 
       /* Applies to both TCS and TES. */
