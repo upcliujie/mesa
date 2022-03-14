0bdb12587e713a5d6bd5064d5dc643c4a3ba8b2d
Karol Herbst <kherbst@redhat.com>
2022-03-14 13:00:23 +0100

    rusticl/api: add leak_ref method to leak refs to the client
    makes it less likely to leak refs

--- src/gallium/frontends/rusticl/api/memory.rs
+++ src/gallium/frontends/rusticl/api/memory.rs
@@ -837,7 +837,7 @@ pub fn enqueue_read_buffer(
         evs,
         Box::new(move |q| b.read_to_user(q, offset, ptr, cb)),
     );
-    event.write_checked(cl_event::from_arc(e.clone()));
+    cl_event::leak_ref(event, &e);
     q.queue(&e);
     if block {
         q.flush(true)?;
@@ -896,7 +896,7 @@ pub fn enqueue_write_buffer(
         evs,
         Box::new(move |q| b.write_from_user(q, offset, ptr, cb)),
     );
-    event.write_checked(cl_event::from_arc(e.clone()));
+    cl_event::leak_ref(event, &e);
     q.queue(&e);
     if block {
         q.flush(true)?;
@@ -1023,7 +1023,7 @@ pub fn enqueue_read_buffer_rect(
             )
         }),
     );
-    event.write_checked(cl_event::from_arc(e.clone()));
+    cl_event::leak_ref(event, &e);
     q.queue(&e);
     if block {
         q.flush(true)?;
@@ -1150,7 +1150,7 @@ pub fn enqueue_write_buffer_rect(
             )
         }),
     );
-    event.write_checked(cl_event::from_arc(e.clone()));
+    cl_event::leak_ref(event, &e);
     q.queue(&e);
     if block {
         q.flush(true)?;
@@ -1295,7 +1295,7 @@ pub fn enqueue_copy_buffer_rect(
             )
         }),
     );
-    event.write_checked(cl_event::from_arc(e.clone()));
+    cl_event::leak_ref(event, &e);
     q.queue(&e);
     Ok(())
 
