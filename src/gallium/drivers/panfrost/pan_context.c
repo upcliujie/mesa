aerisarn/mesa-uwp

commit 304851422a4610170e870a5315fefaa5ec42917f

panfrost: Fix set_sampler_views for big GL
Roughly use the freedreno logic to handle all the extra things that will
come up in our Piglit sooner than later.

Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>
Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13203>

src/gallium/drivers/panfrost/pan_context.c
@@ -627,32 +627,35 @@ panfrost_set_sampler_views(
         struct panfrost_context *ctx = pan_context(pctx);
         ctx->dirty_shader[shader] |= PAN_DIRTY_STAGE_TEXTURE;
 
-        unsigned new_nr = 0;
         unsigned i;
 
-        assert(start_slot == 0);
-
-        if (!views)
-                num_views = 0;
-
         for (i = 0; i < num_views; ++i) {
-                if (views[i])
-                        new_nr = i + 1;
+                struct pipe_sampler_view *view = views ? views[i] : NULL;
+                unsigned p = i + start_slot;
+
                 if (take_ownership) {
-                        pipe_sampler_view_reference((struct pipe_sampler_view **)&ctx->sampler_views[shader][i],
+                        pipe_sampler_view_reference((struct pipe_sampler_view **)&ctx->sampler_views[shader][p],
                                                     NULL);
-                        ctx->sampler_views[shader][i] = (struct panfrost_sampler_view *)views[i];
+                        ctx->sampler_views[shader][i] = (struct panfrost_sampler_view *)view;
                 } else {
-                        pipe_sampler_view_reference((struct pipe_sampler_view **)&ctx->sampler_views[shader][i],
-                                                    views[i]);
+                        pipe_sampler_view_reference((struct pipe_sampler_view **)&ctx->sampler_views[shader][p],
+                                                    view);
                 }
         }
 
-        for (; i < ctx->sampler_view_count[shader]; i++) {
-		pipe_sampler_view_reference((struct pipe_sampler_view **)&ctx->sampler_views[shader][i],
+        for (; i < num_views + unbind_num_trailing_slots; i++) {
+                unsigned p = i + start_slot;
+		pipe_sampler_view_reference((struct pipe_sampler_view **)&ctx->sampler_views[shader][p],
 		                            NULL);
         }
-        ctx->sampler_view_count[shader] = new_nr;
+
+        /* Recalculate sampler view count */
+        ctx->sampler_view_count[shader] = 0;
+
+        for (i = 0; i < ARRAY_SIZE(ctx->sampler_views[shader]); ++i) {
+                if (ctx->sampler_views[shader][i])
+                        ctx->sampler_view_count[shader] = i + 1;
+        }
 }
 
 static void

upcliujie/mesa

commit e7201c218f4284a4e44bfa5427fd4e4b49e9d4a2

panfrost: Optimise recalculation of max sampler view
Previously we always searched through 128 sampler views for set
sampler views, now we never look above the maximum updated view.

Fixes: 304851422a4 ("panfrost: Fix set_sampler_views for big GL")

src/gallium/drivers/panfrost/pan_context.c
@@ -627,11 +627,15 @@ panfrost_set_sampler_views(
         struct panfrost_context *ctx = pan_context(pctx);
         ctx->dirty_shader[shader] |= PAN_DIRTY_STAGE_TEXTURE;
 
+        unsigned new_nr = 0;
         unsigned i;
 
         for (i = 0; i < num_views; ++i) {
                 struct pipe_sampler_view *view = views ? views[i] : NULL;
                 unsigned p = i + start_slot;
+
+                if (view)
+                        new_nr = p + 1;
 
                 if (take_ownership) {
                         pipe_sampler_view_reference((struct pipe_sampler_view **)&ctx->sampler_views[shader][p],
@@ -649,13 +653,21 @@ panfrost_set_sampler_views(
 		                            NULL);
         }
 
-        /* Recalculate sampler view count */
-        ctx->sampler_view_count[shader] = 0;
+        /* If the sampler view count is higher than the greatest sampler view
+         * we touch, it can't change */
+        if (ctx->sampler_view_count[shader] > start_slot + num_views + unbind_num_trailing_slots)
+                return;
 
-        for (i = 0; i < ARRAY_SIZE(ctx->sampler_views[shader]); ++i) {
-                if (ctx->sampler_views[shader][i])
-                        ctx->sampler_view_count[shader] = i + 1;
+        /* If we haven't set any sampler views here, search lower numbers for
+         * set sampler views */
+        if (new_nr == 0) {
+                for (i = 0; i < start_slot; ++i) {
+                        if (ctx->sampler_views[shader][i])
+                                new_nr = i + 1;
+                }
         }
+
+        ctx->sampler_view_count[shader] = new_nr;
 }
 
 static void