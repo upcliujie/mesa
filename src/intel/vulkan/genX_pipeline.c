9fbb8303451b6ea59f49468d9ba2c84d8f5202d0
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2022-03-16 00:25:17 +0200

    anv: fix dynamic sample locations on Gen7/7.5
    3DSTATE_MULTISAMPLE should be baked into the pipeline if not dynamic.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: 27ee40f4c9d8 ("anv: Add support for sample locations")

--- src/intel/vulkan/genX_pipeline.c
+++ src/intel/vulkan/genX_pipeline.c
@@ -930,20 +930,20 @@ emit_ms_state(struct anv_graphics_pipeline *pipeline,
     */
    if (pipeline->base.device->vk.enabled_extensions.EXT_sample_locations) {
       /* If the sample locations are dynamic, 3DSTATE_MULTISAMPLE on Gfx7/7.5
-       * will be emitted dynamically, so skip it here. On Gfx8+
-       * 3DSTATE_SAMPLE_PATTERN will be emitted dynamically, so skip it here.
+       * will be emitted dynamically, so we have nothing to do here. On Gfx8+
+       * 3DSTATE_SAMPLE_PATTERN will be emitted dynamically, so skip it here,
+       * but we still have to emit 3DSTATE_MULTISAMPLE.
        */
-      if (!(dynamic_states & ANV_CMD_DIRTY_DYNAMIC_SAMPLE_LOCATIONS)) {
 #if GFX_VER >= 8
-         genX(emit_sample_pattern)(&pipeline->base.batch,
-                                   pipeline->dynamic_state.sample_locations.samples,
-                                   pipeline->dynamic_state.sample_locations.locations);
-#endif
-      }
-
       genX(emit_multisample)(&pipeline->base.batch,
                              pipeline->dynamic_state.sample_locations.samples,
                              pipeline->dynamic_state.sample_locations.locations);
+      if (!(dynamic_states & ANV_CMD_DIRTY_DYNAMIC_SAMPLE_LOCATIONS)) {
+         genX(emit_sample_pattern)(&pipeline->base.batch,
+                                   pipeline->dynamic_state.sample_locations.samples,
+                                   pipeline->dynamic_state.sample_locations.locations);
+      }
+#endif
    } else {
       /* On Gfx8+ 3DSTATE_MULTISAMPLE does not hold anything we need to modify
        * for sample locations, so we don't have to emit it dynamically.
