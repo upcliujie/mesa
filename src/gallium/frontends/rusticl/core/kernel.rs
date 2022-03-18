a303a5104e3c92f159e1d8ddd687cab638c48352
Karol Herbst <kherbst@redhat.com>
2022-03-18 03:16:05 +0100

    rusticl/kernel: report SIMD width as work group sizes
    for the prefered multiple it should be fine. For CL_KERNEL_WORK_GROUP_SIZE
    we have to query the driver once we compile to the hardware earlier.
    But using the SIMD width is better than reporting 1.

--- src/gallium/frontends/rusticl/core/kernel.rs
+++ src/gallium/frontends/rusticl/core/kernel.rs
@@ -451,6 +451,9 @@ impl Kernel {
                 } else {
                     block[i] = 1;
                 }
+            } else {
+                // we already made sure everything is fine
+                grid[i] /= block[i];
             }
         }
 
