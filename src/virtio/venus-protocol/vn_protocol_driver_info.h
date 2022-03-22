621521698b5f766e9711ef5d70aae4039fc31e8f
Yiwei Zhang <zzyiwei@chromium.org>
2022-03-22 06:33:16 +0000

    venus: update protocol and prepare cs renderer protocol info
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>

--- src/virtio/venus-protocol/vn_protocol_driver_info.h
+++ src/virtio/venus-protocol/vn_protocol_driver_info.h
@@ -142,4 +142,10 @@ vn_info_extension_get(int32_t index)
    return &_vn_info_extensions[index];
 }
 
+static inline bool
+vn_info_extension_mask_test(const uint32_t *mask, uint32_t ext_bit)
+{
+   return mask[ext_bit / 32] & (1 << (ext_bit % 32));
+}
+
 #endif /* VN_PROTOCOL_DRIVER_INFO_H */
