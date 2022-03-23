9ab7cf1caa736cab8457ccece549cf7adf98e048
Timur Kristóf <timur.kristof@gmail.com>
2022-03-23 18:44:57 +0100

    nir: Add upper_bound_u32 index for AMD specific intrinsics.
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>

--- src/compiler/nir/nir_intrinsics.py
+++ src/compiler/nir/nir_intrinsics.py
@@ -226,6 +226,9 @@ index("bool", "is_swizzled")
 # The SLC ("system level coherent") bit of load_buffer_amd/store_buffer_amd
 index("bool", "slc_amd")
 
+# When set, range analysis will use it for nir_unsigned_upper_bound
+index("unsigned", "upper_bound_u32")
+
 # Separate source/dest access flags for copies
 index("enum gl_access_qualifier", "dst_access")
 index("enum gl_access_qualifier", "src_access")
@@ -1331,8 +1334,8 @@ system_value("intersection_opaque_amd", 1, bit_sizes=[1])
 # Load forced VRS rates.
 intrinsic("load_force_vrs_rates_amd", dest_comp=1, bit_sizes=[32], flags=[CAN_ELIMINATE, CAN_REORDER])
 
-intrinsic("load_scalar_arg_amd", dest_comp=0, bit_sizes=[32], indices=[BASE], flags=[CAN_ELIMINATE, CAN_REORDER])
-intrinsic("load_vector_arg_amd", dest_comp=0, bit_sizes=[32], indices=[BASE], flags=[CAN_ELIMINATE, CAN_REORDER])
+intrinsic("load_scalar_arg_amd", dest_comp=0, bit_sizes=[32], indices=[BASE, UPPER_BOUND_U32], flags=[CAN_ELIMINATE, CAN_REORDER])
+intrinsic("load_vector_arg_amd", dest_comp=0, bit_sizes=[32], indices=[BASE, UPPER_BOUND_U32], flags=[CAN_ELIMINATE, CAN_REORDER])
 
 # src[] = { 64-bit base address, 32-bit offset }.
 intrinsic("load_smem_amd", src_comp=[1, 1], dest_comp=0, bit_sizes=[32],
