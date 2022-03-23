aerisarn/mesa-uwp

commit 9904ea2c76e246629fab8bda455eec07aa84f3f9

ci: Convert generate-env.sh to a POSIX compliant script
This shell script will be used in environments (e.g. LAVA) where bash
is not available, hence let's make sure it is POSIX compliant in order
to be able to execute on any modern shell interpreter.

Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
Reviewed-by: Guilherme Gallo <guilherme.gallo@collabora.com>
Reviewed-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15208>

.gitlab-ci/common/generate-env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 for var in \
     ASAN_OPTIONS \
@@ -106,8 +106,8 @@ for var in \
     VK_CPU \
     VK_DRIVER \
     VK_ICD_FILENAMES \
-    ; do
-  if [ -n "${!var+x}" ]; then
-    echo "export $var=${!var@Q}"
-  fi
+    ;
+do
+    eval val=\$${var}
+    [ -z "$val" ] || echo "export $var='$val'"
 done

upcliujie/mesa

commit 2961a9e40580253bc43d0d7f1127433a55be129d

ci: Escape quotes in exports created by generate-env.sh
Make sure the ['] characters are quoted in the export statements
generated by the generate-env.sh script.

Fixes: 9904ea2c76e ("ci: Convert generate-env.sh to a POSIX compliant script")
Reported-by: Daniel Stone <daniels@collabora.com>
Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

.gitlab-ci/common/generate-env.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+#
+# Helper to print an export var statement, escaping any quotes from val.
+#
+#   export var='val'
+#
+# where var and val are passed as function arguments.
+#
+print_export() {
+    local var=$1 val=$2
+
+    printf "export %s='" "${var}"
+    while true; do
+        case ${val} in
+        *\'*)
+            printf %s "${val%%\'*}""'\''"
+            val=${val#*\'}
+            ;;
+        *)
+            printf "%s" "${val}"
+            break
+        esac
+    done
+
+    printf "\'\n"
+}
+
 for var in \
     ASAN_OPTIONS \
     BASE_SYSTEM_FORK_HOST_PREFIX \
@@ -112,5 +138,5 @@ for var in \
     ;
 do
     eval val=\$${var}
-    [ -z "$val" ] || echo "export $var='$val'"
+    [ -z "$val" ] || print_export "${var}" "${val}"
 done