4ed9c0f2a301e739ed86cbccfdeb0bf2c55e9f6c
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-03-29 17:55:27 -0500

    v3dv: Stop directly setting vk_device::alloc
    vk_device_init() will do this.
    
    Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>

--- src/broadcom/vulkan/v3dv_device.c
+++ src/broadcom/vulkan/v3dv_device.c
@@ -1943,11 +1943,6 @@ v3dv_CreateDevice(VkPhysicalDevice physicalDevice,
    device->instance = instance;
    device->pdevice = physical_device;
 
-   if (pAllocator)
-      device->vk.alloc = *pAllocator;
-   else
-      device->vk.alloc = physical_device->vk.instance->alloc;
-
    pthread_mutex_init(&device->mutex, NULL);
 
    result = queue_init(device, &device->queue,
