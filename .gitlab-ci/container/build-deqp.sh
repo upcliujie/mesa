aerisarn/mesa-uwp

commit 4e4b4113784e88abe175f9999c4377ffaa3c9cdf

ci: cherry-pick deqp fix for zlib dependency
Zlib was bumped to 1.2.12 breaking links to the previous 1.2.11.
Unfortunately, no tag currently carries the fix so cherry-pick it for
now.

Signed-off-by: Omar Akkila <omar.akkila@collabora.com>
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15626>

.gitlab-ci/container/build-deqp.sh
@@ -11,6 +11,10 @@ git clone \
     /VK-GL-CTS
 pushd /VK-GL-CTS
 
+# Cherry-pick fix for zlib dependency
+git fetch origin main
+git cherry-pick -x ec1804831b654ac55bd2a7a5dd27a556afe05030
+
 # --insecure is due to SSL cert failures hitting sourceforge for zlib and
 # libpng (sigh).  The archives get their checksums checked anyway, and git
 # always goes through ssh or https.

.gitlab-ci/image-tags.yml
@@ -8,11 +8,11 @@ variables:
    DEBIAN_X86_TEST_BASE_IMAGE: "debian/x86_test-base"
 
    DEBIAN_X86_TEST_IMAGE_PATH: "debian/x86_test-gl"
-   DEBIAN_X86_TEST_GL_TAG: "2022-03-25-virglrenderer"
-   DEBIAN_X86_TEST_VK_TAG: "2022-03-28-vkd3d-proton"
+   DEBIAN_X86_TEST_GL_TAG: "2022-03-29-deqp"
+   DEBIAN_X86_TEST_VK_TAG: "2022-03-29-deqp"
 
    FEDORA_X86_BUILD_TAG: "2022-03-18-spirv-tools-5"
-   KERNEL_ROOTFS_TAG: "2022-03-23-bash"
+   KERNEL_ROOTFS_TAG: "2022-03-29-deqp"
 
    WINDOWS_X64_BUILD_PATH: "windows/x64_build"
    WINDOWS_X64_BUILD_TAG: "2022-20-02-base_split"

upcliujie/mesa

commit 33b608eff623ec7302ea49589d28b209b380ad51

ci/deqp: do not fetch full repository
Do not fetch the full VK-GL-CTS repository to cherrypick a single
commit. Rather download it and apply it.

Fixes: 4e4b4113784 ("ci: cherry-pick deqp fix for zlib dependency")
Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>

.gitlab-ci/container/build-deqp.sh
@@ -12,8 +12,9 @@ git clone \
 pushd /VK-GL-CTS
 
 # Cherry-pick fix for zlib dependency
-git fetch origin main
-git cherry-pick -x ec1804831b654ac55bd2a7a5dd27a556afe05030
+wget -q -O- \
+    https://github.com/KhronosGroup/VK-GL-CTS/commit/08f0e1f360fec787ae8de7022cef29a0c3319d18.patch \
+    | git am
 
 # --insecure is due to SSL cert failures hitting sourceforge for zlib and
 # libpng (sigh).  The archives get their checksums checked anyway, and git

.gitlab-ci/image-tags.yml
@@ -8,11 +8,11 @@ variables:
    DEBIAN_X86_TEST_BASE_IMAGE: "debian/x86_test-base"
 
    DEBIAN_X86_TEST_IMAGE_PATH: "debian/x86_test-gl"
-   DEBIAN_X86_TEST_GL_TAG: "2022-03-29-deqp"
-   DEBIAN_X86_TEST_VK_TAG: "2022-03-29-deqp"
+   DEBIAN_X86_TEST_GL_TAG: "2022-03-31-deqp-cherrypick"
+   DEBIAN_X86_TEST_VK_TAG: "2022-03-31-deqp-cherrypick"
 
    FEDORA_X86_BUILD_TAG: "2022-03-18-spirv-tools-5"
-   KERNEL_ROOTFS_TAG: "2022-03-29-deqp"
+   KERNEL_ROOTFS_TAG: "2022-03-31-deqp-cherrypick"
 
    WINDOWS_X64_BUILD_PATH: "windows/x64_build"
    WINDOWS_X64_BUILD_TAG: "2022-20-02-base_split"