44a8cb2671101232723d99e82f1c1a5038b3ec91
Alyssa Rosenzweig <alyssa@collabora.com>
2022-04-01 17:20:09 -0400

    nir: Add transform feedback sysvals for Panfrost
    These will be used to facilitate transform feedback lowering on Panfrost. In
    principle, this could be used for general transform feedback. However, this is
    unlikely to be useful for other drivers in its current form (Maybe Asahi?). For
    now, leave it vendored.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -146,6 +146,8 @@ visit_intrinsic(nir_shader *shader, nir_intrinsic_instr *instr)
    case nir_intrinsic_load_ring_esgs_amd:
    case nir_intrinsic_load_ring_es2gs_offset_amd:
    case nir_intrinsic_load_sample_positions_pan:
+   case nir_intrinsic_load_xfb_address_pan:
+   case nir_intrinsic_load_num_vertices_pan:
    case nir_intrinsic_load_workgroup_num_input_vertices_amd:
    case nir_intrinsic_load_workgroup_num_input_primitives_amd:
    case nir_intrinsic_load_shader_query_enabled_amd:
