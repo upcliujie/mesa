aerisarn/mesa-uwp

commit 3b547a9b581618a7abfa17128c371cd69a64c8e8

lavapipe: Switch to the common sync framework
The common Vulkan sync framework will do most of the queueing for us.
It will even sort out timeline semaphore dependencies and ensure
everything executes in-order.  All we have to do is make sure our
vk_sync type implements VK_SYNC_FEATURE_WAIT_PENDING.  This lets us get
rid of a big pile of code.

Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15651>

src/gallium/frontends/lavapipe/lvp_wsi.c
@@ -55,32 +55,3 @@ lvp_finish_wsi(struct lvp_physical_device *physical_device)
    wsi_device_finish(&physical_device->wsi_device,
                      &physical_device->vk.instance->alloc);
 }
-
-VKAPI_ATTR VkResult VKAPI_CALL lvp_AcquireNextImage2KHR(
-   VkDevice                                     _device,
-   const VkAcquireNextImageInfoKHR*             pAcquireInfo,
-   uint32_t*                                    pImageIndex)
-{
-   LVP_FROM_HANDLE(lvp_device, device, _device);
-   struct lvp_physical_device *pdevice = device->physical_device;
-
-   VkResult result = wsi_common_acquire_next_image2(&pdevice->wsi_device,
-                                                    _device,
-                                                    pAcquireInfo,
-                                                    pImageIndex);
-
-   LVP_FROM_HANDLE(lvp_fence, fence, pAcquireInfo->fence);
-   LVP_FROM_HANDLE(lvp_semaphore, semaphore, pAcquireInfo->semaphore);
-   if (result == VK_SUCCESS || result == VK_SUBOPTIMAL_KHR) {
-      struct lvp_queue_noop *noop = malloc(sizeof(struct lvp_queue_noop));
-      if (!noop) {
-         return VK_ERROR_OUT_OF_HOST_MEMORY;
-      }
-      noop->fence = fence;
-      noop->sema = semaphore;
-      if (fence)
-         fence->timeline = p_atomic_inc_return(&device->queue.timeline);
-      util_queue_add_job(&device->queue.queue, noop, fence ? &fence->fence : NULL, queue_thread_noop, NULL, 0);
-   }
-   return result;
-}

src/gallium/frontends/lavapipe/lvp_pipe_sync.c
@@ -1,0 +1,257 @@
+/*
+ * Copyright © 2022 Collabora Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "lvp_private.h"
+#include "util/timespec.h"
+
+static void
+lvp_pipe_sync_validate(ASSERTED struct lvp_pipe_sync *sync)
+{
+   if (sync->signaled)
+      assert(sync->fence == NULL);
+}
+
+static VkResult
+lvp_pipe_sync_init(UNUSED struct vk_device *vk_device,
+                   struct vk_sync *vk_sync,
+                   uint64_t initial_value)
+{
+   struct lvp_pipe_sync *sync = vk_sync_as_lvp_pipe_sync(vk_sync);
+
+   mtx_init(&sync->lock, mtx_plain);
+   cnd_init(&sync->changed);
+   sync->signaled = (initial_value != 0);
+   sync->fence = NULL;
+
+   return VK_SUCCESS;
+}
+
+static void
+lvp_pipe_sync_finish(struct vk_device *vk_device,
+                     struct vk_sync *vk_sync)
+{
+   struct lvp_device *device = container_of(vk_device, struct lvp_device, vk);
+   struct lvp_pipe_sync *sync = vk_sync_as_lvp_pipe_sync(vk_sync);
+
+   lvp_pipe_sync_validate(sync);
+   if (sync->fence)
+      device->pscreen->fence_reference(device->pscreen, &sync->fence, NULL);
+   cnd_destroy(&sync->changed);
+   mtx_destroy(&sync->lock);
+}
+
+void
+lvp_pipe_sync_signal_with_fence(struct lvp_device *device,
+                                struct lvp_pipe_sync *sync,
+                                struct pipe_fence_handle *fence)
+{
+   mtx_lock(&sync->lock);
+   lvp_pipe_sync_validate(sync);
+   sync->signaled = fence == NULL;
+   device->pscreen->fence_reference(device->pscreen, &sync->fence, fence);
+   cnd_broadcast(&sync->changed);
+   mtx_unlock(&sync->lock);
+}
+
+static VkResult
+lvp_pipe_sync_signal(struct vk_device *vk_device,
+                     struct vk_sync *vk_sync,
+                     uint64_t value)
+{
+   struct lvp_device *device = container_of(vk_device, struct lvp_device, vk);
+   struct lvp_pipe_sync *sync = vk_sync_as_lvp_pipe_sync(vk_sync);
+
+   mtx_lock(&sync->lock);
+   lvp_pipe_sync_validate(sync);
+   sync->signaled = true;
+   if (sync->fence)
+      device->pscreen->fence_reference(device->pscreen, &sync->fence, NULL);
+   cnd_broadcast(&sync->changed);
+   mtx_unlock(&sync->lock);
+
+   return VK_SUCCESS;
+}
+
+static VkResult
+lvp_pipe_sync_reset(struct vk_device *vk_device,
+                    struct vk_sync *vk_sync)
+{
+   struct lvp_device *device = container_of(vk_device, struct lvp_device, vk);
+   struct lvp_pipe_sync *sync = vk_sync_as_lvp_pipe_sync(vk_sync);
+
+   mtx_lock(&sync->lock);
+   lvp_pipe_sync_validate(sync);
+   sync->signaled = false;
+   if (sync->fence)
+      device->pscreen->fence_reference(device->pscreen, &sync->fence, NULL);
+   cnd_broadcast(&sync->changed);
+   mtx_unlock(&sync->lock);
+
+   return VK_SUCCESS;
+}
+
+static VkResult
+lvp_pipe_sync_move(struct vk_device *vk_device,
+                   struct vk_sync *vk_dst,
+                   struct vk_sync *vk_src)
+{
+   struct lvp_device *device = container_of(vk_device, struct lvp_device, vk);
+   struct lvp_pipe_sync *dst = vk_sync_as_lvp_pipe_sync(vk_dst);
+   struct lvp_pipe_sync *src = vk_sync_as_lvp_pipe_sync(vk_src);
+
+   /* Pull the fence out of the source */
+   mtx_lock(&src->lock);
+   struct pipe_fence_handle *fence = src->fence;
+   bool signaled = src->signaled;
+   src->fence = NULL;
+   src->signaled = false;
+   cnd_broadcast(&src->changed);
+   mtx_unlock(&src->lock);
+
+   mtx_lock(&dst->lock);
+   if (dst->fence)
+      device->pscreen->fence_reference(device->pscreen, &dst->fence, NULL);
+   dst->fence = fence;
+   dst->signaled = signaled;
+   cnd_broadcast(&dst->changed);
+   mtx_unlock(&dst->lock);
+
+   return VK_SUCCESS;
+}
+
+static VkResult
+lvp_pipe_sync_wait_locked(struct lvp_device *device,
+                          struct lvp_pipe_sync *sync,
+                          uint64_t wait_value,
+                          enum vk_sync_wait_flags wait_flags,
+                          uint64_t abs_timeout_ns)
+{
+   assert(!(wait_flags & VK_SYNC_WAIT_ANY));
+
+   lvp_pipe_sync_validate(sync);
+
+   uint64_t now_ns = os_time_get_nano();
+   while (!sync->signaled && !sync->fence) {
+      if (now_ns >= abs_timeout_ns)
+         return VK_TIMEOUT;
+
+      int ret;
+      if (abs_timeout_ns >= INT64_MAX) {
+         /* Common infinite wait case */
+         ret = cnd_wait(&sync->changed, &sync->lock);
+      } else {
+         /* This is really annoying.  The C11 threads API uses CLOCK_REALTIME
+          * while all our absolute timeouts are in CLOCK_MONOTONIC.  Best
+          * thing we can do is to convert and hope the system admin doesn't
+          * change the time out from under us.
+          */
+         uint64_t rel_timeout_ns = abs_timeout_ns - now_ns;
+
+         struct timespec now_ts, abs_timeout_ts;
+         timespec_get(&now_ts, TIME_UTC);
+         if (timespec_add_nsec(&abs_timeout_ts, &now_ts, rel_timeout_ns)) {
+            /* Overflowed; may as well be infinite */
+            ret = cnd_wait(&sync->changed, &sync->lock);
+         } else {
+            ret = cnd_timedwait(&sync->changed, &sync->lock, &abs_timeout_ts);
+         }
+      }
+      if (ret == thrd_error)
+         return vk_errorf(device, VK_ERROR_UNKNOWN, "cnd_timedwait failed");
+
+      lvp_pipe_sync_validate(sync);
+
+      /* We don't trust the timeout condition on cnd_timedwait() because of
+       * the potential clock issues caused by using CLOCK_REALTIME.  Instead,
+       * update now_ns, go back to the top of the loop, and re-check.
+       */
+      now_ns = os_time_get_nano();
+   }
+
+   if (sync->signaled || (wait_flags & VK_SYNC_WAIT_PENDING))
+      return VK_SUCCESS;
+
+   /* Grab a reference before we drop the lock */
+   struct pipe_fence_handle *fence = NULL;
+   device->pscreen->fence_reference(device->pscreen, &fence, sync->fence);
+
+   mtx_unlock(&sync->lock);
+
+   uint64_t rel_timeout_ns =
+      now_ns >= abs_timeout_ns ? 0 : abs_timeout_ns - now_ns;
+   bool complete = device->pscreen->fence_finish(device->pscreen, NULL,
+                                                 fence, rel_timeout_ns);
+
+   device->pscreen->fence_reference(device->pscreen, &fence, NULL);
+
+   mtx_lock(&sync->lock);
+
+   lvp_pipe_sync_validate(sync);
+
+   if (!complete)
+      return VK_TIMEOUT;
+
+   if (sync->fence == fence) {
+      device->pscreen->fence_reference(device->pscreen, &sync->fence, NULL);
+      sync->signaled = true;
+   }
+
+   return VK_SUCCESS;
+}
+
+static VkResult
+lvp_pipe_sync_wait(struct vk_device *vk_device,
+                   struct vk_sync *vk_sync,
+                   uint64_t wait_value,
+                   enum vk_sync_wait_flags wait_flags,
+                   uint64_t abs_timeout_ns)
+{
+   struct lvp_device *device = container_of(vk_device, struct lvp_device, vk);
+   struct lvp_pipe_sync *sync = vk_sync_as_lvp_pipe_sync(vk_sync);
+
+   mtx_lock(&sync->lock);
+
+   VkResult result = lvp_pipe_sync_wait_locked(device, sync, wait_value,
+                                               wait_flags, abs_timeout_ns);
+
+   mtx_unlock(&sync->lock);
+
+   return result;
+}
+
+const struct vk_sync_type lvp_pipe_sync_type = {
+   .size = sizeof(struct lvp_pipe_sync),
+   .features = VK_SYNC_FEATURE_BINARY |
+               VK_SYNC_FEATURE_GPU_WAIT |
+               VK_SYNC_FEATURE_GPU_MULTI_WAIT |
+               VK_SYNC_FEATURE_CPU_WAIT |
+               VK_SYNC_FEATURE_CPU_RESET |
+               VK_SYNC_FEATURE_CPU_SIGNAL |
+               VK_SYNC_FEATURE_WAIT_PENDING,
+   .init = lvp_pipe_sync_init,
+   .finish = lvp_pipe_sync_finish,
+   .signal = lvp_pipe_sync_signal,
+   .reset = lvp_pipe_sync_reset,
+   .move = lvp_pipe_sync_move,
+   .wait = lvp_pipe_sync_wait,
+};

src/gallium/frontends/lavapipe/lvp_private.h
@@ -66,6 +66,8 @@ typedef uint32_t xcb_window_t;
 #include "vk_command_buffer.h"
 #include "vk_command_pool.h"
 #include "vk_queue.h"
+#include "vk_sync.h"
+#include "vk_sync_timeline.h"
 
 #include "wsi_common.h"
 
@@ -137,6 +139,9 @@ struct lvp_physical_device {
    struct pipe_screen *pscreen;
    uint32_t max_images;
 
+   struct vk_sync_timeline_type sync_timeline_type;
+   const struct vk_sync_type *sync_types[3];
+
    VkPhysicalDeviceLimits device_limits;
 
    struct wsi_device                       wsi_device;
@@ -167,35 +172,8 @@ struct lvp_queue {
    struct pipe_context *ctx;
    struct cso_context *cso;
    struct u_upload_mgr *uploader;
-   bool shutdown;
-   uint64_t timeline;
-   struct util_queue queue;
-   simple_mtx_t last_lock;
-   uint64_t last_finished;
-   uint64_t last_fence_timeline;
    struct pipe_fence_handle *last_fence;
-   volatile int count;
    void *state;
-};
-
-struct lvp_semaphore_wait {
-   struct lvp_semaphore *sema;
-   uint64_t wait;
-};
-
-struct lvp_queue_work {
-   struct list_head list;
-   uint32_t cmd_buffer_count;
-   uint32_t timeline_count;
-   uint32_t wait_count;
-   uint32_t signal_count;
-   uint64_t timeline;
-   struct lvp_fence *fence;
-   struct lvp_cmd_buffer **cmd_buffers;
-   struct lvp_semaphore_timeline **timelines;
-   struct lvp_semaphore **signals;
-   VkSemaphore *waits;
-   uint64_t *wait_vals;
 };
 
 struct lvp_pipeline_cache {
@@ -231,6 +209,29 @@ struct lvp_device_memory {
    enum lvp_device_memory_type memory_type;
    int                                          backed_fd;
 };
+
+struct lvp_pipe_sync {
+   struct vk_sync base;
+
+   mtx_t lock;
+   cnd_t changed;
+
+   bool signaled;
+   struct pipe_fence_handle *fence;
+};
+
+extern const struct vk_sync_type lvp_pipe_sync_type;
+
+void lvp_pipe_sync_signal_with_fence(struct lvp_device *device,
+                                     struct lvp_pipe_sync *sync,
+                                     struct pipe_fence_handle *fence);
+
+static inline struct lvp_pipe_sync *
+vk_sync_as_lvp_pipe_sync(struct vk_sync *sync)
+{
+   assert(sync->type == &lvp_pipe_sync_type);
+   return container_of(sync, struct lvp_pipe_sync, base);
+}
 
 struct lvp_image {
    struct vk_image vk;
@@ -470,40 +471,6 @@ struct lvp_event {
    volatile uint64_t event_storage;
 };
 
-struct lvp_fence {
-   struct vk_object_base base;
-   uint64_t timeline;
-   struct util_queue_fence fence;
-   struct pipe_fence_handle *handle;
-   bool signalled;
-};
-
-struct lvp_semaphore_timeline {
-   struct lvp_semaphore_timeline *next;
-   uint64_t signal; //api
-   uint64_t timeline; //queue
-   struct pipe_fence_handle *fence;
-};
-
-struct lvp_semaphore {
-   struct vk_object_base base;
-   bool is_timeline;
-   uint64_t current;
-   simple_mtx_t lock;
-   mtx_t submit_lock;
-   cnd_t submit;
-   void *mem;
-   struct util_dynarray links;
-   struct lvp_semaphore_timeline *timeline;
-   struct lvp_semaphore_timeline *latest;
-   struct pipe_fence_handle *handle;
-};
-
-struct lvp_queue_noop {
-   struct lvp_fence *fence;
-   struct lvp_semaphore *sema;
-};
-
 struct lvp_buffer {
    struct vk_object_base base;
 
@@ -604,9 +571,6 @@ VK_DEFINE_NONDISP_HANDLE_CASTS(lvp_query_pool, base, VkQueryPool,
                                VK_OBJECT_TYPE_QUERY_POOL)
 VK_DEFINE_NONDISP_HANDLE_CASTS(lvp_sampler, base, VkSampler,
                                VK_OBJECT_TYPE_SAMPLER)
-VK_DEFINE_NONDISP_HANDLE_CASTS(lvp_fence, base, VkFence, VK_OBJECT_TYPE_FENCE);
-VK_DEFINE_NONDISP_HANDLE_CASTS(lvp_semaphore, base, VkSemaphore,
-                               VK_OBJECT_TYPE_SEMAPHORE);
 
 struct lvp_write_descriptor {
    uint32_t dst_binding;

src/gallium/frontends/lavapipe/lvp_query.c
@@ -104,7 +104,7 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_GetQueryPoolResults(
    LVP_FROM_HANDLE(lvp_query_pool, pool, queryPool);
    VkResult vk_result = VK_SUCCESS;
 
-   lvp_DeviceWaitIdle(_device);
+   device->vk.dispatch_table.DeviceWaitIdle(_device);
 
    for (unsigned i = firstQuery; i < firstQuery + queryCount; i++) {
       uint8_t *dptr = (uint8_t *)((char *)pData + (stride * (i - firstQuery)));

src/gallium/frontends/lavapipe/meson.build
@@ -21,6 +21,7 @@ liblvp_files = files(
     'lvp_lower_vulkan_resource.c',
     'lvp_lower_vulkan_resource.h',
     'lvp_lower_input_attachments.c',
+    'lvp_pipe_sync.c',
     'lvp_pipeline.c',
     'lvp_pipeline_cache.c',
     'lvp_query.c',

upcliujie/mesa

commit c96fd56b98c994614c19d3d9234b7bd58628a906

lavapipe: Go back to manually signaling in lvp_AcquireNextImage2()
When porting lavapipe to the common sync framework, I stole the dummy
sync signal_for_memory idea from RADV but didn't actually do it
correctly.  Unless you set wsi_device::signal_semaphore_with_memory and
wsi_device::signal_fence_with_memory, it doesn't actually signal
anything.  If you do set those, it works but also results in dummy
syncs being created for present fences which we see as signals.  We
could choose to just skip those like RADV does but that's too magic.
Instead, have our own AcquireNextImage2() again which sets dummy syncs.

Fixes: 3b547a9b5816 ("lavapipe: Switch to the common sync framework")

src/gallium/frontends/lavapipe/lvp_device.c
@@ -26,7 +26,6 @@
 #include "pipe-loader/pipe_loader.h"
 #include "git_sha1.h"
 #include "vk_cmd_enqueue_entrypoints.h"
-#include "vk_sync_dummy.h"
 #include "vk_util.h"
 #include "pipe/p_config.h"
 #include "pipe/p_defines.h"
@@ -1463,15 +1462,6 @@ unref_pipeline_layout(struct vk_device *vk_device, VkPipelineLayout _layout)
    lvp_pipeline_layout_unref(device, layout);
 }
 
-static VkResult
-lvp_create_sync_for_memory(struct vk_device *device,
-                           VkDeviceMemory memory,
-                           bool signal_memory,
-                           struct vk_sync **sync_out)
-{
-   return vk_sync_create(device, &vk_sync_dummy_type, 0, 1, sync_out);
-}
-
 VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateDevice(
    VkPhysicalDevice                            physicalDevice,
    const VkDeviceCreateInfo*                   pCreateInfo,
@@ -1516,7 +1506,6 @@ VKAPI_ATTR VkResult VKAPI_CALL lvp_CreateDevice(
    device->instance = (struct lvp_instance *)physical_device->vk.instance;
    device->physical_device = physical_device;
 
-   device->vk.create_sync_for_memory = lvp_create_sync_for_memory;
    device->vk.ref_pipeline_layout = ref_pipeline_layout;
    device->vk.unref_pipeline_layout = unref_pipeline_layout;
 

src/gallium/frontends/lavapipe/lvp_wsi.c
@@ -23,6 +23,10 @@
 
 #include "lvp_wsi.h"
 
+#include "vk_fence.h"
+#include "vk_semaphore.h"
+#include "vk_sync_dummy.h"
+
 static VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL
 lvp_wsi_proc_addr(VkPhysicalDevice physicalDevice, const char *pName)
 {
@@ -54,4 +58,43 @@ lvp_finish_wsi(struct lvp_physical_device *physical_device)
    physical_device->vk.wsi_device = NULL;
    wsi_device_finish(&physical_device->wsi_device,
                      &physical_device->vk.instance->alloc);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL lvp_AcquireNextImage2KHR(
+   VkDevice                                     _device,
+   const VkAcquireNextImageInfoKHR*             pAcquireInfo,
+   uint32_t*                                    pImageIndex)
+{
+   LVP_FROM_HANDLE(lvp_device, device, _device);
+   struct lvp_physical_device *pdevice = device->physical_device;
+
+   VkResult result = wsi_common_acquire_next_image2(&pdevice->wsi_device,
+                                                    _device,
+                                                    pAcquireInfo,
+                                                    pImageIndex);
+
+   VK_FROM_HANDLE(vk_fence, fence, pAcquireInfo->fence);
+   VK_FROM_HANDLE(vk_semaphore, sem, pAcquireInfo->semaphore);
+   if (result == VK_SUCCESS || result == VK_SUBOPTIMAL_KHR) {
+      VkResult sync_res;
+      if (fence) {
+         vk_fence_reset_temporary(&device->vk, fence);
+         sync_res = vk_sync_create(&device->vk, &vk_sync_dummy_type,
+                                   0 /* flags */, 0 /* initial_value */,
+                                   &fence->temporary);
+         if (sync_res != VK_SUCCESS)
+            return sync_res;
+      }
+
+      if (sem) {
+         vk_semaphore_reset_temporary(&device->vk, sem);
+         sync_res = vk_sync_create(&device->vk, &vk_sync_dummy_type,
+                                   0 /* flags */, 0 /* initial_value */,
+                                   &sem->temporary);
+         if (sync_res != VK_SUCCESS)
+            return sync_res;
+      }
+   }
+
+   return result;
 }