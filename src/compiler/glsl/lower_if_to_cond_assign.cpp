aerisarn/mesa-uwp

commit afee5dc63f8fc2fc086e321d8b7379145e86813d

glsl: Lower if to conditional select instead of conditional assignment
Platforms that don't have flow control also don't have anything that
could be written that has a side effect.  It should be safe to implement
these condition writes as

    foo = csel(condition, bar, foo);

This should eliminate the last thing in the GLSL compiler that can
create new conditions on assignments.  Everything else that can store
something in ir_assignment::condition derives it from a pre-existing
condition.

v2: Fix bad rebase.

Reviewed-by: Matt Turner <mattst88@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14573>

src/compiler/glsl/lower_if_to_cond_assign.cpp
@@ -201,14 +201,18 @@ move_block_to_cond_assign(void *mem_ctx,
                                                 cond_expr->clone(mem_ctx, NULL),
                                                 assign->rhs);
                } else {
-                  assign->condition = cond_expr->clone(mem_ctx, NULL);
+                  assign->rhs =
+                     new(mem_ctx) ir_expression(ir_triop_csel,
+                                                cond_expr->clone(mem_ctx, NULL),
+                                                assign->rhs,
+                                                assign->lhs->as_dereference());
                }
             } else {
-               assign->condition =
-                  new(mem_ctx) ir_expression(ir_binop_logic_and,
-                                             glsl_type::bool_type,
+               assign->rhs =
+                  new(mem_ctx) ir_expression(ir_triop_csel,
                                              cond_expr->clone(mem_ctx, NULL),
-                                             assign->condition);
+                                             assign->rhs,
+                                             assign->lhs->as_dereference());
             }
          }
       }

upcliujie/mesa

commit c9ebbdf9b46943e34b4b8b0319aa6d3f5a5f8c21

glsl: Fix many bugs in lowering to ir_triop_csel
ir_triop_csel is a very restricted opcode.  The key restriction that was
violated in afee5dc63f8 is that all the sources, including the Boolean
selector, must have the same number of vector elements.

In a conditional assignment, the Boolean control was always a scalar.

In a conditional assignment with a write mask, the LHS and the RHS could
have different number of components.

This commit fixes both of those issues.  It also adds a missing clone()
of the LHS that was noticed by Illia.

Fixes: afee5dc63f8 ("glsl: Lower if to conditional select instead of conditional assignment")
Closes: #6296

src/compiler/glsl/lower_if_to_cond_assign.cpp
@@ -53,9 +53,14 @@
 
 #include "compiler/glsl_types.h"
 #include "ir.h"
+#include "ir_builder.h"
+#include "util/bitset.h"
 #include "util/set.h"
 #include "util/hash_table.h" /* Needed for the hashing functions */
 #include "main/macros.h" /* for MAX2 */
+#include "program/prog_instruction.h"
+
+using namespace ir_builder;
 
 namespace {
 
@@ -200,11 +205,46 @@ move_block_to_cond_assign(void *mem_ctx,
                                              cond_expr->clone(mem_ctx, NULL),
                                              assign->rhs);
             } else {
+               ir_rvalue *swiz_lhs =
+                  assign->lhs->as_dereference()->clone(mem_ctx, NULL);
+               const unsigned count = assign->rhs->type->vector_elements;
+
+               if (assign->rhs->type != assign->lhs->type) {
+                  /* This is for cases like
+                   *
+                   *    vec4 a;
+                   *    ...
+                   *    if (condition)
+                   *        a.w = some_scalar;
+                   *
+                   * The original LHS has to be swizzled to select the correct
+                   * elements based on the original write mask.
+                   */
+                  assert(assign->lhs->type->vector_elements >
+                         assign->rhs->type->vector_elements);
+
+                  unsigned j = 0;
+                  unsigned swiz[4] = { 0 };
+
+                  for (unsigned i = 0; i < 4; i++) {
+                     if ((assign->write_mask & (1u << i)) != 0)
+                        swiz[j++] = i;
+                  }
+
+                  swiz_lhs = new(mem_ctx) ir_swizzle(swiz_lhs, swiz, count);
+               }
+
+               assert(cond_expr->type->is_scalar());
+
+               ir_rvalue *swiz_cond = cond_expr->clone(mem_ctx, NULL);
+               if (count != 1)
+                  swiz_cond = swizzle(swiz_cond, SWIZZLE_XXXX, count);
+
+               assert(swiz_cond->type->vector_elements == count);
+
                assign->rhs =
                   new(mem_ctx) ir_expression(ir_triop_csel,
-                                             cond_expr->clone(mem_ctx, NULL),
-                                             assign->rhs,
-                                             assign->lhs->as_dereference());
+                                             swiz_cond, assign->rhs, swiz_lhs);
             }
          }
       }