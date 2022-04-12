fd23dc41fdac401291d5be8f41caac587a896ce5
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2022-04-12 09:01:52 +0200

    radv: disable instance packing to fix pipeline query stats
    RDNA2 is affected by a hardware bug when instance packing is enabled
    for adjacent primitive topologies and instance_count > 1, pipeline
    stats generated by GE are incorrect. It needs to be applied for
    indexed and non-indexed draws.
    
    This is based on PAL waDisableInstancePacking.
    
    This fixes KHR-GL46.pipeline_statistics_query_tests_ARB.* with Zink.
    
    Gitlab: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6257
    Cc: mesa-stable
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- src/amd/vulkan/radv_private.h
+++ src/amd/vulkan/radv_private.h
@@ -1412,7 +1412,7 @@ struct radv_cmd_state {
    /* Index buffer */
    struct radv_buffer *index_buffer;
    uint64_t index_offset;
-   uint32_t index_type;
+   uint32_t index_size;
    uint32_t max_index_count;
    uint64_t index_va;
    int32_t last_index_type;
