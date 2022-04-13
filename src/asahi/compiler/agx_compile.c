b2a3af579aac2cd5203666972400185a534e4774
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2022-04-12 21:40:35 -0400

    agx: Don't lower phis in NIR
    We're ready for them now! Just scalarize.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- src/asahi/compiler/agx_compile.c
+++ src/asahi/compiler/agx_compile.c
@@ -1494,6 +1494,7 @@ agx_optimize_nir(nir_shader *nir)
 
       NIR_PASS(progress, nir, nir_copy_prop);
       NIR_PASS(progress, nir, nir_opt_remove_phis);
+      NIR_PASS(progress, nir, nir_lower_phis_to_scalar, true);
       NIR_PASS(progress, nir, nir_opt_dce);
       NIR_PASS(progress, nir, nir_opt_dead_cf);
       NIR_PASS(progress, nir, nir_opt_cse);
@@ -1522,7 +1523,7 @@ agx_optimize_nir(nir_shader *nir)
 
    NIR_PASS_V(nir, nir_opt_sink, move_all);
    NIR_PASS_V(nir, nir_opt_move, move_all);
-   NIR_PASS_V(nir, nir_convert_from_ssa, true);
+   NIR_PASS_V(nir, nir_lower_phis_to_scalar, true);
 }
 
 /* ABI: position first, then user, then psiz */
