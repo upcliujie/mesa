aerisarn/mesa-uwp

commit bbdf22ce136a7f02a8bd6c7e646d12a7e10db18d

radv: Fix barriers with cp dma
We need to wait for cp dma if VK_PIPELINE_STAGE_2_ALL_TRANSFER_BIT or
VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT are set.

Closes: #5911
Fixes: 4b9bc4791b5 ("radv: only sync CP DMA for transfer operations or bottom pipe")

Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15933>

src/amd/vulkan/radv_cmd_buffer.c
@@ -8452,9 +8452,10 @@ radv_barrier(struct radv_cmd_buffer *cmd_buffer, const VkDependencyInfo *dep_inf
    /* Make sure CP DMA is idle because the driver might have performed a DMA operation for copying a
     * buffer (or a MSAA image using FMASK) or updated a buffer which is a transfer operation.
     */
-   if (src_stage_mask & (VK_PIPELINE_STAGE_2_COPY_BIT |
-                         VK_PIPELINE_STAGE_2_TRANSFER_BIT |
-                         VK_PIPELINE_STAGE_2_BOTTOM_OF_PIPE_BIT))
+   if (src_stage_mask &
+       (VK_PIPELINE_STAGE_2_COPY_BIT | VK_PIPELINE_STAGE_2_CLEAR_BIT |
+        VK_PIPELINE_STAGE_2_TRANSFER_BIT | VK_PIPELINE_STAGE_2_ALL_TRANSFER_BIT |
+        VK_PIPELINE_STAGE_2_BOTTOM_OF_PIPE_BIT | VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT))
       si_cp_dma_wait_for_idle(cmd_buffer);
 
    cmd_buffer->state.flush_bits |= dst_flush_bits;

upcliujie/mesa

commit 20998f9f2346e85bfd38b3eed282c55a541ab3a7

radv: do not wait for CP DMA to be idle for clear operations
Only copy operations might use CP DMA and needs to be synchronized
explicitly, as explained in the comment above.

Also remove the redundant stage transfer bit because they are equivalent
but VK_PIPELINE_STAGE_2_TRANSFER_BIT is the old one.

Fixes: bbdf22ce136 ("radv: Fix barriers with cp dma")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

src/amd/vulkan/radv_cmd_buffer.c
@@ -8453,8 +8453,7 @@ radv_barrier(struct radv_cmd_buffer *cmd_buffer, const VkDependencyInfo *dep_inf
     * buffer (or a MSAA image using FMASK) or updated a buffer which is a transfer operation.
     */
    if (src_stage_mask &
-       (VK_PIPELINE_STAGE_2_COPY_BIT | VK_PIPELINE_STAGE_2_CLEAR_BIT |
-        VK_PIPELINE_STAGE_2_TRANSFER_BIT | VK_PIPELINE_STAGE_2_ALL_TRANSFER_BIT |
+       (VK_PIPELINE_STAGE_2_COPY_BIT | VK_PIPELINE_STAGE_2_ALL_TRANSFER_BIT |
         VK_PIPELINE_STAGE_2_BOTTOM_OF_PIPE_BIT | VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT))
       si_cp_dma_wait_for_idle(cmd_buffer);
 