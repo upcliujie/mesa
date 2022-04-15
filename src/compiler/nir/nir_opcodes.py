aerisarn/mesa-uwp

commit 330e28155f79b533a72fc3376fb54ffbf245b458

nir: add 32-bit bool of fisfinite
Add the bool lowering as well.

Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12207>

src/compiler/nir/nir_opcodes.py
@@ -1245,6 +1245,7 @@ binop("umul24_relaxed", tuint32, _2src_commutative + associative, "src0 * src1")
 
 unop_convert("fisnormal", tbool1, tfloat, "isnormal(src0)")
 unop_convert("fisfinite", tbool1, tfloat, "isfinite(src0)")
+unop_convert("fisfinite32", tint32, tfloat, "isfinite(src0)")
 
 # vc4-specific opcodes
 

src/compiler/nir/nir_lower_bool_to_int32.c
@@ -103,6 +103,8 @@ lower_alu_instr(nir_alu_instr *alu)
 
    case nir_op_bcsel: alu->op = nir_op_b32csel; break;
 
+   case nir_op_fisfinite: alu->op = nir_op_fisfinite32; break;
+
    default:
       assert(alu->dest.dest.ssa.bit_size > 1);
       for (unsigned i = 0; i < op_info->num_inputs; i++)

upcliujie/mesa

commit f00c934dc06dd0ac7cad5e78b250057b70de766b

nir/opcodes: fisfinite32 should return bool32
Otherwise constant-folding will fold it to 0/1 instead of 0/~0.

Fixes: 330e28155f79 ("nir: add 32-bit bool of fisfinite")

src/compiler/nir/nir_opcodes.py
@@ -1292,7 +1292,7 @@ binop("umul24_relaxed", tuint32, _2src_commutative + associative, "src0 * src1")
 
 unop_convert("fisnormal", tbool1, tfloat, "isnormal(src0)")
 unop_convert("fisfinite", tbool1, tfloat, "isfinite(src0)")
-unop_convert("fisfinite32", tint32, tfloat, "isfinite(src0)")
+unop_convert("fisfinite32", tbool32, tfloat, "isfinite(src0)")
 
 # vc4-specific opcodes
 