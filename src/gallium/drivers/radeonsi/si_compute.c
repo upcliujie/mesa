2fb4058825b6f08db5feb143763976dcc03b1ae9
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-04-15 15:32:29 -0500

    shader_info: Make images_used a bitset
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>

--- src/gallium/drivers/radeonsi/si_compute.c
+++ src/gallium/drivers/radeonsi/si_compute.c
@@ -152,10 +152,10 @@ static void si_create_compute_state_async(void *job, void *gdata, int thread_ind
    for (unsigned i = 0; i < MIN2(3, sel->info.base.num_images); i++) {
       /* Don't put MSAA images in SGPRs */
       if (sscreen->info.chip_class < GFX11 &&
-          (sel->info.base.msaa_images & (1 << i))
+          BITSET_TEST(sel->info.base.msaa_images, i))
          break;
 
-      unsigned num_sgprs = sel->info.base.image_buffers & (1 << i) ? 4 : 8;
+      unsigned num_sgprs = BITSET_TEST(sel->info.base.image_buffers, i) ? 4 : 8;
 
       if (align(user_sgprs, num_sgprs) + num_sgprs > 16)
          break;
