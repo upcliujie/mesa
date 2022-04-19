aerisarn/mesa-uwp

commit a969ad1ddfd6b0b94cc484da45293c407203e3ac

iris: Demote DC flush to HDC flush in cache tracker
FLUSH_HDC is sufficient to flush things out to L3, so we'd rather
use that where possible.  It's also emulated via DATA_CACHE_FLUSH
on platforms where it isn't supported, so we can use it unconditionally.

We still use DATA_CACHE_FLUSH for invalidating the data cache, and to
flush the DC-tagged cachelines in L3 to be globally-observable.

Reviewed-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Francisco Jerez <currojerez@riseup.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15275>

src/gallium/drivers/iris/iris_pipe_control.c
@@ -195,7 +195,7 @@ iris_emit_buffer_barrier_for(struct iris_batch *batch,
    const uint32_t flush_bits[NUM_IRIS_DOMAINS] = {
       [IRIS_DOMAIN_RENDER_WRITE] = PIPE_CONTROL_RENDER_TARGET_FLUSH,
       [IRIS_DOMAIN_DEPTH_WRITE] = PIPE_CONTROL_DEPTH_CACHE_FLUSH,
-      [IRIS_DOMAIN_DATA_WRITE] = PIPE_CONTROL_DATA_CACHE_FLUSH,
+      [IRIS_DOMAIN_DATA_WRITE] = PIPE_CONTROL_FLUSH_HDC,
       [IRIS_DOMAIN_OTHER_WRITE] = PIPE_CONTROL_FLUSH_ENABLE,
       [IRIS_DOMAIN_VF_READ] = PIPE_CONTROL_STALL_AT_SCOREBOARD,
       [IRIS_DOMAIN_SAMPLER_READ] = PIPE_CONTROL_STALL_AT_SCOREBOARD,
@@ -205,7 +205,7 @@ iris_emit_buffer_barrier_for(struct iris_batch *batch,
    const uint32_t invalidate_bits[NUM_IRIS_DOMAINS] = {
       [IRIS_DOMAIN_RENDER_WRITE] = PIPE_CONTROL_RENDER_TARGET_FLUSH,
       [IRIS_DOMAIN_DEPTH_WRITE] = PIPE_CONTROL_DEPTH_CACHE_FLUSH,
-      [IRIS_DOMAIN_DATA_WRITE] = PIPE_CONTROL_DATA_CACHE_FLUSH,
+      [IRIS_DOMAIN_DATA_WRITE] = PIPE_CONTROL_FLUSH_HDC,
       [IRIS_DOMAIN_OTHER_WRITE] = PIPE_CONTROL_FLUSH_ENABLE,
       [IRIS_DOMAIN_VF_READ] = PIPE_CONTROL_VF_CACHE_INVALIDATE,
       [IRIS_DOMAIN_SAMPLER_READ] = PIPE_CONTROL_TEXTURE_CACHE_INVALIDATE,

src/gallium/drivers/iris/iris_state.c
@@ -7640,7 +7640,7 @@ batch_mark_sync_for_pipe_control(struct iris_batch *batch, uint32_t flags)
    if ((flags & PIPE_CONTROL_DEPTH_CACHE_FLUSH))
       iris_batch_mark_invalidate_sync(batch, IRIS_DOMAIN_DEPTH_WRITE);
 
-   if ((flags & PIPE_CONTROL_DATA_CACHE_FLUSH))
+   if (flags & (PIPE_CONTROL_FLUSH_HDC | PIPE_CONTROL_DATA_CACHE_FLUSH))
       iris_batch_mark_invalidate_sync(batch, IRIS_DOMAIN_DATA_WRITE);
 
    if ((flags & PIPE_CONTROL_FLUSH_ENABLE))

upcliujie/mesa

commit 2d99ba14b3c5f28303adbcace1886c05b4ec63f6

iris: Disallow simultaneous access via tile cache and data cache
Piglit's spec/arb_gl_spirv/execution/ssbo/unsized-array does the
following operations:

1. Call glBufferSubData to populate an SSBO with initial data "A"
2. Draw using the SSBO (only reading)
3. Call glBufferSubData to overwrite the SSBO with new data "B"
4. Draw using the SSBO (only reading)

This translates to the following cache usage:
1. No caches - this is a CPU write to a fresh buffer
2. Populate HDC and DC with the first set of values (due to the read)
3. The buffer is busy so we use BLORP, which accesses the SSBO via the
   Render Cache and Tile Cache.
4. We read via HDC and DC again.

There is a problem at step 3.  We call iris_emit_buffer_barrier_for on
the BLORP destination, which has IRIS_DOMAIN_RENDER_WRITE.  We see that
there was a previous access via IRIS_DOMAIN_DATA_WRITE, so we flush HDC.

This means that cachelines relating to data "A" still live in L3, tagged
as DC cachelines.  Then we do the BLORP operation.  This introduces new
cachelines in RCC and the L3 related to data "B", tagged as Tile Cache.

I conjecture that adding new TC-tagged L3 cachelines does not evict the
existing DC-tagged L3 cachelines, so the L3 ends up containing entries
for both data "A" and data "B" simultaneously.  Depending whether we
flush tile or data first, we can get the wrong version.

To fix this, we check if the same buffer was accessed from multiple
L3-coherent write domains that have different L3-flush bits.  If so,
we flush out the old data before performing the second write.

Fixes: a969ad1ddfd ("iris: Demote DC flush to HDC flush in cache tracker")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6314

src/gallium/drivers/iris/iris_pipe_control.c
@@ -249,6 +249,10 @@ iris_emit_buffer_barrier_for(struct iris_batch *batch,
                 */
                if (seqno > batch->l3_coherent_seqnos[i])
                   bits |= flush_bits[i];
+
+               if (!iris_domain_is_read_only(access) &&
+                   l3_flush_bits[access] != l3_flush_bits[i])
+                  bits |= l3_flush_bits[i];
             } else {
                /* Domain `i` is L3 coherent but the specified domain is not.
                 * Flush both this cache and L3 out to memory.