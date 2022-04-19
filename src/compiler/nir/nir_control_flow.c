4f52ed11bae613a4d1ea3034ca3211bc394f0739
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2022-04-19 15:05:04 +0300

    nir/cf: return cursor after insertion of cf_list
    This will be useful to cut code from one location and paste it at
    another place and later keep pasting after the previous insertions.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/compiler/nir/nir_control_flow.c
+++ src/compiler/nir/nir_control_flow.c
@@ -547,9 +547,12 @@ update_if_uses(nir_cf_node *node)
 /**
  * Stitch two basic blocks together into one. The aggregate must have the same
  * predecessors as the first and the same successors as the second.
+ *
+ * Returns a cursor pointing at the end of the before block once stiched
+ * together.
  */
 
-static void
+static nir_cursor
 stitch_blocks(nir_block *before, nir_block *after)
 {
    /*
@@ -567,8 +570,16 @@ stitch_blocks(nir_block *before, nir_block *after)
          remove_phi_src(after->successors[1], after);
       unlink_block_successors(after);
       exec_node_remove(&after->cf_node.node);
+
+      return nir_after_block(before);
    } else {
       move_successors(after, before);
+
+      nir_instr *last_before_instr = nir_block_last_instr(before);
+      nir_instr *first_after_instr = nir_block_first_instr(after);
+      nir_cursor junction_cursor = last_before_instr ?
+         nir_after_instr(last_before_instr) :
+         nir_before_instr(first_after_instr);
 
       foreach_list_typed(nir_instr, instr, node, &after->instr_list) {
          instr->block = before;
@@ -576,6 +587,8 @@ stitch_blocks(nir_block *before, nir_block *after)
 
       exec_list_append(&before->instr_list, &after->instr_list);
       exec_node_remove(&after->cf_node.node);
+
+      return junction_cursor;
    }
 }
 
@@ -777,13 +790,17 @@ relink_jump_halt_cf_node(nir_cf_node *node, nir_block *end_block)
    }
 }
 
-void
+/**
+ * Inserts a list at a given cursor. Returns the cursor at the end of the
+ * insertion.
+ */
+nir_cursor
 nir_cf_reinsert(nir_cf_list *cf_list, nir_cursor cursor)
 {
    nir_block *before, *after;
 
    if (exec_list_is_empty(&cf_list->list))
-      return;
+      return cursor;
 
    nir_function_impl *cursor_impl =
       nir_cf_node_get_function(&nir_cursor_current_block(cursor)->cf_node);
@@ -802,8 +819,8 @@ nir_cf_reinsert(nir_cf_list *cf_list, nir_cursor cursor)
 
    stitch_blocks(before,
                  nir_cf_node_as_block(nir_cf_node_next(&before->cf_node)));
-   stitch_blocks(nir_cf_node_as_block(nir_cf_node_prev(&after->cf_node)),
-                 after);
+   return stitch_blocks(nir_cf_node_as_block(nir_cf_node_prev(&after->cf_node)),
+                        after);
 }
 
 void
