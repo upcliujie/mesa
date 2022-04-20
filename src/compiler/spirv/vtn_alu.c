Grima04/mesa-turnip-kgsl

commit 58bcebd987b7c4e7d741f42699d34b8189ab9e79

spirv: Allow [i/u]mulExtended to use new nir opcode
Use new nir opcode nir_[i/u]mul_2x32_64 and extract lower and higher 32
bits as needed instead of emitting mul and mul_high.

v2: Surround the switch case with curly braces (Jason Ekstrand)

Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

src/compiler/spirv/vtn_alu.c
@@ -465,17 +465,21 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
       val->ssa->elems[1]->def = nir_usub_borrow(&b->nb, src[0], src[1]);
       break;
 
-   case SpvOpUMulExtended:
+   case SpvOpUMulExtended: {
       vtn_assert(glsl_type_is_struct(val->ssa->type));
-      val->ssa->elems[0]->def = nir_imul(&b->nb, src[0], src[1]);
-      val->ssa->elems[1]->def = nir_umul_high(&b->nb, src[0], src[1]);
+      nir_ssa_def *umul = nir_umul_2x32_64(&b->nb, src[0], src[1]);
+      val->ssa->elems[0]->def = nir_unpack_64_2x32_split_x(&b->nb, umul);
+      val->ssa->elems[1]->def = nir_unpack_64_2x32_split_y(&b->nb, umul);
       break;
+   }
 
-   case SpvOpSMulExtended:
+   case SpvOpSMulExtended: {
       vtn_assert(glsl_type_is_struct(val->ssa->type));
-      val->ssa->elems[0]->def = nir_imul(&b->nb, src[0], src[1]);
-      val->ssa->elems[1]->def = nir_imul_high(&b->nb, src[0], src[1]);
+      nir_ssa_def *smul = nir_imul_2x32_64(&b->nb, src[0], src[1]);
+      val->ssa->elems[0]->def = nir_unpack_64_2x32_split_x(&b->nb, smul);
+      val->ssa->elems[1]->def = nir_unpack_64_2x32_split_y(&b->nb, smul);
       break;
+   }
 
    case SpvOpFwidth:
       val->ssa->def = nir_fadd(&b->nb,

upcliujie/mesa

commit d1cdb6cce0feb1b0533e279a02d5e2b9f04ed490

spirv: Handle Op*MulExtended for non-32-bit types
Fixes: 58bcebd987b7 ("spirv: Allow [i/u]mulExtended to use new nir opcode")

src/compiler/spirv/vtn_alu.c
@@ -530,17 +530,27 @@ vtn_handle_alu(struct vtn_builder *b, SpvOp opcode,
 
    case SpvOpUMulExtended: {
       vtn_assert(glsl_type_is_struct_or_ifc(dest_type));
-      nir_ssa_def *umul = nir_umul_2x32_64(&b->nb, src[0], src[1]);
-      dest->elems[0]->def = nir_unpack_64_2x32_split_x(&b->nb, umul);
-      dest->elems[1]->def = nir_unpack_64_2x32_split_y(&b->nb, umul);
+      if (src[0]->bit_size == 32) {
+         nir_ssa_def *umul = nir_umul_2x32_64(&b->nb, src[0], src[1]);
+         dest->elems[0]->def = nir_unpack_64_2x32_split_x(&b->nb, umul);
+         dest->elems[1]->def = nir_unpack_64_2x32_split_y(&b->nb, umul);
+      } else {
+         dest->elems[0]->def = nir_imul(&b->nb, src[0], src[1]);
+         dest->elems[1]->def = nir_umul_high(&b->nb, src[0], src[1]);
+      }
       break;
    }
 
    case SpvOpSMulExtended: {
       vtn_assert(glsl_type_is_struct_or_ifc(dest_type));
-      nir_ssa_def *smul = nir_imul_2x32_64(&b->nb, src[0], src[1]);
-      dest->elems[0]->def = nir_unpack_64_2x32_split_x(&b->nb, smul);
-      dest->elems[1]->def = nir_unpack_64_2x32_split_y(&b->nb, smul);
+      if (src[0]->bit_size == 32) {
+         nir_ssa_def *umul = nir_imul_2x32_64(&b->nb, src[0], src[1]);
+         dest->elems[0]->def = nir_unpack_64_2x32_split_x(&b->nb, umul);
+         dest->elems[1]->def = nir_unpack_64_2x32_split_y(&b->nb, umul);
+      } else {
+         dest->elems[0]->def = nir_imul(&b->nb, src[0], src[1]);
+         dest->elems[1]->def = nir_imul_high(&b->nb, src[0], src[1]);
+      }
       break;
    }
 