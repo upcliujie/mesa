aerisarn/mesa-uwp

commit f05dfddeb1adad9f6d4ca18f5e066b3ae23d3e28

u_blitter: fix stencil blit fallback for crocus.
crocus needs the sampler view decl to know it's a uint return value.

Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11280>

src/gallium/auxiliary/util/u_simple_shaders.c
@@ -1173,6 +1173,7 @@ util_make_fs_stencil_blit(struct pipe_context *pipe, bool msaa_src)
       "FRAG\n"
       "DCL IN[0], GENERIC[0], LINEAR\n"
       "DCL SAMP[0]\n"
+      "DCL SVIEW[0], 2D, UINT\n"
       "DCL CONST[0][0]\n"
       "DCL TEMP[0]\n"
 

upcliujie/mesa

commit 4e1f6ec386f1a8f7748c74fc4ca3d071a655790a

util/stencil: fix stencil fallback blit shader texture types.
src/gallium/auxiliary/tgsi/tgsi_scan.c:287: scan_src_operand: Assertion `info->sampler_targets[index] == target' failed.
assert was being triggered by
GTF-GL46.gtf30.GL3Tests.framebuffer_blit.framebuffer_blit_functionality_multisampled_to_singlesampled_blit
using the stencil fallback with zink.

Fixes: f05dfddeb1ad ("u_blitter: fix stencil blit fallback for crocus.")

src/gallium/auxiliary/util/u_simple_shaders.c
@@ -1189,7 +1189,7 @@ util_make_fs_stencil_blit(struct pipe_context *pipe, bool msaa_src)
       "FRAG\n"
       "DCL IN[0], GENERIC[0], LINEAR\n"
       "DCL SAMP[0]\n"
-      "DCL SVIEW[0], 2D, UINT\n"
+      "DCL SVIEW[0], %s, UINT\n"
       "DCL CONST[0][0]\n"
       "DCL TEMP[0]\n"
 
@@ -1208,7 +1208,7 @@ util_make_fs_stencil_blit(struct pipe_context *pipe, bool msaa_src)
    enum tgsi_texture_type tgsi_tex = msaa_src ? TGSI_TEXTURE_2D_MSAA :
                                                 TGSI_TEXTURE_2D;
 
-   sprintf(text, shader_templ, tgsi_texture_names[tgsi_tex]);
+   sprintf(text, shader_templ, tgsi_texture_names[tgsi_tex], tgsi_texture_names[tgsi_tex]);
 
    if (!tgsi_text_translate(text, tokens, ARRAY_SIZE(tokens))) {
       assert(0);