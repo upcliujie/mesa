a643978a63fd3889b1f6d62498b032d45c50a3dd
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-04-28 16:36:42 -0400

    llvmpipe: make it possible to import and bind unbacked resources
    this reworks the existing import to just set some metadata and then
    apply the memory region during bind with the assumption that something
    else is doing the import

--- src/gallium/drivers/llvmpipe/lp_texture.c
+++ src/gallium/drivers/llvmpipe/lp_texture.c
@@ -611,6 +611,12 @@ llvmpipe_resource_from_handle(struct pipe_screen *_screen,
    lpr->dt_format = whandle->format;
    pipe_reference_init(&lpr->base.reference, 1);
    lpr->base.screen = _screen;
+   lpr->row_stride[0] = whandle->stride;
+   unsigned nblocksy = util_format_get_nblocksy(template->format, align(template->height0, LP_RASTER_BLOCK_SIZE));
+   if (whandle->type == WINSYS_HANDLE_TYPE_UNBACKED)
+      lpr->img_stride[0] = whandle->image_stride;
+   else
+      lpr->img_stride[0] = whandle->stride * nblocksy;
 
    /*
     * Looks like unaligned displaytargets work just fine,
@@ -621,28 +627,27 @@ llvmpipe_resource_from_handle(struct pipe_screen *_screen,
    assert(lpr->base.height0 == height);
 #endif
 
-   lpr->dt = winsys->displaytarget_from_handle(winsys,
-                                               template,
-                                               whandle,
-                                               &lpr->row_stride[0]);
-   if (!lpr->dt) {
-      goto no_dt;
+   if (whandle->type != WINSYS_HANDLE_TYPE_UNBACKED) {
+      lpr->dt = winsys->displaytarget_from_handle(winsys,
+                                                  template,
+                                                  whandle,
+                                                  &lpr->row_stride[0]);
+      if (!lpr->dt) {
+         goto no_dt;
+      }
+
+      lpr->sample_stride = lpr->img_stride[0];
+      lpr->size_required = lpr->sample_stride;
+
+      void *data = winsys->displaytarget_map(winsys, lpr->dt, PIPE_MAP_READ_WRITE);
+      if (!data) {
+         winsys->displaytarget_destroy(winsys, lpr->dt);
+         goto no_dt;
+      }
+
+      assert(llvmpipe_resource_is_texture(&lpr->base));
+      lpr->tex_data = data;
    }
-
-   lpr->row_stride[0] = whandle->stride;
-   unsigned nblocksy = util_format_get_nblocksy(template->format, align(template->height0, LP_RASTER_BLOCK_SIZE));
-   lpr->img_stride[0] = whandle->stride * nblocksy;
-   lpr->sample_stride = lpr->img_stride[0];
-   lpr->size_required = lpr->sample_stride;
-
-   void *data = winsys->displaytarget_map(winsys, lpr->dt, PIPE_MAP_READ_WRITE);
-   if (!data) {
-      winsys->displaytarget_destroy(winsys, lpr->dt);
-      goto no_dt;
-   }
-
-   assert(llvmpipe_resource_is_texture(&lpr->base));
-   lpr->tex_data = data;
    lpr->id = id_counter++;
    lpr->dmabuf = true;
 
@@ -1065,12 +1070,14 @@ static void llvmpipe_free_memory_fd(struct pipe_screen *screen,
 
 #endif
 
-static bool llvmpipe_resource_bind_backing(struct pipe_screen *screen,
+static bool llvmpipe_resource_bind_backing(struct pipe_screen *pscreen,
                                            struct pipe_resource *pt,
                                            struct pipe_memory_allocation *pmem,
                                            uint64_t offset)
 {
+   struct llvmpipe_screen *screen = llvmpipe_screen(pscreen);
    struct llvmpipe_resource *lpr = llvmpipe_resource(pt);
+   struct sw_winsys *winsys = screen->winsys;
 
    if (!lpr->backable)
       return FALSE;
@@ -1080,6 +1087,25 @@ static bool llvmpipe_resource_bind_backing(struct pipe_screen *screen,
          return FALSE;
 
       lpr->tex_data = (char *)pmem + offset;
+
+      if (lpr->dmabuf) {
+         if (lpr->dt)
+            winsys->displaytarget_destroy(winsys, lpr->dt);
+         if (pmem) {
+            /* Round up the surface size to a multiple of the tile size to
+             * avoid tile clipping.
+             */
+            const unsigned width = MAX2(1, align(lpr->base.width0, TILE_SIZE));
+            const unsigned height = MAX2(1, align(lpr->base.height0, TILE_SIZE));
+
+            lpr->dt = winsys->displaytarget_create_mapped(winsys,
+                                                          lpr->base.bind,
+                                                          lpr->base.format,
+                                                          width, height,
+                                                          lpr->row_stride[0],
+                                                          lpr->tex_data);
+         }
+      }
    } else
       lpr->data = (char *)pmem + offset;
    lpr->backing_offset = offset;
