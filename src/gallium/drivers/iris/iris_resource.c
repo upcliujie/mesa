aerisarn/mesa-uwp

commit 2bc8c61fd00b2877319f54afeaba3d606fd8532c

iris: Return a 64B stride for clear color plane
Although modifiers which use a clear color plane specify that the
plane's pitch should be ignored, some kernels have been found to require
64-byte alignment.

Cc: mesa-stable
Fixes: db475c81b7c ("iris: Return non-zero stride for clear color plane")
Reported-by: Dongwon Kim <dongwon.kim@intel.com>
Suggested-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/14521>

src/gallium/drivers/iris/iris_resource.c
@@ -1625,14 +1625,18 @@ iris_resource_get_param(struct pipe_screen *pscreen,
       }
       return true;
    case PIPE_RESOURCE_PARAM_STRIDE:
-      *value = wants_cc ? 1 :
+      *value = wants_cc ? 64 :
                wants_aux ? res->aux.surf.row_pitch_B : res->surf.row_pitch_B;
 
       /* Mesa's implementation of eglCreateImage rejects strides of zero (see
        * dri2_check_dma_buf_attribs). Ensure we return a non-zero stride as
        * this value may be queried from GBM and passed into EGL.
+       *
+       * Also, although modifiers which use a clear color plane specify that
+       * the plane's pitch should be ignored, some kernels have been found to
+       * require 64-byte alignment.
        */
-      assert(*value);
+      assert(*value != 0 && *value % 64 == 0);
 
       return true;
    case PIPE_RESOURCE_PARAM_OFFSET:

upcliujie/mesa

commit 23f40f84a972bf8e29acbd31a159e86d2af95417

iris: Fix assertion meant to only target the clear-color stride
Fixes: 2bc8c61fd00 ("iris: Return a 64B stride for clear color plane")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6398
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

src/gallium/drivers/iris/iris_resource.c
@@ -1692,7 +1692,7 @@ iris_resource_get_param(struct pipe_screen *pscreen,
        * the plane's pitch should be ignored, some kernels have been found to
        * require 64-byte alignment.
        */
-      assert(*value != 0 && *value % 64 == 0);
+      assert(*value != 0 && (!wants_cc || *value % 64 == 0));
 
       return true;
    case PIPE_RESOURCE_PARAM_OFFSET: