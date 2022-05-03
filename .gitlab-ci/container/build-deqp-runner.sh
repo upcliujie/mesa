4be7fe449aefdcdc5276ce7736bee9d77d166a30
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2022-05-03 13:59:04 +0300

    ci: Uprev deqp-runner
    Update deqp-runner to an unreleased version providing support for
    graceful termination of child processes by sending the SIGTERM signal.
    
    This functionality is required by crosvm-runner to implement proper
    handling of virtio-vsock related operations for initialization and
    release.
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- .gitlab-ci/container/build-deqp-runner.sh
+++ .gitlab-ci/container/build-deqp-runner.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+# FIXME: Drop this when the following MR is merged and released:
+# https://gitlab.freedesktop.org/anholt/deqp-runner/-/merge_requests/39
+DEQP_RUNNER_GIT_URL=https://gitlab.freedesktop.org/cristicc/deqp-runner.git
+DEQP_RUNNER_GIT_REV=6d642bd4545efbb2fbdcc993ffba4cfd3ec3abb2
+
 if [ -n "${DEQP_RUNNER_GIT_TAG}${DEQP_RUNNER_GIT_REV}" ]; then
     # Build and install from source
     DEQP_RUNNER_CARGO_ARGS="--git ${DEQP_RUNNER_GIT_URL:-https://gitlab.freedesktop.org/anholt/deqp-runner.git}"
