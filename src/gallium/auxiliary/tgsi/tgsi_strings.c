2325b9f0aa004c81c58b533e6b4692733bf70aad
Gert Wollny <gert.wollny@collabora.com>
2022-05-03 09:18:56 +0200

    TGSI: Add support for 16 bit temps, conversion and FLOAT16 immediates
    For virgl it is of interest to emit 16 bit fp values if the hosts
    supports it, so tgsi should support this.
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

--- src/gallium/auxiliary/tgsi/tgsi_strings.c
+++ src/gallium/auxiliary/tgsi/tgsi_strings.c
@@ -59,6 +59,7 @@ static const char *tgsi_file_names[] =
    "MEMORY",
    "CONSTBUF",
    "HWATOMIC",
+   "TEMP16",
 };
 
 const char *tgsi_semantic_names[TGSI_SEMANTIC_COUNT] =
@@ -224,7 +225,7 @@ const char *tgsi_fs_coord_pixel_center_names[2] =
    "INTEGER"
 };
 
-const char *tgsi_immediate_type_names[6] =
+const char *tgsi_immediate_type_names[7] =
 {
    "FLT32",
    "UINT32",
@@ -232,6 +233,7 @@ const char *tgsi_immediate_type_names[6] =
    "FLT64",
    "UINT64",
    "INT64",
+   "FLT16",
 };
 
 const char *tgsi_memory_names[4] =
