9c42ef4d34199a4839698d0f0ce115b0c9abab84
Georg Lehmann <dadschoorse@gmail.com>
2022-05-06 14:48:38 +0200

    radv: Don't retile read only images.
    Avoid unnessecary work on FOREIGN queue release barriers, if we can't modify
    the image there can't a situation where we need to update the presentable dcc
    data.
    
    Signed-off-by: Georg Lehmann <dadschoorse@gmail.com>

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -8259,7 +8259,8 @@ radv_retile_transition(struct radv_cmd_buffer *cmd_buffer, struct radv_image *im
 {
    if (src_layout != VK_IMAGE_LAYOUT_PRESENT_SRC_KHR &&
        (dst_layout == VK_IMAGE_LAYOUT_PRESENT_SRC_KHR ||
-        (dst_queue_mask & (1u << RADV_QUEUE_FOREIGN))))
+        (dst_queue_mask & (1u << RADV_QUEUE_FOREIGN))) &&
+       (image->usage & RADV_IMAGE_USAGE_WRITE_BITS))
       radv_retile_dcc(cmd_buffer, image);
 }
 
