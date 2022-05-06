166ca91a4343cfe25801198538f1eba4a8361a6a
Timothy Arceri <tarceri@itsqueeze.com>
2022-05-06 11:56:11 +1000

    r300: disable GLSL IR loop unrolling
    NIR loop unrolling is already enabled so just let it do its job.
    
    Reviewed-by: Pavel Ondračka <pavel.ondracka@gmail.com>

--- src/gallium/drivers/r300/r300_screen.c
+++ src/gallium/drivers/r300/r300_screen.c
@@ -317,8 +317,6 @@ static int r300_get_shader_param(struct pipe_screen *pscreen,
         case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTERS:
         case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTER_BUFFERS:
             return 0;
-        case PIPE_SHADER_CAP_MAX_UNROLL_ITERATIONS_HINT:
-            return 32;
         default:
             break;
         }
@@ -410,8 +408,6 @@ static int r300_get_shader_param(struct pipe_screen *pscreen,
         case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTERS:
         case PIPE_SHADER_CAP_MAX_HW_ATOMIC_COUNTER_BUFFERS:
             return 0;
-        case PIPE_SHADER_CAP_MAX_UNROLL_ITERATIONS_HINT:
-            return 32;
         default:
             break;
         }
