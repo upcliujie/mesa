616df624fcbe71748570a90400b75b3c83ca8149
Timothy Arceri <tarceri@itsqueeze.com>
2022-05-06 11:26:25 +1000

    asahi: switch to NIR loop unrolling

--- src/asahi/compiler/agx_compile.h
+++ src/asahi/compiler/agx_compile.h
@@ -227,6 +227,7 @@ static const nir_shader_compiler_options agx_nir_options = {
    .force_indirect_unrolling = (nir_var_shader_in | nir_var_shader_out | nir_var_function_temp),
    .lower_int64_options = (nir_lower_int64_options) ~(nir_lower_iadd64 | nir_lower_imul_2x32_64),
    .lower_doubles_options = nir_lower_dmod,
+   .max_unroll_iterations = 32,
 };
 
 #endif
