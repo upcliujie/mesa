cdf61fc4d5b76796fbe2da44727abe7462535555
Qiang Yu <yuq825@gmail.com>
2022-05-07 17:38:04 +0800

    ac/nir: use nir_intrinsic_load_lshs_vertex_stride_amd
    For radeonsi which pass this value by argument.
    
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Signed-off-by: Qiang Yu <yuq825@gmail.com>

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -1019,8 +1019,7 @@ radv_lower_io_to_mem(struct radv_device *device, struct radv_pipeline_stage *sta
    if (nir->info.stage == MESA_SHADER_VERTEX) {
       if (info->vs.as_ls) {
          ac_nir_lower_ls_outputs_to_mem(nir, info->vs.tcs_in_out_eq,
-                                        info->vs.tcs_temp_only_input_mask,
-                                        info->vs.num_linked_outputs);
+                                        info->vs.tcs_temp_only_input_mask);
          return true;
       } else if (info->vs.as_es) {
          ac_nir_lower_es_outputs_to_mem(nir, device->physical_device->rad_info.gfx_level,
@@ -1028,10 +1027,10 @@ radv_lower_io_to_mem(struct radv_device *device, struct radv_pipeline_stage *sta
          return true;
       }
    } else if (nir->info.stage == MESA_SHADER_TESS_CTRL) {
-      ac_nir_lower_hs_inputs_to_mem(nir, info->vs.tcs_in_out_eq, info->tcs.num_linked_inputs);
+      ac_nir_lower_hs_inputs_to_mem(nir, info->vs.tcs_in_out_eq);
       ac_nir_lower_hs_outputs_to_mem(
          nir, device->physical_device->rad_info.gfx_level, info->tcs.tes_reads_tess_factors,
-         info->tcs.tes_inputs_read, info->tcs.tes_patch_inputs_read, info->tcs.num_linked_inputs,
+         info->tcs.tes_inputs_read, info->tcs.tes_patch_inputs_read,
          info->tcs.num_linked_outputs, info->tcs.num_linked_patch_outputs, true);
 
       return true;
