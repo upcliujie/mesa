aerisarn/mesa-uwp

commit 31d26ebf1b9d0f819c894aec10c29a44052a995a

pan/mdg: Fill from TLS before spilling non-SSA nodes
Otherwise the data already written to the node will get overwritten.

Cc: mesa-stable
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11212>

src/panfrost/midgard/midgard_ra.c
@@ -865,8 +865,14 @@ mir_spill_register(
         /* For special reads, figure out how many bytes we need */
         unsigned read_bytemask = 0;
 
+        /* If multiple instructions write to this destination, we'll have to
+         * fill from TLS before writing */
+        unsigned write_count = 0;
+
         mir_foreach_instr_global_safe(ctx, ins) {
                 read_bytemask |= mir_bytemask_of_read_components(ins, spill_node);
+                if (ins->dest == spill_node)
+                        ++write_count;
         }
 
         /* For TLS, replace all stores to the spilled node. For
@@ -899,7 +905,15 @@ mir_spill_register(
 
                                 mir_insert_instruction_after_scheduled(ctx, block, ins, st);
                         } else {
-                                ins->dest = spill_index++;
+                                unsigned dest = spill_index++;
+
+                                if (write_count > 1 && mir_bytemask(ins) != 0xF) {
+                                        midgard_instruction read =
+                                                v_load_store_scratch(dest, spill_slot, false, 0xF);
+                                        mir_insert_instruction_before_scheduled(ctx, block, ins, read);
+                                }
+
+                                ins->dest = dest;
                                 ins->no_spill |= (1 << spill_class);
 
                                 midgard_instruction st =

upcliujie/mesa

commit 6f5d02b0f9fcdd9fe8c9cc758bd3249a250c9bb5

pan/mdg: Fix mask usage when filling before a spill
Check the bytemask against 0xFFFF rather than 0xF so that the fill is
skipped for a .xyzw write rather than a .x write.

Set the mask on the store to 0xF when doing a read so that all
components are written back.

Fixes: 31d26ebf1b9 ("pan/mdg: Fill from TLS before spilling non-SSA nodes")

src/panfrost/midgard/midgard_ra.c
@@ -945,10 +945,15 @@ mir_spill_register(
                         } else {
                                 unsigned dest = spill_index++;
 
-                                if (write_count > 1 && mir_bytemask(ins) != 0xF) {
+                                unsigned bytemask = mir_bytemask(ins);
+                                unsigned write_mask = mir_from_bytemask(mir_round_bytemask_up(
+                                                                           bytemask, 32), 32);
+
+                                if (write_count > 1 && bytemask != 0xFFFF) {
                                         midgard_instruction read =
                                                 v_load_store_scratch(dest, spill_slot, false, 0xF);
                                         mir_insert_instruction_before_scheduled(ctx, block, ins, read);
+                                        write_mask = 0xF;
                                 }
 
                                 ins->dest = dest;
@@ -976,7 +981,7 @@ mir_spill_register(
                                         dest = spill_index++;
 
                                 midgard_instruction st =
-                                        v_load_store_scratch(dest, spill_slot, true, ins->mask);
+                                        v_load_store_scratch(dest, spill_slot, true, write_mask);
                                 mir_insert_instruction_after_scheduled(ctx, block, ins, st);
 
                                 if (move) {