Grima04/mesa-turnip-kgsl

commit 1cc72b2aef82373247466c2e7b81970c867ad0fa

pps: Gfx-pps v0.3.0
Add the gfx-pps backbone in `src/pps`.

v2: Simplify supported drivers creation.
v3: No default getter is provided for counters.
v4: Open DRM device in read/write mode.
v5: Wait for datasource to be started.
v6: Set FIFO scheduler while sampling counters.

Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>
Acked-by: Emma Anholt <emma@anholt.net>
Reviewed-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: John Bates <jbates@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9652>

src/tool/pps/pps_device.cc
@@ -1,0 +1,141 @@
+/*
+ * Copyright © 2020 Collabora, Ltd.
+ * Author: Antonio Caggiano <antonio.caggiano@collabora.com>
+ * Author: Rohan Garg <rohan.garg@collabora.com>
+ * Author: Robert Beckett <bob.beckett@collabora.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "pps_device.h"
+
+#include <cassert>
+#include <fcntl.h>
+#include <memory>
+#include <unistd.h>
+#include <xf86drm.h>
+
+namespace pps
+{
+#define MAX_DRM_DEVICES 64
+
+uint32_t DrmDevice::device_count()
+{
+   drmDevicePtr devices[MAX_DRM_DEVICES] = {};
+   int num_devices = drmGetDevices2(0, devices, MAX_DRM_DEVICES);
+   drmFreeDevices(devices, num_devices);
+   return static_cast<uint32_t>(num_devices);
+}
+
+/// @return The name of a DRM device, empty string in case of error
+std::string query_drm_name(const int fd)
+{
+   assert(fd && "Failed to query DrmDevice: invalid fd");
+
+   std::string name = "";
+
+   if (drmVersionPtr version = drmGetVersion(fd)) {
+      name = std::string(version->name, version->name_len);
+      drmFreeVersion(version);
+   }
+
+   return name;
+}
+
+/// @return A DRM device, nullopt in case of error
+std::optional<DrmDevice> create_drm_device(int fd, int32_t gpu_num)
+{
+   if (fd < 0 || gpu_num < 0) {
+      return std::nullopt;
+   }
+
+   // Try getting the name
+   std::string name = query_drm_name(fd);
+   if (name.empty()) {
+      return std::nullopt;
+   }
+
+   auto ret = DrmDevice();
+   ret.fd = fd;
+   ret.gpu_num = gpu_num;
+   ret.name = name;
+   return ret;
+}
+
+std::vector<DrmDevice> DrmDevice::create_all()
+{
+   std::vector<DrmDevice> ret = {};
+
+   drmDevicePtr devices[MAX_DRM_DEVICES] = {};
+   int num_devices = drmGetDevices2(0, devices, MAX_DRM_DEVICES);
+   if (num_devices <= 0) {
+      return ret;
+   }
+
+   for (int32_t gpu_num = 0; gpu_num < num_devices; gpu_num++) {
+      drmDevicePtr device = devices[gpu_num];
+      if ((device->available_nodes & (1 << DRM_NODE_RENDER))) {
+         int fd = open(device->nodes[DRM_NODE_RENDER], O_RDWR);
+
+         // If it can create a device, push it into the vector
+         if (auto drm_device = create_drm_device(fd, gpu_num)) {
+            ret.emplace_back(std::move(drm_device.value()));
+         }
+      }
+   }
+
+   drmFreeDevices(devices, num_devices);
+   return ret;
+}
+
+std::optional<DrmDevice> DrmDevice::create(int32_t gpu_num)
+{
+   std::optional<DrmDevice> ret = std::nullopt;
+
+   if (gpu_num < 0) {
+      return ret;
+   }
+
+   drmDevicePtr devices[MAX_DRM_DEVICES] = {};
+   int num_devices = drmGetDevices2(0, devices, MAX_DRM_DEVICES);
+
+   if (num_devices > 0 && gpu_num < num_devices) {
+      drmDevicePtr device = devices[gpu_num];
+      int fd = open(device->nodes[DRM_NODE_RENDER], O_RDONLY);
+      ret = create_drm_device(fd, gpu_num);
+   }
+
+   drmFreeDevices(devices, num_devices);
+   return ret;
+}
+
+DrmDevice::DrmDevice(DrmDevice &&other)
+   : fd {other.fd}
+   , gpu_num {other.gpu_num}
+   , name {std::move(other.name)}
+{
+   other.fd = -1;
+   other.gpu_num = -1;
+}
+
+DrmDevice &DrmDevice::operator=(DrmDevice &&other)
+{
+   std::swap(fd, other.fd);
+   std::swap(gpu_num, other.gpu_num);
+   std::swap(name, other.name);
+   return *this;
+}
+
+DrmDevice::~DrmDevice()
+{
+   if (fd >= 0) {
+      close(fd);
+   }
+}
+
+DrmDevice::operator bool() const
+{
+   return !name.empty();
+}
+
+} // namespace pps

meson.build
@@ -2047,6 +2047,8 @@ endif
 gcc_lto_quirk = (cc.get_id() == 'gcc') ? ['-fno-lto'] : []
 
 with_perfetto = get_option('perfetto')
+with_datasources = get_option('datasources')
+with_any_datasource = with_datasources.length() != 0
 if with_perfetto
   dep_perfetto = dependency('perfetto', fallback: ['perfetto', 'dep_perfetto'])
 endif
@@ -2176,7 +2178,12 @@ lines += 'HUD lmsensors:   ' + (dep_lmsensors.found() ? 'yes' : 'no')
 
 lines += ''
 lines += 'Shared-glapi:    ' + (with_shared_glapi ? 'yes' : 'no')
+
+lines += ''
 lines += 'Perfetto:        ' + (with_perfetto ? 'yes' : 'no')
+if with_any_datasource
+  lines += 'Perfetto ds:     ' + ' '.join(with_datasources)
+endif
 
 
 indent = '        '

src/meson.build
@@ -25,6 +25,8 @@ inc_gallium = include_directories('gallium/include')
 inc_gallium_aux = include_directories('gallium/auxiliary')
 inc_amd_common = include_directories('amd/common')
 inc_amd_common_llvm = include_directories('amd/llvm')
+inc_tool = include_directories('tool')
+pps_datasources = []
 
 libglsl_util = static_library(
   'glsl_util',
@@ -139,3 +141,5 @@ if with_glx != 'disabled' and not with_glvnd
     variables : ['glx_tls=@0@'.format(use_elf_tls ? 'yes' : 'no')],
   )
 endif
+
+subdir('tool')

src/tool/meson.build
@@ -1,0 +1,8 @@
+# Copyright © 2021 Collabora, Ltd.
+# Author: Antonio Caggiano <antonio.caggiano@collabora.com>
+#
+# SPDX-License-Identifier: MIT
+
+if with_perfetto
+  subdir('pps')
+endif

src/tool/pps/.clang-format
@@ -1,0 +1,21 @@
+BasedOnStyle: WebKit
+AlignTrailingComments: 'true'
+AllowAllParametersOfDeclarationOnNextLine: 'false'
+AllowShortFunctionsOnASingleLine: None
+AlwaysBreakBeforeMultilineStrings: 'true'
+BinPackArguments: 'false'
+BinPackParameters: 'false'
+BreakBeforeBinaryOperators: None
+BreakBeforeBraces: Linux
+ColumnLimit: '100'
+Cpp11BracedListStyle: 'true'
+KeepEmptyLinesAtTheStartOfBlocks: 'false'
+NamespaceIndentation: None
+PointerAlignment: Right
+SortIncludes: 'true'
+SpaceAfterTemplateKeyword: 'false'
+Standard: Cpp11
+TabWidth: '3'
+IndentWidth: '3'
+ConstructorInitializerIndentWidth: '3'
+ContinuationIndentWidth: '3'

src/tool/pps/meson.build
@@ -1,0 +1,44 @@
+# Copyright © 2020-2021 Collabora, Ltd.
+# Author: Antonio Caggiano <antonio.caggiano@collabora.com>
+#
+# SPDX-License-Identifier: MIT
+
+pps_sources = [
+  'pps.cc',
+  'pps_device.cc',
+  'pps_driver.cc',
+  'pps_counter.cc',
+]
+
+include_pps = include_directories('../')
+
+dep_drm = dependency('libdrm')
+pps_deps = [dep_drm, dep_perfetto]
+pps_deps += pps_datasources
+
+lib_pps = static_library(
+  'pps',
+  sources: pps_sources,
+  include_directories: [include_pps, inc_src],
+  dependencies: pps_deps,
+  cpp_args: '-std=c++17'
+)
+
+dep_pps = declare_dependency(
+  link_with: lib_pps,
+  include_directories: [include_pps, inc_src]
+)
+
+producer_sources = [
+  'pps_datasource.cc',
+  'pps_producer.cc'
+]
+
+executable(
+  'pps-producer',
+  sources: producer_sources,
+  include_directories: [include_pps, inc_src],
+  dependencies: [dep_pps, dep_perfetto],
+  cpp_args: '-std=c++17',
+  install: true
+)

src/tool/pps/pps.cc
@@ -1,0 +1,26 @@
+/*
+ * Copyright © 2020 Collabora, Ltd.
+ * Author: Antonio Caggiano <antonio.caggiano@collabora.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "pps.h"
+
+#include <cerrno>
+#include <cstring>
+
+namespace pps
+{
+bool check(int res, const char *msg)
+{
+   if (res < 0) {
+      char *err_msg = std::strerror(errno);
+      PERFETTO_ELOG("%s: %s", msg, err_msg);
+      return false;
+   }
+
+   return true;
+}
+
+} // namespace pps

src/tool/pps/pps.h
@@ -1,0 +1,38 @@
+/*
+ * Copyright © 2020 Collabora, Ltd.
+ * Author: Antonio Caggiano <antonio.caggiano@collabora.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <perfetto.h>
+
+#define PPS_LOG PERFETTO_LOG
+#define PPS_LOG_IMPORTANT PERFETTO_ILOG
+#define PPS_LOG_ERROR PERFETTO_ELOG
+#define PPS_LOG_FATAL PERFETTO_FATAL
+
+namespace pps
+{
+enum class State {
+   Stop,  // initial state, or stopped by the tracing service
+   Start, // running, sampling data
+};
+
+/// @brief Checks whether a return value is valid
+/// @param res Result from a syscall
+/// @param msg Message to prepend to strerror
+/// @return True if ok, false otherwise
+bool check(int res, const char *msg);
+
+/// @param num Numerator
+/// @param den Denominator
+/// @return A ratio between two floating point numbers, or 0 if the denominator is 0
+constexpr double ratio(double num, double den)
+{
+   return den > 0.0 ? num / den : 0.0;
+}
+
+} // namespace pps

upcliujie/mesa

commit c1e7dc17d5695ba934730db604e65040e633fdb9

pps: Open writable renderer node in DrmDevice::create
DrmDevice::create_all correctly opened the node with O_RDWR, while
DrmDevice::create was not, causing failure to create writable buffer.

Fixes pps-config on Freedreno.

Fixes: 1cc72b2aef82373247466c2e7b81970c867ad0fa
("pps: Gfx-pps v0.3.0")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

src/tool/pps/pps_device.cc
@@ -101,7 +101,7 @@ std::optional<DrmDevice> DrmDevice::create(int32_t gpu_num)
 
    if (num_devices > 0 && gpu_num < num_devices) {
       drmDevicePtr device = devices[gpu_num];
-      int fd = open(device->nodes[DRM_NODE_RENDER], O_RDONLY);
+      int fd = open(device->nodes[DRM_NODE_RENDER], O_RDWR);
       ret = create_drm_device(fd, gpu_num);
    }
 