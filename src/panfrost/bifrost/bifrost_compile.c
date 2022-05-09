94c2bb7439ec9b51249987f7d9599dca6ed85695
Alyssa Rosenzweig <alyssa@collabora.com>
2022-05-09 17:39:43 -0400

    Revert "panfrost: Fix pack_32_2x16 implementation"
    This reverts commit 76cea8e27b3bb16cf752962ca53da42ab4bf51e4.
    
    move can take a vector source.

--- src/panfrost/bifrost/bifrost_compile.c
+++ src/panfrost/bifrost/bifrost_compile.c
@@ -2093,6 +2093,7 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
          * are the exceptions that need to handle swizzles specially. */
 
         switch (instr->op) {
+        case nir_op_pack_32_2x16:
         case nir_op_vec2:
         case nir_op_vec3:
         case nir_op_vec4: {
@@ -2157,10 +2158,7 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
                 return;
         }
 
-        case nir_op_mov:
-        case nir_op_pack_32_2x16: {
-                unsigned src_comps = nir_src_num_components(instr->src[0].src);
-
+        case nir_op_mov: {
                 bi_index idx = bi_src_index(&instr->src[0].src);
                 bi_index unoffset_srcs[4] = { idx, idx, idx, idx };
 
@@ -2171,8 +2169,8 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
                         comps > 3 ? instr->src[0].swizzle[3] : 0,
                 };
 
-                if (src_sz == 1) src_sz = 16;
-                bi_make_vec_to(b, dst, unoffset_srcs, channels, src_comps, src_sz);
+                if (sz == 1) sz = 16;
+                bi_make_vec_to(b, dst, unoffset_srcs, channels, comps, sz);
                 return;
         }
 
