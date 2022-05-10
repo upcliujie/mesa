d9a42abc7f9dc8c77832d085df436cc91362ff16
Konstantin Seurer <konstantin.seurer@gmail.com>
2022-05-10 16:10:31 +0200

    spirv: Add plumbing for ray_cull_mask
    Add a new cull_mask system value that is exposed
    by the ray_cull_mask capability of
    SPV_KHR_ray_cull_mask.
    
    Signed-off-by: Konstantin Seurer <konstantin.seurer@gmail.com>

--- src/compiler/spirv/spirv_to_nir.c
+++ src/compiler/spirv/spirv_to_nir.c
@@ -4782,6 +4782,10 @@ vtn_handle_preamble_instruction(struct vtn_builder *b, SpvOp opcode,
          spv_check_supported(intel_subgroup_buffer_block_io, cap);
          break;
 
+      case SpvCapabilityRayCullMaskKHR:
+         spv_check_supported(ray_cull_mask, cap);
+         break;
+
       case SpvCapabilityRayTracingKHR:
          spv_check_supported(ray_tracing, cap);
          break;
