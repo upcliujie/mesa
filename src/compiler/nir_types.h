0ae7139e1ecc61f524a013e3c702f3be549b3c47
Timothy Arceri <tarceri@itsqueeze.com>
2022-05-11 19:39:02 +1000

    nir: add glsl_type_contains_sampler() wrapper
    This will be used in the following patch.

--- src/compiler/nir_types.h
+++ src/compiler/nir_types.h
@@ -161,6 +161,7 @@ bool glsl_type_is_boolean(const struct glsl_type *type);
 bool glsl_type_is_integer(const struct glsl_type *type);
 bool glsl_type_contains_64bit(const struct glsl_type *type);
 bool glsl_type_contains_image(const struct glsl_type *type);
+bool glsl_type_contains_sampler(const struct glsl_type *type);
 bool glsl_sampler_type_is_shadow(const struct glsl_type *type);
 bool glsl_sampler_type_is_array(const struct glsl_type *type);
 bool glsl_struct_type_is_packed(const struct glsl_type *type);
