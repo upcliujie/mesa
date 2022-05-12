6bdb12da178e0a8333515a82e58ba73ae0925934
Karol Herbst <kherbst@redhat.com>
2022-05-12 23:57:24 +0200

    clc: undefine spirv defs to work around LLVMs headers

--- src/compiler/clc/clc_helpers.cpp
+++ src/compiler/clc/clc_helpers.cpp
@@ -785,7 +785,9 @@ clc_compile_to_llvm_module(LLVMContext &llvm_ctx,
       // LLVM's optimizations can produce code that the translator can't translate
       "-O0",
       // Ensure inline functions are actually emitted
-      "-fgnu89-inline"
+      "-fgnu89-inline",
+      "-U__SPIR__",
+      "-U__SPIRV__",
    };
    // We assume there's appropriate defines for __OPENCL_VERSION__ and __IMAGE_SUPPORT__
    // being provided by the caller here.
