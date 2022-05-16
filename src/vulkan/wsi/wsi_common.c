aerisarn/mesa-uwp

commit a8b009aed6b543e9ebfacca54cf9ec4ae6fa5b85

vulkan/wsi: fix missing unistd include
fixes: c72ff19a

Closes #6428

Reviewed-by: Simon Ser <contact@emersion.fr>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16353>

src/vulkan/wsi/wsi_common.c
@@ -38,6 +38,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 VkResult
 wsi_device_init(struct wsi_device *wsi,

upcliujie/mesa

commit 757711afe5202dd03feb446844b506235afe0af3

vulkan/wsi: unbreak win32-support
There's no unistd.h on Windows, let's not include it unconditionally.

Fixes: a8b009aed6b ("vulkan/wsi: fix missing unistd include")

src/vulkan/wsi/wsi_common.c
@@ -38,7 +38,10 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#ifndef _WIN32
 #include <unistd.h>
+#endif
 
 VkResult
 wsi_device_init(struct wsi_device *wsi,