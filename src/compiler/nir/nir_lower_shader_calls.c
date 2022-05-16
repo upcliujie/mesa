c2b8df6794b687694b85ef9bf13396aa983683f0
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2022-05-16 14:42:00 +0300

    nir/lower_shader_calls: add more rematerializable intrinsics
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/compiler/nir/nir_lower_shader_calls.c
+++ src/compiler/nir/nir_lower_shader_calls.c
@@ -156,6 +156,8 @@ can_remat_instr(nir_instr *instr, struct brw_bitset *remat)
       case nir_intrinsic_vulkan_resource_reindex:
       case nir_intrinsic_load_vulkan_descriptor:
       case nir_intrinsic_load_push_constant:
+      case nir_intrinsic_load_global_constant:
+      case nir_intrinsic_load_global_const_block_intel:
          /* These intrinsics don't need to be spilled as long as they don't
           * depend on any spilled values.
           */
