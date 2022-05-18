aerisarn/mesa-uwp

commit 76cea8e27b3bb16cf752962ca53da42ab4bf51e4

panfrost: Fix pack_32_2x16 implementation
Fixes: 6f0eff548c1 ("pan/bi: Implement packing ops between 32-bit vec1 and 16-bit vec2")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16181>

src/panfrost/bifrost/bifrost_compile.c
@@ -1982,7 +1982,6 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
          * are the exceptions that need to handle swizzles specially. */
 
         switch (instr->op) {
-        case nir_op_pack_32_2x16:
         case nir_op_vec2:
         case nir_op_vec3:
         case nir_op_vec4: {
@@ -2047,7 +2046,10 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
                 return;
         }
 
-        case nir_op_mov: {
+        case nir_op_mov:
+        case nir_op_pack_32_2x16: {
+                unsigned src_comps = nir_src_num_components(instr->src[0].src);
+
                 bi_index idx = bi_src_index(&instr->src[0].src);
                 bi_index unoffset_srcs[4] = { idx, idx, idx, idx };
 
@@ -2058,8 +2060,8 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
                         comps > 3 ? instr->src[0].swizzle[3] : 0,
                 };
 
-                if (sz == 1) sz = 16;
-                bi_make_vec_to(b, dst, unoffset_srcs, channels, comps, sz);
+                if (src_sz == 1) src_sz = 16;
+                bi_make_vec_to(b, dst, unoffset_srcs, channels, src_comps, src_sz);
                 return;
         }
 

upcliujie/mesa

commit ed7e7c6bf4e1f4d5911f7d62e4a0cef54e357658

pan/bi: Fix mov and pack_32_2x16
Move can take in a vector and write a scalar, depending on the swizzle. We need
to handle this case. Split out mov and pack_32_2x16 so we can specify correct
behaviour for both. Also drop unused 1-bit boolean stuff which obscured the fix.

Fixes: 76cea8e27b3 ("panfrost: Fix pack_32_2x16 implementation")
Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

src/panfrost/bifrost/bifrost_compile.c
@@ -2157,10 +2157,7 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
                 return;
         }
 
-        case nir_op_mov:
-        case nir_op_pack_32_2x16: {
-                unsigned src_comps = nir_src_num_components(instr->src[0].src);
-
+        case nir_op_mov: {
                 bi_index idx = bi_src_index(&instr->src[0].src);
                 bi_index unoffset_srcs[4] = { idx, idx, idx, idx };
 
@@ -2171,8 +2168,23 @@ bi_emit_alu(bi_builder *b, nir_alu_instr *instr)
                         comps > 3 ? instr->src[0].swizzle[3] : 0,
                 };
 
-                if (src_sz == 1) src_sz = 16;
-                bi_make_vec_to(b, dst, unoffset_srcs, channels, src_comps, src_sz);
+                bi_make_vec_to(b, dst, unoffset_srcs, channels, comps, src_sz);
+                return;
+        }
+
+        case nir_op_pack_32_2x16: {
+                assert(nir_src_num_components(instr->src[0].src) == 2);
+                assert(comps == 1);
+
+                bi_index idx = bi_src_index(&instr->src[0].src);
+                bi_index unoffset_srcs[4] = { idx, idx, idx, idx };
+
+                unsigned channels[2] = {
+                        instr->src[0].swizzle[0],
+                        instr->src[0].swizzle[1]
+                };
+
+                bi_make_vec_to(b, dst, unoffset_srcs, channels, 2, 16);
                 return;
         }
 