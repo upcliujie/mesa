aerisarn/mesa-uwp

commit b171a6baa21977395a72d578306f30faa521dd87

d3d12: Add video encode implementation of pipe_video_codec
Acked-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16286>

src/gallium/drivers/d3d12/d3d12_video_encoder_bitstream_builder_h264.cpp
@@ -1,0 +1,257 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "d3d12_video_encoder_bitstream_builder_h264.h"
+
+inline H264_SPEC_PROFILES
+Convert12ToSpecH264Profiles(D3D12_VIDEO_ENCODER_PROFILE_H264 profile12)
+{
+   switch (profile12) {
+      case D3D12_VIDEO_ENCODER_PROFILE_H264_MAIN:
+      {
+         return H264_PROFILE_MAIN;
+      } break;
+      case D3D12_VIDEO_ENCODER_PROFILE_H264_HIGH:
+      {
+         return H264_PROFILE_HIGH;
+      } break;
+      case D3D12_VIDEO_ENCODER_PROFILE_H264_HIGH_10:
+      {
+         return H264_PROFILE_HIGH10;
+      } break;
+      default:
+      {
+         unreachable("Unsupported D3D12_VIDEO_ENCODER_PROFILE_H264");
+      } break;
+   }
+}
+
+void
+d3d12_video_bitstream_builder_h264::build_sps(const D3D12_VIDEO_ENCODER_PROFILE_H264 &               profile,
+                                              const D3D12_VIDEO_ENCODER_LEVELS_H264 &                level,
+                                              const DXGI_FORMAT &                                    inputFmt,
+                                              const D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264 &   codecConfig,
+                                              const D3D12_VIDEO_ENCODER_SEQUENCE_GOP_STRUCTURE_H264 &gopConfig,
+                                              uint32_t                                    seq_parameter_set_id,
+                                              uint32_t                                    max_num_ref_frames,
+                                              D3D12_VIDEO_ENCODER_PICTURE_RESOLUTION_DESC sequenceTargetResolution,
+                                              D3D12_BOX                                   frame_cropping_codec_config,
+                                              std::vector<uint8_t> &                      headerBitstream,
+                                              std::vector<uint8_t>::iterator              placingPositionStart,
+                                              size_t &                                    writtenBytes)
+{
+   H264_SPEC_PROFILES profile_idc          = Convert12ToSpecH264Profiles(profile);
+   uint32_t           constraint_set3_flag = 0;
+   uint32_t           level_idc            = 0;
+   d3d12_video_encoder_convert_from_d3d12_level_h264(
+      level,
+      level_idc,
+      constraint_set3_flag /*Always 0 except if level is 11 or 1b in which case 0 means 11, 1 means 1b*/);
+
+   // constraint_set3_flag is for Main profile only and levels 11 or 1b: levels 11 if off, level 1b if on. Always 0 for
+   // HIGH/HIGH10 profiles
+   if ((profile == D3D12_VIDEO_ENCODER_PROFILE_H264_HIGH) || (profile == D3D12_VIDEO_ENCODER_PROFILE_H264_HIGH_10)) {
+      // Force 0 for high profiles
+      constraint_set3_flag = 0;
+   }
+
+   assert((inputFmt == DXGI_FORMAT_NV12) || (inputFmt == DXGI_FORMAT_P010));
+
+   // Assume NV12 YUV 420 8 bits
+   uint32_t bit_depth_luma_minus8   = 0;
+   uint32_t bit_depth_chroma_minus8 = 0;
+
+   // In case is 420 10 bits fix it
+   if (inputFmt == DXGI_FORMAT_P010) {
+      bit_depth_luma_minus8   = 2;
+      bit_depth_chroma_minus8 = 2;
+   }
+
+   // Calculate sequence resolution sizes in MBs
+   // Always in MBs since we don't support interlace in D3D12 Encode
+   uint32_t pic_width_in_mbs_minus1 = static_cast<uint32_t>(std::ceil(sequenceTargetResolution.Width / 16.0)) - 1;
+   uint32_t pic_height_in_map_units_minus1 =
+      static_cast<uint32_t>(std::ceil(sequenceTargetResolution.Height / 16.0)) - 1;
+
+   uint32_t frame_cropping_flag               = 0;
+   if (frame_cropping_codec_config.left 
+      || frame_cropping_codec_config.right 
+      || frame_cropping_codec_config.top
+      || frame_cropping_codec_config.bottom
+   ) {
+      frame_cropping_flag               = 1;
+   }
+
+   H264_SPS spsStructure = { static_cast<uint32_t>(profile_idc),
+                             constraint_set3_flag,
+                             level_idc,
+                             seq_parameter_set_id,
+                             bit_depth_luma_minus8,
+                             bit_depth_chroma_minus8,
+                             gopConfig.log2_max_frame_num_minus4,
+                             gopConfig.pic_order_cnt_type,
+                             gopConfig.log2_max_pic_order_cnt_lsb_minus4,
+                             max_num_ref_frames,
+                             0,   // gaps_in_frame_num_value_allowed_flag
+                             pic_width_in_mbs_minus1,
+                             pic_height_in_map_units_minus1,
+                             ((codecConfig.ConfigurationFlags &
+                               D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264_FLAG_USE_ADAPTIVE_8x8_TRANSFORM) != 0) ?
+                                1u :
+                                0u,   // direct_8x8_inference_flag
+                             frame_cropping_flag,
+                             frame_cropping_codec_config.left,
+                             frame_cropping_codec_config.right,
+                             frame_cropping_codec_config.top,
+                             frame_cropping_codec_config.bottom };
+
+   // Print built PPS structure
+   debug_printf(
+      "[D3D12 d3d12_video_bitstream_builder_h264] H264_SPS Structure generated before writing to bitstream:\n");
+   print_sps(spsStructure);
+
+   // Convert the H264 SPS structure into bytes
+   m_h264Encoder.sps_to_nalu_bytes(&spsStructure, headerBitstream, placingPositionStart, writtenBytes);
+}
+
+void
+d3d12_video_bitstream_builder_h264::write_end_of_stream_nalu(std::vector<uint8_t> &         headerBitstream,
+                                                             std::vector<uint8_t>::iterator placingPositionStart,
+                                                             size_t &                       writtenBytes)
+{
+   m_h264Encoder.write_end_of_stream_nalu(headerBitstream, placingPositionStart, writtenBytes);
+}
+
+void
+d3d12_video_bitstream_builder_h264::write_end_of_sequence_nalu(std::vector<uint8_t> &         headerBitstream,
+                                                               std::vector<uint8_t>::iterator placingPositionStart,
+                                                               size_t &                       writtenBytes)
+{
+   m_h264Encoder.write_end_of_sequence_nalu(headerBitstream, placingPositionStart, writtenBytes);
+}
+
+void
+d3d12_video_bitstream_builder_h264::build_pps(const D3D12_VIDEO_ENCODER_PROFILE_H264 &                   profile,
+                                              const D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264 &       codecConfig,
+                                              const D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_H264 &pictureControl,
+                                              uint32_t                       pic_parameter_set_id,
+                                              uint32_t                       seq_parameter_set_id,
+                                              std::vector<uint8_t> &         headerBitstream,
+                                              std::vector<uint8_t>::iterator placingPositionStart,
+                                              size_t &                       writtenBytes)
+{
+   BOOL bIsHighProfile =
+      ((profile == D3D12_VIDEO_ENCODER_PROFILE_H264_HIGH) || (profile == D3D12_VIDEO_ENCODER_PROFILE_H264_HIGH_10));
+
+   H264_PPS ppsStructure = {
+      pic_parameter_set_id,
+      seq_parameter_set_id,
+      ((codecConfig.ConfigurationFlags & D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264_FLAG_ENABLE_CABAC_ENCODING) != 0) ?
+         1u :
+         0u,   // entropy_coding_mode_flag
+      0,   // pic_order_present_flag (bottom_field_pic_order_in_frame_present_flag) - will use pic_cnt 0 or 2, always
+           // off ; used with pic_cnt_type 1 and deltas.
+      static_cast<uint32_t>(std::max(static_cast<int32_t>(pictureControl.List0ReferenceFramesCount) - 1,
+                                     0)),   // num_ref_idx_l0_active_minus1
+      static_cast<uint32_t>(std::max(static_cast<int32_t>(pictureControl.List1ReferenceFramesCount) - 1,
+                                     0)),   // num_ref_idx_l1_active_minus1
+      ((codecConfig.ConfigurationFlags &
+        D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264_FLAG_USE_CONSTRAINED_INTRAPREDICTION) != 0) ?
+         1u :
+         0u,   // constrained_intra_pred_flag
+      ((codecConfig.ConfigurationFlags &
+        D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264_FLAG_USE_ADAPTIVE_8x8_TRANSFORM) != 0) ?
+         1u :
+         0u   // transform_8x8_mode_flag
+   };
+
+   // Print built PPS structure
+   debug_printf(
+      "[D3D12 d3d12_video_bitstream_builder_h264] H264_PPS Structure generated before writing to bitstream:\n");
+   print_pps(ppsStructure);
+
+   // Convert the H264 SPS structure into bytes
+   m_h264Encoder.pps_to_nalu_bytes(&ppsStructure, headerBitstream, bIsHighProfile, placingPositionStart, writtenBytes);
+}
+
+void
+d3d12_video_bitstream_builder_h264::print_pps(const H264_PPS &pps)
+{
+   // Be careful that build_pps also wraps some other NALU bytes in pps_to_nalu_bytes so bitstream returned by build_pps
+   // won't be exactly the bytes from the H264_PPS struct
+
+   static_assert(sizeof(H264_PPS) ==
+                 (sizeof(uint32_t) *
+                  8), "Update the number of uint32_t in struct in assert and add case below if structure changes");
+
+   // Declared fields from definition in d3d12_video_encoder_bitstream_builder_h264.h
+
+   debug_printf("[D3D12 d3d12_video_bitstream_builder_h264] H264_PPS values below:\n");
+   debug_printf("pic_parameter_set_id: %d\n", pps.pic_parameter_set_id);
+   debug_printf("seq_parameter_set_id: %d\n", pps.seq_parameter_set_id);
+   debug_printf("entropy_coding_mode_flag: %d\n", pps.entropy_coding_mode_flag);
+   debug_printf("pic_order_present_flag: %d\n", pps.pic_order_present_flag);
+   debug_printf("num_ref_idx_l0_active_minus1: %d\n", pps.num_ref_idx_l0_active_minus1);
+   debug_printf("num_ref_idx_l1_active_minus1: %d\n", pps.num_ref_idx_l1_active_minus1);
+   debug_printf("constrained_intra_pred_flag: %d\n", pps.constrained_intra_pred_flag);
+   debug_printf("transform_8x8_mode_flag: %d\n", pps.transform_8x8_mode_flag);
+   debug_printf(
+      "[D3D12 d3d12_video_bitstream_builder_h264] H264_PPS values end\n--------------------------------------\n");
+}
+
+void
+d3d12_video_bitstream_builder_h264::print_sps(const H264_SPS &sps)
+{
+   // Be careful when calling this method that build_sps also wraps some other NALU bytes in sps_to_nalu_bytes so
+   // bitstream returned by build_sps won't be exactly the bytes from the H264_SPS struct From definition in
+   // d3d12_video_encoder_bitstream_builder_h264.h
+
+   static_assert(sizeof(H264_SPS) ==
+                 (sizeof(uint32_t) *
+                  19), "Update the number of uint32_t in struct in assert and add case below if structure changes");
+
+   // Declared fields from definition in d3d12_video_encoder_bitstream_builder_h264.h
+
+   debug_printf("[D3D12 d3d12_video_bitstream_builder_h264] H264_SPS values below:\n");
+   debug_printf("profile_idc: %d\n", sps.profile_idc);
+   debug_printf("constraint_set3_flag: %d\n", sps.constraint_set3_flag);
+   debug_printf("level_idc: %d\n", sps.level_idc);
+   debug_printf("seq_parameter_set_id: %d\n", sps.seq_parameter_set_id);
+   debug_printf("bit_depth_luma_minus8: %d\n", sps.bit_depth_luma_minus8);
+   debug_printf("bit_depth_chroma_minus8: %d\n", sps.bit_depth_chroma_minus8);
+   debug_printf("log2_max_frame_num_minus4: %d\n", sps.log2_max_frame_num_minus4);
+   debug_printf("pic_order_cnt_type: %d\n", sps.pic_order_cnt_type);
+   debug_printf("log2_max_pic_order_cnt_lsb_minus4: %d\n", sps.log2_max_pic_order_cnt_lsb_minus4);
+   debug_printf("max_num_ref_frames: %d\n", sps.max_num_ref_frames);
+   debug_printf("gaps_in_frame_num_value_allowed_flag: %d\n", sps.gaps_in_frame_num_value_allowed_flag);
+   debug_printf("pic_width_in_mbs_minus1: %d\n", sps.pic_width_in_mbs_minus1);
+   debug_printf("pic_height_in_map_units_minus1: %d\n", sps.pic_height_in_map_units_minus1);
+   debug_printf("direct_8x8_inference_flag: %d\n", sps.direct_8x8_inference_flag);
+   debug_printf("frame_cropping_flag: %d\n", sps.frame_cropping_flag);
+   debug_printf("frame_cropping_rect_left_offset: %d\n", sps.frame_cropping_rect_left_offset);
+   debug_printf("frame_cropping_rect_right_offset: %d\n", sps.frame_cropping_rect_right_offset);
+   debug_printf("frame_cropping_rect_top_offset: %d\n", sps.frame_cropping_rect_top_offset);
+   debug_printf("frame_cropping_rect_bottom_offset: %d\n", sps.frame_cropping_rect_bottom_offset);
+   debug_printf(
+      "[D3D12 d3d12_video_bitstream_builder_h264] H264_SPS values end\n--------------------------------------\n");
+}

src/gallium/drivers/d3d12/d3d12_video_enc.h
@@ -1,0 +1,321 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef D3D12_VIDEO_ENC_H
+#define D3D12_VIDEO_ENC_H
+
+#include "d3d12_video_types.h"
+#include "d3d12_video_encoder_references_manager.h"
+#include "d3d12_video_dpb_storage_manager.h"
+#include "d3d12_video_encoder_bitstream_builder_h264.h"
+
+///
+/// Pipe video interface starts
+///
+
+/**
+ * creates a video encoder
+ */
+struct pipe_video_codec *
+d3d12_video_encoder_create_encoder(struct pipe_context *context, const struct pipe_video_codec *templ);
+
+/**
+ * destroy this video encoder
+ */
+void
+d3d12_video_encoder_destroy(struct pipe_video_codec *codec);
+
+/**
+ * start encoding of a new frame
+ */
+void
+d3d12_video_encoder_begin_frame(struct pipe_video_codec * codec,
+                                struct pipe_video_buffer *target,
+                                struct pipe_picture_desc *picture);
+
+/**
+ * encode to a bitstream
+ */
+void
+d3d12_video_encoder_encode_bitstream(struct pipe_video_codec * codec,
+                                     struct pipe_video_buffer *source,
+                                     struct pipe_resource *    destination,
+                                     void **                   feedback);
+
+/**
+ * get encoder feedback
+ */
+void
+d3d12_video_encoder_get_feedback(struct pipe_video_codec *codec, void *feedback, unsigned *size);
+
+/**
+ * end encoding of the current frame
+ */
+void
+d3d12_video_encoder_end_frame(struct pipe_video_codec * codec,
+                              struct pipe_video_buffer *target,
+                              struct pipe_picture_desc *picture);
+
+/**
+ * flush any outstanding command buffers to the hardware
+ * should be called before a video_buffer is acessed by the gallium frontend again
+ */
+void
+d3d12_video_encoder_flush(struct pipe_video_codec *codec);
+
+///
+/// Pipe video interface ends
+///
+
+enum d3d12_video_encoder_config_dirty_flags
+{
+   d3d12_video_encoder_config_dirty_flag_none                   = 0x0,
+   d3d12_video_encoder_config_dirty_flag_codec                  = 0x1,
+   d3d12_video_encoder_config_dirty_flag_profile                = 0x2,
+   d3d12_video_encoder_config_dirty_flag_level                  = 0x4,
+   d3d12_video_encoder_config_dirty_flag_codec_config           = 0x8,
+   d3d12_video_encoder_config_dirty_flag_input_format           = 0x10,
+   d3d12_video_encoder_config_dirty_flag_resolution             = 0x20,
+   d3d12_video_encoder_config_dirty_flag_rate_control           = 0x40,
+   d3d12_video_encoder_config_dirty_flag_slices                 = 0x80,
+   d3d12_video_encoder_config_dirty_flag_gop                    = 0x100,
+   d3d12_video_encoder_config_dirty_flag_motion_precision_limit = 0x200,
+};
+DEFINE_ENUM_FLAG_OPERATORS(d3d12_video_encoder_config_dirty_flags);
+
+///
+/// d3d12_video_encoder functions starts
+///
+
+struct d3d12_video_encoder
+{
+   struct pipe_video_codec base;
+   struct pipe_screen *    m_screen;
+   struct d3d12_screen *   m_pD3D12Screen;
+
+   ///
+   /// D3D12 objects and context info
+   ///
+
+   const uint m_NodeMask  = 0u;
+   const uint m_NodeIndex = 0u;
+
+   ComPtr<ID3D12Fence> m_spFence;
+   uint                m_fenceValue = 1u;
+
+   ComPtr<ID3D12VideoDevice3>            m_spD3D12VideoDevice;
+   ComPtr<ID3D12VideoEncoder>            m_spVideoEncoder;
+   ComPtr<ID3D12VideoEncoderHeap>        m_spVideoEncoderHeap;
+   ComPtr<ID3D12CommandQueue>            m_spEncodeCommandQueue;
+   ComPtr<ID3D12CommandAllocator>        m_spCommandAllocator;
+   ComPtr<ID3D12VideoEncodeCommandList2> m_spEncodeCommandList;
+   ComPtr<ID3D12CommandQueue>            m_spCopyQueue;
+   std::vector<D3D12_RESOURCE_BARRIER>   m_transitionsBeforeCloseCmdList;
+
+   std::unique_ptr<d3d12_video_encoder_references_manager_interface> m_upDPBManager;
+   std::unique_ptr<d3d12_video_dpb_storage_manager_interface>        m_upDPBStorageManager;
+   std::unique_ptr<d3d12_video_bitstream_builder_interface>          m_upBitstreamBuilder;
+
+   bool m_needsGPUFlush = false;
+
+   ComPtr<ID3D12Resource> m_spResolvedMetadataBuffer;
+   ComPtr<ID3D12Resource> m_spMetadataOutputBuffer;
+
+   std::vector<uint8_t> m_BitstreamHeadersBuffer;
+
+   struct
+   {
+      bool m_fArrayOfTexturesDpb;
+
+      D3D12_VIDEO_ENCODER_SUPPORT_FLAGS                          m_SupportFlags;
+      D3D12_VIDEO_ENCODER_VALIDATION_FLAGS                       m_ValidationFlags;
+      D3D12_FEATURE_DATA_VIDEO_ENCODER_RESOLUTION_SUPPORT_LIMITS m_currentResolutionSupportCaps;
+      union
+      {
+         D3D12_VIDEO_ENCODER_PROFILE_H264 m_H264Profile;
+         D3D12_VIDEO_ENCODER_PROFILE_HEVC m_HEVCProfile;
+      } m_encoderSuggestedProfileDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_LEVELS_H264                 m_H264LevelSetting;
+         D3D12_VIDEO_ENCODER_LEVEL_TIER_CONSTRAINTS_HEVC m_HEVCLevelSetting;
+      } m_encoderLevelSuggestedDesc = {};
+
+      // Required size for the layout-resolved metadata buffer of current frame to be encoded
+      size_t m_resolvedLayoutMetadataBufferRequiredSize;
+
+      // The maximum number of slices that the output of the current frame to be encoded will contain
+      uint32_t m_MaxSlicesInOutput;
+
+      D3D12_FEATURE_DATA_VIDEO_ENCODER_RESOURCE_REQUIREMENTS m_ResourceRequirementsCaps;
+
+   } m_currentEncodeCapabilities;
+
+   struct
+   {
+      d3d12_video_encoder_config_dirty_flags m_ConfigDirtyFlags = d3d12_video_encoder_config_dirty_flag_none;
+
+      D3D12_VIDEO_ENCODER_PICTURE_RESOLUTION_DESC m_currentResolution = {};
+      D3D12_BOX m_FrameCroppingCodecConfig = {};
+
+      D3D12_FEATURE_DATA_FORMAT_INFO m_encodeFormatInfo = {};
+
+      D3D12_VIDEO_ENCODER_CODEC m_encoderCodecDesc = {};
+
+      D3D12_VIDEO_ENCODER_SEQUENCE_CONTROL_FLAGS m_seqFlags = D3D12_VIDEO_ENCODER_SEQUENCE_CONTROL_FLAG_NONE;
+
+      /// As the following D3D12 Encode types have pointers in their structures, we need to keep a deep copy of them
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_PROFILE_H264 m_H264Profile;
+         D3D12_VIDEO_ENCODER_PROFILE_HEVC m_HEVCProfile;
+      } m_encoderProfileDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_LEVELS_H264                 m_H264LevelSetting;
+         D3D12_VIDEO_ENCODER_LEVEL_TIER_CONSTRAINTS_HEVC m_HEVCLevelSetting;
+      } m_encoderLevelDesc = {};
+
+      struct
+      {
+         D3D12_VIDEO_ENCODER_RATE_CONTROL_MODE  m_Mode;
+         D3D12_VIDEO_ENCODER_RATE_CONTROL_FLAGS m_Flags;
+         DXGI_RATIONAL                          m_FrameRate;
+         union
+         {
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_CQP  m_Configuration_CQP;
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_CBR  m_Configuration_CBR;
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_VBR  m_Configuration_VBR;
+            D3D12_VIDEO_ENCODER_RATE_CONTROL_QVBR m_Configuration_QVBR;
+         } m_Config;
+      } m_encoderRateControlDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_H264 m_H264Config;
+         D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_HEVC m_HEVCConfig;
+      } m_encoderCodecSpecificConfigDesc = {};
+
+
+      D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE m_encoderSliceConfigMode;
+      union
+      {
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES m_SlicesPartition_H264;
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES m_SlicesPartition_HEVC;
+      } m_encoderSliceConfigDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_SEQUENCE_GOP_STRUCTURE_H264 m_H264GroupOfPictures;
+         D3D12_VIDEO_ENCODER_SEQUENCE_GOP_STRUCTURE_HEVC m_HEVCGroupOfPictures;
+      } m_encoderGOPConfigDesc = {};
+
+      union
+      {
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_H264 m_H264PicData;
+         D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_HEVC m_HEVCPicData;
+      } m_encoderPicParamsDesc = {};
+
+      D3D12_VIDEO_ENCODER_MOTION_ESTIMATION_PRECISION_MODE m_encoderMotionPrecisionLimit =
+         D3D12_VIDEO_ENCODER_MOTION_ESTIMATION_PRECISION_MODE_MAXIMUM;
+
+      D3D12_VIDEO_ENCODER_INTRA_REFRESH m_IntraRefresh = { D3D12_VIDEO_ENCODER_INTRA_REFRESH_MODE_NONE, 0 };
+      uint32_t                          m_IntraRefreshCurrentFrameIndex = 0;
+
+   } m_currentEncodeConfig;
+};
+
+bool
+d3d12_video_encoder_create_command_objects(struct d3d12_video_encoder *pD3D12Enc);
+bool
+d3d12_video_encoder_reconfigure_session(struct d3d12_video_encoder *pD3D12Enc,
+                                        struct pipe_video_buffer *  srcTexture,
+                                        struct pipe_picture_desc *  picture);
+bool
+d3d12_video_encoder_update_current_encoder_config_state(struct d3d12_video_encoder *pD3D12Enc,
+                                                        struct pipe_video_buffer *  srcTexture,
+                                                        struct pipe_picture_desc *  picture);
+bool
+d3d12_video_encoder_reconfigure_encoder_objects(struct d3d12_video_encoder *pD3D12Enc,
+                                                struct pipe_video_buffer *  srcTexture,
+                                                struct pipe_picture_desc *  picture);
+D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA
+d3d12_video_encoder_get_current_picture_param_settings(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_LEVEL_SETTING
+d3d12_video_encoder_get_current_level_desc(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION
+d3d12_video_encoder_get_current_codec_config_desc(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_PROFILE_DESC
+d3d12_video_encoder_get_current_profile_desc(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_RATE_CONTROL
+d3d12_video_encoder_get_current_rate_control_settings(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA
+d3d12_video_encoder_get_current_slice_param_settings(struct d3d12_video_encoder *pD3D12Enc);
+D3D12_VIDEO_ENCODER_SEQUENCE_GOP_STRUCTURE
+d3d12_video_encoder_get_current_gop_desc(struct d3d12_video_encoder *pD3D12Enc);
+uint32_t
+d3d12_video_encoder_get_current_max_dpb_capacity(struct d3d12_video_encoder *pD3D12Enc);
+void
+d3d12_video_encoder_create_reference_picture_manager(struct d3d12_video_encoder *pD3D12Enc);
+void
+d3d12_video_encoder_update_picparams_tracking(struct d3d12_video_encoder *pD3D12Enc,
+                                              struct pipe_video_buffer *  srcTexture,
+                                              struct pipe_picture_desc *  picture);
+void
+d3d12_video_encoder_calculate_metadata_resolved_buffer_size(uint32_t maxSliceNumber, size_t &bufferSize);
+uint32_t
+d3d12_video_encoder_calculate_max_slices_count_in_output(
+   D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE                          slicesMode,
+   const D3D12_VIDEO_ENCODER_PICTURE_CONTROL_SUBREGIONS_LAYOUT_DATA_SLICES *slicesConfig,
+   uint32_t                                                                 MaxSubregionsNumberFromCaps,
+   D3D12_VIDEO_ENCODER_PICTURE_RESOLUTION_DESC                              sequenceTargetResolution,
+   uint32_t                                                                 SubregionBlockPixelsSize);
+bool
+d3d12_video_encoder_prepare_output_buffers(struct d3d12_video_encoder *pD3D12Enc,
+                                           struct pipe_video_buffer *  srcTexture,
+                                           struct pipe_picture_desc *  picture);
+uint32_t
+d3d12_video_encoder_build_codec_headers(struct d3d12_video_encoder *pD3D12Enc);
+void
+d3d12_video_encoder_extract_encode_metadata(
+   struct d3d12_video_encoder *                               pD3D12Dec,
+   ID3D12Resource *                                           pResolvedMetadataBuffer,
+   size_t                                                     resourceMetadataSize,
+   D3D12_VIDEO_ENCODER_OUTPUT_METADATA &                      encoderMetadata,
+   std::vector<D3D12_VIDEO_ENCODER_FRAME_SUBREGION_METADATA> &pSubregionsMetadata);
+
+D3D12_VIDEO_ENCODER_CODEC
+d3d12_video_encoder_get_current_codec(struct d3d12_video_encoder *pD3D12Enc);
+
+bool d3d12_video_encoder_negotiate_requested_features_and_d3d12_driver_caps(struct d3d12_video_encoder *pD3D12Enc, D3D12_FEATURE_DATA_VIDEO_ENCODER_SUPPORT &capEncoderSupportData);
+bool d3d12_video_encoder_query_d3d12_driver_caps(struct d3d12_video_encoder *pD3D12Enc, D3D12_FEATURE_DATA_VIDEO_ENCODER_SUPPORT &capEncoderSupportData);
+bool d3d12_video_encoder_check_subregion_mode_support(struct d3d12_video_encoder *pD3D12Enc, D3D12_VIDEO_ENCODER_FRAME_SUBREGION_LAYOUT_MODE requestedSlicesMode);
+
+///
+/// d3d12_video_encoder functions ends
+///
+
+#endif

upcliujie/mesa

commit 76ee4f737c4e7a0e7ad0e34a482cb86416923e5c

d3d12: add missing cmath includes
If we want to use the std:: version of the math functions, we need to
include <cmath>.

Fixes: b171a6baa21 ("d3d12: Add video encode implementation of pipe_video_codec")

src/gallium/drivers/d3d12/d3d12_video_dec_h264.cpp
@@ -24,6 +24,8 @@
 #include "d3d12_video_dec.h"
 #include "d3d12_video_dec_h264.h"
 
+#include <cmath>
+
 void
 d3d12_video_decoder_refresh_dpb_active_references_h264(struct d3d12_video_decoder *pD3D12Dec)
 {

src/gallium/drivers/d3d12/d3d12_video_enc.cpp
@@ -48,6 +48,8 @@
 #include "util/u_memory.h"
 #include "util/u_video.h"
 
+#include <cmath>
+
 /**
  * flush any outstanding command buffers to the hardware
  * should be called before a video_buffer is acessed by the gallium frontend again

src/gallium/drivers/d3d12/d3d12_video_enc_h264.cpp
@@ -27,6 +27,8 @@
 #include "d3d12_screen.h"
 #include "d3d12_format.h"
 
+#include <cmath>
+
 void
 d3d12_video_encoder_update_current_rate_control_h264(struct d3d12_video_encoder *pD3D12Enc,
                                                      pipe_h264_enc_picture_desc *picture)

src/gallium/drivers/d3d12/d3d12_video_encoder_bitstream_builder_h264.cpp
@@ -23,6 +23,8 @@
 
 #include "d3d12_video_encoder_bitstream_builder_h264.h"
 
+#include <cmath>
+
 inline H264_SPEC_PROFILES
 Convert12ToSpecH264Profiles(D3D12_VIDEO_ENCODER_PROFILE_H264 profile12)
 {