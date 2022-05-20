3663d8fd9ac5ebb1a1bdb8ccc40dc0dd6a5e1785
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-05-19 20:44:26 -0500

    vulkan/wsi: Use a single semaphore for cross-queue blits
    There's no point in having one-per-image since we signal and then
    immediately wait on it.  We have one fence per image because the
    throttling does wait on previous images.

--- src/vulkan/wsi/wsi_common_private.h
+++ src/vulkan/wsi/wsi_common_private.h
@@ -94,7 +94,7 @@ struct wsi_swapchain {
    VkDevice device;
    VkAllocationCallbacks alloc;
    VkFence* fences;
-   VkSemaphore* buffer_blit_semaphores;
+   VkSemaphore blit_queue_semaphore;
    VkPresentModeKHR present_mode;
 
    struct wsi_image_info image_info;
