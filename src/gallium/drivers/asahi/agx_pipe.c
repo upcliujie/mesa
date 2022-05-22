1d0f0f6da11a44054f79a17b4fa4e6f9dbbe68e4
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2022-05-21 23:22:27 -0400

    asahi,agx: Don't set PIPE_CAP_POINT_COORD_ORIGIN_UPPER_LEFT
    Tell the state tracker our point coordinates have a lower left origin
    instead of an upper left origin, and remove our point coordinate
    flipping code. Saves an instruction in any shader that reads
    gl_PointCoord.y
    
    Note: the OpenGL blob also emits an "fadd $y', ^y.neg, 1.0" to flip
    point coordinates, so this isn't just a Metal weirdness.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- src/gallium/drivers/asahi/agx_pipe.c
+++ src/gallium/drivers/asahi/agx_pipe.c
@@ -819,6 +819,8 @@ agx_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
    case PIPE_CAP_SEAMLESS_CUBE_MAP:
    case PIPE_CAP_SEAMLESS_CUBE_MAP_PER_TEXTURE:
       return true;
+   case PIPE_CAP_POINT_COORD_ORIGIN_UPPER_LEFT:
+      return false;
    case PIPE_CAP_FS_POINT_IS_SYSVAL:
       return false;
 
