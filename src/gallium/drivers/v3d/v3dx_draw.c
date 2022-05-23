ae8a80b2df6e9f035adfbc9ce99f583b7b2b2276
Erik Faye-Lund <erik.faye-lund@collabora.com>
2022-05-23 14:58:48 +0200

    v3dv: degrade STATIC_ASSERT to assert
    size is a variable here, so static_assert doesn't support it in the
    condition.
    
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>

--- src/gallium/drivers/v3d/v3dx_draw.c
+++ src/gallium/drivers/v3d/v3dx_draw.c
@@ -770,7 +770,7 @@ v3d_emit_gl_shader_state(struct v3d_context *v3d,
                         attr.maximum_index = 0xffffff;
 #endif
                 }
-                STATIC_ASSERT(sizeof(vtx->attrs) >= V3D_MAX_VS_INPUTS / 4 * size);
+                assert(sizeof(vtx->attrs) >= V3D_MAX_VS_INPUTS / 4 * size);
         }
 
         if (num_elements_to_emit == 0) {
