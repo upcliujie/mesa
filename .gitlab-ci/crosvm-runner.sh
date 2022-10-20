feae925150be658b1b0e58790e4dfbb10efcb437
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2022-10-20 15:41:32 +0200

    -strace

--- .gitlab-ci/crosvm-runner.sh
+++ .gitlab-ci/crosvm-runner.sh
@@ -100,7 +100,7 @@ NIR_DEBUG="novalidate" \
 LIBGL_ALWAYS_SOFTWARE=${CROSVM_LIBGL_ALWAYS_SOFTWARE} \
 GALLIUM_DRIVER=${CROSVM_GALLIUM_DRIVER} \
 VK_ICD_FILENAMES=$CI_PROJECT_DIR/install/share/vulkan/icd.d/${CROSVM_VK_DRIVER}_icd.x86_64.json \
-strace -e trace=%process,%signal,prctl,ioctl -t -f -s 250 -o /results/strace.log /usr/bin/timeout 60m crosvm --no-syslog run \
+crosvm --no-syslog run \
     --gpu "${CROSVM_GPU_ARGS}" -m ${CROSVM_MEMORY:-4096} -c ${CROSVM_CORES:-2} --disable-sandbox \
     --shared-dir /:my_root:type=fs:writeback=true:timeout=60:cache=always \
     --host-ip "192.168.30.1" --netmask "255.255.255.0" --mac "AA:BB:CC:00:00:12" \
