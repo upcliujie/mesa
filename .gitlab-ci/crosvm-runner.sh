9b58544fd80bec6d034c81640cd421638b3796d6
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2022-10-20 12:31:11 +0200

    stdout

--- .gitlab-ci/crosvm-runner.sh
+++ .gitlab-ci/crosvm-runner.sh
@@ -100,13 +100,13 @@ NIR_DEBUG="novalidate" \
 LIBGL_ALWAYS_SOFTWARE=${CROSVM_LIBGL_ALWAYS_SOFTWARE} \
 GALLIUM_DRIVER=${CROSVM_GALLIUM_DRIVER} \
 VK_ICD_FILENAMES=$CI_PROJECT_DIR/install/share/vulkan/icd.d/${CROSVM_VK_DRIVER}_icd.x86_64.json \
-strace -f -s 250 -o /results/strace.log /usr/bin/timeout 36m crosvm --no-syslog run \
+crosvm --no-syslog run \
     --gpu "${CROSVM_GPU_ARGS}" -m ${CROSVM_MEMORY:-4096} -c ${CROSVM_CORES:-2} --disable-sandbox \
     --shared-dir /:my_root:type=fs:writeback=true:timeout=60:cache=always \
     --host-ip "192.168.30.1" --netmask "255.255.255.0" --mac "AA:BB:CC:00:00:12" \
     -s $VM_SOCKET \
     --cid ${VSOCK_CID} -p "${CROSVM_KERN_ARGS}" \
-    /lava-files/${KERNEL_IMAGE_NAME:-bzImage} > ${VM_TEMP_DIR}/crosvm 2>&1
+    /lava-files/${KERNEL_IMAGE_NAME:-bzImage}
 
 CROSVM_RET=$?
 echo After Crosvm
@@ -116,14 +116,6 @@ echo After Crosvm
     CROSVM_RET=$(cat ${VM_TEMP_DIR}/exit_code 2>/dev/null)
     # Force error when the guest script's exit code is not available
     CROSVM_RET=${CROSVM_RET:-1}
-}
-
-# Show crosvm output on error to help with debugging
-[ ${CROSVM_RET} -eq 0 ] || {
-    set +x
-    echo "Dumping crosvm output.." >&2
-    cat ${VM_TEMP_DIR}/crosvm >&2
-    set -x
 }
 
 exit ${CROSVM_RET}
