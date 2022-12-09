aerisarn/mesa-uwp

commit edca30efbfdadd357800c55f57c42cc5c0f044f8

freedreno/drm: Fix suballoc alignment
IBO/etc descriptors need to be aligned to 64.. this was handled
correctly in the stateobj case (for tex descriptors) but not in
the streaming case (for IBO).

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19710>

src/freedreno/drm/freedreno_ringbuffer_sp.c
@@ -99,7 +99,7 @@ fd_submit_suballoc_ring_bo(struct fd_submit *submit,
       suballoc_offset =
          fd_ringbuffer_size(fd_submit->suballoc_ring) + suballoc_ring->offset;
 
-      suballoc_offset = align(suballoc_offset, 0x10);
+      suballoc_offset = align(suballoc_offset, SUBALLOC_ALIGNMENT);
 
       if ((size + suballoc_offset) > suballoc_bo->size) {
          suballoc_bo = NULL;
@@ -657,8 +657,7 @@ fd_ringbuffer_sp_new_object(struct fd_pipe *pipe, uint32_t size)
     */
    simple_mtx_lock(&dev->suballoc_lock);
 
-   /* Maximum known alignment requirement is a6xx's TEX_CONST at 16 dwords */
-   fd_ring->offset = align(dev->suballoc_offset, 64);
+   fd_ring->offset = align(dev->suballoc_offset, SUBALLOC_ALIGNMENT);
    if (!dev->suballoc_bo ||
        fd_ring->offset + size > fd_bo_size(dev->suballoc_bo)) {
       if (dev->suballoc_bo)

src/freedreno/drm/freedreno_priv.h
@@ -54,6 +54,8 @@
 extern simple_mtx_t table_lock;
 
 #define SUBALLOC_SIZE (32 * 1024)
+/* Maximum known alignment requirement is a6xx's TEX_CONST at 16 dwords */
+#define SUBALLOC_ALIGNMENT 64
 #define RING_FLAGS (FD_BO_GPUREADONLY | FD_BO_CACHED_COHERENT | _FD_BO_RING)
 
 /*

upcliujie/mesa

commit 03dc1a184fa590970f030ef8a6258b2f8c61d210

freedreno: align user const stateobjs to 16 rather than 64
This fixes a gfxbench gl_driver2_off perf regression and brings the fps
from 102 to 107 on a618.

Fixes: edca30efbfd ("freedreno/drm: Fix suballoc alignment")

src/freedreno/drm/freedreno_ringbuffer.h
@@ -71,8 +71,13 @@ enum fd_ringbuffer_flags {
     */
    FD_RINGBUFFER_GROWABLE = 0x4,
 
+   /* Hint that the stateobj is for user consts.  When suballocating, we can
+    * choose an alignment smaller than SUBALLOC_ALIGNMENT.
+    */
+   FD_RINGBUFFER_USER_CONSTS = 0x8,
+
    /* Internal use only: */
-   _FD_RINGBUFFER_OBJECT = 0x8,
+   _FD_RINGBUFFER_OBJECT = 0x10,
 };
 
 /* A submit object manages/tracks all the state buildup for a "submit"

src/freedreno/drm/freedreno_ringbuffer_sp.c
@@ -85,7 +85,8 @@ fd_submit_append_bo(struct fd_submit_sp *submit, struct fd_bo *bo)
 
 static void
 fd_submit_suballoc_ring_bo(struct fd_submit *submit,
-                           struct fd_ringbuffer_sp *fd_ring, uint32_t size)
+                           struct fd_ringbuffer_sp *fd_ring,
+                           uint32_t alignment, uint32_t size)
 {
    struct fd_submit_sp *fd_submit = to_fd_submit_sp(submit);
    unsigned suballoc_offset = 0;
@@ -99,7 +100,7 @@ fd_submit_suballoc_ring_bo(struct fd_submit *submit,
       suballoc_offset =
          fd_ringbuffer_size(fd_submit->suballoc_ring) + suballoc_ring->offset;
 
-      suballoc_offset = align(suballoc_offset, SUBALLOC_ALIGNMENT);
+      suballoc_offset = align(suballoc_offset, alignment);
 
       if ((size + suballoc_offset) > suballoc_bo->size) {
          suballoc_bo = NULL;
@@ -140,7 +141,9 @@ fd_submit_sp_new_ringbuffer(struct fd_submit *submit, uint32_t size,
    fd_ring->base.refcnt = 1;
 
    if (flags & FD_RINGBUFFER_STREAMING) {
-      fd_submit_suballoc_ring_bo(submit, fd_ring, size);
+      const uint32_t alignment = flags & FD_RINGBUFFER_USER_CONSTS ?
+         16 : SUBALLOC_ALIGNMENT;
+      fd_submit_suballoc_ring_bo(submit, fd_ring, alignment, size);
    } else {
       if (flags & FD_RINGBUFFER_GROWABLE)
          size = SUBALLOC_SIZE;

src/gallium/drivers/freedreno/a6xx/fd6_const.c
@@ -264,7 +264,8 @@ fd6_build_user_consts(struct fd6_emit *emit)
    unsigned sz = emit->prog->user_consts_cmdstream_size;
 
    struct fd_ringbuffer *constobj =
-      fd_submit_new_ringbuffer(ctx->batch->submit, sz, FD_RINGBUFFER_STREAMING);
+      fd_submit_new_ringbuffer(ctx->batch->submit, sz,
+            FD_RINGBUFFER_STREAMING | FD_RINGBUFFER_USER_CONSTS);
 
    /* TODO would be nice to templatize the variants (ie. HAS_GS and HAS_TESS) */
 