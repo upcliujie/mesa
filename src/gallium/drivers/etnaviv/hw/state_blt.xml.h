35ccbe361194cbd06db310118fad29a904fec8ab
Christian Gmeiner <christian.gmeiner@gmail.com>
2023-01-08 22:42:16 +0100

    etnaviv: update headers from rnndb
    Update to etna_viv commit cc832753d36d.
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- src/gallium/drivers/etnaviv/hw/state_blt.xml.h
+++ src/gallium/drivers/etnaviv/hw/state_blt.xml.h
@@ -8,13 +8,13 @@ http://0x04.net/cgit/index.cgi/rules-ng-ng
 git clone git://0x04.net/rules-ng-ng
 
 The rules-ng-ng source files this header was generated from are:
-- state.xml     (  27198 bytes, from 2022-08-16 16:28:18)
+- state.xml     (  27198 bytes, from 2023-01-08 21:35:22)
 - common.xml    (  35468 bytes, from 2020-01-04 20:02:31)
-- common_3d.xml (  15058 bytes, from 2022-10-07 06:11:53)
-- state_hi.xml  (  34803 bytes, from 2022-08-16 16:28:18)
+- common_3d.xml (  15058 bytes, from 2023-01-08 21:35:22)
+- state_hi.xml  (  34804 bytes, from 2023-01-08 19:36:28)
 - copyright.xml (   1597 bytes, from 2018-02-10 13:09:26)
 - state_2d.xml  (  51552 bytes, from 2018-02-10 13:09:26)
-- state_3d.xml  (  84326 bytes, from 2022-10-07 06:11:53)
+- state_3d.xml  (  84822 bytes, from 2023-01-08 21:35:55)
 - state_blt.xml (  14424 bytes, from 2022-10-07 06:11:53)
 - state_vg.xml  (   5975 bytes, from 2018-02-10 13:09:26)
 
