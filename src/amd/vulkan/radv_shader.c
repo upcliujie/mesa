ee42670c491fe093448170bfb445c57acb433dba
Timur Kristóf <timur.kristof@gmail.com>
2023-03-22 14:51:54 +0100

    radv: Use analysis result for determining needed deferred args.

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -1277,8 +1277,7 @@ radv_consider_culling(const struct radv_physical_device *pdevice, struct nir_sha
 
 static void
 setup_ngg_lds_layout(struct radv_device *device, nir_shader *nir, struct radv_shader_info *info,
-                     ac_nir_before_cull_analysis *analysis,
-                     const struct ac_repacked_args *needs_deferred, unsigned max_vtx_in)
+                     ac_nir_before_cull_analysis *analysis, unsigned max_vtx_in)
 {
    unsigned scratch_lds_base = 0;
    gl_shader_stage stage = nir->info.stage;
@@ -1297,7 +1296,7 @@ setup_ngg_lds_layout(struct radv_device *device, nir_shader *nir, struct radv_sh
                                             info->outinfo.export_prim_id,
                                             false, /* user edge flag */
                                             info->has_ngg_culling,
-                                            needs_deferred);
+                                            &analysis->needs_deferred);
 
       unsigned total_es_lds_bytes = pervertex_lds_bytes * max_vtx_in;
       scratch_lds_base = ALIGN(total_es_lds_bytes, 8u);
@@ -1377,15 +1376,12 @@ void radv_lower_ngg(struct radv_device *device, struct radv_pipeline_stage *ngg_
    if (info->has_ngg_culling)
       radv_optimize_nir_algebraic(nir, false);
 
-   ac_nir_before_cull_analysis an = {0};
-   struct ac_repacked_args needs_deferred = {
-      .vertex_id = true,
-      .instance_id = BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_INSTANCE_ID),
-      .tess_coord = true,
-      .rel_patch_id = true,
-      .primitive_id = BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_PRIMITIVE_ID),
+   ac_nir_before_cull_analysis an = {
+      .instance_rate_inputs =
+         nir->info.stage == MESA_SHADER_VERTEX
+            ? pl_key->vs.instance_rate_inputs << VERT_ATTRIB_GENERIC0 : 0,
    };
-   setup_ngg_lds_layout(device, nir, &ngg_stage->info, &an, &needs_deferred, max_vtx_in);
+   setup_ngg_lds_layout(device, nir, &ngg_stage->info, &an, max_vtx_in);
 
    ac_nir_lower_ngg_options options = {0};
    options.family = device->physical_device->rad_info.family;
@@ -1410,7 +1406,7 @@ void radv_lower_ngg(struct radv_device *device, struct radv_pipeline_stage *ngg_
       options.passthrough = info->is_ngg_passthrough;
       options.export_primitive_id = info->outinfo.export_prim_id;
       options.instance_rate_inputs = pl_key->vs.instance_rate_inputs << VERT_ATTRIB_GENERIC0;
-      options.needs_deferred = needs_deferred;
+      options.needs_deferred = an.needs_deferred;
 
       NIR_PASS_V(nir, ac_nir_lower_ngg_nogs, &options);
 
