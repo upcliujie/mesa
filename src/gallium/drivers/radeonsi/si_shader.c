787e7a646ac4434a237165194e3b8a99ac04d1fe
Timur Kristóf <timur.kristof@gmail.com>
2023-03-22 16:13:57 +0100

    radeonsi: Use analysis result for determining needed deferred args.

--- src/gallium/drivers/radeonsi/si_shader.c
+++ src/gallium/drivers/radeonsi/si_shader.c
@@ -1677,13 +1677,8 @@ static void si_lower_ngg(struct si_shader *shader, nir_shader *nir)
             BITSET_SET(nir->info.system_values_read, SYSTEM_VALUE_PRIMITIVE_ID);
       }
 
-      ac_nir_before_cull_analysis before_cull_analysis = {0};
-      struct ac_repacked_args needs_deferred = {
-         .vertex_id = true,
-         .instance_id = BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_INSTANCE_ID),
-         .tess_coord = true,
-         .rel_patch_id = true,
-         .primitive_id = BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_PRIMITIVE_ID),
+      ac_nir_before_cull_analysis before_cull_analysis = {
+         .instance_rate_inputs = instance_rate_inputs,
       };
 
       if (options.can_cull) {
@@ -1702,7 +1697,7 @@ static void si_lower_ngg(struct si_shader *shader, nir_shader *nir)
       options.export_primitive_id = key->ge.mono.u.vs_export_prim_id;
       options.instance_rate_inputs = instance_rate_inputs;
       options.user_clip_plane_enable_mask = clip_plane_enable;
-      options.needs_deferred = needs_deferred;
+      options.needs_deferred = before_cull_analysis.needs_deferred;
 
       NIR_PASS_V(nir, ac_nir_lower_ngg_nogs, &options);
    } else {
