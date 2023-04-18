632a421e579db66b5b1777d4332d266f6f5ee425
Eric Engestrom <eric@igalia.com>
2023-04-18 16:24:34 +0100

    ci: document which image tags build-libdrm.sh needs bumped
    This was part of another commit that turned out to be wrong, but this
    part is still useful to keep :)
    
    Signed-off-by: Eric Engestrom <eric@igalia.com>

--- .gitlab-ci/container/build-libdrm.sh
+++ .gitlab-ci/container/build-libdrm.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # shellcheck disable=SC2086 # we want word splitting
 
+# When changing this file, you need to bump the following
+# .gitlab-ci/image-tags.yml tags:
+# DEBIAN_BUILD_TAG
+# DEBIAN_BUILD_MINGW_TAG
+# DEBIAN_X86_TEST_ANDROID_TAG
+# DEBIAN_X86_TEST_GL_TAG
+# DEBIAN_X86_TEST_VK_TAG
+# ALPINE_X86_BUILD_TAG
+# FEDORA_X86_BUILD_TAG
+# KERNEL_ROOTFS_TAG
+
 set -ex
 
 export LIBDRM_VERSION=libdrm-2.4.110
