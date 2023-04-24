942f43731f8199d944ffbf11e5962879ddd28302
Oleksii Bozhenko <oleksii.bozhenko@globallogic.com>
2023-04-24 14:13:12 +0300

    dri: fix releasing context
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/8826
    
    Signed-by: Oleksii Bozhenko <oleksii.bozhenko@globallogic.com>

--- src/glx/dri_common.c
+++ src/glx/dri_common.c
@@ -493,7 +493,8 @@ driReleaseDrawables(struct glx_context *gc)
    const struct glx_display *priv = gc->psc->display;
 
    releaseDrawable(priv, gc->currentDrawable);
-   releaseDrawable(priv, gc->currentReadable);
+   if (gc->currentDrawable != gc->currentReadable)
+      releaseDrawable(priv, gc->currentReadable);
 
    gc->currentDrawable = None;
    gc->currentReadable = None;
