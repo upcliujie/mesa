13b3be8045ddc5a94cac4b5d8b4ef36fca0ce3a1
norablackcat <blackcatgames@protonmail.com>
2023-06-23 19:08:58 -0600

    rusticl/device: expose device_attribute_queries

--- src/gallium/frontends/rusticl/core/device.rs
+++ src/gallium/frontends/rusticl/core/device.rs
@@ -569,6 +569,19 @@ impl Device {
             add_ext(1, 0, 0, "cl_arm_shared_virtual_memory");
         }
 
+        match self.screen.device_vendor().to_ascii_lowercase().as_str() {
+            "amd" => {
+                add_ext(4, 0, 0, "cl_amd_device_attribute_query");
+            }
+            "intel" => {
+                add_ext(1, 0, 0, "cl_intel_device_attribute_query");
+            }
+            "nvidia" => {
+                add_ext(1, 0, 0, "cl_nv_device_attribute_query");
+            }
+            _ => {}
+        }
+
         self.extensions = exts;
         self.clc_features = feats;
         self.extension_string = format!("{} {}", PLATFORM_EXTENSION_STR, exts_str.join(" "));
