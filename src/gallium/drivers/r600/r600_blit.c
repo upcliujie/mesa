aerisarn/mesa-uwp

commit 3d6e44fd2a1a2790263fcd6d24a1cc606fed0de9

r300,r600,svga: save the FS constant buffer for u_blitter to fix a regression
Fixes: 773a23eb6da - gallium/u_blitter: clear color buffers using color from a constant buffer
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6548
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6539

Tested-by: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Charmaine Lee <charmainel@vmware.com>
Tested-by: Pavel Ondračka <pavel.ondracka@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16711>

src/gallium/drivers/r600/r600_blit.c
@@ -77,6 +77,8 @@ static void r600_blitter_begin(struct pipe_context *ctx, enum r600_blitter_op op
 		util_blitter_save_depth_stencil_alpha(rctx->blitter, rctx->dsa_state.cso);
 		util_blitter_save_stencil_ref(rctx->blitter, &rctx->stencil_ref.pipe_state);
                 util_blitter_save_sample_mask(rctx->blitter, rctx->sample_mask.sample_mask, rctx->ps_iter_samples);
+		util_blitter_save_fragment_constant_buffer_slot(rctx->blitter,
+								&rctx->constbuf_state[PIPE_SHADER_FRAGMENT].cb[0]);
 	}
 
 	if (op & R600_SAVE_FRAMEBUFFER)

src/gallium/drivers/r300/r300_blit.c
@@ -72,6 +72,15 @@ static void r300_blitter_begin(struct r300_context* r300, enum r300_blitter_op o
     util_blitter_save_vertex_buffer_slot(r300->blitter, r300->vertex_buffer);
     util_blitter_save_vertex_elements(r300->blitter, r300->velems);
 
+    struct pipe_constant_buffer cb = {
+       /* r300 doesn't use the size for FS at all. The shader determines it.
+        * Set something for blitter.
+        */
+       .buffer_size = 4,
+       .user_buffer = ((struct r300_constant_buffer*)r300->fs_constants.state)->ptr,
+    };
+    util_blitter_save_fragment_constant_buffer_slot(r300->blitter, &cb);
+
     if (op & R300_SAVE_FRAMEBUFFER) {
         util_blitter_save_framebuffer(r300->blitter, r300->fb_state.state);
     }

src/gallium/drivers/svga/svga_pipe_clear.c
@@ -58,6 +58,8 @@ begin_blit(struct svga_context *svga)
                                          (void*)svga->curr.depth);
    util_blitter_save_stencil_ref(svga->blitter, &svga->curr.stencil_ref);
    util_blitter_save_sample_mask(svga->blitter, svga->curr.sample_mask, 0);
+   util_blitter_save_fragment_constant_buffer_slot(svga->blitter,
+                                                   &svga->curr.constbufs[PIPE_SHADER_FRAGMENT][0]);
 }
 
 

upcliujie/mesa

commit 65a18f76ce93b7c27d9113446555d1b48371dcd1

r600: fix refcnt imbalance related to util_blitter_save_fragment_constant_buffer_slot()
Indeed, util_blitter_clear() requires a call to
util_blitter_save_fragment_constant_buffer_slot(),
but most other blitter functions do not.

For instance, this issue is triggered with:
"piglit/bin/arb_shader_storage_buffer_object-max-ssbo-size fs -auto -fbo"
while setting GALLIUM_REFCNT_LOG=refcnt.log.

Fixes: 3d6e44fd2a1a ("r300,r600,svga: save the FS constant buffer for u_blitter to fix a regression")
Signed-off-by: Patrick Lerda <patrick9876@free.fr>

src/gallium/drivers/r600/r600_blit.c
@@ -30,11 +30,12 @@
 enum r600_blitter_op /* bitmask */
 {
 	R600_SAVE_FRAGMENT_STATE = 1,
-	R600_SAVE_TEXTURES       = 2,
-	R600_SAVE_FRAMEBUFFER    = 4,
-	R600_DISABLE_RENDER_COND = 8,
+	R600_SAVE_FRAGMENT_CONSTANT = 2,
+	R600_SAVE_TEXTURES       = 4,
+	R600_SAVE_FRAMEBUFFER    = 8,
+	R600_DISABLE_RENDER_COND = 16,
 
-	R600_CLEAR         = R600_SAVE_FRAGMENT_STATE,
+	R600_CLEAR         = R600_SAVE_FRAGMENT_STATE | R600_SAVE_FRAGMENT_CONSTANT,
 
 	R600_CLEAR_SURFACE = R600_SAVE_FRAGMENT_STATE | R600_SAVE_FRAMEBUFFER,
 
@@ -77,8 +78,10 @@ static void r600_blitter_begin(struct pipe_context *ctx, enum r600_blitter_op op
 		util_blitter_save_depth_stencil_alpha(rctx->blitter, rctx->dsa_state.cso);
 		util_blitter_save_stencil_ref(rctx->blitter, &rctx->stencil_ref.pipe_state);
                 util_blitter_save_sample_mask(rctx->blitter, rctx->sample_mask.sample_mask, rctx->ps_iter_samples);
-		util_blitter_save_fragment_constant_buffer_slot(rctx->blitter,
-								&rctx->constbuf_state[PIPE_SHADER_FRAGMENT].cb[0]);
+
+		if (op & R600_SAVE_FRAGMENT_CONSTANT)
+			util_blitter_save_fragment_constant_buffer_slot(
+				rctx->blitter, &rctx->constbuf_state[PIPE_SHADER_FRAGMENT].cb[0]);
 	}
 
 	if (op & R600_SAVE_FRAMEBUFFER)