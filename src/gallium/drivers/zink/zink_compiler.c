5364dd677749877dde90b9ba69379e2bace031db
antonino <antonino.maniscalco@collabora.com>
2023-08-08 18:48:11 +0200

    zink: handle gl_clamp

--- src/gallium/drivers/zink/zink_compiler.c
+++ src/gallium/drivers/zink/zink_compiler.c
@@ -3848,6 +3848,9 @@ lower_st_key_sysvals_instr(nir_builder *b, nir_instr *instr, void *data)
       return true;
       break;
    }
+   case nir_intrinsic_load_txc_sat:
+      component = 1 + nir_intrinsic_txc_sat_id(intrin);
+      break;
    default:
       return false;
    }
@@ -3930,6 +3933,8 @@ zink_optimized_st_emulation_passes(nir_shader *nir, struct zink_shader *zs,
    case MESA_SHADER_FRAGMENT:
       if (key->small_key.clamp_color)
          NIR_PASS_V(nir, nir_lower_clamp_color_outputs);
+      if (key->small_key.lower_txc_sat)
+         NIR_PASS_V(nir, nir_lower_tex, NULL);
       break;
 
    default: break;
@@ -3960,6 +3965,7 @@ zink_emulation_passes(nir_shader *nir, struct zink_shader *zs)
       break;
    case MESA_SHADER_FRAGMENT:
       NIR_PASS_V(nir, nir_lower_clamp_color_outputs_enable);
+      NIR_PASS_V(nir, nir_lower_tex, NULL);
       need_optimize = true;
       break;
 
