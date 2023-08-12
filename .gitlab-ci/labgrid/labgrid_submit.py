a3e4245e1d494666ba5e7e5058a0ea778d849f0b
David Heidelberg <david@ixit.cz>
2023-08-13 00:17:20 +0200

    ci/labgrid: implement watchdog
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- .gitlab-ci/labgrid/labgrid_submit.py
+++ .gitlab-ci/labgrid/labgrid_submit.py
@@ -5,8 +5,27 @@
 """Send job to Labgrid while providing status and job run result."""
 
 import sys
+import os
+import time
+import threading
 import argparse
 import labgrid
+
+result: list[str] = []
+
+
+def is_job_hanging(max_idle_time: int):
+    """Periodically check if the file changed, if not exit the script"""
+    current_time: int = time.time()
+
+    file_modified_time: int = os.path.getmtime("results/logs/test/console_main")
+    time_diff: int = current_time - file_modified_time
+
+    if time_diff > max_idle_time:
+        print(f"Test log didn't changed within {time_diff} seconds. Stopping...")
+        return True
+
+    return False
 
 
 def parse_arguments():
@@ -14,6 +33,16 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Script to execute commands with a specified timeout.")
     parser.add_argument("--job-timeout", type=int, default=30, help="Timeout for command execution in minutes.")
     return parser.parse_args()
+
+
+def run_stage2(command, timeout: int):
+    '''Executes stage2 script'''
+    global result
+    try:
+        result = command.run_check('/init-stage2.sh', timeout=timeout)
+    except labgrid.driver.exception.ExecutionError:
+        print("init-stage2.sh failed")
+        result = []
 
 
 def main():
@@ -38,11 +67,17 @@ def main():
     except labgrid.driver.exception.ExecutionError:
         print("init-stage1.sh failed")
 
-    result: list[str] = []
-    try:
-        result = command.run_check('/init-stage2.sh', timeout=args.job_timeout * 60)
-    except labgrid.driver.exception.ExecutionError:
-        print("init-stage2.sh failed")
+    stage2_t = threading.Thread(target=run_stage2,
+                                args=(command, args.job_timeout * 60))
+    stage2_t.daemon = True
+    stage2_t.start()
+
+    max_idle_time: int = 300
+
+    while stage2_t.is_alive():
+        if is_job_hanging(max_idle_time):
+            break
+        time.sleep(5)
 
     strategy.transition("off")
 
