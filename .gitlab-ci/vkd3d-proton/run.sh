aerisarn/mesa-uwp

commit 5941fc8ccd306942c7c5af5584fac64751278123

ci: enable shellcheck on whole .gitlab-ci
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21977>

.gitlab-ci/vkd3d-proton/run.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# shellcheck disable=SC2035 # FIXME glob
 
 set -ex
 
-if [ "x$VK_DRIVER" = "x" ]; then
+if [[ -z "$VK_DRIVER" ]]; then
     exit 1
 fi
 
@@ -19,7 +20,7 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$INSTALL/lib/:/vkd3d-proton-tests/x64/"
 
 # Sanity check to ensure that our environment is sufficient to make our tests
 # run against the Mesa built by CI, rather than any installed distro version.
-MESA_VERSION=$(cat "$INSTALL/VERSION" | sed 's/\./\\./g')
+MESA_VERSION=$(sed 's/\./\\./g' "$INSTALL/VERSION")
 
 # Set the Vulkan driver to use.
 export VK_ICD_FILENAMES="$INSTALL/share/vulkan/icd.d/${VK_DRIVER}_icd.x86_64.json"
@@ -29,14 +30,6 @@ export WINEDEBUG="-all"
 export WINEPREFIX="/vkd3d-proton-wine64"
 export WINEESYNC=1
 
-print_red() {
-    RED='\033[0;31m'
-    NC='\033[0m' # No Color
-    printf "${RED}"
-    "$@"
-    printf "${NC}"
-}
-
 # wrapper to supress +x to avoid spamming the log
 quiet() {
     set +x
@@ -45,9 +38,8 @@ quiet() {
 }
 
 set +e
-vulkaninfo | tee /tmp/version.txt | grep \"Mesa $MESA_VERSION\(\s\|$\)\"
-
-if [ $? -ne 0 ]; then
+if ! vulkaninfo | tee /tmp/version.txt | grep "\"Mesa $MESA_VERSION\(\s\|$\)\"";
+then
     printf "%s\n" "Found $(cat /tmp/version.txt), expected $MESA_VERSION"
 fi
 set -e
@@ -61,10 +53,9 @@ fi
 quiet printf "%s\n" "Running vkd3d-proton testsuite..."
 
 set +e
-/vkd3d-proton-tests/x64/bin/d3d12 > $RESULTS/vkd3d-proton.log
-
-if [ $? != 0 ]; then
-    quiet print_red printf "%s\n" "Failed, see vkd3d-proton.log!"
+if ! /vkd3d-proton-tests/x64/bin/d3d12 > "$RESULTS/vkd3d-proton.log";
+then
+    error printf "%s\n" "Failed, see vkd3d-proton.log!"
 
     # Collect all the failures
     VKD3D_PROTON_RESULTS="${VKD3D_PROTON_RESULTS:-vkd3d-proton-results}"
@@ -82,7 +73,7 @@ if [ $? != 0 ]; then
 
     # Make sure that the failures found in this run match the current expectation
     if ! diff -q ".gitlab-ci/vkd3d-proton/$VKD3D_PROTON_RESULTS.txt.baseline" "$RESULTSFILE"; then
-        quiet print_red printf "%s\n" "Changes found, see vkd3d-proton.log!"
+        error printf "%s\n" "Changes found, see vkd3d-proton.log!"
         quiet diff --color=always -u ".gitlab-ci/vkd3d-proton/$VKD3D_PROTON_RESULTS.txt.baseline" "$RESULTSFILE"
     fi
 

.gitlab-ci/bare-metal/cisco-2960-poe-off.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2086 # we want word splitting
 
 if [ -z "$BM_POE_INTERFACE" ]; then
     echo "Must supply the PoE Interface to power down"
@@ -11,7 +12,6 @@ if [ -z "$BM_POE_ADDRESS" ]; then
 fi
 
 SNMP_KEY="1.3.6.1.4.1.9.9.402.1.2.1.1.1.$BM_POE_INTERFACE"
-SNMP_ON="i 1"
 SNMP_OFF="i 4"
 
-snmpset -v2c -r 3 -t 30 -cmesaci $BM_POE_ADDRESS $SNMP_KEY $SNMP_OFF
+snmpset -v2c -r 3 -t 30 -cmesaci "$BM_POE_ADDRESS" "$SNMP_KEY" $SNMP_OFF

.gitlab-ci/bare-metal/cisco-2960-poe-on.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2086 # we want word splitting
 
 if [ -z "$BM_POE_INTERFACE" ]; then
     echo "Must supply the PoE Interface to power up"
@@ -16,6 +17,6 @@ SNMP_KEY="1.3.6.1.4.1.9.9.402.1.2.1.1.1.$BM_POE_INTERFACE"
 SNMP_ON="i 1"
 SNMP_OFF="i 4"
 
-snmpset -v2c -r 3 -t 10 -cmesaci $BM_POE_ADDRESS $SNMP_KEY $SNMP_OFF
+snmpset -v2c -r 3 -t 10 -cmesaci "$BM_POE_ADDRESS" "$SNMP_KEY" $SNMP_OFF
 sleep 3s
-snmpset -v2c -r 3 -t 10 -cmesaci $BM_POE_ADDRESS $SNMP_KEY $SNMP_ON
+snmpset -v2c -r 3 -t 10 -cmesaci "$BM_POE_ADDRESS" "$SNMP_KEY" $SNMP_ON

.gitlab-ci/bare-metal/cros-servo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# shellcheck disable=SC1091 # The relative paths in this file only become valid at runtime.
+# shellcheck disable=SC2034
+# shellcheck disable=SC2086 # we want word splitting
 
 # Boot script for Chrome OS devices attached to a servo debug connector, using
 # NFS and TFTP to boot.

.gitlab-ci/bare-metal/eth008-power-down.sh
@@ -7,4 +7,4 @@ if [ -z "$relay" ]; then
     exit 1
 fi
 
-$CI_PROJECT_DIR/install/bare-metal/eth008-power-relay.py $ETH_HOST $ETH_PORT off $relay
+"$CI_PROJECT_DIR"/install/bare-metal/eth008-power-relay.py "$ETH_HOST" "$ETH_PORT" off "$relay"

.gitlab-ci/bare-metal/eth008-power-up.sh
@@ -7,6 +7,6 @@ if [ -z "$relay" ]; then
     exit 1
 fi
 
-$CI_PROJECT_DIR/install/bare-metal/eth008-power-relay.py $ETH_HOST $ETH_PORT off $relay
+"$CI_PROJECT_DIR"/install/bare-metal/eth008-power-relay.py "$ETH_HOST" "$ETH_PORT" off "$relay"
 sleep 5
-$CI_PROJECT_DIR/install/bare-metal/eth008-power-relay.py $ETH_HOST $ETH_PORT on $relay
+"$CI_PROJECT_DIR"/install/bare-metal/eth008-power-relay.py "$ETH_HOST" "$ETH_PORT" on "$relay"

.gitlab-ci/bare-metal/expect-output.sh
@@ -5,26 +5,27 @@ set -e
 STRINGS=$(mktemp)
 ERRORS=$(mktemp)
 
-trap "rm $STRINGS; rm $ERRORS;" EXIT
+trap 'rm $STRINGS; rm $ERRORS;' EXIT
 
 FILE=$1
 shift 1
 
 while getopts "f:e:" opt; do
   case $opt in
-    f) echo "$OPTARG" >> $STRINGS;;
-    e) echo "$OPTARG" >> $STRINGS ; echo "$OPTARG" >> $ERRORS;;
+    f) echo "$OPTARG" >> "$STRINGS";;
+    e) echo "$OPTARG" >> "$STRINGS" ; echo "$OPTARG" >> "$ERRORS";;
+    *) exit
   esac
 done
 shift $((OPTIND -1))
 
 echo "Waiting for $FILE to say one of following strings"
-cat $STRINGS
+cat "$STRINGS"
 
-while ! egrep -wf $STRINGS $FILE; do
+while ! grep -E -wf "$STRINGS" "$FILE"; do
   sleep 2
 done
 
-if egrep -wf $ERRORS $FILE; then
+if grep -E -wf "$ERRORS" "$FILE"; then
   exit 1
 fi

.gitlab-ci/bare-metal/fastboot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# shellcheck disable=SC1091 # The relative paths in this file only become valid at runtime.
+# shellcheck disable=SC2034
+# shellcheck disable=SC2086 # we want word splitting
 
 . "$SCRIPTS_DIR"/setup-test-env.sh
 
 BM=$CI_PROJECT_DIR/install/bare-metal
 CI_COMMON=$CI_PROJECT_DIR/install/common
 
-if [ -z "$BM_SERIAL" -a -z "$BM_SERIAL_SCRIPT" ]; then
+if [ -z "$BM_SERIAL" ] && [ -z "$BM_SERIAL_SCRIPT" ]; then
   echo "Must set BM_SERIAL OR BM_SERIAL_SCRIPT in your gitlab-runner config.toml [[runners]] environment"
   echo "BM_SERIAL:"
   echo "  This is the serial device to talk to for waiting for fastboot to be ready and logging from the kernel."
@@ -84,10 +87,10 @@ else
   fi
 
   pushd rootfs
-  find -H | \
-    egrep -v "external/(openglcts|vulkancts|amber|glslang|spirv-tools)" |
-    egrep -v "traces-db|apitrace|renderdoc" | \
-    egrep -v $EXCLUDE_FILTER | \
+  find -H . | \
+    grep -E -v "external/(openglcts|vulkancts|amber|glslang|spirv-tools)" |
+    grep -E -v "traces-db|apitrace|renderdoc" | \
+    grep -E -v $EXCLUDE_FILTER | \
     cpio -H newc -o | \
     xz --check=crc32 -T4 - > $CI_PROJECT_DIR/rootfs.cpio.gz
   popd

upcliujie/mesa

commit 3e4bcd66a33d874246d4eb3041396f92f0aa0978

ci/vkd3d: don't ignore errors
Fixes: 5941fc8ccd306942c7c5 ("ci: enable shellcheck on whole .gitlab-ci")
Signed-off-by: Eric Engestrom <eric@igalia.com>

.gitlab-ci/vkd3d-proton/run.sh
@@ -52,7 +52,6 @@ fi
 
 quiet printf "%s\n" "Running vkd3d-proton testsuite..."
 
-set +e
 if ! /vkd3d-proton-tests/x64/bin/d3d12 > "$RESULTS/vkd3d-proton.log";
 then
     error printf "%s\n" "Failed, see vkd3d-proton.log!"