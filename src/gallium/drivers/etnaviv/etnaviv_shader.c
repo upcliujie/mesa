0c7d4136edc52a73fceb0f98a697f0a8fd83a3a5
Christian Gmeiner <cgmeiner@igalia.com>
2023-10-25 14:47:48 +0200

    etnaviv: Replace hard coded value with ETNA_NUM_VARYINGS
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>

--- src/gallium/drivers/etnaviv/etnaviv_shader.c
+++ src/gallium/drivers/etnaviv/etnaviv_shader.c
@@ -154,7 +154,7 @@ etna_link_shaders(struct etna_context *ctx, struct compiled_shader_state *cs,
    cs->VS_OUTPUT_COUNT = 1 + link.num_varyings; /* position + varyings */
 
    /* vs outputs (varyings) */
-   DEFINE_ETNA_BITARRAY(vs_output, 16, 8) = {0};
+   DEFINE_ETNA_BITARRAY(vs_output, ETNA_NUM_VARYINGS, 8) = {0};
    int varid = 0;
    etna_bitarray_set(vs_output, 8, varid++, vs->vs_pos_out_reg);
    for (int idx = 0; idx < link.num_varyings; ++idx)
