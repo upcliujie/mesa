46138c1bcdd4ec8e3ea013163308ec60b65ae669
Konstantin Seurer <konstantin.seurer@gmail.com>
2023-11-19 22:09:38 +0100

    lavapipe: Advertise VK_EXT_depth_bias_control

--- src/gallium/frontends/lavapipe/lvp_device.c
+++ src/gallium/frontends/lavapipe/lvp_device.c
@@ -180,6 +180,7 @@ static const struct vk_device_extension_table lvp_device_extensions_supported = 
    .EXT_calibrated_timestamps             = true,
    .EXT_color_write_enable                = true,
    .EXT_conditional_rendering             = true,
+   .EXT_depth_bias_control                = true,
    .EXT_depth_clip_enable                 = true,
    .EXT_depth_clip_control                = true,
    .EXT_depth_range_unrestricted          = true,
@@ -494,6 +495,12 @@ lvp_get_features(const struct lvp_physical_device *pdevice,
 
       /* VK_EXT_image_sliced_view_of_3d */
       .imageSlicedViewOf3D = true,
+
+      /* VK_EXT_depth_bias_control */
+      .depthBiasControl = true,
+      .leastRepresentableValueForceUnormRepresentation = true,
+      .floatRepresentation = true,
+      .depthBiasExact = true,
 
       /* VK_EXT_depth_clip_control */
       .depthClipControl = true,
