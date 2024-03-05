27bfe63b4315bc44b1e749eadf640818e1f51a21
Connor Abbott <cwabbott0@gmail.com>
2024-03-05 06:26:46 -0500

    vulkan/runtime: Add shared BVH building framework
    This is mostly adapted from radv's BVH building. This defines a common
    "IR" for BVH trees, two algorithms for constructing it, and a callback
    that the driver implements for encoding. The framework takes care of
    parallelizing the different passes, so the driver just has to split the
    encoding process into "stages" and implement just one part for each
    stage.

--- src/vulkan/runtime/vk_acceleration_structure.h
+++ src/vulkan/runtime/vk_acceleration_structure.h
@@ -26,6 +26,12 @@
 #define VK_ACCELERATION_STRUCTURE_H
 
 #include "vk_object.h"
+#include "vk_meta.h"
+#include "radix_sort/radix_sort_vk.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 struct vk_acceleration_structure {
    struct vk_object_base base;
@@ -39,5 +45,63 @@ VkDeviceAddress vk_acceleration_structure_get_va(struct vk_acceleration_structur
 
 VK_DEFINE_NONDISP_HANDLE_CASTS(vk_acceleration_structure, base, VkAccelerationStructureKHR,
                                VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_KHR)
+
+#define MAX_ENCODE_PASSES 2
+#define MAX_UPDATE_PASSES 2
+
+struct vk_acceleration_structure_build_ops {
+   VkDeviceSize (*get_as_size)(VkDevice device,
+                               VkGeometryTypeKHR geometry_type,
+                               uint32_t leaf_count);
+   VkDeviceSize (*get_update_scratch_size)(struct vk_device *device, uint32_t leaf_count);
+   uint32_t (*get_encode_key[MAX_ENCODE_PASSES])(VkAccelerationStructureTypeKHR type,
+                                                 VkBuildAccelerationStructureFlagBitsKHR flags);
+   VkResult (*encode_bind_pipeline[MAX_ENCODE_PASSES])(VkCommandBuffer cmd_buffer,
+                                                       uint32_t key);
+   void (*encode_as[MAX_ENCODE_PASSES])(VkCommandBuffer cmd_buffer,
+                                        struct vk_acceleration_structure *dst,
+                                        VkDeviceAddress intermediate_as_addr,
+                                        VkDeviceAddress intermediate_header_addr,
+                                        uint32_t leaf_count,
+                                        VkGeometryTypeKHR geometry_type,
+                                        uint32_t key);
+   void (*init_update_scratch)(VkCommandBuffer cmd_buffer,
+                               VkDeviceAddress scratch,
+                               uint32_t leaf_count,
+                               struct vk_acceleration_structure *src_as,
+                               struct vk_acceleration_structure *dst_as);
+   void (*update_bind_pipeline[MAX_ENCODE_PASSES])(VkCommandBuffer cmd_buffer);
+   void (*update_as[MAX_ENCODE_PASSES])(VkCommandBuffer cmd_buffer,
+                                        VkDeviceAddress scratch,
+                                        uint32_t leaf_count,
+                                        VkGeometryTypeKHR geometry_type,
+                                        struct vk_acceleration_structure *dst,
+                                        struct vk_acceleration_structure *src);
+};
+
+struct vk_acceleration_structure_build_args {
+   uint32_t subgroup_size;
+   const radix_sort_vk_t *radix_sort;
+};
+
+void vk_cmd_build_acceleration_structures(VkCommandBuffer cmdbuf,
+                                          struct vk_device *device,
+                                          struct vk_meta_device *meta,
+                                          uint32_t info_count,
+                                          const VkAccelerationStructureBuildGeometryInfoKHR *pInfos,
+                                          const VkAccelerationStructureBuildRangeInfoKHR *const *ppBuildRangeInfos,
+                                          const struct vk_acceleration_structure_build_args *args);
+
+void vk_get_as_build_sizes(VkDevice _device, VkAccelerationStructureBuildTypeKHR buildType,
+                           const VkAccelerationStructureBuildGeometryInfoKHR *pBuildInfo,
+                           const uint32_t *pMaxPrimitiveCounts,
+                           VkAccelerationStructureBuildSizesInfoKHR *pSizeInfo,
+                           const struct vk_acceleration_structure_build_args *args);
+
+bool vk_acceleration_struct_vtx_format_supported(VkFormat format);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
