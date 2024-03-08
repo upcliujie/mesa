74b764fffb8e0a74cce2c89d9ab3d249b5424fa0
Daniel Schürmann <daniel@schuermann.dev>
2024-03-08 15:44:42 +0100

    nir/divergence_analysis: handle loop continue constructs

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -1130,7 +1130,6 @@ visit_if(nir_if *if_stmt, struct divergence_state *state)
 static bool
 visit_loop(nir_loop *loop, struct divergence_state *state)
 {
-   assert(!nir_loop_has_continue_construct(loop));
    bool progress = false;
    nir_block *loop_header = nir_loop_first_block(loop);
    nir_block *loop_preheader = nir_block_cf_tree_prev(loop_header);
@@ -1161,6 +1160,18 @@ visit_loop(nir_loop *loop, struct divergence_state *state)
    do {
       progress |= visit_cf_list(&loop->body, &loop_state);
       repeat = false;
+
+      if (nir_loop_has_continue_construct(loop)) {
+         /* Handle the phis at the loop continue construct which merge
+          * all loop-carried values.
+          */
+         nir_foreach_phi(phi, nir_loop_first_continue_block(loop)) {
+            progress |= visit_loop_header_phi(phi, NULL,
+                                              loop_state.divergent_loop_continue);
+         }
+
+         progress |= visit_cf_list(&loop->continue_list, &loop_state);
+      }
 
       /* revisit loop header phis to see if something has changed */
       nir_foreach_phi(phi, loop_header) {
