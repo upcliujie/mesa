5a6d5f39dcd510ea5b9acbd3b6b28c1a81322a1e
Rohan Garg <rohan.garg@intel.com>
2024-03-22 13:04:19 +0100

    iris: allow fast clearing formats with int channels on Gen12+
    This helps fix a number of GLCTS tests that have color/depth attachments
    by allowing fast clears with a integer clear color on the resource.
    
    Fixes:
      - KHR-GL46.texture_cube_map_array.color_depth_attachments
      - KHR-GLES31.core.texture_cube_map_array.color_depth_attachments
      - KHR-GLESEXT.texture_cube_map_array.color_depth_attachments
      - dEQP-GLES3.functional.fbo.render.resize.*
    
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>

--- src/gallium/drivers/iris/iris_clear.c
+++ src/gallium/drivers/iris/iris_clear.c
@@ -42,7 +42,7 @@ iris_is_color_fast_clear_compatible(struct iris_context *ice,
    struct iris_batch *batch = &ice->batches[IRIS_BATCH_RENDER];
    const struct intel_device_info *devinfo = batch->screen->devinfo;
 
-   if (isl_format_has_int_channel(format)) {
+   if (isl_format_has_int_channel(format) && devinfo->ver < 12) {
       perf_debug(&ice->dbg, "Integer fast clear not enabled for %s\n",
                  isl_format_get_name(format));
       return false;
