199a022087775e0572e3971b02914328e4fdc21a
Michael J. Baars <mjbaars1977.backup@gmail.com>
2024-04-05 14:49:00 +0000

    Update si_get.c

--- src/gallium/drivers/radeonsi/si_get.c
+++ src/gallium/drivers/radeonsi/si_get.c
@@ -165,7 +165,7 @@ static int si_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
    case PIPE_CAP_FENCE_SIGNAL:
    case PIPE_CAP_NATIVE_FENCE_FD:
    case PIPE_CAP_CL_GL_SHARING:
-	case PIPE_CAP_BLEND_MINMAX_FACTOR:
+   case PIPE_CAP_BLEND_MINMAX_FACTOR:
       return 1;
 
    case PIPE_CAP_TEXTURE_TRANSFER_MODES:
