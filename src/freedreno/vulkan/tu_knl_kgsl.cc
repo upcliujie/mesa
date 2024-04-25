871635c0f53432ea7490dd72d8f3b768310dac63
Connor Abbott <cwabbott0@gmail.com>
2024-04-25 08:10:06 -0400

    tu: Plumb through raytracing fuse

--- src/freedreno/vulkan/tu_knl_kgsl.cc
+++ src/freedreno/vulkan/tu_knl_kgsl.cc
@@ -1406,6 +1406,17 @@ static const struct tu_knl kgsl_knl_funcs = {
       .queue_submit = kgsl_queue_submit,
 };
 
+static bool
+tu_kgsl_get_raytracing(int fd)
+{
+   uint32_t value;
+   int ret = get_kgsl_prop(fd, KGSL_PROP_IS_RAYTRACING_ENABLED, &value, sizeof(value));
+   if (ret)
+      return false;
+
+   return value;
+}
+
 VkResult
 tu_knl_kgsl_load(struct tu_instance *instance, int fd)
 {
@@ -1445,6 +1456,8 @@ tu_knl_kgsl_load(struct tu_instance *instance, int fd)
    device->dev_id.chip_id = info.chip_id;
    device->gmem_size = debug_get_num_option("TU_GMEM", info.gmem_sizebytes);
    device->gmem_base = gmem_iova;
+
+   device->has_raytracing = tu_kgsl_get_raytracing(fd);
 
    device->submitqueue_priority_count = 1;
    
