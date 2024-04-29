a8d9180ed37cf3dda4831f3b0bec28435bdfc450
Boris Brezillon <boris.brezillon@collabora.com>
2024-04-29 10:01:26 +0200

    panvk: Add helpers to save/restore the compute/graphics state for meta operations
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>

--- src/panfrost/vulkan/jm/panvk_vX_cmd_dispatch.c
+++ src/panfrost/vulkan/jm/panvk_vX_cmd_dispatch.c
@@ -163,3 +163,53 @@ panvk_per_arch(CmdDispatchIndirect)(VkCommandBuffer commandBuffer,
 {
    panvk_stub();
 }
+
+void
+panvk_per_arch(cmd_meta_compute_start)(
+   struct panvk_cmd_buffer *cmdbuf,
+   struct panvk_cmd_meta_compute_save_ctx *save_ctx)
+{
+   const struct panvk_descriptor_set *set0 =
+      cmdbuf->state.compute.desc_state.sets[0];
+   struct panvk_descriptor_set *push_set0 =
+      cmdbuf->state.compute.desc_state.push_sets[0];
+
+   save_ctx->set0 = set0;
+   if (push_set0 && push_set0 == set0) {
+      save_ctx->push_set0.desc_count = push_set0->desc_count;
+      save_ctx->push_set0.descs_dev_addr = push_set0->descs.dev;
+      memcpy(save_ctx->push_set0.desc_storage, push_set0->descs.host,
+             push_set0->desc_count * PANVK_DESCRIPTOR_SIZE);
+   }
+
+   save_ctx->push_constants = cmdbuf->state.push_constants;
+   save_ctx->cs.shader = cmdbuf->state.compute.shader;
+   save_ctx->cs.desc = cmdbuf->state.compute.cs.desc;
+}
+
+void
+panvk_per_arch(cmd_meta_compute_end)(
+   struct panvk_cmd_buffer *cmdbuf,
+   const struct panvk_cmd_meta_compute_save_ctx *save_ctx)
+{
+   struct panvk_descriptor_set *push_set0 =
+      cmdbuf->state.gfx.desc_state.push_sets[0];
+
+   cmdbuf->state.gfx.desc_state.sets[0] = save_ctx->set0;
+   if (save_ctx->push_set0.desc_count) {
+      memcpy(push_set0->descs.host, save_ctx->push_set0.desc_storage,
+             save_ctx->push_set0.desc_count * PANVK_DESCRIPTOR_SIZE);
+      push_set0->descs.dev = save_ctx->push_set0.descs_dev_addr;
+      push_set0->desc_count = save_ctx->push_set0.desc_count;
+   }
+
+   if (memcmp(cmdbuf->state.push_constants.data, save_ctx->push_constants.data,
+              sizeof(cmdbuf->state.push_constants.data))) {
+      cmdbuf->state.push_constants = save_ctx->push_constants;
+      cmdbuf->state.compute.push_uniforms = 0;
+      cmdbuf->state.gfx.push_uniforms = 0;
+   }
+
+   cmdbuf->state.compute.shader = save_ctx->cs.shader;
+   cmdbuf->state.compute.cs.desc = save_ctx->cs.desc;
+}
