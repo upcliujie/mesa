5901ebbbae87e662c850ecb13815a3802dc6fdd5
Ian Douglas Scott <idscott@system76.com>
2024-05-10 11:57:22 -0700

    wsi/wayland: Use `wl_fixes` to destroy `wl_registry`
    Destroy `wl_registry` objects using
    https://gitlab.freedesktop.org/wayland/wayland/-/merge_requests/388.
    
    Not sure how best to make use of unreleased `libwayland` features here.
    But anyway, this works as a placeholder, and can be tested by setting
    `VK_ICD_FILENAMES`, `LD_LIBRARY_PATH`, and
    `__EGL_VENDOR_LIBRARY_FILENAMES`.
    
    I guess it will need to be made conditional libwayland version once
    the changes are merged in libwayland. Or the libwayland version with
    this feature would be a hard dependency.

--- src/egl/drivers/dri2/egl_dri2.h
+++ src/egl/drivers/dri2/egl_dri2.h
@@ -54,6 +54,7 @@ struct wl_drm;
 struct wl_registry;
 struct wl_shm;
 struct wl_surface;
+struct wl_fixes;
 struct zwp_linux_dmabuf_v1;
 struct zwp_linux_dmabuf_feedback_v1;
 #endif
@@ -305,6 +306,7 @@ struct dri2_egl_display {
    uint32_t wl_drm_version, wl_drm_name;
    struct wl_shm *wl_shm;
    struct wl_event_queue *wl_queue;
+   struct wl_fixes *wl_fixes;
    struct zwp_linux_dmabuf_v1 *wl_dmabuf;
    struct dri2_wl_formats formats;
    struct zwp_linux_dmabuf_feedback_v1 *wl_dmabuf_feedback;
