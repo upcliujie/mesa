960bc7b5e4ce73dd5372827a5ffb9d760451e2ac
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2024-05-12 15:21:36 +0300

    anv: simplify loading driver internal constants
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>

--- src/intel/vulkan/anv_nir_compute_push_layout.c
+++ src/intel/vulkan/anv_nir_compute_push_layout.c
@@ -26,8 +26,6 @@
 #include "compiler/brw_nir.h"
 #include "util/mesa-sha1.h"
 
-#define sizeof_field(type, field) sizeof(((type *)0)->field)
-
 void
 anv_nir_compute_push_layout(nir_shader *nir,
                             const struct anv_physical_device *pdevice,
@@ -109,16 +107,18 @@ anv_nir_compute_push_layout(nir_shader *nir,
        * the shader.
        */
       const uint32_t push_reg_mask_start =
-         offsetof(struct anv_push_constants, push_reg_mask[nir->info.stage]);
-      const uint32_t push_reg_mask_end = push_reg_mask_start + sizeof(uint64_t);
+         anv_drv_const_offset(push_reg_mask[nir->info.stage]);
+      const uint32_t push_reg_mask_end = push_reg_mask_start +
+                                         anv_drv_const_size(push_reg_mask[nir->info.stage]);
       push_start = MIN2(push_start, push_reg_mask_start);
       push_end = MAX2(push_end, push_reg_mask_end);
    }
 
    if (nir->info.stage == MESA_SHADER_FRAGMENT && fragment_dynamic) {
       const uint32_t fs_msaa_flags_start =
-         offsetof(struct anv_push_constants, gfx.fs_msaa_flags);
-      const uint32_t fs_msaa_flags_end = fs_msaa_flags_start + sizeof(uint32_t);
+         anv_drv_const_offset(gfx.fs_msaa_flags);
+      const uint32_t fs_msaa_flags_end = fs_msaa_flags_start +
+                                         anv_drv_const_size(gfx.fs_msaa_flags);
       push_start = MIN2(push_start, fs_msaa_flags_start);
       push_end = MAX2(push_end, fs_msaa_flags_end);
    }
@@ -131,8 +131,8 @@ anv_nir_compute_push_layout(nir_shader *nir,
        * push constants one dword less than the full amount including
        * gl_SubgroupId.
        */
-      assert(push_end <= offsetof(struct anv_push_constants, cs.subgroup_id));
-      push_end = offsetof(struct anv_push_constants, cs.subgroup_id);
+      assert(push_end <= anv_drv_const_offset(cs.subgroup_id));
+      push_end = anv_drv_const_offset(cs.subgroup_id);
    }
 
    /* Align push_start down to a 32B boundary and make it no larger than
@@ -259,7 +259,7 @@ anv_nir_compute_push_layout(nir_shader *nir,
 
       if (robust_flags & BRW_ROBUSTNESS_UBO) {
          const uint32_t push_reg_mask_offset =
-            offsetof(struct anv_push_constants, push_reg_mask[nir->info.stage]);
+            anv_drv_const_offset(push_reg_mask[nir->info.stage]);
          assert(push_reg_mask_offset >= push_start);
          prog_data->push_reg_mask_param =
             (push_reg_mask_offset - push_start) / 4;
@@ -315,7 +315,7 @@ anv_nir_compute_push_layout(nir_shader *nir,
          container_of(prog_data, struct brw_wm_prog_data, base);
 
       const uint32_t fs_msaa_flags_offset =
-         offsetof(struct anv_push_constants, gfx.fs_msaa_flags);
+         anv_drv_const_offset(gfx.fs_msaa_flags);
       assert(fs_msaa_flags_offset >= push_start);
       wm_prog_data->msaa_flags_param =
          (fs_msaa_flags_offset - push_start) / 4;
