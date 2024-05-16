4af29e36cf007e9833371dbea42cfc7d2b451b76
Yogesh Mohan Marimuthu <yogesh.mohanmarimuthu@amd.com>
2024-05-16 07:47:01 +0530

    winsys/amdgpu: use VM_ALWAYS_VALID for all VRAM and GTT allocations

--- src/gallium/winsys/amdgpu/drm/amdgpu_bo.c
+++ src/gallium/winsys/amdgpu/drm/amdgpu_bo.c
@@ -611,6 +611,12 @@ static struct amdgpu_winsys_bo *amdgpu_create_bo(struct amdgpu_winsys *aws,
    if (flags & RADEON_FLAG_GTT_WC)
       request.flags |= AMDGPU_GEM_CREATE_CPU_GTT_USWC;
 
+   if (aws->info.is_userq_enabled &&
+       initial_domain & (RADEON_DOMAIN_VRAM_GTT | RADEON_DOMAIN_DOORBELL)) {
+      if (flags & RADEON_FLAG_NO_INTERPROCESS_SHARING)
+         request.flags |= AMDGPU_GEM_CREATE_VM_ALWAYS_VALID;
+   }
+
    if (flags & RADEON_FLAG_DISCARDABLE &&
        aws->info.drm_minor >= 47)
       request.flags |= AMDGPU_GEM_CREATE_DISCARDABLE;
