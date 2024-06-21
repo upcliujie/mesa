aec63f42e73af5c8b314ae21408b49dc733c31b9
Erik Faye-Lund <erik.faye-lund@collabora.com>
2024-06-21 14:43:10 +0200

    docs: use sphinx.ext.extlinks instead of vk-feat role
    This built-in extension does exactly the same thing, except it's less
    configuration. Let's use it.

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'hawkmoth',
     'nir',
     'redirects',
+    'sphinx.ext.extlinks',
     'sphinx.ext.graphviz',
 ]
 
@@ -238,3 +239,10 @@ rst_prolog = '''
 .. |out| replace:: **[out]**
 .. |inout| replace:: **[inout]**
 '''
+
+# -- Options for ExtLinks -------------------------------------------------
+
+extlinks = {
+    'vk-feat': ('https://registry.khronos.org/vulkan/specs/1.3-extensions/html/vkspec.html#features-%s',
+                '%s')
+}
