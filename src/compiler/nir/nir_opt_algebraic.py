b37acbe233fa45e3e2e1ca4008c704b168544a1f
Konstantin Seurer <konstantin.seurer@gmail.com>
2024-07-07 17:01:31 +0200

    nir/opt_algebraic: Fix uint sizes

--- src/compiler/nir/nir_opt_algebraic.py
+++ src/compiler/nir/nir_opt_algebraic.py
@@ -1921,8 +1921,8 @@ optimizations.extend([
     ('iadd', ('u2u32', a), ('u2u32', b))),
 
    # Lowered pack followed by lowered unpack, for the high bits
-   (('u2u32', ('ushr', ('ior', ('ishl', a, 32), ('u2u64', b)), 32)), ('u2u32', a)),
-   (('u2u16', ('ushr', ('ior', ('ishl', a, 16), ('u2u32', b)), 16)), ('u2u16', a)),
+   (('u2u32', ('ushr', ('ior', ('ishl', a, 32), ('u2u64', 'b@32')), 32)), ('u2u32', a)),
+   (('u2u16', ('ushr', ('ior', ('ishl', a, 16), ('u2u32', 'b@16')), 16)), ('u2u16', a)),
 ])
 
 # After the ('extract_u8', a, 0) pattern, above, triggers, there will be
