e5166b991fe0da350513dcf247a43420dd04d564
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2024-07-17 12:58:48 +0200

    radv: fix programming DB_RENDER_CONTROL for NULL depth/stencil on GFX11_5
    It should be programmed like GFX11.
    
    Cc: mesa-stable
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30225>
    (cherry picked from commit d2ae0c9ef8fcc46c3b6155fab90a7fa704cc5ba5)

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -3103,7 +3103,7 @@ radv_emit_null_ds_state(struct radv_cmd_buffer *cmd_buffer)
    /* On GFX11, DB_Z_INFO.NUM_SAMPLES should always match MSAA_EXPOSED_SAMPLES. It affects VRS,
     * occlusion queries and Primitive Ordered Pixel Shading if depth and stencil are not bound.
     */
-   if (gfx_level == GFX11) {
+   if (gfx_level == GFX11 || gfx_level == GFX11_5) {
       num_samples = util_logbase2(radv_get_rasterization_samples(cmd_buffer));
       radv_gfx11_set_db_render_control(device, 1, &db_render_control);
    }
