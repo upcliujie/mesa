e94346c435d8286f2f357f9e7e2d5a7ba1955159
David Rosca <david.rosca@amd.com>
2024-07-30 08:35:30 +0200

    gallium: Remove pipe_h265_enc_picture_desc decoded_curr_pic and reference_frames
    This is not used by any driver, and it's not useful on its own anyway.

--- src/gallium/frontends/va/picture_hevc_enc.c
+++ src/gallium/frontends/va/picture_hevc_enc.c
@@ -48,15 +48,9 @@ vlVaHandleVAEncPictureParameterBufferTypeHEVC(vlVaDriver *drv, vlVaContext *cont
 {
    VAEncPictureParameterBufferHEVC *h265;
    vlVaBuffer *coded_buf;
-   int i;
 
    h265 = buf->data;
-   context->desc.h265enc.decoded_curr_pic = h265->decoded_curr_pic.picture_id;
    context->desc.h265enc.not_referenced = !h265->pic_fields.bits.reference_pic_flag;
-
-   for (i = 0; i < 15; i++)
-      context->desc.h265enc.reference_frames[i] = h265->reference_frames[i].picture_id;
-
    context->desc.h265enc.pic_order_cnt = h265->decoded_curr_pic.pic_order_cnt;
    coded_buf = handle_table_get(drv->htab, h265->coded_buf);
    if (!coded_buf)
