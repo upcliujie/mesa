3fb872d6da42a4ddc5a6946eaf377eca8d2d9187
Mary Guillemard <mary.guillemard@collabora.com>
2024-08-02 09:42:33 +0200

    bi: Add bi_foreach_ssa_dest
    Will be used for DCE.
    
    Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>

--- src/panfrost/compiler/compiler.h
+++ src/panfrost/compiler/compiler.h
@@ -1048,8 +1048,12 @@ bi_src_index(nir_src *src)
 #define bi_foreach_dest(ins, v) for (unsigned v = 0; v < ins->nr_dests; ++v)
 
 #define bi_foreach_ssa_src(ins, v)                                             \
-   for (unsigned v = 0; v < ins->nr_srcs; ++v)                                 \
+   bi_foreach_src(ins, v)                                                      \
       if (ins->src[v].type == BI_INDEX_NORMAL)
+
+#define bi_foreach_ssa_dest(ins, v)                                            \
+   bi_foreach_dest(ins, v)                                                     \
+      if (ins->dest[v].type == BI_INDEX_NORMAL)
 
 #define bi_foreach_instr_and_src_in_tuple(tuple, ins, s)                       \
    bi_foreach_instr_in_tuple(tuple, ins)                                       \
