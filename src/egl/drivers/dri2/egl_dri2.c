alexvorxx/Mesa-VirGL

commit c8c46bf90066748d550f492abf6fc03136cde348

egl: fix fd passing on init with zink
this should be passed with gbm but not for any other display type

Fixes: 5ab29555cf0 ("egl/x11: pass render fd to screen create for zink")

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30588>

src/egl/drivers/dri2/egl_dri2.c
@@ -807,7 +807,13 @@ dri2_create_screen(_EGLDisplay *disp)
       }
    }
 
-   int screen_fd = dri2_dpy->swrast ? -1 : dri2_dpy->fd_render_gpu;
+   int screen_fd = -1;
+   if (!dri2_dpy->swrast) {
+#ifdef HAVE_DRM_PLATFORM
+      if (!dri2_dpy->kopper || dri2_dpy->gbm_dri)
+#endif
+          screen_fd = dri2_dpy->fd_render_gpu;
+   }
    dri2_dpy->dri_screen_render_gpu = driCreateNewScreen3(
       0, screen_fd, dri2_dpy->loader_extensions, type,
       &dri2_dpy->driver_configs, false, disp);

upcliujie/mesa

commit 7295c99076152674ce4b15eb0ba8eab7b088de2a

egl: really fix kopper fd passing
for real this time.

Fixes: c8c46bf9006 ("egl: fix fd passing on init with zink")

src/egl/drivers/dri2/egl_dri2.c
@@ -809,10 +809,24 @@ dri2_create_screen(_EGLDisplay *disp)
 
    int screen_fd = -1;
    if (!dri2_dpy->swrast) {
-#ifdef HAVE_DRM_PLATFORM
-      if (!dri2_dpy->kopper || dri2_dpy->gbm_dri)
-#endif
-          screen_fd = dri2_dpy->fd_render_gpu;
+      switch (disp->Platform) {
+      case _EGL_PLATFORM_SURFACELESS:
+      case _EGL_PLATFORM_DEVICE:
+      case _EGL_PLATFORM_DRM:
+      case _EGL_PLATFORM_WAYLAND:
+      case _EGL_PLATFORM_ANDROID:
+         screen_fd = dri2_dpy->fd_render_gpu;
+         break;
+      case _EGL_PLATFORM_X11:
+      case _EGL_PLATFORM_XCB:
+         /* no kopper fd on x11 only */
+         if (!dri2_dpy->kopper)
+            screen_fd = dri2_dpy->fd_render_gpu;
+         break;
+      default:
+         unreachable("uh-oh");
+         break;
+      }
    }
    dri2_dpy->dri_screen_render_gpu = driCreateNewScreen3(
       0, screen_fd, dri2_dpy->loader_extensions, type,